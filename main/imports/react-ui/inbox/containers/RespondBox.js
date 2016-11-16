3235c12a0fa5b3ad70021d81ab4a9e231fa9a409
batamar <batamar.b@nmtec.co>
2016-11-16 17:56:40 +0800

    some comment to message replacement

--- main/imports/react-ui/inbox/containers/RespondBox.js
+++ main/imports/react-ui/inbox/containers/RespondBox.js
@@ -1,11 +1,11 @@
 import { composeWithTracker } from 'react-komposer';
-import { addComment } from '/imports/api/conversations/methods';
+import { addMessage } from '/imports/api/conversations/methods';
 import { RespondBox } from '../components';
 
 
 function composer(props, onData) {
   const sendMessage = (message, callback) => {
-    addComment.call(message, callback);
+    addMessage.call(message, callback);
   };
 
   onData(null, {
