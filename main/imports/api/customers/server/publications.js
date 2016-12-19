7ea8fddc1aba92f9dec02cba8c3d2dbe4a0fc2da
Ganzorig Bayarsaikhan <ganzorig.b@nmma.co>
2016-12-19 16:12:55 +0800

    added pagination on customers list

--- main/imports/api/customers/server/publications.js
+++ main/imports/api/customers/server/publications.js
@@ -1,12 +1,19 @@
+/* eslint-disable new-cap */
+
 import { Meteor } from 'meteor/meteor';
 import { check, Match } from 'meteor/check';
-
+import { Counts } from 'meteor/tmeasday:publish-counts';
 import { Brands } from '/imports/api/brands/brands';
 import { Customers } from '../customers';
 
 
 Meteor.publishComposite('customers.list', function customersList(queryString) {
-  check(queryString, Match.OneOf(undefined, Object)); // eslint-disable-line new-cap
+  check(queryString, {
+    limit: Match.Optional(Number),
+    page: Match.Optional(String),
+  });
+
+  Counts.publish(this, 'customers.list.count', Customers.find(), { noReady: true });
 
   if (!this.userId) {
     return { find() { this.ready(); } };
@@ -14,7 +21,14 @@ Meteor.publishComposite('customers.list', function customersList(queryString) {
 
   return {
     find() {
-      return Customers.find({}, { fields: Customers.publicFields });
+      return Customers.find(
+        {},
+        {
+          fields: Customers.publicFields,
+          sort: { createdAt: -1 },
+          limit: queryString.limit || 0,
+        }
+      );
     },
 
     children: [{
--- main/imports/react-ui/customers/containers/CustomersListContainer.js
+++ main/imports/react-ui/customers/containers/CustomersListContainer.js
@@ -1,17 +1,33 @@
 import { composeWithTracker } from 'react-komposer';
 import { Meteor } from 'meteor/meteor';
+import { FlowRouter } from 'meteor/kadira:flow-router';
+import { Counts } from 'meteor/tmeasday:publish-counts';
 import { Customers } from '/imports/api/customers/customers';
 import { Loader } from '/imports/react-ui/common';
 import { CustomersList } from '../components';
 
 
-function composer(props, onData) {
-  const customersHandle = Meteor.subscribe('customers.list', {});
+function composer({ queryParams }, onData) {
+  let hasMore = false;
+  const CUSTOMERS_PER_PAGE = 15;
+  const pageNumber = parseInt(queryParams.page, 10) || 1;
+  const limit = CUSTOMERS_PER_PAGE * pageNumber;
+  const params = Object.assign({ limit }, queryParams);
+  const customersCount = Counts.get('customers.list.count');
+  const customersHandle = Meteor.subscribe('customers.list', params);
   const integrationsHandle = Meteor.subscribe('integrations.list', {});
   const customers = Customers.find({}, { sort: { lastSeenAt: -1 } }).fetch();
 
+  const loadMore = () => {
+    const qParams = { page: pageNumber + 1 };
+    FlowRouter.setQueryParams(qParams);
+  };
+
+  if (customersCount > pageNumber * CUSTOMERS_PER_PAGE) {
+    hasMore = true;
+  }
   if (customersHandle.ready() && integrationsHandle.ready()) {
-    onData(null, { customers });
+    onData(null, { customers, loadMore, hasMore });
   }
 }
 
