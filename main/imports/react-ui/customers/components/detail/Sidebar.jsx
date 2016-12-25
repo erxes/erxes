b4000c0917cf1d2987331f2034c8a76988ba92b4
Boldkhuu Batbaatar <hi@boldkhuu.com>
2016-12-25 16:05:31 +0800

    customers: contact button

--- main/imports/react-ui/customers/components/detail/Sidebar.jsx
+++ main/imports/react-ui/customers/components/detail/Sidebar.jsx
@@ -1,9 +1,8 @@
 import React, { PropTypes, Component } from 'react';
-import { Label, Collapse } from 'react-bootstrap';
+import { Button, Label, Dropdown, MenuItem } from 'react-bootstrap';
 import moment from 'moment';
-import Alert from 'meteor/erxes-notifier';
 import { Wrapper } from '/imports/react-ui/layout/components';
-import { NameCard, EmptyState, Tagger } from '/imports/react-ui/common';
+import { NameCard } from '/imports/react-ui/common';
 import TaggerSection from './TaggerSection.jsx';
 
 
@@ -27,6 +26,25 @@ class Sidebar extends Component {
 
     return (
       <Wrapper.Sidebar>
+        <div className="action-btn">
+          <Dropdown id="contact-user" pullRight>
+            <Button bsStyle="success">
+              <i className="icon ion-chatbox" /> Send an In App Message
+            </Button>
+            <Dropdown.Toggle bsStyle="success" />
+            <Dropdown.Menu>
+              <MenuItem header>Twitter</MenuItem>
+              <MenuItem eventKey="1">Mention</MenuItem>
+              <MenuItem eventKey="2">Send a direct message</MenuItem>
+              <MenuItem header>Facebook</MenuItem>
+              <MenuItem eventKey="3">Send a message</MenuItem>
+              <MenuItem eventKey="4" divider />
+              <MenuItem eventKey="3">Block</MenuItem>
+              <MenuItem eventKey="3">Delete</MenuItem>
+              <MenuItem eventKey="3">Resubscribe to email</MenuItem>
+            </Dropdown.Menu>
+          </Dropdown>
+        </div>
         <Wrapper.Sidebar.Section>
           <h3>Customer details</h3>
           <ul className="filters no-link">
