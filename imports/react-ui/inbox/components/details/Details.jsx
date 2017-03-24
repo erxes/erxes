5501e1667ca3e68826315736e8f5bd8e86c074fd
Ganzorig Bayarsaikhan <ganzorig.b@nmma.co>
2017-03-24 11:36:49 +0800

    conversation scroll start on bottom

--- imports/react-ui/inbox/components/details/Details.jsx
+++ imports/react-ui/inbox/components/details/Details.jsx
@@ -1,4 +1,4 @@
-import React, { PropTypes } from 'react';
+import React, { PropTypes, Component } from 'react';
 import { Wrapper } from '/imports/react-ui/layout/components';
 import Sidebar from './Sidebar.jsx';
 import RightSidebar from './RightSidebar.jsx';
@@ -15,55 +15,75 @@ const propTypes = {
   setAttachmentPreview: PropTypes.func.isRequired,
 };
 
-function Details(props) {
-  const {
-    conversation,
-    messages,
-    channelId,
-    changeStatus,
-    attachmentPreview,
-    setAttachmentPreview,
-  } = props;
+class Details extends Component {
+  componentDidMount() {
+    this.node.scrollTop = this.node.scrollHeight;
+  }
 
-  const content = (
-    <div className="margined">
-      <Conversation
-        conversation={conversation}
-        messages={messages}
-        attachmentPreview={attachmentPreview}
-      />
-    </div>
-  );
+  componentWillUpdate() {
+    const { node } = this;
+    this.shouldScrollBottom = (node.scrollTop + node.offsetHeight) === node.scrollHeight;
+  }
 
-  const breadcrumb = [
-    { title: 'Inbox', link: '/inbox' },
-    { title: 'Conversation' },
-  ];
+  componentDidUpdate() {
+    if (this.shouldScrollBottom) {
+      this.node.scrollTop = this.node.scrollHeight;
+    }
+  }
 
-  return (
-    <div>
-      <Wrapper
-        header={<Wrapper.Header breadcrumb={breadcrumb} />}
-        leftSidebar={
-          <Sidebar
+  render() {
+    const {
+      conversation,
+      messages,
+      channelId,
+      changeStatus,
+      attachmentPreview,
+      setAttachmentPreview,
+    } = this.props;
+
+    const content = (
+      <div className="scroll-area" ref={(node) => { this.node = node; }}>
+        <div className="margined">
+          <Conversation
             conversation={conversation}
-            messagesCount={messages.length}
-            changeStatus={changeStatus}
-            channelId={channelId}
+            messages={messages}
+            attachmentPreview={attachmentPreview}
           />
-        }
+        </div>
+      </div>
+    );
 
-        content={content}
-        footer={
-          <RespondBox
-            conversation={conversation}
-            setAttachmentPreview={setAttachmentPreview}
-          />
-        }
-        rightSidebar={<RightSidebar conversation={conversation} />}
-      />
-    </div>
-  );
+    const breadcrumb = [
+      { title: 'Inbox', link: '/inbox' },
+      { title: 'Conversation' },
+    ];
+
+    return (
+      <div>
+        <Wrapper
+          header={<Wrapper.Header breadcrumb={breadcrumb} />}
+          leftSidebar={
+            <Sidebar
+              conversation={conversation}
+              messagesCount={messages.length}
+              changeStatus={changeStatus}
+              channelId={channelId}
+            />
+          }
+
+          content={content}
+          footer={
+            <RespondBox
+              conversation={conversation}
+              setAttachmentPreview={setAttachmentPreview}
+            />
+          }
+          rightSidebar={<RightSidebar conversation={conversation} />}
+        />
+      </div>
+    );
+  }
+
 }
 
 Details.propTypes = propTypes;
