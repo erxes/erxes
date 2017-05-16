f0a00e24cc7db967c4de7e2debf4fbce3fed7211
Ganzorig Bayarsaikhan <ganzorig.b@nmma.co>
2017-05-16 15:15:57 +0800

    play sound when new notification. closed issue #149

--- imports/react-ui/layout/components/QuickNavigation.js
+++ imports/react-ui/layout/components/QuickNavigation.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { OverlayTrigger, Dropdown, MenuItem, Popover, Badge } from 'react-bootstrap';
 import { Meteor } from 'meteor/meteor';
@@ -10,52 +10,62 @@ const propTypes = {
   unreadCount: PropTypes.number.isRequired,
 };
 
-function QuickNavigation({ unreadCount }) {
-  const user = Meteor.user();
-  const popoverNotification = (
-    <Popover id="popover-notification" title="Notifications">
-      <NotificationsLatest />
-    </Popover>
-  );
-  return (
-    <nav className="quick-nav">
-      <div className="nav-item">
-        <OverlayTrigger
-          trigger="click"
-          rootClose
-          placement="bottom"
-          containerPadding={15}
-          overlay={popoverNotification}
-        >
-          <div className="action-button">
-            <i className="ion-android-notifications" />
-            <Badge>
-              {unreadCount !== 0 ? unreadCount : null}
-            </Badge>
-          </div>
-        </OverlayTrigger>
-      </div>
-      <div className="nav-item">
-        <Dropdown id="dropdown-user" pullRight>
-          <DropdownToggle bsRole="toggle">
-            <NameCard.Avatar user={user} />
-          </DropdownToggle>
-          <Dropdown.Menu>
-            <NameCard user={user} />
-            <MenuItem divider />
-            <MenuItem href={FlowRouter.path('/settings/profile')}>
-              Edit Profile
-            </MenuItem>
-            <MenuItem href={FlowRouter.path('settings/change-password')}>
-              Change password
-            </MenuItem>
-            <MenuItem divider />
-            <MenuItem href={FlowRouter.path('/logout')}>Sign out</MenuItem>
-          </Dropdown.Menu>
-        </Dropdown>
-      </div>
-    </nav>
-  );
+class QuickNavigation extends Component {
+  componentDidUpdate(prevProps) {
+    if (prevProps.unreadCount < this.props.unreadCount) {
+      const audio = new Audio('/sound/notify.mp3');
+      audio.play();
+    }
+  }
+
+  render() {
+    const unreadCount = this.props.unreadCount;
+    const user = Meteor.user();
+    const popoverNotification = (
+      <Popover id="popover-notification" title="Notifications">
+        <NotificationsLatest />
+      </Popover>
+    );
+    return (
+      <nav className="quick-nav">
+        <div className="nav-item">
+          <OverlayTrigger
+            trigger="click"
+            rootClose
+            placement="bottom"
+            containerPadding={15}
+            overlay={popoverNotification}
+          >
+            <div className="action-button">
+              <i className="ion-android-notifications" />
+              <Badge>
+                {unreadCount !== 0 ? unreadCount : null}
+              </Badge>
+            </div>
+          </OverlayTrigger>
+        </div>
+        <div className="nav-item">
+          <Dropdown id="dropdown-user" pullRight>
+            <DropdownToggle bsRole="toggle">
+              <NameCard.Avatar user={user} />
+            </DropdownToggle>
+            <Dropdown.Menu>
+              <NameCard user={user} />
+              <MenuItem divider />
+              <MenuItem href={FlowRouter.path('/settings/profile')}>
+                Edit Profile
+              </MenuItem>
+              <MenuItem href={FlowRouter.path('settings/change-password')}>
+                Change password
+              </MenuItem>
+              <MenuItem divider />
+              <MenuItem href={FlowRouter.path('/logout')}>Sign out</MenuItem>
+            </Dropdown.Menu>
+          </Dropdown>
+        </div>
+      </nav>
+    );
+  }
 }
 
 QuickNavigation.propTypes = propTypes;
