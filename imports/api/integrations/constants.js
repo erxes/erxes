a41cf534a4878b08ed7e666c204798f8bb6e19aa
batamar <batamar.b@nmtec.co>
2017-05-20 16:33:35 +0800

    allowed empty values in formSchema some fields

--- imports/api/integrations/constants.js
+++ imports/api/integrations/constants.js
@@ -10,12 +10,12 @@ export const FORM_LOAD_TYPES = {
   SHOUTBOX: 'shoutbox',
   POPUP: 'popup',
   EMBEDDED: 'embedded',
-  ALL_LIST: ['shoutbox', 'popup', 'embedded'],
+  ALL_LIST: ['', 'shoutbox', 'popup', 'embedded'],
 };
 
 export const FORM_SUCCESS_ACTIONS = {
   EMAIL: 'email',
   REDIRECT: 'redirect',
   ONPAGE: 'onPage',
-  ALL_LIST: ['email', 'redirect', 'onPage'],
+  ALL_LIST: ['', 'email', 'redirect', 'onPage'],
 };
