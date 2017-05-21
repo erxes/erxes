79b77cbe2f15d8da7421b4402b562fc89cd7f4d1
batamar <batamar.b@nmtec.co>
2017-05-21 16:48:18 +0800

    fixed add messenger form bug

--- imports/react-ui/settings/integrations/containers/Messenger.js
+++ imports/react-ui/settings/integrations/containers/Messenger.js
@@ -1,14 +1,21 @@
+import { Meteor } from 'meteor/meteor';
 import { compose } from 'react-komposer';
+import { Brands } from '/imports/api/brands/brands';
 import { getTrackerLoader, composerOptions } from '/imports/react-ui/utils';
 import { Messenger } from '../components';
-import composer from './commonComposer';
+import { saveCallback } from './utils';
 
-export default compose(
-  getTrackerLoader(
-    composer({
-      addMethodName: 'addMessenger',
-      editMethodName: 'editMessenger',
-    }),
-    composerOptions({ spinner: true }),
-  ),
-)(Messenger);
+const composer = (props, onData) => {
+  const brandsHandler = Meteor.subscribe('brands.list', 0);
+  const brands = Brands.find().fetch();
+
+  const save = doc => saveCallback({ doc }, 'addMessenger', 'editMessenger', props.integration);
+
+  if (brandsHandler.ready()) {
+    return onData(null, { brands, save });
+  }
+
+  return null;
+};
+
+export default compose(getTrackerLoader(composer, composerOptions({ spinner: true })))(Messenger);
