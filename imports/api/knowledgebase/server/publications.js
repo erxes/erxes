b12ea51bf34ddd97e7aeefdd5edd74fdf15bbd0f
Javkhlan Shirendev <javkhlan.sh@nmtec.co>
2017-06-12 19:44:02 +0900

    publications for 'knowledgebase' groups

---
+++ imports/api/knowledgebase/server/publications.js
@@ -0,0 +1,32 @@
+/**
+ * Form publications
+ */
+
+import { Meteor } from 'meteor/meteor';
+import { check, Match } from 'meteor/check';
+import { KbGroups } from '../collections';
+
+// form list
+Meteor.publish('kbgroups.list', limit => {
+  check(limit, Match.Optional(Number));
+
+  return KBGroups.find();
+});
+
+// form detail
+Meteor.publish('kbgroups.detail', id => {
+  check(id, String);
+
+  return KBGroups.find({ createdUser: this.userId, _id: id });
+});
+
+// // form field list
+// Meteor.publish('forms.fieldList', formIds => {
+//   check(formIds, [String]);
+
+//   const selector = {
+//     $and: [{ formId: { $in: formIds } }],
+//   };
+
+//   return Fields.find(selector, { sort: { order: 1 } });
+// });
