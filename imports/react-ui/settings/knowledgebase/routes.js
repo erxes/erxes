3e13fea81023f2bf3091960230c42eb017bcf585
Javkhlan Shirendev <javkhlan.sh@nmtec.co>
2017-06-28 19:20:15 +0900

    refactor

--- imports/react-ui/settings/knowledgebase/routes.js
+++ imports/react-ui/settings/knowledgebase/routes.js
@@ -2,9 +2,7 @@ import React from 'react';
 import { mount } from 'react-mounter';
 import { MainLayout } from '/imports/react-ui/layout/containers';
 import settingsRoute from '../routes';
-
 import { List } from './containers';
-
 import { AddKnowledgeBase } from './components';
 
 const knowledgebase = settingsRoute.group({
@@ -26,64 +24,3 @@ knowledgebase.route('/add', {
     mount(MainLayout, { content: <AddKnowledgeBase /> });
   },
 });
-
-// integrations.route('/add', {
-//   name: 'settings/knowledgebase/add',
-
-//   action() {
-//     mount(MainLayout, { content: <AddKbGroup /> });
-//   },
-// });
-
-// integrations.route('/messenger', {
-//   name: 'settings/integrations/messenger',
-
-//   action() {
-//     mount(MainLayout, { content: <Messenger /> });
-//   },
-// });
-
-// integrations.route('/messenger/appearance/:integrationId', {
-//   name: 'settings/integrations/messenger/appearance',
-
-//   action({ integrationId }) {
-//     mount(MainLayout, {
-//       content: <MessengerAppearance integrationId={integrationId} />,
-//     });
-//   },
-// });
-
-// integrations.route('/messenger/availability/:integrationId', {
-//   name: 'settings/integrations/messenger/availability',
-
-//   action({ integrationId }) {
-//     mount(MainLayout, {
-//       content: <MessengerAvailability integrationId={integrationId} />,
-//     });
-//   },
-// });
-
-// // twitter ===========
-// integrations.route('/twitter', {
-//   name: 'settings/integrations/twitter',
-
-//   action() {
-//     mount(MainLayout, { content: <Twitter type="link" /> });
-//   },
-// });
-
-// integrations.route('/add', {
-//   name: 'settings/integrations/add',
-
-//   action() {
-//     mount(MainLayout, { content: <AddIntegration /> });
-//   },
-// });
-
-// integrations.route('/oauth/twitter_callback', {
-//   name: 'settings/integrations/twitter/oauth/callback',
-
-//   action() {
-//     mount(MainLayout, { content: <Twitter type="form" /> });
-//   },
-// });
