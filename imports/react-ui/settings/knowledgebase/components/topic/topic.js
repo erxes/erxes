f0e5747afff1b4bc99a5b5eb0deaadda72d8e18c
Javkhlan Shirendev <javkhlan.sh@nmtec.co>
2017-07-31 06:33:55 +0900

    update

--- imports/react-ui/settings/knowledgebase/components/topic/topic.js
+++ imports/react-ui/settings/knowledgebase/components/topic/topic.js
@@ -9,15 +9,59 @@ import {
 } from 'react-bootstrap';
 import ReactMarkdown from 'react-markdown';
 import SelectBrand from '../SelectBrand';
+import CopyToClipboard from 'react-copy-to-clipboard';
 import { CommonItem } from '../common';
 
 class KbTopic extends CommonItem {
   constructor(props, context) {
     super(props, context);
 
+    let code = '';
+
+    // showed install code automatically in edit mode
+    if (props.item) {
+      code = this.constructor.getInstallCode(props.item._id);
+    }
+
     this.state = {
+      code,
       copied: false,
     };
+
+    this.handleBrandChange = this.handleBrandChange.bind(this);
+  }
+
+  handleBrandChange() {
+    if (this.props.item && this.props.item._id) {
+      const code = this.constructor.getInstallCode(this.props.item._id);
+      this.setState({ code, copied: false });
+    }
+  }
+
+  static installCodeIncludeScript(type) {
+    return `
+      (function() {
+        var script = document.createElement('script');
+        script.src = "${Meteor.settings.public.CDN_HOST}/${type}Widget.bundle.js";
+        script.async = true;
+
+        var entry = document.getElementsByTagName('script')[0];
+        entry.parentNode.insertBefore(script, entry);
+      })();
+    `;
+  }
+
+  static getInstallCode(topicId) {
+    return `
+      <script>
+        window.erxesSettings = {
+          messenger: {
+            topic_id: "${topicId}"
+          },
+        };
+        ${KbTopic.installCodeIncludeScript('knowledgeBase')}
+      </script>
+    `;
   }
 
   render() {
@@ -36,8 +80,26 @@ class KbTopic extends CommonItem {
           <FormControl type="text" defaultValue={item.description} />
         </FormGroup>
 
-        <SelectBrand brands={brands} defaultValue={item.brandId} />
+        <SelectBrand
+          brands={brands}
+          defaultValue={item.brandId}
+          onChange={this.handleBrandChange}
+        />
 
+        {this.renderInstallCode()}
+
+        <Modal.Footer>
+          <ButtonToolbar className="pull-right">
+            <Button type="submit" bsStyle="primary">Save</Button>
+          </ButtonToolbar>
+        </Modal.Footer>
+      </form>
+    );
+  }
+
+  renderInstallCode() {
+    if (this.props.item && this.props.item._id) {
+      return (
         <FormGroup controlId="install-code">
           <ControlLabel>Install code</ControlLabel>
           <div className="markdown-wrapper">
@@ -54,14 +116,10 @@ class KbTopic extends CommonItem {
               : null}
           </div>
         </FormGroup>
-
-        <Modal.Footer>
-          <ButtonToolbar className="pull-right">
-            <Button type="submit" bsStyle="primary">Save</Button>
-          </ButtonToolbar>
-        </Modal.Footer>
-      </form>
-    );
+      );
+    } else {
+      return null;
+    }
   }
 
   handleSubmit(e) {
