3fe9b023d9e457f9421183fdd6fec50a3bd3a13d
Javkhlan Shirendev <javkhlan.sh@nmtec.co>
2017-08-11 16:55:12 +0900

    import fix

--- imports/startup/server/register-api.js
+++ imports/startup/server/register-api.js
@@ -10,7 +10,6 @@ import '/imports/api/brands/server/publications';
 import '/imports/api/brands/methods';
 
 import '/imports/api/customers/server/publications';
-import '/imports/api/customers/server/methods';
 import '/imports/api/customers/methods';
 
 import '/imports/api/channels/server/publications';
