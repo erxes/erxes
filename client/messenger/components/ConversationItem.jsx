ed161d74d5b6529efd2f1691680d19f616f09036
Ganzorig <ganzorig.b@nmma.co>
2017-09-01 14:27:55 +0800

    changed default avatar url. issue closed #31

--- client/messenger/components/ConversationItem.jsx
+++ client/messenger/components/ConversationItem.jsx
@@ -13,7 +13,7 @@ const propTypes = {
 function ConversationItem({ conversation, notificationCount, goToConversation }) {
   const { _id, content, createdAt } = conversation;
   const participatedUser = conversation.participatedUsers[0];
-  const avatar = (participatedUser && participatedUser.details.avatar) || 'https://crm.nmma.co/images/userDefaultIcon.png';
+  const avatar = (participatedUser && participatedUser.details.avatar) || '/static/images/user.png';
   const fullName = (participatedUser && participatedUser.details.fullName) || 'Support staff';
 
   return (
