21a19eebc58bade7716c4130b1fb2f3ee759efcf
batamar <batamar.b@nmtec.co>
2017-09-10 17:35:38 +0800

    removed publication files

--- imports/startup/server/register-api.js
+++ imports/startup/server/register-api.js
@@ -6,35 +6,16 @@ import '/imports/api/users/server/methods';
 
 import '/imports/api/server/methods';
 
-import '/imports/api/brands/server/publications';
 import '/imports/api/brands/server/methods';
-
-import '/imports/api/customers/server/publications';
 import '/imports/api/customers/server/methods';
-
-import '/imports/api/channels/server/publications';
 import '/imports/api/channels/server/methods';
-
-import '/imports/api/integrations/server/publications';
 import '/imports/api/integrations/server/methods';
 import '/imports/api/integrations/social/server';
-
-import '/imports/api/conversations/server/publications';
 import '/imports/api/conversations/server/methods';
 import '/imports/api/conversations/server/cronJobs';
-
-import '/imports/api/tags/server/publications';
 import '/imports/api/tags/server/methods';
-
-import '/imports/api/forms/server/publications';
 import '/imports/api/forms/server/methods';
-
-import '/imports/api/responseTemplates/server/publications';
 import '/imports/api/responseTemplates/server/methods';
-
-import '/imports/api/emailTemplates/server/publications';
 import '/imports/api/emailTemplates/methods';
-
-import '/imports/api/engage/server/publications';
 import '/imports/api/engage/server/methods';
 import '/imports/api/engage/server/cronJobs';
