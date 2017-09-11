d6ec5ac88fdc0e206aa4dbbfc405233ff2c64c89
Ganzorig Bayarsaikhan <ganzorig.b@nmma.co>
2017-09-11 13:45:16 +0800

    not working on safari automatically scroll issue closed #209

--- imports/react-ui/layout/components/PageContent.js
+++ imports/react-ui/layout/components/PageContent.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
 const propTypes = {
   actionBar: PropTypes.node,
   footer: PropTypes.node,
   children: PropTypes.node,
+  relative: PropTypes.bool,
 };
 
-function PageContent({ actionBar, footer, children }) {
+function PageContent({ actionBar, footer, children, relative }) {
+  const contentClassName = classNames('content', { 'relative-content': relative });
+
   return (
     <div className="main-content">
       {actionBar}
-      <div className="content">{children}</div>
+      <div className={contentClassName}>{children}</div>
       {footer ? <div className="footer">{footer}</div> : false}
     </div>
   );
