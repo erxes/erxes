f629b96214300c66dc150da5abb0a47679455498
batamar <batamar.b@nmtec.co>
2017-09-25 13:04:37 +0800

    Add comments

--- src/data/resolvers/queries/tags.js
+++ src/data/resolvers/queries/tags.js
@@ -1,6 +1,12 @@
 import { Tags } from '../../../db/models';
 
 export default {
+  /**
+   * Tags list
+   * @param {Object} args
+   * @param {Strign} args.type
+   * @return {Promise} filtered tag objects by type
+   */
   tags(root, { type }) {
     return Tags.find({ type });
   },
--- src/data/resolvers/queries/users.js
+++ src/data/resolvers/queries/users.js
@@ -1,6 +1,12 @@
 import { Users } from '../../../db/models';
 
 export default {
+  /**
+   * Users list
+   * @param {Object} args
+   * @param {Integer} args.limit
+   * @return {Promise} sorted and filtered users objects
+   */
   users(root, { limit }) {
     const users = Users.find({});
     const sort = { username: 1 };
@@ -12,10 +18,20 @@ export default {
     return users.sort(sort);
   },
 
+  /**
+   * Get one user
+   * @param {Object} args
+   * @param {String} args._id
+   * @return {Promise} found user
+   */
   userDetail(root, { _id }) {
     return Users.findOne({ _id });
   },
 
+  /**
+   * Get all users count. We will use it in pager
+   * @return {Promise} total count
+   */
   usersTotalCount() {
     return Users.find({}).count();
   },
