326afabdea8efb2e54d0d9a46f4c9fb919974bbc
Javkhlan Shirendev <javkhlan.sh@nmtec.co>
2017-09-25 17:46:45 +0900

    #28 removed unnecessary files

--- client/knowledgebase/containers/Message.js
+++
@@ -1,9 +0,0 @@
-import { connect } from 'react-redux';
-import { connection } from '../connection';
-import { Message } from '../components';
-
-const mapStateToProps = () => ({
-  color: connection.data.uiOptions && connection.data.uiOptions.color,
-});
-
-export default connect(mapStateToProps)(Message);
