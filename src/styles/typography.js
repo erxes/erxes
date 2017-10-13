3914f65750854c948ba233d4ca5e8cb866a95104
Ganzorig Bayarsaikhan <ganzorig.b@nmma.co>
2017-10-13 11:16:54 +0800

    Add some typhographys on storybook

---
+++ src/styles/typography.js
@@ -0,0 +1,27 @@
+export const fontWeightLight = 300;
+export const fontWeightRegular = 500;
+export const fontWeightMedium = 600;
+
+export const fontSizeHeading1 = 64;
+export const fontSizeHeading2 = 48;
+export const fontSizeHeading3 = 40;
+export const fontSizeHeading4 = 32;
+export const fontSizeHeading5 = 24;
+export const fontSizeHeading6 = 20;
+export const fontSizeHeading7 = 16;
+export const fontSizeHeading8 = 11;
+
+export const fontSizeBody = 13;
+export const fontSizeUppercase = 11;
+
+export const lineHeightHeading1 = (72 / fontSizeHeading1);
+export const lineHeightHeading2 = (56 / fontSizeHeading2);
+export const lineHeightHeading3 = (48 / fontSizeHeading3);
+export const lineHeightHeading4 = (40 / fontSizeHeading4);
+export const lineHeightHeading5 = (32 / fontSizeHeading5);
+export const lineHeightHeading6 = (28 / fontSizeHeading6);
+export const lineHeightHeading7 = (24 / fontSizeHeading7);
+export const lineHeightHeading8 = (16 / fontSizeHeading8);
+
+export const lineHeightBody = (36 / fontSizeBody);
+export const lineHeightUppercase = (11 / fontSizeUppercase);
--- stories/index.js
+++ stories/index.js
@@ -2,7 +2,15 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withKnobs, text, select } from "@storybook/addon-knobs";
 
+import Typography from "./Typography";
 import Button from "../src/components/Button";
+
+storiesOf('Typography', module)
+  .addDecorator(withKnobs)
+  .add("Primary", () => (
+    <Typography />
+  ));
+
 
 storiesOf('Button', module)
   .addDecorator(withKnobs)
