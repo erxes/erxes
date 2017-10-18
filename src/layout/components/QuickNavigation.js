d394ef84cb1ed0911b8eb7f5575ba59eaa5f3844
Ganzorig Bayarsaikhan <ganzorig.b@nmma.co>
2017-10-18 09:16:36 +0800

    Add quicknavigation

--- src/layout/components/QuickNavigation.js
+++ src/layout/components/QuickNavigation.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import styled from "styled-components"
 import { colors, dimensions } from '../../styles';
 import { rgba } from '../../utils/color';
@@ -7,14 +6,14 @@ import { rgba } from '../../utils/color';
 const UserHelper = styled.div`
   padding: 0 ${dimensions.coreSpacing}px;
   float: right;
-  background: ${rgba(colors.colorWhite, 0.2)};
+  background: ${rgba(colors.colorWhite, 0.1)};
 `;
 
 const propTypes = {
 
 }
 
-const QuickNavigation = (props) => {
+const QuickNavigation = () => {
   return (
     <UserHelper>Ganzorig</UserHelper>
   )
