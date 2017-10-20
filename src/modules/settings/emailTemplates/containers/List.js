c93f574b39a346bc563e92a044cce9b42a75c57d
batamar <batamar.b@nmtec.co>
2017-10-21 01:40:16 +0800

    Use email template mutations

--- src/modules/settings/emailTemplates/containers/List.js
+++ src/modules/settings/emailTemplates/containers/List.js
@@ -2,6 +2,16 @@ import { gql, graphql } from 'react-apollo';
 import { commonListComposer } from '../../utils';
 import { List } from '../components';
 
+const commonParamsDef = `
+  $name: String!,
+  $content: String,
+`;
+
+const commonParams = `
+  name: $name,
+  content: $content,
+`;
+
 export default commonListComposer({
   name: 'emailTemplates',
 
@@ -35,6 +45,43 @@ export default commonListComposer({
     `,
     {
       name: 'totalCountQuery'
+    }
+  ),
+
+  gqlAddMutation: graphql(
+    gql`
+      mutation emailTemplatesAdd(${commonParamsDef}) {
+        emailTemplatesAdd(${commonParams}) {
+          _id
+        }
+      }
+    `,
+    {
+      name: 'addMutation'
+    }
+  ),
+
+  gqlEditMutation: graphql(
+    gql`
+      mutation emailTemplatesEdit($_id: String!, ${commonParamsDef}) {
+        emailTemplatesEdit(_id: $_id, ${commonParams}) {
+          _id
+        }
+      }
+    `,
+    {
+      name: 'editMutation'
+    }
+  ),
+
+  gqlRemoveMutation: graphql(
+    gql`
+      mutation emailTemplatesRemove($_id: String!) {
+        emailTemplatesRemove(_id: $_id)
+      }
+    `,
+    {
+      name: 'removeMutation'
     }
   ),
 
