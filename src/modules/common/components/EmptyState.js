653073b2a72e451593fdd520ad0b61b976c885ee
Anu-ujin <anu.b@nmma.co>
2017-10-24 20:49:59 +0800

    Move Loader to styled-component

--- src/modules/common/components/EmptyState.js
+++ src/modules/common/components/EmptyState.js
@@ -1,30 +1,64 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Button } from 'react-bootstrap';
+import styled, { css } from 'styled-components';
+import Button from './Button';
+import { colors } from '../styles';
 
-const propTypes = {
+const EmptyStateStyled = styled.div`
+  ${props => css`
+    display: flex;
+    flex-direction: column;
+    height: ${props.esSize === 'small' ? '80px' : '100%'};
+    min-height: ${props.esSize === 'small' && '80px'};
+    justify-content: center;
+    text-align: center;
+    font-size: ${props.esSize === 'small' ? '12px' : '16px'};
+    padding: ${props.esSize === 'small' ? '10px 20px' : '20px'};
+    color: ${colors.colorCoreGray};
+    background: ${colors.colorWhite};
+    position: ${props.esSize === 'full' && 'absolute'};
+    bottom: ${props.esSize === 'full' && 0};
+    top: ${props.esSize === 'full' && 0};
+    left: ${props.esSize === 'full' && 0};
+    right: ${props.esSize === 'full' && 0};
+    z-index: ${props.esSize === 'full' && 0};
+  `};
+
+  > i {
+    font-size: ${props => (props.esSize === 'small' ? '28px' : '62px')};
+    line-height: ${props => (props.esSize === 'small' ? '40px' : '78px')};
+    color: ${colors.colorShadowGray};
+  }
+
+  > a {
+    margin-top: 10px;
+  }
+`;
+
+EmptyState.propTypes = {
   text: PropTypes.string.isRequired,
   icon: PropTypes.object.isRequired,
-  size: PropTypes.string,
+  esSize: PropTypes.oneOf(['full', 'small']),
   linkUrl: PropTypes.string,
   linkText: PropTypes.string
 };
 
-function EmptyState({ text, icon, size, linkUrl, linkText }) {
-  const classNames = `empty-state ${size}`;
+function EmptyState({ text, icon, esSize, linkUrl, linkText }) {
   return (
-    <div className={classNames}>
+    <EmptyStateStyled esSize={esSize}>
       {icon}
       {text}
       {linkUrl && linkText ? (
-        <Button bsSize="small" href={linkUrl}>
+        <Button btnStyle="simple" size="small" href={linkUrl}>
           {linkText}
         </Button>
       ) : null}
-    </div>
+    </EmptyStateStyled>
   );
 }
 
-EmptyState.propTypes = propTypes;
+EmptyState.defaultProps = {
+  esSize: 'small'
+};
 
 export default EmptyState;
