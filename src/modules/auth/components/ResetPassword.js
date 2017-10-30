59e5684f2a4488a74d887b33b4b8792a962e20e0
Ganzorig Bayarsaikhan <ganzorig.b@nmma.co>
2017-10-30 22:36:00 +0800

    Revert "Redesign ui xangai"

--- src/modules/auth/components/ResetPassword.js
+++ src/modules/auth/components/ResetPassword.js
@@ -1,10 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { FormGroup, FormControl, Col, Grid } from 'react-bootstrap';
+import { FormGroup, FormControl } from 'react-bootstrap';
 import { Button } from 'modules/common/components';
-import { Alert } from 'modules/common/utils';
-import { AuthContent, AuthDescription, AuthBox } from '../styles';
-
 
 const propTypes = {
   resetPassword: PropTypes.func.isRequired
@@ -33,39 +30,22 @@ class ResetPassword extends Component {
 
   render() {
     return (
-      <AuthContent className="auth-content">
-        <Grid>
-          <Col md={7}>
-            <AuthDescription>
-              <img src="/images/logo.png" alt="erxes" />
-              <h1>Customer engagement. Redefined.</h1>
-              <p>
-                erxes is an AI meets open source messaging platform for sales
-                and marketing teams.
-              </p>
-              <a href="http://erxes.io/">« Go to home page</a>
-            </AuthDescription>
-          </Col>
-          <Col md={5}>
-            <AuthBox>
-              <h2>Set your new password</h2>
-              <form onSubmit={this.handleSubmit}>
-                <FormGroup>
-                  <FormControl
-                    type="password"
-                    placeholder="new password"
-                    required
-                    onChange={this.handlePasswordChange}
-                  />
-                </FormGroup>
-                <Button type="submit" block>
-                  Change password
-                </Button>
-              </form>
-            </AuthBox>
-          </Col>
-        </Grid>
-      </AuthContent>
+      <div className="auth-box">
+        <h2>Set your new password</h2>
+        <form onSubmit={this.handleSubmit}>
+          <FormGroup>
+            <FormControl
+              type="password"
+              placeholder="new password"
+              required
+              onChange={this.handlePasswordChange}
+            />
+          </FormGroup>
+          <Button type="submit" block>
+            Change password
+          </Button>
+        </form>
+      </div>
     );
   }
 }
