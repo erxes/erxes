a16f6b88f0bb1693b182e5df6067ba4a346603c6
Ganzorig Bayarsaikhan <ganzorig.b@nmma.co>
2017-11-23 16:36:23 +0800

    Remove console log

--- src/modules/inbox/components/Inbox.js
+++ src/modules/inbox/components/Inbox.js
@@ -70,7 +70,6 @@ class Inbox extends Component {
   }
 
   renderRightSidebar(currentConversation) {
-    console.log(currentConversation);
     if (currentConversation._id) {
       return (
         <EditInformation
