06481600d7d67c5a0d361a076c2a0fbb309619e9
Khangarid Bayarsaikhan <khangarid.b@nmma.com>
2017-11-27 18:40:42 +0800

    Merged internalnoterow function

--- src/modules/activityLogs/components/InternalNotes.js
+++ src/modules/activityLogs/components/InternalNotes.js
@@ -10,33 +10,27 @@ import {
   ActivityContent
 } from 'modules/activityLogs/styles';
 
-const InternalNoteRow = props => {
-  const { data } = props;
-
-  return data.list.map(item => {
-    if (item.action !== 'internal_note-create') return null;
-    return (
-      <ActivityRow key={item.id}>
-        <ActivityWrapper>
-          <AvatarWrapper>
-            <NameCard.Avatar user={item.createdUser} size={50} />
-          </AvatarWrapper>
-          <ActivityCaption>{item.by.details.fullName}</ActivityCaption>
-          <div>{moment(item.createdAt).fromNow()}</div>
-        </ActivityWrapper>
-
-        <ActivityContent>{item.content}</ActivityContent>
-      </ActivityRow>
-    );
-  });
-};
-
 const InternalNotes = props => {
   const { activityLog } = props;
 
-  return activityLog.map((item, index) => (
-    <InternalNoteRow key={index} data={item} />
-  ));
+  return activityLog.map(item =>
+    item.list.map(item => {
+      if (item.action !== 'internal_note-create') return null;
+      return (
+        <ActivityRow key={item.id}>
+          <ActivityWrapper>
+            <AvatarWrapper>
+              <NameCard.Avatar user={item.createdUser} size={50} />
+            </AvatarWrapper>
+            <ActivityCaption>{item.by.details.fullName}</ActivityCaption>
+            <div>{moment(item.createdAt).fromNow()}</div>
+          </ActivityWrapper>
+
+          <ActivityContent>{item.content}</ActivityContent>
+        </ActivityRow>
+      );
+    })
+  );
 };
 
 InternalNotes.propTypes = {
