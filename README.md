afdb93ecce93b04fe49f4c69cbc92156eef0fea9
Munkhbold <munkhbold.d@nmtec.co>
2017-11-29 12:31:17 +0800

    add installation shell & initial data

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # API for erxes apps
+
+
+## Installation
+
+This repository is the main web app of the erxes platform that consists of 2 other repositories:
+
+- [Flatform](https://github.com/erxes/erxes)
+- [Widgets](https://github.com/erxes/erxes-widgets)
+
+Clone erxes repository and install its dependencies:
+```Shell
+git clone https://github.com/erxes/erxes-app-api
+cd erxes-app-api
+yarn install
+```
+
+Linux
+```Shell
+sudo apt-get install redis-server
+```
+
+Mac OS
+```Shell
+brew install redis
+```
+
+Run redis server
+```Shell
+redis-server
+```
+
+Create `.env.sample` from default settings file and configure it on your own:
+```Shell
+cp .env.sample .env
+```
+
+To start the app:
+```Shell
+yarn dev
+```
---
+++ install.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+## Installation
+
+if [ ! -d .git ]; then
+  echo 'Clone erxes repository and install its dependencies:'
+  git clone https://github.com/erxes/erxes-app-api
+  cd erxes-app-api
+  yarn install
+fi
+
+
+if [ "$UNAME" = "Linux" ] ; then
+  ### Linux ###
+  sudo apt-get install redis-server
+fi
+if [ "$UNAME" = "Darwin" ] ; then
+  ### Mac OS ###
+  brew install redis
+fi
+
+echo 'Load initial data';
+yarn loadInitData
+
+echo 'Run redis server';
+redis-server &
+
+echo 'Create `.env.sample` from default settings file and configure it on your own:'
+cp .env.sample .env
+
+CURRENT_FOLDER=${PWD##*/}
+if ['$CURRENT_FOLDER' = 'erxes-app-api']; then
+  cd ..
+fi
+
+curl https://github.com/erxes/erxes-widgets/blob/feature-company/install.sh | sh
