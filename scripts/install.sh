099917b9d481ee092932ace7137b8330fb25beac
Munkhbold <munkhbold.d@nmtec.co>
2017-11-30 15:12:54 +0800

    add node & mongodb in installation script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,14 +1,52 @@
 #!/bin/sh
 
-
 UNAME=$(uname)
 
 if [ "$UNAME" = "Darwin" ] ; then
   ### OSX ###
-  brew install yarn
+
+  if type node > /dev/null ; then
+    brew install node
+  fi
+
+  # mongodb
+  if ! type mongo > /dev/null ; then
+    brew update
+    brew install mongodb
+    mkdir -p /data/db
+    chmod 777 /data/db
+    mongod
+  fi
+
+  # yarn
+  if ! type yarn > /dev/null ; then
+    brew install yarn
+  fi
+
 elif [ "$UNAME" = "Linux" ] ; then
   ### Linux ###
-  sudo apt-get install yarn
+
+  # nodejs
+  if ! type node > /dev/null ; then
+    curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+    sudo apt-get install -y nodejs
+  fi
+
+  # mongodb
+  if ! type mongo > /dev/null ; then
+    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
+    echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
+    sudo apt-get update
+    sudo apt-get install -y mongodb-org
+    sudo service mongod start
+  fi
+
+  # yarn
+  if ! type yarn > /dev/null ; then
+    curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+    echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+    sudo apt-get update && sudo apt-get install yarn
+  fi
 fi
 
 if [ ! -d .git ]; then
@@ -28,5 +66,4 @@ if [ $CURRENT_FOLDER = 'erxes' ]; then
 fi
 
 echo 'Install erxes-app-api'
-
 curl https://raw.githubusercontent.com/erxes/erxes-app-api/feature-graphql-mutations-mu/scripts/install.sh | sh
