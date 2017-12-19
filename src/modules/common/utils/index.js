b345271fc129c82009c7d17174e38e463f59e48d
Khangarid Bayarsaikhan <khangarid.b@nmma.com>
2017-12-19 17:13:24 +0800

    Added renderfullname on utils

--- src/modules/common/utils/index.js
+++ src/modules/common/utils/index.js
@@ -5,4 +5,19 @@ import toggleCheckBoxes from './toggleCheckBoxes';
 import Rotate from './animateRotate';
 import confirm from './confirmation/confirm';
 
-export { Alert, uploadHandler, router, Rotate, confirm, toggleCheckBoxes };
+const renderFullName = data => {
+  if (data.firstName || data.lastName) {
+    return (data.firstName || '') + ' ' + (data.lastName || '');
+  }
+  return data.email || data.phone || 'N/A';
+};
+
+export {
+  Alert,
+  uploadHandler,
+  router,
+  Rotate,
+  confirm,
+  toggleCheckBoxes,
+  renderFullName
+};
