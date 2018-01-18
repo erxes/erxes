167174aba054f489a4a3f5ddf6befd9b70f0d6b2
batamar <batamar.b@nmtec.co>
2018-01-18 20:58:17 +0800

    Change find location service

--- client/utils.js
+++ client/utils.js
@@ -2,7 +2,8 @@ export const getBrowserInfo = async () => {
   let location;
 
   try {
-    const response = await fetch('https://ipinfo.io/json');
+    const response = await fetch('https://freegeoip.net/json/');
+
     location = await response.json();
   } catch (e) {
     console.log(e.message); // eslint-disable-line
@@ -11,9 +12,9 @@ export const getBrowserInfo = async () => {
 
   return {
     remoteAddress: location.ip,
-    region: location.region,
+    region: location.region_name,
     city: location.city,
-    country: location.country,
+    country: location.country_name,
     url: location.pathname, // eslint-disable-line
     hostname: location.origin, // eslint-disable-line
     language: navigator.language, // eslint-disable-line
