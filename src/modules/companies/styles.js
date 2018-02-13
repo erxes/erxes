daa8eb1b718898f39f2aa31af301c9b1ae4e1559
Hangarid <boljmor_garid@yahoo.com>
2018-02-13 13:55:18 +0800

    companies table wrapper update

--- src/modules/companies/styles.js
+++ src/modules/companies/styles.js
@@ -43,6 +43,7 @@ const CustomersWrapper = styled.div`
 const CompaniesTableWrapper = styled.div`
   td {
     max-width: 250px;
+
     overflow: hidden;
     text-overflow: ellipsis;
     white-space: nowrap;
