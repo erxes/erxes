136c8e904e53ca0ec55b0fb7cdd1687f39ff5112
Hangarid <boljmor_garid@yahoo.com>
2018-02-13 13:55:49 +0800

    companies table wrapper spacing

--- src/modules/companies/styles.js
+++ src/modules/companies/styles.js
@@ -43,7 +43,6 @@ const CustomersWrapper = styled.div`
 const CompaniesTableWrapper = styled.div`
   td {
     max-width: 250px;
-
     overflow: hidden;
     text-overflow: ellipsis;
     white-space: nowrap;
