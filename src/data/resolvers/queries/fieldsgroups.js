dbaf46d552ea29d9e042912f74ccda9c1f15c125
Khangarid Bayarsaikhan <khangarid.b@nmma.com>
2018-02-21 12:48:53 +0800

    Added visibleonly on fieldsgroups query

--- src/data/resolvers/queries/fieldsgroups.js
+++ src/data/resolvers/queries/fieldsgroups.js
@@ -4,14 +4,22 @@ import { moduleRequireLogin } from '../../permissions';
 const fieldsGroupQueries = {
   /**
    * Fields list
-   * @param {Object} args
-   * @return {Promise} sorted fields list
+   * @param {String} contentType - Sort by content type
+   * @param {Boolean} visibleOnly - Visible only fields
+   *
+   * @return {Promise} sorted fields group list
    */
-  fieldsgroups(root, { contentType }) {
+  fieldsgroups(root, { contentType, visibleOnly }) {
     const query = {};
 
+    // querying by content type
     if (contentType) {
       query.contentType = contentType;
+    }
+
+    // retreiving visible only groups
+    if (visibleOnly) {
+      query.visible = visibleOnly;
     }
 
     return FieldsGroups.find(query).sort({ order: 1 });
