08838d452717c4dcf0179a0fbbd6abc8a539275e
Khangarid Bayarsaikhan <khangarid.b@nmma.com>
2018-02-21 12:36:24 +0800

    Added checkIsDefinedByErxes method, fixed variable names

--- src/db/models/FieldsGroups.js
+++ src/db/models/FieldsGroups.js
@@ -18,15 +18,34 @@ const FieldGroupSchema = mongoose.Schema({
 });
 
 class FieldGroup {
+  /** Check if Group is defined by erxes by default
+   *
+   * @param {String} _id - Id of group object to check
+   *
+   * @return {Promise} Newly created Group
+   */
+  static async checkIsDefinedByErxes(_id) {
+    const groupObj = this.findOne({ _id });
+
+    // Checking if the group is defined by the erxes
+    if (groupObj.isDefinedByErxes) throw new Error('Cant update this group');
+  }
+
   /** Create new field group
    *
    * @param {String} name - Group name to be created with
-   * @param {String} nestedUnder - Id of parent group
-   * @param {Number} order - Order number of groups
+   * @param {String} contentType - Content type
+   * @param {String} description - Group description
    *
-   * @return {Promise} - Newly created Group
+   * @return {Promise} Newly created Group
    */
-  static async createFieldsGroup({ name, contentType, description, visible, order }) {
+  static async createFieldsGroup({ name, contentType, description }) {
+    // Newly created group must be visible
+    const visible = true;
+
+    // Automatically setting order of group to the bottom
+    const order = (await this.count({})) + 1;
+
     return this.create({
       name,
       contentType,
@@ -40,10 +59,15 @@ class FieldGroup {
    * Update field group
    * @param {String} _id - Field group id to update
    * @param {Object} doc - Field values to update
+   * @param {String} doc.name - Field group name
+   * @param {String} doc.description - Field group description
    *
    * @return {Promise} Updated field object
    */
   static async updateFieldsGroup(_id, doc) {
+    // Can not edit group that is defined by erxes
+    await this.checkIsDefinedByErxes(_id);
+
     await this.update({ _id }, { $set: doc });
 
     return this.findOne({ _id });
@@ -60,6 +84,9 @@ class FieldGroup {
 
     if (!groupObj) throw new Error(`Group not found with id of ${_id}`);
 
+    // Can not delete group that is defined by erxes
+    await this.checkIsDefinedByErxes(_id);
+
     groupObj.remove();
 
     return _id;
@@ -73,6 +100,9 @@ class FieldGroup {
    * @return {Promise} Result
    */
   static async updateFieldsGroupVisible(_id, visible) {
+    // Can not update group that is defined by erxes
+    await this.checkIsDefinedByErxes(_id);
+
     // Updating visible
     await this.update({ _id }, { $set: { visible } });
 
@@ -87,6 +117,9 @@ class FieldGroup {
    * @return {Promise} Result
    */
   static async updateFieldsGroupOrder(_id, order) {
+    // Can not update group that is defined by erxes
+    await this.checkIsDefinedByErxes(_id);
+
     // Updating order
     await this.update({ _id }, { $set: { order } });
 
