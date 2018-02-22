1772f09f7009d341e26e18da949b0ee4d9b9c4f5
Khangarid Bayarsaikhan <khangarid.b@nmma.co>
2018-02-22 19:12:36 +0800

    Added fields group db model, mutations, queries test

---
+++ src/__tests__/fieldGroupDb.test.js
@@ -0,0 +1,119 @@
+/* eslint-env jest */
+/* eslint-disable no-underscore-dangle */
+
+import { connect, disconnect } from '../db/connection';
+import { Fields, FieldsGroups } from '../db/models';
+import { FIELDS_GROUPS_CONTENT_TYPES } from '../data/constants';
+import { fieldGroupFactory, userFactory } from '../db/factories';
+
+beforeAll(() => connect());
+
+afterAll(() => disconnect());
+
+/**
+ * Fields groups related tests
+ */
+describe('Fields', () => {
+  let _fieldGroup;
+
+  beforeEach(async () => {
+    // creating field group
+    _fieldGroup = await fieldGroupFactory({
+      contentType: FIELDS_GROUPS_CONTENT_TYPES.CUSTOMER,
+      order: 1,
+      isDefinedByErxes: true,
+    });
+  });
+
+  afterEach(async () => {
+    // Clearing test data
+    await Fields.remove({});
+  });
+
+  test('Create group', async () => {
+    expect.assertions(4);
+    const user = await userFactory({});
+
+    const doc = {
+      name: 'Name',
+      description: 'Description',
+      contentType: FIELDS_GROUPS_CONTENT_TYPES.CUSTOMER,
+      lastUpdatedBy: user._id,
+    };
+
+    const groupObj = await FieldsGroups.createFieldsGroup(doc);
+
+    expect(groupObj.name).toBe(doc.name);
+    expect(groupObj.description).toBe(doc.description);
+    expect(groupObj.contentType).toBe(doc.contentType);
+    expect(groupObj.lastUpdatedBy).toBe(doc.lastUpdatedBy);
+  });
+
+  test('Update group', async () => {
+    expect.assertions(4);
+    const user = await userFactory({});
+    const fieldGroup = await fieldGroupFactory({});
+
+    const doc = {
+      name: 'test name',
+      description: 'test description',
+      lastUpdatedBy: user._id,
+    };
+
+    try {
+      await FieldsGroups.updateFieldsGroup(_fieldGroup._id, doc);
+    } catch (e) {
+      expect(e.message).toBe('Cant update this group');
+    }
+
+    const groupObj = await FieldsGroups.updateFieldsGroup(fieldGroup._id, doc);
+
+    expect(groupObj.name).toBe(doc.name);
+    expect(groupObj.description).toBe(doc.description);
+    expect(groupObj.lastUpdatedBy).toBe(doc.lastUpdatedBy);
+  });
+
+  test('Remove group', async () => {
+    expect.assertions(2);
+    const fieldGroup = await fieldGroupFactory({});
+
+    try {
+      await FieldsGroups.removeFieldsGroup(_fieldGroup._id);
+    } catch (e) {
+      expect(e.message).toBe('Cant update this group');
+    }
+
+    await FieldsGroups.removeFieldsGroup(fieldGroup._id);
+
+    expect(await FieldsGroups.findOne({ _id: fieldGroup._id })).toBeNull();
+  });
+
+  test('Remove group with fake group, with exception', async () => {
+    expect.assertions(1);
+
+    const _id = '1333131';
+
+    try {
+      await FieldsGroups.removeFieldsGroup(_id);
+    } catch (e) {
+      expect(e.message).toBe(`Group not found with id of ${_id}`);
+    }
+  });
+
+  test('Update group visible', async () => {
+    expect.assertions(2);
+    const fieldGroup = await fieldGroupFactory({ visible: true });
+    const user = await userFactory({});
+
+    try {
+      await FieldsGroups.updateFieldsGroupVisible(_fieldGroup._id, true, user._id);
+    } catch (e) {
+      expect(e.message).toBe('Cant update this group');
+    }
+    const visible = false;
+
+    const groupObj = await FieldsGroups.updateFieldsGroupVisible(fieldGroup._id, visible, user._id);
+
+    expect(groupObj.visible).toBe(visible);
+  });
+});
---
+++ src/__tests__/fieldGroupMutations.test.js
@@ -0,0 +1,116 @@
+/* eslint-env jest */
+/* eslint-disable no-underscore-dangle */
+
+import faker from 'faker';
+import { connect, disconnect } from '../db/connection';
+import { FieldsGroups, Users } from '../db/models';
+import { FIELDS_GROUPS_CONTENT_TYPES } from '../data/constants';
+import { userFactory, fieldGroupFactory } from '../db/factories';
+import fieldsGroupsMutations from '../data/resolvers/mutations/fieldsgroups';
+
+beforeAll(() => connect());
+
+afterAll(() => disconnect());
+
+describe('Fields mutations', () => {
+  let _user;
+  let _fieldGroup;
+
+  beforeEach(async () => {
+    // Creatubg test data
+    _user = await userFactory({});
+    _fieldGroup = await fieldGroupFactory({});
+  });
+
+  afterEach(async () => {
+    // Clearing test data
+    await FieldsGroups.remove({});
+    await Users.remove({});
+  });
+
+  test('Check login required', async () => {
+    expect.assertions(4);
+
+    const check = async fn => {
+      try {
+        await fn({}, {}, {}, {});
+      } catch (e) {
+        expect(e.message).toEqual('Login required');
+      }
+    };
+
+    // add
+    check(fieldsGroupsMutations.fieldsGroupsAdd);
+
+    // edit
+    check(fieldsGroupsMutations.fieldsGroupsEdit);
+
+    // remove
+    check(fieldsGroupsMutations.fieldsGroupsRemove);
+
+    // update visible
+    check(fieldsGroupsMutations.fieldsGroupsUpdateVisible);
+  });
+
+  test('Create field', async () => {
+    const mockedMethod = jest.spyOn(FieldsGroups, 'createFieldsGroup');
+
+    const doc = {
+      name: faker.random.word(),
+      description: faker.random.word(),
+      contentType: FIELDS_GROUPS_CONTENT_TYPES.CUSTOMER,
+      lastUpdatedBy: _user._id,
+    };
+
+    await fieldsGroupsMutations.fieldsGroupsAdd({}, doc, { user: _user });
+
+    expect(FieldsGroups.createFieldsGroup).toBeCalledWith(doc);
+
+    mockedMethod.mockRestore();
+  });
+
+  test('Update field group', async () => {
+    FieldsGroups.updateFieldsGroup = jest.fn();
+
+    const doc = {
+      name: faker.random.word(),
+      description: faker.random.word(),
+      lastUpdatedBy: _user._id,
+    };
+
+    await fieldsGroupsMutations.fieldsGroupsEdit(
+      {},
+      { _id: _fieldGroup._id, ...doc },
+      { user: _user },
+    );
+
+    expect(FieldsGroups.updateFieldsGroup).toBeCalledWith(_fieldGroup._id, doc);
+  });
+
+  test('Remove field group', async () => {
+    FieldsGroups.removeFieldsGroup = jest.fn();
+
+    await fieldsGroupsMutations.fieldsGroupsRemove({}, { _id: _fieldGroup._id }, { user: _user });
+
+    expect(FieldsGroups.removeFieldsGroup).toBeCalledWith(_fieldGroup._id);
+  });
+
+  test('Update visible', async () => {
+    FieldsGroups.updateFieldsGroupVisible = jest.fn();
+
+    const visible = false;
+    const lastUpdatedBy = _user._id;
+
+    await fieldsGroupsMutations.fieldsGroupsUpdateVisible(
+      {},
+      { _id: _fieldGroup._id, visible, lastUpdatedBy },
+      { user: _user },
+    );
+
+    expect(FieldsGroups.updateFieldsGroupVisible).toBeCalledWith(
+      _fieldGroup._id,
+      visible,
+      lastUpdatedBy,
+    );
+  });
+});
