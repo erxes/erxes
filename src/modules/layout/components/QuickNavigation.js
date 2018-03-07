d815ae4e60345a32018de6689a11b8e695cc2e17
Khangarid Bayarsaikhan <khangarid.b@nmma.co>
2018-03-07 14:05:05 +0800

    Removed profile sidebar into quick navigation

--- src/modules/layout/components/QuickNavigation.js
+++ src/modules/layout/components/QuickNavigation.js
@@ -3,9 +3,19 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { Dropdown, MenuItem } from 'react-bootstrap';
 import styled from 'styled-components';
-import { NameCard, DropdownToggle, Icon } from 'modules/common/components';
+import {
+  NameCard,
+  DropdownToggle,
+  Icon,
+  ModalTrigger
+} from 'modules/common/components';
 import { UserHelper } from '../styles';
 import { Widget } from 'modules/notifications/containers';
+import {
+  ChangePassword,
+  NotificationSettings
+} from 'modules/settings/profile/containers';
+import { Signature } from 'modules/settings/email/containers';
 
 const UserInfo = styled.div`
   display: flex;
@@ -65,13 +75,40 @@ const QuickNavigation = (
             </NameCardWrapper>
             <MenuItem divider />
             <li>
-              <Link to="/settings/profile">{__('Edit Profile')}</Link>
-            </li>
-            <li>
-              <Link to="/settings/change-password">
-                {__('Change password')}
+              <Link to={`/settings/team/details/${currentUser._id}`}>
+                {__('Edit Profile')}
               </Link>
             </li>
+            <ModalTrigger
+              title="Change Password"
+              trigger={
+                <li>
+                  <a>{__('Change Password')}</a>
+                </li>
+              }
+            >
+              <ChangePassword />
+            </ModalTrigger>
+            <ModalTrigger
+              title="Email signatures"
+              trigger={
+                <li>
+                  <a>{__('Email signatures')}</a>
+                </li>
+              }
+            >
+              <Signature />
+            </ModalTrigger>
+            <ModalTrigger
+              title="Notification settings"
+              trigger={
+                <li>
+                  <a>{__('Notification settings')}</a>
+                </li>
+              }
+            >
+              <NotificationSettings />
+            </ModalTrigger>
             <MenuItem divider />
             <MenuItem onClick={logout}>{__('Sign out')}</MenuItem>
           </Dropdown.Menu>
