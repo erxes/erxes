a95e4c4b98844553c381bfae478e53439291533e
Munkhjin Badamkhatan <munkhjin0223@gmail.com>
2018-03-23 09:31:52 +0800

    Fix undefined error

--- src/modules/settings/deals/containers/Boards.js
+++ src/modules/settings/deals/containers/Boards.js
@@ -6,64 +6,71 @@ import { Alert, confirm } from 'modules/common/utils';
 import { queries, mutations } from '../graphql';
 import { Boards } from '../components';
 
-const BoardsContainer = props => {
-  const { boardsQuery, addMutation, editMutation, removeMutation } = props;
-  const { __ } = this.context;
+class BoardsContainer extends React.Component {
+  render() {
+    const {
+      boardsQuery,
+      addMutation,
+      editMutation,
+      removeMutation
+    } = this.props;
+    const { __ } = this.context;
 
-  const boards = boardsQuery.dealBoards || [];
+    const boards = boardsQuery.dealBoards || [];
 
-  // remove action
-  const remove = _id => {
-    confirm().then(() => {
-      removeMutation({
-        variables: { _id }
+    // remove action
+    const remove = _id => {
+      confirm().then(() => {
+        removeMutation({
+          variables: { _id }
+        })
+          .then(() => {
+            boardsQuery.refetch();
+
+            Alert.success(__('Successfully deleted.'));
+          })
+          .catch(error => {
+            Alert.error(error.message);
+          });
+      });
+    };
+
+    // create or update action
+    const save = ({ doc }, callback, board) => {
+      let mutation = addMutation;
+
+      // if edit mode
+      if (board) {
+        mutation = editMutation;
+        doc._id = board._id;
+      }
+
+      mutation({
+        variables: doc
       })
         .then(() => {
           boardsQuery.refetch();
 
-          Alert.success(__('Successfully deleted.'));
+          Alert.success(__('Successfully saved!'));
+
+          callback();
         })
         .catch(error => {
           Alert.error(error.message);
         });
-    });
-  };
+    };
 
-  // create or update action
-  const save = ({ doc }, callback, board) => {
-    let mutation = addMutation;
+    const extendedProps = {
+      ...this.props,
+      boards,
+      save,
+      remove,
+      loading: boardsQuery.loading
+    };
 
-    // if edit mode
-    if (board) {
-      mutation = editMutation;
-      doc._id = board._id;
-    }
-
-    mutation({
-      variables: doc
-    })
-      .then(() => {
-        boardsQuery.refetch();
-
-        Alert.success(__('Successfully saved!'));
-
-        callback();
-      })
-      .catch(error => {
-        Alert.error(error.message);
-      });
-  };
-
-  const extendedProps = {
-    ...props,
-    boards,
-    save,
-    remove,
-    loading: boardsQuery.loading
-  };
-
-  return <Boards {...extendedProps} />;
-};
+    return <Boards {...extendedProps} />;
+  }
+}
 
 BoardsContainer.propTypes = {
   boardsQuery: PropTypes.object,
