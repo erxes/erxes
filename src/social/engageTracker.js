274e7db8baa69a45dccdfa8e057778b9fbe5faa5
Khangarid Bayarsaikhan <khangarid.b@nmma.co>
2018-03-26 19:53:01 +0800

    Added body parser

--- src/social/engageTracker.js
+++ src/social/engageTracker.js
@@ -1,4 +1,8 @@
+import bodyParser from 'body-parser';
+
 export const trackEngages = expressApp => {
+  expressApp.use(`/service/engage/tracker`, bodyParser.json());
+
   expressApp.get(`/service/engage/tracker`, (req, res) => {
     console.log(req.header, req.body);
 
