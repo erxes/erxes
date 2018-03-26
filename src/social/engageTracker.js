f6eb224ba2f9c585989bc054d0c31a39ad142ef3
Khangarid Bayarsaikhan <khangarid.b@nmma.co>
2018-03-26 19:47:24 +0800

    Fixed bug on json parse

--- src/social/engageTracker.js
+++ src/social/engageTracker.js
@@ -1,12 +1,12 @@
 export const trackEngages = expressApp => {
   expressApp.get(`/service/engage/tracker`, (req, res) => {
-    console.log(req.header, JSON.parse(req.body));
+    console.log(req.header, req.body);
 
     res.end('success');
   });
 
   expressApp.post(`/service/engage/tracker`, (req, res) => {
-    console.log(req.header, JSON.parse(req.body));
+    console.log(req.header, req.body);
 
     res.end('success');
   });
