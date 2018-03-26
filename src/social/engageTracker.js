1e45aceefbed5f4e3b324d7c03a0e476fb488e7c
Khangarid Bayarsaikhan <khangarid.b@nmma.co>
2018-03-26 19:24:27 +0800

    Added post req tracker for engage

--- src/social/engageTracker.js
+++ src/social/engageTracker.js
@@ -1,5 +1,13 @@
 export const trackEngages = expressApp => {
   expressApp.get(`/service/engage/tracker`, (req, res) => {
     console.log(req);
+
+    res.end('success');
+  });
+
+  expressApp.post(`/service/engage/tracker`, (req, res) => {
+    console.log(req);
+
+    res.end('success');
   });
 };
