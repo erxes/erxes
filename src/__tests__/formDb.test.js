bfefd7676520d938a0542c3568cbfce7765006a2
batamar <batamar.b@nmtec.co>
2018-03-28 13:27:59 +0800

    Some refactor

--- src/__tests__/formDb.test.js
+++ src/__tests__/formDb.test.js
@@ -2,8 +2,8 @@
 /* eslint-disable no-underscore-dangle */
 
 import { connect, disconnect } from '../db/connection';
-import { userFactory, formFactory } from '../db/factories';
-import { Forms, Users } from '../db/models';
+import { userFactory, formFactory, fieldFactory } from '../db/factories';
+import { Forms, Users, Fields } from '../db/models';
 import toBeType from 'jest-tobetype';
 
 expect.extend(toBeType);
@@ -84,5 +84,64 @@ describe('form update', () => {
     expect(formAfterUpdate.createdUserId).toBe(_form.createdUserId);
     expect(formAfterUpdate.code).toBe(_form.code);
     expect(_form.createdDate).toBeType('object');
+  });
+});
+
+describe('form remove', async () => {
+  let _form;
+
+  beforeEach(async () => {
+    _form = await formFactory({});
+  });
+
+  afterEach(async () => {
+    await Forms.remove({});
+  });
+
+  test('check if form removal is working successfully', async () => {
+    await Forms.removeForm(_form._id);
+
+    const formCount = await Forms.find({}).count();
+
+    expect(formCount).toBe(0);
+  });
+});
+
+describe('form duplication', () => {
+  let _user;
+  let _form;
+
+  beforeEach(async () => {
+    _user = await userFactory({});
+    _form = await formFactory({ createdUserId: _user._id });
+    await fieldFactory({ contentTypeId: _form._id });
+    await fieldFactory({ contentTypeId: _form._id });
+    await fieldFactory({ contentTypeId: _form._id });
+  });
+
+  afterEach(async () => {
+    await Users.remove({});
+    await Fields.remove({});
+    await Forms.remove({});
+  });
+
+  test('test whether form duplication method is working successfully', async () => {
+    const duplicatedForm = await Forms.duplicate(_form._id);
+
+    expect(duplicatedForm.title).toBe(`${_form.title} duplicated`);
+    expect(duplicatedForm.description).toBe(_form.description);
+    expect(duplicatedForm.code).toBeType('string');
+    expect(duplicatedForm.code.length).toEqual(6);
+    expect(duplicatedForm.createdUserId).toBe(_form.createdUserId);
+
+    const fieldsCount = await Fields.find({}).count();
+
+    const duplicatedFieldsCount = await Fields.find({
+      contentType: 'form',
+      contentTypeId: duplicatedForm._id,
+    }).count();
+
+    expect(fieldsCount).toEqual(6);
+    expect(duplicatedFieldsCount).toEqual(3);
   });
 });
