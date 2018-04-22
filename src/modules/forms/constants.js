018c323e081cfee91b926ff7eb4924fc27c1595a
munkhorgil <munkhorgil@github.com>
2018-04-22 23:00:37 +0800

    Form validation

---
+++ src/modules/forms/constants.js
@@ -0,0 +1,26 @@
+const VALIDATION_ERROR = {
+  title: 'Write title',
+  brand: 'Choose brand',
+  language: 'Choose language',
+  type: 'Choose type',
+  successAction: 'Choose success action',
+  fromEmail: 'Write from email',
+  userEmailTitle: 'Write user email title',
+  userEmailContent: 'Write user email content',
+  adminEmails: 'Write admin emails',
+  adminEmailsTitle: 'Write admin email title',
+  adminEmailsContent: 'Write admin email content',
+  thankContent: 'Write thank content',
+  redirectUrl: 'Write redirect url',
+  formTitle: 'Write form title',
+  formDesc: 'Write form description',
+  formBtnText: 'Write form button text',
+  theme: 'Choose theme',
+  calloutTitle: 'Write call out title',
+  calloutBtnText: 'Write call out button text',
+  bodyValue: 'Write body value',
+  logoPreviewUrl: 'Insert logo preview url ',
+  fields: 'Insert fields'
+};
+
+export default VALIDATION_ERROR;
