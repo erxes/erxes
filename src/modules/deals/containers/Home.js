4cdaa9efa1a9eb2cb210851a53f0b1832356204f
batamar <batamar.b@nmtec.co>
2018-04-25 15:42:31 +0800

    Deal home refactor

--- src/modules/deals/containers/Home.js
+++ src/modules/deals/containers/Home.js
@@ -1,121 +1,67 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { withRouter } from 'react-router';
 import { compose, graphql } from 'react-apollo';
 import gql from 'graphql-tag';
 import { router as routerUtils } from 'modules/common/utils';
 import { Home } from '../components';
 import { queries } from '../graphql';
 
-class HomeContainer extends React.Component {
-  componentWillReceiveProps() {
-    const { history, currentBoard } = this.props;
-
-    if (!routerUtils.getParam(history, 'id') && currentBoard) {
-      routerUtils.setParams(history, { id: currentBoard._id });
-    }
-  }
-
+/*
+ * Main board component
+ */
+class Main extends React.Component {
   render() {
-    const { boardsQuery } = this.props;
+    const {
+      history,
+      queryParams,
+      boardsQuery,
+      boardGetDefaultQuery = {},
+      boardDetailQuery = {}
+    } = this.props;
 
-    const boards = boardsQuery.dealBoards || [];
+    const currentBoard = boardDetailQuery.dealBoardDetail;
+    const defaultBoard = boardGetDefaultQuery.dealBoardGetDefault;
+
+    if (!queryParams.id && defaultBoard) {
+      routerUtils.setParams(history, { id: defaultBoard._id });
+      return null;
+    }
 
     const extendedProps = {
       ...this.props,
-      boards
+      currentBoard,
+      boards: boardsQuery.dealBoards || []
     };
 
     return <Home {...extendedProps} />;
   }
 }
 
-HomeContainer.propTypes = {
+Main.propTypes = {
+  history: PropTypes.object,
+  queryParams: PropTypes.object,
   boardsQuery: PropTypes.object,
-  currentBoard: PropTypes.object,
-  history: PropTypes.object
-};
-
-const HomeContainerWithData = compose(
-  graphql(gql(queries.boards), {
-    name: 'boardsQuery'
-  })
-)(HomeContainer);
-
-const BoardDetail = props => {
-  const { boardDetailQuery } = props;
-
-  const currentBoard = boardDetailQuery.dealBoardDetail;
-
-  const extendedProps = {
-    ...props,
-    currentBoard
-  };
-
-  return <HomeContainerWithData {...extendedProps} />;
-};
-
-BoardDetail.propTypes = {
+  boardGetDefaultQuery: PropTypes.object,
   boardDetailQuery: PropTypes.object
 };
 
-const BoardDetailContainer = compose(
+const MainContainer = compose(
+  graphql(gql(queries.boards), {
+    name: 'boardsQuery'
+  }),
+  graphql(gql(queries.boardGetDefault), {
+    name: 'boardGetDefaultQuery',
+    options: ({ queryParams }) => ({
+      skip: queryParams.id
+    })
+  }),
   graphql(gql(queries.boardDetail), {
     name: 'boardDetailQuery',
-    options: ({ currentBoardId = '' }) => ({
-      variables: { _id: currentBoardId }
+    options: ({ queryParams }) => ({
+      skip: !queryParams.id,
+      variables: { _id: queryParams.id }
     })
   })
-)(BoardDetail);
+)(Main);
 
-/*
- * We will use this component when there is no current board id
- * in query string
- */
-const DefaultBoard = props => {
-  const { boardGetDefaultQuery } = props;
-
-  const defaultBoard = boardGetDefaultQuery.dealBoardGetDefault;
-
-  const extendedProps = {
-    ...props,
-    currentBoard: defaultBoard
-  };
-
-  return <HomeContainerWithData {...extendedProps} />;
-};
-
-DefaultBoard.propTypes = {
-  boardGetDefaultQuery: PropTypes.object
-};
-
-const DefaultBoardContainer = compose(
-  graphql(gql(queries.boardGetDefault), {
-    name: 'boardGetDefaultQuery'
-  })
-)(DefaultBoard);
-
-/*
- * Main board component
- */
-const MainContainer = props => {
-  const { history } = props;
-  const currentBoardId = routerUtils.getParam(history, 'id');
-
-  if (currentBoardId) {
-    const extentedProps = {
-      ...props,
-      currentBoardId
-    };
-
-    return <BoardDetailContainer {...extentedProps} />;
-  }
-
-  return <DefaultBoardContainer {...props} />;
-};
-
-MainContainer.propTypes = {
-  history: PropTypes.object
-};
-
-export default withRouter(MainContainer);
+export default MainContainer;
