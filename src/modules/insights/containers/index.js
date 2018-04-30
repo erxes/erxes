069277bd9393999add26f18876dcb355f4058eee
Munkhjin Badamkhatan <munkhjin0223@gmail.com>
2018-04-30 11:02:28 +0800

    Delete duplicated codes

--- src/modules/insights/containers/index.js
+++ src/modules/insights/containers/index.js
@@ -1,13 +1,5 @@
 import VolumeAndReponseReport from './VolumeAndReponseReport';
 import FirstAndCloseResponseReport from './FirstAndCloseResponseReport';
-import FirstResponse from './FirstResponse';
-import ResponseCloseReport from './ResponseCloseReport';
 import Reports from './Reports';
 
-export {
-  VolumeAndReponseReport,
-  FirstAndCloseResponseReport,
-  FirstResponse,
-  ResponseCloseReport,
-  Reports
-};
+export { VolumeAndReponseReport, FirstAndCloseResponseReport, Reports };
