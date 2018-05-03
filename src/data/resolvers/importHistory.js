38acfe8b45f8c4f46bda10f0625a60dd0dd2812a
Khangarid Bayarsaikhan <khangarid.b@nmma.co>
2018-05-03 19:30:04 +0800

    Fixed resolver on import history

--- src/data/resolvers/importHistory.js
+++ src/data/resolvers/importHistory.js
@@ -2,6 +2,6 @@ import { Users } from '../../db/models';
 
 export default {
   user(history) {
-    return Users.findOne({ _id: history.importedUserId });
+    return Users.findOne({ _id: history.userId });
   },
 };
