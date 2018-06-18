39e0ad5100759a436d8fc73c8122757d1a68a934
Ganzorig Bayarsaikhan <ganzorig.b@nmma.co>
2018-06-18 20:25:34 +0700

    Fix scroll positioning and add loader

--- src/modules/inbox/components/conversationDetail/ConversationDetail.js
+++ src/modules/inbox/components/conversationDetail/ConversationDetail.js
@@ -1,20 +1,43 @@
 import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { MainContent } from 'modules/layout/styles';
+import { Sidebar as EmptySidebar } from 'modules/layout/components';
+import { Spinner } from 'modules/common/components';
 import { WorkArea, Sidebar } from 'modules/inbox/containers/conversationDetail';
+import { ContentBox } from 'modules/layout/styles';
 
 export default class ConversationDetail extends Component {
-  render() {
+  renderSidebar() {
     const { currentConversation } = this.props;
 
+    if (currentConversation.customerId) {
+      return <Sidebar conversation={currentConversation} />;
+    }
+    return (
+      <EmptySidebar full>
+        <Spinner />
+      </EmptySidebar>
+    );
+  }
+
+  renderContent() {
+    const { currentConversation } = this.props;
+
+    if (currentConversation.customerId) {
+      return <WorkArea {...this.props} />;
+    }
+    return (
+      <ContentBox>
+        <Spinner />
+      </ContentBox>
+    );
+  }
+
+  render() {
     return (
       <Fragment>
-        <MainContent>
-          {currentConversation._id && <WorkArea {...this.props} />}
-        </MainContent>
-        {currentConversation.customerId && (
-          <Sidebar conversation={currentConversation} />
-        )}
+        <MainContent>{this.renderContent()}</MainContent>
+        {this.renderSidebar()}
       </Fragment>
     );
   }
