521e346ae33c779a00379e923b289f778f3765a7
batamar <batamar.b@nmtec.co>
2018-08-01 19:24:34 +0800

    Added build commands

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,8 @@
-import * as path from 'path';
-import * as express from 'express';
-import * as bodyParser from 'body-parser';
-import * as cors from 'cors';
-import * as dotenv from 'dotenv';
+import * as bodyParser from "body-parser";
+import * as cors from "cors";
+import * as dotenv from "dotenv";
+import * as express from "express";
+import * as path from "path";
 
 dotenv.config();
 
@@ -13,37 +13,37 @@ app.use(bodyParser.json());
 
 app.use(cors());
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 // generated scripts
-app.use('/build', express.static(path.join(__dirname, '../static')));
-app.use('/static', express.static('public'));
+app.use("/build", express.static(path.join(__dirname, "../static")));
+app.use("/static", express.static("public"));
 
 // routes
-app.get('/messenger', (req, res) => {
-  res.render('widget', { type: 'messenger' });
+app.get("/messenger", (req, res) => {
+  res.render("widget", { type: "messenger" });
 });
 
 // remove this code after replacing all scripts
-app.get('/inapp', (req, res) => {
-  res.render('widget', { type: 'messenger' });
+app.get("/inapp", (req, res) => {
+  res.render("widget", { type: "messenger" });
 });
 
-app.get('/chat', (req, res) => {
-  res.render('widget', { type: 'messenger' });
+app.get("/chat", (req, res) => {
+  res.render("widget", { type: "messenger" });
 });
 
-app.get('/form', (req, res) => {
-  res.render('widget', { type: 'form' });
+app.get("/form", (req, res) => {
+  res.render("widget", { type: "form" });
 });
 
-app.get('/test', (req, res) => {
-  res.render('widget-test');
+app.get("/test", (req, res) => {
+  res.render("widget-test");
 });
 
-app.get('/knowledgebase', (req, res) => {
-  res.render('widget', { type: 'knowledgebase' });
+app.get("/knowledgebase", (req, res) => {
+  res.render("widget", { type: "knowledgebase" });
 });
 
 const port = process.env.PORT || 3200;
