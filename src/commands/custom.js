3152d1505f5d84af981dda0ef4b33292781fbc2a
Khangarid Bayarsaikhan <khangarid.b@nmma.co>
2018-08-11 01:55:50 +0800

    Updated customCommand for facebook conversations

--- src/commands/custom.js
+++ src/commands/custom.js
@@ -1,53 +1,13 @@
 import { connect, disconnect } from '../db/connection';
-import { Companies, Customers } from '../db/models';
+import { Conversations, ConversationMessages } from '../db/models';
 
 export const customCommand = async () => {
   connect();
 
-  const companies = await Companies.find({
-    name: { $exists: true },
-    primaryName: { $exists: false },
-  });
+  const conversations = await Conversations.find({ facebookData: { $exists: true } });
 
-  try {
-    Companies.collection.dropIndexes({ name: 1 });
-  } catch (e) {
-    console.log(e);
-  }
-
-  for (const company of companies) {
-    await Companies.update(
-      { _id: company._id },
-      {
-        $set: { names: [company.name], primaryName: company.name },
-      },
-    );
-  }
-
-  const customers = await Customers.find({
-    $or: [{ email: { $exists: true } }, { phone: { $exists: true } }],
-  });
-
-  try {
-    Customers.collection.dropIndexes({ email: 1 });
-  } catch (e) {
-    console.log(e);
-  }
-
-  for (const customer of customers) {
-    if (customer.email && !customer.primaryEmail) {
-      await Customers.update(
-        { _id: customer._id },
-        { $set: { primaryEmail: customer.email, emails: [customer.email] } },
-      );
-    }
-
-    if (customer.phone && !customer.primaryPhone) {
-      await Customers.update(
-        { _id: customer._id },
-        { $set: { primaryPhone: customer.phone, phones: [customer.phone] } },
-      );
-    }
+  for (let conversation of conversations) {
+    await ConversationMessages.remove({ conversationId: conversation._id });
   }
 
   disconnect();
