83811b35d6230bd7dee00a839c7f1579d2d2a7d2
Anu-ujin <anu.b@nmma.co>
2018-08-10 18:28:45 +0800

    undo unused change

--- src/modules/common/components/Tip.js
+++ src/modules/common/components/Tip.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { Tooltip, OverlayTrigger } from 'react-bootstrap';
 
 const propTypes = {
-  text: PropTypes.string,
+  text: PropTypes.string.isRequired,
   children: PropTypes.element.isRequired,
   placement: PropTypes.string
 };
