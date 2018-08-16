f25a92ad021f5c170c1009cf35c09b90d390dccc
Munkhjin Badamkhatan <munkhjin0223@gmail.com>
2018-08-16 19:28:22 +0800

    Add board config local storage

--- src/modules/deals/containers/Home.js
+++ src/modules/deals/containers/Home.js
@@ -2,9 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { compose, graphql } from 'react-apollo';
 import gql from 'graphql-tag';
+
 import { router as routerUtils } from 'modules/common/utils';
+import {
+  STORAGE_BOARD_KEY,
+  STORAGE_PIPELINE_KEY
+} from 'modules/common/constants';
 import { Home } from '../components';
 import { queries } from '../graphql';
+
+const storageBoardId = localStorage.getItem(STORAGE_BOARD_KEY);
 
 /*
  * Main board component
@@ -15,16 +22,28 @@ class Main extends React.Component {
       history,
       queryParams,
       boardsQuery,
-      boardGetDefaultQuery = {},
+      boardGetLastQuery = {},
       boardDetailQuery = {}
     } = this.props;
 
     const currentBoard = boardDetailQuery.dealBoardDetail;
-    const defaultBoard = boardGetDefaultQuery.dealBoardGetDefault;
 
-    if (!queryParams.id && defaultBoard) {
-      routerUtils.setParams(history, { id: defaultBoard._id });
-      return null;
+    if (!queryParams.id) {
+      let boardId = storageBoardId;
+
+      const lastBoard = boardGetLastQuery.dealBoardGetLast;
+
+      if (!boardId && lastBoard) {
+        boardId = lastBoard._id;
+      }
+
+      if (boardId) {
+        routerUtils.setParams(history, { id: boardId });
+        return null;
+      }
+    } else {
+      localStorage.setItem(STORAGE_BOARD_KEY, queryParams.id);
+      localStorage.setItem(STORAGE_PIPELINE_KEY, null);
     }
 
     const extendedProps = {
@@ -41,7 +60,7 @@ Main.propTypes = {
   history: PropTypes.object,
   queryParams: PropTypes.object,
   boardsQuery: PropTypes.object,
-  boardGetDefaultQuery: PropTypes.object,
+  boardGetLastQuery: PropTypes.object,
   boardDetailQuery: PropTypes.object
 };
 
@@ -49,9 +68,9 @@ const MainContainer = compose(
   graphql(gql(queries.boards), {
     name: 'boardsQuery'
   }),
-  graphql(gql(queries.boardGetDefault), {
-    name: 'boardGetDefaultQuery',
-    skip: ({ queryParams }) => queryParams.id
+  graphql(gql(queries.boardGetLast), {
+    name: 'boardGetLastQuery',
+    skip: () => storageBoardId
   }),
   graphql(gql(queries.boardDetail), {
     name: 'boardDetailQuery',
