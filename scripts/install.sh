4a3d22a94421f861d3fa69d507061260cd77b433
Shinebayar G <rnmkrmn@gmail.com>
2018-08-29 17:43:22 +0800

    update installation script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,55 +1,12 @@
 #!/bin/sh
 
-UNAME=$(uname)
-
-if [ "$UNAME" = "Darwin" ] ; then
-  ### OSX ###
-
-  if type node > /dev/null ; then
-    brew install node
-  fi
-
-  # mongodb
-  if ! type mongo > /dev/null ; then
-    brew update
-    brew install mongodb
-    mkdir -p /data/db
-    chmod 777 /data/db
-    mongod
-  fi
-
-  # yarn
-  if ! type yarn > /dev/null ; then
-    brew install yarn
-  fi
-
-elif [ "$UNAME" = "Linux" ] ; then
-  ### Linux ###
-
-  # nodejs
-  if ! type node > /dev/null ; then
-    curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-    sudo apt-get install -y nodejs
-  fi
-
-  # mongodb
-  if ! type mongo > /dev/null ; then
-    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
-    echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
-    sudo apt-get update
-    sudo apt-get install -y mongodb-org
-    sudo service mongod start
-  fi
-
-  # yarn
-  if ! type yarn > /dev/null ; then
-    curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-    echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-    sudo apt-get update && sudo apt-get install yarn
-  fi
-fi
-
-if [ ! -d .git ]; then
+inside_git_repo="$(git rev-parse --is-inside-work-tree 2>/dev/null)"
+if [ "$inside_git_repo" ]; then
+  echo "Git repository detected. Please run the script in non git directory."
+  exit 0
+else
+  mkdir erxes.io
+  cd erxes.io
   echo 'Clone erxes repository and install its dependencies:'
   git clone https://github.com/erxes/erxes.git
   cd erxes
@@ -58,12 +15,12 @@ if [ ! -d .git ]; then
 fi
 
 echo 'Create `.env.sample` from default settings file and configure it on your own:'
-cp .env.sample .env.development
+cp .env.sample .env
 
 CURRENT_FOLDER=${PWD##*/}
 if [ $CURRENT_FOLDER = 'erxes' ]; then
   cd ..
 fi
 
-echo 'Install erxes-app-api'
-curl https://raw.githubusercontent.com/erxes/erxes-app-api/master/scripts/install.sh | sh
+echo 'Install erxes-api'
+curl https://raw.githubusercontent.com/erxes/erxes-api/master/scripts/install.sh | sh
