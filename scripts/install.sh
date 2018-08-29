2d742ad9dbe864a0df92964104b7b6170a07a1b5
Shinebayar G <rnmkrmn@gmail.com>
2018-08-29 18:03:10 +0800

    update installation script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,39 +1,21 @@
 #!/bin/sh
 
-## Installation
-
-if [ ! -d .git ]; then
-  echo 'Clone erxes repository and install its dependencies:'
-  git clone https://github.com/erxes/erxes-app-api
-  cd erxes-app-api
-  yarn install
-fi
-
-UNAME=$(uname)
-
-if [ $UNAME = "Linux" ] ; then
-  ### Linux ###
-  if ! type redis-server > /dev/null ; then
-    sudo apt install -y redis-server
-  fi
-fi
-if [ $UNAME = "Darwin" ] ; then
-  ### Mac OS ###
-  if ! type redis-server > /dev/null ; then
-    brew install redis
-  fi
-fi
+echo 'Clone erxes-api repository and install its dependencies:'
+git clone https://github.com/erxes/erxes-api.git
+cd erxes-api
+git checkout master
+yarn install
 
 echo 'Create `.env.sample` from default settings file and configure it on your own:'
 cp .env.sample .env
 
-
-echo 'Load initial data';
-yarn loadInitialData
+echo 'Creating admin user';
+yarn initProject
 
 CURRENT_FOLDER=${PWD##*/}
-if [ $CURRENT_FOLDER = 'erxes-app-api' ]; then
+if [ $CURRENT_FOLDER = 'erxes-api' ]; then
   cd ..
 fi
 
-curl https://raw.githubusercontent.com/erxes/erxes-widgets/master/scripts/install.sh | sh
+echo 'Install erxes-widgets-api'
+curl https://raw.githubusercontent.com/erxes/erxes-widgets-api/master/scripts/install.sh | sh
