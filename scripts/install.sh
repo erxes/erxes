32379b65f724ec4f1d73cad7b36e5d020e40d885
Shinebayar G <rnmkrmn@gmail.com>
2018-08-29 19:49:22 +0800

    fix comment

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 echo 'Clone erxes-widgets repository and install its dependencies:'
-#git clone https://github.com/erxes/erxes-widgets.git
-#cd erxes-widgets
-#git checkout master
-#yarn install
+git clone https://github.com/erxes/erxes-widgets.git
+cd erxes-widgets
+git checkout master
+yarn install
 
 echo 'Create `.env.sample` from default settings file and configure it on your own:'
-#cp .env.sample .env
+cp .env.sample .env
 
 CURRENT_FOLDER=${PWD##*/}
 if [ $CURRENT_FOLDER = 'erxes-widgets' ]; then
