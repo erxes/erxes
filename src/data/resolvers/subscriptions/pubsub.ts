9a1182b1b1fbb614a8dd46b5cfa731efce07212d
Khangarid Bayarsaikhan <khangarid.b@nmma.co>
2018-09-04 17:33:28 +0800

    Converted subscriptions and app entry point to typescript

--- src/data/resolvers/subscriptions/pubsub.ts
+++ src/data/resolvers/subscriptions/pubsub.ts
@@ -1,11 +1,14 @@
-import { RedisPubSub } from 'graphql-redis-subscriptions';
-import * as Redis from 'ioredis';
-import * as dotenv from 'dotenv';
+import * as dotenv from "dotenv";
+import { RedisPubSub } from "graphql-redis-subscriptions";
+import * as Redis from "ioredis";
 
 // load environment variables
 dotenv.config();
 
-const { REDIS_HOST = 'localhost', REDIS_PORT = 6379 } = process.env;
+const {
+  REDIS_HOST = "localhost",
+  REDIS_PORT = 6379
+}: { REDIS_HOST?: string; REDIS_PORT?: number } = process.env;
 
 // Docs on the different redis options
 // https://github.com/NodeRedis/node_redis#options-object-properties
@@ -18,7 +21,7 @@ const redisOptions = {
   retry_strategy: options => {
     // reconnect after
     return Math.max(options.attempt * 100, 3000);
-  },
+  }
 };
 
 const pubsub = new RedisPubSub({
@@ -28,7 +31,7 @@ const pubsub = new RedisPubSub({
     }
   },
   publisher: new Redis(redisOptions),
-  subscriber: new Redis(redisOptions),
+  subscriber: new Redis(redisOptions)
 });
 
 export default pubsub;
