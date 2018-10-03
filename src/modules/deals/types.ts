d0e93e897affbb840737cf9c161984dd13107ff1
Khangarid Bayarsaikhan <khangarid.b@nmma.co>
2018-10-03 16:02:32 +0800

    Added deal status in ccommon deal

--- src/modules/deals/types.ts
+++ src/modules/deals/types.ts
@@ -1,34 +1,35 @@
-import { IUser } from "../auth/types";
-import { ICompany } from "../companies/types";
-import { ICustomer } from "../customers/types";
+import { IUser } from '../auth/types';
+import { ICompany } from '../companies/types';
+import { ICustomer } from '../customers/types';
 
 export interface IBoard {
-	_id: string;
+  _id: string;
 }
 
 export interface IPipeline {
-	_id: string;
-	name: string;
+  _id: string;
+  name: string;
 }
 
 export interface IStage {
-	_id: string;
-	name: string;
-	type: string;
-	index: number;
-	itemId: string;
+  _id: string;
+  name: string;
+  type: string;
+  index: number;
+  itemId: string;
 }
 
 export interface IDeal {
-	_id: string;
-	name: string;
-	stageId: string;
-	assignedUsers: IUser[];
-	companies: ICompany[];
-	customers: ICustomer[];
-	pipeline: IPipeline;
-	closeDate: Date;
-	amount: number;
-	modifiedAt: Date;
-	products: any;
+  _id: string;
+  name: string;
+  stageId: string;
+  assignedUsers: IUser[];
+  companies: ICompany[];
+  customers: ICustomer[];
+  pipeline: IPipeline;
+  closeDate: Date;
+  amount: number;
+  modifiedAt: Date;
+  stage?: IStage;
+  products: any;
 }
