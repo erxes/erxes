f4aea8a68053ecc739743f0c9f214c6ac0546d3e
batamar <batamar.b@nmtec.co>
2018-10-07 16:37:43 +0800

    Added large dataset demo

--- src/modules/deals/containers/Board.tsx
+++ src/modules/deals/containers/Board.tsx
@@ -1,45 +1,137 @@
-import gql from 'graphql-tag';
+import { injectGlobal } from 'emotion';
 import * as React from 'react';
-import { compose, graphql } from 'react-apollo';
-import { Board } from '../components';
-import { queries } from '../graphql';
-import { IBoard } from '../types';
-import { DealConsumer, DealProvider } from './DealContext';
+import { DragDropContext, Droppable } from 'react-beautiful-dnd';
+import styled from 'styled-components';
+import styledTS from 'styled-components-ts';
+import { colors } from '../constants';
+import { generateQuoteMap } from '../data';
+import reorder, { reorderQuoteMap } from '../reorder';
+import Column from './Column';
+
+const ParentContainer = styledTS<any>(styled.div)`
+  height: ${({ height }) => height};
+  overflow-x: hidden;
+  overflow-y: auto;
+`;
+
+const Container = styled('div')`
+  min-height: 100vh;
+
+  /* like display:flex but will allow bleeding over the window width */
+  min-width: 100vw;
+  display: inline-flex;
+`;
 
 type Props = {
-  currentBoard?: IBoard;
-  pipelinesQuery: any;
+  containerHeight?: string;
 };
 
-class BoardContainer extends React.Component<Props> {
+type State = {
+  columns;
+  ordered: string[];
+};
+
+const initial = generateQuoteMap(500);
+
+export default class Board extends React.Component<Props, State> {
+  /* eslint-disable react/sort-comp */
+
+  state: State = {
+    columns: initial,
+    ordered: Object.keys(initial)
+  };
+
+  boardRef?: HTMLElement;
+
+  componentDidMount() {
+    /* stylelint-disable max-empty-lines */
+    // eslint-disable-next-line no-unused-expressions
+    injectGlobal`
+      body {
+        background: ${colors.blue.deep};
+      }
+    `;
+    /* stylelint-enable */
+  }
+
+  onDragEnd = result => {
+    // dropped nowhere
+    if (!result.destination) {
+      return;
+    }
+
+    const source = result.source;
+    const destination = result.destination;
+
+    // did not move anywhere - can bail early
+    if (
+      source.droppableId === destination.droppableId &&
+      source.index === destination.index
+    ) {
+      return;
+    }
+
+    // reordering column
+    if (result.type === 'COLUMN') {
+      const ordered: string[] = reorder(
+        this.state.ordered,
+        source.index,
+        destination.index
+      );
+
+      this.setState({
+        ordered
+      });
+
+      return;
+    }
+
+    const data = reorderQuoteMap({
+      quoteMap: this.state.columns,
+      source,
+      destination
+    });
+
+    this.setState({
+      columns: data.quoteMap
+    });
+  };
+
   render() {
-    const { pipelinesQuery } = this.props;
+    const columns = this.state.columns;
+    const ordered: string[] = this.state.ordered;
+    const { containerHeight } = this.props;
 
-    const pipelines = pipelinesQuery.dealPipelines || [];
-
-    const extendedProps = {
-      ...this.props,
-      pipelines,
-      loading: pipelinesQuery.loading
-    };
+    const board = (
+      <Droppable
+        droppableId="board"
+        type="COLUMN"
+        direction="horizontal"
+        ignoreContainerClipping={Boolean(containerHeight)}
+      >
+        {provided => (
+          <Container innerRef={provided.innerRef} {...provided.droppableProps}>
+            {ordered.map((key: string, index: number) => (
+              <Column
+                key={key}
+                index={index}
+                title={key}
+                quotes={columns[key]}
+              />
+            ))}
+          </Container>
+        )}
+      </Droppable>
+    );
 
     return (
-      <DealProvider>
-        <DealConsumer>
-          {({ states, onDragEnd }) => (
-            <Board {...extendedProps} states={states} onDragEnd={onDragEnd} />
-          )}
-        </DealConsumer>
-      </DealProvider>
+      <DragDropContext onDragEnd={this.onDragEnd}>
+        {this.props.containerHeight ? (
+          <ParentContainer height={containerHeight}>{board}</ParentContainer>
+        ) : (
+          board
+        )}
+      </DragDropContext>
     );
   }
 }
-
-export default compose(
-  graphql(gql(queries.pipelines), {
-    name: 'pipelinesQuery',
-    options: ({ currentBoard }: { currentBoard: IBoard }) => ({
-      variables: { boardId: currentBoard ? currentBoard._id : '' }
-    })
-  })
-)(BoardContainer);
---
+++ src/modules/deals/containers/Column.tsx
@@ -0,0 +1,59 @@
+import * as React from 'react';
+import { Draggable } from 'react-beautiful-dnd';
+import styled from 'styled-components';
+import styledTS from 'styled-components-ts';
+import { borderRadius, colors, grid } from '../constants';
+import QuoteList from './QuoteList';
+import Title from './Title';
+
+const Container = styled('div')`
+  margin: ${grid}px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Header = styledTS<any>(styled('div'))`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-top-left-radius: ${borderRadius}px;
+  border-top-right-radius: ${borderRadius}px;
+  background-color: ${({ isDragging }) =>
+    isDragging ? colors.blue.lighter : colors.blue.light};
+  transition: background-color 0.1s ease;
+
+  &:hover {
+    background-color: ${colors.blue.lighter};
+  }
+`;
+
+type Props = {
+  title: string;
+  quotes: any[];
+  index: number;
+};
+
+export default class Column extends React.Component<Props> {
+  render() {
+    const title: string = this.props.title;
+    const quotes: any[] = this.props.quotes;
+    const index: number = this.props.index;
+    return (
+      <Draggable draggableId={title} index={index}>
+        {(provided, snapshot) => (
+          <Container innerRef={provided.innerRef} {...provided.draggableProps}>
+            <Header isDragging={snapshot.isDragging}>
+              <Title
+                isDragging={snapshot.isDragging}
+                {...provided.dragHandleProps}
+              >
+                {title}
+              </Title>
+            </Header>
+            <QuoteList listId={title} listType="QUOTE" quotes={quotes} />
+          </Container>
+        )}
+      </Draggable>
+    );
+  }
+}
