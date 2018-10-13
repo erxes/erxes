422b247d74619eef8a005ec7e67f14c9b062ff27
Khangarid Bayarsaikhan <khangarid.b@nmma.co>
2018-10-13 20:45:52 +0800

    Fixed knowledgeBase bug when there is no data

--- src/modules/knowledgeBase/containers/KnowledgeBase.tsx
+++ src/modules/knowledgeBase/containers/KnowledgeBase.tsx
@@ -9,9 +9,9 @@ import { queries } from '../graphql';
 
 interface IProps extends IRouterProps {
   currentCategoryId: string;
-  articlesCountQuery: any;
-  categoryDetailQuery: any;
-  lastCategoryQuery: any;
+  articlesCountQuery?: any;
+  categoryDetailQuery?: any;
+  lastCategoryQuery?: any;
   queryParams: any;
 }
 
@@ -24,31 +24,28 @@ const KnowledgeBase = (props: IProps) => {
     history
   } = props;
 
-  if (lastCategoryQuery.loading) {
-    return null;
+  const lastCategory =
+    lastCategoryQuery && lastCategoryQuery.knowledgeBaseCategoriesGetLast;
+
+  if (!queryParams.id && lastCategory) {
+    routerUtils.setParams(history, { id: lastCategory._id });
   }
 
-  const lastCategory = lastCategoryQuery.knowledgeBaseCategoriesGetLast;
-  const articlesCount = articlesCountQuery.knowledgeBaseArticlesTotalCount || 0;
-  const currentCategory = categoryDetailQuery.knowledgeBaseCategoryDetail || {};
+  const articlesCount =
+    (articlesCountQuery &&
+      articlesCountQuery.knowledgeBaseArticlesTotalCount) ||
+    0;
 
-  let updatedProps = {
+  const currentCategory =
+    (categoryDetailQuery && categoryDetailQuery.knowledgeBaseCategoryDetail) ||
+    {};
+
+  const updatedProps = {
     ...props,
     articlesCount,
     currentCategory,
     currentCategoryId: currentCategory._id || ''
   };
-
-  if (!queryParams.id) {
-    routerUtils.setParams(history, { id: lastCategory._id });
-
-    updatedProps = {
-      ...props,
-      articlesCount,
-      currentCategory: lastCategory,
-      currentCategoryId: lastCategory._id
-    };
-  }
 
   return <KnowledgeBaseComponent {...updatedProps} />;
 };
@@ -59,15 +56,18 @@ export default compose(
     options: ({ queryParams }: { queryParams: any }) => ({
       variables: { _id: queryParams.id || '' },
       fetchPolicy: 'network-only'
-    })
+    }),
+    skip: ({ queryParams }) => !queryParams.id
   }),
   graphql(gql(queries.knowledgeBaseArticlesTotalCount), {
     name: 'articlesCountQuery',
     options: ({ queryParams }: { queryParams: any }) => ({
       variables: { categoryIds: [queryParams.id] || [''] }
-    })
+    }),
+    skip: ({ queryParams }) => !queryParams.id
   }),
   graphql(gql(queries.categoriesGetLast), {
-    name: 'lastCategoryQuery'
+    name: 'lastCategoryQuery',
+    skip: ({ queryParams }) => queryParams.id
   })
 )(withRouter<IProps>(KnowledgeBase));
