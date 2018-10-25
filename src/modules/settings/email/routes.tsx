0702e74ad5acb5fda9492215c03f85c991c0f84a
Anu-ujin <anu.b@nmma.co>
2018-10-25 15:33:37 +0800

    Fix routes in settings

--- src/modules/settings/email/routes.tsx
+++ src/modules/settings/email/routes.tsx
@@ -3,32 +3,30 @@ import * as React from 'react';
 import { Route } from 'react-router-dom';
 import { List, Signature } from './containers';
 
-const routes = () => {
-  const emails = ({ location }) => {
-    return <List queryParams={queryString.parse(location.search)} />;
-  };
-
-  const emailSignatures = ({ location }) => {
-    return <Signature queryParams={queryString.parse(location.search)} />;
-  };
-
-  return (
-    <React.Fragment>
-      <Route
-        key="/settings/emails/"
-        exact={true}
-        path="/settings/emails/"
-        component={emails}
-      />
-
-      <Route
-        key="/settings/emails/signatures"
-        exact={true}
-        path="/settings/emails/signatures"
-        component={emailSignatures}
-      />
-    </React.Fragment>
-  );
+const emails = ({ location }) => {
+  return <List queryParams={queryString.parse(location.search)} />;
 };
+
+const emailSignatures = ({ location }) => {
+  return <Signature queryParams={queryString.parse(location.search)} />;
+};
+
+const routes = () => (
+  <React.Fragment>
+    <Route
+      key="/settings/emails/"
+      exact={true}
+      path="/settings/emails/"
+      component={emails}
+    />
+
+    <Route
+      key="/settings/emails/signatures"
+      exact={true}
+      path="/settings/emails/signatures"
+      component={emailSignatures}
+    />
+  </React.Fragment>
+);
 
 export default routes;
--- src/modules/settings/general/routes.tsx
+++ src/modules/settings/general/routes.tsx
@@ -3,12 +3,10 @@ import * as React from 'react';
 import { Route } from 'react-router-dom';
 import { List } from './containers';
 
-const routes = () => {
-  const general = ({ location }) => {
-    return <List queryParams={queryString.parse(location.search)} />;
-  };
-
-  return <Route path="/settings/general/" component={general} />;
+const general = ({ location }) => {
+  return <List queryParams={queryString.parse(location.search)} />;
 };
+
+const routes = () => <Route path="/settings/general/" component={general} />;
 
 export default routes;
