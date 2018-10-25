3ee28bdd4d3c05558c99f715f4e507aa2b0b5b63
Khangarid Bayarsaikhan <khangarid.b@nmma.co>
2018-10-25 18:45:39 +0800

    Fixed account defaults route warning

--- src/modules/settings/general/routes.tsx
+++ src/modules/settings/general/routes.tsx
@@ -1,10 +1,9 @@
-import queryString from 'query-string';
 import * as React from 'react';
 import { Route } from 'react-router-dom';
 import { List } from './containers';
 
-const general = ({ location }) => {
-  return <List queryParams={queryString.parse(location.search)} />;
+const general = () => {
+  return <List />;
 };
 
 const routes = () => <Route path="/settings/general/" component={general} />;
