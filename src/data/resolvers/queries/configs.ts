1eea2d8633a9b2e4fb2142cb66720d095554eb8f
Khangarid Bayarsaikhan <khangarid.b@nmma.co>
2018-10-26 18:23:43 +0800

    Added project versions query

--- src/data/resolvers/queries/configs.ts
+++ src/data/resolvers/queries/configs.ts
@@ -1,5 +1,45 @@
+import * as gitRepoInfo from 'git-repo-info';
+import * as path from 'path';
 import { Configs } from '../../../db/models';
 import { moduleRequireLogin } from '../../permissions';
+
+interface IInfo {
+  branch: string; // current branch
+  sha: string; // current sha
+  abbreviatedSha: string; // first 10 chars of the current sha
+  tag: string; // tag for the current sha (or `null` if no tag exists)
+  lastTag: string; // tag for the closest tagged ancestor
+  //   (or `null` if no ancestor is tagged)
+  commitsSinceLastTag: string; // number of commits since the closest tagged ancestor
+  //   (`0` if this commit is tagged, or `Infinity` if no ancestor is tagged)
+  committer: string; // committer for the current sha
+  committerDate: string; // commit date for the current sha
+  author: string; // author for the current sha
+  authorDate: string; // authored date for the current sha
+  commitMessage: string; // commit message for the current sha
+}
+
+const getGitInfos = (projectPath: string) => {
+  let packageVersion: string = 'N/A';
+
+  try {
+    packageVersion = require(path.join(projectPath, 'package.json')).version;
+  } catch (e) {
+    return;
+  }
+
+  const info: IInfo = gitRepoInfo(projectPath);
+
+  return {
+    packageVersion,
+    lastCommittedUser: info.committer || info.author || 'N/A',
+    lastCommittedDate: info.committerDate || info.authorDate || 'N/A',
+    lastCommitMessage: info.commitMessage || 'N/A',
+    branch: info.branch,
+    sha: info.sha,
+    abbreviatedSha: info.abbreviatedSha,
+  };
+};
 
 const configQueries = {
   /**
@@ -7,6 +47,24 @@ const configQueries = {
    */
   configsDetail(_root, { code }: { code: string }) {
     return Configs.findOne({ code });
+  },
+
+  versions(_root) {
+    const { erxesPath, apiPath, widgetPath, widgetApiPath } = process.env;
+
+    const erxesProjectPath = erxesPath || `${process.cwd()}/../erxes`;
+    const apiProjectPath = apiPath || process.cwd();
+    const widgetProjectPath = widgetPath || `${process.cwd()}/../erxes-widgets`;
+    const widgetApiProjectPath = widgetApiPath || `${process.cwd()}/../erxes-widgets-api`;
+
+    const response = {
+      erxesVersion: getGitInfos(erxesProjectPath),
+      apiVersion: getGitInfos(apiProjectPath),
+      widgetVersion: getGitInfos(widgetProjectPath),
+      widgetApiVersion: getGitInfos(widgetApiProjectPath),
+    };
+
+    return response;
   },
 };
 
--- src/data/schema/config.ts
+++ src/data/schema/config.ts
@@ -4,10 +4,28 @@ export const types = `
     code: String!
     value: [String]!
   }
+
+  type GitInfos {
+    packageVersion: String
+    lastCommittedUser: String
+    lastCommittedDate: String
+    lastCommitMessage: String
+    branch: String
+    sha: String
+    abbreviatedSha: String
+  }
+
+  type ProjectInfos {
+    erxesVersion: GitInfos
+    apiVersion: GitInfos
+    widgetVersion: GitInfos
+    widgetApiVersion: GitInfos
+  }
 `;
 
 export const queries = `
   configsDetail(code: String!): Config
+  versions: ProjectInfos
 `;
 
 export const mutations = `
