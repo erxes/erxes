ec6fc5c8b042d8f97ed71d38a3761aeec3861fad
Anu-ujin <anu.b@nmma.co>
2018-10-30 14:13:31 +0800

    Fix inbox sidebar isIdle hover color

--- src/modules/common/styles/colors.ts
+++ src/modules/common/styles/colors.ts
@@ -27,7 +27,7 @@ const bgDark = rgba('#000', 0.95);
 const bgLight = '#FAFAFA';
 const bgActive = '#F0F0F0';
 const bgUnread = '#F7F8FC';
-const bgInternal = '#FFFCCC';
+const bgInternal = '#FFEDED';
 const bgIdle = '#FCE1E1';
 
 // Link colors
