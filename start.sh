fd4416ab73cc46d972fcd137b3e91df101381501
shinebayar-g <shinebayar.gansukh@gmail.com>
2019-01-03 17:28:48 +0800

    Add startup script on dockerfile to support Google credentials

---
+++ start.sh
@@ -0,0 +1,11 @@
+echo "`jo \`env | grep type\``" >> /erxes-api/google_cred.json
+echo "`jo \`env | grep project_id\``" >> /erxes-api/google_cred.json
+echo "`jo \`env | grep private_key_id\``" >> /erxes-api/google_cred.json
+echo "`jo \`env | grep private_key\``" >> /erxes-api/google_cred.json
+echo "`jo \`env | grep client_email\``" >> /erxes-api/google_cred.json
+echo "`jo \`env | grep client_id\``" >> /erxes-api/google_cred.json
+echo "`jo \`env | grep auth_uri\``" >> /erxes-api/google_cred.json
+echo "`jo \`env | grep token_uri\``" >> /erxes-api/google_cred.json
+echo "`jo \`env | grep auth_provider_x509_cert_url\``" >> /erxes-api/google_cred.json
+echo "`jo \`env | grep client_x509_cert_url\``" >> /erxes-api/google_cred.json
+yarn start
