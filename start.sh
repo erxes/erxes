b172974a6951f9ef433b3d273ddec39d2c6af362
shinebayar-g <shinebayar.gansukh@gmail.com>
2019-01-03 20:33:27 +0800

    Update start script for string escape

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 echo "`jo \`env | grep type\``" >> /erxes-api/google_cred.json
 echo "`jo \`env | grep project_id\``" >> /erxes-api/google_cred.json
 echo "`jo \`env | grep private_key_id\``" >> /erxes-api/google_cred.json
-echo "`jo \`env | grep private_key\``" >> /erxes-api/google_cred.json
+echo "`jo private_key="$private_key"`" >> /erxes-api/google_cred.json
 echo "`jo \`env | grep client_email\``" >> /erxes-api/google_cred.json
 echo "`jo \`env | grep client_id\``" >> /erxes-api/google_cred.json
 echo "`jo \`env | grep auth_uri\``" >> /erxes-api/google_cred.json
