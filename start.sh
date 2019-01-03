349955e0446b0b54d8ca0235b85614b7854cd5a0
shinebayar-g <shinebayar.gansukh@gmail.com>
2019-01-03 22:02:33 +0800

    Fix start script JSON

--- start.sh
+++ start.sh
@@ -1,11 +1,12 @@
-echo "`jo \`env | grep type\``" >> /erxes-api/google_cred.json
-echo "`jo \`env | grep project_id\``" >> /erxes-api/google_cred.json
-echo "`jo \`env | grep private_key_id\``" >> /erxes-api/google_cred.json
-echo "`jo private_key="$private_key"`" >> /erxes-api/google_cred.json
-echo "`jo \`env | grep client_email\``" >> /erxes-api/google_cred.json
-echo "`jo \`env | grep client_id\``" >> /erxes-api/google_cred.json
-echo "`jo \`env | grep auth_uri\``" >> /erxes-api/google_cred.json
-echo "`jo \`env | grep token_uri\``" >> /erxes-api/google_cred.json
-echo "`jo \`env | grep auth_provider_x509_cert_url\``" >> /erxes-api/google_cred.json
-echo "`jo \`env | grep client_x509_cert_url\``" >> /erxes-api/google_cred.json
+echo "`jo \`env | grep type\` \
+          \`env | grep project_id\` \
+          \`env | grep private_key_id\` \
+          \`env | grep client_email\` \
+          \`env | grep client_id\` \
+          \`env | grep auth_uri\` \
+          \`env | grep token_uri\` \
+          \`env | grep auth_provider_x509_cert_url\` \
+          \`env | grep client_x509_cert_url\` \
+          private_key="$private_key" \
+          `" > test.json
 yarn start
