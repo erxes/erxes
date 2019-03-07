facaa4f747c0416ba5ea610908b4d6696dd00858
Munkhbold <munkhbold.d@nmtec.co>
2019-03-07 15:44:56 +0800

    complete account mutations

--- src/__tests__/accountMutations.test.ts
+++ src/__tests__/accountMutations.test.ts
@@ -1,4 +1,5 @@
 import * as sinon from 'sinon';
+import accountMutations from '../data/resolvers/mutations/accounts';
 import { graphqlRequest } from '../db/connection';
 import {
   accountFactory,
@@ -10,6 +11,8 @@ import {
 } from '../db/factories';
 import { Accounts, Brands, ConversationMessages, Conversations, Customers, Integrations, Users } from '../db/models';
 import { utils } from '../trackers/gmailTracker';
+import { googleUtils } from '../trackers/googleTracker';
+import { socUtils } from '../trackers/twitterTracker';
 
 describe('Accounts mutations', () => {
   let _account;
@@ -39,8 +42,24 @@ describe('Accounts mutations', () => {
     await Integrations.deleteMany({});
   });
 
+  test(`test if Error('Login required') exception is working as intended`, async () => {
+    expect.assertions(3);
+
+    const expectError = async func => {
+      try {
+        await func(null, {}, {});
+      } catch (e) {
+        expect(e.message).toBe('Login required');
+      }
+    };
+
+    expectError(accountMutations.accountsRemove);
+    expectError(accountMutations.accountsAddTwitter);
+    expectError(accountMutations.accountsAddGmail);
+  });
+
   test('Remove account mutation test', async () => {
-    const mutation = `
+    const mutationQuery = `
       mutation accountsRemove($_id: String!) {
         accountsRemove(_id: $_id)
       }
@@ -70,8 +89,14 @@ describe('Accounts mutations', () => {
       },
     });
 
+    try {
+      await graphqlRequest(mutationQuery, 'accountsRemove', { _id: 'fakeId' }, context);
+    } catch (e) {
+      expect(e[0].message).toBe(`Account not found id with fakeId`);
+    }
+
     const mock = sinon.stub(utils, 'stopReceivingEmail').callsFake();
-    await graphqlRequest(mutation, 'accountsRemove', { _id: _account._id }, context);
+    await graphqlRequest(mutationQuery, 'accountsRemove', { _id: _account._id }, context);
     mock.restore();
 
     expect(await Accounts.findOne({ _id: _account._id })).toBe(null);
@@ -79,5 +104,94 @@ describe('Accounts mutations', () => {
     expect(await Customers.findOne({ integrationId: _integration._id })).toBe(null);
     expect(await Conversations.findOne({ integrationId: _integration._id })).toBe(null);
     expect(await ConversationMessages.findOne({ conversationId: conversation._id })).toBe(null);
+  });
+
+  test('accountsAddGmail', async () => {
+    const mutationQuery = `
+      mutation accountsAddGmail($code: String!) {
+        accountsAddGmail(code: $code){
+          id
+        }
+      }
+    `;
+    const params = {
+      access_token: 'access_token',
+      refresh_token: 'refresh_token',
+      expiry_date: 'expiry_date',
+      scope: 'scope',
+    };
+    const emailAddress = 'test@gmail.com';
+
+    const getAccessToken = jest.spyOn(googleUtils, 'getAccessToken');
+    getAccessToken.mockImplementation(() => params);
+
+    const getGmailUserProfile = jest.spyOn(utils, 'getGmailUserProfile');
+    const result = { data: { emailAddress: 'test@gmail.com' } };
+    getGmailUserProfile.mockImplementation(() => result);
+
+    const { id } = await graphqlRequest(mutationQuery, 'accountsAddGmail', { code: 'code' }, context);
+
+    expect(getAccessToken.mock.calls.length).toBe(1);
+    expect(getGmailUserProfile.mock.calls.length).toBe(1);
+
+    const newAccount = await Accounts.findOne({ _id: id });
+
+    if (!newAccount) {
+      throw new Error('Account does not exists');
+    }
+
+    expect(newAccount).toBeDefined();
+    expect(newAccount.uid).toBe(emailAddress);
+    expect(newAccount.name).toBe(emailAddress);
+    expect(newAccount.kind).toBe('gmail');
+    expect(newAccount.token).toBe(params.access_token);
+    expect(newAccount.tokenSecret).toBe(params.refresh_token);
+    expect(newAccount.expireDate).toBe(params.expiry_date);
+    expect(newAccount.scope).toBe(params.scope);
+  });
+
+  test('accountsAddTwitter', async () => {
+    const mutationQuery = `
+      mutation accountsAddTwitter($queryParams: TwitterIntegrationAuthParams) {
+        accountsAddTwitter(queryParams: $queryParams){
+          id
+          name
+          kind
+        }
+      }
+    `;
+
+    const params = {
+      tokens: {
+        auth: {
+          token: 'token',
+          token_secret: 'token_secret',
+        },
+      },
+      info: {
+        name: 'name',
+        id: 'id',
+      },
+    };
+
+    const authenticate = jest.spyOn(socUtils, 'authenticate');
+    authenticate.mockImplementation(() => params);
+
+    const { id } = await graphqlRequest(mutationQuery, 'accountsAddTwitter', {}, context);
+
+    expect(authenticate.mock.calls.length).toBe(1);
+
+    const newAccount = await Accounts.findOne({ _id: id });
+
+    if (!newAccount) {
+      throw new Error('Account does not exists');
+    }
+
+    expect(newAccount).toBeDefined();
+    expect(newAccount.uid).toBe(params.info.id);
+    expect(newAccount.name).toBe(params.info.name);
+    expect(newAccount.kind).toBe('twitter');
+    expect(newAccount.token).toBe(params.tokens.auth.token);
+    expect(newAccount.tokenSecret).toBe(params.tokens.auth.token_secret);
   });
 });
