700c2633c5450812b23ed733edf0f42bcee72002
Khangarid Bayarsaikhan <32699094+khangaridb@users.noreply.github.com>
2019-04-03 12:50:58 +0800

    Fix fb feed (#370)
    * Fixed facebook  invalid date, Fb login will now update user's access token
    * Fixed engage message mutation test

--- src/trackers/facebook.ts
+++ src/trackers/facebook.ts
@@ -17,7 +17,7 @@ import { IIntegrationDocument } from '../db/models/definitions/integrations';
 import { findPostComments, graphRequest, IComments, IPost } from './facebookTracker';
 
 interface IPostParams {
-  created_time?: string;
+  created_time?: number;
   post_id?: string;
   video_id?: string;
   link?: string;
@@ -37,7 +37,7 @@ interface IPostParams {
 
 interface ICommentParams {
   post_id: string;
-  created_time?: string;
+  created_time?: number;
   parent_id?: string;
   item?: string;
   comment_id?: string;
@@ -195,7 +195,7 @@ export class SaveWebhookResponse {
     }
 
     if (created_time) {
-      doc.createdTime = created_time;
+      doc.createdTime = (created_time * 1000).toString();
     }
 
     return doc;
@@ -226,7 +226,7 @@ export class SaveWebhookResponse {
     }
 
     if (created_time) {
-      doc.createdTime = created_time;
+      doc.createdTime = (created_time * 1000).toString();
     }
 
     // Counting post comments only
@@ -727,7 +727,7 @@ export class SaveWebhookResponse {
           senderId: comment.from.id,
           senderName: comment.from.name,
           parentId: comment.parent && comment.parent.id,
-          createdTime: comment.created_time,
+          createdTime: (comment.created_time || 0 * 1000).toString(),
           commentCount: comment.comments.summary.total_count,
         },
         restoring: true,
@@ -868,15 +868,6 @@ export const facebookReply = async (
 
     // save commentId and parentId in message object
     await ConversationMessages.updateOne({ _id: message._id }, { $set: { facebookData } });
-
-    // finding parent post and increasing comment count
-    await ConversationMessages.updateMany(
-      {
-        'facebookData.isPost': true,
-        conversationId: message.conversationId,
-      },
-      { $inc: { 'facebookData.commentCount': 1 } },
-    );
   }
 };
 
