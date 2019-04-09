13dc3e5730a21da5d4e84413ef74e139cd9711b8
Anu-Ujin Bat-Ulzii <anu.b@nmma.co>
2019-04-09 15:42:20 +0800

    Show social accounts and full basic infos (#877)
    * Show social accounts and full basic infos
    
    * Show full action description

--- src/modules/common/components/nameCard/NameCard.tsx
+++ src/modules/common/components/nameCard/NameCard.tsx
@@ -91,7 +91,7 @@ class NameCard extends React.Component<Props> {
 
     if (user || firstLine || secondLine) {
       first = firstLine || this.renderUserName();
-      second = (user && user.email) || this.renderUserName();
+      second = secondLine || (user && user.email) || this.renderUserName();
     }
 
     if (customer) {
