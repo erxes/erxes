c185a62a6c424b9d0945b8fc67fc521fd07c35df
uuganbayar.b <uuganbayar.b@nmma.co>
2019-04-16 19:47:42 +0800

    en/index remove

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -24,15 +24,16 @@ const users = [
 const siteConfig = {
   title: 'Erxes', // Title for your website.
   tagline: 'Documentation',
-  url: 'https://your-docusaurus-test-site.com', // Your website URL
+  url: 'https://docs.erxes.io', // Your website URL
   baseUrl: '/', // Base URL for your project */
+  cname: 'docs.erxes.io',
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'Erxes',
-  organizationName: 'Erxes Inc',
+  projectName: 'docs',
+  organizationName: 'erxes',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
