b39b53dfec18ec39817e890d81c2f025e49222e9
shinebayar-g <shinebayar.gansukh@gmail.com>
2019-04-16 18:04:57 +0800

    Update url

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -21,8 +21,9 @@ const users = [{
 const siteConfig = {
   title: 'Test Site', // Title for your website.
   tagline: 'A website for testing',
-  url: 'https://erxes.github.io', // Your website URL
+  url: 'https://docs.erxes.io', // Your website URL
   baseUrl: '/', // Base URL for your project */
+  cname: 'docs.erxes.io',
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
