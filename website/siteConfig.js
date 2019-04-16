311ebfb931f28822eed2e32461ef923aef19127e
shinebayar-g <shinebayar.gansukh@gmail.com>
2019-04-16 17:20:21 +0800

    Setup GH pages

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -9,39 +9,48 @@
 // site configuration options.
 
 // List of projects/orgs using your project for the users page.
-const users = [
-  {
-    caption: 'User1',
-    // You will need to prepend the image path with your baseUrl
-    // if it is not '/', like: '/test-site/img/docusaurus.svg'.
-    image: '/img/docusaurus.svg',
-    infoLink: 'https://www.facebook.com',
-    pinned: true,
-  },
-];
+const users = [{
+  caption: 'User1',
+  // You will need to prepend the image path with your baseUrl
+  // if it is not '/', like: '/test-site/img/docusaurus.svg'.
+  image: '/docs/img/docusaurus.svg',
+  infoLink: 'https://www.facebook.com',
+  pinned: true,
+}, ];
 
 const siteConfig = {
   title: 'Test Site', // Title for your website.
   tagline: 'A website for testing',
-  url: 'https://your-docusaurus-test-site.com', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  url: 'https://erxes.github.io', // Your website URL
+  baseUrl: '/docs/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'test-site',
-  organizationName: 'facebook',
+  projectName: 'docs',
+  organizationName: 'erxes',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
 
   // For no header links in the top nav bar -> headerLinks: [],
-  headerLinks: [
-    {doc: 'doc1', label: 'Docs'},
-    {doc: 'doc4', label: 'API'},
-    {page: 'help', label: 'Help'},
-    {blog: true, label: 'Blog'},
+  headerLinks: [{
+      doc: 'doc1',
+      label: 'Docs'
+    },
+    {
+      doc: 'doc4',
+      label: 'API'
+    },
+    {
+      page: 'help',
+      label: 'Help'
+    },
+    {
+      blog: true,
+      label: 'Blog'
+    },
   ],
 
   // If you have users set above, you add it here:
