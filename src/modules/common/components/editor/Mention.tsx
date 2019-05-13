223b6c7a9210b38052768e959c1fee26dc8d0eff
munkhjin <munkhjin0223@gmail.com>
2019-05-13 20:25:43 +0800

    Delete console.log

--- src/modules/common/components/editor/Mention.tsx
+++ src/modules/common/components/editor/Mention.tsx
@@ -18,9 +18,6 @@ interface ITeamMembers {
 const MentionEntry = props => {
   const { mention, theme, searchValue, ...parentProps } = props;
 
-  // parentProps.className = parentProps.className + ' ' + 'mentionSuggestionsContainer';
-  console.log('parentProps: ', parentProps); // tslint:disable-line
-
   return (
     <div {...parentProps}>
       <div className="mentionSuggestionsEntryContainer">
