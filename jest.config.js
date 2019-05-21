fe8ab6300f4c53f3229803d9b6c90ce458aa15dd
Buyantogtokh <buyantogtokh.s@nmma.co>
2019-05-21 13:11:30 +0800

    Changed string quote

--- jest.config.js
+++ jest.config.js
@@ -1,21 +1,21 @@
 module.exports = {
-  preset: 'ts-jest',
-  roots: ['<rootDir>/src'],
+  preset: "ts-jest",
+  roots: ["<rootDir>/src"],
   transform: {
-    '^.+\\.tsx$': 'ts-jest'
+    "^.+\\.tsx$": "ts-jest"
   },
    // If you want to use testRegex instead in your configuration, you MUST set testMatch to null or Jest will bail.
   testMatch: null,
-  testRegex: '/__tests__/.*\\.(ts|js|tsx)$',
-  testEnvironment: 'jsdom',
-  moduleFileExtensions: ['ts', 'js', 'json', 'node', 'tsx'],
-  modulePaths: ['<rootDir>/src'],
+  testRegex: "/__tests__/.*\\.(ts|js|tsx)$",
+  testEnvironment: "jsdom",
+  moduleFileExtensions: ["ts", "js", "json", "node", "tsx"],
+  modulePaths: ["<rootDir>/src"],
   globals: {
-    'ts-jest': {
-      tsConfigFile: 'tsconfig.json'
+    "ts-jest": {
+      tsConfigFile: "tsconfig.json"
     },
     // without it, compilation error occurs in apolloClient
-    'fetch': {}
+    "fetch": {}
   },
-  setupTestFrameworkScriptFile: './src/setupTests.ts',
+  setupTestFrameworkScriptFile: "./src/setupTests.ts",
 };
