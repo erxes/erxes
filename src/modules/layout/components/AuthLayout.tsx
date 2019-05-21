ea84d4a8ff827192345a5c09b93f1f65691807d6
Ujin's iMac <anu.b@nmma.co>
2019-05-21 14:29:21 +0800

    Add check ios or android condition

--- src/modules/layout/components/AuthLayout.tsx
+++ src/modules/layout/components/AuthLayout.tsx
@@ -16,22 +16,39 @@ type Props = {
 };
 
 class AuthLayout extends React.Component<Props, {}> {
-  renderRecommendMobileVersion() {
-    if (navigator.userAgent.indexOf('Mobile') !== -1) {
-      return (
-        <SmallWrapper>
-          <CenterContent>
-            <AppName>
-              <b>Erxes Inc</b>
-              <span>Download ios app for free on the App Store.</span>
-            </AppName>
+  renderContent(desciption: string, link: string) {
+    return (
+      <SmallWrapper>
+        <CenterContent>
+          <AppName>
+            <b>Erxes Inc</b>
+            <span>{desciption}</span>
+          </AppName>
+          <a href={link} target="_blank">
             <Button btnStyle="link" size="small">
               Get
             </Button>
-          </CenterContent>
-          {/* <img src="/images/ios.png" /> */}
-        </SmallWrapper>
-      );
+          </a>
+        </CenterContent>
+      </SmallWrapper>
+    );
+  }
+
+  renderRecommendMobileVersion() {
+    if (navigator.userAgent.indexOf('Mobile') !== -1) {
+      if (navigator.userAgent.match(/iPhone|iPad|iPod/i)) {
+        return this.renderContent(
+          'Download ios app for free on the App Store.',
+          'https://itunes.apple.com/zw/app/erxes-inc/id1454657885?mt=8&fbclid=IwAR1_A-3dPkw4oUh3r-4lpAvs_Ie5FWOTy1dduFy7eJZbpWKJJ9ukzu9ZNUc'
+        );
+      }
+
+      if (navigator.userAgent.match(/Android/i)) {
+        return this.renderContent(
+          'Download android app for free on the Google play.',
+          'https://play.google.com/store/apps/details?id=io.erxes.erxes_android&fbclid=IwAR1bVPBSE0pC_KUNNjOJQA4upb1AuTUfqFcDaHTHTptyke7rNvuvb2mgwb0'
+        );
+      }
     }
 
     return null;
@@ -39,13 +56,6 @@ class AuthLayout extends React.Component<Props, {}> {
 
   render() {
     const { content } = this.props;
-
-    // tslint:disable-next-line:no-console
-    console.log(
-      window.orientation,
-      navigator.userAgent,
-      navigator.userAgent.indexOf('Mobile') !== -1
-    );
 
     return (
       <Authlayout>
