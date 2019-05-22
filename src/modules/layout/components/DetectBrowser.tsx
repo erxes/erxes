a3897f76470106bea1bcfdba915f5a744a498f16
Ujin's iMac <anu.b@nmma.co>
2019-05-22 15:32:41 +0800

    Add close warning function

--- src/modules/layout/components/DetectBrowser.tsx
+++ src/modules/layout/components/DetectBrowser.tsx
@@ -3,26 +3,59 @@ import { __ } from 'modules/common/utils';
 import { AlertItem } from 'modules/common/utils/Alert/Alert';
 import * as React from 'react';
 import styled from 'styled-components';
+import styledTS from 'styled-components-ts';
+import { Icon } from '../../common/components';
 
-const OldBrowserWarning = styled(AlertItem)`
+const OldBrowserWarning = styledTS<{ visible?: boolean }>(styled(AlertItem))`
   position: absolute;
-  left: ${dimensions.headerSpacing - 5}%;
+  left: ${dimensions.headerSpacing - 15}%;
+  max-width: 500px;
+  text-align: center;
+  visibility: ${props => (props.visible ? 'visible' : 'hidden')};
+  transition: all 0.1s;
+
+  > div {
+    margin-top: ${dimensions.unitSpacing - 5}px;
+  }
+
+  .icon-cancel {
+    float: right;
+    cursor: pointer;
+  }
 `;
 
-class DetectBrowser extends React.Component {
-  renderWarning(name: string, oldVersionNumber: string) {
+type State = {
+  isVisible: boolean;
+};
+
+class DetectBrowser extends React.Component<{}, State> {
+  constructor(props: {}) {
+    super(props);
+
+    this.state = { isVisible: true };
+  }
+
+  closeAlert = () => {
+    this.setState({ isVisible: false });
+  };
+
+  renderWarning(name: string, oldVersionNumber: number) {
     const { userAgent } = navigator;
 
     const splittedVersion = userAgent.split(name)[1];
     const browserVersion = splittedVersion.split('.')[0];
 
-    if (browserVersion < oldVersionNumber) {
+    if (Number(browserVersion) < oldVersionNumber) {
       return (
-        <OldBrowserWarning type="error">
-          <b>{__('Please upgrade your browser to use erxes.')}</b>
+        <OldBrowserWarning type="error" visible={this.state.isVisible}>
+          <b>
+            <Icon icon="information" />{' '}
+            {__('Please upgrade your browser to use erxes!')}
+            <Icon icon="cancel" size={12} onClick={this.closeAlert} />
+          </b>
           <div>
             {__(
-              'Unfortunately, You are running on a browser that may not be fully compatible with erxes.'
+              'Unfortunately, You are running on a browser that may not be fully compatible with erxes. '
             )}
             {__(`Please use ${name} version ${oldVersionNumber}+.`)}
           </div>
@@ -37,26 +70,26 @@ class DetectBrowser extends React.Component {
     const { userAgent } = navigator;
 
     if (userAgent.indexOf('Chrome') !== -1) {
-      this.renderWarning('Chrome', '58');
+      return this.renderWarning('Chrome/', 90);
     }
 
     if (
       userAgent.indexOf('Safari') !== -1 &&
       userAgent.indexOf('Chrome') === -1
     ) {
-      this.renderWarning('Version', '11');
+      return this.renderWarning('Version/', 11);
     }
 
     if (userAgent.indexOf('Firefox') !== -1) {
-      this.renderWarning('Firefox', '59');
+      return this.renderWarning('Firefox/', 59);
     }
 
     if (userAgent.indexOf('Opera') !== -1 || userAgent.indexOf('OPR') !== -1) {
-      this.renderWarning('Opera', '45');
+      return this.renderWarning('Opera/', 45);
     }
 
     if (userAgent.indexOf('Edge') !== -1) {
-      this.renderWarning('Edge', '16');
+      return this.renderWarning('Edge/', 16);
     }
 
     return null;
