c7d954ccb54d9b5423e6cc60127f4ae8d51a691d
batamar <batamar.b@nmtec.co>
2019-06-04 15:18:50 +0800

    Cleanup

--- conversations.ts
+++ conversations.ts
@@ -1,61 +1,6 @@
 import { Document, Schema } from 'mongoose';
 import { field } from '../utils';
-import { CONVERSATION_STATUSES, FACEBOOK_DATA_KINDS } from './constants';
-
-export interface ITwitterResponse {
-  id?: number;
-  id_str: string;
-  created_at?: string;
-  isDirectMessage?: boolean;
-
-  // media content
-  entities?: any;
-  extended_entities?: any;
-  extended_tweet?: any;
-
-  // direct message
-  sender_id?: number;
-  sender_id_str?: string;
-  recipient_id?: number;
-  recipient_id_str?: string;
-
-  // timeline
-  in_reply_to_status_id?: number;
-  in_reply_to_status_id_str?: string;
-  in_reply_to_user_id?: number;
-  in_reply_to_user_id_str?: string;
-  in_reply_to_screen_name?: string;
-  is_quote_status?: boolean;
-  favorited?: boolean;
-  retweeted?: boolean;
-  quote_count?: number;
-  reply_count?: number;
-  retweet_count?: number;
-  favorite_count?: number;
-}
-
-export interface ITwitterResponseDocument extends ITwitterResponse, Document {
-  id?: number;
-}
-
-export interface IFacebook {
-  kind?: string;
-  senderName?: string;
-  senderId?: string;
-  recipientId?: string;
-
-  // when wall post
-  postId?: string;
-
-  pageId?: string;
-}
-
-export interface IGmail {
-  messageId?: string;
-}
-
-export interface IFacebookDocument extends IFacebook, Document {}
-export interface IGmailDocument extends IGmail, Document {}
+import { CONVERSATION_STATUSES } from './constants';
 
 export interface IConversation {
   content?: string;
@@ -75,9 +20,6 @@ export interface IConversation {
 
   // number of total conversations
   number?: number;
-  twitterData?: ITwitterResponse;
-  facebookData?: IFacebook;
-  gmailData?: IGmail;
 
   firstRespondedUserId?: string;
   firstRespondedDate?: Date;
@@ -86,95 +28,9 @@ export interface IConversation {
 // Conversation schema
 export interface IConversationDocument extends IConversation, Document {
   _id: string;
-  twitterData?: ITwitterResponseDocument;
-  facebookData?: IFacebookDocument;
-  gmailData?: IGmailDocument;
   createdAt: Date;
   updatedAt: Date;
 }
-
-/*
- * Twitter response schema
- * Using in conversation, conversation message
- * Saving fields with underscores because, we want to store it exactly
- * like twitter response so that we can use it in findParentTweets helper to
- * not send extra request to twitter
- */
-export const twitterResponseSchema = new Schema(
-  {
-    id: field({ type: Number, optional: true }),
-    id_str: field({ type: String }),
-    created_at: field({ type: Date, optional: true }),
-    isDirectMessage: field({ type: Boolean }),
-
-    // media content
-    entities: field({ type: Object, optional: true }),
-    extended_entities: field({ type: Object, optional: true }),
-    extended_tweet: field({ type: Object, optional: true }),
-
-    // direct message
-    sender_id: field({ type: Number }),
-    sender_id_str: field({ type: String }),
-    recipient_id: field({ type: Number }),
-    recipient_id_str: field({ type: String }),
-
-    // timeline
-    in_reply_to_status_id: field({ type: Number, optional: true }),
-    in_reply_to_status_id_str: field({ type: String, optional: true }),
-    in_reply_to_user_id: field({ type: Number, optional: true }),
-    in_reply_to_user_id_str: field({ type: String, optional: true }),
-    in_reply_to_screen_name: field({ type: String, optional: true }),
-    is_quote_status: field({ type: Boolean }),
-    favorited: field({ type: Boolean }),
-    retweeted: field({ type: Boolean }),
-    quote_count: field({ type: Number, optional: true }),
-    reply_count: field({ type: Number, optional: true }),
-    retweet_count: field({ type: Number, optional: true }),
-    favorite_count: field({ type: Number, optional: true }),
-  },
-  { _id: false },
-);
-
-// facebook schema
-const facebookSchema = new Schema(
-  {
-    kind: field({
-      type: String,
-      enum: FACEBOOK_DATA_KINDS.ALL,
-    }),
-    senderName: field({
-      type: String,
-    }),
-    senderId: field({
-      type: String,
-      index: true,
-    }),
-    recipientId: field({
-      type: String,
-      index: true,
-    }),
-
-    // when wall post
-    postId: field({
-      type: String,
-    }),
-
-    pageId: field({
-      type: String,
-    }),
-  },
-  { _id: false },
-);
-
-// facebook schema
-const gmailSchema = new Schema(
-  {
-    messageId: field({
-      type: String,
-    }),
-  },
-  { _id: false },
-);
 
 // Conversation schema
 export const conversationSchema = new Schema({
@@ -209,9 +65,6 @@ export const conversationSchema = new Schema({
 
   // number of total conversations
   number: field({ type: Number }),
-  twitterData: field({ type: twitterResponseSchema }),
-  facebookData: field({ type: facebookSchema }),
-  gmailData: field({ type: gmailSchema }),
 
   firstRespondedUserId: field({ type: String }),
   firstRespondedDate: field({ type: Date }),
