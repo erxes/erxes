a03d6d8a74a02e091ecc08570c30339cfa2ddb92
Munkhjin <munkhjin0223@users.noreply.github.com>
2019-06-12 21:41:00 +0800

    Feature ticket (#444)
    erxes/erxes#979

--- deals.ts
+++ deals.ts
@@ -1,41 +1,11 @@
 import { Document, Schema } from 'mongoose';
 import { field } from '../utils';
-import { PIPELINE_VISIBLITIES, PROBABILITY, PRODUCT_TYPES } from './constants';
+import { PRODUCT_TYPES } from './constants';
 
 interface ICommonFields {
   userId?: string;
   createdAt?: Date;
   order?: number;
-}
-
-export interface IBoard extends ICommonFields {
-  name?: string;
-  isDefault?: boolean;
-}
-
-export interface IBoardDocument extends IBoard, Document {
-  _id: string;
-}
-
-export interface IPipeline extends ICommonFields {
-  name?: string;
-  boardId?: string;
-  visiblity?: string;
-  memberIds?: string[];
-}
-
-export interface IPipelineDocument extends IPipeline, Document {
-  _id: string;
-}
-
-export interface IStage extends ICommonFields {
-  name?: string;
-  probability?: string;
-  pipelineId?: string;
-}
-
-export interface IStageDocument extends IStage, Document {
-  _id: string;
 }
 
 export interface IProduct {
@@ -90,40 +60,6 @@ const commonFieldsSchema = {
   }),
   order: field({ type: Number }),
 };
-
-export const boardSchema = new Schema({
-  _id: field({ pkey: true }),
-  name: field({ type: String }),
-  isDefault: field({
-    type: Boolean,
-    default: false,
-  }),
-  ...commonFieldsSchema,
-});
-
-export const pipelineSchema = new Schema({
-  _id: field({ pkey: true }),
-  name: field({ type: String }),
-  boardId: field({ type: String }),
-  visiblity: field({
-    type: String,
-    enum: PIPELINE_VISIBLITIES.ALL,
-    default: PIPELINE_VISIBLITIES.PUBLIC,
-  }),
-  memberIds: field({ type: [String] }),
-  ...commonFieldsSchema,
-});
-
-export const stageSchema = new Schema({
-  _id: field({ pkey: true }),
-  name: field({ type: String }),
-  probability: field({
-    type: String,
-    enum: PROBABILITY.ALL,
-  }), // Win probability
-  pipelineId: field({ type: String }),
-  ...commonFieldsSchema,
-});
 
 export const productSchema = new Schema({
   _id: field({ pkey: true }),
