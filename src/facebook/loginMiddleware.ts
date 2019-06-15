61f842fc8bb24c5b7f9e8d8608ada7cfa3b0fe8d
batamar <batamar.b@nmtec.co>
2019-06-15 19:25:29 +0800

    Added debugger in fb login middleware

--- src/facebook/loginMiddleware.ts
+++ src/facebook/loginMiddleware.ts
@@ -1,4 +1,5 @@
 import * as graph from 'fbgraph';
+import { debugFacebook, debugRequest, debugResponse } from '../debuggers';
 import Accounts from '../models/Accounts';
 import { graphRequest } from './utils';
 
@@ -18,6 +19,8 @@ const loginMiddleware = (req, res) => {
     redirect_uri: `${DOMAIN}/fblogin`,
   };
 
+  debugRequest(debugFacebook, req);
+
   // we don't have a code yet
   // so we'll redirect to the oauth dialog
   if (!req.query.code) {
@@ -27,51 +30,58 @@ const loginMiddleware = (req, res) => {
       scope: conf.scope,
     });
 
+    // checks whether a user denied the app facebook login/permissions
     if (!req.query.error) {
-      // checks whether a user denied the app facebook login/permissions
-      res.redirect(authUrl);
+      debugResponse(debugFacebook, req, authUrl);
+      return res.redirect(authUrl);
     } else {
-      res.send('access denied');
+      debugResponse(debugFacebook, req, 'access denied');
+      return res.send('access denied');
     }
   }
+
+  const config = {
+    client_id: conf.client_id,
+    redirect_uri: conf.redirect_uri,
+    client_secret: conf.client_secret,
+    code: req.query.code,
+  };
+
+  debugResponse(debugFacebook, req, JSON.stringify(config));
 
   // If this branch executes user is already being redirected back with
   // code (whatever that is)
   // code is set
   // we'll send that and get the access token
-  return graph.authorize(
-    {
-      client_id: conf.client_id,
-      redirect_uri: conf.redirect_uri,
-      client_secret: conf.client_secret,
-      code: req.query.code,
-    },
-    async (_err, facebookRes) => {
-      const { access_token } = facebookRes;
+  return graph.authorize(config, async (_err, facebookRes) => {
+    const { access_token } = facebookRes;
 
-      const userAccount: { id: string; first_name: string; last_name: string } = await graphRequest.get(
-        'me?fields=id,first_name,last_name',
-        access_token,
-      );
+    const userAccount: { id: string; first_name: string; last_name: string } = await graphRequest.get(
+      'me?fields=id,first_name,last_name',
+      access_token,
+    );
 
-      const name = `${userAccount.first_name} ${userAccount.last_name}`;
+    const name = `${userAccount.first_name} ${userAccount.last_name}`;
 
-      const account = await Accounts.findOne({ uid: userAccount.id });
+    const account = await Accounts.findOne({ uid: userAccount.id });
 
-      if (account) {
-        await Accounts.updateOne({ _id: account._id }, { $set: { token: access_token } });
-      } else {
-        await Accounts.create({
-          token: access_token,
-          name,
-          kind: 'facebook',
-          uid: userAccount.id,
-        });
-      }
+    if (account) {
+      await Accounts.updateOne({ _id: account._id }, { $set: { token: access_token } });
+    } else {
+      await Accounts.create({
+        token: access_token,
+        name,
+        kind: 'facebook',
+        uid: userAccount.id,
+      });
+    }
 
-      return res.redirect(`${MAIN_APP_DOMAIN}/settings/integrations?fbAuthorized=true`);
-    },
-  );
+    const url = `${MAIN_APP_DOMAIN}/settings/integrations?fbAuthorized=true`;
+
+    debugResponse(debugFacebook, req, url);
+
+    return res.redirect(url);
+  });
 };
 
 export default loginMiddleware;
