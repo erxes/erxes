60c10451de1c5fc1dda2cd0b7b5ee569bab666ff
Ujin's iMac <anu.b@nmma.co>
2019-06-20 13:06:12 +0800

    Fix kb category form

--- src/modules/knowledgeBase/containers/category/CategoryForm.tsx
+++ src/modules/knowledgeBase/containers/category/CategoryForm.tsx
@@ -1,15 +1,11 @@
 import gql from 'graphql-tag';
-import { Alert, withProps } from 'modules/common/utils';
+import { ButtonMutate } from 'modules/common/components';
+import { IButtonMutateProps } from 'modules/common/types';
+import { __ } from 'modules/common/utils';
 import * as React from 'react';
-import { compose, graphql } from 'react-apollo';
 import { CategoryForm } from '../../components';
 import { mutations, queries } from '../../graphql';
-import {
-  AddCategoriesMutationResponse,
-  CategoryVariables,
-  EditCategoriesMutationResponse,
-  ICategory
-} from '../../types';
+import { ICategory } from '../../types';
 
 type Props = {
   category: ICategory;
@@ -18,52 +14,41 @@ type Props = {
   refetchTopics: () => void;
 };
 
-type FinalProps = Props &
-  AddCategoriesMutationResponse &
-  EditCategoriesMutationResponse;
+const KnowledgeBaseContainer = (props: Props) => {
+  const { category, topicIds } = props;
 
-const KnowledgeBaseContainer = (props: FinalProps) => {
-  const {
-    category,
-    topicIds,
-    addCategoriesMutation,
-    editCategoriesMutation,
-    refetchTopics
-  } = props;
-
-  // create or update action
-  const save = ({ doc }, callback, object) => {
-    let mutation = addCategoriesMutation;
-
-    // if edit mode
-    if (object) {
-      mutation = editCategoriesMutation;
-      doc._id = object._id;
-    }
-
-    mutation({
-      variables: doc
-    })
-      .then(() => {
-        Alert.success(
-          `You successfully ${object ? 'updated' : 'added'} a category`
-        );
-
-        callback();
-
-        // only fetch when new category is added
-        if (!object) {
-          refetchTopics();
+  const renderButton = ({
+    name,
+    values,
+    isSubmitted,
+    callback,
+    object
+  }: IButtonMutateProps) => {
+    return (
+      <ButtonMutate
+        mutation={
+          object
+            ? mutations.knowledgeBaseCategoriesEdit
+            : mutations.knowledgeBaseCategoriesAdd
         }
-      })
-      .catch(error => {
-        Alert.error(error.message);
-      });
+        variables={values}
+        callback={callback}
+        refetchQueries={getRefetchQueries(topicIds)}
+        isSubmitted={isSubmitted}
+        type="submit"
+        icon="checked-1"
+        successMessage={`You successfully ${
+          object ? 'updated' : 'added'
+        } a ${name}`}
+      >
+        {__('Save')}
+      </ButtonMutate>
+    );
   };
 
   const extendedProps = {
     ...props,
-    save,
+    renderButton,
     currentTopicId: topicIds,
     category
   };
@@ -71,39 +56,20 @@ const KnowledgeBaseContainer = (props: FinalProps) => {
   return <CategoryForm {...extendedProps} />;
 };
 
-const commonOptions = ({ topicIds }) => {
-  return {
-    refetchQueries: [
-      {
-        query: gql(queries.knowledgeBaseCategories),
-        variables: {
-          topicIds: [topicIds]
-        }
-      },
-      {
-        query: gql(queries.knowledgeBaseCategoriesTotalCount),
-        variables: { topicIds: [topicIds] }
+const getRefetchQueries = (topicIds: string) => {
+  return [
+    {
+      query: gql(queries.knowledgeBaseCategories),
+      variables: {
+        topicIds: [topicIds]
       }
-    ]
-  };
+    },
+    {
+      query: gql(queries.knowledgeBaseCategoriesTotalCount),
+      variables: { topicIds: [topicIds] }
+    },
+    { query: gql(queries.knowledgeBaseTopics) }
+  ];
 };
 
-export default withProps<Props>(
-  compose(
-    graphql<Props, EditCategoriesMutationResponse, CategoryVariables>(
-      gql(mutations.knowledgeBaseCategoriesEdit),
-      {
-        name: 'editCategoriesMutation',
-        options: commonOptions
-      }
-    ),
-
-    graphql<Props, AddCategoriesMutationResponse, CategoryVariables>(
-      gql(mutations.knowledgeBaseCategoriesAdd),
-      {
-        name: 'addCategoriesMutation',
-        options: commonOptions
-      }
-    )
-  )(KnowledgeBaseContainer)
-);
+export default KnowledgeBaseContainer;
