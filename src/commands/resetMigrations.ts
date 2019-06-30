74c09e8863771201869f9393432f64298c0c6f01
batamar <batamar.b@nmtec.co>
2019-06-30 20:39:44 +0800

    Added resetMigrations command

---
+++ src/commands/resetMigrations.ts
@@ -0,0 +1,21 @@
+import * as dotenv from 'dotenv';
+import * as mongoose from 'mongoose';
+
+dotenv.config();
+
+const options = {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+};
+
+const main = async () => {
+  const mongoClient = await mongoose.createConnection(process.env.MONGO_URL || '', options);
+
+  const migrations = mongoClient.db.collection('migrations');
+
+  await migrations.deleteMany({});
+
+  process.exit();
+};
+
+main();
