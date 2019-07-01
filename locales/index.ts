043359a2b42c3a257eef94355fd67213e6a57072
batamar <batamar.b@nmtec.co>
2019-07-01 19:37:57 +0800

    Renamed locale ch to zh

--- locales/index.ts
+++ locales/index.ts
@@ -1,18 +1,20 @@
 const en = require("./en.json");
 const mn = require("./mn.json");
 const es = require("./es.json");
+const fr = require("./fr.json");
 const ptbr = require("./pt-br.json");
 const np = require("./np.json");
-const ch = require("./ch.json");
+const zh = require("./zh.json");
 const vn = require("./vn.json");
 
 const languages: any = {
   en,
   mn,
   es,
+  fr,
   ptbr,
   np,
-  ch,
+  zh,
   vn
 };
 
