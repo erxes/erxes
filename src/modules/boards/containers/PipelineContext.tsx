f414410260cb9cf07d376c15911f25adb45c15f6
batamar <batamar.b@nmtec.co>
2019-08-15 20:45:49 +0800

    remove unused code

--- src/modules/boards/containers/PipelineContext.tsx
+++ src/modules/boards/containers/PipelineContext.tsx
@@ -44,7 +44,6 @@ interface IStore {
   onAddItem: (stageId: string, item: IItem) => void;
   onRemoveItem: (itemId: string, stageId: string) => void;
   onUpdateItem: (item: IItem, prevStageId?: string) => void;
-  queryParams: any;
 }
 
 const PipelineContext = React.createContext({} as IStore);
@@ -338,7 +337,6 @@ export class PipelineProvider extends React.Component<Props, State> {
 
   render() {
     const { itemMap, stageLoadMap, stageIds } = this.state;
-    const { queryParams } = this.props;
 
     return (
       <PipelineContext.Provider
@@ -352,8 +350,7 @@ export class PipelineProvider extends React.Component<Props, State> {
           onUpdateItem: this.onUpdateItem,
           itemMap,
           stageLoadMap,
-          stageIds,
-          queryParams
+          stageIds
         }}
       >
         {this.props.children}
