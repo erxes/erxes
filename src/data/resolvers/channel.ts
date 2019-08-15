958d857844e851bdf64eae8c1bc65090b4fc781b
Munkh-Orgil <munkhorgil@live.com>
2019-08-15 13:34:47 +0800

    erxes/erxes#1183

--- src/data/resolvers/channel.ts
+++ src/data/resolvers/channel.ts
@@ -1,8 +1,19 @@
-import { Integrations } from '../../db/models';
+import { Integrations, Users } from '../../db/models';
 import { IChannelDocument } from '../../db/models/definitions/channels';
 
 export default {
   integrations(channel: IChannelDocument) {
     return Integrations.find({ _id: { $in: channel.integrationIds } });
+  },
+
+  members(channel: IChannelDocument) {
+    const selector = {
+      _id: 1,
+      email: 1,
+      'details.avatar': 1,
+      'details.fullName': 1,
+    };
+
+    return Users.find({ _id: { $in: channel.memberIds } }).select(selector);
   },
 };
--- src/data/schema/channel.ts
+++ src/data/schema/channel.ts
@@ -11,6 +11,7 @@ export const types = `
     openConversationCount: Int
 
     integrations: [Integration]
+    members: [User]
   }
 `;
 
