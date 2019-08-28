3e4f1ea216c1c7a83a9d4b9e8f59bfa2b2bd6182
Munkh-Orgil <munkhorgil@live.com>
2019-08-28 17:37:41 +0800

    change action integration middleware (#550)
    * change customer action name in integration middleware
    * little refactor

--- src/middlewares/integrationsApiMiddleware.ts
+++ src/middlewares/integrationsApiMiddleware.ts
@@ -16,17 +16,29 @@ const integrationsApiMiddleware = async (req, res) => {
   const { action, metaInfo, payload } = req.body;
   const doc = JSON.parse(payload);
 
-  if (action === 'create-customer') {
+  if (action === 'create-or-update-customer') {
     const integration = await Integrations.findOne({ _id: doc.integrationId });
 
     if (!integration) {
       throw new Error(`Integration not found: ${doc.integrationId}`);
     }
 
-    const customer = await Customers.createCustomer({
-      ...doc,
-      scopeBrandIds: integration.brandId,
-    });
+    const { primaryPhone } = doc;
+
+    let customer;
+
+    if (primaryPhone) {
+      customer = await Customers.findOne({ primaryPhone }).lean();
+    }
+
+    if (customer) {
+      await Customers.updateCustomer(customer._id, doc);
+    } else {
+      customer = await Customers.createCustomer({
+        ...doc,
+        scopeBrandIds: integration.brandId,
+      });
+    }
 
     return res.json({ _id: customer._id });
   }
