ae5c154ab9033c05e22704d2f10b2ba96b902c92
Buyantogtokh <buyantogtokh.s@nmma.co>
2019-09-06 10:30:07 +0800

    improve ModifiableSelect component tests

--- src/__tests__/common/components/ModifiableSelect.test.tsx
+++ src/__tests__/common/components/ModifiableSelect.test.tsx
@@ -1,10 +1,13 @@
 import { mount, shallow } from 'enzyme';
-import * as React from 'react';
+import toJson from 'enzyme-to-json';
+import React from 'react';
+
 import ModifiableSelect from '../../../modules/common/components/ModifiableSelect';
 
-describe('Modifiable component', () => {
+describe('Testing ModifiableSelect component', () => {
+  let select;
   const defaultProps = {
-    options: [],
+    options: ['11111111', '22222222'],
     value: '80',
     placeholder: 'phone',
     buttonText: 'add',
@@ -12,31 +15,104 @@ describe('Modifiable component', () => {
     onChange: (params: { options: any[]; selectedOption: any }) => null
   };
 
-  test('renders shallow successfully', () => {
+  beforeEach(() => {
+    select = mount(<ModifiableSelect {...defaultProps} />);
+  });
+
+  afterEach(() => {
+    select.unmount();
+  });
+
+  test('matches snapshot', () => {
+    const shallowSelect = shallow(<ModifiableSelect {...defaultProps} />);
+
+    expect(toJson(shallowSelect)).toMatchSnapshot();
+  });
+
+  test('renders successfully', () => {
     shallow(<ModifiableSelect {...defaultProps} />);
   });
 
   test('renders with default props', () => {
-    const control = mount(<ModifiableSelect {...defaultProps} />);
-    const props = control.props();
+    const props = select.props();
 
     expect(props).toMatchObject(defaultProps);
   });
 
-  test('renders mount test 2 different props', () => {
-    defaultProps.buttonText = 'Add name';
-    defaultProps.placeholder = 'Primary name';
+  test('properly renders add button', () => {
+    const initialState: any = select.state();
 
-    const rendered = mount(<ModifiableSelect {...defaultProps} />);
-    const props = rendered.props();
+    // renders add button only
+    if (!initialState.adding) {
+      const buttons = select.find('button');
 
-    expect(defaultProps).toMatchObject(props);
+      expect(buttons.length).toBe(1);
+    }
   });
 
-  test('render expect Modifiliable', () => {
-    const rendered = mount(<ModifiableSelect {...defaultProps} />);
-    const found = rendered.find('span').debug();
-    const founded = found.search('className="Select-placeholder"');
-    expect(founded).toBeGreaterThan(-1);
+  test('properly renders input with save & cancel buttons', () => {
+    const props = {
+      ...defaultProps,
+      adding: true
+    };
+
+    const modifiableSelect = mount(<ModifiableSelect {...props} />);
+    const initialState: any = modifiableSelect.state();
+
+    // renders input with save & cancel buttons
+    if (initialState.adding) {
+      const buttons = modifiableSelect.find('button');
+
+      expect(buttons.length).toBe(2);
+    }
+  });
+
+  test('properly adds item to option state', () => {
+    const initialState: any = select.state();
+
+    // clicking add button
+    const addButton = select.find('span').at(3);
+    addButton.simulate('click');
+
+    // clicking save button
+    const saveButton = select.find('button').at(1);
+    saveButton.simulate('click');
+
+    const changedState: any = select.state();
+
+    expect(changedState.options.length).toBeGreaterThan(
+      initialState.options.length
+    );
+  });
+
+  test('handleAdding() functions properly', () => {
+    const instance: any = select.instance();
+
+    instance.handleAdding();
+
+    const state: any = select.state();
+
+    expect(state.adding).toBe(true);
+  });
+
+  test('handleCancelAdding() functions properly', () => {
+    const instance: any = select.instance();
+
+    instance.handleCancelAdding();
+
+    const state: any = select.state();
+
+    expect(state.adding).toBe(false);
+  });
+
+  test('removeItem() functions properly', () => {
+    const instance: any = select.instance();
+
+    instance.removeItem(defaultProps.options[0]);
+
+    const state: any = select.state();
+
+    expect(state.options).not.toContain(defaultProps.options[0]);
+    expect(state.selectedOption).toBe('');
   });
 });
