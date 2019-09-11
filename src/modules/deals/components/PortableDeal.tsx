e5a976bd8a4a7db03a7ccc5135b8586a60b8af80
munkhsaikhan <munkhsaikhan.g@nmma.co>
2019-09-11 13:33:44 +0800

    fix open items from customer and company sidebar (#1260)

--- src/modules/deals/components/PortableDeal.tsx
+++ src/modules/deals/components/PortableDeal.tsx
@@ -53,6 +53,7 @@ class Deal extends React.Component<Props, { isPopupVisible: boolean }> {
         onAdd={onAdd}
         onRemove={onRemove}
         onUpdate={onUpdate}
+        isPopupVisible={this.state.isPopupVisible}
         beforePopupClose={beforePopupClose}
       />
     );
