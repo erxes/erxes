5f2c78e8f981c1f1f765e439b0060f68570b40b4
Ganzorig Bayarsaikhan <ganzorig.b@nmma.co>
2019-09-19 14:28:44 +0800

    Add scatter plot

--- src/modules/growthHacks/components/priorityMatrix/Left.tsx
+++ src/modules/growthHacks/components/priorityMatrix/Left.tsx
@@ -1,3 +1,4 @@
+import { ScatterPlot } from '@nivo/scatterplot';
 import React from 'react';
 
 type Props = {
@@ -6,6 +7,43 @@ type Props = {
 };
 
 class Left extends React.Component<Props> {
+  renderChart = () => {
+    return (
+      <ScatterPlot
+        width={900}
+        height={500}
+        data={[
+          {
+            id: 'apples',
+            data: [
+              { x: '2018-01-01', y: 7 },
+              { x: '2018-01-02', y: 5 },
+              { x: '2018-01-03', y: 11 },
+              { x: '2018-01-04', y: 9 },
+              { x: '2018-01-05', y: 12 },
+              { x: '2018-01-06', y: 16 },
+              { x: '2018-01-07', y: 13 },
+              { x: '2018-01-08', y: 13 }
+            ]
+          },
+          {
+            id: 'oranges',
+            data: [
+              { x: '2018-01-04', y: 14 },
+              { x: '2018-01-05', y: 14 },
+              { x: '2018-01-06', y: 15 },
+              { x: '2018-01-07', y: 11 },
+              { x: '2018-01-08', y: 10 },
+              { x: '2018-01-09', y: 12 },
+              { x: '2018-01-10', y: 9 },
+              { x: '2018-01-11', y: 7 }
+            ]
+          }
+        ]}
+      />
+    );
+  };
+
   render() {
     const growthHacks = this.props.growthHacks.map(growthHack => (
       <span key={growthHack._id}>
@@ -23,6 +61,7 @@ class Left extends React.Component<Props> {
 
     return (
       <div>
+        {this.renderChart()}
         <b>growthHacksPriorityMatrix:</b>
         <br /> {growthHacksPriorityMatrix}
         <br />
