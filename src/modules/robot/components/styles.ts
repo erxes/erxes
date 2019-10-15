a2d584442eb2d6d73d91ba3ab4a83cb72895a87b
Ganzorig Bayarsaikhan <ganzorig.b@nmma.co>
2019-10-15 09:28:54 +0800

    Onboard back and close button stick on top

--- src/modules/robot/components/styles.ts
+++ src/modules/robot/components/styles.ts
@@ -57,6 +57,8 @@ const NavButton = styledTS<{ right?: boolean }>(styled.div)`
   margin-top: -5px;
   float: ${props => props.right && 'right'};
   background: ${props => props.right && colors.bgActive};
+  position: sticky;
+  top: 0;
 
   &:hover {
     background: ${props =>
