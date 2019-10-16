95c5a26388a3b116b8c69467e2c1446480c43462
munkhjin <munkhjin0223@gmail.com>
2019-10-16 19:31:36 +0800

    remove unused code

--- src/modules/boards/components/portable/index.ts
+++ src/modules/boards/components/portable/index.ts
@@ -1,7 +1,6 @@
-import Details from '../Details';
 import AddForm from './AddForm';
 import AddTrigger from './AddTrigger';
 import ItemChooser from './ItemChooser';
 import Items from './Items';
 
-export { Items, Details, AddForm, AddTrigger, ItemChooser };
+export { Items, AddForm, AddTrigger, ItemChooser };
