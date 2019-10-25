1cd8943dbf4e0d536370663cbc2c46c5efaeb8ab
Ganzorig Bayarsaikhan <ganzorig.b@nmma.co>
2019-10-25 10:50:36 +0800

    Fix scroll issue on conversion view

--- src/modules/boards/styles/common.ts
+++ src/modules/boards/styles/common.ts
@@ -29,8 +29,7 @@ export const ScrolledContent = styled.div`
   padding: 4px 0 8px;
   margin: 6px 10px 4px 5px;
   flex: 1;
-  overflow-x: auto;
-  overflow-y: hidden;
+  overflow: auto;
 `;
 
 export const RootBack = styled.div`
