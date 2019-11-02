24a2fd23f1a15570411d7dd28b5ca639897e382b
Buyantogtokh <buyantogtokh.s@nmma.co>
2019-11-02 17:28:39 +0800

    refactor deals data schema (#616)

--- src/data/schema/deal.ts
+++ src/data/schema/deal.ts
@@ -29,44 +29,7 @@ export const types = `
   }
 `;
 
-export const queries = `
-  dealDetail(_id: String!): Deal
-  deals(
-    initialStageId: String
-    pipelineId: String
-    stageId: String
-    customerIds: [String]
-    companyIds: [String]
-    date: ItemDate
-    skip: Int
-    search: String
-    assignedUserIds: [String]
-    productIds: [String]
-    nextDay: String
-    nextWeek: String
-    nextMonth: String
-    noCloseDate: String
-    overdue: String
-    labelIds: [String]
-    ${conformityQueryFields}
-  ): [Deal]
-  dealsTotalAmounts(
-    date: ItemDate
-    pipelineId: String
-    customerIds: [String]
-    companyIds: [String]
-    assignedUserIds: [String]
-    productIds: [String]
-    nextDay: String
-    nextWeek: String
-    nextMonth: String
-    noCloseDate: String
-    overdue: String
-    ${conformityQueryFields}
-  ): DealTotalAmounts
-`;
-
-const commonParams = `
+const commonMutationParams = `
   stageId: String,
   assignedUserIds: [String],
   attachments: [AttachmentInput],
@@ -78,9 +41,40 @@ const commonParams = `
   isComplete: Boolean
 `;
 
+const commonQueryParams = `
+  date: ItemDate
+  pipelineId: String
+  customerIds: [String]
+  companyIds: [String]
+  assignedUserIds: [String]
+  productIds: [String]
+  nextDay: String
+  nextWeek: String
+  nextMonth: String
+  noCloseDate: String
+  overdue: String
+  labelIds: [String]
+  search: String
+`;
+
+export const queries = `
+  dealDetail(_id: String!): Deal
+  deals(
+    initialStageId: String
+    stageId: String
+    skip: Int
+    ${commonQueryParams}
+    ${conformityQueryFields}
+  ): [Deal]
+  dealsTotalAmounts(
+    ${commonQueryParams}
+    ${conformityQueryFields}
+  ): DealTotalAmounts
+`;
+
 export const mutations = `
-  dealsAdd(name: String!, ${commonParams}): Deal
-  dealsEdit(_id: String!, name: String, ${commonParams}): Deal
+  dealsAdd(name: String!, ${commonMutationParams}): Deal
+  dealsEdit(_id: String!, name: String, ${commonMutationParams}): Deal
   dealsChange( _id: String!, destinationStageId: String): Deal
   dealsUpdateOrder(stageId: String!, orders: [OrderItem]): [Deal]
   dealsRemove(_id: String!): Deal
