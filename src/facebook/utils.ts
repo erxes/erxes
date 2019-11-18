5a45bf94e3a374ac0fe36fea470ce30e9b3a2eb3
Narmandakh Enkhtuvshin <37796969+Enkhtuvshin0513@users.noreply.github.com>
2019-11-18 16:32:58 +0800

    show error message while send reply on deleted comment

--- src/facebook/utils.ts
+++ src/facebook/utils.ts
@@ -152,6 +152,10 @@ export const sendReply = async (url: string, data: any, recipientId: string, int
       await integration.updateOne({ facebookPageTokensMap });
     }
 
+    if (e.message.includes('does not exist')) {
+      throw new Error('Comment has been deleted by the customer');
+    }
+
     throw new Error(e.message);
   }
 };
