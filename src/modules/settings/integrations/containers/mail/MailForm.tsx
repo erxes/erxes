2ba9c35f0349e83d71fdf9b803ff403f6f513bc6
Munkh-Orgil <munkhorgil@live.com>
2019-11-28 13:23:16 +0800

    mail optimistic ui

--- src/modules/settings/integrations/containers/mail/MailForm.tsx
+++ src/modules/settings/integrations/containers/mail/MailForm.tsx
@@ -1,17 +1,22 @@
 import gql from 'graphql-tag';
-import ButtonMutate from 'modules/common/components/ButtonMutate';
 import Spinner from 'modules/common/components/Spinner';
-import { IButtonMutateProps } from 'modules/common/types';
-import { __, withProps } from 'modules/common/utils';
+import { Alert, withProps } from 'modules/common/utils';
+import { queries as messageQueries } from 'modules/inbox/graphql';
 import { IMail } from 'modules/inbox/types';
 import { mutations, queries } from 'modules/settings/integrations/graphql';
 import * as React from 'react';
 import { compose, graphql } from 'react-apollo';
 import MailForm from '../../components/mail/MailForm';
 import { IntegrationsQueryResponse } from '../../types';
+import {
+  defaultCustomerFields,
+  defaultMailFields,
+  defaultMessageFields
+} from './constants';
 
 type Props = {
   integrationId?: string;
+  conversationId?: string;
   refetchQueries?: string[];
   fromEmail?: string;
   kind: string;
@@ -19,9 +24,11 @@ type Props = {
   isReply?: boolean;
   toggleReply?: () => void;
   closeModal?: () => void;
+  closeReply?: () => void;
 };
 
 type FinalProps = {
+  sendMailMutation: any;
   integrationsQuery: IntegrationsQueryResponse;
 } & Props;
 
@@ -30,12 +37,14 @@ const MailFormContainer = (props: FinalProps) => {
     mailData,
     integrationId,
     integrationsQuery,
-    refetchQueries,
     fromEmail,
     kind,
+    conversationId,
     isReply,
     toggleReply,
-    closeModal
+    closeModal,
+    closeReply,
+    sendMailMutation
   } = props;
 
   if (integrationsQuery.loading) {
@@ -44,30 +53,95 @@ const MailFormContainer = (props: FinalProps) => {
 
   const integrations = integrationsQuery.integrations || [];
 
-  const renderButton = ({
-    values,
-    isSubmitted,
-    callback
-  }: IButtonMutateProps) => {
-    return (
-      <ButtonMutate
-        mutation={mutations.integrationSendMail}
-        variables={values}
-        callback={callback}
-        refetchQueries={refetchQueries}
-        isSubmitted={isSubmitted}
-        btnSize="small"
-        type="submit"
-        icon="message"
-        successMessage="You have successfully sent a email"
-      >
-        {__('Send')}
-      </ButtonMutate>
-    );
+  const save = ({
+    variables,
+    optimisticResponse,
+    update
+  }: {
+    variables: any;
+    optimisticResponse?: any;
+    update?: any;
+  }) => {
+    return sendMailMutation({ variables, optimisticResponse, update })
+      .then(() => {
+        Alert.success('You have successfully sent a email');
+
+        if (closeModal) {
+          closeModal();
+        }
+      })
+      .catch(e => {
+        Alert.error(e);
+
+        if (closeModal) {
+          closeModal();
+        }
+      });
+  };
+
+  const sendMail = ({ variables }: { variables: any }) => {
+    if (!isReply) {
+      return save({ variables });
+    }
+
+    const email = mailData ? mailData.integrationEmail : '';
+
+    const integrationSendMail = {
+      _id: Math.round(Math.random() * -1000000),
+      ...defaultMessageFields,
+      conversationId,
+      content: variables.body,
+      customer: {
+        ...defaultCustomerFields,
+        firstName: email,
+        primaryEmail: email
+      },
+      mailData: {
+        ...defaultMailFields,
+        bcc: [{ __typename: 'Email', email: variables.bcc }],
+        to: [{ __typename: 'Email', email: variables.to }],
+        from: [{ __typename: 'Email', email: variables.to }],
+        cc: [{ __typename: 'Email', email: variables.cc }],
+        body: variables.body,
+        subject: variables.subject,
+        attachments: variables.attachments,
+        integrationEmail: variables.from
+      }
+    };
+
+    const optimisticResponse = { __typename: 'Mutation', integrationSendMail };
+
+    const update = store => {
+      const selector = {
+        query: gql(messageQueries.conversationMessages),
+        variables: { conversationId, limit: 10 }
+      };
+
+      // Read the data from our cache for this query.
+      try {
+        const data = store.readQuery(selector);
+        const messages = data.conversationMessages || [];
+
+        messages.push(integrationSendMail);
+
+        // Write our data back to the cache.
+        store.writeQuery({ ...selector, data });
+
+        if (closeReply) {
+          closeReply();
+        }
+      } catch (e) {
+        Alert.error(e);
+        return;
+      }
+    };
+
+    // Invoke mutation
+    return save({ variables, optimisticResponse, update });
   };
 
   const updatedProps = {
-    renderButton,
+    sendMail,
     integrations,
     integrationId,
     fromEmail,
@@ -94,6 +168,9 @@ export default withProps<Props>(
           };
         }
       }
-    )
+    ),
+    graphql<Props>(gql(mutations.integrationSendMail), {
+      name: 'sendMailMutation'
+    })
   )(MailFormContainer)
 );
