776a0336ff1871c4cbdac4fde503a99ed8b82f17
BatAmar Battulga <batamar.b@nmtec.co>
2019-12-02 19:11:27 +0800

    fix customer merge button in inbox

--- src/modules/inbox/components/conversationDetail/sidebar/Sidebar.tsx
+++ src/modules/inbox/components/conversationDetail/sidebar/Sidebar.tsx
@@ -15,7 +15,7 @@ import { IConversation } from '../../../types';
 
 const ActionSection = asyncComponent(
   () =>
-    import(/* webpackChunkName:"Inbox-Sidebar-ActionSection" */ 'modules/customers/components/common/ActionSection'),
+    import(/* webpackChunkName:"Inbox-Sidebar-ActionSection" */ 'modules/customers/containers/common/ActionSection'),
   { height: '25px', width: '80px' }
 );
 
