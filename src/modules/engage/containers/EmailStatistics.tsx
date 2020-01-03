483ee739660a4f848b1dfd23764f10db554a6769
Ganzorig Bayarsaikhan <ganzorig.b@nmma.co>
2020-01-03 13:55:46 +0800

    Check emailstatistic and pipeline empty

--- src/modules/engage/containers/EmailStatistics.tsx
+++ src/modules/engage/containers/EmailStatistics.tsx
@@ -1,5 +1,7 @@
 import gql from 'graphql-tag';
 import * as compose from 'lodash.flowright';
+import EmptyState from 'modules/common/components/EmptyState';
+import Spinner from 'modules/common/components/Spinner';
 import React from 'react';
 import { graphql } from 'react-apollo';
 import { withProps } from '../../common/utils';
@@ -19,15 +21,17 @@ const EmailStatisticsContainer = (props: FinalProps) => {
   const { engageMessageDetailQuery } = props;
 
   if (engageMessageDetailQuery.error) {
-    return null;
+    return <EmptyState size="full" text="Error" icon="ban" />;
   }
 
   if (engageMessageDetailQuery.loading) {
-    return null;
+    return <Spinner />;
   }
 
   if (!engageMessageDetailQuery.engageMessageDetail) {
-    return null;
+    return (
+      <EmptyState size="full" text="Message not found" icon="web-section-alt" />
+    );
   }
 
   const message = engageMessageDetailQuery.engageMessageDetail;
