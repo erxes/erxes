275f5da98b57bedd97bf1e645c4abbc1133470df
BatAmar Battulga <batamar.b@nmtec.co>
2020-01-10 13:57:24 +0800

    moved twitter config

---
+++ ui/src/modules/settings/general/components/IntegrationConfigs.tsx
@@ -0,0 +1,167 @@
+import Button from 'modules/common/components/Button';
+import { FormControl } from 'modules/common/components/form';
+import FormGroup from 'modules/common/components/form/Group';
+import ControlLabel from 'modules/common/components/form/Label';
+import HeaderDescription from 'modules/common/components/HeaderDescription';
+import { __ } from 'modules/common/utils';
+import ActionBar from 'modules/layout/components/ActionBar';
+import Wrapper from 'modules/layout/components/Wrapper';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { ContentBox } from '../../styles';
+import { IConfigsMap } from '../types';
+
+type Props = {
+  save: (configsMap: IConfigsMap) => void;
+  configsMap: IConfigsMap;
+};
+
+type State = {
+  configsMap: IConfigsMap,
+};
+
+class IntegrationConfigs extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+
+    this.state = {
+      configsMap: props.configsMap,
+    };
+  }
+
+  save = e => {
+    e.preventDefault();
+
+    const { configsMap } = this.state;
+
+    this.props.save(configsMap);
+  };
+
+  onChangeConfig = (code: string, value) => {
+    const { configsMap } = this.state;
+
+    configsMap[code] = value;
+
+    this.setState({ configsMap });
+  };
+
+  onChangeInput = (code: string, e) => {
+    this.onChangeConfig(code, e.target.value);
+  };
+
+  render() {
+    const { configsMap } = this.state;
+
+    const actionFooter = (
+      <ActionBar
+        right={
+          <Button.Group>
+            <Link to="/settings/">
+              <Button size="small" btnStyle="simple" icon="cancel-1">
+                Cancel
+              </Button>
+            </Link>
+
+            <Button
+              size="small"
+              btnStyle="success"
+              onClick={this.save}
+              icon="checked-1"
+            >
+              Save
+            </Button>
+          </Button.Group>
+        }
+      />
+    );
+
+    const content = (
+      <ContentBox>
+        <FormGroup>
+          <ControlLabel>FACEBOOK_APP_ID</ControlLabel>
+
+          <FormControl
+            defaultValue={configsMap.FACEBOOK_APP_ID}
+            onChange={this.onChangeInput.bind(this, 'FACEBOOK_APP_ID')}
+          />
+        </FormGroup>
+
+        <FormGroup>
+          <ControlLabel>FACEBOOK_APP_SECRET</ControlLabel>
+
+          <FormControl
+            defaultValue={configsMap.FACEBOOK_APP_SECRET}
+            onChange={this.onChangeInput.bind(this, 'FACEBOOK_APP_SECRET')}
+          />
+        </FormGroup>
+
+        <FormGroup>
+          <ControlLabel>FACEBOOK_VERIFY_TOKEN</ControlLabel>
+
+          <FormControl
+            defaultValue={configsMap.FACEBOOK_VERIFY_TOKEN}
+            onChange={this.onChangeInput.bind(this, 'FACEBOOK_VERIFY_TOKEN')}
+          />
+        </FormGroup>
+
+        <FormGroup>
+          <ControlLabel>TWITTER_CONSUMER_KEY</ControlLabel>
+
+          <FormControl
+            defaultValue={configsMap.TWITTER_CONSUMER_KEY}
+            onChange={this.onChangeInput.bind(this, 'TWITTER_CONSUMER_KEY')}
+          />
+        </FormGroup>
+
+        <FormGroup>
+          <ControlLabel>TWITTER_ACCESS_TOKEN</ControlLabel>
+
+          <FormControl
+            defaultValue={configsMap.TWITTER_ACCESS_TOKEN}
+            onChange={this.onChangeInput.bind(this, 'TWITTER_ACCESS_TOKEN')}
+          />
+        </FormGroup>
+
+        <FormGroup>
+          <ControlLabel>TWITTER_ACCESS_TOKEN_SECRET</ControlLabel>
+
+          <FormControl
+            defaultValue={configsMap.TWITTER_ACCESS_TOKEN_SECRET}
+            onChange={this.onChangeInput.bind(this, 'TWITTER_ACCESS_TOKEN_SECRET')}
+          />
+        </FormGroup>
+
+        <FormGroup>
+          <ControlLabel>TWITTER_WEBHOOK_ENV</ControlLabel>
+
+          <FormControl
+            defaultValue={configsMap.TWITTER_WEBHOOK_ENV}
+            onChange={this.onChangeInput.bind(this, 'TWITTER_WEBHOOK_ENV')}
+          />
+        </FormGroup>
+
+      </ContentBox>
+    );
+
+    return (
+      <Wrapper
+        actionBar={
+          <Wrapper.ActionBar
+            left={
+              <HeaderDescription
+                icon="/images/actions/25.svg"
+                title="General"
+                description="Set up your initial account settings so that things run smoothly in unison."
+              />
+            }
+          />
+        }
+        content={content}
+        footer={actionFooter}
+        center={true}
+      />
+    );
+  }
+}
+
+export default IntegrationConfigs;
--- ui/src/modules/settings/general/routes.tsx
+++ ui/src/modules/settings/general/routes.tsx
@@ -3,9 +3,20 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 
 const List = asyncComponent(() =>
-  import(/* webpackChunkName: "Settings List - Generatel" */ './containers/List')
+  import(/* webpackChunkName: "Settings List - General" */ './containers/List')
 );
 
-const routes = () => <Route path="/settings/general/" component={List} />;
+const IntegrationConfigs = asyncComponent(() =>
+  import(/* webpackChunkName: "Integration configs" */ './containers/IntegrationConfigs')
+);
+
+const routes = () => {
+  return (
+    <React.Fragment>
+      <Route path="/settings/general/" component={List} />;
+      <Route path="/settings/integration-configs/" component={IntegrationConfigs} />;
+    </React.Fragment>
+  )
+};
 
 export default routes;
