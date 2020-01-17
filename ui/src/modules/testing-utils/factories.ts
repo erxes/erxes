c73d6c27432b508adbb362ed763048fe18524c51
Mungunshagai <e.mungunshagai@gmail.com>
2020-01-17 16:54:35 +0800

    add checklist factory

--- ui/src/modules/testing-utils/factories.ts
+++ ui/src/modules/testing-utils/factories.ts
@@ -1,5 +1,6 @@
 import dayjs from 'dayjs';
 import * as Factory from 'factory.ts';
+import { IChecklist, IChecklistItem } from 'modules/checklists/types';
 import { IBrand } from 'modules/settings/brands/types';
 
 export const brandFactory = Factory.Sync.makeFactory<IBrand>({
@@ -9,4 +10,25 @@ export const brandFactory = Factory.Sync.makeFactory<IBrand>({
   createdAt: dayjs().format('YYYY-MM-DD'),
   description: 'special brand',
   emailConfig: { type: '', template: '' }
+});
+
+const checkListItemFactory = Factory.Sync.makeFactory<IChecklistItem>({
+  _id: '1',
+  checklistId: 'listId',
+  isChecked: false,
+  content: 'react test'
+});
+
+export const checkListFactory = Factory.Sync.makeFactory<IChecklist>({
+  _id: '1',
+  createdUserId: 'string',
+  createdDate: new Date(),
+  items: [
+    checkListItemFactory.build(),
+    checkListItemFactory.build({ _id: '2' })
+  ],
+  percent: 80,
+  contentType: 'string',
+  contentTypeId: 'string',
+  title: 'string'
 });
