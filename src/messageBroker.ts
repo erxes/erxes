77e79eb844005400b99f69c7dab27fdcd376db0d
BatAmar Battulga <batamar.b@nmtec.co>
2020-02-01 16:51:19 +0800

    caught exception in rabbitmq init

--- src/messageBroker.ts
+++ src/messageBroker.ts
@@ -61,84 +61,82 @@ export const sendMessage = async (queueName: string, data?: any) => {
 
 const initConsumer = async () => {
   // Consumer
-  try {
-    connection = await amqplib.connect(RABBITMQ_HOST);
-    channel = await connection.createChannel();
+  connection = await amqplib.connect(RABBITMQ_HOST);
+  channel = await connection.createChannel();
 
-    // listen for rpc queue =========
-    await channel.assertQueue('rpc_queue:erxes-integrations');
+  // listen for rpc queue =========
+  await channel.assertQueue('rpc_queue:erxes-integrations');
 
-    channel.consume('rpc_queue:erxes-integrations', async msg => {
-      if (msg !== null) {
-        debugBase(`Received rpc queue message ${msg.content.toString()}`);
+  channel.consume('rpc_queue:erxes-integrations', async msg => {
+    if (msg !== null) {
+      debugBase(`Received rpc queue message ${msg.content.toString()}`);
 
-        const response = await receiveRpcMessage(JSON.parse(msg.content.toString()));
+      const response = await receiveRpcMessage(JSON.parse(msg.content.toString()));
 
-        channel.sendToQueue(msg.properties.replyTo, Buffer.from(JSON.stringify(response)), {
-          correlationId: msg.properties.correlationId,
-        });
+      channel.sendToQueue(msg.properties.replyTo, Buffer.from(JSON.stringify(response)), {
+        correlationId: msg.properties.correlationId,
+      });
 
-        channel.ack(msg);
-      }
-    });
+      channel.ack(msg);
+    }
+  });
 
-    // graphql subscriptions call =========
-    await channel.assertQueue('callPublish');
+  // graphql subscriptions call =========
+  await channel.assertQueue('callPublish');
 
-    channel.consume('callPublish', async msg => {
-      if (msg !== null) {
-        const params = JSON.parse(msg.content.toString());
+  channel.consume('callPublish', async msg => {
+    if (msg !== null) {
+      const params = JSON.parse(msg.content.toString());
 
-        graphqlPubsub.publish(params.name, params.data);
+      graphqlPubsub.publish(params.name, params.data);
 
-        channel.ack(msg);
-      }
-    });
+      channel.ack(msg);
+    }
+  });
 
-    // listen for integrations api =========
-    await channel.assertQueue('integrationsNotification');
+  // listen for integrations api =========
+  await channel.assertQueue('integrationsNotification');
 
-    channel.consume('integrationsNotification', async msg => {
-      if (msg !== null) {
-        await receiveIntegrationsNotification(JSON.parse(msg.content.toString()));
-        channel.ack(msg);
-      }
-    });
+  channel.consume('integrationsNotification', async msg => {
+    if (msg !== null) {
+      await receiveIntegrationsNotification(JSON.parse(msg.content.toString()));
+      channel.ack(msg);
+    }
+  });
 
-    // listen for engage api ===========
-    await channel.assertQueue('engages-api:set-donot-disturb');
+  // listen for engage api ===========
+  await channel.assertQueue('engages-api:set-donot-disturb');
 
-    channel.consume('engages-api:set-donot-disturb', async msg => {
-      if (msg !== null) {
-        const data = JSON.parse(msg.content.toString());
+  channel.consume('engages-api:set-donot-disturb', async msg => {
+    if (msg !== null) {
+      const data = JSON.parse(msg.content.toString());
 
-        await Customers.updateOne({ _id: data.customerId }, { $set: { doNotDisturb: 'Yes' } });
+      await Customers.updateOne({ _id: data.customerId }, { $set: { doNotDisturb: 'Yes' } });
 
-        channel.ack(msg);
-      }
-    });
+      channel.ack(msg);
+    }
+  });
 
-    // listen for spark notification  =========
-    await channel.assertQueue('sparkNotification');
+  // listen for spark notification  =========
+  await channel.assertQueue('sparkNotification');
 
-    channel.consume('sparkNotification', async msg => {
-      if (msg !== null) {
-        debugBase(`Received spark notification ${msg.content.toString()}`);
+  channel.consume('sparkNotification', async msg => {
+    if (msg !== null) {
+      debugBase(`Received spark notification ${msg.content.toString()}`);
 
-        const data = JSON.parse(msg.content.toString());
+      const data = JSON.parse(msg.content.toString());
 
-        delete data.subdomain;
+      delete data.subdomain;
 
-        RobotEntries.createEntry(data)
-          .then(() => debugBase('success'))
-          .catch(e => debugBase(e.message));
+      RobotEntries.createEntry(data)
+        .then(() => debugBase('success'))
+        .catch(e => debugBase(e.message));
 
-        channel.ack(msg);
-      }
-    });
-  } catch (e) {
-    debugBase(e.message);
-  }
+      channel.ack(msg);
+    }
+  });
 };
 
-initConsumer();
+initConsumer().catch(e => {
+  debugBase(`Error ocurred during rabbitmq init ${e.message}`);
+});
