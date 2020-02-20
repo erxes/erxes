619b197c953781c5453b2be28fa49c4858483146
Batchimeg.a <batchimeg01@gmail.com>
2020-02-20 13:50:11 +0800

    change google button

--- ui/src/modules/settings/integrations/components/Accounts.tsx
+++ ui/src/modules/settings/integrations/components/Accounts.tsx
@@ -42,27 +42,33 @@ class Accounts extends React.Component<Props, { accountId?: string }> {
     });
   }
 
-  renderAccountAction() {
+  actionButton() {
     const { renderForm, onAdd, kind } = this.props;
-    const { accountId } = this.state;
 
-    if (!accountId || accountId === '') {
-      if (!renderForm) {
-        if (kind === 'gmail' || kind === 'nylas-gmail') {
-          return (
-            <a href="#name" onClick={onAdd}>
-              <img src='/images/btn_google_signin_dark_pressed_web@2x.png' width='125px' height='30px' alt='google button' />
-            </a>
-          );
-        }
+    if (!renderForm) {
+      if (kind === 'gmail' || kind === 'nylas-gmail') {
         return (
-          <Button btnStyle="primary" size="small" icon="add" onClick={onAdd}>
-            Add Account
-          </Button>
+          <a href="#name" onClick={onAdd}>
+            <img src='/images/btn_google_signin_dark_pressed_web@2x.png' width='130px' height='30px' alt='google button' />
+          </a>
         );
       }
 
-      return renderForm();
+      return (
+        <Button btnStyle="primary" size="small" icon="add" onClick={onAdd}>
+          Add Account
+        </Button>
+      );
+    }
+
+    return renderForm();
+  }
+
+  renderAccountAction() {
+    const { accountId } = this.state;
+
+    if (!accountId) {
+      return this.actionButton();
     }
 
     return (
