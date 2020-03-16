eb66c2157397a0f6c9e744e2a1961b0107169767
munkhsaikhan <munkhsaikhan.g@nmma.co>
2020-03-17 02:28:11 +0800

    trial debugLog

--- src/middlewares/userMiddleware.ts
+++ src/middlewares/userMiddleware.ts
@@ -16,11 +16,7 @@ const userMiddleware = async (req, _res, next) => {
       // verify user token and retrieve stored user information
       const { user } = jwt.verify(token, Users.getSecret());
 
-      debugBase(
-        `graphqlPubsub publish: userId: ${user._id}, sessionCode: ${req.headers.session_code}, req: ${Object.keys(
-          req,
-        )} headers: ${req.headers}`,
-      );
+      debugBase(`full req keys ${Object.keys(req)} headers: ${req.headers}`);
       // save user in request
       req.user = user;
       req.user.loginToken = token;
