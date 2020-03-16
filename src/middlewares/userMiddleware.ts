f9a78aefb81315763fd11d8ca05f2920538d093d
munkhsaikhan <munkhsaikhan.g@nmma.co>
2020-03-17 04:16:14 +0800

    degug req log

--- src/middlewares/userMiddleware.ts
+++ src/middlewares/userMiddleware.ts
@@ -8,6 +8,7 @@ import { debugBase } from '../debuggers';
  * @param {Object} res - Response object
  * @param {Function} next - Next function
  */
+
 const userMiddleware = async (req, _res, next) => {
   const token = req.cookies['auth-token'];
 
@@ -16,9 +17,9 @@ const userMiddleware = async (req, _res, next) => {
       // verify user token and retrieve stored user information
       const { user } = jwt.verify(token, Users.getSecret());
 
-      const sessionCode = (req.headers && req.headers.session_code) || req.session_code;
+      const sessionCode = (req.headers && req.headers.session_code) || req.session_code || '';
 
-      debugBase(`.... ${sessionCode} ${Object.keys(req)} ...${Object.keys(req.headers)}, ...${req}, ...${req.headers}`);
+      dlog(req, '');
 
       // save user in request
       req.user = user;
@@ -30,6 +31,23 @@ const userMiddleware = async (req, _res, next) => {
   }
 
   next();
+};
+
+const dlog = (obj, tab) => {
+  tab = tab.concat('..../');
+  if (tab === '..../..../..../..../') {
+    return;
+  }
+  for (const key of Object.keys(obj)) {
+    try {
+      debugBase(`${tab} ${key}: ${obj[key]}`);
+      if (typeof obj[key] === typeof []) {
+        dlog(obj[key], tab);
+      }
+    } catch (e) {
+      debugBase(`${tab} ${key}::`);
+    }
+  }
 };
 
 export default userMiddleware;
