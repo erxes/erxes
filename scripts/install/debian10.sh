81cf80dd916c47bde2cb619263c38f7c907c34ba
Batnasan Byambasuren <batnasan06@gmail.com>
2020-03-18 20:21:52 +1100

    enable elasticsearch daemon

--- scripts/install/debian10.sh
+++ scripts/install/debian10.sh
@@ -63,6 +63,8 @@ wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | apt-key add -
 echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | tee -a /etc/apt/sources.list.d/elastic-7.x.list
 apt-get -qqy update
 apt-get -qqy install elasticsearch
+systemctl enable elasticsearch
+systemctl start elasticsearch
 
 # Nginx
 apt-get -qqy install -y nginx
