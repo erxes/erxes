1a436d88ad0280a705084511e28ea41f0033c166
Batnasan Byambasuren <batnasan06@gmail.com>
2020-03-31 20:04:12 +1100

    fix mongo repo

--- scripts/install/debian10.sh
+++ scripts/install/debian10.sh
@@ -32,7 +32,7 @@ apt-get -qqy install -y curl wget gnupg apt-transport-https software-properties-
 # MongoDB
 echo "Installing MongoDB"
 wget -qO - https://www.mongodb.org/static/pgp/server-3.6.asc | apt-key add -
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.6 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
+echo "deb http://repo.mongodb.org/apt/debian stretch/mongodb-org/3.6 main" | tee /etc/apt/sources.list.d/mongodb-org-3.6.list
 apt-get -qqy update
 apt-get install -y mongodb-org=3.6.17 mongodb-org-server=3.6.8 mongodb-org-shell=3.6.17 mongodb-org-mongos=3.6.8 mongodb-org-tools=3.6.8
 echo "Installed MongoDB successfully"
