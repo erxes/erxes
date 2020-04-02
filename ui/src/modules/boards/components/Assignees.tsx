72a66173e68d0e48db0a4ef9f81052da8ba2f109
Ganzorig Bayarsaikhan <ganzorig.b@nmma.co>
2020-04-02 23:06:42 +0800

    perf(deal): improve performance when deal item dragging

---
+++ ui/src/modules/boards/components/Assignees.tsx
@@ -0,0 +1,42 @@
+import { IUser } from 'modules/auth/types';
+import { getUserAvatar } from 'modules/common/utils';
+import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  > img {
+    border-radius: 14px;
+    float: left;
+    margin-left: 2px;
+  }
+`;
+
+type Props = {
+  users: IUser[];
+  limit?: number;
+};
+
+function Assignees(props: Props) {
+  const getFullName = (user: IUser) => {
+    return user.details ? user.details.fullName : 'Unknown';
+  };
+
+  const { users = [], limit = 3 } = props;
+
+  return (
+    <Wrapper>
+      {users.slice(0, limit).map(user => (
+        <img
+          alt={getFullName(user)}
+          title={getFullName(user)}
+          key={user._id}
+          src={getUserAvatar(user)}
+          width={28}
+          height={28}
+        />
+      ))}
+    </Wrapper>
+  );
+}
+
+export default Assignees;
