77f0bacc5e8e5a27d8f6c5392c310337565d0402
BatAmar Battulga <batamar.b@nmtec.co>
2020-04-25 01:47:29 +0800

    moved workers startup to api #767

--- src/startup.ts
+++ src/startup.ts
@@ -2,21 +2,24 @@ import * as fs from 'fs';
 import { Users } from './db/models';
 import { addToArray } from './redisClient';
 
-const init = async () => {
+export const cacheUsers = async () => {
   const userIds = await Users.find({}).distinct('_id');
 
   userIds.forEach(id => {
     addToArray('userIds', id);
   });
+};
 
-  const makeDirs = () => {
-    const dir = `${__dirname}/private/xlsTemplateOutputs`;
+export const makeDirs = () => {
+  const dir = `${__dirname}/private/xlsTemplateOutputs`;
 
-    if (!fs.existsSync(dir)) {
-      fs.mkdirSync(dir);
-    }
-  };
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir);
+  }
+};
 
+const init = async () => {
+  await cacheUsers();
   makeDirs();
 };
 
