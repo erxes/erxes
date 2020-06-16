38b4ad1d8874071b4008581e782ee11b277e45fb
Ganzorig Bayarsaikhan <ganzorig.b@nmma.co>
2020-06-17 07:34:51 +0800

    stylee(docs): change blog home page

---
+++ docs/src/components/clients.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import useBaseUrl from '@docusaurus/useBaseUrl';
+import styles from './clients.module.css';
+
+const clients = [
+	{
+		src: 'img/clients/kfc.png',
+		alt: 'KFC'
+	},
+	{
+		src: 'img/clients/xerox.png',
+		alt: 'Xerox'
+	},
+	{
+		src: 'img/clients/pizzahut.png',
+		alt: 'Pizza hut'
+	},
+	{
+		src: 'img/clients/toto.png',
+		alt: 'TOTO'
+	},
+	{
+		src: 'img/clients/toyota.png',
+		alt: 'Toyota'
+	},
+	{
+		src: 'img/clients/bridgestone.png',
+		alt: 'Bridgestone'
+	},
+	{
+		src: 'img/clients/vw.png',
+		alt: 'Volkswagen'
+	},
+	{
+		src: 'img/clients/godiva.png',
+		alt: 'Godiva'
+	},
+	{
+		src: 'img/clients/hankook.png',
+		alt: 'Hankook'
+	},
+];
+
+export const Clients = () => {
+  return (
+		<section className={styles.clientsWrapper}>
+			<div className="container">
+				<div className={styles.clients}>
+					{clients.map(({src, alt}) => {
+						return (
+							<img
+								key={alt}
+								alt={alt}
+								src={useBaseUrl(src)}
+							/>
+						);
+					})}
+				</div>
+			</div>
+		</section>
+  );
+};
