6d5699f32ee68d61110620d2ebf61ab3c92346ac
Munkh-Orgil <munkhorgil@live.com>
2020-06-23 01:27:18 +0800

    remove unused mutations erxes/erxes#2072

--- src/data/dataSources/integrations.ts
+++ src/data/dataSources/integrations.ts
@@ -39,10 +39,6 @@ export default class IntegrationsAPI extends RESTDataSource {
     return this.post('/integrations/remove', params);
   }
 
-  public async createAccount(params) {
-    return this.post('/nylas/auth/callback', params);
-  }
-
   public async removeAccount(params) {
     return this.post('/accounts/remove', params);
   }
