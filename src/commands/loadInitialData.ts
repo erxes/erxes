7b7daf8b2e1147ea3d845d17a434ac9c69034620
munkhsaikhan <munkhsaikhan.g@nmma.co>
2020-06-29 18:06:39 +0800

    loadInitialData refactor (#802)

--- src/commands/loadInitialData.ts
+++ src/commands/loadInitialData.ts
@@ -1,16 +1,29 @@
+import * as dotenv from 'dotenv';
 import * as shelljs from 'shelljs';
 import { getEnv } from '../data/utils';
+import { connect } from '../db/connection';
 import { Users } from '../db/models';
+
+dotenv.config();
 
 const main = async () => {
   const MONGO_URL = getEnv({ name: 'MONGO_URL' });
 
-  const result = await shelljs.exec(`mongorestore --uri "${MONGO_URL}" --db erxes ./src/initialData/common`, {
+  const connection = await connect();
+
+  const dbName = connection.connection.db.databaseName;
+  console.log(`drop and create database: ${dbName}`)
+
+  await connection.connection.dropDatabase();
+
+  const result = await shelljs.exec(`mongorestore --uri "${MONGO_URL}" --db ${dbName} ./src/initialData/common`, {
     silent: true,
   });
   const output = result.stderr + result.stdout;
 
   console.log(output);
+
+  console.log(`success, imported initial data to: ${dbName}`)
 
   const generator = require('generate-password');
   const newPwd = generator.generate({
