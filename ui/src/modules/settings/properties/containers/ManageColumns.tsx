44f0f2853337c9471756f8c792be46b65641044b
Enkhtuvshin0513 <enhtuwshinn@gmail.com>
2020-07-27 23:45:48 +0800

    update

--- ui/src/modules/settings/properties/containers/ManageColumns.tsx
+++ ui/src/modules/settings/properties/containers/ManageColumns.tsx
@@ -132,6 +132,7 @@ export default withProps<Props>(
                 'ownerId',
                 'status',
                 'integrationId',
+                'categoryId',
                 'emailValidationStatus',
                 'phoneValidationStatus',
                 'location.countryCode'
