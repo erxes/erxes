c2a3c074c65e73d6c8a3def0d16c4f11462473e3
Enkhtuvshin0513 <enhtuwshinn@gmail.com>
2020-07-29 17:45:56 +0800

    improve field check function in import

--- src/data/modules/fields/utils.ts
+++ src/data/modules/fields/utils.ts
@@ -1,8 +1,65 @@
-import { Fields } from '../../../db/models';
+import { Companies, Customers, Fields, Products } from '../../../db/models';
+import { BOARD_BASIC_INFOS } from '../fileExporter/constants';
+
+const generateBasicInfosFromSchema = async (queSchema: any, namePrefix: string) => {
+  const queFields: string[] = [];
+
+  // field definations
+  const paths = queSchema.paths;
+
+  for (const name of Object.keys(paths)) {
+    const path = paths[name];
+
+    const label = path.options.label;
+    const type = path.instance;
+
+    if (['String', 'Number', 'Date', 'Boolean'].includes(type) && label) {
+      // add to fields list
+      queFields.push(`${namePrefix}${name}`);
+    }
+  }
+
+  return queFields;
+};
 
 // Checking field names, all field names must be configured correctly
 export const checkFieldNames = async (type: string, fields: string[]) => {
   const properties: any[] = [];
+  let schema: any;
+  let basicInfos: string[] = [];
+
+  switch (type) {
+    case 'company':
+      schema = Companies.schema;
+      break;
+
+    case 'customer':
+      schema = Customers.schema;
+      break;
+
+    case 'product':
+      schema = Products.schema;
+      break;
+
+    case 'deal':
+    case 'task':
+    case 'ticket':
+      basicInfos = BOARD_BASIC_INFOS;
+      break;
+  }
+
+  if (schema) {
+    basicInfos = [...basicInfos, ...(await generateBasicInfosFromSchema(schema, ''))];
+
+    for (const name of Object.keys(schema.paths)) {
+      const path = schema.paths[name];
+
+      // extend fields list using sub schema fields
+      if (path.schema) {
+        basicInfos = [...basicInfos, ...(await generateBasicInfosFromSchema(path.schema, `${name}.`))];
+      }
+    }
+  }
 
   for (let fieldName of fields) {
     if (!fieldName) {
@@ -14,6 +71,12 @@ export const checkFieldNames = async (type: string, fields: string[]) => {
     const property: { [key: string]: any } = {};
 
     const fieldObj = await Fields.findOne({ text: fieldName, contentType: type });
+
+    // Collecting basic fields
+    if (basicInfos.includes(fieldName)) {
+      property.name = fieldName;
+      property.type = 'basic';
+    }
 
     // Collecting custom fields
     if (fieldObj) {
@@ -55,10 +118,9 @@ export const checkFieldNames = async (type: string, fields: string[]) => {
       property.name = 'stageId';
       property.type = 'stageName';
     }
-    // Collecting basic fields
-    else {
-      property.name = fieldName;
-      property.type = 'basic';
+
+    if (!property.type) {
+      throw new Error(`Bad column name ${fieldName}`);
     }
 
     properties.push(property);
