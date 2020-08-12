59416da5caa527d4af2e419820f1a686fb0591ec
munkhjin <munkhjin0223@gmail.com>
2020-08-12 14:14:52 +0800

    fix permission query test

--- src/__tests__/permissionQueries.test.ts
+++ src/__tests__/permissionQueries.test.ts
@@ -183,7 +183,7 @@ describe('permissionQueries', () => {
 
     const modules = await graphqlRequest(qry, 'permissionActions');
 
-    expect(modules.length).toBe(188);
+    expect(modules.length).toBe(189);
   });
 });
 
