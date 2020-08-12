d15552b45da5e4938dc76bd1f1ebca9a0ee09b3b
amrakun <amraa.888@gmail.com>
2020-08-12 22:13:52 +0800

    singlequote

--- ui/src/modules/customers/containers/CustomersList.tsx
+++ ui/src/modules/customers/containers/CustomersList.tsx
@@ -1,17 +1,17 @@
-import { getEnv } from "apolloClient";
-import gql from "graphql-tag";
-import * as compose from "lodash.flowright";
-import { __, Alert, withProps } from "modules/common/utils";
-import { generatePaginationParams } from "modules/common/utils/router";
-import queryString from "query-string";
-import React from "react";
-import { graphql } from "react-apollo";
-import { withRouter } from "react-router-dom";
-import Bulk from "../../common/components/Bulk";
-import { IRouterProps } from "../../common/types";
-import { ListConfigQueryResponse } from "../../companies/types";
-import CustomersList from "../components/list/CustomersList";
-import { mutations, queries } from "../graphql";
+import { getEnv } from 'apolloClient';
+import gql from 'graphql-tag';
+import * as compose from 'lodash.flowright';
+import { __, Alert, withProps } from 'modules/common/utils';
+import { generatePaginationParams } from 'modules/common/utils/router';
+import queryString from 'query-string';
+import React from 'react';
+import { graphql } from 'react-apollo';
+import { withRouter } from 'react-router-dom';
+import Bulk from '../../common/components/Bulk';
+import { IRouterProps } from '../../common/types';
+import { ListConfigQueryResponse } from '../../companies/types';
+import CustomersList from '../components/list/CustomersList';
+import { mutations, queries } from '../graphql';
 import {
   ListQueryVariables,
   MainQueryResponse,
@@ -21,7 +21,7 @@ import {
   RemoveMutationVariables,
   VerifyMutationResponse,
   VerifyMutationVariables,
-} from "../types";
+} from '../types';
 
 type Props = {
   queryParams: any;
@@ -51,7 +51,7 @@ class CustomerListContainer extends React.Component<FinalProps, State> {
     this.state = {
       loading: false,
       mergeCustomerLoading: false,
-      responseId: "",
+      responseId: ''
     };
   }
 
@@ -63,7 +63,7 @@ class CustomerListContainer extends React.Component<FinalProps, State> {
       customersMerge,
       customersVerify,
       type,
-      history,
+      history
     } = this.props;
 
     let columnsConfig =
@@ -73,18 +73,18 @@ class CustomerListContainer extends React.Component<FinalProps, State> {
     const localConfig = localStorage.getItem(`erxes_${type}_columns_config`);
 
     if (localConfig) {
-      columnsConfig = JSON.parse(localConfig).filter((conf) => conf.checked);
+      columnsConfig = JSON.parse(localConfig).filter(conf => conf.checked);
     }
 
     const removeCustomers = ({ customerIds }, emptyBulk) => {
       customersRemove({
-        variables: { customerIds },
+        variables: { customerIds }
       })
         .then(() => {
           emptyBulk();
-          Alert.success("You successfully deleted a customer");
+          Alert.success('You successfully deleted a customer');
         })
-        .catch((e) => {
+        .catch(e => {
           Alert.error(e.message);
         });
     };
@@ -95,16 +95,16 @@ class CustomerListContainer extends React.Component<FinalProps, State> {
       customersMerge({
         variables: {
           customerIds: ids,
-          customerFields: data,
-        },
+          customerFields: data
+        }
       })
         .then((result: any) => {
           callback();
           this.setState({ mergeCustomerLoading: false });
-          Alert.success("You successfully merged a customer");
+          Alert.success('You successfully merged a customer');
           history.push(`/contacts/details/${result.data.customersMerge._id}`);
         })
-        .catch((e) => {
+        .catch(e => {
           Alert.error(e.message);
           this.setState({ mergeCustomerLoading: false });
         });
@@ -115,15 +115,15 @@ class CustomerListContainer extends React.Component<FinalProps, State> {
 
       customersVerify({
         variables: {
-          verificationType,
-        },
+          verificationType
+        }
       })
         .then(() => {
           Alert.success(
-            "Your request has been successfully sent. Your contacts will be verified after a while"
+            'Your request has been successfully sent. Your contacts will be verified after a while'
           );
         })
-        .catch((e) => {
+        .catch(e => {
           Alert.error(__(e.message));
         });
     };
@@ -139,25 +139,25 @@ class CustomerListContainer extends React.Component<FinalProps, State> {
       }
 
       if (bulk.length > 0) {
-        queryParams.ids = bulk.map((customer) => customer._id);
+        queryParams.ids = bulk.map(customer => customer._id);
       }
 
-      columnsConfig.forEach((checked) => {
+      columnsConfig.forEach(checked => {
         checkedConfigs.push(checked);
       });
 
       const exportQuery = {
         ...queryParams,
         type,
-        configs: JSON.stringify(columnsConfig),
+        configs: JSON.stringify(columnsConfig)
       };
 
       const stringified = queryString.stringify(exportQuery);
 
-      window.open(`${REACT_APP_API_URL}/file-export?${stringified}`, "_blank");
+      window.open(`${REACT_APP_API_URL}/file-export?${stringified}`, '_blank');
     };
 
-    const searchValue = this.props.queryParams.searchValue || "";
+    const searchValue = this.props.queryParams.searchValue || '';
 
     const { list = [], totalCount = 0 } =
       customersMainQuery.customersMain || {};
@@ -174,10 +174,10 @@ class CustomerListContainer extends React.Component<FinalProps, State> {
       responseId: this.state.responseId,
       removeCustomers,
       verifyCustomers,
-      mergeCustomerLoading: this.state.mergeCustomerLoading,
+      mergeCustomerLoading: this.state.mergeCustomerLoading
     };
 
-    const content = (props) => {
+    const content = props => {
       return <CustomersList {...updatedProps} {...props} />;
     };
 
@@ -206,7 +206,7 @@ const generateParams = ({ queryParams, type }) => {
     type,
     sortDirection: queryParams.sortDirection
       ? parseInt(queryParams.sortDirection, 10)
-      : undefined,
+      : undefined
   };
 };
 
@@ -215,38 +215,38 @@ export default withProps<Props>(
     graphql<Props, MainQueryResponse, ListQueryVariables>(
       gql(queries.customersMain),
       {
-        name: "customersMainQuery",
+        name: 'customersMainQuery',
         options: ({ queryParams, type }) => ({
-          variables: generateParams({ queryParams, type }),
-        }),
+          variables: generateParams({ queryParams, type })
+        })
       }
     ),
     graphql<Props, ListConfigQueryResponse, {}>(
       gql(queries.customersListConfig),
       {
-        name: "customersListConfigQuery",
+        name: 'customersListConfigQuery'
       }
     ),
     // mutations
     graphql<Props, RemoveMutationResponse, RemoveMutationVariables>(
       gql(mutations.customersRemove),
       {
-        name: "customersRemove",
+        name: 'customersRemove'
       }
     ),
     graphql<Props, MergeMutationResponse, MergeMutationVariables>(
       gql(mutations.customersMerge),
       {
-        name: "customersMerge",
+        name: 'customersMerge',
         options: {
-          refetchQueries: ["customersMain", "customerCounts"],
-        },
+          refetchQueries: ['customersMain', 'customerCounts']
+        }
       }
     ),
     graphql<Props, VerifyMutationResponse, VerifyMutationVariables>(
       gql(mutations.customersVerify),
       {
-        name: "customersVerify",
+        name: 'customersVerify'
       }
     )
   )(withRouter<IRouterProps>(CustomerListContainer))
