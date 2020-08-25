bba3bae79a18bc33e65a8f4fdced26941d80a30d
BatAmar Battulga <batamar.b@nmtec.co>
2020-08-25 12:03:29 +0800

    fixed pubsub redis error

--- src/pubsub.ts
+++ src/pubsub.ts
@@ -7,10 +7,14 @@ import * as Redis from 'ioredis';
 // load environment variables
 dotenv.config();
 
-const { REDIS_HOST }: { REDIS_HOST?: string } = process.env;
+const { REDIS_HOST, REDIS_PORT, REDIS_PASSWORD } = process.env;
 
 const createPubsubInstance = () => {
   if (REDIS_HOST) {
+    redisOptions.host = REDIS_HOST;
+    redisOptions.port = REDIS_PORT;
+    redisOptions.password = REDIS_PASSWORD;
+
     return new RedisPubSub({
       connectionListener: error => {
         if (error) {
