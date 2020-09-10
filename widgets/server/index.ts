731d3c089bebad7cce266fee2d0d2605cef9ae88
BatAmar Battulga <batamar.b@nmtec.co>
2020-09-10 18:05:10 +0800

    added widget status

--- widgets/server/index.ts
+++ widgets/server/index.ts
@@ -1,24 +1,29 @@
-import * as bodyParser from "body-parser";
-import * as cors from "cors";
-import * as dotenv from "dotenv";
-import * as express from "express";
-import * as path from "path";
+import * as bodyParser from 'body-parser';
+import * as cors from 'cors';
+import * as dotenv from 'dotenv';
+import * as express from 'express';
+import * as path from 'path';
 
 dotenv.config();
 
 const app = express();
+
+// for health checking
+app.get('/status', async (_req, res) => {
+  res.end('ok');
+});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(cors());
 
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 // generated scripts
-app.use("/build", express.static(path.join(__dirname, "../static")));
-app.use("/static", express.static("public"));
+app.use('/build', express.static(path.join(__dirname, '../static')));
+app.use('/static', express.static('public'));
 
 const getEnv = () => {
   const { ROOT_URL, API_URL, API_SUBSCRIPTIONS_URL } = process.env;
@@ -30,28 +35,28 @@ const getEnv = () => {
   });
 };
 
-app.get("/events", (req, res) => {
-  res.render("widget", { type: "events", env: getEnv() });
+app.get('/events', (req, res) => {
+  res.render('widget', { type: 'events', env: getEnv() });
 });
 
-app.get("/messenger", (req, res) => {
-  res.render("widget", { type: "messenger", env: getEnv() });
+app.get('/messenger', (req, res) => {
+  res.render('widget', { type: 'messenger', env: getEnv() });
 });
 
-app.get("/form", (req, res) => {
-  res.render("widget", { type: "form", env: getEnv() });
+app.get('/form', (req, res) => {
+  res.render('widget', { type: 'form', env: getEnv() });
 });
 
-app.get("/knowledgebase", (req, res) => {
-  res.render("widget", {
-    type: "knowledgebase",
+app.get('/knowledgebase', (req, res) => {
+  res.render('widget', {
+    type: 'knowledgebase',
     env: getEnv(),
     kbTopicId: req.query.topicId
   });
 });
 
-app.get("/test", (req, res) => {
-  res.render("widget-test");
+app.get('/test', (req, res) => {
+  res.render('widget-test');
 });
 
 const port = process.env.PORT || 3200;
