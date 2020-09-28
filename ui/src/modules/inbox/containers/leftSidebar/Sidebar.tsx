2e289bd5acfcbc4cc4c811b8d29b30d33e353954
Mungunshagai Enkhbat <e.mungunshagai@gmail.com>
2020-09-28 12:26:21 +0800

    feat(conversation): added resolve all button

--- ui/src/modules/inbox/containers/leftSidebar/Sidebar.tsx
+++ ui/src/modules/inbox/containers/leftSidebar/Sidebar.tsx
@@ -1,26 +1,55 @@
 import { AppConsumer } from 'appContext';
+import gql from 'graphql-tag';
+import * as compose from 'lodash.flowright';
 import Bulk from 'modules/common/components/Bulk';
 import { IBulkContentProps } from 'modules/common/components/Bulk';
+import { Alert, withProps } from 'modules/common/utils';
 import DumbSidebar from 'modules/inbox/components/leftSidebar/Sidebar';
 import React from 'react';
+import { graphql } from 'react-apollo';
 import { withRouter } from 'react-router-dom';
 import { IRouterProps } from '../../../common/types';
+import { mutations } from '../../graphql';
+import {
+  ResolveAllMutationResponse,
+  ResolveAllMutationVariables
+} from '../../types';
 import { getConfig, setConfig } from '../../utils';
+import { refetchSidebarConversationsOptions } from '../../utils';
+import { InboxManagementActionConsumer } from '../Inbox';
 
 type Props = {
   queryParams: any;
   currentConversationId?: string;
 } & IRouterProps;
 
+type FinalProps = Props & ResolveAllMutationResponse;
+
 const STORAGE_KEY = 'erxes_additional_sidebar_config';
 
-class Sidebar extends React.Component<Props> {
+class Sidebar extends React.Component<FinalProps> {
   toggle = ({ isOpen }: { isOpen: boolean }) => {
     const config = getConfig(STORAGE_KEY);
 
     config.showAddition = isOpen;
 
     setConfig(STORAGE_KEY, config);
+  };
+
+  // resolve all conversation
+  resolveAll = notifyHandler => () => {
+    this.props
+      .resolveAllMutation({ variables: this.props.queryParams })
+      .then(() => {
+        if (notifyHandler) {
+          notifyHandler();
+        }
+
+        Alert.success('The conversation has been resolved!');
+      })
+      .catch(e => {
+        Alert.error(e.message);
+      });
   };
 
   render() {
@@ -35,22 +64,28 @@ class Sidebar extends React.Component<Props> {
     }
 
     const { currentConversationId, queryParams, history } = this.props;
-
     const content = ({ bulk, toggleBulk, emptyBulk }: IBulkContentProps) => {
       return (
         <AppConsumer>
           {({ currentUser }) => (
-            <DumbSidebar
-              currentUser={currentUser}
-              currentConversationId={currentConversationId}
-              queryParams={queryParams}
-              history={history}
-              bulk={bulk}
-              emptyBulk={emptyBulk}
-              toggleBulk={toggleBulk}
-              config={getConfig(STORAGE_KEY)}
-              toggleSidebar={this.toggle}
-            />
+            <InboxManagementActionConsumer>
+              {({ notifyConsumersOfManagementAction }) => (
+                <DumbSidebar
+                  currentUser={currentUser}
+                  currentConversationId={currentConversationId}
+                  queryParams={queryParams}
+                  history={history}
+                  bulk={bulk}
+                  emptyBulk={emptyBulk}
+                  toggleBulk={toggleBulk}
+                  config={getConfig(STORAGE_KEY)}
+                  toggleSidebar={this.toggle}
+                  resolveAll={this.resolveAll(
+                    notifyConsumersOfManagementAction
+                  )}
+                />
+              )}
+            </InboxManagementActionConsumer>
           )}
         </AppConsumer>
       );
@@ -60,4 +95,16 @@ class Sidebar extends React.Component<Props> {
   }
 }
 
-export default withRouter<Props>(Sidebar);
+export default withRouter<Props>(
+  withProps<Props>(
+    compose(
+      graphql<Props, ResolveAllMutationResponse, ResolveAllMutationVariables>(
+        gql(mutations.resolveAll),
+        {
+          name: 'resolveAllMutation',
+          options: () => refetchSidebarConversationsOptions()
+        }
+      )
+    )(Sidebar)
+  )
+);
