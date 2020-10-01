75da1dc05bd61e5ab673394d4b318e7de2703395
munkhjin <munkhjin0223@gmail.com>
2020-10-01 20:27:26 +0800

    update

---
+++ ui/src/modules/settings/integrations/components/webhook/Form.tsx
@@ -0,0 +1,109 @@
+import Button from 'modules/common/components/Button';
+import FormControl from 'modules/common/components/form/Control';
+import Form from 'modules/common/components/form/Form';
+import FormGroup from 'modules/common/components/form/Group';
+import ControlLabel from 'modules/common/components/form/Label';
+import { ModalFooter } from 'modules/common/styles/main';
+import { IButtonMutateProps, IFormProps } from 'modules/common/types';
+import { __ } from 'modules/common/utils';
+import React from 'react';
+import SelectBrand from '../../containers/SelectBrand';
+import SelectChannels from '../../containers/SelectChannels';
+
+type Props = {
+  renderButton: (props: IButtonMutateProps) => JSX.Element;
+  callback: () => void;
+  onChannelChange: () => void;
+  channelIds: string[];
+};
+
+const typeOptions = [
+  {
+    label: 'Customer',
+    value: 'customer'
+  },
+  {
+    label: 'Conversation',
+    value: 'conversation'
+  }
+];
+
+class Webhook extends React.Component<Props> {
+  generateDoc = (values: { name: string; type: string; brandId: string }) => {
+    return {
+      name: values.name,
+      brandId: values.brandId,
+      kind: 'webhook',
+      data: {
+        type: values.type
+      }
+    };
+  };
+
+  renderContent = (formProps: IFormProps) => {
+    const { renderButton, callback, onChannelChange, channelIds } = this.props;
+    const { values, isSubmitted } = formProps;
+
+    return (
+      <>
+        <FormGroup>
+          <ControlLabel required={true}>Name</ControlLabel>
+          <FormControl
+            {...formProps}
+            name="name"
+            required={true}
+            autoFocus={true}
+          />
+        </FormGroup>
+
+        <FormGroup>
+          <ControlLabel>Type</ControlLabel>
+          <FormControl
+            {...formProps}
+            name="type"
+            componentClass="select"
+            options={typeOptions}
+          />
+        </FormGroup>
+
+        <SelectBrand
+          isRequired={true}
+          formProps={formProps}
+          description={__(
+            'Which specific Brand does this integration belong to?'
+          )}
+        />
+
+        <SelectChannels
+          defaultValue={channelIds}
+          isRequired={true}
+          onChange={onChannelChange}
+        />
+
+        <ModalFooter>
+          <Button
+            btnStyle="simple"
+            type="button"
+            onClick={callback}
+            icon="times-circle"
+            uppercase={false}
+          >
+            Cancel
+          </Button>
+          {renderButton({
+            name: 'integration',
+            values: this.generateDoc(values),
+            isSubmitted,
+            callback
+          })}
+        </ModalFooter>
+      </>
+    );
+  };
+
+  render() {
+    return <Form renderContent={this.renderContent} />;
+  }
+}
+
+export default Webhook;
