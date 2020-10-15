7f1441b8860a71ebe030042e2207d14d26bf3920
Munkh-Orgil <munkhorgil@live.com>
2020-10-15 18:58:09 +0800

    add some style

--- ui/src/modules/common/components/SelectWithCreate.tsx
+++ ui/src/modules/common/components/SelectWithCreate.tsx
@@ -1,31 +1,39 @@
 import client from 'apolloClient';
 import gql from 'graphql-tag';
 import debounce from 'lodash/debounce';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Select from 'react-select-plus';
 import styled from 'styled-components';
-import { __ } from '../utils';
+import { __, Alert } from '../utils';
 import Button from './Button';
-
-type Props = {
-  name: string;
-  placeholder?: string;
-  queryName: string;
-  customQuery: string;
-  selector: {
-    label: string
-    value: string
-  }
-};
-
-type Option = {
-  label: string;
-  value: string;
-};
+import Icon from './Icon';
 
 const Wrapper = styled.div`
   display: flex;
   align-items: center;
+`;
+
+const OptionWrapper = styled(Wrapper)`
+  padding: 8px 16px;
+  font-weight: 500;
+  border-bottom: 1px solid #eee;
+
+  &:last-child {
+    border: none;
+  }
+
+  &:hover {
+    background: #fafafa;
+    cursor: default;
+  }
+
+  i {
+    color: #ea475d;
+
+    &:hover {
+      cursor: pointer;
+    }
+  }
 `;
 
 const FillContent = styled.div`
@@ -33,7 +41,50 @@ const FillContent = styled.div`
   margin-right: 5px;
 `;
 
-function generateOptions(options: any = [], selector: { label: string, value: string }) {
+type OptionProps = {
+  option: any;
+  onSelect: (option: any[], e: any) => void;
+};
+
+function Option({ option, onSelect }: OptionProps) {
+  const { onRemove } = option;
+  const onClick = e => onSelect(option, e);
+  const onRemoveClick = () => onRemove(option.value);
+
+  return (
+    <OptionWrapper onClick={onClick}>
+      <FillContent>{option.label}</FillContent>
+      <Icon
+        style={{ float: 'right' }}
+        onClick={onRemoveClick}
+        icon="times-circle"
+      />
+    </OptionWrapper>
+  );
+}
+
+type Option = {
+  label: string;
+  value: string;
+  onRemove?: (value: string) => void;
+};
+
+type Props = {
+  name: string;
+  placeholder?: string;
+  queryName: string;
+  customQuery: string;
+  selector: Option;
+};
+
+type Field = {
+  search: { label: string, options: Option[] };
+  added: { label: string, options: Option[] };
+};
+
+type SelectOptions = Array<{ label: string, options: Option[] }>;
+
+function generateOptions(options: any = [], selector: Option) {
   if (options.length === 0) {
     return [];
   }
@@ -44,22 +95,36 @@ function generateOptions(options: any = [], selector: { label: string, value: st
   }));
 };
 
-function SelectWithCreate(props: Props) {
-  const { placeholder, queryName, customQuery, name, selector } = props;
-  const [options, setOptions] = useState([]);
-  const [searchValue, setSearchValue] = useState('');
-  const [value, setValue] = useState('');
-  const [companyFound, setCompanyFound] = useState(false);
+const defaultFieldValues = {
+  search: {
+    label: 'Search',
+    options: []
+  },
+  added: {
+    label: 'Added',
+    options: []
+  }
+};
 
-  const handleChange = (option: Option) => {
-    return setValue(option.value);
-  };
+function SelectWithCreate({
+ placeholder,
+ queryName,
+ customQuery,
+ name,
+ selector 
+}: Props) {
+  const [fields, setFields] = useState<Field>(defaultFieldValues);
+  const [selectOptions, setSelectOptions] = useState<SelectOptions>([]);
+  const [selectedValue, setSelectedValue] = useState<Option | null>(null);
+  const [searchValue, setSearchValue] = useState<string>('');
 
-  const handleInput = (input: string) => {
-    return setSearchValue(input);
-  };
+  useEffect(() => {
+    const updatedOptions = [fields.search, fields.added];
 
-  React.useEffect(() => {
+    setSelectOptions(updatedOptions);
+  }, [fields]);
+
+  useEffect(() => {
     const fetch = () => {
       if (searchValue.length === 0) {
         return;
@@ -70,11 +135,13 @@ function SelectWithCreate(props: Props) {
         variables: { searchValue },
       })
         .then(({ data }) => {
-          const response = data[queryName];
-          console.log(response.length > 0, response)
+          const list = data[queryName];
 
-          setCompanyFound(response.length > 0);
-          setOptions(generateOptions(response, selector));
+          const currentFields = {...fields};
+
+          currentFields.search.options = generateOptions(list, selector);
+
+          setFields(currentFields);
         });
     };
 
@@ -82,29 +149,76 @@ function SelectWithCreate(props: Props) {
 
   }, [searchValue]);
 
+  const handleChange = (option) => {
+    setSearchValue('');
+    setSelectedValue(option);
+  };
+
+  const handleInput = (input: string) => {
+    return setSearchValue(input);
+  };
+
+  const handleAdd = () => {
+    const currentFields = {...fields};
+    const { options } = currentFields.added;
+
+    const foundIndex = options.findIndex(option => option.label === searchValue);
+
+    if (foundIndex !== -1) {
+      return Alert.error('Already exist');
+    }
+
+    Alert.success('Name added');
+
+    options.push({
+      label: searchValue,
+      value: searchValue,
+      onRemove: handleRemove
+    })
+
+    setSearchValue('');
+    setFields(currentFields);
+  };
+
+  const handleRemove = (value: string) => {
+    const currentFields = {...fields};
+    const { options } = currentFields.added;
+
+    const filteredOptions = options.filter(option => option.value !== value);
+
+    currentFields.added.options = filteredOptions;
+
+    setSearchValue('');
+    setSelectedValue(null);
+    setFields(currentFields);
+  };
+
   return (
     <Wrapper>
       <FillContent>
         <Select
           placeholder={placeholder}
-          value={value}
-          options={options}
-          onBlurResetsInput={false}
-          onCloseResetsInput={false}
+          value={selectedValue}
+          options={selectOptions}
+          clearable={true}
           onChange={handleChange}
+          onBlurResetsInput={false}
+          onSelectResetsInput={true}
           onInputChange={handleInput}
+          optionComponent={Option}
         />
       </FillContent>
-      {!companyFound ? (
+      {(
         <Button
           size="small"
           btnStyle="primary"
           uppercase={false}
+          onClick={handleAdd}
           icon="plus-circle"
         >
           {`${__('Add')} ${__(name)}`}
         </Button>
-      ) : null}
+      )}
     </Wrapper>
   );
 }
