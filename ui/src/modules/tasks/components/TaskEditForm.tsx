8412e81b63a87725d03f94ce63222b556c18bc47
munkhsaikhan <munkhsaikhan.g@nmma.co>
2020-10-23 00:27:52 +0800

    little test, ui yml

--- ui/src/modules/tasks/components/TaskEditForm.tsx
+++ ui/src/modules/tasks/components/TaskEditForm.tsx
@@ -3,12 +3,7 @@ import Left from 'modules/boards/components/editForm/Left';
 import Sidebar from 'modules/boards/components/editForm/Sidebar';
 import Top from 'modules/boards/components/editForm/Top';
 import { FlexContent } from 'modules/boards/styles/item';
-import {
-  IEditFormContent,
-  IItem,
-  IItemParams,
-  IOptions
-} from 'modules/boards/types';
+import { IEditFormContent, IItem, IItemParams, IOptions } from 'modules/boards/types';
 import TaskTimer, { STATUS_TYPES } from 'modules/common/components/Timer';
 import PortableDeals from 'modules/deals/components/PortableDeals';
 import PortableTickets from 'modules/tickets/components/PortableTickets';
@@ -107,7 +102,7 @@ export default class TaskEditForm extends React.Component<Props> {
       formContent: this.renderFormContent,
       extraFields: this.state
     };
-
+    console.log('qqqqqqqqq')
     return <EditForm {...extendedProps} />;
   }
 }
