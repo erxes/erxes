ea13faa94f710f0aeb488d4861757fc65368e379
BatAmar Battulga <batamar.b@nmtec.co>
2020-12-07 20:37:02 +0800

    added backup feature

--- cli/commands/update.js
+++ cli/commands/update.js
@@ -1,7 +1,7 @@
 const fse = require("fs-extra");
 const execa = require("execa");
 const start = require('./start');
-const { filePath, log } = require('./utils');
+const { filePath, log, execCommand } = require('./utils');
 
 module.exports = async function() {
   try {
@@ -15,11 +15,12 @@ module.exports = async function() {
     }
 
     log('Backing up privite folder ...');
-
     await fse.copy(filePath('build/api/private'), filePath('private'));
 
-    log('Removing old build ...');
+    log('Dumping database ...');
+    await execCommand('mongodump');
 
+    log('Removing old build ...');
     await fse.remove(filePath('build'));
 
     await start();
