21e02795a95b0bfb8e0df00978b47dd8b75f1604
Ganzorig Bayarsaikhan <ganzorig.b@nmma.co>
2020-12-09 00:42:19 +0800

    style(version): improve releas info, version text

--- ui/src/modules/settings/status/components/ReleaseInfo.tsx
+++ ui/src/modules/settings/status/components/ReleaseInfo.tsx
@@ -1,18 +1,67 @@
+import HeaderDescription from 'modules/common/components/HeaderDescription';
+import { __ } from 'modules/common/utils';
 import parseMd from 'modules/common/utils/parseMd';
+import Wrapper from 'modules/layout/components/Wrapper';
 import React from 'react';
+import styled from 'styled-components';
 
 type Props = {
   info;
 };
 
+const Container = styled.div`
+  padding: 30px;
+
+  h2 {
+    margin-top: 0;
+  }
+
+  h3 {
+    font-size: 22px;
+    margin-bottom: 15px;
+  }
+
+  em {
+    text-transform: capitalize;
+  }
+`;
+
 const ReleaseInfo = (props: Props) => {
   const releaseInfo = props.info.releaseInfo || {};
 
+  const breadcrumb = [
+    { title: __('Settings'), link: '/settings' },
+    { title: __('System status') }
+  ];
+
+  const content = (
+    <Container>
+      <div
+        dangerouslySetInnerHTML={{
+          __html: parseMd(releaseInfo.body || '')
+        }}
+      />
+    </Container>
+  );
+
   return (
-    <p
-      dangerouslySetInnerHTML={{
-        __html: parseMd(releaseInfo.body || '')
-      }}
+    <Wrapper
+      header={
+        <Wrapper.Header title={__('Release Info')} breadcrumb={breadcrumb} />
+      }
+      actionBar={
+        <Wrapper.ActionBar
+          left={
+            <HeaderDescription
+              icon="/images/actions/28.svg"
+              title="Release Info"
+              description={`This allows you to see erxes's real-time information about releases.`}
+            />
+          }
+        />
+      }
+      content={content}
+      center={true}
     />
   );
 };
