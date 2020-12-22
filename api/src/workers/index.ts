47be2907ccbea0d477f109c4944a60e74d881fa5
Devraj Ash <65898509+autoemail@users.noreply.github.com>
2020-12-22 17:16:15 +0530

    updated index.ts
    In async(_req, res) instead of using _req it is more convenient to use req which will help developer to understand and they will be less confused.

--- api/src/workers/index.ts
+++ api/src/workers/index.ts
@@ -18,7 +18,7 @@ const app = express();
 app.disable('x-powered-by');
 
 // for health check
-app.get('/health', async (_req, res) => {
+app.get('/health', async (req, res) => {
   res.end('ok');
 });
 
