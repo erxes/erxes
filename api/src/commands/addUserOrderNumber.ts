3daddf4a2f5518c8c9290cd12dc9fb84b36f8d7b
Munkh-Orgil <munkhorgil@live.com>
2021-01-04 17:14:40 +0800

    fix command

--- api/src/commands/addUserOrderNumber.ts
+++ api/src/commands/addUserOrderNumber.ts
@@ -1,19 +1,37 @@
-import { connect } from '../db/connection';
+import * as dotenv from 'dotenv';
+import { connect, disconnect } from '../db/connection';
 import { Users } from '../db/models';
+
+dotenv.config();
 
 const main = async () => {
   await connect();
 
-  const users = await Users.find().lean();
+  const users = await Users.find({});
 
   const formatNumber = n => ('00' + n).slice(-3);
 
-  users.map(async (user, index) => {
-    await Users.update(
-      { _id: user._id },
-      { $addFields: { code: formatNumber(index) } }
-    );
-  });
+  const doc: any = [];
+
+  let count = 0;
+
+  for (const user of users) {
+    doc.push({
+      updateOne: {
+        filter: {
+          _id: user._id
+        },
+        update: {
+          $set: { code: formatNumber(count) }
+        }
+      }
+    });
+
+    count++;
+  }
+
+  await Users.bulkWrite(doc);
+  await disconnect();
 
   process.exit();
 };
