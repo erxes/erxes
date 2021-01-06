236ba190731b707c6a95d79308337c39d5a6d8f9
Enkhtuvshin0513 <enhtuwshinn@gmail.com>
2021-01-06 19:55:46 +0800

    fix dashboard`s pie chart

--- dashboard-ui/src/modules/dashboard/components/ChartRenderer.tsx
+++ dashboard-ui/src/modules/dashboard/components/ChartRenderer.tsx
@@ -185,6 +185,7 @@ const TypeToChartComponent = {
             <Pie
               isAnimationActive={false}
               data={renderData(resultSet.chartPivot)}
+              nameKey="x"
               dataKey={resultSet.seriesNames[0].key}
               fill="#8884d8"
             >
