cf166fa49162d37484c26773a81a503787b104e0
Buyantogtokh <buyantogtokh.s@nmma.co>
2021-01-08 01:02:28 +0800

    small update

--- engages-email-sender/src/__tests__/deliveryReportsDb.test.ts
+++ engages-email-sender/src/__tests__/deliveryReportsDb.test.ts
@@ -1,4 +1,5 @@
 import { DeliveryReports, Stats } from '../models';
+import { prepareAvgStats } from '../utils';
 import { statsFactory } from './factories';
 import './setup';
 
@@ -40,6 +41,24 @@ test('DeliveryReports: updateOrCreateReport', async done => {
   });
 
   expect(deliveryReportObj.status).toBe('complaint');
+
+  done();
+});
+
+test('Stats: Test average bounce & complaint percent', async done => {
+  const s1 = await statsFactory({});
+  const s2 = await statsFactory({});
+  const averageStat = await prepareAvgStats();
+
+  const bounce1 = (s1.bounce * 100) / s1.total;
+  const bounce2 = (s2.bounce * 100) / s2.total;
+  const complaint1 = (s1.complaint * 100) / s1.total;
+  const complaint2 = (s2.complaint * 100) / s2.total;
+
+  expect(averageStat[0].avgBouncePercent).toBe((bounce1 + bounce2) / 2);
+  expect(averageStat[0].avgComplaintPercent).toBe(
+    (complaint1 + complaint2) / 2
+  );
 
   done();
 });
--- engages-email-sender/src/sender.ts
+++ engages-email-sender/src/sender.ts
@@ -170,7 +170,7 @@ export const start = async (data: {
 
     const MAIN_API_DOMAIN = getEnv({ name: 'MAIN_API_DOMAIN' });
 
-    const unSubscribeUrl = `${MAIN_API_DOMAIN}/unsubscribe/?cid=${customer._id}`;
+    const unsubscribeUrl = `${MAIN_API_DOMAIN}/unsubscribe/?cid=${customer._id}`;
 
     // replace customer attributes =====
     let replacedContent = content;
@@ -182,7 +182,7 @@ export const start = async (data: {
       }
     }
 
-    replacedContent += `<div style="padding: 10px; color: #ccc; text-align: center; font-size:12px;">If you want to use service like this click <a style="text-decoration: underline; color: #ccc;" href="https://erxes.io" target="_blank">here</a> to read more. Also you can opt out from our email subscription <a style="text-decoration: underline;color: #ccc;" rel="noopener" target="_blank" href="${unSubscribeUrl}">here</a>.  <br>© 2020 erxes inc Growth Marketing Platform </div>`;
+    replacedContent += `<div style="padding: 10px; color: #ccc; text-align: center; font-size:12px;">If you want to use service like this click <a style="text-decoration: underline; color: #ccc;" href="https://erxes.io" target="_blank">here</a> to read more. Also you can opt out from our email subscription <a style="text-decoration: underline;color: #ccc;" rel="noopener" target="_blank" href="${unsubscribeUrl}">here</a>.  <br>© 2021 erxes inc Growth Marketing Platform </div>`;
 
     try {
       await transporter.sendMail({
