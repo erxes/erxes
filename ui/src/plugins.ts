9ac71cfaca3a625caacf50f1f0771135a705bf6a
munkhsaikhan <munkhsaikhan.g@nmma.co>
2021-01-11 09:19:38 +0800

    plugins ui importer

---
+++ ui/src/plugins.ts
@@ -0,0 +1,9 @@
+export default {
+  'erxes-plugin-car': require('../../plugins/erxes-plugin-car/ui').default,
+
+  'erxes-plugin-loyalty': require('../../plugins/erxes-plugin-loyalty/ui')
+    .default,
+
+  'erxes-plugin-sync-erkhet': require('../../plugins/erxes-plugin-sync-erkhet/ui')
+    .default
+};
