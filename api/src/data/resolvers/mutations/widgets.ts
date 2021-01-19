59ff052e996afd6ddc7ad21863599ab16536159b
Soyombo <soyombo.b@nmtec.co>
2021-01-19 23:01:05 +0800

    Update widgets.ts

--- api/src/data/resolvers/mutations/widgets.ts
+++ api/src/data/resolvers/mutations/widgets.ts
@@ -385,7 +385,7 @@ const widgetMutations = {
       cachedCustomerId: args.cachedCustomerId
     });
 
-    return { status: 'ok', messageId: message._id, customerId: customer._id };
+    return { status: 'ok', messageId: message._id };
   },
 
   widgetsLeadIncreaseViewCount(_root, { formId }: { formId: string }) {
