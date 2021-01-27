6cb0abfe62390a16a29e8aa5f4e73988881d7f8f
soyombo <soyombo.b@nmtec.co>
2021-01-28 05:08:18 +0800

    test fix

--- api/src/__tests__/widgetMutations.test.ts
+++ api/src/__tests__/widgetMutations.test.ts
@@ -1216,24 +1216,11 @@ describe('lead', () => {
       formId: form._id
     };
 
-    const spyEmail = jest.spyOn(widgetMutations, 'widgetsSendEmail');
-
-    const mutation = `
-      mutation widgetsSendEmail($toEmails: [String], $fromEmail: String, $title: String, $content: String, $formId: String, $customerId: String) {
-        widgetsSendEmail(toEmails: $toEmails, fromEmail: $fromEmail, title: $title, content: $content, formId: $formId, customerId: $customerId)
-      }
-    `;
-
-    spyEmail.mockImplementation(() => Promise.resolve());
-
-    const response = await graphqlRequest(
-      mutation,
-      'widgetsSendEmail',
-      emailParams
+    const response = await widgetMutations.widgetsSendEmail(
+      {},
+      { ...emailParams }
     );
 
-    expect(response).toBe(null);
-
-    spyEmail.mockRestore();
+    expect(response).toBe(undefined);
   });
 });
