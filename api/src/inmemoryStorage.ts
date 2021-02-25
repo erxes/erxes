34e7dd47329e5ebdea32491a6e08a182a6f2a440
munkhjin <munkhjin0223@gmail.com>
2021-02-25 10:11:26 +0800

    refactor in memory cache

--- api/src/inmemoryStorage.ts
+++ api/src/inmemoryStorage.ts
@@ -1,5 +1,6 @@
 import * as dotenv from 'dotenv';
 import memoryStorage from 'erxes-inmemory-storage';
+import { debugError } from './debuggers';
 
 // load environment variables
 dotenv.config();
@@ -14,6 +15,66 @@ export const initMemoryStorage = () => {
     port: REDIS_PORT,
     password: REDIS_PASSWORD
   });
+};
+
+export const removeKey = async (key: string) => {
+  try {
+    return await client.removeKey(key);
+  } catch (e) {
+    debugError(
+      `For removeKey of inmemoryStorage, key: ${key}. Error: ${e.message}`
+    );
+  }
+};
+
+export const removeFromArray = async (setKey: string, setMember: any) => {
+  try {
+    await client.removeFromArray(setKey, setMember);
+  } catch (e) {
+    debugError(
+      `For removeFromArray of inmemoryStorage, ${setKey}: ${setMember}. Error: ${e.message}`
+    );
+  }
+};
+
+export const addToArray = async (setKey: string, setMember: any) => {
+  try {
+    await client.addToArray(setKey, setMember);
+  } catch (e) {
+    debugError(
+      `For addToArray of inmemoryStorage, ${setKey}: ${setMember}. Error: ${e.message}`
+    );
+  }
+};
+
+export const inArray = async (setKey: string, setMember: any) => {
+  try {
+    return await client.inArray(setKey, setMember);
+  } catch (e) {
+    debugError(
+      `For inArray of inmemoryStorage, ${setKey}: ${setMember}. Error: ${e.message}`
+    );
+  }
+};
+
+export const set = async (key: string, value: any) => {
+  try {
+    client.set(key, value);
+  } catch (e) {
+    debugError(
+      `For set of inmemoryStorage, key: ${key}, value: ${value}. Error: ${e.message}`
+    );
+  }
+};
+
+export const get = async (key: string, defaultValue?: any) => {
+  try {
+    return await client.get(key, defaultValue);
+  } catch (e) {
+    debugError(
+      `For get of inmemoryStorage, key: ${key}, default value: ${defaultValue}. Error: ${e.message}`
+    );
+  }
 };
 
 export default function() {
