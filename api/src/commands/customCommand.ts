77f2b63f65f4d43bf55c9d6d4298bb499b0a7f9d
Buyantogtokh <buyantogtokh.s@nmma.co>
2021-03-10 02:51:18 +0800

    add custom command to restore missing permissions

--- api/src/commands/customCommand.ts
+++ api/src/commands/customCommand.ts
@@ -1,43 +1,63 @@
 import * as dotenv from 'dotenv';
-import { MESSAGE_KINDS } from '../data/constants';
 import { connect } from '../db/connection';
-import { EngageMessages } from '../db/models';
+import { Permissions } from '../db/models';
+import { IPermissionDocument } from '../db/models/definitions/permissions';
 
 dotenv.config();
+
+const fixPermissions = async (
+  permissions: IPermissionDocument[],
+  checkAction: string
+) => {
+  const ids: string[] = [];
+
+  for (const perm of permissions) {
+    if (
+      perm.requiredActions &&
+      perm.requiredActions.length > 0 &&
+      !perm.requiredActions.includes(checkAction)
+    ) {
+      ids.push(perm._id);
+    }
+  }
+
+  if (ids.length > 0) {
+    console.log(`Fixing ${ids.length} permissions for action "${checkAction}"`);
+
+    await Permissions.updateMany(
+      { _id: { $in: ids } },
+      { $push: { requiredActions: checkAction } }
+    );
+  }
+};
 
 const command = async () => {
   await connect();
 
-  const campaigns = await EngageMessages.find({
-    kind: { $ne: MESSAGE_KINDS.VISITOR_AUTO },
-    tagIds: { $exists: true, $not: { $size: 0 } },
-    customerTagIds: { $exists: false }
+  const inboxPermissions = await Permissions.find({
+    module: 'inbox',
+    action: 'inboxAll'
+  });
+  const brandPermissions = await Permissions.find({
+    module: 'brands',
+    action: 'brandsAll'
+  });
+  const channelPermissions = await Permissions.find({
+    module: 'channels',
+    action: 'channelsAll'
+  });
+  const productPermissions = await Permissions.find({
+    module: 'products',
+    action: 'productsAll'
   });
 
-  console.log(`Found ${campaigns.length} campaigns`);
+  await fixPermissions(inboxPermissions, 'conversationResolveAll');
 
-  const bulkOptions: any[] = [];
+  await fixPermissions(brandPermissions, 'exportBrands');
 
-  for (const campaign of campaigns) {
-    bulkOptions.push({
-      updateOne: {
-        filter: {
-          _id: campaign._id
-        },
-        update: {
-          $set: {
-            customerTagIds: campaign.tagIds || []
-          }
-        }
-      }
-    });
-  }
+  await fixPermissions(channelPermissions, 'exportChannels');
 
-  if (bulkOptions.length > 0) {
-    await EngageMessages.bulkWrite(bulkOptions);
-  }
-
-  console.log(`Completed with ${bulkOptions.length} rows`);
+  await fixPermissions(productPermissions, 'productsMerge');
 
   process.exit();
 };
