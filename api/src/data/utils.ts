863a4e6f257afe520be6cf903460605792ed9452
Buyantogtokh <buyantogtokh.s@nmma.co>
2021-03-15 14:28:27 +0800

    split long sms into chunks in conversation (#2680)

--- api/src/data/utils.ts
+++ api/src/data/utils.ts
@@ -904,3 +904,21 @@ export const isUsingElk = () => {
 
   return ELK_SYNCER === 'false' ? false : true;
 };
+
+/**
+ * Splits text into chunks of strings limited by given character count
+ * .{1,100}(\s|$)
+ * . - matches any character (except for line terminators)
+ * {1,100} - matches the previous token between 1 and 100 times, as many times as possible, giving back as needed (greedy)
+ * (\s|$) - capturing group
+ * \s - matches any whitespace character
+ * $ - asserts position at the end of the string
+ *
+ * @param str text to be split
+ * @param size character length of each chunk
+ */
+export const splitStr = (str: string, size: number): string[] => {
+  const cleanStr = strip(str);
+
+  return cleanStr.match(new RegExp(new RegExp(`.{1,${size}}(\s|$)`, 'g')));
+};
