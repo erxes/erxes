29bfebfb6cc78c386e82902834db69e85e46fc04
Buyantogtokh <buyantogtokh.s@nmma.co>
2021-03-18 00:08:26 +0800

    Enhancement/enable permission fixing (#2683)
    enhancement(permissions): enable permission fixing

--- api/src/data/permissions/utils.ts
+++ api/src/data/permissions/utils.ts
@@ -1,6 +1,8 @@
-import { Permissions, Users } from '../../db/models';
+import { Permissions, Users, UsersGroups } from '../../db/models';
+import { IPermissionDocument } from '../../db/models/definitions/permissions';
 import { IUserDocument } from '../../db/models/definitions/users';
 import { get, set } from '../../inmemoryStorage';
+import { moduleObjects } from './actions/permission';
 
 export interface IModuleMap {
   name: string;
@@ -164,32 +166,105 @@ export const userActionsMap = async (
     groupId: { $in: user.groupIds }
   });
 
-  const totalPermissions = [
+  const totalPermissions: IPermissionDocument[] = [
     ...userPermissions,
     ...groupPermissions,
     ...(user.customPermissions || [])
   ];
   const allowedActions: IActionMap = {};
 
-  const check = (name: string, allowed: boolean) => {
+  const check = (name: string, allowed: boolean, _id: string) => {
     if (typeof allowedActions[name] === 'undefined') {
       allowedActions[name] = allowed;
     }
 
+    // if a specific permission is denied elsewhere, follow that rule
     if (allowedActions[name] && !allowed) {
       allowedActions[name] = false;
     }
   };
 
-  for (const { requiredActions, allowed, action } of totalPermissions) {
+  for (const { _id, requiredActions, allowed, action } of totalPermissions) {
     if (requiredActions.length > 0) {
       for (const actionName of requiredActions) {
-        check(actionName, allowed);
+        check(actionName, allowed, _id);
       }
     } else {
-      check(action, allowed);
+      check(action, allowed, _id);
     }
   }
 
   return allowedActions;
+};
+
+/**
+ * If a permission is added or removed from the constants & forgotten from required actions,
+ * this util will fix that.
+ */
+export const fixPermissions = async (): Promise<string[]> => {
+  const modules = Object.getOwnPropertyNames(moduleObjects);
+  const result: string[] = [];
+
+  for (const mod of modules) {
+    const moduleItem: IModuleMap = moduleObjects[mod];
+
+    if (moduleItem && moduleItem.actions) {
+      const allAction: IActionsMap | undefined = moduleItem.actions.find(
+        a => a.description === 'All'
+      );
+      const otherActions = moduleItem.actions
+        .filter(a => a.description !== 'All')
+        .map(a => a.name);
+
+      if (
+        allAction &&
+        allAction.use &&
+        allAction.use.length > 0 &&
+        allAction.name
+      ) {
+        // array with the most actions will be recovered
+        const mostActions =
+          allAction.use.length > otherActions.length
+            ? allAction.use
+            : otherActions;
+
+        const permissions: IPermissionDocument[] = await Permissions.find({
+          module: mod,
+          action: allAction.name,
+          $or: [
+            { requiredActions: { $eq: null } },
+            { requiredActions: { $not: { $size: mostActions.length } } }
+          ]
+        }).lean();
+
+        for (const perm of permissions) {
+          await Permissions.updateOne(
+            { _id: perm._id },
+            { $set: { requiredActions: mostActions } }
+          );
+
+          let message = '';
+
+          if (perm.userId) {
+            const user = await Users.findOne({ _id: perm.userId });
+
+            message = user
+              ? `user "${user.email || user.username}"`
+              : perm.userId;
+          }
+          if (perm.groupId) {
+            const group = await UsersGroups.findOne({ _id: perm.groupId });
+
+            message = group ? `user group "${group.name}"` : perm.groupId;
+          }
+
+          result.push(
+            `Permission "${allAction.name}" of module "${mod}" has been fixed for ${message}`
+          );
+        }
+      } // end allAction checking
+    } // end module item checking
+  } // end module name loop
+
+  return result;
 };
