079cfdebb3840de8139264c37b015acc05e181b4
Buyantogtokh <buyantogtokh.s@nmma.co>
2021-04-15 01:08:37 +0800

    add sms delivery query & types

---
+++ api/src/data/resolvers/queries/smsDeliveries.ts
@@ -0,0 +1,48 @@
+import { debugExternalApi } from '../../../debuggers';
+import { moduleRequireLogin } from '../../permissions/wrappers';
+import { IContext } from '../../types';
+
+interface ISmsDeliveryParams {
+  type: string;
+  to: string;
+  page?: number;
+  perPage?: number;
+}
+
+const smsDeliveryQueries = {
+  async smsDeliveries(
+    _root,
+    { type, to }: ISmsDeliveryParams,
+    { dataSources }: IContext
+  ) {
+    if (!type) {
+      throw new Error('SMS delivery type must be chosen');
+    }
+
+    try {
+      let response: any = {};
+
+      if (type === 'campaign') {
+        response = await dataSources.EngagesAPI.getSmsDeliveries({ type, to });
+      }
+      if (type === 'integration') {
+        response = await dataSources.IntegrationsAPI.getSmsDeliveries({
+          type,
+          to
+        });
+      }
+      if (response.status !== 'ok' && response.error) {
+        throw new Error(response.error);
+      }
+
+      return { list: response.data, totalCount: response.totalCount };
+    } catch (e) {
+      debugExternalApi(e);
+      return { error: e.message };
+    }
+  }
+};
+
+moduleRequireLogin(smsDeliveryQueries);
+
+export default smsDeliveryQueries;
