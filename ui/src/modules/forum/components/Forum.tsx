939ad01a6e294d5899a92757f1cf6ee60892a09c
Bat-Amgalan Nasan-Ochir <batamgalan.n@nmma.co>
2021-05-26 17:32:39 +0800

    add huge forum updates

--- ui/src/modules/forum/components/Forum.tsx
+++ ui/src/modules/forum/components/Forum.tsx
@@ -1,33 +1,103 @@
 import Wrapper from 'modules/layout/components/Wrapper';
+import Button from 'modules/common/components/Button';
+import ModalTrigger from 'modules/common/components/ModalTrigger';
+import Pagination from 'modules/common/components/pagination/Pagination';
+import { __ } from 'modules/common/utils';
+import { Title } from 'modules/common/styles/main';
 import React from 'react';
 
 import { ForumList } from '../containers/forum';
+import { ITopic } from '../types';
+import { DiscussionList, DiscussionForm } from '../containers/discussion';
 
-class Forum extends React.Component {
+type Props = {
+  currentTopic: ITopic;
+  queryParams: any;
+  discussionsCount: number;
+};
+
+class Forum extends React.Component<Props> {
   breadcrumb() {
+    const currentTopic = this.props.currentTopic || {
+      title: ''
+    };
+
     const list = [{ title: 'Forum', link: '/forum' }];
+    const topicLink = `/forum?id=${currentTopic._id}`;
+
+    if (currentTopic.title) {
+      list.push({
+        title: currentTopic.title,
+        link: topicLink
+      });
+    }
 
     return list;
   }
 
   render() {
+    const { queryParams, currentTopic, discussionsCount } = this.props;
+
+    const trigger = (
+      <Button btnStyle="primary" uppercase={false} icon="plus-circle">
+        Add Discussion
+      </Button>
+    );
+
+    const content = props => (
+      <DiscussionForm
+        {...props}
+        queryParams={queryParams}
+        currentTopicId={currentTopic._id}
+      />
+    );
+
+    const actionBarLeft = currentTopic._id && (
+      <ModalTrigger
+        title="Add Discussion"
+        trigger={trigger}
+        size="lg"
+        autoOpenKey="showForumAddDiscussionModal"
+        content={content}
+        enforceFocus={false}
+      />
+    );
+
+    const leftActionBar = (
+      <Title>
+        {currentTopic.title}
+        <span>
+          ({discussionsCount}{' '}
+          {discussionsCount > 1 ? __('discussions') : __('discussion')})
+        </span>
+      </Title>
+    );
+
     return (
       <Wrapper
         header={
-          <Wrapper.Header title={`${'Forum'}`} breadcrumb={this.breadcrumb()} />
-        }
-        leftSidebar={<ForumList />}
-        transparent={true}
-        content={''}
-        /* content={
-          <ArticleList
-            queryParams={queryParams}
-            currentCategoryId={currentCategory._id}
-            topicId={
-              currentCategory.firstTopic && currentCategory.firstTopic._id
-            }
+          <Wrapper.Header
+            title={`${currentTopic.title}`}
+            breadcrumb={this.breadcrumb()}
           />
-        } */
+        }
+        leftSidebar={
+          <ForumList
+            currentTopicId={currentTopic._id}
+            queryParams={queryParams}
+          />
+        }
+        actionBar={
+          <Wrapper.ActionBar left={leftActionBar} right={actionBarLeft} />
+        }
+        footer={currentTopic._id && <Pagination count={10} />}
+        transparent={true}
+        content={
+          <DiscussionList
+            queryParams={queryParams}
+            currentTopicId={currentTopic._id}
+          />
+        }
       />
     );
   }
