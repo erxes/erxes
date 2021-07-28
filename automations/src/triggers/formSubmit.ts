34c945bf4fe836b23a860a09abb1696094f52f52
soyombo <soyombo.b@nmtec.co>
2021-07-28 09:15:36 +0800

    formSubmit update

--- automations/src/triggers/formSubmit.ts
+++ automations/src/triggers/formSubmit.ts
@@ -1,7 +1,18 @@
+import Automations from "../models/Automations"
+
 export const formSubmit = async ({ trigger, data, targetId }) => {
-  trigger
-  data
-  targetId
+
+  const automations = await Automations.find({ 'triggers.config.formId': targetId, 'triggers.type': 'formSubmit' });
+
+  if (automations.length === 0) {
+    return false;
+  }
+
+
+  // check each actions of automations and process action
+  for (const { actions } of automations) {
+    console.log('actions: ', actions)
+  }
 
   return true;
 }
