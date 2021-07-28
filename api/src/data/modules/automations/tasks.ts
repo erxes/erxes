0c27a9a87381317dc5e9ec90efe0cae64887f11b
soyombo <soyombo.b@nmtec.co>
2021-07-28 09:48:29 +0800

    formupdate create task

--- api/src/data/modules/automations/tasks.ts
+++ api/src/data/modules/automations/tasks.ts
@@ -1,11 +1,24 @@
-import { Tasks, Stages } from '../../../db/models';
+import { Tasks, Stages, Conformities } from '../../../db/models';
 import { graphqlPubsub } from '../../../pubsub';
 import { sendError, sendSuccess } from './utils';
 
 export const receiveRpcMessageTasks = async (action, doc) => {
   if (action === 'add-task') {
     try {
+      if (doc.conversationId) {
+        doc.sourceConversationIds = [doc.conversationId];
+      }
+
       const task = await Tasks.createTask({ ...doc });
+
+      if (doc.customerId) {
+        await Conformities.addConformity({
+          mainType: 'task',
+          mainTypeId: task._id,
+          relType: 'customer',
+          relTypeId: doc.customerId
+        });
+      }
 
       const stage = await Stages.getStage(task.stageId);
 
