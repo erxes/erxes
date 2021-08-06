2b1cdb7d3241a5e2bc144e80a205c4ab4e1f8057
soyombo <soyombo.b@nmtec.co>
2021-08-06 14:48:10 +0800

    Update activityLogs.ts

--- api/src/cronJobs/activityLogs.ts
+++ api/src/cronJobs/activityLogs.ts
@@ -4,7 +4,14 @@ import { RABBITMQ_QUEUES } from '../data/constants';
 import { ACTIVITY_LOG_ACTIONS, putActivityLog } from '../data/logUtils';
 import { fetchSegment } from '../data/modules/segments/queryBuilder';
 import { connect } from '../db/connection';
-import { Companies, Customers, Segments, Tickets } from '../db/models';
+import {
+  Companies,
+  Customers,
+  Deals,
+  Segments,
+  Tasks,
+  Tickets
+} from '../db/models';
 import messageBroker from '../messageBroker';
 
 /**
@@ -19,39 +26,62 @@ export const createActivityLogsFromSegments = async () => {
   for (const segment of segments) {
     const ids = await fetchSegment(segment);
 
-    const associatedCustomers = await fetchSegment(segment, {
-      associatedCustomers: true
-    });
+    // const customers = await Customers.find({ _id: { $in: ids } }, { _id: 1 });
+    // const customerIds = customers.map(c => c._id);
 
-    const customers = await Customers.find({ _id: { $in: ids } }, { _id: 1 });
-    const customerIds = customers.map(c => c._id);
+    // const companies = await Companies.find({ _id: { $in: ids } }, { _id: 1 });
+    // const companyIds = companies.map(c => c._id);
 
-    const companies = await Companies.find({ _id: { $in: ids } }, { _id: 1 });
-    const companyIds = companies.map(c => c._id);
+    // const tickets = await Tickets.find({ _id: { $in: ids } }, { _id: 1 });
+    // const ticketIds = tickets.map(c => c._id);
 
-    const tickets = await Tickets.find({ _id: { $in: ids } }, { _id: 1 });
-    const ticketIds = tickets.map(c => c._id);
+    let model: any = Customers;
+
+    switch (segment.contentType) {
+      case 'customer':
+        model = Customers;
+        break;
+
+      case 'lead':
+        model = Customers;
+        break;
+
+      case 'visitor':
+        model = Customers;
+        break;
+
+      case 'company':
+        model = Companies;
+        break;
+
+      case 'deal':
+        model = Deals;
+        break;
+
+      case 'task':
+        model = Tasks;
+        break;
+
+      case 'ticket':
+        model = Tickets;
+        break;
+
+      default:
+        break;
+    }
+
+    const result = await model.find({ _id: { $in: ids } }, { _id: 1 });
+    const contentIds = result.map(c => c._id);
 
     await putActivityLog({
       action: ACTIVITY_LOG_ACTIONS.CREATE_SEGMENT_LOG,
-      data: { segment, contentIds: customerIds, type: 'customer' }
+      data: { segment, contentIds, type: segment.contentType }
     });
 
-    await putActivityLog({
-      action: ACTIVITY_LOG_ACTIONS.CREATE_SEGMENT_LOG,
-      data: { segment, contentIds: companyIds, type: 'company' }
-    });
-
-    await putActivityLog({
-      action: ACTIVITY_LOG_ACTIONS.CREATE_SEGMENT_LOG,
-      data: { segment, contentIds: ticketIds, type: 'ticket' }
-    });
-
-    if (associatedCustomers.length > 0) {
+    if (contentIds.length > 0) {
       messageBroker().sendMessage(RABBITMQ_QUEUES.AUTOMATIONS_TRIGGER, {
-        mainType: segment.contentType,
-        type: 'customer',
-        data: associatedCustomers,
+        type: segment.contentType,
+        data: contentIds,
         targetId: segment.id
       });
     }
