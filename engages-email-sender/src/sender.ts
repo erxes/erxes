7b74acda874153a11f90b52773e4ccd6b2cbba05
BatAmar Battulga <batamar.b@nmtec.co>
2021-08-19 12:30:44 +0800

    changed engage mail footer text

--- engages-email-sender/src/sender.ts
+++ engages-email-sender/src/sender.ts
@@ -75,7 +75,7 @@ export const start = async (data: IEmailParams) => {
       }
     }
 
-    replacedContent += `<div style="padding: 10px; color: #ccc; text-align: center; font-size:12px;">If you want to use service like this click <a style="text-decoration: underline; color: #ccc;" href="https://erxes.io" target="_blank">here</a> to read more. Also you can opt out from our email subscription <a style="text-decoration: underline;color: #ccc;" rel="noopener" target="_blank" href="${unsubscribeUrl}">here</a>.  <br>© 2021 erxes inc Growth Marketing Platform </div>`;
+    replacedContent += `<div style="padding: 10px; color: #ccc; text-align: center; font-size:12px;">You are receiving this email because you have signed up for our services. <br /> <a style="text-decoration: underline;color: #ccc;" rel="noopener" target="_blank" href="${unsubscribeUrl}">Unsubscribe</a> </div>`;
 
     try {
       await transporter.sendMail({
