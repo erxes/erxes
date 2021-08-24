887d0f50fe425f2927ed5fb6d347ff0621571b35
munkhjin <munkhjin0223@gmail.com>
2021-08-24 17:14:19 +0800

    update

--- ui/src/modules/automations/components/Histories.tsx
+++ ui/src/modules/automations/components/Histories.tsx
@@ -1,14 +1,32 @@
 import React from 'react';
 import dayjs from 'dayjs';
+import Table from 'modules/common/components/table';
+import { __ } from 'modules/common/utils';
+import withTableWrapper from 'modules/common/components/table/withTableWrapper';
 
 export default function Histories(props: any) {
-  return (
-    <div>
-      {props.histories.map((row, index) => (
-        <div key={index}>
-          {row.description} {dayjs(row.createdAt).format('lll')}
-        </div>
-      ))}
-    </div>
-  );
+  const renderContent = () => {
+    return (
+      <withTableWrapper.Wrapper>
+        <Table whiteSpace="nowrap" bordered={true} hover={true}>
+          <thead>
+            <tr>
+              <th>{__('Description')}</th>
+              <th>{__('Created date')}</th>
+            </tr>
+          </thead>
+          <tbody id="automationHistories">
+            {props.histories.map((row, index) => (
+              <tr key={index}>
+                <td>{row.description}</td>
+                <td>{dayjs(row.createdAt).format('lll')}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </withTableWrapper.Wrapper>
+    );
+  };
+
+  return renderContent();
 }
