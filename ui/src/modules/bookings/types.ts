3acdc891855caa620843516d05a9248037c39754
Bat-Amgalan Nasan-Ochir <batamgalan.n@nmma.co>
2021-09-02 15:14:07 +0800

    add types

---
+++ ui/src/modules/bookings/types.ts
@@ -0,0 +1,39 @@
+import { QueryResponse } from 'modules/common/types';
+
+export interface IBooking {
+  name?: string;
+  size?: string;
+  images?: string[];
+  font?: string;
+  fontColor?: string;
+  columnColor?: string;
+  activeColumn?: string;
+  rowColor?: string;
+  activeRow?: string;
+  columnShape?: string;
+  rowShape?: string;
+}
+
+export interface IBookingDocument extends IBooking {
+  _id: string;
+}
+
+// query types
+export type BookingsQueryResponse = {
+  bookings: IBookingDocument[];
+} & QueryResponse;
+
+export type BookingDetailQueryResponse = {
+  bookingDetail: IBookingDocument;
+} & QueryResponse;
+
+// mutation types
+export type BookingRemoveMutationVariables = {
+  _id: string;
+};
+
+export type BookingRemoveMutationResponse = {
+  bookingsRemoveMutation: (params: {
+    variables: BookingRemoveMutationVariables;
+  }) => Promise<any>;
+};
