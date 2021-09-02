97e294adf5a3ea9ccc64921397625a408d91c24d
batchimeg-a <batchimeg01@gmail.com>
2021-09-02 16:49:26 +0800

    fix dropdownUI

--- ui/src/modules/leads/components/step/OptionStep.tsx
+++ ui/src/modules/leads/components/step/OptionStep.tsx
@@ -8,6 +8,7 @@ import { IFormData } from 'modules/forms/types';
 import { LANGUAGES } from 'modules/settings/general/constants';
 import SelectBrand from 'modules/settings/integrations/containers/SelectBrand';
 import SelectChannels from 'modules/settings/integrations/containers/SelectChannels';
+import Select from 'react-select-plus';
 import { IField } from 'modules/settings/properties/types';
 import { Description } from 'modules/settings/styles';
 import React from 'react';
@@ -15,6 +16,10 @@ import { IBrand } from '../../../settings/brands/types';
 import { BackgroundSelector, FlexItem } from './style';
 
 type Props = {
+  onChange: (
+    name: 'brand' | 'language' | 'isRequireOnce' | 'channelIds' | 'theme',
+    value: any
+  ) => void;
   type: string;
   formData: IFormData;
   color: string;
@@ -22,19 +27,29 @@ type Props = {
   title?: string;
   language?: string;
   isRequireOnce?: boolean;
-  onChange: (
-    name: 'brand' | 'language' | 'isRequireOnce' | 'channelIds' | 'theme',
-
-    value: any
-  ) => void;
   fields?: IField[];
   brand?: IBrand;
   channelIds?: string[];
   onFieldEdit?: () => void;
 };
 
-class OptionStep extends React.Component<Props, {}> {
+type State = {
+  language?: string;
+};
+
+class OptionStep extends React.Component<Props, State> {
   onChangeFunction = (name: any, value: any) => {
+    this.props.onChange(name, value);
+  };
+
+  onSelectChange = (e, name) => {
+    let value = '';
+
+    if (e) {
+      value = e.value;
+    }
+
+    this.setState({ [name]: value });
     this.props.onChange(name, value);
   };
 
@@ -68,11 +83,7 @@ class OptionStep extends React.Component<Props, {}> {
       this.onChangeFunction('channelIds', values);
     };
 
-    const onChangeLanguage = e =>
-      this.onChangeFunction(
-        'language',
-        (e.currentTarget as HTMLInputElement).value
-      );
+    const onChangeLanguage = e => this.onSelectChange(e, 'language');
 
     const onSwitchHandler = e => {
       this.onChangeFunction('isRequireOnce', e.target.checked);
@@ -111,20 +122,13 @@ class OptionStep extends React.Component<Props, {}> {
           />
           <FormGroup>
             <ControlLabel>Language</ControlLabel>
-            <FormControl
-              componentClass="select"
-              defaultValue={language}
-              id="languageCode"
+            <Select
+              id="language"
+              value={language}
+              options={LANGUAGES}
               onChange={onChangeLanguage}
-            >
-              <option />
-
-              {LANGUAGES.map((item, index) => (
-                <option key={index} value={item.value}>
-                  {item.label}
-                </option>
-              ))}
-            </FormControl>
+              clearable={false}
+            />
           </FormGroup>
 
           <FormGroup>
