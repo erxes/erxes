6bad54b54cc2217fb60a3f58080795e3bab250ac
Enkhtuvshin0513 <enhtuwshinn@gmail.com>
2021-09-07 14:06:37 +0800

    migrate

---
+++ api/src/commands/segmentMigration.ts
@@ -0,0 +1,52 @@
+import * as dotenv from 'dotenv';
+import { connect } from '../db/connection';
+import { Segments } from '../db/models';
+
+dotenv.config();
+
+const command = async () => {
+  await connect();
+
+  const segments = await Segments.find({});
+
+  console.log(`segments count = ${segments.length}`);
+
+  for (const segment of segments) {
+    const conditions = segment.conditions;
+
+    if (!conditions || conditions.length === 0) {
+      return;
+    }
+
+    const contentType = segment.contentType as
+      | 'customer'
+      | 'company'
+      | 'deal'
+      | 'task'
+      | 'task'
+      | 'ticket'
+      | 'form_submission'
+      | 'conversation';
+
+    for (const condition of conditions) {
+      condition.propertyType = contentType;
+    }
+
+    const subSegment = await Segments.create({ conditions });
+
+    const newConditions = [
+      { type: 'subSegment', subSegmentId: subSegment._id }
+    ];
+
+    await Segments.updateOne(
+      { _id: segment._id },
+      { $set: { conditions: newConditions } }
+    );
+  }
+
+  console.log(`Process finished at: ${new Date()}`);
+
+  process.exit();
+};
+
+command();
