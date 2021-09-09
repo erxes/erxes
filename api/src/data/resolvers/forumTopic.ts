6b232376edd3f804e3c2f9207f1221ace40f3d7f
Bat-Amgalan Nasan-Ochir <batamgalan.n@nmma.co>
2021-09-09 14:47:52 +0800

    add pagination in topic resolver

--- api/src/data/resolvers/forumTopic.ts
+++ api/src/data/resolvers/forumTopic.ts
@@ -1,10 +1,14 @@
 import { ForumDiscussions, Forums } from '../../db/models';
 
 import { ITopicDocument } from '../../db/models/definitions/forums';
+import { paginate } from '../utils';
 
 export default {
-  discussions(topic: ITopicDocument) {
-    return ForumDiscussions.find({ topicId: topic._id });
+  discussions(topic: ITopicDocument, _params, _context, { variableValues }) {
+    return paginate(
+      ForumDiscussions.find({ topicId: topic._id }),
+      variableValues
+    ).sort({ modifiedDate: -1 });
   },
   forum(topic: ITopicDocument) {
     return Forums.findOne({ _id: topic.forumId });
