505d34cc88eba021980524f070533a2fd354b1c4
BatAmar Battulga <batamar.b@nmtec.co>
2021-09-09 18:10:03 +0800

    upgraded erxes-ui version

--- ui/src/modules/layout/components/Navigation.tsx
+++ ui/src/modules/layout/components/Navigation.tsx
@@ -44,29 +44,32 @@ class Navigation extends React.Component<IProps> {
     }
   }
 
-   getLink = (url) => {
+  getLink = url => {
     const storageValue = window.localStorage.getItem('pagination:perPage');
+
     let parsedStorageValue;
+
     try {
-      parsedStorageValue = JSON.parse(storageValue || '')
+      parsedStorageValue = JSON.parse(storageValue || '');
     } catch {
-      parsedStorageValue = {}
+      parsedStorageValue = {};
     }
 
     if (url.includes('?')) {
-      const pathname = url.split('?')[0]
+      const pathname = url.split('?')[0];
 
-      if(!url.includes('perPage') && parsedStorageValue[pathname]){
-          return `${url}&perPage=${parsedStorageValue[pathname]}`
-      } 
-      return url
-    }  
+      if (!url.includes('perPage') && parsedStorageValue[pathname]) {
+        return `${url}&perPage=${parsedStorageValue[pathname]}`;
+      }
+      return url;
+    }
 
     if (parsedStorageValue[url]) {
-      return `${url}?perPage=${parsedStorageValue[url]}`
+      return `${url}?perPage=${parsedStorageValue[url]}`;
     }
-    return url
-   }
+
+    return url;
+  };
 
   renderSubNavItem = (child, index: number) => {
     return (
