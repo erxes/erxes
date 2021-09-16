137822c6ca759ec26024a22575ff9a19cd76da94
soyombo <soyombo.b@nmtec.co>
2021-09-16 23:15:48 +0800

    fix conversation convert mutation

--- api/src/data/resolvers/mutations/conversations.ts
+++ api/src/data/resolvers/mutations/conversations.ts
@@ -734,7 +734,7 @@ const conversationMutations = {
       doc.customerIds = [conversation.customerId];
       doc.assignedUserIds = [conversation.assignedUserId];
 
-      const item = await itemsAdd(doc, type, user, create, docModifier);
+      const item = await itemsAdd(doc, type, create, user, docModifier);
 
       return item._id;
     }
