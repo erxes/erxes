e2c2900c278e45f053cc4750ded43e6d741a426f
Munkhjin <munkhjin0223@gmail.com>
2021-09-17 19:59:04 +0800

    fix tests

--- api/src/__tests__/departmentQueries.test.ts
+++ api/src/__tests__/departmentQueries.test.ts
@@ -4,27 +4,30 @@ import { departmentFactory } from '../db/factories';
 import './setup.ts';
 
 describe('Department queries', () => {
-    test('Get departments', async () => {
-        await departmentFactory({});
-        await departmentFactory({});
+  test('Get departments', async () => {
+    await departmentFactory({});
+    await departmentFactory({});
 
-        const query = `
+    const query = `
             query departments {
                 departments {
                     _id
+                    users {
+                        _id
+                    }
                 }
             }
         `;
 
-        const response = await graphqlRequest(query, 'departments');
-      
-        expect(response.length).toBe(2);
-    });
+    const response = await graphqlRequest(query, 'departments');
 
-    test('Get department', async () => {
-        const department = await departmentFactory({});
+    expect(response.length).toBe(2);
+  });
 
-        const query = `
+  test('Get department', async () => {
+    const department = await departmentFactory({});
+
+    const query = `
             query departmentDetail($_id: String!) {
                 departmentDetail(_id: $_id) {
                     _id
@@ -33,8 +36,10 @@ describe('Department queries', () => {
             }
         `;
 
-        const response = await graphqlRequest(query, 'departmentDetail', { _id: department._id });
-      
-        expect(response).toBeDefined();
+    const response = await graphqlRequest(query, 'departmentDetail', {
+      _id: department._id
     });
-})
+
+    expect(response).toBeDefined();
+  });
+});
