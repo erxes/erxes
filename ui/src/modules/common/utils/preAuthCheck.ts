3a93b94f8ae81ba2ec9dfa0f0b9efaa88af1de37
MyagmarsurenM <myagmarsuren.m@nmma.co>
2021-09-21 10:36:36 +0800

    clear console

--- ui/src/modules/common/utils/preAuthCheck.ts
+++ ui/src/modules/common/utils/preAuthCheck.ts
@@ -2,7 +2,6 @@ export const preAuthCheck = async (preAuth: any) => {
   let data = {};
   await preAuth.then(result => {
     data = Object.assign({}, result);
-    console.log(data);
   });
   return data;
 };
