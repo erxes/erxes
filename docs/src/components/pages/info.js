3bce51c95c77fd6ce242e210b9a673c923af184b
ariunzayarin <ariunzaya.ei@gmail.com>
2021-09-27 17:15:17 +0800

    Info component added

--- docs/src/components/pages/info.js
+++ docs/src/components/pages/info.js
@@ -1,12 +1,39 @@
 import React from "react";
-// import Button from "../../../node_modules/erxes-ui/lib/components/Button";
+import Info from "erxes-ui/lib/components/Info";
+import styles from "../styles.module.css";
 
-function Alerts() {
+export default function Infos() {
   return (
     <div className="container">
-        Alert
+      <div className="title">
+        <h1>Info</h1>
+        <p>Custom button styles for actions in forms, dialogs, and more with support for multiple sizes, states, and more.</p>
+      </div>
+      <div className="examples">
+        <h1>Examples</h1>
+        <p>Use any of the available button style types to quickly create a styled button. Just modify the <b><u>btnStyle</u></b> prop.</p>
+        <div className={styles.exampleTab}>
+          <div className={styles.styledButtons}>
+            <Info>Default</Info>{' '}
+            
+          </div>
+          <div className={styles.codes}>
+            {/* <textarea> */}
+              &lt;&gt;
+              &lt;Button btnStyle="primary"&gt;Primary&lt;/Button&gt;{' '}
+              &lt;Button btnStyle="success"&gt;Success&lt;/Button&gt;{' '}
+              &lt;Button btnStyle="danger"&gt;Danger&lt;/Button&gt;{' '}
+              &lt;Button btnStyle="warning"&gt;Warning&lt;/Button&gt;{' '}
+              &lt;Button btnStyle="simple"&gt;Simple&lt;/Button&gt;{' '}
+              &lt;Button btnStyle="link"&gt;Link&lt;/Button&gt;{' '}
+              &lt;/&gt;
+            
+            {/* </textarea> */}
+          </div>
+        </div>
+        {/* <Button btnStyle="primary" size="large">Large button</Button> */}
+      </div>
+      
     </div>
   );
 }
-
-export default Alerts;
