e0fc427c1606506b4b693a60f272df1542b4fae3
Dulguun Otgon <dulguun0225@gmail.com>
2021-09-28 12:17:43 +0800

    optimized product resolvers with batching and caching

---
+++ api/src/data/dataLoaders/tag.ts
@@ -0,0 +1,16 @@
+import * as DataLoader from 'dataloader';
+import * as _ from 'underscore';
+import { Tags } from '../../db/models';
+import { ITagDocument } from '../../db/models/definitions/tags';
+
+export default function generateDataLoaderTag() {
+  return new DataLoader<string, ITagDocument>(
+    async (ids: readonly string[]) => {
+      const result: ITagDocument[] = await Tags.find({ _id: { $in: ids } });
+      const resultById = _.indexBy(result, '_id');
+      return ids.map(
+        id => resultById[id] || new Error(`Cannot find Tag with id = ${id}`)
+      );
+    }
+  );
+}
