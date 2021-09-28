bd55247702258cbe8a7f77970174764d5e7ce8c4
ariunzayarin <ariunzaya.ei@gmail.com>
2021-09-28 10:24:58 +0800

    info added

--- docs/src/components/pages/info.js
+++ docs/src/components/pages/info.js
@@ -11,29 +11,18 @@ export default function Infos() {
       </div>
       <div className="examples">
         <h1>Examples</h1>
-        <p>Use any of the available infos style types to quickly create a styled button. Just modify the <b><u>btnStyle</u></b> prop.</p>
+        <p>Use any of the available infos style types to quickly create a styled infos. Just modify the <b><u>btnStyle</u></b> prop.</p>
         <div className={styles.tab}>
-          <div className={styles.styledButtons}>
+          <div className={styles.styled}>
             <Info>Default</Info>{' '}
             
           </div>
-          <div className={styles.codes}>
-            {/* <textarea> */}
-              &lt;&gt;
-              &lt;Button btnStyle="primary"&gt;Primary&lt;/Button&gt;{' '}
-              &lt;Button btnStyle="success"&gt;Success&lt;/Button&gt;{' '}
-              &lt;Button btnStyle="danger"&gt;Danger&lt;/Button&gt;{' '}
-              &lt;Button btnStyle="warning"&gt;Warning&lt;/Button&gt;{' '}
-              &lt;Button btnStyle="simple"&gt;Simple&lt;/Button&gt;{' '}
-              &lt;Button btnStyle="link"&gt;Link&lt;/Button&gt;{' '}
-              &lt;/&gt;
-            
-            {/* </textarea> */}
-          </div>
+          
         </div>
         {/* <Button btnStyle="primary" size="large">Large button</Button> */}
       </div>
       
     </div>
+    
   );
 }
