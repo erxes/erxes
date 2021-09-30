d13c10897894f380d3c48e53f73fc9bdce94f4d6
anu-ujin <anu.b@nmma.co>
2021-10-01 01:21:57 +0800

    fix setProperty style

--- ui/src/modules/automations/components/forms/actions/styles.ts
+++ ui/src/modules/automations/components/forms/actions/styles.ts
@@ -84,3 +84,19 @@ export const ActionBox = styledTS<{
       }
     }
   `;
+
+export const PropertyGroupWrapper = styled.div`
+  border: 1px solid ${colors.colorSecondary};
+  position: relative;
+  border-radius: ${dimensions.unitSpacing - 5}px;
+  padding: ${dimensions.unitSpacing}px 15px;
+  margin-bottom: ${dimensions.coreSpacing}px;
+
+  button {
+    position: absolute;
+    padding: 5px 8px;
+    top: -${dimensions.unitSpacing - 5}px;
+    right: -${dimensions.unitSpacing}px;
+    background: ${colors.bgActive} !important;
+  }
+`;
