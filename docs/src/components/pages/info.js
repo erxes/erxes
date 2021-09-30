d549cc0777a620877b0c484a88f3a3804a2188af
ariunzayarin <ariunzaya.ei@gmail.com>
2021-09-30 16:42:16 +0800

    info new

--- docs/src/components/pages/info.js
+++ docs/src/components/pages/info.js
@@ -1,9 +1,63 @@
 import React from "react";
+import Info from "erxes-ui/lib/components/Info";
+import Icon from "erxes-ui/lib/components/Icon";
 import styles from "../styles.module.css";
-import { InfoStyle } from "../infoStyle"
+import { CopyBlock, dracula } from "react-code-blocks";
+// import Icon from '../../../ui/src/modules/common/components/Icon';
 
 export default function Infos() {
-  return(
-    <InfoStyle title="Info" titledef="this is info example" definition="you can copy and us this infos"/>
-  )
+  return (
+    <>
+      <div>
+        <h1>Info</h1>
+        <p>
+          Provide contextual messages for typical user actions with info
+          messages.
+        </p>
+      </div>
+      <div>
+        <h1>Examples</h1>
+        <p>Infos are available for any length of text. For proper styling, use one of the five variants.</p>
+        <div className={styles.code}>
+          <div>
+            <Info
+              iconShow="https://erxes.s3.amazonaws.com/icons/grinning.svg"
+              title="Default"
+            >
+              This is default info
+            </Info>
+            <Info type="info" title="Info" iconShow="info-circle">
+              This is info
+            </Info>{" "}
+            <Info type="danger" title="Danger">
+              This is danger info
+            </Info>{" "}
+            <Info type="warning" title="Warning">
+              This is warning info
+            </Info>{" "}
+            <Info type="success" title="Success">
+              This is success info
+            </Info>{" "}
+          </div>
+          <br />
+          <CopyBlock
+            language="html"
+            text={`<>
+          <Info iconShow="https://erxes.s3.amazonaws.com/icons/grinning.svg" title="Default">
+            This is default info
+          </Info>
+          <Info type="info" title="Info" iconShow="info-circle"> This is info </Info>{" "}
+          <Info type="danger" title="Danger"> This is danger info </Info>{" "}
+          <Info type="warning" title="Warning"> This is warning info </Info>{" "}
+          <Info type="success" title="Success"> This is success info </Info>{" "}
+          </>`}
+            theme={dracula}
+            showLineNumbers={false}
+            codeBlock
+          />
+        </div>
+      </div>
+     
+    </>
+  );
 }
