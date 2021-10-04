6e705ca9e08d67bdebb84a0bc2abb69c392d1e22
Munkhsaikhan Ganchimeg <munkhsaikhan.g@nmma.co>
2021-10-04 10:00:04 +0800

    clear log

--- api/src/middlewares/webhookMiddleware.ts
+++ api/src/middlewares/webhookMiddleware.ts
@@ -82,8 +82,6 @@ const webhookMiddleware = async (req, res, next) => {
 
     const params = req.body;
 
-    console.log(params);
-
     if (webhookData.script) {
       const vm = new NodeVM({
         sandbox: { params }
