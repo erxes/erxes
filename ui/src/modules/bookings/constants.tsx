ac0bc9da5e41c29ca4e0869d19ac0e6f070239bd
mambatukaa <sbatuka9@gmail.com>
2021-10-07 13:18:46 +0800

    refactor

--- ui/src/modules/bookings/constants.tsx
+++ ui/src/modules/bookings/constants.tsx
@@ -1,14 +1,3 @@
-export const BOOKING_CONTENT_ACTION = {
-  ONEPAGE: 'onePage',
-  TWOPAGE: 'twoPage',
-  THREEPAGE: 'threePage',
-  ALL_LIST: [
-    { label: '1 page', value: 'onePage' },
-    { label: '2 page', value: 'twoPage' },
-    { label: '3 page', value: 'threePage' }
-  ]
-};
-
 export const BOOKING_ITEM_SHAPE = {
   TRIANGLE: 'triangle',
   CIRCLE: 'circle',
@@ -17,23 +6,6 @@ export const BOOKING_ITEM_SHAPE = {
     { label: 'Triangle', value: 'triangle' },
     { label: 'Circle', value: 'circle' },
     { label: 'Square', value: 'square' }
-  ]
-};
-
-export const USER_FILTERS = {
-  ALL_LIST: [
-    { label: 'Sqm', value: 'sqm' },
-    { label: 'Bedrooms', value: 'bedrooms' },
-    { label: 'Bathrooms', value: 'bathrooms' }
-  ]
-};
-
-export const PRODUCT_PROPERTIES = {
-  ALL_LIST: [
-    { label: 'Title', value: 'title' },
-    { label: 'Description', value: 'description' },
-    { label: 'Sqm', value: 'sqm' },
-    { label: 'Bedrooms', value: 'bedrooms' }
   ]
 };
 
