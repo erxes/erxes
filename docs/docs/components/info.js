324b3a828cf31302c7788f426d4506acdaf4fc15
ariunzayarin <ariunzaya.ei@gmail.com>
2021-10-07 12:31:53 +0800

    info.md added icon

--- docs/docs/components/info.js
+++ docs/docs/components/info.js
@@ -2,17 +2,40 @@ import React from "react";
 import Info from "erxes-ui/lib/components/Info";
 import styles from "../../src/components/styles.module.css";
 import CodeBlock from "@theme/CodeBlock";
+import "erxes-icon/css/erxes.min.css";
 
+function InfoIcon() {
+  return (<>
+    <div>
+      <Info
+        iconShow="envelope-alt"
+        type="primary"
+        title="Primary"
+      >This is primary info</Info>
+      <Info type="info" title="Info" iconShow="info-circle">This is info</Info>
+      <Info type="danger" title="Danger" iconShow="times-circle">This is danger info</Info>
+      <Info type="warning" title="Warning" iconShow="exclamation-triangle">This is warning info</Info>
+      <Info type="success" title="Success" iconShow="check-circle">This is success info</Info>
+    </div>
+    <CodeBlock className="language-jsx">{`<>
+      <Info type="info" title="Info" iconShow="info-circle"> This is info </Info>
+      <Info type="danger" title="Danger" iconShow="times-circle"> This is danger info </Info>
+      <Info iconShow="envelope-alt" type="primary" title="Primary"> This is primary info </Info>
+      <Info type="success" title="Success" iconShow="check-circle"> This is success info </Info>
+      <Info type="warning" title="Warning" iconShow="exclamation-triangle"> This is warning info </Info>
+</>`}</CodeBlock>
+    </>
+  );
+}
 function Infos() {
   return (<>
     <div>
       <Info title="Default">This is default info</Info>
       <Info
-        // iconShow="https://erxes.s3.amazonaws.com/icons/grinning.svg"
         type="primary"
         title="Primary"
       >This is primary info</Info>
-      <Info type="info" title="Info" iconShow="info-circle">This is info</Info>
+      <Info type="info" title="Info">This is info</Info>
       <Info type="danger" title="Danger">This is danger info</Info>
       <Info type="warning" title="Warning">This is warning info</Info>
       <Info type="success" title="Success">This is success info</Info>
@@ -29,4 +52,4 @@ function Infos() {
   );
 }
 
-export { Infos }
+export { InfoIcon, Infos }
--- docs/docs/components/info.md
+++ docs/docs/components/info.md
@@ -3,7 +3,7 @@ id: info
 title: Info
 ---
 
-import { Infos } from './info.js'
+import { Infos, InfoIcon } from './info.js'
 
 <p>Provide contextual messages for typical user actions with info messages.</p>
 
@@ -12,3 +12,8 @@ import { Infos } from './info.js'
 <p>Infos are available for any length of text. Just choose one of the five variants and modify the{" "} <code>type</code> prop.</p>
 
 <Infos></Infos>
+
+## Icon
+
+<p>You can add icons using <code>iconShow</code> prop.</p>
+<InfoIcon></InfoIcon>
