43e1494e855f2453b2ee543cfac6984ff4a8b4c0
nandirb <nandir.be@gmail.com>
2021-10-07 16:26:48 +0800

    some updates

--- widgets/client/booking/components/Product.tsx
+++ widgets/client/booking/components/Product.tsx
@@ -1,6 +1,9 @@
 import * as React from 'react';
 import { IProduct } from '../types';
 import Slider from "react-slick";
+import { readFile } from '../../utils';
+import "~slick-carousel/slick/slick.css"; 
+import "~slick-carousel/slick/slick-theme.css";
 
 type Props = {
   product?: IProduct;
@@ -10,41 +13,32 @@ function Product({ product }: Props) {
   if (!product) {
     return null;
   }
-  // const baseUrl="sdsdfsdfg"
-  // console.log(product)
-  // const settings = {
-  //   customPaging: function(i:any) {
-  //     return (
-  //       <a>
-  //         <img src={`${baseUrl}/abstract0${i + 1}.jpg`} />
-  //       </a>
-  //     );
-  //   },
-  //   dots: true,
-  //   dotsClass: "slick-dots slick-thumb",
-  //   infinite: true,
-  //   speed: 500,
-  //   slidesToShow: 1,
-  //   slidesToScroll: 1
-  // };
+  
+  const settings = {
+    customPaging: function(i:any) {
+      return (
+        <a>
+          <img src={readFile(product.attachment && product.attachment.url)} alt={product.attachment.name} />
+        </a>
+      );
+    },
+    dots: true,
+    dotsClass: "slick-dots slick-thumb",
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  };
 
   return (
     <div>
       <h1>{product.name}</h1>
-      {/* <Slider {...settings}>
-          <div>
-            <img src={baseUrl + "/abstract01.jpg"} />
-          </div>
-          <div>
-            <img src={baseUrl + "/abstract02.jpg"} />
-          </div>
-          <div>
-            <img src={baseUrl + "/abstract03.jpg"} />
-          </div>
-          <div>
-            <img src={baseUrl + "/abstract04.jpg"} />
-          </div>
-        </Slider> */}
+      <Slider {...settings}>
+          {product.attachmentMore?.map( (img:any) => <div>
+                 <img src={readFile(img && img.url)} alt={img.name} />
+                </div>)
+          }
+        </Slider>
      </div>
   );
 }
