6daa75549cc93c251ef1c36f1c09f56ed3d8b29b
soyombo <soyombo.b@nmtec.co>
2021-10-08 10:59:52 +0800

    up

---
+++ plugins/erxes-plugin-pos/api/messageBroker.ts
@@ -0,0 +1,39 @@
+// import { paginate } from 'erxes-api-utils'
+
+const sendError = (message) => ({
+  status: "error",
+  errorMessage: message,
+});
+
+const sendSuccess = (data) => ({
+  status: "success",
+  data,
+});
+
+export default [
+  {
+    method: "RPCQueue",
+    channel: "rpc_queue:erxes-pos",
+    handler: async (msg, { models }) => {
+      const { action, data } = msg;
+      let filter = {};
+      let sort = {};
+
+      switch (action) {
+        case 'getUsers':
+          try {
+            filter = {};
+            sort = { username: 1 };
+
+            const users = models.Users.find(filter).sort(sort);
+
+            return sendSuccess({
+              users
+            });
+          } catch (e) {
+            return sendError(e.message);
+          }
+      }
+    }
+  }
+]
