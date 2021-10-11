97b05f497401538cd7a82e5444352919ba88233e
ariunzayarin <ariunzaya.ei@gmail.com>
2021-10-11 13:56:25 +0800

    created table

---
+++ docs/docs/components/table.js
@@ -0,0 +1,171 @@
+import React from "react";
+import Table from "erxes-ui/lib/components/table/index";
+import CodeBlock from "@theme/CodeBlock";
+import withTableWrapper from "erxes-ui/lib/components/table/withTableWrapper";
+
+function Tables() {
+  return (
+    <>
+      <Table>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Username</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td colSpan="2">Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </Table>
+      <CodeBlock className="language-jsx">{`<>
+      <Table>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Username</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td colSpan="2">Larry the Bird</td>
+          <td>@twitter</td>
+        </tr>
+      </tbody>
+    </Table>
+</>`}</CodeBlock>
+    </>
+  );
+}
+function TableHover() {
+  return (
+    <div>
+      <Table hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Username</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td colSpan="2">Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </Table>
+      <CodeBlock className="language-jsx">{`<>
+      <Table hover>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Username</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td colSpan="2">Larry the Bird</td>
+          <td>@twitter</td>
+        </tr>
+      </tbody>
+    </Table>
+</>`}</CodeBlock>
+    </div>
+  );
+}
+// function TableWrapper(){
+//   return(
+//  <withTableWrapper >
+//       <thead>
+//     <tr>
+//       <th>#</th>
+//       <th>First Name</th>
+//       <th>Last Name</th>
+//       <th>Username</th>
+//     </tr>
+//   </thead>
+//   <tbody>
+//     <tr>
+//       <td>1</td>
+//       <td>Mark</td>
+//       <td>Otto</td>
+//       <td>@mdo</td>
+//     </tr>
+//     <tr>
+//       <td>2</td>
+//       <td>Jacob</td>
+//       <td>Thornton</td>
+//       <td>@fat</td>
+//     </tr>
+//     <tr>
+//       <td>3</td>
+//       <td colSpan="2">Larry the Bird</td>
+//       <td>@twitter</td>
+//     </tr>
+//   </tbody>
+//       </withTableWrapper>
+//   )
+// }
+export { Tables, TableHover };
---
+++ docs/docs/components/table.md
@@ -0,0 +1,15 @@
+---
+id: table
+title: Table
+---
+
+import { Tables, TableHover } from './table.js'
+
+## Merge cells
+<p>Merge cells by <code>colSpan</code> prop.</p>
+<Tables></Tables>
+
+## Table hover
+
+<p>Activate hover of table row by <code>hover</code> prop.</p>
+<TableHover></TableHover>
