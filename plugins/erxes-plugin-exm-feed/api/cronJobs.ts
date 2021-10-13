16ff47d423913762595011b215e3ed4bdbdf3359
Munkhjin <munkhjin0223@gmail.com>
2021-10-13 20:50:31 +0800

    fix time

--- plugins/erxes-plugin-exm-feed/api/cronJobs.ts
+++ plugins/erxes-plugin-exm-feed/api/cronJobs.ts
@@ -93,7 +93,7 @@ export const createCeremonies = async models => {
 // 20:00
 export default [
   {
-    schedule: '50 12 * * *',
+    schedule: '00 13 * * *',
     handler: async ({ models }) => {
       await createCeremonies(models);
     }
