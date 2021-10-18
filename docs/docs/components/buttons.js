db6da02ce58874ce1911d5e935271d551b5c1f36
anu-ujin <anu.b@nmma.co>
2021-10-18 19:31:07 +0800

    fix

--- docs/docs/components/buttons.js
+++ docs/docs/components/buttons.js
@@ -8,51 +8,65 @@ import "erxes-icon/css/erxes.min.css";
 export function ButtonComponent(props) {
   const { type, buttons = [], icons = [], table = [] } = props;
 
-  const renderBlock = ( propName, defaultBtn, icon) => {
-      return (
-        <>
-          <div className={styles.styled}>
-            {defaultBtn && <Button>{defaultBtn}</Button>}
-            {buttons.map((btn, index) => {
-              const props = {
-                [propName]:
-                  propName === "type" || propName === "size"
-                    ? btn.toLowerCase()
-                    : true,
-                icon: icon && icons[index],
-              };
+  const renderBlock = (propName, defaultBtn, icon) => {
+    return (
+      <>
+        <div className={styles.styled}>
+          {defaultBtn && <Button>{defaultBtn}</Button>}
+          {buttons.map((btn, index) => {
+            const props = {
+              [propName]:
+                propName === "type" || propName === "size"
+                  ? btn.toLowerCase()
+                  : true,
+              icon: icon && icons[index],
+            };
 
-              return (
-                <Button key={index} {...props}>
-                  {btn}
-                </Button>
-              );
-            })}
-          </div>
-          <CodeBlock className="language-jsx">
-            {`<>\n\t<Button>${
-              defaultBtn ? defaultBtn : "Default"
-            }</Button>${buttons.map(
-              (btn) => `\n\t<Button ${{ ...props }}>${btn}</Button>`
-            )}\n</>`}
-          </CodeBlock>
-        </>
-      );
+            return (
+              <Button key={index} {...props}>
+                {btn}
+              </Button>
+            );
+          })}
+        </div>
+        <CodeBlock className="language-jsx">
+          {`<>\n\t<Button>${
+            defaultBtn ? defaultBtn : "Default"
+          }</Button>${buttons.map(
+            (btn) => `\n\t<Button ${{ ...props }}>${btn}</Button>`
+          )}\n</>`}
+        </CodeBlock>
+      </>
+    );
   };
 
   if (type === "type") {
-    return renderBlock( "btnStyle", "Default");
+    return renderBlock("btnStyle", "Default");
   }
 
   if (type === "size") {
-    return renderBlock( "size");
+    return renderBlock("size");
   }
 
+  if (type === "activity") {
+    return renderBlock("disabled", "Normal");
+  }
+
+  if (type === "uppercase") {
+    return renderBlock("uppercase", "Normal");
+  }
+
+  if (type === "block") {
+    return renderBlock("block", "Block");
+  }
+
+  if (type === "icon") {
+    return renderBlock("btnStyle", "Default", "icon");
+  }
 
   if (type === "APIbutton") {
     return renderApiTable("Button", table);
   }
-
 
   return null;
 }
