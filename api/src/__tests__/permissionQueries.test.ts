1bc440c77d8d3e4e7aa13b46833e8788742a3360
mambatukaa <sbatuka9@gmail.com>
2021-10-18 12:00:40 +0800

    permission test update

--- api/src/__tests__/permissionQueries.test.ts
+++ api/src/__tests__/permissionQueries.test.ts
@@ -201,7 +201,7 @@ describe('permissionQueries', () => {
 
     const modules = await graphqlRequest(qry, 'permissionActions');
 
-    expect(modules.length).toBe(214);
+    expect(modules.length).toBe(216);
   });
 });
 
