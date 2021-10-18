0395b72f84028d29d86b1a1bd184fba0194982d2
enkhzule <enkhzul.e@nmtec.co>
2021-10-18 19:29:38 +0800

    h

--- docs/docs/components/buttons.js
+++ docs/docs/components/buttons.js
@@ -8,8 +8,7 @@ import "erxes-icon/css/erxes.min.css";
 export function ButtonComponent(props) {
   const { type, buttons = [], icons = [], table = [] } = props;
 
-  const renderBlock = (kind, propName, defaultBtn, icon) => {
-    if (type === kind) {
+  const renderBlock = ( propName, defaultBtn, icon) => {
       return (
         <>
           <div className={styles.styled}>
@@ -18,7 +17,7 @@ export function ButtonComponent(props) {
               const props = {
                 [propName]:
                   propName === "type" || propName === "size"
-                    ? e.toLowerCase()
+                    ? btn.toLowerCase()
                     : true,
                 icon: icon && icons[index],
               };
@@ -39,153 +38,21 @@ export function ButtonComponent(props) {
           </CodeBlock>
         </>
       );
-    }
   };
 
-  if (type === "btnStyle") {
-    return renderBlock("type", "btnStyle", "Default");
+  if (type === "type") {
+    return renderBlock( "btnStyle", "Default");
   }
 
   if (type === "size") {
-    return renderBlock("size", "size");
+    return renderBlock( "size");
   }
 
-  renderBlock("activity", "disabled", "Normal");
-  renderBlock("uppercase", "uppercase", "Normal");
-  renderBlock("block", "block", "Block");
-  renderBlock("icon", "btnStyle", "Default", "icon");
 
   if (type === "APIbutton") {
     return renderApiTable("Button", table);
   }
 
-  // if (type === "type") {
-  //   return (
-  //     <>
-  //       <div className={styles.styled}>
-  //         <Button>Default</Button>
-  //         {buttons.map((e) => (
-  //           <Button key={Math.random()} btnStyle={e.toLowerCase()}>
-  //             {e}
-  //           </Button>
-  //         ))}
-  //       </div>
-  //       <CodeBlock className="language-jsx">
-  //         {`<>`}
-  //         {`\n\t<Button>Default</Button>`}
-  //         {`${buttons.map(
-  //           (e) => `\n\t<Button btnStyle="${e.toLowerCase()}">${e}</Button>`
-  //         )}`}
-  //         {`\n</>`}
-  //       </CodeBlock>
-  //     </>
-  //   );
-  // }
-
-  // if (type === "size") {
-  //   return (
-  //     <>
-  //       <div className={styles.styled}>
-  //         {buttons.map((e) => (
-  //           <Button key={Math.random()} size={e.toLowerCase()}>
-  //             {e}
-  //           </Button>
-  //         ))}
-  //       </div>
-  //       <CodeBlock className="language-jsx">
-  //         {`<>`}
-  //         {`\n\t<Button>Default</Button>`}
-  //         {`${buttons.map(
-  //           (e) => `\n\t<Button size="${e.toLowerCase()}">${e}</Button>`
-  //         )}`}
-  //         {`\n</>`}
-  //       </CodeBlock>
-  //     </>
-  //   );
-  // }
-
-  // if (type === "activity") {
-  //   return (
-  //     <>
-  //       <div className={styles.styled}>
-  //         <Button>Normal</Button>
-  //         <Button key={Math.random()} disabled>
-  //           Disabled
-  //         </Button>
-  //       </div>
-  //       <CodeBlock className="language-jsx">
-  //         {`<>`}
-  //         {`\n\t<Button>Normal</Button>`}
-  //         {`\n\t<Button disabled>Disabled</Button>`}
-  //         {`\n</>`}
-  //       </CodeBlock>
-  //     </>
-  //   );
-  // }
-
-  // if (type === "uppercase") {
-  //   return (
-  //     <>
-  //       <div className={styles.styled}>
-  //         <Button>Normal</Button>
-  //         <Button key={Math.random()} uppercase>
-  //           Uppercase
-  //         </Button>
-  //       </div>
-  //       <CodeBlock className="language-jsx">
-  //         {`<>`}
-  //         {`\n\t<Button>Normal</Button>`}
-  //         {`\n\t<Button uppercase>Uppercase</Button>`}
-  //         {`\n</>`}
-  //       </CodeBlock>
-  //     </>
-  //   );
-  // }
-
-  // if (type === "block") {
-  //   return (
-  //     <>
-  //       <div className={styles.styled}>
-  //         <Button key={Math.random()} block>
-  //           Block
-  //         </Button>
-  //       </div>
-  //       <CodeBlock className="language-jsx">
-  //         {`<>`}
-  //         {`\n\t<Button block>Block</Button>`}
-  //         {`\n</>`}
-  //       </CodeBlock>
-  //     </>
-  //   );
-  // }
-
-  // if (type === "icon") {
-  //   return (
-  //     <>
-  //       <div className={styles.styled}>
-  //         {buttons.map((e, index) => (
-  //           <Button
-  //             key={Math.random()}
-  //             btnStyle={e.toLowerCase()}
-  //             icon={icons[index]}
-  //           >
-  //             {e}
-  //           </Button>
-  //         ))}
-  //       </div>
-  //       <CodeBlock className="language-jsx">
-  //         {`<>`}
-  //         {`${buttons.map(
-  //           (e, index) =>
-  //             `\n\t<Button btnStyle="${e.toLowerCase()}" icon="${
-  //               icons[index]
-  //             }">${e}</Button>`
-  //         )}`}
-  //         {`\n</>`}
-  //       </CodeBlock>
-  //     </>
-  //   );
-  // }
 
   return null;
 }
