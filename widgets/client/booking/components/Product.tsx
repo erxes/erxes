8848557cd582b683a2c0c618a97bdba6dca6f4dc
anu-ujin <anu.b@nmma.co>
2021-10-19 16:21:23 +0800

    add slick slider

--- widgets/client/booking/components/Product.tsx
+++ widgets/client/booking/components/Product.tsx
@@ -1,13 +1,10 @@
-import * as React from 'react';
-import { HiArrowSmLeft, HiArrowSmRight } from "react-icons/hi"
-import { IBookingData } from '../types';
-import Slider from 'react-slick';
-import { readFile } from '../../utils';
-import { IProduct } from '../../types';
-import Button from './common/Button';
-
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import * as React from "react";
+import { HiArrowSmLeft, HiArrowSmRight } from "react-icons/hi";
+import { IBookingData } from "../types";
+import Slider from "react-slick";
+import { readFile } from "../../utils";
+import { IProduct } from "../../types";
+import Button from "./common/Button";
 
 type Props = {
   product?: IProduct;
@@ -22,7 +19,7 @@ function Product({
   booking,
   goToBookings,
   showForm,
-  showPopup
+  showPopup,
 }: Props) {
   if (!product || !booking) {
     return null;
@@ -31,7 +28,7 @@ function Product({
   const { widgetColor } = booking.style;
 
   const showFull = (img: any) => {
-    const image = document.getElementById('img-active') as HTMLImageElement;
+    const image = document.getElementById("img-active") as HTMLImageElement;
     if (image) {
       image.src = readFile(img && img.url);
     }
@@ -40,11 +37,11 @@ function Product({
   const settings = {
     dots: true,
     infinite: true,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    slidesToShow: 3,
-    slidesToScroll: 1
+    speed: 500,
+    slidesToShow: 2,
+    slidesToScroll: 2,
   };
+  console.log(product);
 
   return (
     <div className="product">
@@ -61,33 +58,36 @@ function Product({
           </div>
           <div>
             <Slider {...settings}>
-              {product.attachmentMore?.map((img: any) => (
-                // tslint:disable-next-line: jsx-key
-                <div onClick={() => showFull(img)}>
-                  {' '}
+              {(product.attachmentMore || []).map((img, index) => (
+                <div key={index} onClick={() => showFull(img)}>
                   <img
                     id={img.name}
                     src={readFile(img && img.url)}
                     alt={img.name}
-                  />{' '}
+                    style={{ width: "200px", height: "150px" }}
+                  />
                 </div>
               ))}
             </Slider>
           </div>
         </div>
         <div>
-          <div><strong>Price per unit:</strong> {product.unitPrice}</div>
-          <div><strong>Sqm:</strong> {product.sku}</div>
-          <div><strong>Floor:</strong> {product.category.name}</div>
+          <div>
+            <strong>Price per unit:</strong> {product.unitPrice}
+          </div>
+          <div>
+            <strong>Sqm:</strong> {product.sku}
+          </div>
+          <div>
+            <strong>Floor:</strong> {product.category.name}
+          </div>
           <Button
-            text={'Book product'}
+            text={"Book product"}
             type=""
             onClickHandler={() => showPopup()}
             style={{ backgroundColor: widgetColor, marginTop: "100px" }}
           />
         </div>
-
-
       </div>
 
       <div className="footer">
@@ -97,12 +97,9 @@ function Product({
           onClickHandler={goToBookings}
           style={{ backgroundColor: widgetColor, left: 0 }}
         />
-
       </div>
     </div>
   );
 }
-
-
 
 export default Product;
