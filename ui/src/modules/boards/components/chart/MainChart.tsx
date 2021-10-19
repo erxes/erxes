7c122961105dbf9c8435b0f780ffe36e0be21a7a
batchimeg-a <batchimeg01@gmail.com>
2021-10-19 16:03:12 +0800

    update chart

--- ui/src/modules/boards/components/chart/MainChart.tsx
+++ ui/src/modules/boards/components/chart/MainChart.tsx
@@ -11,6 +11,25 @@ import {
 export default function MainChart({ component, data, children }) {
   const ChartComponent = component;
 
+  const CustomizedAxisTick = props => {
+    const { x, y, payload } = props;
+
+    return (
+      <g transform={`translate(${x},${y})`}>
+        <text
+          x={0}
+          y={0}
+          dy={16}
+          textAnchor="end"
+          fill="#666"
+          transform="rotate(-35)"
+        >
+          {payload.value}
+        </text>
+      </g>
+    );
+  };
+
   return (
     <ResponsiveContainer width="100%" height="100%" backgroundColor="#ffffff">
       <ChartComponent
@@ -18,10 +37,30 @@ export default function MainChart({ component, data, children }) {
         margin={{ top: 100, left: 130, bottom: 150, right: 200 }}
       >
         <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" padding={{ top: 10 }} />
-        <YAxis padding={{ right: 10 }} />
+        <XAxis
+          dataKey="name"
+          padding={{ top: 10 }}
+          label={{
+            value: 'Assignee',
+            position: 'insideBottomRight',
+            offset: 0
+          }}
+          tick={<CustomizedAxisTick />}
+          height={60}
+          interval={0}
+          angle={30}
+          dx={20}
+        />
+        <YAxis
+          label={{
+            value: 'Number of tasks by stages',
+            angle: -90,
+            position: 'insideLeft'
+          }}
+          padding={{ right: 10 }}
+        />
         <Tooltip />
-        <Legend height={0} />
+        <Legend verticalAlign="top" height={36} />
         {children}
       </ChartComponent>
     </ResponsiveContainer>
