ca0617da2d476da68bbe90888279ece2856ceafb
munkhsaikhan.g@nmma.co <munkhsaikhan.g@nmma.co>
2021-10-25 15:34:23 +0800

    lint

--- automations/src/actions/utils.ts
+++ automations/src/actions/utils.ts
@@ -1,6 +1,6 @@
-import { sendRPCMessage } from "../messageBroker";
-import { sendRequest } from 'erxes-api-utils';
 import { IExecution } from '../models/Executions';
+import { sendRequest } from 'erxes-api-utils';
+import { sendRPCMessage } from '../messageBroker';
 
 const createObjectWrapper = (execution: IExecution, doc: any, model: string, type: string, channel?: string) => {
   const conformityTypes = ['deal', 'task', 'ticket', 'customer', 'company']
@@ -26,63 +26,63 @@ const createObjectWrapper = (execution: IExecution, doc: any, model: string, typ
 export const getContext = async (execution: IExecution) => {
   return {
     tag: {
-      getObject: (id) => { return sendRPCMessage('get-object', { model: `Tags`, call: 'getTag', id }) },
-      findObjects: (selector) => { return sendRPCMessage('find-objects', { model: `Tags`, selector }) },
+      getObject: (id) => (sendRPCMessage('get-object', { model: `Tags`, call: 'getTag', id })),
+      findObjects: (selector) => (sendRPCMessage('find-objects', { model: `Tags`, selector })),
     },
     contact: {
-      getObject: (id) => { return sendRPCMessage('get-object', { model: `Customers`, call: 'getCustomer', id }) },
-      findObjects: (selector) => { return sendRPCMessage('find-objects', { model: `Customers`, selector }) },
-      associations: (relType, mainTypeId?, model?) => { return sendRPCMessage('find-conformities', { mainType: 'customer', mainTypeId: mainTypeId || execution.target._id, relType, model }) },
-      createObject: (doc) => { return createObjectWrapper(execution, doc, 'Customers', 'customer') },
-      setAssociotions: (mainTypeId, relType, relTypeIds) => { return sendRPCMessage('set-conformities', { mainType: 'customer', mainTypeId, relType, relTypeIds }) },
-      setProperty: (doc, id?) => { return sendRPCMessage('set-property', { ...doc, _id: doc._id || id || execution.target._id, model: 'Customers' }) },
-      removeItem: (ids) => { return sendRPCMessage('remove-objects', { model: 'Customers', call: 'removeCustomers', ids }) },
+      getObject: (id) => (sendRPCMessage('get-object', { model: `Customers`, call: 'getCustomer', id })),
+      findObjects: (selector) => (sendRPCMessage('find-objects', { model: `Customers`, selector })),
+      associations: (relType, mainTypeId?, model?) => (sendRPCMessage('find-conformities', { mainType: 'customer', mainTypeId: mainTypeId || execution.target._id, relType, model })),
+      createObject: (doc) => (createObjectWrapper(execution, doc, 'Customers', 'customer')),
+      setAssociotions: (mainTypeId, relType, relTypeIds) => (sendRPCMessage('set-conformities', { mainType: 'customer', mainTypeId, relType, relTypeIds })),
+      setProperty: (doc, id?) => (sendRPCMessage('set-property', { ...doc, _id: doc._id || id || execution.target._id, model: 'Customers' })),
+      removeItem: (ids) => (sendRPCMessage('remove-objects', { model: 'Customers', call: 'removeCustomers', ids })),
     },
     company: {
-      getObject: (id) => { return sendRPCMessage('get-object', { model: `Companies`, call: 'getCompany', id }) },
-      findObjects: (selector) => { return sendRPCMessage('find-objects', { model: `Companies`, selector }) },
-      associations: (relType, mainTypeId?) => { return sendRPCMessage('find-conformities', { mainType: 'company', mainTypeId: mainTypeId || execution.target._id, relType }) },
-      createObject: (doc) => { return createObjectWrapper(execution, doc, 'Companies', 'company') },
-      setAssociotions: (mainTypeId, relType, relTypeIds) => { return sendRPCMessage('set-conformities', { mainType: 'company', mainTypeId, relType, relTypeIds }) },
-      setProperty: (doc, id?) => { return sendRPCMessage('set-property', { ...doc, _id: doc._id || id || execution.target._id, model: 'Companies' }) },
-      removeItem: (ids) => { return sendRPCMessage('remove-objects', { model: 'Companies', call: 'removeCompanies', ids }) },
+      getObject: (id) => (sendRPCMessage('get-object', { model: `Companies`, call: 'getCompany', id })),
+      findObjects: (selector) => (sendRPCMessage('find-objects', { model: `Companies`, selector })),
+      associations: (relType, mainTypeId?) => (sendRPCMessage('find-conformities', { mainType: 'company', mainTypeId: mainTypeId || execution.target._id, relType })),
+      createObject: (doc) => (createObjectWrapper(execution, doc, 'Companies', 'company')),
+      setAssociotions: (mainTypeId, relType, relTypeIds) => (sendRPCMessage('set-conformities', { mainType: 'company', mainTypeId, relType, relTypeIds })),
+      setProperty: (doc, id?) => (sendRPCMessage('set-property', { ...doc, _id: doc._id || id || execution.target._id, model: 'Companies' })),
+      removeItem: (ids) => (sendRPCMessage('remove-objects', { model: 'Companies', call: 'removeCompanies', ids })),
     },
     deal: {
-      getObject: (id) => { return sendRPCMessage('get-object', { model: `Deals`, call: 'getDeal', id }) },
-      findObjects: (selector) => { return sendRPCMessage('find-objects', { model: `Deals`, selector }) },
-      associations: (relType, mainTypeId?) => { return sendRPCMessage('find-conformities', { mainType: 'deal', mainTypeId: mainTypeId || execution.target._id, relType }) },
-      createObject: (doc) => { return createObjectWrapper(execution, doc, 'Deals', 'deal', 'add-deal') },
-      setAssociotions: (mainTypeId, relType, relTypeIds) => { return sendRPCMessage('set-conformities', { mainType: 'deal', mainTypeId, relType, relTypeIds }) },
-      setProperty: (doc, id?) => { return sendRPCMessage('set-property', { ...doc, _id: doc._id || id || execution.target._id, model: 'Deals' }) },
-      removeItem: (ids) => { return sendRPCMessage('remove-deal', { model: 'Deals', call: 'removeDeals', ids }) },
+      getObject: (id) => (sendRPCMessage('get-object', { model: `Deals`, call: 'getDeal', id })),
+      findObjects: (selector) => (sendRPCMessage('find-objects', { model: `Deals`, selector })),
+      associations: (relType, mainTypeId?) => (sendRPCMessage('find-conformities', { mainType: 'deal', mainTypeId: mainTypeId || execution.target._id, relType })),
+      createObject: (doc) => (createObjectWrapper(execution, doc, 'Deals', 'deal', 'add-deal')),
+      setAssociotions: (mainTypeId, relType, relTypeIds) => (sendRPCMessage('set-conformities', { mainType: 'deal', mainTypeId, relType, relTypeIds })),
+      setProperty: (doc, id?) => (sendRPCMessage('set-property', { ...doc, _id: doc._id || id || execution.target._id, model: 'Deals' })),
+      removeItem: (ids) => (sendRPCMessage('remove-deal', { model: 'Deals', call: 'removeDeals', ids })),
     },
     task: {
-      getObject: (id) => { return sendRPCMessage('get-object', { model: `Tasks`, call: 'getTask', id }) },
-      findObjects: (selector) => { return sendRPCMessage('find-objects', { model: `Tasks`, selector }) },
-      associations: (relType, mainTypeId?) => { return sendRPCMessage('find-conformities', { mainType: 'task', mainTypeId: mainTypeId || execution.target._id, relType }) },
-      createObject: (doc) => { return createObjectWrapper(execution, doc, 'Tasks', 'task', 'add-task') },
-      setAssociotions: (mainTypeId, relType, relTypeIds) => { return sendRPCMessage('set-conformities', { mainType: 'task', mainTypeId, relType, relTypeIds }) },
-      setProperty: (doc, id?) => { return sendRPCMessage('set-property', { ...doc, _id: doc._id || id || execution.target._id, model: 'Tasks' }) },
-      removeItem: (ids) => { return sendRPCMessage('remove-task', { model: 'Tasks', call: 'removeTasks', ids }) },
+      getObject: (id) => (sendRPCMessage('get-object', { model: `Tasks`, call: 'getTask', id })),
+      findObjects: (selector) => (sendRPCMessage('find-objects', { model: `Tasks`, selector })),
+      associations: (relType, mainTypeId?) => (sendRPCMessage('find-conformities', { mainType: 'task', mainTypeId: mainTypeId || execution.target._id, relType })),
+      createObject: (doc) => (createObjectWrapper(execution, doc, 'Tasks', 'task', 'add-task')),
+      setAssociotions: (mainTypeId, relType, relTypeIds) => (sendRPCMessage('set-conformities', { mainType: 'task', mainTypeId, relType, relTypeIds })),
+      setProperty: (doc, id?) => (sendRPCMessage('set-property', { ...doc, _id: doc._id || id || execution.target._id, model: 'Tasks' })),
+      removeItem: (ids) => (sendRPCMessage('remove-task', { model: 'Tasks', call: 'removeTasks', ids })),
     },
     ticket: {
-      getObject: (id) => { return sendRPCMessage('get-object', { model: `Tickets`, call: 'getTicket', id }) },
-      findObjects: (selector) => { return sendRPCMessage('find-objects', { model: `Tickets`, selector }) },
-      associations: (relType, mainTypeId?) => { return sendRPCMessage('find-conformities', { mainType: 'ticket', mainTypeId: mainTypeId || execution.target._id, relType }) },
-      createObject: (doc) => { return createObjectWrapper(execution, doc, 'Tickets', 'ticket', 'add-ticket') },
-      setAssociotions: (mainTypeId, relType, relTypeIds) => { return sendRPCMessage('set-conformities', { mainType: 'ticket', mainTypeId, relType, relTypeIds }) },
-      setProperty: (doc, id?) => { return sendRPCMessage('set-property', { ...doc, _id: doc._id || id || execution.target._id, model: 'Tickets' }) },
-      removeItem: (ids) => { return sendRPCMessage('remove-ticket', { model: 'Tickets', call: 'removeTickets', ids }) },
+      getObject: (id) => (sendRPCMessage('get-object', { model: `Tickets`, call: 'getTicket', id })),
+      findObjects: (selector) => (sendRPCMessage('find-objects', { model: `Tickets`, selector })),
+      associations: (relType, mainTypeId?) => (sendRPCMessage('find-conformities', { mainType: 'ticket', mainTypeId: mainTypeId || execution.target._id, relType })),
+      createObject: (doc) => (createObjectWrapper(execution, doc, 'Tickets', 'ticket', 'add-ticket')),
+      setAssociotions: (mainTypeId, relType, relTypeIds) => (sendRPCMessage('set-conformities', { mainType: 'ticket', mainTypeId, relType, relTypeIds })),
+      setProperty: (doc, id?) => (sendRPCMessage('set-property', { ...doc, _id: doc._id || id || execution.target._id, model: 'Tickets' })),
+      removeItem: (ids) => (sendRPCMessage('remove-ticket', { model: 'Tickets', call: 'removeTickets', ids })),
     },
     conversation: {
-      getObject: (id) => { return sendRPCMessage('get-object', { model: `Conversations`, call: 'getConversation', id }) },
-      findObjects: (selector) => { return sendRPCMessage('find-objects', { model: `Conversations`, selector }) },
-      setProperty: (doc, id?) => { return sendRPCMessage('set-property', { ...doc, _id: doc._id || id || execution.target._id, model: 'Conversations' }) },
+      getObject: (id) => (sendRPCMessage('get-object', { model: `Conversations`, call: 'getConversation', id })),
+      findObjects: (selector) => (sendRPCMessage('find-objects', { model: `Conversations`, selector })),
+      setProperty: (doc, id?) => (sendRPCMessage('set-property', { ...doc, _id: doc._id || id || execution.target._id, model: 'Conversations' })),
     },
     teamMember: {
-      getObject: (id) => { return sendRPCMessage('get-object', { model: `Users`, call: 'getUser', id }) },
-      findObjects: (selector) => { return sendRPCMessage('find-objects', { model: `Users`, selector }) },
-      setProperty: (doc, id?) => { return sendRPCMessage('set-property', { ...doc, _id: doc._id || id || execution.target._id, model: 'Users' }) },
+      getObject: (id) => (sendRPCMessage('get-object', { model: `Users`, call: 'getUser', id })),
+      findObjects: (selector) => (sendRPCMessage('find-objects', { model: `Users`, selector })),
+      setProperty: (doc, id?) => (sendRPCMessage('set-property', { ...doc, _id: doc._id || id || execution.target._id, model: 'Users' })),
     },
     notification: {
 
@@ -93,9 +93,9 @@ export const getContext = async (execution: IExecution) => {
       }
     },
     plugins: {
-      getObject: (model, id) => { return sendRPCMessage('get-object', { model, id }) },
-      findObjects: (model, selector) => { return sendRPCMessage('find-objects', { model, selector }) },
-      setProperty: (model, doc, id?) => { return sendRPCMessage('set-property', { ...doc, _id: doc._id || id || execution.target._id, model }) },
+      getObject: (model, id) => (sendRPCMessage('get-object', { model, id })),
+      findObjects: (model, selector) => (sendRPCMessage('find-objects', { model, selector })),
+      setProperty: (model, doc, id?) => (sendRPCMessage('set-property', { ...doc, _id: doc._id || id || execution.target._id, model })),
     }
   }
 }
