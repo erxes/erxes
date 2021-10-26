a8abcb04d12f4cbeb68b90b8a61fda5a395ab301
soyombo <soyombo.b@nmtec.co>
2021-10-26 20:52:47 +0800

    Update routes.ts

--- plugins/erxes-plugin-pos/api/routes.ts
+++ plugins/erxes-plugin-pos/api/routes.ts
@@ -1,21 +1,115 @@
+import { getEnv } from 'erxes-api-utils';
+
 export default {
   routes: [
     {
-      method: 'GET',
-      path: '/pos/users',
+      method: 'POST',
+      path: '/pos',
       handler: async ({ req, models }) => {
-        if (req.headers.pos_token === 'pos_token') {
-          return await models.Users.find();
+        const { pos_token } = req.headers;
+        const pos = await models.Pos.getPos(models, { token: pos_token });
+
+        const data: any = {};
+
+        data.pos = pos;
+
+        // collect admin users
+        if (pos.adminIds) {
+          data.adminUsers = await models.Users.find({
+            _id: { $in: pos.adminIds }
+          });
         }
-      }
-    },
-    {
-      method: 'GET',
-      path: '/pos/users',
-      handler: async ({ req, models }) => {
-        if (req.headers.pos_token === 'pos_token') {
-          return await models.Users.find();
+
+        // collect cashiers
+        if (pos.cashierIds) {
+          data.cashiers = await models.Users.find({
+            _id: { $in: pos.cashierIds }
+          });
         }
+
+        if (pos.formIntegrationIds) {
+          const leadIntegrations = await models.Integrations.find({
+            _id: { $in: pos.formIntegrationIds },
+            kind: 'lead'
+          });
+
+          const WIDGETS_DOMAIN = getEnv({ name: 'WIDGETS_DOMAIN' });
+
+          console.log('WIDGETS_DOMAIN: ', WIDGETS_DOMAIN);
+
+          const forms = [];
+
+          for (const integration of leadIntegrations) {
+            const form = await models.Forms.getForm(integration.formId);
+            const brand = await models.Brands.getBrand({
+              _id: integration.brandId
+            });
+
+            const installScript = `<script>
+            window.erxesSettings = {
+              forms: [{
+                brand_id: "${brand.code}",
+                form_id: "${form.code}"
+              }],
+            };
+          
+          (function() {
+            var script = document.createElement('script');
+            script.src = "${WIDGETS_DOMAIN}/build/formWidget.bundle.js";
+            script.async = true;
+          
+            var entry = document.getElementsByTagName('script')[0];
+            entry.parentNode.insertBefore(script, entry);
+          })();
+          
+          </script>`;
+            forms.push({ installScript, name: integration.name });
+          }
+
+          data.forms = forms;
+        }
+
+        const groups = await models.ProductGroups.groups(models, pos._id);
+
+        const productGroups = [];
+
+        for (const group of groups) {
+          const productCategories = await models.ProductCategories.find({
+            $and: [
+              { _id: { $in: group.categoryIds } },
+              { _id: { $nin: group.excludedCategoryIds } }
+            ]
+          });
+          const categories = [];
+
+          for (const category of productCategories) {
+            const products = await models.Products.find({
+              categoryId: category._id,
+              _id: { $nin: group.excludedProductIds }
+            });
+
+            category.products = products;
+
+            const cat = {
+              _id: category._id,
+              name: category.name,
+              description: category.description,
+              code: category.code,
+              parentId: category.parentId,
+              order: category.order,
+              products
+            };
+
+            categories.push(cat);
+          }
+
+          group.categories = categories;
+          productGroups.push(group);
+        }
+
+        data.productGroups = productGroups;
+
+        return data;
       }
     }
   ]
