bd22d940713ffa7225772377b66b3696c4efc312
anu-ujin <anu.b@nmma.co>
2021-11-03 03:08:12 +0800

    some fix

--- widgets/client/booking/components/common/FilterableList.tsx
+++ widgets/client/booking/components/common/FilterableList.tsx
@@ -1,5 +1,5 @@
-import * as React from 'react';
-import { ICategoryTree, IStyle } from '../../types';
+import * as React from "react";
+import { ICategoryTree, IStyle } from "../../types";
 
 type Props = {
   items?: ICategoryTree[];
@@ -31,9 +31,9 @@ class FilterableList extends React.Component<Props, State> {
 
     this.state = {
       isOpen: false,
-      key: '',
+      key: "",
       items: props.items,
-      parentIds: {}
+      parentIds: {},
     };
   }
 
@@ -49,7 +49,7 @@ class FilterableList extends React.Component<Props, State> {
   componentWillReceiveProps(nextProps: any) {
     if (JSON.stringify(this.props.items) !== JSON.stringify(nextProps.items)) {
       this.setState({
-        items: nextProps.items
+        items: nextProps.items,
       });
     }
   }
@@ -68,7 +68,7 @@ class FilterableList extends React.Component<Props, State> {
   };
 
   groupByParent = (array: any[]) => {
-    const key = 'parentId';
+    const key = "parentId";
 
     return array.reduce((rv, x) => {
       (rv[x[key]] = rv[x[key]] || []).push(x);
@@ -84,40 +84,30 @@ class FilterableList extends React.Component<Props, State> {
     this.setState({ parentIds });
   };
 
-  renderIcons(item: any, hasChildren: boolean, isOpen: boolean, color: string) {
-    const arrowsize = '0.3em';
+  renderIcons(isOpen: boolean, color: string) {
+    const arrowsize = "0.3em";
     const downTraingle = {
       borderColor: `${color} transparent transparent transparent`,
-      borderStyle: 'solid',
+      borderStyle: "solid",
       borderWidth: `${arrowsize} ${arrowsize} 0px ${arrowsize}`,
-      height: '0px',
-      width: '0px'
+      height: "0px",
+      width: "0px",
     };
 
     const rightTraingle = {
       borderColor: `transparent transparent transparent ${color}`,
-      borderStyle: 'solid',
+      borderStyle: "solid",
       borderWidth: `${arrowsize} 0px ${arrowsize} ${arrowsize}`,
-      height: '0px',
-      width: '0px'
+      height: "0px",
+      width: "0px",
     };
 
-    return hasChildren ? (
-      <div>
-        {' '}
-        {isOpen ? <div style={downTraingle} /> : <div style={rightTraingle} />}
-      </div>
-    ) : null;
+    return <div style={isOpen ? downTraingle : rightTraingle} />;
   }
 
   renderItem(item: any, hasChildren: boolean, stockCnt: number) {
     const { changeRoute, styles } = this.props;
-    const {
-      widgetColor,
-      productAvailable,
-      productUnavailable,
-      productSelected
-    } = styles;
+    const { widgetColor, productAvailable } = styles;
     const { key } = this.state;
 
     if (key && item.name.toLowerCase().indexOf(key.toLowerCase()) < 0) {
@@ -125,8 +115,10 @@ class FilterableList extends React.Component<Props, State> {
     }
 
     const isOpen = this.state.parentIds[item._id] || !!key;
+    const isDisabled = item.status === "disabled" || item.count === 0;
+    const isSelected = item._id === this.props.selectedItem;
 
-    let color = stockCnt === 0 ? productUnavailable : widgetColor;
+    let color = stockCnt === 0 ? "#AAA" : widgetColor;
 
     if (stockCnt > 0 && stockCnt < 10) {
       color = productAvailable;
@@ -139,30 +131,27 @@ class FilterableList extends React.Component<Props, State> {
     return (
       <li
         key={item._id}
-        className={`list flex-sb ${
-          item.status === 'disabled' || item.count === 0 ? 'card-disabled' : ''
+        className={`list flex-sb ${isDisabled ? "card-disabled" : ""} ${
+          isSelected ? "selected" : ""
         }`}
-        style={
-          item._id === this.props.selectedItem
-            ? { fontWeight: 500, color: productSelected }
-            : { fontWeight: 400, color }
-        }
       >
-        <div className="flex-center">
+        <div className="flex-items-center">
+          {hasChildren && (
+            <div
+              className="toggle-nav"
+              onClick={() => this.onToggle(item._id, isOpen)}
+            >
+              {this.renderIcons(isOpen, color)}
+            </div>
+          )}
           <div
-            className="toggle-nav"
-            onClick={() => this.onToggle(item._id, isOpen)}
-          >
-            {this.renderIcons(item, hasChildren, isOpen, color)}
-          </div>
-          <div
-            style={{
-              fontSize: '1em',
-              marginRight: '2em'
-            }}
+            className={`list-item`}
             onClick={() => handleClick(item)}
+            style={{
+              color: !isDisabled ? color : "",
+            }}
           >
-            {item.name || '[undefined]'}
+            {item.name || "[undefined]"}
           </div>
         </div>
         <div className={`circle center `} style={{ backgroundColor: color }}>
@@ -196,30 +185,24 @@ class FilterableList extends React.Component<Props, State> {
     return this.renderItem(parent, false, stockCnt);
   }
 
-  renderItems() {
-    const { loading, parentId } = this.props;
-    const { items } = this.state;
-
-    if (loading) {
-      return null;
-    }
-
-    if (items.length === 0) {
-      return null;
-    }
-
-    const parents = items.filter(item => item.parentId === parentId);
-    const subFields = items.filter(item => item.parentId);
-
-    return parents.map(parent => this.renderTree(parent, subFields));
-  }
-
   togglePopover = () => {
     this.setState({ isOpen: !this.state.isOpen });
   };
 
   render() {
-    return <div>{this.renderItems()}</div>;
+    const { loading, parentId } = this.props;
+    const { items } = this.state;
+
+    if (loading || items.length === 0) {
+      return null;
+    }
+
+    const parents = items.filter((item) => item.parentId === parentId);
+    const subFields = items.filter((item) => item.parentId);
+
+    return (
+      <ul>{parents.map((parent) => this.renderTree(parent, subFields))}</ul>
+    );
   }
 }
 
