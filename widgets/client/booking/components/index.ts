d67526f2c93429826b02e68f86f84dc674c94a7c
BatAmar Battulga <mrbatamar@gmail.com>
2021-11-03 22:16:41 +0800

    removed booking from widgets

--- widgets/client/booking/components/index.ts
+++
@@ -1,7 +0,0 @@
-import App from './App';
-import Intro from './Intro';
-import Booking from './Booking';
-import Floor from './Floor';
-import Product from './Product';
-
-export { App, Intro, Booking, Floor, Product };
