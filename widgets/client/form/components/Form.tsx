879dec8e5f4af72a290320097199483690e01325
Gerelsukh <gerelsukh0705@gmail.com>
2021-11-11 13:41:24 +0800

    conflict fix

--- widgets/client/form/components/Form.tsx
+++ widgets/client/form/components/Form.tsx
@@ -428,7 +428,6 @@ class Form extends React.Component<Props, State> {
       <div className="erxes-form">
         {this.renderHead(thankTitle || form.title)}
         <div className="erxes-form-content">
-<<<<<<< HEAD
           <div className="erxes-callout-body">
             <img
               onLoad={this.props.setHeight}
@@ -437,24 +436,6 @@ class Form extends React.Component<Props, State> {
             />
             {thankContent ||
               __('Thanks for your message. We will respond as soon as we can.')}
-=======
-          <div className="erxes-success-body">
-            <img
-              className="erxes-success-image"
-              onLoad={this.props.setHeight}
-              src={readFile(successImage)}
-              alt={thankTitle}
-            />
-          </div>
-
-          <div className="erxes-result">
-            <p>
-              {thankContent ||
-                __(
-                  'Thanks for your message. We will respond as soon as we can.'
-                )}
-            </p>
->>>>>>> 17b8bd00150ff62a252b659b1393d4569ebaa451
           </div>
         </div>
       </div>
