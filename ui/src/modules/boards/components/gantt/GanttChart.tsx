84519be90ceacbbdd6e2cc9cde0c7b35d5873d65
Munkhjin <munkhjin0223@gmail.com>
2021-11-22 15:37:14 +0800

    fix onHorizonChange

--- ui/src/modules/boards/components/gantt/GanttChart.tsx
+++ ui/src/modules/boards/components/gantt/GanttChart.tsx
@@ -132,7 +132,7 @@ const GanttChart = (props: Props) => {
   const [timelineMode, setTimelineMode] = useState('month');
 
   const onHorizonChange = (start, end) => {
-    const result = data.filter(item => {
+    const result = dbData.filter(item => {
       return (
         (item.start < start && item.end > end) ||
         (item.start > start && item.start < end) ||
