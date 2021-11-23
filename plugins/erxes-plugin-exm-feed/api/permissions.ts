8e31e18465242efece594222e44d229d11898823
Munkhjin <munkhjin0223@gmail.com>
2021-11-23 19:33:25 +0800

    delete plugin

--- plugins/erxes-plugin-exm-feed/api/permissions.ts
+++
@@ -1,9 +0,0 @@
-export default [
-  { name: 'showExmActivityFeed', description: 'Show exm activity feed' },
-  { name: 'manageExmActivityFeed', description: 'Manage exm activity feed' },
-  {
-    name: 'exmActivityFeedAll',
-    description: 'All',
-    use: ['showExmActivityFeed', 'manageExmActivityFeed']
-  }
-];
