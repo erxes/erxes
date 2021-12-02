bf9aecb001331a0c016d47e679d2f236f4895443
Munkhtenger Gankhuyag <skyline.mn12@gmail.com>
2021-12-02 11:17:18 +0800

    feat(widget): messenger search filter, recent & add conversation component implementation. (#3133)

--- widgets/client/messenger/components/ConversationList.tsx
+++ widgets/client/messenger/components/ConversationList.tsx
@@ -1,13 +1,15 @@
-import * as React from "react";
-import { iconPlus } from "../../icons/Icons";
+import * as React from 'react';
+import { iconPlus, iconSearch } from "../../icons/Icons";
 import { __ } from "../../utils";
 import { ConversationItem } from "../containers";
 import { IConversation } from "../types";
+import { TopBar } from '../containers';
 
 type Props = {
   conversations: IConversation[];
   goToConversation: (conversationId: string) => void;
   createConversation: (e: React.FormEvent<HTMLLIElement>) => void;
+  goToHome: () => void;
   loading: boolean;
 };
 
@@ -16,40 +18,75 @@ function ConversationList(props: Props) {
     conversations,
     goToConversation,
     loading,
-    createConversation
+    createConversation,
+    goToHome
   } = props;
+  const [searchValue, setSearchValue] = React.useState<string>('');
+  const [ conversationList, setConversationList] = React.useState<IConversation[]>([]);
+
+  React.useEffect(() => {
+    
+    if(!loading) {
+      setConversationList(conversations);
+    }
+
+    if(searchValue) {
+      setConversationList(result => result.filter(conv => conv.content.toString().toLowerCase().indexOf(searchValue.toLowerCase()) > -1));
+    }
+  }, [searchValue, loading]);
 
   if (loading) {
     return <div className="loader" />;
-  }
+  }  
 
   const createButton = () => {
     return (
-      <li onClick={createConversation} className="erxes-list-item">
-        <div className="erxes-left-side">
+      <ul className="erxes-last-section">
+        <li onClick={createConversation} className="erxes-create-btn">
           <span>{iconPlus}</span>
+          <span className="erxes-start-text">{__("Start new conversation")}</span>
+        </li>
+      </ul>
+    );
+  };
+
+  const searchButton = () => {
+    return (
+      <li className="erxes-list-item">
+        <div className="erxes-left-side">
+          <span>{iconSearch}</span>
         </div>
         <div className="erxes-right-side">
-          <div className="erxes-name">{__("Start new conversation")}</div>
-          <div className="erxes-last-message">
-            {__("Talk with support staff")}
+          <div className="erxes-name">
+            <input type="text" onChange={(e) => setSearchValue(e.target.value)} placeholder="Search for a conversation..." value={searchValue} />
           </div>
         </div>
       </li>
     );
-  };
+  }
 
   return (
-    <ul className="erxes-conversation-list">
-      {createButton()}
-      {conversations.map(conversation => (
-        <ConversationItem
-          key={conversation._id}
-          conversation={conversation}
-          goToConversation={goToConversation}
+    <>
+    <TopBar
+          middle={
+          `Previous Conversations`
+          }
+          onLeftButtonClick={goToHome}
         />
-      ))}
-    </ul>
+        <div className="erxes-content">
+        <ul className="erxes-conversation-list">
+          {searchButton()}
+          {conversationList.map(conversation => (
+            <ConversationItem
+              key={conversation._id}
+              conversation={conversation}
+              goToConversation={goToConversation}
+            />
+          ))}
+        </ul>
+        {createButton()}
+      </div>
+    </>
   );
 }
 
