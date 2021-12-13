c9b672ad2c50805631dd712629965514f08ce5c8
batchimeg-a <batchimeg01@gmail.com>
2021-12-13 11:53:37 +0800

    fix ui for Gantt

--- ui/src/modules/boards/styles/viewtype.ts
+++ ui/src/modules/boards/styles/viewtype.ts
@@ -53,10 +53,11 @@ export const DateType = styled.div`
 
 export const GanttContainer = styled.div`
   display: flex;
-  flex-direction: column;
   justify-content: flex-start;
   align-items: center;
-  height: 100%;
+  flex-direction: column;
+  overflow: hidden;
+  height: 94%;
 `;
 
 export const TimelineContainer = styled.div`
@@ -65,7 +66,7 @@ export const TimelineContainer = styled.div`
   justify-content: flex-start;
   align-items: center;
   width: 100%;
-  height: 700px;
+  height: 100%;
 `;
 
 export const NavContainer = styled.div`
