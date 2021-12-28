df262f5f165625e1306b196bbdb40dbf6ca6c4e1
BatAmar Battulga <mrbatamar@gmail.com>
2021-12-28 15:21:27 +0800

    watching config changes

--- pls/engages/ui/.erxes/plugin-src/configs.js
+++ pls/engages/ui/.erxes/plugin-src/configs.js
@@ -5,4 +5,24 @@ module.exports = {
     "./routes": "./src/routes.tsx",
     "./settings": "./src/Settings.tsx",
   },
+  routes: {
+    url: "http://localhost:3001/remoteEntry.js",
+    scope: "engages",
+    module: "./routes",
+  },
+  menus: [
+    {
+      text: "Campaigns",
+      url: "/campaigns",
+      icon: "icon-megaphone",
+      location: "mainNavigation",
+    },
+    {
+      text: "Campaigns settings",
+      icon: "icon-megaphone",
+      location: "settings",
+      scope: "engages",
+      component: './settings'
+    },
+  ],
 };
