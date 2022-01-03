3d783eea14407bf04d6ee85bec4af65184bc933f
Dulguun Otgon <dulguun0225@gmail.com>
2022-01-03 12:57:14 +0800

    plugin-engages-api start

--- packages/plugin-engages-api/src/index.ts
+++ packages/plugin-engages-api/src/index.ts
@@ -1,18 +1,23 @@
-import * as bodyParser from 'body-parser';
 import * as dotenv from 'dotenv';
+// load environment variables
+dotenv.config();
+import * as bodyParser from 'body-parser';
 import * as express from 'express';
 import { filterXSS } from 'xss';
 import configs from './api/configs';
 import deliveryReports from './api/deliveryReports';
 import telnyx from './api/telnyx';
+import { buildSubgraphSchema } from "@apollo/federation";
+import { ApolloServer, gql } from "apollo-server-express";
+import * as http from 'http';
 
-// load environment variables
-dotenv.config();
 
 import { connect } from './connection';
 import { debugBase, debugError, debugInit } from './debuggers';
 import { initBroker } from './messageBroker';
 import { trackEngages } from './trackers/engageTracker';
+import { GraphQLResolverMap } from 'apollo-graphql';
+import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
 
 export const app = express();
 
@@ -53,7 +58,33 @@ app.use((error, _req, res, _next) => {
 
 const { MONGO_URL, NODE_ENV, PORT, TEST_MONGO_URL } = process.env;
 
-app.listen(PORT, () => {
+const typeDefs = gql`
+  extend type Query {
+    one: Int
+  }
+`
+
+const resolvers: GraphQLResolverMap = {
+  Query: {
+    one: async () => { return 1; }
+  }
+}
+
+const httpServer = http.createServer(app);
+
+const server = new ApolloServer({
+  schema: buildSubgraphSchema([{ typeDefs, resolvers }]),
+  // for graceful shutdown
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],    
+});
+
+
+async function starServer() {
+  await server.start();
+  server.applyMiddleware({ app, path: '/graphql' });
+  await new Promise<void>(resolve => httpServer.listen({ port: PORT }, resolve));
+  console.log(`🚀 Engages graphql api ready at http://localhost:${PORT}${server.graphqlPath}`);
+
   let mongoUrl = MONGO_URL;
 
   if (NODE_ENV === 'test') {
@@ -68,4 +99,6 @@ app.listen(PORT, () => {
   });
 
   debugInit(`Engages server is running on port ${PORT}`);
-});
+}
+
+starServer();
