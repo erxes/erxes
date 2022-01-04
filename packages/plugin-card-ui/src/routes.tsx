932d53599f5f188a13d0294fbe59e0467eed5d68
anu-ujin <anu.b@nmma.co>
2022-01-04 14:17:05 +0800

    update

---
+++ packages/plugin-card-ui/src/routes.tsx
@@ -0,0 +1,43 @@
+// import { getDefaultBoardAndPipelines } from 'modules/boards/utils';
+import React from "react";
+import { Redirect, Route } from "react-router-dom";
+
+const deals = () => {
+  const dealsLink = "/deal/board";
+
+  // const { defaultBoards, defaultPipelines } = getDefaultBoardAndPipelines();
+
+  // const [defaultBoardId, defaultPipelineId] = [
+  //   defaultBoards.deal,
+  //   defaultPipelines.deal
+  // ];
+
+  // if (defaultBoardId && defaultPipelineId) {
+  //   dealsLink = `/deal/board?id=${defaultBoardId}&pipelineId=${defaultPipelineId}`;
+  // }
+
+  return <Redirect to={dealsLink} />;
+};
+
+const boards = ({ location }) => {
+  // const queryParams = queryString.parse(location.search);
+
+  return <div>Hi, Deal</div>;
+};
+
+const routes = () => {
+  return (
+    <React.Fragment>
+      <Route key="deals" exact={true} path="/deal" render={deals} />
+
+      <Route
+        key="deals/board"
+        exact={true}
+        path="/deal/board"
+        component={boards}
+      />
+    </React.Fragment>
+  );
+};
+
+export default routes;
