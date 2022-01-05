802253826511eda624fb0b44eff0761d009cc3be
Batchimeg <batchimeg01@gmail.com>
2022-01-05 13:37:45 +0800

    added forms in erxes-ui

--- packages/erxes-ui/src/forms/types.ts
+++ packages/erxes-ui/src/forms/types.ts
@@ -19,3 +19,62 @@ export interface IForm extends IFormCommonFIelds {
 export interface IFormData extends IFormCommonFIelds {
   fields?: IField[];
 }
+
+// mutation types
+export type AddFormMutationVariables = {
+  title?: string;
+  description?: string;
+  buttonText?: string;
+  type: string;
+  numberOfPages?: number;
+};
+
+export type AddFormMutationResponse = {
+  addFormMutation: (params: {
+    variables: AddFormMutationVariables;
+  }) => Promise<any>;
+};
+
+export type BulkEditAndAddMutationVariables = {
+  contentType: string;
+  contentTypeId?: string;
+  addingFields?: IField[];
+  editingFields?: IField[];
+};
+
+export type FieldsBulkAddAndEditMutationResponse = {
+  fieldsBulkAddAndEditMutation: (params: {
+    variables: BulkEditAndAddMutationVariables;
+  }) => Promise<void>;
+};
+
+export type EditFormMutationVariables = {
+  _id: string;
+  title?: string;
+  description?: string;
+  buttonText?: string;
+  type: string;
+  numberOfPages?: number;
+};
+
+export type EditFormMutationResponse = {
+  editFormMutation: (params: {
+    variables: EditFormMutationVariables;
+  }) => Promise<any>;
+};
+
+export type FormDetailQueryResponse = {
+  formDetail: IForm;
+  loading: boolean;
+  refetch: () => void;
+};
+
+export type RemoveFieldMutationVariables = {
+  removeFieldsData: IField[];
+};
+
+export type RemoveFieldMutationResponse = {
+  removeFieldMutation: (params: {
+    variable: RemoveFieldMutationVariables;
+  }) => Promise<void>;
+};
