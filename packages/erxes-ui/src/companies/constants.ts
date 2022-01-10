ff5a4b9154dffca9e10ffa337c88c1fe00689c80
Batchimeg <batchimeg01@gmail.com>
2022-01-10 18:43:48 +0800

    add in erxes-ui

--- packages/erxes-ui/src/companies/constants.ts
+++ packages/erxes-ui/src/companies/constants.ts
@@ -256,3 +256,61 @@ export const COUNTRIES = [
   'Zambia',
   'Zimbabwe'
 ];
+
+export const COMPANY_DATAS = {
+  owner: 'Owner',
+  parentCompany: 'Parent Company',
+  links: 'Links',
+
+  ALL: [
+    { field: 'owner', label: 'Owner' },
+    { field: 'parentCompany', label: 'Parent Company' },
+    { field: 'links', label: 'Links' }
+  ]
+};
+
+export const COMPANY_INFO = {
+  avatar: 'Logo',
+  primaryName: 'Primary Name',
+  size: 'Size',
+  industry: 'Industry',
+  plan: 'Plan',
+  primaryEmail: 'Primary Email',
+  primaryPhone: 'Primary Phone',
+  businessType: 'Business Type',
+  description: 'Description',
+  isSubscribed: 'Subscribed',
+  location: 'Headquarters Country',
+
+  ALL: [
+    { field: 'avatar', label: 'Logo' },
+    { field: 'primaryName', label: 'Primary Name' },
+    { field: 'size', label: 'Size' },
+    { field: 'industry', label: 'Industry' },
+    { field: 'plan', label: 'Plan' },
+    { field: 'primaryEmail', label: 'Primary Email' },
+    { field: 'primaryPhone', label: 'Primary Phone' },
+    { field: 'businessType', label: 'Business Type' },
+    { field: 'description', label: 'Description' },
+    { field: 'isSubscribed', label: 'Subscribed' },
+    { field: 'location', label: 'Headquarters Country' }
+  ]
+};
+
+export const COMPANY_LINKS = {
+  linkedIn: 'LinkedIn',
+  twitter: 'Twitter',
+  facebook: 'Facebook',
+  youtube: 'Youtube',
+  github: 'Github',
+  website: 'Website',
+
+  ALL: [
+    { field: 'linkedIn', label: 'LinkedIn' },
+    { field: 'twitter', label: 'Twitter' },
+    { field: 'facebook', label: 'Facebook' },
+    { field: 'youtube', label: 'Youtube' },
+    { field: 'github', label: 'Github' },
+    { field: 'website', label: 'Website' }
+  ]
+};
