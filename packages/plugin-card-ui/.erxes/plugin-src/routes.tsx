18139ad7dc11534a18d3868d3c1a902fc97ef096
mambatukaa <sbatuka9@gmail.com>
2022-01-11 10:32:11 +0800

    little update

--- packages/plugin-card-ui/.erxes/plugin-src/routes.tsx
+++ packages/plugin-card-ui/.erxes/plugin-src/routes.tsx
@@ -1,11 +1,13 @@
 // import { getDefaultBoardAndPipelines } from 'modules/boards/utils';
 import React from 'react';
 import DealRoutes from './deals/routes';
+import TaskRoutes from './tasks/routes';
 
 const routes = () => {
   return (
     <React.Fragment>
       <DealRoutes />
+      <TaskRoutes />
     </React.Fragment>
   );
 };
