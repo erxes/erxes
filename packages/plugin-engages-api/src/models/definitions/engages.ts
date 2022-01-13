0db32b0abdfa089025a4c0b56c6a1fc816979e3c
Buyantogtokh <buyantogtokh.s@nmma.co>
2022-01-13 12:08:42 +0800

    restore engage types

--- packages/plugin-engages-api/src/models/definitions/engages.ts
+++ packages/plugin-engages-api/src/models/definitions/engages.ts
@@ -1,7 +1,82 @@
 import { Schema } from 'mongoose';
 import { ruleSchema, field, schemaWrapper } from '@erxes/api-utils';
-
+import { IRule } from '@erxes/common-types';
 import { MESSAGE_KINDS, MESSENGER_KINDS, CAMPAIGN_METHODS, SENT_AS_CHOICES } from '../../constants';
+
+interface IEmail {
+  attachments?: any;
+  subject?: string;
+  content?: string;
+  replyTo?: string;
+  sender?: string;
+  templateId?: string;
+}
+
+interface IEmailDocument extends IEmail, Document {}
+
+interface IScheduleDate {
+  type?: string;
+  month?: string | number;
+  day?: string | number;
+  dateTime?: string | Date;
+}
+
+
+interface IScheduleDateDocument extends IScheduleDate, Document {}
+
+interface IMessenger {
+  brandId?: string;
+  kind?: string;
+  sentAs?: string;
+  content: string;
+  rules?: IRule[];
+}
+
+interface IMessengerDocument extends IMessenger, Document {}
+
+export interface IShortMessage {
+  content: string;
+  from?: string;
+  fromIntegrationId: string;
+}
+
+export interface IEngageMessage {
+  kind: string;
+  segmentIds?: string[];
+  brandIds?: string[];
+  // normal tagging
+  tagIds?: string[];
+  // customer selection tags
+  customerTagIds?: string[];
+  customerIds?: string[];
+  title: string;
+  fromUserId?: string;
+  method: string;
+  isDraft?: boolean;
+  isLive?: boolean;
+  stopDate?: Date;
+  messengerReceivedCustomerIds?: string[];
+  email?: IEmail;
+  scheduleDate?: IScheduleDate;
+  messenger?: IMessenger;
+  lastRunAt?: Date;
+  shortMessage?: IShortMessage;
+
+  totalCustomersCount?: number;
+  validCustomersCount?: number;
+  runCount?: number;
+}
+
+export interface IEngageMessageDocument extends IEngageMessage, Document {
+  scheduleDate?: IScheduleDateDocument;
+  createdBy: string;
+  createdAt: Date;
+
+  email?: IEmailDocument;
+  messenger?: IMessengerDocument;
+
+  _id: string;
+}
 
 // Mongoose schemas =======================
 export const scheduleDateSchema = new Schema(
