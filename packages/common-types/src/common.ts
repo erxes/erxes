4a9de21b5e87c8f793a31ff29699d2310ff53644
Buyantogtokh <buyantogtokh.s@nmma.co>
2022-01-13 14:50:34 +0800

    rm duplicated types

--- packages/common-types/src/common.ts
+++ packages/common-types/src/common.ts
@@ -23,27 +23,6 @@ export interface ICustomField {
   dateValue?: Date;
 }
 
-export interface IBrandEmailConfig {
-  type?: string;
-  template?: string;
-}
-
-interface IBrandEmailConfigDocument extends IBrandEmailConfig, Document {}
-
-export interface IBrand {
-  code?: string;
-  name?: string;
-  description?: string;
-  userId?: string;
-  emailConfig?: IBrandEmailConfig;
-}
-
-export interface IBrandDocument extends IBrand, Document {
-  _id: string;
-  emailConfig?: IBrandEmailConfigDocument;
-  createdAt: Date;
-}
-
 export interface IBrowserInfo {
   language?: string;
   url?: string;
