861644da97ccfae40fb6ee56c3dd93500ac363b8
ganaa <ganfass@gmail.com>
2022-01-16 18:17:06 +0800

    fix plugin-knowledgebase-api

--- packages/plugin-knowledgebase-api/src/graphql/resolvers/index.ts
+++ packages/plugin-knowledgebase-api/src/graphql/resolvers/index.ts
@@ -2,10 +2,18 @@ import knowledgeBaseMutations from './knowledgeBaseMutations';
 import knowledgeBaseQueries from './knowledgeBaseQueries';
 import customScalars from '@erxes/api-utils/src/customScalars';
 import KnowledgeBaseTopic from './knowledgeBaseTopic';
+import KnowledgeBaseArticle from './knowledgeBaseArticle';
+import {
+  KnowledgeBaseCategory,
+  KnowledgeBaseParentCategory,
+} from './knowledgeBaseCategory';
 
 const resolvers: any = {
   ...customScalars,
   KnowledgeBaseTopic,
+  KnowledgeBaseCategory,
+  KnowledgeBaseParentCategory,
+  KnowledgeBaseArticle,
   Mutation: {
     ...knowledgeBaseMutations,
   },
