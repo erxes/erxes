d2b35b034cd013c82585c7694f25837dd8632c47
ariunzayarin <ariunzaya.ei@gmail.com>
2022-01-26 14:06:52 +0800

    add app.tsx to plugin-card-ui

--- packages/plugin-card-ui/src/index.js
+++ packages/plugin-card-ui/src/index.js
@@ -1,7 +1,7 @@
-import Settings from "./Settings";
+import App from "./App";
 import '@erxes/ui/src/styles/global-styles';
 import 'erxes-icon/css/erxes.min.css';
 import '../public/style.min.css';
 import "@nateradebaugh/react-datetime/css/react-datetime.css";
 
-export default Settings;
+export default App;
