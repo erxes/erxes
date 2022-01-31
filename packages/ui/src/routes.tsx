5369fa74c6d7b999dc9e1c8392749d55af035101
mambatukaa <sbatuka9@gmail.com>
2022-01-31 19:33:53 +0800

    remove automations route

--- packages/ui/src/routes.tsx
+++ packages/ui/src/routes.tsx
@@ -22,7 +22,6 @@ import TagsRoutes from './modules/tags/routes';
 // import TicketRoutes from './modules/tickets/routes';
 import TutorialRoutes from './modules/tutorial/routes';
 import VideoCallRoutes from './modules/videoCall/routes';
-// import AutomationsRoutes from './modules/automations/routes';
 import ImportExportRoutes from './modules/importExport/routes';
 // import BookingsRoutes from './modules/bookings/routes';
 
@@ -105,7 +104,6 @@ const renderRoutes = currentUser => {
           <TutorialRoutes />
           {/* <CalendarRoutes /> */}
           {/* <DashboardRoutes /> */}
-          {/* <AutomationsRoutes /> */}
           <ImportExportRoutes />
           {/* <BookingsRoutes /> */}
 
