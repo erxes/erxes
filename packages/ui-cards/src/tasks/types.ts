6ba3f91999935afbff2d23dc9e906e2b615d8a3b
mambatukaa <sbatuka9@gmail.com>
2022-02-01 11:37:04 +0800

    add task types in ui-cards

---
+++ packages/ui-cards/src/tasks/types.ts
@@ -0,0 +1,47 @@
+import { IItem } from '@erxes/ui-cards/src/boards/types';
+import { IActivityLogForMonth } from '@erxes/ui/src/activityLogs/types';
+
+export type ITask = IItem;
+
+export type EditMutationVariables = {
+  _id: string;
+  name?: string;
+  assignedUserIds: string[];
+  closeDate: Date;
+  description: string;
+  reminderMinute: number;
+  isComplete: boolean;
+};
+
+export type EditMutationResponse = ({
+  variables: EditMutationVariables
+}) => Promise<any>;
+
+export type RemoveMutationResponse = {
+  removeMutation: (params: { variables: { _id: string } }) => Promise<void>;
+};
+
+export type ActivityLogQueryResponse = {
+  activityLogs: IActivityLogForMonth[];
+  loading: boolean;
+};
+
+export type TaskDetailQueryResponse = {
+  taskDetail: ITask;
+  loading: boolean;
+  refetch: () => void;
+};
+
+export type TasksQueryResponse = {
+  tasks: ITask[];
+  loading: boolean;
+  refetch: () => void;
+  fetchMore: any;
+};
+
+export type TasksTotalCountQueryResponse = {
+  tasksTotalCount: number;
+  loading: boolean;
+  refetch: () => void;
+  fetchMore: any;
+};
