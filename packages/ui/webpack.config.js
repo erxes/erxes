d58cf302b82be7d8d4be3e89be820172b09a4ef7
mambatukaa <sbatuka9@gmail.com>
2022-02-06 12:42:22 +0800

    add some updates on ui webpack.config

--- packages/ui/webpack.config.js
+++ packages/ui/webpack.config.js
@@ -1,11 +1,11 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
-const path = require("path");
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
-const InterpolateHtmlPlugin = require("interpolate-html-plugin");
-const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const InterpolateHtmlPlugin = require('interpolate-html-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 
 // replace accordingly './.env' with the path of your .env file
-const envs = require("dotenv").config({ path: "./.env" });
+const envs = require('dotenv').config({ path: './.env' });
 
 const depNames = [
   '@apollo/react-hooks',
@@ -23,21 +23,21 @@ const depNames = [
   'erxes-ui',
   'graphql',
   'graphql-tag',
-  'i18n-react',
+  // 'i18n-react',
   'lodash',
   'lodash.flowright',
   'query-string',
   'react',
   'react-apollo',
-  'react-bootstrap',
+  // 'react-bootstrap',
   'react-dom',
   'react-router-dom',
-  'react-transition-group',
+  // 'react-transition-group',
   'styled-components',
   'styled-components-ts',
   'subscriptions-transport-ws',
   'validator'
-]
+];
 
 const shared = {};
 
@@ -47,7 +47,7 @@ for (const name of depNames) {
 
 module.exports = {
   resolve: {
-    extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
+    extensions: ['.tsx', '.ts', '.jsx', '.js', '.json']
   },
 
   output: {
@@ -63,35 +63,59 @@ module.exports = {
     rules: [
       {
         test: /\.m?js/,
-        type: "javascript/auto",
+        type: 'javascript/auto',
         resolve: {
-          fullySpecified: false,
-        },
+          fullySpecified: false
+        }
       },
       {
         test: /\.(css|s[ac]ss)$/i,
-        use: ["style-loader", "css-loader", "postcss-loader"],
+        use: ['style-loader', 'css-loader', 'postcss-loader']
       },
       {
         test: /\.(ts|tsx|js|jsx)$/,
         exclude: /node_modules/,
+        include: [
+          path.resolve(__dirname, 'src'),
+          path.resolve(__dirname, '../erxes-ui/src'),
+          path.resolve(__dirname, '../ui-settings/src'),
+          path.resolve(__dirname, '../ui-contacts/src'),
+          path.resolve(__dirname, '../ui-forms/src'),
+          path.resolve(__dirname, '../ui-inbox/src'),
+          path.resolve(__dirname, '../ui-team/src'),
+          path.resolve(__dirname, '../ui-products/src'),
+          path.resolve(__dirname, '../ui-cards/src'),
+          path.resolve(__dirname, '../ui-knowledgeBase/src'),
+        ],
         use: {
-          loader: "babel-loader",
-        },
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-typescript',
+              '@babel/preset-react',
+              '@babel/preset-env'
+            ],
+            plugins: [['@babel/transform-runtime']]
+          }
+        }
       },
       {
         test: /\.json$/,
         use: {
-          loader: "json-loader",
-        },
-      },
-    ],
+          loader: 'json-loader'
+        }
+      }
+    ]
   },
 
   resolve: {
-    modules: [path.resolve(__dirname, "src"), "node_modules"],
-    fallback: { "path": require.resolve("path-browserify"), "timers": require.resolve("timers-browserify"), "util": require.resolve("util/") },
-    extensions: ["*", ".js", ".jsx", ".ts", ".tsx"],
+    modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+    fallback: {
+      path: require.resolve('path-browserify'),
+      timers: require.resolve('timers-browserify'),
+      util: require.resolve('util/')
+    },
+    extensions: ['*', '.js', '.jsx', '.ts', '.tsx']
   },
 
   plugins: [
@@ -100,17 +124,17 @@ module.exports = {
       PUBLIC_URL: ''
     }),
     new HtmlWebPackPlugin({
-      template: path.resolve( __dirname, 'public/index.html' ),
+      template: path.resolve(__dirname, 'public/index.html'),
       filename: 'index.html',
-      inject: true,
+      inject: true
     }),
     new ModuleFederationPlugin({
-      name: "main",
-      filename: "remoteEntry.js",
+      name: 'main',
+      filename: 'remoteEntry.js',
       exposes: {
         QuickNavigation: './src/modules/layout/components/QuickNavigation.tsx'
       },
       shared
-    }),
-  ],
+    })
+  ]
 };
