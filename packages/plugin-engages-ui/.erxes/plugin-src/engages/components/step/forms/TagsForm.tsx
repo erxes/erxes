53a124f9b87e36110176fcd9da635f5552bc0e8b
mambatukaa <sbatuka9@gmail.com>
2022-02-10 13:34:27 +0800

    huge update

---
+++ packages/plugin-engages-ui/.erxes/plugin-src/engages/components/step/forms/TagsForm.tsx
@@ -0,0 +1,26 @@
+import { IButtonMutateProps } from '@erxes/ui/src/types';
+import { StepFormWrapper } from '../../../styles';
+import TagForm from '@erxes/ui/src/tags/components/Form';
+import { ITag } from '@erxes/ui/src/tags/types';
+import React from 'react';
+
+type Props = {
+  tags?: ITag[];
+  renderButton: (props: IButtonMutateProps) => JSX.Element;
+  afterSave: () => void;
+};
+
+const Form = ({ renderButton, afterSave, tags }: Props) => {
+  return (
+    <StepFormWrapper>
+      <TagForm
+        type="customer"
+        renderButton={renderButton}
+        afterSave={afterSave}
+        tags={tags || []}
+      />
+    </StepFormWrapper>
+  );
+};
+
+export default Form;
