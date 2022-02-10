307cf15ddb14cb8d23b4b42807f8ff6ae83655ad
Munkhjin Badamkhatan <munkhjin0223@gmail.com>
2022-02-10 17:51:43 +0800

    add relType for conformity related queries

--- api/src/data/resolvers/queries/types.ts
+++ api/src/data/resolvers/queries/types.ts
@@ -1,6 +1,7 @@
 export interface IConformityQueryParams {
   conformityMainType?: string;
   conformityMainTypeId?: string;
+  conformityRelType?: string;
   conformityIsRelated?: boolean;
   conformityIsSaved?: boolean;
 }
