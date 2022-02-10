c46fa4a4743d0427af1bdc00db676fdfde590d88
mambatukaa <sbatuka9@gmail.com>
2022-02-10 11:50:12 +0800

    refactor

--- packages/plugin-clientPortal/.erxes/plugin-src/index.js
+++ packages/plugin-clientPortal/.erxes/plugin-src/index.js
@@ -2,6 +2,5 @@ import App from './App';
 import '@erxes/ui/src/styles/global-styles';
 import '../public/style.min.css';
 import 'erxes-icon/css/erxes.min.css';
-import '@nateradebaugh/react-datetime/css/react-datetime.css';
 
 export default App;
