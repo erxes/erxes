96e3362f15e9a45944b2cab0aa1d555a68445014
mambatukaa <sbatuka9@gmail.com>
2022-02-10 14:51:30 +0800

    add smsDeliveries routes on engage

--- packages/plugin-engages-ui/src/routes.tsx
+++ packages/plugin-engages-ui/src/routes.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import EngageRoutes from './engages/routes';
 import EmailDeliveryRoutes from '@erxes/ui-settings/src/emailDelivery/routes';
+import SmsDeliveriesRoutes from '@erxes/ui-settings/src/smsDeliveries/routes';
 
 const routes = () => {
   return (
     <React.Fragment>
       <EngageRoutes />
       <EmailDeliveryRoutes />
+      <SmsDeliveriesRoutes />
     </React.Fragment>
   );
 };
