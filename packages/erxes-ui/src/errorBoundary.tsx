07aba4b6f3043e9ed810712f09de5b381ee0c4c0
ariunzayarin <ariunzaya.ei@gmail.com>
2022-02-16 16:41:28 +0800

    fix path

--- packages/erxes-ui/src/errorBoundary.tsx
+++ packages/erxes-ui/src/errorBoundary.tsx
@@ -1,7 +1,5 @@
 import React from "react";
-// import Alert from "./utils/Alert";
-import ModalTrigger from "./components/ModalTrigger";
-import Button from "./components/Button";
+import Alert from "./utils/Alert";
 
 class ErrorBoundary extends React.Component<{}, { error; errorInfo }> {
   constructor(props) {
@@ -21,20 +19,20 @@ class ErrorBoundary extends React.Component<{}, { error; errorInfo }> {
   render() {
     // let eroor = this.state.errorInfo.componentStack;
     // let infoo = eroor.toString();
-    
+
     if (this.state.errorInfo) {
       // Error path
-      // Alert.error(this.state.errorInfo.componentStack.toString());;
-      // return <div>Error occured</div>
-      return(
-        <ModalTrigger
-        trigger={<Button>Detail</Button>}
-        title="error"
-        content={()=> <div style={{ whiteSpace: 'pre-wrap' }}>{this.state.error && this.state.error.toString()}
+      Alert.error(this.state.error.toString(), 10000);
+      return (
+          <div>
+          <h5>Error occured.</h5>
+          <details style={{ whiteSpace: 'pre-wrap' }}>
+            {this.state.error && this.state.error.toString()}
             <br />
-            {this.state.errorInfo.componentStack}</div>}
-        />
-      )
+            {this.state.errorInfo.componentStack}
+          </details>
+        </div>
+      );
     }
     // Normally, just render children
     return this.props.children;
