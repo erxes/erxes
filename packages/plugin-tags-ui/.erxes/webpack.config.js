2e1ba01cc5a7e5932387223468a834c148ec5980
mambatukaa <sbatuka9@gmail.com>
2022-02-16 16:52:32 +0800

    add core-ui

--- packages/plugin-tags-ui/.erxes/webpack.config.js
+++ packages/plugin-tags-ui/.erxes/webpack.config.js
@@ -124,7 +124,9 @@ module.exports = {
     new ModuleFederationPlugin({
       name: configs.name,
       filename: "remoteEntry.js",
-      remotes: {},
+      remotes: {
+        coreui: "coreui@http://localhost:3000/remoteEntry.js"
+      },
       exposes,
       shared: {
         ...shared,
