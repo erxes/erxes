eae789f813c9ae1a796a137f990cca8e61961bba
mambatukaa <sbatuka9@gmail.com>
2022-02-16 11:45:51 +0800

    add little update

--- packages/plugin-calendar-ui/.erxes/webpack.config.js
+++ packages/plugin-calendar-ui/.erxes/webpack.config.js
@@ -27,7 +27,8 @@ const shared = {};
 
 for (const name of depNames) {
   shared[name] = {
-    singleton: true
+    singleton: true,
+    requiredVersion: deps[name]
   };
 }
 
@@ -124,7 +125,9 @@ module.exports = {
     new ModuleFederationPlugin({
       name: configs.name,
       filename: 'remoteEntry.js',
-      remotes: {},
+      remotes: {
+        coreui: "coreui@http://localhost:3000/remoteEntry.js"
+      },
       exposes,
       shared: {
         ...shared,
