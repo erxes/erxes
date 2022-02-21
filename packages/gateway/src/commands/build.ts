23f869e71cf916d90fc1388b4a165bf3e3a85313
BatAmar Battulga <mrbatamar@gmail.com>
2022-02-21 17:10:10 +0800

    fixed gateway build

--- packages/gateway/src/commands/build.ts
+++ packages/gateway/src/commands/build.ts
@@ -88,11 +88,7 @@ const main = async () => {
 
   console.log("Yarn build ....");
 
-  if (type === "gateway") {
-    await execCommand("tsc -p tsconfig.prod.json");
-  } else {
-    await execCommand("yarn build");
-  }
+  await execCommand("yarn build");
 
   console.log("Moving node_modules ....");
 
