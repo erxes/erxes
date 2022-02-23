520a05caa50be0442145da0757dd4b24867f0ea2
BatAmar Battulga <mrbatamar@gmail.com>
2022-02-23 13:49:08 +0800

    copied subscriptionPlugin when build

--- packages/api-plugin-template.erxes/fix-build.js
+++ packages/api-plugin-template.erxes/fix-build.js
@@ -1,10 +1,22 @@
 var deps = require('../package.json');
 var fs = require("fs")
 
-var pluginName = deps.name.replace('@erxes', '');
+async function main() {
+  var pluginName = deps.name.replace('@erxes', '');
 
-fs.rename(`./dist/${pluginName}`, './dist/main', function(err) {
-  if (err) {
-    console.log(err)
+  try {
+    await fs.promises.copyFile('../src/graphql/subscriptionPlugin.js', `./dist/${pluginName}/src/graphql/subscriptionPlugin.js`);
+  } catch (e) {
+    console.log(e.message)
   }
+
+  fs.rename(`./dist/${pluginName}`, './dist/main', function(err) {
+    if (err) {
+      console.log(err)
+    }
+  })
+}
+
+main().then(() => {
+  process.exit();
 })
