b6fe6054f91d6b64fc485fd87d8113c95bcc0713
Ujin's MacBook <macintosh@Macintoshs-MacBook-Pro.local>
2022-02-28 12:16:27 +0800

    some fix

--- packages/plugin-logs-ui/.erxes/plugin-src/routes.tsx
+++ packages/plugin-logs-ui/.erxes/plugin-src/routes.tsx
@@ -1,22 +1,21 @@
-import asyncComponent from '@erxes/ui/src/components/AsyncComponent';
-import queryString from 'query-string';
-import * as React from 'react';
-import { Route } from 'react-router-dom';
+import asyncComponent from "@erxes/ui/src/components/AsyncComponent";
+import queryString from "query-string";
+import * as React from "react";
+import { Route } from "react-router-dom";
 
 const LogList = asyncComponent(() =>
-  import(/* webpackChunkName: "Settings - Logs" */ './containers/LogList')
+  import(/* webpackChunkName: "Settings - Logs" */ "./containers/LogList")
 );
 
 const logList = ({ location, history }) => {
-  console.log('bbbbvsdkfhsdkjk');
   const queryParams = queryString.parse(location.search);
-  // return <LogList queryParams={queryParams} history={history} />;
-  return <h1>hahahha</h1>;
+
+  return <LogList queryParams={queryParams} history={history} />;
 };
 
 const routes = () => {
-  console.log("hahahha")
-  return <Route exact={true} path="/settings/logs/" component={logList} />
+  console.log("hahahha");
+  return <Route exact={true} path="/settings/logs/" component={logList} />;
 };
 
 export default routes;
