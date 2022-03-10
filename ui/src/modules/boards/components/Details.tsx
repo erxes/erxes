c37878e54e6e42f592deb95f2f0e58286357b7d0
soyombo <soyombo.b@nmtec.co>
2022-03-10 16:16:06 +0800

    Update Details.tsx

--- ui/src/modules/boards/components/Details.tsx
+++ ui/src/modules/boards/components/Details.tsx
@@ -2,11 +2,12 @@ import { renderFullName } from 'modules/common/utils';
 import { ICompany } from 'modules/companies/types';
 import { ICustomer } from 'modules/customers/types';
 import { IProduct } from 'modules/settings/productService/types';
+import { ICustomData } from 'modules/settings/properties/types';
 import React from 'react';
 import { ItemIndicator } from '../styles/stage';
 
 type Props = {
-  items: ICompany[] | ICustomer[] | IProduct[] | { name: string }[];
+  items: ICompany[] | ICustomer[] | IProduct[] | ICustomData[];
   color: string;
 };
 
