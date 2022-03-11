30bc7e87ff8ed0847596d412035c1e71ba8420a3
Ujin's MacBook <macintosh@Macintoshs-MacBook-Pro.local>
2022-03-11 15:17:39 +0800

    fix

--- packages/plugin-contacts-ui/src/customers/components/list/Sidebar.tsx
+++ packages/plugin-contacts-ui/src/customers/components/list/Sidebar.tsx
@@ -1,29 +1,36 @@
-import Wrapper from '@erxes/ui/src/layout/components/Wrapper';
-import React from 'react';
-import BrandFilter from '../../containers/filters/BrandFilter';
-import IntegrationFilter from '../../containers/filters/IntegrationFilter';
-import LeadFilter from '../../containers/filters/LeadFilter';
-import LeadStatusFilter from '../../containers/filters/LeadStatusFilter';
-import SegmentFilter from '../../containers/filters/SegmentFilter';
-import TagFilter from '../../containers/filters/TagFilter';
+import Wrapper from "@erxes/ui/src/layout/components/Wrapper";
+import React from "react";
+import BrandFilter from "../../containers/filters/BrandFilter";
+import IntegrationFilter from "../../containers/filters/IntegrationFilter";
+import LeadFilter from "../../containers/filters/LeadFilter";
+import LeadStatusFilter from "../../containers/filters/LeadStatusFilter";
+import SegmentFilter from "../../containers/filters/SegmentFilter";
+import TagFilter from "../../containers/filters/TagFilter";
 import { isEnabled } from "@erxes/ui/src/utils/core";
 
 function Sidebar({
   loadingMainQuery,
-  type
+  type,
 }: {
   loadingMainQuery: boolean;
   type: string;
 }) {
-  
   return (
     <Wrapper.Sidebar>
-      {isEnabled("segments") && <SegmentFilter type={type} loadingMainQuery={loadingMainQuery} />}
-      {isEnabled("tags") && <TagFilter type={type} loadingMainQuery={loadingMainQuery} />}
-      {isEnabled("integrations") && <IntegrationFilter type={type} loadingMainQuery={loadingMainQuery} />}
+      {isEnabled("segments") && (
+        <SegmentFilter type={type} loadingMainQuery={loadingMainQuery} />
+      )}
+      {isEnabled("tags") && (
+        <TagFilter type={type} loadingMainQuery={loadingMainQuery} />
+      )}
+      {isEnabled("inbox") && (
+        <IntegrationFilter type={type} loadingMainQuery={loadingMainQuery} />
+      )}
       <BrandFilter type={type} loadingMainQuery={loadingMainQuery} />
-      {isEnabled("inbox") && <LeadFilter type={type} loadingMainQuery={loadingMainQuery} />}
-      {type === 'lead' && (
+      {isEnabled("inbox") && (
+        <LeadFilter type={type} loadingMainQuery={loadingMainQuery} />
+      )}
+      {type === "inbox" && (
         <LeadStatusFilter type={type} loadingMainQuery={loadingMainQuery} />
       )}
     </Wrapper.Sidebar>
