9906d4a3f513547f1b5b28318849d10158f6db0b
enkhtuvshin <amex@AMEXs-Mac-mini.local>
2022-03-15 23:05:48 +0800

    upd

--- packages/core/enabled-services.js
+++ packages/core/enabled-services.js
@@ -1,26 +1,12 @@
 module.exports = {
-<<<<<<< HEAD
   logs: false,
   tags: true,
 
   internalnotes: false,
   notifications: false,
   inbox: true,
-=======
-  automations: false,
-  calendar: false,
-  cards: true,
-  clientportal: false,
-  contacts: true,
-  engages: false,
-  inbox: false,
-  internalnotes: true,
-  knowledgebase: false,
-  logs: true,
-  notifications: false,
->>>>>>> 3bf12597ac6741a2798597afa4af2c22b77f6679
   products: false,
   segments: false,
   tags: false,
-  forms: false,
+  forms: false
 };
