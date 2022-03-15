520c0d4f68d476af3f9b62fade506ed985a2334a
BatAmar Battulga <mrbatamar@gmail.com>
2022-03-15 23:02:57 +0800

    conflict fix

--- packages/core/enabled-services.js
+++ packages/core/enabled-services.js
@@ -1,12 +1,4 @@
 module.exports = {
-<<<<<<< HEAD
-  logs: false,
-  tags: true,
-
-  internalnotes: false,
-  notifications: false,
-  inbox: true,
-=======
   automations: false,
   calendar: false,
   cards: true,
@@ -18,7 +10,6 @@ module.exports = {
   knowledgebase: false,
   logs: true,
   notifications: false,
->>>>>>> 3bf12597ac6741a2798597afa4af2c22b77f6679
   products: false,
   segments: false,
   tags: false,
