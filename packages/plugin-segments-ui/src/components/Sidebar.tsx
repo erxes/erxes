6c9a4d58d76f20fc32b5ec5fdd239224a0719c95
enkhtuvshin <amex@AMEXs-Mac-mini.local>
2022-03-22 12:26:48 +0800

    little fix

--- packages/plugin-segments-ui/src/components/Sidebar.tsx
+++ packages/plugin-segments-ui/src/components/Sidebar.tsx
@@ -23,7 +23,7 @@ function ListItem(type, contentType) {
 
 function Sidebar({
   types,
-  type
+  contentType
 }: {
   types: Array<{ contentType: string; description: string }>;
   contentType: string;
