f9ce53d38261a33ec5536b0eafe9d6ebea45aded
Ujin's MacBook <macintosh@Macintoshs-MacBook-Pro.local>
2022-03-22 14:49:06 +0800

    dd

--- packages/core/enabled-services.js
+++ packages/core/enabled-services.js
@@ -1,17 +1,17 @@
 module.exports = {
   automations: false,
   calendar: false,
-  cards: false,
+  cards: true,
   clientportal: false,
   contacts: true,
   engages: false,
-  inbox: false,
+  inbox: true,
   internalnotes: false,
   knowledgebase: false,
-  logs: true,
+  logs: false,
   notifications: false,
   products: false,
   segments: false,
-  tags: true,
-  forms: false
+  tags: false,
+  forms: false,
 };
