82a07124e94cf904615f11d5473f1d4d622a5d9f
JerryG <ganfass@gmail.com>
2022-03-22 17:28:42 +0800

    inject-plugiin-clientportal-api (#3258)

---
+++ packages/plugin-clientportal-api/src/connectionResolver.ts
@@ -0,0 +1,46 @@
+import { MongoClient } from 'mongodb';
+import * as mongoose from 'mongoose';
+import { mainDb } from './configs';
+import { IContext as IMainContext } from '@erxes/api-utils/src';
+import {
+  IClientPortalModel,
+  loadClientPortalClass,
+} from './models/ClientPortal';
+import { IClientPortalDocument } from './models/definitions/clientPortal';
+
+export interface ICoreIModels {
+  Fields;
+}
+export interface IModels {
+  ClientPortals: IClientPortalModel;
+}
+
+export interface IContext extends IMainContext {
+  subdomain: string;
+  models: IModels;
+}
+
+export let models: IModels;
+
+export const generateModels = async (
+  hostnameOrSubdomain: string
+): Promise<IModels> => {
+  if (models) {
+    return models;
+  }
+
+  loadClasses(mainDb);
+
+  return models;
+};
+
+export const loadClasses = (db: mongoose.Connection): IModels => {
+  models = {} as IModels;
+
+  models.ClientPortals = db.model<IClientPortalDocument, IClientPortalModel>(
+    'clientportals',
+    loadClientPortalClass(models)
+  );
+
+  return models;
+};
