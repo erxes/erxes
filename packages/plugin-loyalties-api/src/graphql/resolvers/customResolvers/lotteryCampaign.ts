3025a090125e4648f6a789753f23ab8ef91f1463
munkhsaikhan.g@nmma.co <munkhsaikhan.g@nmma.co>
2022-03-25 15:53:18 +0800

    little

--- packages/plugin-loyalties-api/src/graphql/resolvers/customResolvers/lotteryCampaign.ts
+++ packages/plugin-loyalties-api/src/graphql/resolvers/customResolvers/lotteryCampaign.ts
@@ -2,7 +2,7 @@ import { IContext } from '../../../connectionResolver';
 import { ILotteryCampaignDocument } from '../../../models/definitions/lotteryCampaigns';
 
 export default {
-  async lotteryesCount(lotteryCampaign: ILotteryCampaignDocument, _args, { models }: IContext) {
+  async lotteriesCount(lotteryCampaign: ILotteryCampaignDocument, _args, { models }: IContext) {
     return models.Lotteries.find({ campaignId: lotteryCampaign._id }).countDocuments();
   },
 }
