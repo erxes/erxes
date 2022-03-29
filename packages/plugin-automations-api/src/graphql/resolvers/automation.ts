2e62064b7ea5ae442548d231dce25db6cef885e6
mambatukaa <sbatuka9@gmail.com>
2022-03-29 14:03:54 +0800

    update resolver

--- packages/plugin-automations-api/src/graphql/resolvers/automation.ts
+++ packages/plugin-automations-api/src/graphql/resolvers/automation.ts
@@ -2,11 +2,17 @@ import { IContext } from "../../connectionResolver";
 import { IAutomationDoc } from '../../models/definitions/automaions';
 
 export default {
-  createdUser(automation: IAutomationDoc, {}, { coreModels }: IContext) {
-    return coreModels.Users.findOne({ _id: automation.createdBy });
+  createdUser(automation: IAutomationDoc) {
+    return automation.createdBy && {
+      __typename: 'User',
+      _id: automation.createdBy
+    }
   },
 
-  updatedUser(automation: IAutomationDoc, {}, { coreModels }: IContext) {
-    return coreModels.Users.findOne({ _id: automation.updatedBy });
+  updatedUser(automation: IAutomationDoc) {
+    return automation.updatedBy && {
+      __typename: 'User',
+      _id: automation.updatedBy
+    }
   }
 };
