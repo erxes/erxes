3c39257dc955d54b17a4bb1546fde5c2d1f1f6d8
Buyantogtokh <buyantogtokh.s@nmma.co>
2022-04-05 12:03:59 +0800

    update plugin menus

--- packages/core-ui/public/js/plugins.js
+++ packages/core-ui/public/js/plugins.js
@@ -164,4 +164,47 @@ window.plugins = [
       }
     ]
   },
+  {
+    name: "forms",
+    port: 3005,
+    exposes: { "./routes": "./src/routes.tsx" },
+    routes: {
+      url: "http://localhost:3005/remoteEntry.js",
+      scope: "forms",
+      module: "./routes",
+    },
+    menus: [
+      {
+        text: "Properties",
+        to: "/settings/properties",
+        image: "/images/icons/erxes-01.svg",
+        location: "settings",
+        scope: "forms",
+        action: "",
+        permissions: [],
+      },
+    ],
+  },
+  {
+    name: "logs",
+    port: 3040,
+    exposes: { "./routes": "./src/routes.tsx" },
+    routes: {
+      url: "http://localhost:3040/remoteEntry.js",
+      scope: "logs",
+      module: "./routes",
+    },
+    menus: [
+      {
+        text: "Logs",
+        to: "/settings/logs",
+        image: "/images/icons/erxes-33.png",
+        location: "settings",
+        scope: "logs",
+        component: "./settings",
+        action: "",
+        permissions: [],
+      },
+    ],
+  },
 ];
