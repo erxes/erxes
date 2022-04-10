f7d193500f48ec1511efeca5bc577ac33d75d732
munkhsaikhan.g@nmma.co <munkhsaikhan.g@nmma.co>
2022-04-10 23:12:08 +0800

    fix

--- packages/plugin-automations-ui/.erxes/plugin-src/containers/forms/actions/subForms/LoyaltyForm.tsx
+++ packages/plugin-automations-ui/.erxes/plugin-src/containers/forms/actions/subForms/LoyaltyForm.tsx
@@ -1,11 +1,11 @@
-import * as compose from 'lodash.flowright';
-import Form from '../../../../components/forms/actions/subForms/LoyaltyForm';
-import gql from 'graphql-tag';
-import React from 'react';
-import { graphql } from 'react-apollo';
 import { queries as voucherCampaignQueries } from '@erxes/plugin-loyalties-ui/src/configs/voucherCampaign/graphql';
 import { VoucherCampaignQueryResponse } from '@erxes/plugin-loyalties-ui/src/configs/voucherCampaign/types';
 import { withProps } from '@erxes/ui/src/utils';
+import gql from 'graphql-tag';
+import * as compose from 'lodash.flowright';
+import React from 'react';
+import { graphql } from 'react-apollo';
+import Form from '../../../../components/forms/actions/subForms/LoyaltyForm';
 
 
 type Props = {
