55936a599259a89f8c2e7a442e3c589ead2b2e42
BatAmar Battulga <mrbatamar@gmail.com>
2022-04-16 18:05:19 +0800

    debug

--- packages/plugin-cards-ui/.erxes/plugin-src/routes.tsx
+++ packages/plugin-cards-ui/.erxes/plugin-src/routes.tsx
@@ -7,6 +7,8 @@ import GrowthHackingRoutes from './growthHacks/routes';
 import GrowthSettings from './settings/growthHacks/routes';
 import BoardSettings from './settings/boards/routes';
 
+console.log('debug: in routes')
+
 const routes = () => {
   return (
     <React.Fragment>
