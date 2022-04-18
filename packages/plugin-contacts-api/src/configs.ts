8d7391d1a8cfd77a167c18c3cd2c0a4f2444ac95
mambatukaa <sbatuka9@gmail.com>
2022-04-18 14:18:48 +0800

    refactor permissions on contacts-api

--- packages/plugin-contacts-api/src/configs.ts
+++ packages/plugin-contacts-api/src/configs.ts
@@ -23,6 +23,7 @@ import tags from './tags'
 import internalNotes from './internalNotes';
 import automations from './automations';
 import search from './search';
+import permissions from './permissions';
 
 export let mainDb;
 export let graphqlPubsub;
@@ -39,102 +40,7 @@ export let debug;
 
 export default {
   name: 'contacts',
-  permissions: {
-    companies: {
-      name: 'companies',
-      description: 'Companies',
-      actions: [
-        {
-          name: 'companiesAll',
-          description: 'All',
-          use: [
-            'companiesAdd',
-            'companiesEdit',
-            'companiesRemove',
-            'companiesMerge',
-            'showCompanies',
-            'showCompaniesMain',
-            'exportCompanies'
-          ]
-        },
-        {
-          name: 'companiesAdd',
-          description: 'Add companies'
-        },
-        {
-          name: 'companiesEdit',
-          description: 'Edit companies'
-        },
-        {
-          name: 'companiesRemove',
-          description: 'Remove companies'
-        },
-        {
-          name: 'companiesMerge',
-          description: 'Merge companies'
-        },
-        {
-          name: 'showCompanies',
-          description: 'Show companies'
-        },
-        {
-          name: 'showCompaniesMain',
-          description: 'Show companies main'
-        },
-        {
-          name: 'exportCompanies',
-          description: 'Export companies to xls file'
-        }
-      ]
-    },
-    customers: {
-      name: 'customers',
-      description: 'Customers',
-      actions: [
-        {
-          name: 'customersAll',
-          description: 'All',
-          use: [
-            'showCustomers',
-            'customersAdd',
-            'customersEdit',
-            'customersMerge',
-            'customersRemove',
-            'exportCustomers',
-            'customersChangeState'
-          ]
-        },
-        {
-          name: 'exportCustomers',
-          description: 'Export customers'
-        },
-        {
-          name: 'showCustomers',
-          description: 'Show customers'
-        },
-        {
-          name: 'customersAdd',
-          description: 'Add customer'
-        },
-        {
-          name: 'customersEdit',
-          description: 'Edit customer'
-        },
-        {
-          name: 'customersMerge',
-          description: 'Merge customers'
-        },
-        {
-          name: 'customersRemove',
-          description: 'Remove customers'
-        },
-        {
-          name: 'customersChangeState',
-          description: 'Change customer state'
-        }
-      ]
-    }
-  },
+  permissions,
   graphql: async sd => {
     serviceDiscovery = sd;
 
