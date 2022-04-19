79ee2a07f2af17a8d85225a1b1188510d7be4389
BatAmar Battulga <mrbatamar@gmail.com>
2022-04-19 22:43:12 +0800

    removed .erxes

--- packages/plugin-cards-ui/.erxes/plugin-src/growthHacks/components/editForm/index.tsx
+++
@@ -1,6 +0,0 @@
-import Left from './Left';
-import StageForm from './StageForm';
-import Top from './Top';
-import Vote from './Vote';
-
-export { Left, StageForm, Top, Vote };
--- packages/plugin-contacts-ui/.erxes/plugin-src/customers/containers/CustomerForm.tsx
+++
@@ -1,3 +0,0 @@
-import CustomerForm from '@erxes/ui/src/customers/containers/CustomerForm';
-
-export default CustomerForm;
