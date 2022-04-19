701bbdf4d9855eb77aad3b3eb306fc0fdc148f87
Ujin's MacBook <macintosh@Macintoshs-MacBook-Pro.local>
2022-04-19 19:37:07 +0800

    fix query name

--- packages/ui-settings/src/templates/components/productCategory/CategoryList.tsx
+++ packages/ui-settings/src/templates/components/productCategory/CategoryList.tsx
@@ -1,23 +1,20 @@
-import { __ } from '@erxes/ui/src/utils';
-import Sidebar from '@erxes//ui/src/layout/components/Sidebar';
-import Wrapper from '@erxes//ui/src/layout/components/Wrapper';
-import { SidebarCounter, SidebarList } from '@erxes//ui/src/layout/styles';
-import { SidebarListItem } from '@erxes/ui-settings/src/styles';
-import React from 'react';
-import { Link } from 'react-router-dom';
+import { __ } from "@erxes/ui/src/utils";
+import Sidebar from "@erxes//ui/src/layout/components/Sidebar";
+import Wrapper from "@erxes//ui/src/layout/components/Wrapper";
+import { SidebarCounter, SidebarList } from "@erxes//ui/src/layout/styles";
+import { SidebarListItem } from "@erxes/ui-settings/src/styles";
+import React from "react";
+import { Link } from "react-router-dom";
 
-import TagFilter from '../../containers/TagFilter';
-import ProductTypeFilter from '../product/filters/ProdcutTypeFilter';
+import TagFilter from "../../containers/TagFilter";
+import ProductTypeFilter from "../product/filters/ProdcutTypeFilter";
 import { isEnabled } from "@erxes/ui/src/utils/core";
 
 const TEMPLATE_TYPES = {
-  email_templates: 'Emails',
-  response_templates: 'Chat Responses',
-  growth_hacking: 'Growth Hacking',
-  template: 'Products & Services',
-  segments: 'Segments',
-  sales_pipeline: 'Sales Pipeline',
-  automation: 'Automation'
+  email_templates: "Emails",
+  response_templates: "Chat Responses",
+  growth_hacking: "Growth Hacking",
+  template: "Products & Services",
 };
 
 const { Section } = Wrapper.Sidebar;
@@ -31,18 +28,18 @@ interface IProps {
 }
 
 const coming_soon = {
-  margin: '10px',
-  paddingLeft: '10px'
+  margin: "10px",
+  paddingLeft: "10px",
 };
 
 const coming_soon1 = {
-  margin: '10px',
-  color: 'brown'
+  margin: "10px",
+  color: "brown",
 };
 class List extends React.Component<IProps> {
   isActive = (id: string) => {
     const { queryParams } = this.props;
-    const currentGroup = queryParams ? queryParams.type : '';
+    const currentGroup = queryParams ? queryParams.type : "";
 
     return currentGroup === id;
   };
@@ -53,24 +50,15 @@ class List extends React.Component<IProps> {
 
     for (const key of Object.keys(TEMPLATE_TYPES)) {
       const name = TEMPLATE_TYPES[key];
-      const condition =
-        key === 'segments' || key === 'sales_pipeline' || key === 'automation';
-      const count = condition ? (
-        <p style={coming_soon1}>Coming soon</p>
-      ) : (
-        types[key] || 0
-      );
+
+      const count = types[key] || 0;
       const subroute =
-        key === 'response_templates'
-          ? 'response-templates'
-          : key === 'growth_hacking'
-          ? 'boards/growthHackTemplate'
+        key === "response_templates"
+          ? "response-templates"
+          : key === "growth_hacking"
+          ? "boards/growthHackTemplate"
           : key;
-      const link = condition ? (
-        <p style={coming_soon}> {name} </p>
-      ) : (
-        <Link to={`/settings/${subroute}?type=${key}`}>{name}</Link>
-      );
+      const link = <Link to={`/settings/${subroute}?type=${key}`}>{name}</Link>;
 
       result.push(
         <SidebarListItem key={key} isActive={this.isActive(key)}>
@@ -86,7 +74,7 @@ class List extends React.Component<IProps> {
   renderCategoryHeader() {
     return (
       <>
-        <Section.Title>{__('Types')}</Section.Title>
+        <Section.Title>{__("Types")}</Section.Title>
       </>
     );
   }
