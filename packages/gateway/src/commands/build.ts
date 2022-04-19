180bca39d4aaac3fc074191483374e96da5065f7
BatAmar Battulga <mrbatamar@gmail.com>
2022-04-19 18:35:34 +0800

    trigger

--- packages/gateway/src/commands/build.ts
+++ packages/gateway/src/commands/build.ts
@@ -112,6 +112,11 @@ const main = async () => {
   } else {
     await execute(() => fse.move("../node_modules", "./dist/node_modules"));
   }
+
+  if (type === "core") {
+    console.log("Moving private folders ....");
+    await execute(() => fse.move("./src/private", "./dist/core/src/private"));
+  }
 };
 
 main()
