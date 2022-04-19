c0d873ca31de15158ebed7956cd792a41d6ad0c9
munkhsaikhan.g@nmma.co <munkhsaikhan.g@nmma.co>
2022-04-19 19:02:26 +0800

    loyalties ui, some code delete

--- packages/plugin-loyalties-ui/src/configs/general/components/Sidebar.tsx
+++ packages/plugin-loyalties-ui/src/configs/general/components/Sidebar.tsx
@@ -1,6 +1,4 @@
 import { __ } from '@erxes/ui/src/utils';
-import { Button } from '@erxes/ui/src/components';
-import {  MainStyleTopHeader as TopHeader } from '@erxes/ui/src/styles/eindex';
 import { Sidebar as LeftSidebar, SidebarList as List } from '@erxes/ui/src/layout';
 import React from 'react';
 import { Link } from 'react-router-dom';
