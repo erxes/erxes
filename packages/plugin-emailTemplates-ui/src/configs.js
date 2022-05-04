f05b84fd151e35ba067e64ed41b8185a2cddc90a
BatAmar Battulga <mrbatamar@gmail.com>
2022-05-04 15:39:16 +0800

    rename email templates

--- packages/plugin-emailTemplates-ui/src/configs.js
+++ packages/plugin-emailTemplates-ui/src/configs.js
@@ -1,12 +1,12 @@
 module.exports = {
-  name: "emailTemplates",
+  name: "emailtemplates",
   port: 3020,
   exposes: {
     "./routes": "./src/routes.tsx",
   },
   routes: {
     url: "http://localhost:3020/remoteEntry.js",
-    scope: "emailTemplates",
+    scope: "emailtemplates",
     module: "./routes",
   },
   menus: [
@@ -15,7 +15,7 @@ module.exports = {
       to: "/settings/email-templates",
       image: "/images/icons/erxes-09.svg",
       location: "settings",
-      scope: "emailTemplates",
+      scope: "emailtemplates",
       action: "",
       permissions: [],
     },
