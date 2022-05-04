ea035b4e160b3400972b8e81b47e9422b5b3273a
Ujin's MacBook <macintosh@Macintoshs-MacBook-Pro.local>
2022-05-04 20:39:36 +0800

    update

--- packages/plugin-contacts-ui/src/customers/containers/CustomersList.tsx
+++ packages/plugin-contacts-ui/src/customers/containers/CustomersList.tsx
@@ -25,6 +25,7 @@ import {
   ChangeStatusMutationResponse,
   ChangeStatusMutationVariables,
 } from "@erxes/ui-contacts/src/customers/types";
+import { isEnabled } from "@erxes/ui/src/utils/core";
 
 type Props = {
   queryParams: any;
@@ -280,22 +281,27 @@ const getRefetchQueries = (queryParams?: any, type?: string) => {
     {
       query: gql(queries.customerCounts),
       variables: { type, only: "byTag" },
+      skip: !isEnabled("tags") ? true : false,
     },
     {
       query: gql(queries.customerCounts),
       variables: { type, only: "byForm" },
+      skip: !isEnabled("inbox") ? true : false,
     },
     {
       query: gql(queries.customerCounts),
       variables: { type, only: "byIntegrationType" },
+      skip: !isEnabled("inbox") ? true : false,
     },
     {
       query: gql(queries.customerCounts),
       variables: { type, only: "byLeadStatus" },
+      skip: !isEnabled("inbox") ? true : false,
     },
     {
       query: gql(queries.customerCounts),
       variables: { type, only: "bySegment" },
+      skip: !isEnabled("segments") ? true : false,
     },
     {
       query: gql(queries.customerCounts),
