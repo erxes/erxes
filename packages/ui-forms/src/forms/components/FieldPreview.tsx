0a65774e2b707b856838bbaca6f93f8107db0e7b
soyombo <40427263+soyombo-baterdene@users.noreply.github.com>
2022-05-12 03:06:01 +0800

    property type: objectlist (#3301)
    * add objectlist

--- packages/ui-forms/src/forms/components/FieldPreview.tsx
+++ packages/ui-forms/src/forms/components/FieldPreview.tsx
@@ -1,18 +1,17 @@
-import { IConfig } from '@erxes/ui-settings/src/general/types';
 import GenerateField from '@erxes/ui-settings/src/properties/components/GenerateField';
-import { IField } from '@erxes/ui/src/types';
+import { IField, ILocationOption } from '@erxes/ui/src/types';
 import React from 'react';
 import { FieldItem } from '../styles';
 
 type Props = {
   field: IField;
-  configs: IConfig[];
   onClick?: (field: IField) => void;
+  onChangeLocationOptions?: (locationOptions: ILocationOption[]) => void;
 };
 
 class FieldPreview extends React.Component<Props, {}> {
   render() {
-    const { field, configs, onClick } = this.props;
+    const { field, onClick, onChangeLocationOptions } = this.props;
     const hasLogic = field.logics ? field.logics.length > 0 : false;
 
     const onClickItem = () => {
@@ -27,7 +26,13 @@ class FieldPreview extends React.Component<Props, {}> {
         selectType={field.type === 'select' || field.type === 'multiSelect'}
         onClick={onClickItem}
       >
-        <GenerateField field={field} hasLogic={hasLogic} configs={configs} />
+        <GenerateField
+          field={field}
+          hasLogic={hasLogic}
+          currentLocation={{ lat: 0, lng: 0 }}
+          isPreview={true}
+          onChangeLocationOptions={onChangeLocationOptions}
+        />
       </FieldItem>
     );
   }
