691b57c56d4ab084efdd835175360e994e520342
enkhtuvshin <amex@AMEXs-Mac-mini.local>
2022-05-11 09:38:30 +0800

    upd

--- packages/core/src/commands/migrateSegment.ts
+++ packages/core/src/commands/migrateSegment.ts
@@ -22,34 +22,44 @@ const command = async () => {
 
   SegmentsCollections = db.collection('segments');
 
-  const segments = await SegmentsCollections.find({
+  const segments1 = await SegmentsCollections.find({
     name: { $exists: true }
   }).toArray();
 
-  for (const segment of segments) {
-    if (segment.conditions && segment.conditions.length > 0) {
-      const subSegmentId = segment.conditions[0].subSegmentId;
-
-      const doc = {
-        conditionsConjunction: segment.conditionsConjunction,
-        scopeBrandIds: segment.scopeBrandIds,
-        contentType: segment.contentType,
-        name: segment.name,
-        description: segment.description,
-        subOf: segment.subOf,
-        color: segment.color,
-        boardId: segment.boardId,
-        pipelineId: segment.pipelineId
-      };
+  for (const segment1 of segments1) {
+    if (segment1.conditions && segment1.conditions.length > 0) {
+      const subSegmentId = segment1.conditions[0].subSegmentId;
 
       await SegmentsCollections.updateMany(
         {
-          subOf: segment._id
+          subOf: segment1._id
         },
         { $set: { subOf: subSegmentId } }
       );
+    }
+  }
 
-      await SegmentsCollections.remove({ _id: segment._id });
+  const segments2 = await SegmentsCollections.find({
+    name: { $exists: true }
+  }).toArray();
+
+  for (const segment2 of segments2) {
+    if (segment2.conditions && segment2.conditions.length > 0) {
+      const subSegmentId = segment2.conditions[0].subSegmentId;
+
+      const doc = {
+        conditionsConjunction: segment2.conditionsConjunction,
+        scopeBrandIds: segment2.scopeBrandIds,
+        contentType: segment2.contentType,
+        name: segment2.name,
+        description: segment2.description,
+        subOf: segment2.subOf,
+        color: segment2.color,
+        boardId: segment2.boardId,
+        pipelineId: segment2.pipelineId
+      };
+
+      await SegmentsCollections.remove({ _id: segment2._id });
 
       await SegmentsCollections.updateOne({ _id: subSegmentId }, { $set: doc });
     }
