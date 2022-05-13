72585138dc812a28ed0635ae742b28d6e0213c5a
BatAmar Battulga <mrbatamar@gmail.com>
2022-05-13 15:41:24 +0800

    build test

--- packages/plugin-emailtemplates-api/src/configs.ts
+++ packages/plugin-emailtemplates-api/src/configs.ts
@@ -1,6 +1,6 @@
 import typeDefs from './graphql/typeDefs';
 import resolvers from './graphql/resolvers';
-import { generateModels, test } from './connectionResolver';
+import { generateModels } from './connectionResolver';
 
 import { initBroker } from './messageBroker';
 import logs from './logUtils';
