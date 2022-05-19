61150332fa329fb4a1ed767573faddb311c348a1
BatAmar Battulga <mrbatamar@gmail.com>
2022-05-19 23:14:47 +0800

    updated forms essyncer

--- packages/plugin-forms-api/src/essyncer.js
+++ packages/plugin-forms-api/src/essyncer.js
@@ -1,5 +1,20 @@
 module.exports = [
   {
+    name: "forms",
+    schema: "{}",
+    script: "",
+  },
+  {
+    name: "fields",
+    schema: "{}",
+    script: "",
+  },
+  {
+    name: "fields_groups",
+    schema: "{}",
+    script: "",
+  },
+  {
     name: "form_submissions",
     schema: "{}",
     script: "",
