7afc6909526cde500fa027cadedd5e5438fc6edb
Ariukasq1 <Ariuka.sq@gmail.com>
2022-05-20 19:35:49 +0800

    pos plugin update

--- packages/plugin-pos-api/src/graphql/resolvers/mutations/index.ts
+++ packages/plugin-pos-api/src/graphql/resolvers/mutations/index.ts
@@ -1,6 +1,5 @@
 import Pos from './pos';
 import Order from './orders';
 import Payment from './payments';
-import PosUsers from './posUsers';
 
-export default { ...Pos, ...Order, ...Payment, ...PosUsers };
+export default { ...Pos, ...Order, ...Payment };
