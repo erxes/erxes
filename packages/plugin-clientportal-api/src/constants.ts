3ab1f8034384c312b5a74b15efc1608791b9c391
soyombo <soyombo.b@nmtec.co>
2022-05-24 16:03:06 +0800

    Delete constants.ts

--- packages/plugin-clientportal-api/src/constants.ts
+++
@@ -1,9 +0,0 @@
-export const AUTH_MESSAGES = {
-  API_ENDPOINT_NOT_FOUND_ERR: 'Api endpoint does not found',
-  SERVER_ERR: 'Something went wrong',
-  AUTH_HEADER_MISSING_ERR: 'auth header is missing',
-  AUTH_TOKEN_MISSING_ERR: 'auth token is missing',
-  JWT_DECODE_ERR: 'incorrect token',
-  USER_NOT_FOUND_ERR: 'User not found',
-  ACCESS_DENIED_ERR: 'Access deny for user'
-};
