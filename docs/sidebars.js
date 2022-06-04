46d32cdb6d28bf17a17423a1ab614d62a5878b16
BatAmar Battulga <mrbatamar@gmail.com>
2022-06-04 10:05:11 +0800

    added plugin docs

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -46,6 +46,7 @@ module.exports = {
       label: "Developer's Guide",
       items: [
         "developer/developer",
+        "developer/plugin",
         "developer/documentation_guide",
         {
           "Administrator's Guide": [
