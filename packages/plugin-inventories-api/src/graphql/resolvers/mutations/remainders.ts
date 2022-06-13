cb6c30d3ff62afab2fdac3d6cbc30c8265d0f621
munkhsaikhan.g@nmma.co <munkhsaikhan.g@nmma.co>
2022-06-13 14:16:05 +0800

    up

--- packages/plugin-inventories-api/src/graphql/resolvers/mutations/remainders.ts
+++ packages/plugin-inventories-api/src/graphql/resolvers/mutations/remainders.ts
@@ -1,10 +1,8 @@
 import { checkPermission } from '@erxes/api-utils/src/permissions';
 import { IContext } from '../../../connectionResolver';
-import { sendProductsMessage } from '../../../messageBroker';
-import { ISafeRemItemDocument } from '../../../models/definitions/safeRemainders';
-import { IRemainderDocument } from '../../../models/definitions/remainders';
+import { updateLiveRemainder } from './utils';
 
-interface IUpdateRemaindersParams {
+export interface IUpdateRemaindersParams {
   productCategoryId?: string;
   productIds?: string[];
   departmentId: string;
@@ -15,107 +13,9 @@ const remainderMutations = {
   async updateRemainders(
     _root,
     params: IUpdateRemaindersParams,
-    { models, subdomain, user }: IContext
+    { models, subdomain }: IContext
   ) {
-    const { productCategoryId, productIds, departmentId, branchId } = params;
-    const selector: any = {};
-    let allProductIds: string[] = [];
-
-    if (departmentId) {
-      selector.departmentId = departmentId;
-    }
-
-    if (branchId) {
-      selector.branchId = branchId;
-    }
-
-    if (productCategoryId) {
-      const products = await sendProductsMessage({
-        subdomain,
-        action: 'find',
-        data: {
-          query: {},
-          categoryId: productCategoryId
-        },
-        isRPC: true
-      });
-
-      const pIds = products.map(p => p._id);
-      selector.productId = { $in: pIds };
-      allProductIds = allProductIds.concat(pIds);
-    }
-
-    if (productIds) {
-      selector.productId = { $in: productIds };
-      allProductIds = allProductIds.concat(productIds);
-    }
-
-    const safeRems = await models.SafeRemItems.find(selector).lean();
-
-    const remainders: IRemainderDocument[] = await models.Remainders.find(
-      selector
-    ).lean();
-
-    const resultRems: IRemainderDocument[] = [];
-    for (const prodId of allProductIds) {
-      let safe: ISafeRemItemDocument | undefined = undefined;
-      const safed = safeRems.filter(
-        s =>
-          s.productId === prodId &&
-          s.departmentId === departmentId &&
-          s.branchId === branchId
-      );
-      if (safed.length) {
-        safe = safed[0];
-      }
-
-      const trSelector: any = {
-        departmentId,
-        branchId,
-        productId: prodId
-      };
-
-      if (safe && safe.lastTrDate) {
-        trSelector.date = { $gt: safe.lastTrDate };
-      }
-
-      const trs = await models.TrItems.find(trSelector).lean();
-      let remCount = safe ? safe.count : 0;
-
-      for (const tr of trs) {
-        remCount += tr.isDebit ? tr.count : -1 * tr.count;
-      }
-
-      const realRem = remainders.find(
-        s =>
-          s.productId === prodId &&
-          s.departmentId === departmentId &&
-          s.branchId === branchId
-      );
-      if (realRem && realRem._id) {
-        if (realRem.count === remCount) {
-          resultRems.push(realRem);
-        } else {
-          models.Remainders.updateOne(
-            { _id: realRem._id },
-            { $set: { count: remCount } }
-          );
-          resultRems.push(
-            await models.Remainders.getRemainderObject(realRem._id)
-          );
-        }
-      } else {
-        models.Remainders.create({
-          productId: prodId,
-          departmentId,
-          branchId,
-          count: remCount
-          // quantity:
-          // uomId:
-        });
-      }
-    }
-    return resultRems;
+    return updateLiveRemainder({ subdomain, ...params });
   }
 };
 
--- packages/plugin-inventories-api/src/graphql/resolvers/mutations/safeRemainders.ts
+++ packages/plugin-inventories-api/src/graphql/resolvers/mutations/safeRemainders.ts
@@ -6,6 +6,7 @@ import {
   ISafeRemItemDocument
 } from '../../../models/definitions/safeRemainders';
 import { SAFE_REMAINDER_STATUSES } from '../../../models/definitions/constants';
+import { updateLiveRemainder } from './utils';
 
 const remainderMutations = {
   async createSafeRemainder(
@@ -20,6 +21,12 @@ const remainderMutations = {
       departmentId,
       branchId
     } = params;
+    await updateLiveRemainder({
+      subdomain,
+      departmentId,
+      branchId,
+      productCategoryId
+    });
 
     const safeRemainder = await models.SafeRemainders.createRemainder({
       date,
@@ -68,13 +75,16 @@ const remainderMutations = {
     const bulkOps: any[] = [];
 
     for (const product of products) {
+      const live = liveRemainders.find(l => l.productId === product._id) || 0;
+
       bulkOps.push({
         modifiedAt: now,
         lastTrDate: now,
         remainderId: safeRemainder._id,
         productId: product._id,
         uomId: product.uomId || defaultUomId,
-        count: liveRemainders.find(l => l.productId === product._id) || 0,
+        preCount: live,
+        count: live,
         branchId: safeRemainder.branchId,
         departmentId: safeRemainder.departmentId
       });
