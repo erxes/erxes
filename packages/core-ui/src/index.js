0124581c88878fc9e9f599a05a34b4f345c80469
BatAmar Battulga <mrbatamar@gmail.com>
2022-06-14 18:01:55 +0800

    PUBLIC_PATH fix

--- packages/core-ui/src/index.js
+++ packages/core-ui/src/index.js
@@ -1,7 +1,5 @@
 const { REACT_APP_PUBLIC_PATH } = window.env || {};
 
-console.log(REACT_APP_PUBLIC_PATH);
-
 if (REACT_APP_PUBLIC_PATH) {
   __webpack_public_path__ = `${REACT_APP_PUBLIC_PATH}/`;
 }
