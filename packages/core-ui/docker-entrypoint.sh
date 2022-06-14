14bddff2824cdd576c2d26ebe004212c843ec398
BatAmar Battulga <mrbatamar@gmail.com>
2022-06-14 18:46:19 +0800

    PUBLIC_PATH fix

--- packages/core-ui/docker-entrypoint.sh
+++ packages/core-ui/docker-entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo "window.env = `jo \`env | grep REACT_APP_\``" > /usr/share/nginx/html/js/env.js
 sed -i 's/${NGINX_HOST}/'"$NGINX_HOST"'/' /etc/nginx/conf.d/default.conf
-sed -i 's/%REACT_APP_PUBLIC_PATH%/'"$REACT_APP_PUBLIC_PATH"'/' /etc/nginx/html/index.html
+sed -i 's/%REACT_APP_PUBLIC_PATH%/'"$REACT_APP_PUBLIC_PATH"'/g' /usr/share/nginx/html/index.html
 exec "$@"
