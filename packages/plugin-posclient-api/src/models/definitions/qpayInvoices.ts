ee1069e3636fecb28b165de77e666928d4b4fc67
Ariukasq1 <Ariuka.sq@gmail.com>
2022-06-15 14:03:34 +0800

    adding field

--- packages/plugin-posclient-api/src/models/definitions/qpayInvoices.ts
+++ packages/plugin-posclient-api/src/models/definitions/qpayInvoices.ts
@@ -22,10 +22,10 @@ export interface IQPayInvoice {
 }
 
 const qpayUrlSchema = new Schema({
-  name: { type: String, label: 'Bank name' },
-  description: { type: String, label: 'Bank description' },
-  logo: { type: String, label: 'Bank logo' },
-  link: { type: String, label: 'Bank payment link' }
+  name: field({ type: String, label: 'Bank name' }),
+  description: field({ type: String, label: 'Bank description' }),
+  logo: field({ type: String, label: 'Bank logo' }),
+  link: field({ type: String, label: 'Bank payment link' })
 });
 
 export interface IQpayInvoiceDocument extends IQPayInvoice, Document {
@@ -34,20 +34,28 @@ export interface IQpayInvoiceDocument extends IQPayInvoice, Document {
 
 export const qpayInvoiceSchema = new Schema({
   _id: field({ pkey: true }),
-  senderInvoiceNo: {
+  senderInvoiceNo: field({
     type: String,
     optional: true,
     label: 'Order id'
-  },
-  amount: { type: String, optional: true, label: 'Amount' },
-  qpayInvoiceId: { type: String, optional: true, label: 'QPay invoice id' },
-  qrText: { type: String, optional: true, label: 'QR text' },
-  qpayPaymentId: { type: String, optional: true, label: 'QPay payment id' },
-  status: { type: String, default: 'open', label: 'Invoice status' },
-  paymentDate: {
+  }),
+  amount: field({ type: String, optional: true, label: 'Amount' }),
+  qpayInvoiceId: field({
+    type: String,
+    optional: true,
+    label: 'QPay invoice id'
+  }),
+  qrText: field({ type: String, optional: true, label: 'QR text' }),
+  qpayPaymentId: field({
+    type: String,
+    optional: true,
+    label: 'QPay payment id'
+  }),
+  status: field({ type: String, default: 'open', label: 'Invoice status' }),
+  paymentDate: field({
     type: Date,
     label: 'Updated Date for Qpay payment'
-  },
-  urls: { type: qpayUrlSchema, label: 'QPay urls' },
+  }),
+  urls: field({ type: qpayUrlSchema, label: 'QPay urls' }),
   createdAt: getDateFieldDefinition('Created at')
 });
