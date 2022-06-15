16727e72f3943980d744a4b1ebdbab32b6e2180d
mambatukaa <sbatuka9@gmail.com>
2022-06-15 12:37:52 +0800

    add ui-automations and commons

---
+++ packages/ui-automations/src/styles.ts
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+import { dimensions, colors } from '@erxes/ui/src/styles';
+
+// ! main
+export const ScrolledContent = styled.div`
+  flex: 1;
+  overflow: auto;
+`;
+
+export const HeaderContent = styled.div`
+  flex: 1;
+  textarea {
+    border-bottom: none;
+    min-height: auto;
+    padding: 5px 0;
+    &:focus {
+      border-bottom: 1px solid ${colors.colorSecondary};
+    }
+  }
+`;
+
+export const BoardHeader = styled(HeaderContent)`
+  .header-row {
+    display: flex;
+    justify-content: space-between;
+
+    > div > span {
+      color: ${colors.colorSecondary};
+      font-weight: 500;
+      cursor: pointer;
+      margin-left: ${dimensions.unitSpacing}px;
+    }
+  }
+`;
