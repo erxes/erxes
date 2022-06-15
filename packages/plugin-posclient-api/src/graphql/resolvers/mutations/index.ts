a2829d6b0ba1de4ea72ab35680e666955adfd90e
Ariukasq1 <Ariuka.sq@gmail.com>
2022-06-15 15:58:47 +0800

    update

--- packages/plugin-posclient-api/src/graphql/resolvers/mutations/index.ts
+++ packages/plugin-posclient-api/src/graphql/resolvers/mutations/index.ts
@@ -1,4 +1,17 @@
 import Order from './orders';
 import Payment from './payments';
+import Configs from './configs';
+import Customers from './customers';
+import PosUser from './posUsers';
+import User from './users';
+import ErxesMutation from './erxesMutations';
 
-export default { ...Order, ...Payment };
+export default {
+  ...Order,
+  ...Payment,
+  ...Configs,
+  ...Customers,
+  ...PosUser,
+  ...User,
+  ...ErxesMutation
+};
--- packages/plugin-posclient-api/src/graphql/resolvers/queries/index.ts
+++ packages/plugin-posclient-api/src/graphql/resolvers/queries/index.ts
@@ -1,5 +1,23 @@
-import Orders from './orders';
+import Order from './orders';
 import Payment from './payments';
-import Products from './products';
+import Configs from './configs';
+import Customers from './customers';
+import PosUser from './posUsers';
+import ErxesQuery from './erxesQueries';
+import Logs from './logs';
+import Product from './products';
+import Report from './report';
+import User from './user';
 
-export default { ...Orders, ...Payment, ...Products };
+export default {
+  ...Order,
+  ...Payment,
+  ...Configs,
+  ...Customers,
+  ...PosUser,
+  ...ErxesQuery,
+  ...Logs,
+  ...Product,
+  ...Report,
+  ...User
+};
