f6bf4b9556f4ec8ed8a83a7bccf574cb0798467c
mambatukaa <sbatuka9@gmail.com>
2022-06-27 12:26:30 +0800

    little fix

--- packages/plugin-notifications-api/src/graphql/resolvers/notification.ts
+++ packages/plugin-notifications-api/src/graphql/resolvers/notification.ts
@@ -2,9 +2,11 @@ import { INotificationDocument } from '../../models/definitions/notifications';
 
 export default {
   createdUser(notif: INotificationDocument) {
-    return {
-      __typename: 'User',
-      _id: notif.createdUser
-    }
-  },
+    return (
+      notif.createdUser && {
+        __typename: 'User',
+        _id: notif.createdUser
+      }
+    );
+  }
 };
