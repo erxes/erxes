7ec179b32743d04eb55825c5ae4e3a4dfa4f551e
Wlkr123 <104069564+Wlkr123@users.noreply.github.com>
2022-06-28 15:17:18 +0800

    updates

--- packages/plugin-loyalties-api/src/graphql/resolvers/mutations/index.ts
+++ packages/plugin-loyalties-api/src/graphql/resolvers/mutations/index.ts
@@ -3,12 +3,12 @@ import DonateCampaigns from './donateCampaigns';
 import Donates from './donates';
 import Lotteries from './lotteries';
 import LotteryCampaigns from './lotteryCampaigns';
-import Loyalties from './loyalties';
-import ScoreLogs from './scoreLogs';
 import SpinCampaigns from './spinCampaigns';
 import Spins from './spins';
 import VoucherCampaigns from './voucherCampaigns';
 import Vouchers from './vouchers';
+import Loyalties from './loyalties';
+import ScoreLogs from './scoreLogs';
 
 export {
   Configs,
