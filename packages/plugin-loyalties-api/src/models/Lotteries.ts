4433baf61dabe083ae78e5b0f72366b54d375dc1
Wlkr123 <104069564+Wlkr123@users.noreply.github.com>
2022-06-28 17:10:26 +0800

    updates

--- packages/plugin-loyalties-api/src/models/Lotteries.ts
+++ packages/plugin-loyalties-api/src/models/Lotteries.ts
@@ -2,11 +2,7 @@ import { Model } from 'mongoose';
 import { IModels } from '../connectionResolver';
 import { IBuyParams } from './definitions/common';
 import { LOTTERY_STATUS } from './definitions/constants';
-import {
-  ILottery,
-  ILotteryDocument,
-  lotterySchema
-} from './definitions/lotteries';
+import { ILottery, ILotteryDocument, lotterySchema } from './definitions/lotteries';
 import { getRandomNumber } from './utils';
 
 export interface ILotteryModel extends Model<ILotteryDocument> {
@@ -23,45 +19,28 @@ export const loadLotteryClass = (models: IModels, subdomain: string) => {
       const lottery = await models.Lotteries.findOne({ _id });
 
       if (!lottery) {
-        throw new Error('not found lottery rule');
+        throw new Error('not found lottery rule')
       }
 
       return lottery;
     }
 
     public static async createLottery(doc: ILottery) {
-      const {
-        campaignId,
-        ownerType,
-        ownerId,
-        voucherCampaignId = '',
-        userId = ''
-      } = doc;
+      const { campaignId, ownerType, ownerId, voucherCampaignId = '', userId = '' } = doc;
       if (!ownerId || !ownerType) {
         throw new Error('Not create lottery, owner is undefined');
       }
 
-      const lotteryCampaign = await models.LotteryCampaigns.getLotteryCampaign(
-        campaignId
-      );
+      const lotteryCampaign = await models.LotteryCampaigns.getLotteryCampaign(campaignId);
 
       const now = new Date();
 
-      // if (lotteryCampaign.startDate > now || lotteryCampaign.endDate < now) {
-      //   throw new Error('Not create lottery, expired');
-      // }
+      if (lotteryCampaign.startDate > now || lotteryCampaign.endDate < now) {
+        throw new Error('Not create lottery, expired');
+      }
 
       const number = getRandomNumber(lotteryCampaign.numberFormat);
-      return await models.Lotteries.create({
-        campaignId,
-        ownerType,
-        ownerId,
-        createdAt: now,
-        number,
-        status: LOTTERY_STATUS.NEW,
-        voucherCampaignId,
-        userId
-      });
+      return await models.Lotteries.create({ campaignId, ownerType, ownerId, createdAt: now, number, status: LOTTERY_STATUS.NEW, voucherCampaignId, userId });
     }
 
     public static async updateLottery(_id: string, doc: ILottery) {
@@ -77,16 +56,9 @@ export const loadLotteryClass = (models: IModels, subdomain: string) => {
 
       const now = new Date();
 
-      return await models.Lotteries.updateOne(
-        { _id },
-        {
+      return await models.Lotteries.updateOne({ _id }, {
           $set: {
-            campaignId,
-            ownerType,
-            ownerId,
-            modifiedAt: now,
-            status,
-            userId
+            campaignId, ownerType, ownerId, modifiedAt: now, status, userId
           }
         }
       );
@@ -99,26 +71,19 @@ export const loadLotteryClass = (models: IModels, subdomain: string) => {
         throw new Error('can not buy lottery, owner is undefined');
       }
 
-      const lotteryCampaign = await models.LotteryCampaigns.getLotteryCampaign(
-        campaignId
-      );
+      const lotteryCampaign = await models.LotteryCampaigns.getLotteryCampaign(campaignId);
 
       if (!lotteryCampaign.buyScore) {
         throw new Error('can not buy this lottery');
       }
 
-      await models.ScoreLogs.changeScore({
-        ownerType,
-        ownerId,
-        changeScore: -1 * lotteryCampaign.buyScore * count,
-        description: 'buy lottery'
-      });
+      await models.ScoreLogs.changeScore({ ownerType, ownerId, changeScore: -1 * lotteryCampaign.buyScore * count, description: 'buy lottery'});
 
       return models.Lotteries.createLottery({ campaignId, ownerType, ownerId });
     }
 
     public static async removeLotteries(_ids: string[]) {
-      return models.Lotteries.deleteMany({ _id: { $in: _ids } });
+      return models.Lotteries.deleteMany({ _id: { $in: _ids } })
     }
   }
 
