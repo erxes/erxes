f3ccef8c78bec25ddfedcdd7a0794e05336afbfa
Wlkr123 <104069564+Wlkr123@users.noreply.github.com>
2022-06-28 16:33:07 +0800

    updates

--- packages/plugin-loyalties-api/src/graphql/resolvers/mutations/lotteryCampaigns.ts
+++ packages/plugin-loyalties-api/src/graphql/resolvers/mutations/lotteryCampaigns.ts
@@ -4,25 +4,25 @@ import { ILotteryCampaign } from '../../../models/definitions/lotteryCampaigns';
 
 const lotteriesMutations = {
   async lotteryCampaignsAdd(_root, doc: ILotteryCampaign, { models }: IContext) {
-    return models.LotteryCampaigns.createLotteryCampaign(doc);
+    return models.LotteryCampaigns.createLotteryCampaign(doc)
   },
 
   async lotteryCampaignsEdit(_root, { _id, ...doc }: ILotteryCampaign & { _id: string }, { models }: IContext) {
-    return models.LotteryCampaigns.updateLotteryCampaign(_id, doc);
+    return models.LotteryCampaigns.updateLotteryCampaign(_id, doc)
   },
 
   async lotteryCampaignsRemove(_root, { _ids }: { _ids: string[] }, { models }: IContext) {
-    return models.LotteryCampaigns.removeLotteryCampaigns(_ids);
+    return models.LotteryCampaigns.removeLotteryCampaigns(_ids)
   },
 
   async doLottery(_root, params: { campaignId: string; awardId: string }, { models }: IContext) {
-    return models.LotteryCampaigns.doLottery(params);
+    return models.LotteryCampaigns.doLottery(params)
   },
   async doLotteryMultiple(_root, params: { campaignId: string; awardId: string; multiple: number }, { models }: IContext) {
-    return models.LotteryCampaigns.multipleDoLottery(params);
+    return models.LotteryCampaigns.multipleDoLottery(params)
   },
   async getNextChar(_root, params: { campaignId: string; awardId: string; prevChars: string }, { models }: IContext) {
-    return models.LotteryCampaigns.getNextChar(params);
+    return models.LotteryCampaigns.getNextChar(params)
   }
 };
 
