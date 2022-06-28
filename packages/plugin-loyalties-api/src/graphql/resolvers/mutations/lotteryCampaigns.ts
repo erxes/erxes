5ea9736df7308c286f2123cbbceef48fa6bbad95
Wlkr123 <104069564+Wlkr123@users.noreply.github.com>
2022-06-28 16:32:07 +0800

    updates

--- packages/plugin-loyalties-api/src/graphql/resolvers/mutations/lotteryCampaigns.ts
+++ packages/plugin-loyalties-api/src/graphql/resolvers/mutations/lotteryCampaigns.ts
@@ -3,25 +3,25 @@ import { IContext } from '../../../connectionResolver';
 import { ILotteryCampaign } from '../../../models/definitions/lotteryCampaigns';
 
 const lotteriesMutations = {
-  async lotteryCampaignsAdd(_root,doc: ILotteryCampaign,{ models }: IContext) {
+  async lotteryCampaignsAdd(_root, doc: ILotteryCampaign, { models }: IContext) {
     return models.LotteryCampaigns.createLotteryCampaign(doc);
   },
 
-  async lotteryCampaignsEdit(_root,{ _id, ...doc }: ILotteryCampaign & { _id: string },{ models }: IContext) {
+  async lotteryCampaignsEdit(_root, { _id, ...doc }: ILotteryCampaign & { _id: string }, { models }: IContext) {
     return models.LotteryCampaigns.updateLotteryCampaign(_id, doc);
   },
 
-  async lotteryCampaignsRemove(_root,{ _ids }: { _ids: string[] },{ models }: IContext) {
+  async lotteryCampaignsRemove(_root, { _ids }: { _ids: string[] }, { models }: IContext) {
     return models.LotteryCampaigns.removeLotteryCampaigns(_ids);
   },
 
-  async doLottery(_root,params: { campaignId: string; awardId: string },{ models }: IContext) {
+  async doLottery(_root, params: { campaignId: string; awardId: string }, { models }: IContext) {
     return models.LotteryCampaigns.doLottery(params);
   },
-  async doLotteryMultiple(_root,params: { campaignId: string; awardId: string; multiple: number },{ models }: IContext) {
+  async doLotteryMultiple(_root, params: { campaignId: string; awardId: string; multiple: number }, { models }: IContext) {
     return models.LotteryCampaigns.multipleDoLottery(params);
   },
-  async getNextChar(_root,params: { campaignId: string; awardId: string; prevChars: string },{ models }: IContext) {
+  async getNextChar(_root, params: { campaignId: string; awardId: string; prevChars: string }, { models }: IContext) {
     return models.LotteryCampaigns.getNextChar(params);
   }
 };
