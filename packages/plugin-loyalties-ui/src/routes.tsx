970391cc842dd9c4a46c86ace73ac6aa79628b80
Wlkr123 <104069564+Wlkr123@users.noreply.github.com>
2022-06-29 13:27:48 +0800

    updates

--- packages/plugin-loyalties-ui/src/routes.tsx
+++ packages/plugin-loyalties-ui/src/routes.tsx
@@ -1,8 +1,8 @@
-import queryString from 'query-string';
-import React from 'react';
-import Settings from './configs/general/containers/Settings';
-import { Route } from 'react-router-dom';
-import asyncComponent from '@erxes/ui/src/components/AsyncComponent';
+import queryString from "query-string";
+import React from "react";
+import Settings from "./configs/general/containers/Settings";
+import { Route } from "react-router-dom";
+import asyncComponent from "@erxes/ui/src/components/AsyncComponent";
 
 const VoucherCampaigns = asyncComponent(() =>
   import(/* webpackChunkName: "KnowledgeBase" */ './configs/voucherCampaign/containers/List')
