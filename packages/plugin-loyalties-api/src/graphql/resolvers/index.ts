88a7f024035fc7a14ba8d18d6221c4bfddc550bc
Wlkr123 <104069564+Wlkr123@users.noreply.github.com>
2022-06-29 14:04:19 +0800

    updates

--- packages/plugin-loyalties-api/src/graphql/resolvers/index.ts
+++ packages/plugin-loyalties-api/src/graphql/resolvers/index.ts
@@ -1,80 +1,80 @@
 import customScalars from '@erxes/api-utils/src/customScalars';
 import Donate from './customResolvers/donate';
 import DonateCampaign from './customResolvers/donateCampaign';
+import loyaltyConfigQueries from './queries/configs';
+import Voucher from './customResolvers/voucher';
+import VoucherCampaign from './customResolvers/voucherCampaign';
+import Spin from './customResolvers/spin';
+import SpinCampaign from './customResolvers/spinCampaign';
 import Lottery from './customResolvers/lottery';
 import LotteryCampaign from './customResolvers/lotteryCampaign';
 import ScoreLog from './customResolvers/scoreLog';
-import Spin from './customResolvers/spin';
-import SpinCampaign from './customResolvers/spinCampaign';
-import Voucher from './customResolvers/voucher';
-import VoucherCampaign from './customResolvers/voucherCampaign';
 import {
   Configs as LoyaltyConfigMutations,
-  DonateCampaigns as DonateCampaignMutations,
   Donates as DonateMutations,
+  DonateCampaigns as DonateCampaignMutations,
+  Vouchers as VoucherMutations,
+  VoucherCampaigns as VoucherCampaignMutations,
+  Spins as SpinMutations,
+  SpinCampaigns as SpinCampaignMutations,
   Lotteries as LotteryMutations,
   LotteryCampaigns as LotteryCampaignMutations,
   Loyalties as LoyaltyMutations,
   ScoreLogs as ScoreLogMutations,
-  SpinCampaigns as SpinCampaignMutations,
-  Spins as SpinMutations,
-  VoucherCampaigns as VoucherCampaignMutations,
-  Vouchers as VoucherMutations
 } from './mutations';
-import loyaltyConfigQueries from './queries/configs';
 
 import {
-  DonateCampaigns as DonateCampaignQueries,
   Donates as DonateQueries,
+  DonateCampaigns as DonateCampaignQueries,
+  Spins as SpinQueries,
+  SpinCampaigns as SpinCampaignQueries,
   Lotteries as LotteryQueries,
   LotteryCampaigns as LotteryCampaignQueries,
   Loyalties as LoyaltyQueries,
-  ScoreLogs as ScoreLogQueries,
-  SpinCampaigns as SpinCampaignQueries,
-  Spins as SpinQueries,
+  Vouchers as VoucherQueries,
   VoucherCampaigns as VoucherCampaignQueries,
-  Vouchers as VoucherQueries
+  ScoreLogs as ScoreLogQueries,
 } from './queries';
 
-const resolvers: any = async serviceDiscovery => (
-  
+const resolvers: any = async (serviceDiscovery) => (
+
   {
-  ...customScalars,
-  Donate,
-  DonateCampaign,
-  Voucher,
-  VoucherCampaign,
-  Spin,
-  SpinCampaign,
-  Lottery,
-  LotteryCampaign,
-  ScoreLog,
-  Mutation: {
-    ...LoyaltyConfigMutations,
-    ...DonateMutations,
-    ...DonateCampaignMutations,
-    ...VoucherMutations,
-    ...VoucherCampaignMutations,
-    ...SpinMutations,
-    ...SpinCampaignMutations,
-    ...LotteryMutations,
-    ...LotteryCampaignMutations,
-    ...LoyaltyMutations,
-    ...ScoreLogMutations
-  },
-  Query: {
-    ...loyaltyConfigQueries,
-    ...DonateQueries,
-    ...DonateCampaignQueries,
-    ...SpinQueries,
-    ...SpinCampaignQueries,
-    ...LotteryQueries,
-    ...LotteryCampaignQueries,
-    ...VoucherQueries,
-    ...VoucherCampaignQueries,
-    ...ScoreLogQueries,
-    ...LoyaltyQueries
-  }
-});
+    ...customScalars,
+    Donate,
+    DonateCampaign,
+    Voucher,
+    VoucherCampaign,
+    Spin,
+    SpinCampaign,
+    Lottery,
+    LotteryCampaign,
+    ScoreLog,
+    Mutation: {
+      ...LoyaltyConfigMutations,
+      ...DonateMutations,
+      ...DonateCampaignMutations,
+      ...VoucherMutations,
+      ...VoucherCampaignMutations,
+      ...SpinMutations,
+      ...SpinCampaignMutations,
+      ...LotteryMutations,
+      ...LotteryCampaignMutations,
+      ...LoyaltyMutations,
+      ...ScoreLogMutations
+    },
+    Query: {
+      ...loyaltyConfigQueries,
+      ...DonateQueries,
+      ...DonateCampaignQueries,
+      ...SpinQueries,
+      ...SpinCampaignQueries,
+      ...LotteryQueries,
+      ...LotteryCampaignQueries,
+      ...VoucherQueries,
+      ...VoucherCampaignQueries,
+      ...ScoreLogQueries,
+      ...LoyaltyQueries
+    }
+  });
 
 export default resolvers;
