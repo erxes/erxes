db08e363f261f153d4106f32c6050daf1544edbc
munkhsaikhan.g@nmma.co <munkhsaikhan.g@nmma.co>
2022-07-04 13:19:44 +0800

    little fix afterQuery on syncerkhet

--- packages/plugin-syncerkhet-api/src/afterQueries.ts
+++ packages/plugin-syncerkhet-api/src/afterQueries.ts
@@ -15,58 +15,59 @@ export const afterQueryHandlers = async (subdomain, data) => {
 
   const { pipelineId } = args;
 
-  if (pipelineId && (await isEnabled('syncerkhet'))) {
-    try {
-      const configs = await sendCoreMessage({
-        subdomain,
-        action: 'getConfig',
-        data: { code: 'ERKHET', defaultValue: {} },
-        isRPC: true
-      });
-
-      const remConfigs = await sendCoreMessage({
-        subdomain,
-        action: 'getConfig',
-        data: { code: 'remainderConfig', defaultValue: {} },
-        isRPC: true
-      });
-
-      if (!Object.keys(remConfigs).includes(pipelineId)) {
-        return results;
-      }
-
-      const remConfig = remConfigs[pipelineId];
-
-      const codes = (results || []).map(item => item.code);
-
-      const response = await sendRequest({
-        url: configs.getRemainderApiUrl,
-        method: 'GET',
-        params: {
-          kind: 'remainder',
-          api_key: configs.apiKey,
-          api_secret: configs.apiSecret,
-          check_relate: codes.length < 4 ? '1' : '',
-          accounts: remConfig.account,
-          locations: remConfig.location,
-          inventories: codes.join(',')
-        }
-      });
-
-      const jsonRes = JSON.parse(response);
-      let responseByCode = jsonRes;
-
-      if (remConfig.account && remConfig.location) {
-        responseByCode = jsonRes[remConfig.account][remConfig.location] || {};
-      }
-
-      for (const r of results) {
-        r.name = r.name.concat(` (${responseByCode[r.code] || '-0'})`);
-      }
-    } catch (e) {
-      console.log(e.message);
-      return results;
-    }
+  if (!pipelineId || !(await isEnabled('syncerkhet'))) {
     return results;
   }
+  try {
+    const configs = await sendCoreMessage({
+      subdomain,
+      action: 'getConfig',
+      data: { code: 'ERKHET', defaultValue: {} },
+      isRPC: true
+    });
+
+    const remConfigs = await sendCoreMessage({
+      subdomain,
+      action: 'getConfig',
+      data: { code: 'remainderConfig', defaultValue: {} },
+      isRPC: true
+    });
+
+    if (!Object.keys(remConfigs).includes(pipelineId)) {
+      return results;
+    }
+
+    const remConfig = remConfigs[pipelineId];
+
+    const codes = (results || []).map(item => item.code);
+
+    const response = await sendRequest({
+      url: configs.getRemainderApiUrl,
+      method: 'GET',
+      params: {
+        kind: 'remainder',
+        api_key: configs.apiKey,
+        api_secret: configs.apiSecret,
+        check_relate: codes.length < 4 ? '1' : '',
+        accounts: remConfig.account,
+        locations: remConfig.location,
+        inventories: codes.join(',')
+      }
+    });
+
+    const jsonRes = JSON.parse(response);
+    let responseByCode = jsonRes;
+
+    if (remConfig.account && remConfig.location) {
+      responseByCode = jsonRes[remConfig.account][remConfig.location] || {};
+    }
+
+    for (const r of results) {
+      r.name = r.name.concat(` (${responseByCode[r.code] || '-0'})`);
+    }
+  } catch (e) {
+    console.log(e.message);
+    return results;
+  }
+  return results;
 };
