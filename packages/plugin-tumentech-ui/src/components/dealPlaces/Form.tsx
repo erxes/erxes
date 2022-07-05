4322356e36afb2a77ce5a5b5015b6fa877003427
soyombo <soyombo.b@nmtec.co>
2022-07-05 18:44:45 +0800

    Update Form.tsx

--- packages/plugin-tumentech-ui/src/components/dealPlaces/Form.tsx
+++ packages/plugin-tumentech-ui/src/components/dealPlaces/Form.tsx
@@ -1,9 +1,8 @@
 import Button from '@erxes/ui/src/components/Button';
-import FormControl from '@erxes/ui/src/components/form/Control';
 import Form from '@erxes/ui/src/components/form/Form';
 import FormGroup from '@erxes/ui/src/components/form/Group';
 import ControlLabel from '@erxes/ui/src/components/form/Label';
-import { MapContainer, ModalFooter } from '@erxes/ui/src/styles/main';
+import { ModalFooter } from '@erxes/ui/src/styles/main';
 import { IButtonMutateProps, IFormProps } from '@erxes/ui/src/types';
 import { __ } from '@erxes/ui/src/utils/core';
 import React, { useState } from 'react';
