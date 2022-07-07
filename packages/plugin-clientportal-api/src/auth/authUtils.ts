39b2fced76afc5d7df961bfbe0592844f9d428d3
soyombo <soyombo.b@nmtec.co>
2022-07-07 12:29:54 +0800

    Update authUtils.ts

--- packages/plugin-clientportal-api/src/auth/authUtils.ts
+++ packages/plugin-clientportal-api/src/auth/authUtils.ts
@@ -1,5 +1,4 @@
 import * as jwt from 'jsonwebtoken';
-import { debugInfo } from '@erxes/api-utils/src/debuggers';
 
 export const createJwtToken = payload => {
   const token = jwt.sign(payload, process.env.JWT_TOKEN_SECRET || '', {
@@ -23,17 +22,4 @@ export const verifyJwtToken = token => {
   } catch (err) {
     throw new Error(err.message);
   }
-};
-
-export const authCookieOptions = (secure: boolean) => {
-  const oneDay = 1 * 24 * 3600 * 1000; // 1 day
-
-  const cookieOptions = {
-    httpOnly: true,
-    expires: new Date(Date.now() + oneDay),
-    maxAge: oneDay,
-    secure
-  };
-
-  return cookieOptions;
 };
