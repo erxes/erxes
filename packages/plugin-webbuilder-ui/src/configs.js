ac6975ad40c176d1369e7b9e14d607a4cb6d7d30
mambatukaa <sbatuka9@gmail.com>
2022-07-24 10:56:15 +0800

    refactor config

--- packages/plugin-webbuilder-ui/src/configs.js
+++ packages/plugin-webbuilder-ui/src/configs.js
@@ -1,33 +1,20 @@
 module.exports = {
   name: 'webbuilder',
-  port: 3012,
-  scope: 'webbuilder',
+  port: 3027,
   exposes: {
     './routes': './src/routes.tsx'
   },
   routes: {
-    url: 'http://localhost:3012/remoteEntry.js',
+    url: 'http://localhost:3027/remoteEntry.js',
     scope: 'webbuilder',
     module: './routes'
   },
   menus: [
     {
-      text: "Web builder",
-      url: "/webbuilder/pages",
-      icon: "icon-car",
-      location: "mainNavigation"
-    },
-    {
-      text: "Content type",
-      url: "/webbuilder/contenttypes",
-      icon: "icon-book",
-      location: "mainNavigation"
-    },
-    {
-      text: "Entry",
-      url: "/webbuilder/entries",
-      icon: "icon-home",
-      location: "mainNavigation"
+      text: 'Web builder',
+      url: '/webbuilder/pages',
+      icon: 'icon-car',
+      location: 'mainNavigation'
     }
-  ],
+  ]
 };
