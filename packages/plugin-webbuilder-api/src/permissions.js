a01b0fed40bdec20200aa0b245f48cb8f4911a6c
mambatukaa <sbatuka9@gmail.com>
2022-07-25 13:16:03 +0800

    add permission

---
+++ packages/plugin-webbuilder-api/src/permissions.js
@@ -0,0 +1,21 @@
+module.exports = {
+  chats: {
+    name: 'webbuilder',
+    description: 'Webbuilder',
+    actions: [
+      {
+        name: 'webbuilderAll',
+        description: 'All',
+        use: ['showWebbuilder', 'manageWebbuilder']
+      },
+      {
+        name: 'showWebbuilder',
+        description: 'Show webbuilder'
+      },
+      {
+        name: 'manageWebbuilder',
+        description: 'Manage webbuilder'
+      }
+    ]
+  }
+};
