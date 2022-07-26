d315fc8d72fbba18357c6e6069ea91ba406744c4
Wlkr123 <104069564+Wlkr123@users.noreply.github.com>
2022-07-26 12:26:07 +0800

    updates

--- packages/plugin-loyalties-api/src/messageBroker.ts
+++ packages/plugin-loyalties-api/src/messageBroker.ts
@@ -1,27 +1,27 @@
-import { generateModels } from './connectionResolver';
-import { ISendMessageArgs, sendMessage } from '@erxes/api-utils/src/core';
-import { serviceDiscovery } from './configs';
-import { checkVouchersSale } from './utils';
+import { generateModels } from "./connectionResolver";
+import { ISendMessageArgs, sendMessage } from "@erxes/api-utils/src/core";
+import { serviceDiscovery } from "./configs";
+import { checkVouchersSale } from "./utils";
 
 let client;
 
-export const initBroker = async cl => {
+export const initBroker = async (cl) => {
   client = cl;
 
   const { consumeRPCQueue } = client;
 
   consumeRPCQueue(
-    'loyalties:voucherCampaigns.find',
+    "loyalties:voucherCampaigns.find",
     async ({ subdomain, data }) => {
       const models = await generateModels(subdomain);
 
       return {
         data: await models.VoucherCampaigns.find(data).lean(),
-        status: 'success'
+        status: "success"
       };
     }
   );
-  consumeRPCQueue('loyalties:checkLoyalties', async ({ subdomain, data }) => {
+  consumeRPCQueue("loyalties:checkLoyalties", async ({ subdomain, data }) => {
     const models = await generateModels(subdomain);
     const { ownerType, ownerId, products } = data;
     return {
@@ -32,7 +32,7 @@ export const initBroker = async cl => {
         ownerId,
         products
       ),
-      status: 'success'
+      status: "success"
     };
   });
 };
@@ -43,7 +43,7 @@ export const sendProductsMessage = async (
   return sendMessage({
     client,
     serviceDiscovery,
-    serviceName: 'products',
+    serviceName: "products",
     ...args
   });
 };
@@ -54,7 +54,7 @@ export const sendContactsMessage = async (
   return sendMessage({
     client,
     serviceDiscovery,
-    serviceName: 'contacts',
+    serviceName: "contacts",
     ...args
   });
 };
@@ -63,7 +63,7 @@ export const sendCoreMessage = async (args: ISendMessageArgs): Promise<any> => {
   return sendMessage({
     client,
     serviceDiscovery,
-    serviceName: 'core',
+    serviceName: "core",
     ...args
   });
 };
@@ -74,7 +74,7 @@ export const sendNotificationsMessage = async (
   return sendMessage({
     client,
     serviceDiscovery,
-    serviceName: 'notifications',
+    serviceName: "notifications",
     ...args
   });
 };
@@ -88,7 +88,7 @@ export const sendRPCMessage = async (channel, message): Promise<any> => {
 };
 
 export const sendNotification = (subdomain: string, data) => {
-  return sendNotificationsMessage({ subdomain, action: 'send', data });
+  return sendNotificationsMessage({ subdomain, action: "send", data });
 };
 
 export default function() {
