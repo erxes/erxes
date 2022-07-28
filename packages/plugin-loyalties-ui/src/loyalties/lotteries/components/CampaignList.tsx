aee966c839c9959c744482e193918acef9e1eb7a
Wlkr123 <104069564+Wlkr123@users.noreply.github.com>
2022-07-28 13:48:11 +0800

    fix some bugs

--- packages/plugin-loyalties-ui/src/loyalties/lotteries/components/CampaignList.tsx
+++ packages/plugin-loyalties-ui/src/loyalties/lotteries/components/CampaignList.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import queryString from 'query-string';
-import { DataWithLoader, Icon, Tip } from '@erxes/ui/src/components';
+import {
+  DataWithLoader,
+  Icon,
+  Tip
+} from '@erxes/ui/src/components';
 import { __, router } from '@erxes/ui/src/utils';
 import { Sidebar, Wrapper } from '@erxes/ui/src/layout';
 import { ILotteryCampaign } from '../../../configs/lotteryCampaign/types';
@@ -20,7 +24,9 @@ interface IProps {
 
 class List extends React.Component<IProps> {
   clearCategoryFilter = () => {
-    router.setParams(this.props.history, { campaignId: null });
+    router.setParams(this.props.history, {
+      campaignId: null
+    });
   };
 
   isActive = (id: string) => {
@@ -44,8 +50,13 @@ class List extends React.Component<IProps> {
       const name = `${campaign.title} (${campaign.lotteriesCount})`;
 
       result.push(
-        <SidebarListItem key={campaign._id} isActive={this.isActive(campaign._id)}>
-          <Link to={`?${qryString}&campaignId=${campaign._id}`}>{name}</Link>
+        <SidebarListItem
+          key={campaign._id}
+          isActive={this.isActive(campaign._id)}
+        >
+          <Link to={`?${qryString}&campaignId=${campaign._id}`}>
+            {name}
+          </Link>
         </SidebarListItem>
       );
     }
@@ -62,9 +73,19 @@ class List extends React.Component<IProps> {
             {__('Manage Lottery Campaigns')}
           </Link>
           <Section.QuickButtons>
-            {router.getParam(this.props.history, 'campaignId') && (
-              <a href="#cancel" tabIndex={0} onClick={this.clearCategoryFilter}>
-                <Tip text={__('Clear filter')} placement="bottom">
+            {router.getParam(
+              this.props.history,
+              'campaignId'
+            ) && (
+              <a
+                href="#cancel"
+                tabIndex={0}
+                onClick={this.clearCategoryFilter}
+              >
+                <Tip
+                  text={__('Clear filter')}
+                  placement="bottom"
+                >
                   <Icon icon="cancel-1" />
                 </Tip>
               </a>
@@ -78,13 +99,25 @@ class List extends React.Component<IProps> {
   renderCategoryList() {
     const { lotteryCampaignsCount, loading } = this.props;
 
-    return <DataWithLoader data={this.renderContent()} loading={loading} count={lotteryCampaignsCount} emptyText="There is no lottery campaigns" emptyIcon="folder-2" size="small" />;
+    return (
+      <DataWithLoader
+        data={this.renderContent()}
+        loading={loading}
+        count={lotteryCampaignsCount}
+        emptyText="There is no lottery campaigns"
+        emptyIcon="folder-2"
+        size="small"
+      />
+    );
   }
 
   render() {
     return (
       <Sidebar>
-        <Section maxHeight={188} collapsible={this.props.lotteryCampaignsCount > 5}>
+        <Section
+          maxHeight={188}
+          collapsible={this.props.lotteryCampaignsCount > 5}
+        >
           {this.renderCategoryHeader()}
           {this.renderCategoryList()}
         </Section>
