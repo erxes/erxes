1b4558c104de3e3b8218b3488510f8f4bb782be5
munkhsaikhan.g@nmma.co <munkhsaikhan.g@nmma.co>
2022-08-02 15:48:06 +0800

    lint fix gateway

--- packages/gateway/src/subscription/pubsub.ts
+++ packages/gateway/src/subscription/pubsub.ts
@@ -1,26 +1,26 @@
-import * as dotenv from "dotenv";
+import * as dotenv from 'dotenv';
 dotenv.config();
-import { RedisPubSub } from "graphql-redis-subscriptions";
-import * as Redis from "ioredis";
+import { RedisPubSub } from 'graphql-redis-subscriptions';
+import * as Redis from 'ioredis';
 
 const { REDIS_HOST, REDIS_PORT, REDIS_PASSWORD } = process.env;
 
 const pubsub = new RedisPubSub({
-  connectionListener: (error) => {
+  connectionListener: error => {
     if (error) {
       console.log(error);
     }
   },
   publisher: new Redis({
     host: REDIS_HOST,
-    port: parseInt(REDIS_PORT || "6379"),
-    password: REDIS_PASSWORD,
+    port: parseInt(REDIS_PORT || '6379'),
+    password: REDIS_PASSWORD
   }),
   subscriber: new Redis({
     host: REDIS_HOST,
-    port: parseInt(REDIS_PORT || "6379"),
-    password: REDIS_PASSWORD,
-  }),
-});
+    port: parseInt(REDIS_PORT || '6379'),
+    password: REDIS_PASSWORD
+  })
+} as any);
 
 export default pubsub;
