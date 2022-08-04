659c39f5d5cd2f5db33cad0fb46da75a8de4ea10
Dashjamts23 <51987172+rei2232@users.noreply.github.com>
2022-08-04 15:35:50 +0800

    update syncerkhet check deal

--- packages/plugin-syncerkhet-ui/src/components/FilterDeal.tsx
+++ packages/plugin-syncerkhet-ui/src/components/FilterDeal.tsx
@@ -1,72 +1,137 @@
 import {
-  DataWithLoader,
-  FormControl,
-  Info,
-  Pagination,
-  Spinner,
+  ControlLabel,
+  Button,
+  FormGroup,
+  DateControl,
+  __,
   Table,
-  __
+  DataWithLoader
 } from '@erxes/ui/src';
-import { Wrapper } from '@erxes/ui/src/layout';
-import { IRouterProps } from '@erxes/ui/src/types';
+import BoardSelectContainer from '@erxes/ui-cards/src/boards/containers/BoardSelect';
+import { Sidebar, Wrapper } from '@erxes/ui/src/layout';
+import { MainStyleDateContainer as DateContainer } from '@erxes/ui/src/styles/eindex';
 import React from 'react';
-import { withRouter, Link } from 'react-router-dom';
-import Sidebar from '../components/Sidebar';
-import * as dayjs from 'dayjs';
-import { Title } from '@erxes/ui/src/styles/main';
-class FilterDeals extends React.Component {
+import { Title } from '@erxes/ui-settings/src/styles';
+type Props = {
+  config: any;
+};
+type State = {
+  configs: any;
+};
+class FilterDeals extends React.Component<Props, State> {
   constructor(props) {
     super(props);
   }
 
+  config = {
+    boardId: '',
+    pipelineId: '',
+    stageId: ''
+  };
+  onChangeBoard = (boardId: string) => {
+    // this.setState({ config: { ...this.state.config, boardId } });
+    this.config.boardId = boardId;
+  };
+
+  onChangePipeline = (pipelineId: string) => {
+    // this.setState({ config: { ...this.state.config, pipelineId } });
+    this.config.pipelineId = pipelineId;
+  };
+
+  onChangeStage = (stageId: string) => {
+    // this.setState({ config: { ...this.state.config, stageId } });
+    this.config.stageId = stageId;
+  };
   render() {
-    const { checked, deals, loading } = this.props;
+    // const { checked, deals, loading } = this.props;
 
     const tablehead = ['Board name', 'Pipeline name', 'Stage Name', 'Amount'];
 
-    if (loading) {
-      return <Spinner />;
-    }
+    // if (loading) {
+    //   return <Spinner />;
+    // }
 
-    const Content = error ? (
-      <Info>{error.message}</Info>
-    ) : (
+    const Content = (
       <Table>
         <thead>
           <tr>
-            {tablehead.map(p => (
+            <th>Deal name</th>
+            <th>Amount</th>
+            <th>Checked?</th>
+            {/* {tablehead.map(p => (
               <th key={p}>{p}</th>
-            ))}
+            ))} */}
           </tr>
         </thead>
         <tbody>
-          {deals?.map((p, i) => (
-            <tr key={i}>
-              <td>
-                <Link to={`/${route(p.ownerType)}/details/${p.ownerId}`}>
-                  {p.owner.email}
-                </Link>
-              </td>
-              <td>{p.owner.details.fullName}</td>
-              <td>{p.ownerType}</td>
-              <td>{p.changeScore}</td>
-              <td>{p.owner.score}</td>
-              <td>{dayjs(p.createdAt).format('lll')}</td>
-            </tr>
-          ))}
+          <tr>
+            <td>td</td>
+            <td>td</td>
+            <td>td</td>
+          </tr>
+          <tr>
+            <td>td</td>
+            <td>td</td>
+            <td>td</td>
+          </tr>
         </tbody>
       </Table>
     );
 
     const header = <Wrapper.Header title={__('Check deals') + `(${1})`} />;
 
+    const sideDealFilter = (
+      <>
+        <FormGroup>
+          <ControlLabel>Choose Stage</ControlLabel>
+          <BoardSelectContainer
+            type="deal"
+            autoSelectStage={false}
+            boardId={this.config.boardId}
+            pipelineId={this.config.pipelineId}
+            stageId={this.config.stageId}
+            onChangeBoard={this.onChangeBoard}
+            onChangePipeline={this.onChangePipeline}
+            onChangeStage={this.onChangeStage}
+          />
+        </FormGroup>
+        <FormGroup>
+          <ControlLabel>Choose Start Date</ControlLabel>
+          <DateContainer>
+            <DateControl
+              required={true}
+              name="startDate"
+              placeholder={__('Start date')}
+              // value={voucherCampaign.startDate}
+              // onChange={this.onDateInputChange.bind(this, 'startDate')}
+            />
+          </DateContainer>
+        </FormGroup>
+        <FormGroup>
+          <ControlLabel>Choose End Date</ControlLabel>
+          <DateContainer>
+            <DateControl
+              required={true}
+              name="endDate"
+              placeholder={__('End date')}
+              // value={voucherCampaign.startDate}
+              // onChange={this.onDateInputChange.bind(this, 'startDate')}
+            />
+          </DateContainer>
+        </FormGroup>
+        <Button btnStyle="primary" icon="check-circle">
+          Search
+        </Button>
+      </>
+    );
     const sidebar = (
-      <Sidebar
-        loadingMainQuery={loading}
-        queryParams={queryParams}
-        history={history}
-        refetch={refetch}
-      />
+      <Sidebar children={sideDealFilter} />
+      // <Sidebar
+      //   loadingMainQuery={loading}
+      //   queryParams={queryParams}
+      //   history={history}
+      //   refetch={refetch}
+      // />
     );
 
     const content = (
@@ -77,8 +142,7 @@ class FilterDeals extends React.Component {
         />
         <DataWithLoader
           data={Content}
-          loading={loading}
-          //   count={total}
+          // count={20}
           emptyText="Empty list"
           emptyImage="/images/actions/1.svg"
         />
--- packages/plugin-syncerkhet-ui/src/configs.js
+++ packages/plugin-syncerkhet-ui/src/configs.js
@@ -19,12 +19,12 @@ module.exports = {
       action: 'syncErkhetConfig',
       permission: "syncErkhetConfig",
     },
-    // {
-    //   text: 'Sync Erkhet Deals Check',
-    //   to: '/sync-erkhet-check',
-    //   image: '/images/icons/erxes-04.svg',
-    //   location: "mainNavigation",
-    //   scope: 'syncerkhet'
-    // }
+    {
+      text: 'Sync Erkhet Deals Check',
+      to: '/sync-erkhet-check',
+      image: '/images/icons/erxes-04.svg',
+      location: "settings",
+      scope: 'syncerkhet'
+    }
   ]
 };
