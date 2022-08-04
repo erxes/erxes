5a4836fd24a5d342f6e02864c9e414dd80944df5
Dashjamts23 <51987172+rei2232@users.noreply.github.com>
2022-08-04 16:05:10 +0800

    update syncherkhet

--- packages/plugin-syncerkhet-ui/src/containers/FilterDeals.tsx
+++ packages/plugin-syncerkhet-ui/src/containers/FilterDeals.tsx
@@ -1,16 +1,12 @@
 import { Bulk } from '@erxes/ui/src/components';
-import { IRouterProps } from '@erxes/ui/src/types';
-import { withProps, router } from '@erxes/ui/src/utils/core';
-import gql from 'graphql-tag';
-import * as compose from 'lodash.flowright';
 import React from 'react';
-import { graphql } from 'react-apollo';
-import { withRouter } from 'react-router-dom';
 import FilterDeal from '../components/FilterDeal';
-import { queries } from '../graphql';
 type Props = {
-  checked: any;
-  deals: any;
+  checkedDealIds: any;
+  dealsIds: any;
+  boardId: any;
+  pipelineId: any;
+  stageId: any;
   loading: boolean;
 };
 
@@ -23,7 +19,7 @@ class FilterDealsContainer extends React.Component<Props> {
     const content = props => <FilterDeal {...props} />;
 
     const refetch = () => {
-      this.props.checked.refetch();
+      this.props.checkedDealIds.refetch();
     };
 
     return <Bulk content={content} refetch={refetch} />;
