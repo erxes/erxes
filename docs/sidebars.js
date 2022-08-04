a4a4dc246790d01e590522150285f5347bd32e8d
Ariukasq1 <Ariuka.sq@gmail.com>
2022-08-04 21:42:44 +0800

    update

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -1,138 +1,150 @@
 module.exports = {
   docs: [
     {
-      type: "category",
-      label: "Getting Started",
-      items:[
+      type: 'category',
+      label: 'What is erxes?',
+      items: [
         {
-          "Deploying on server": [
-              "overview/deployment-overview",
+          Overview: [
+            'introduction/Welcome-to-erxes',
+            'introduction/with-erxes',
+            'introduction/open-source',
+            'introduction/support'
+          ]
+        },
+        'overview/quickstart'
+      ]
+    },
+    {
+      type: 'category',
+      label: 'Getting Started',
+      items: [
+        {
+          'Deploying on server': [
+            'overview/deployment-overview',
             {
-              "Installation Guide": [
+              'Installation Guide': [
                 {
-                  "Ubuntu": [
-                              "installation/ubuntu",
-                              {
-                                Installation: [
-                                  "installation/ubuntu-quickstart",
-                                  "installation/ubuntu-step-by-step",
-                                  "installation/ubuntu-troubleshooting",
-                                ],
-                              },
-                            ],
-                  },
-                "installation/docker",
-                "installation/upgrade",
+                  Ubuntu: [
+                    'installation/ubuntu',
+                    {
+                      Installation: [
+                        'installation/ubuntu-quickstart',
+                        'installation/ubuntu-step-by-step',
+                        'installation/ubuntu-troubleshooting'
+                      ]
+                    }
+                  ]
+                },
+                'installation/docker',
+                'installation/upgrade'
               ]
-            },
+            }
           ]
         },
         {
-          "Contributing on open source": [
-            "getting-started/contributing-guide",
-            "getting-started/submitting",
-            "getting-started/commit"
+          'Contributing on open source': [
+            'getting-started/contributing-guide',
+            'getting-started/submitting',
+            'getting-started/commit'
           ]
         }
       ]
     },
     {
-      type: "category",
-      label: "Overview",
-      items: [
-        "overview/overview",
-        "overview/architecture-overview",
-      ],
+      type: 'category',
+      label: 'Overview',
+      items: ['overview/overview', 'overview/architecture-overview']
     },
     {
-      type: "category",
+      type: 'category',
       label: "Developer's Guide",
       items: [
-        "developer/developer",
-        "developer/documentation_guide",
+        'developer/developer',
+        'developer/documentation_guide',
         {
           "Administrator's Guide": [
-              "administrator/creating-first-user",
-              "administrator/environment-variables",
-              "administrator/system-config",
-              "administrator/migration",
-            ] 
-          },
+            'administrator/creating-first-user',
+            'administrator/environment-variables',
+            'administrator/system-config',
+            'administrator/migration'
+          ]
+        },
         {
           Tutorials: [
-           "developer/graphql-api",
-           "developer/android-sdk",
-           "developer/ios-sdk",
-           "developer/push-notifications",
-           "user/script-install",
+            'developer/graphql-api',
+            'developer/android-sdk',
+            'developer/ios-sdk',
+            'developer/push-notifications',
+            'user/script-install'
           ]
         },
         {
           Integrations: [
-          "developer/integrations-overview/facebook",
-          "developer/integrations-overview/twitter",
-          "developer/integrations-overview/gmail",
-          "developer/integrations-overview/google-cloud-storage",
-          "developer/integrations-overview/aws-s3",
-          "developer/integrations-overview/aws-ses",
-          "developer/integrations-overview/nylas-integrations",
-          "developer/integrations-overview/whatsApp-integration",
-          "developer/integrations-overview/sunshine-conversations"
+            'developer/integrations-overview/facebook',
+            'developer/integrations-overview/twitter',
+            'developer/integrations-overview/gmail',
+            'developer/integrations-overview/google-cloud-storage',
+            'developer/integrations-overview/aws-s3',
+            'developer/integrations-overview/aws-ses',
+            'developer/integrations-overview/nylas-integrations',
+            'developer/integrations-overview/whatsApp-integration',
+            'developer/integrations-overview/sunshine-conversations'
           ]
         },
-        "developer/troubleshooting" 
-      ],
+        'developer/troubleshooting'
+      ]
     },
     {
-      type: "category",
-      label: "Changelog",
-      items:[
-        {  
-        type: 'link',
-        label: 'Release Notes', // The link label
-        href: 'https://github.com/erxes/erxes/releases', // The external URL
-        },
-      ],
+      type: 'category',
+      label: 'Changelog',
+      items: [
+        {
+          type: 'link',
+          label: 'Release Notes', // The link label
+          href: 'https://github.com/erxes/erxes/releases' // The external URL
+        }
+      ]
     }
   ],
   tutorials: [
     {
-      type: "category",
+      type: 'category',
       label: "User's Guide",
       items: [
-        "user/subscription-getting-started",
-        "user/initial-setup",
-        "user/general-settings",
-        "user/engage-phone-settings",
-        "user/team-inbox",
-        "user/knowledge-base",
-        "user/popups",
-        "user/script-install",
-        "user/contacts",
-        "user/segments",
-        "user/sales-pipeline",
-        "user/engage",
-        "user/profile-settings",
-        "user/notification",
-        "user/mobile-apps",
-        "user/import",
-      ],
-    },
+        'user/subscription-getting-started',
+        'user/initial-setup',
+        'user/general-settings',
+        'user/engage-phone-settings',
+        'user/team-inbox',
+        'user/knowledge-base',
+        'user/popups',
+        'user/script-install',
+        'user/contacts',
+        'user/segments',
+        'user/sales-pipeline',
+        'user/engage',
+        'user/profile-settings',
+        'user/notification',
+        'user/mobile-apps',
+        'user/import'
+      ]
+    }
   ],
   components: [
     {
-      type: "category",
-      label: "Components",
+      type: 'category',
+      label: 'Components',
       items: [
-        "components/Button/buttons",
-        "components/EmptyState/emptystate",
-        "components/ErrorMsg/errormsg",
-        "components/Info/info",
-        "components/Label/label",
-        "components/Namecard/namecard",
-        "components/Spinner/spinners",
-        "components/Table/table",
-      ],
-    },
-  ],
+        'components/Button/buttons',
+        'components/EmptyState/emptystate',
+        'components/ErrorMsg/errormsg',
+        'components/Info/info',
+        'components/Label/label',
+        'components/Namecard/namecard',
+        'components/Spinner/spinners',
+        'components/Table/table'
+      ]
+    }
+  ]
 };
