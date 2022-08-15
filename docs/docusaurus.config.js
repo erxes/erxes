368735d2cbafd61ddc584fcd0a5640d3db7d50b7
BatAmar Battulga <mrbatamar@gmail.com>
2022-08-15 15:54:38 +0800

    updated url

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: "erxes Inc",
   tagline:
     "erxes is a free and open fair-code licensed all-in-one growth marketing and management tool for a smoother customer journey",
-  url: "https://www.erxes.org",
+  url: "https://docs.erxes.io",
   baseUrl: "/",
   favicon: "img/favicon.png",
   organizationName: "erxes", // Usually your GitHub org/user name.
