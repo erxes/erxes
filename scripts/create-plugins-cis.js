25785f9e23aca89c299d37da7aee8cc1c6383d62
BatAmar Battulga <mrbatamar@gmail.com>
2022-08-20 13:19:21 +0800

    ci update

--- scripts/create-plugins-cis.js
+++ scripts/create-plugins-cis.js
@@ -13,12 +13,12 @@ const filePath = (pathName) => {
 var workflowsPath = (fileName) => filePath(`./.github/workflows/${fileName}`);
 
 var plugins = [
-  // { name: 'ads', api: true },
+  { name: 'ads', api: true },
   { name: 'cars', ui: true, api: true },
-  // { name: 'loan', ui: true, api: true },
-  // { name: 'neighbor', ui: true, api: true },
-  // { name: 'rentpay', ui: true, api: true },
-  // { name: 'tumentech', ui: true, api: true }
+  { name: 'loan', ui: true, api: true },
+  { name: 'neighbor', ui: true, api: true },
+  { name: 'rentpay', ui: true, api: true },
+  { name: 'tumentech', ui: true, api: true }
 ];
 
 const pluginsMap = {};
