920652bd7fe6cdb6d63e07a772b71db822b9bcec
BatAmar Battulga <mrbatamar@gmail.com>
2022-08-24 14:13:22 +0800

    cli nginx update

--- cli/commands/docker/utils.js
+++ cli/commands/docker/utils.js
@@ -613,10 +613,12 @@ const up = async ({ uis, fromInstaller }) => {
 
   const commonParams = `
     proxy_set_header Connection 'upgrade';
-    proxy_set_header Host $host;
     proxy_set_header Host $http_host;
     proxy_set_header X-Real-IP $remote_addr;
+    proxy_set_header X-Forwarded-Host $host;
+    proxy_set_header X-Forwarded-Server $host;
     proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+    proxy_set_header X-Forwarded-Proto $scheme;
     proxy_http_version 1.1;
   `;
 
