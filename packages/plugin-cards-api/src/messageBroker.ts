c99e9a7e130e83d7785e4ee90abe88a2c1f2988d
Wlkr123 <104069564+Wlkr123@users.noreply.github.com>
2022-09-01 14:18:47 +0800

    update

--- packages/plugin-cards-api/src/messageBroker.ts
+++ packages/plugin-cards-api/src/messageBroker.ts
@@ -1,12 +1,6 @@
 import { sendToWebhook as sendWebhook } from '@erxes/api-utils/src';
-import {
-  ISendMessageArgs,
-  sendMessage
-} from '@erxes/api-utils/src/core';
-import {
-  graphqlPubsub,
-  serviceDiscovery
-} from './configs';
+import { ISendMessageArgs, sendMessage } from '@erxes/api-utils/src/core';
+import { graphqlPubsub, serviceDiscovery } from './configs';
 import { generateModels } from './connectionResolver';
 import {
   generateAmounts,
@@ -22,45 +16,30 @@ let client;
 export const initBroker = async cl => {
   client = cl;
 
-  const {
-    consumeQueue,
-    consumeRPCQueue
-  } = client;
+  const { consumeQueue, consumeRPCQueue } = client;
 
-  consumeRPCQueue(
-    'cards:tickets.create',
-    async ({ subdomain, data }) => {
-      const models = await generateModels(
-        subdomain
-      );
+  consumeRPCQueue('cards:tickets.create', async ({ subdomain, data }) => {
+    const models = await generateModels(subdomain);
 
-      return {
-        status: 'success',
-        data: await models.Tickets.create(data)
-      };
-    }
-  );
+    return {
+      status: 'success',
+      data: await models.Tickets.create(data)
+    };
+  });
 
-  consumeRPCQueue(
-    'cards:tasks.create',
-    async ({ subdomain, data }) => {
-      const models = await generateModels(
-        subdomain
-      );
+  consumeRPCQueue('cards:tasks.create', async ({ subdomain, data }) => {
+    const models = await generateModels(subdomain);
 
-      return {
-        status: 'success',
-        data: await models.Tasks.create(data)
-      };
-    }
-  );
+    return {
+      status: 'success',
+      data: await models.Tasks.create(data)
+    };
+  });
 
   consumeRPCQueue(
     'cards:tasks.remove',
     async ({ subdomain, data: { _ids } }) => {
-      const models = await generateModels(
-        subdomain
-      );
+      const models = await generateModels(subdomain);
 
       return {
         status: 'success',
@@ -69,26 +48,19 @@ export const initBroker = async cl => {
     }
   );
 
-  consumeRPCQueue(
-    'cards:deals.create',
-    async ({ subdomain, data }) => {
-      const models = await generateModels(
-        subdomain
-      );
+  consumeRPCQueue('cards:deals.create', async ({ subdomain, data }) => {
+    const models = await generateModels(subdomain);
 
-      return {
-        status: 'success',
-        data: await models.Deals.create(data)
-      };
-    }
-  );
+    return {
+      status: 'success',
+      data: await models.Deals.create(data)
+    };
+  });
 
   consumeRPCQueue(
     'cards:deals.remove',
     async ({ subdomain, data: { _ids } }) => {
-      const models = await generateModels(
-        subdomain
-      );
+      const models = await generateModels(subdomain);
 
       return {
         status: 'success',
@@ -97,301 +69,185 @@ export const initBroker = async cl => {
     }
   );
 
-  consumeRPCQueue(
-    'cards:tickets.find',
-    async ({ subdomain, data }) => {
-      const models = await generateModels(
-        subdomain
-      );
+  consumeRPCQueue('cards:tickets.find', async ({ subdomain, data }) => {
+    const models = await generateModels(subdomain);
 
-      return {
-        status: 'success',
-        data: await models.Tickets.find(
-          data
-        ).lean()
-      };
-    }
-  );
+    return {
+      status: 'success',
+      data: await models.Tickets.find(data).lean()
+    };
+  });
 
-  consumeRPCQueue(
-    'cards:tickets.findOne',
-    async ({ subdomain, data }) => {
-      const models = await generateModels(
-        subdomain
-      );
+  consumeRPCQueue('cards:tickets.findOne', async ({ subdomain, data }) => {
+    const models = await generateModels(subdomain);
 
-      return {
-        status: 'success',
-        data: await models.Tickets.findOne(
-          data
-        ).lean()
-      };
-    }
-  );
+    return {
+      status: 'success',
+      data: await models.Tickets.findOne(data).lean()
+    };
+  });
 
   consumeRPCQueue(
     'cards:tickets.remove',
     async ({ subdomain, data: { _ids } }) => {
-      const models = await generateModels(
-        subdomain
-      );
+      const models = await generateModels(subdomain);
 
       return {
         status: 'success',
-        data: await models.Tickets.removeTickets(
-          _ids
-        )
+        data: await models.Tickets.removeTickets(_ids)
       };
     }
   );
 
-  consumeRPCQueue(
-    'cards:stages.find',
-    async ({ subdomain, data }) => {
-      const models = await generateModels(
-        subdomain
-      );
+  consumeRPCQueue('cards:stages.find', async ({ subdomain, data }) => {
+    const models = await generateModels(subdomain);
 
-      return {
-        status: 'success',
-        data: await models.Stages.find(
-          data
-        ).lean()
-      };
-    }
-  );
+    return {
+      status: 'success',
+      data: await models.Stages.find(data).lean()
+    };
+  });
 
-  consumeRPCQueue(
-    'cards:stages.findOne',
-    async ({ subdomain, data }) => {
-      const models = await generateModels(
-        subdomain
-      );
+  consumeRPCQueue('cards:stages.findOne', async ({ subdomain, data }) => {
+    const models = await generateModels(subdomain);
 
-      return {
-        status: 'success',
-        data: await models.Stages.findOne(
-          data
-        ).lean()
-      };
-    }
-  );
+    return {
+      status: 'success',
+      data: await models.Stages.findOne(data).lean()
+    };
+  });
 
-  consumeRPCQueue(
-    'cards:tasks.find',
-    async ({ subdomain, data }) => {
-      const models = await generateModels(
-        subdomain
-      );
+  consumeRPCQueue('cards:tasks.find', async ({ subdomain, data }) => {
+    const models = await generateModels(subdomain);
 
-      return {
-        status: 'success',
-        data: await models.Tasks.find(data).lean()
-      };
-    }
-  );
+    return {
+      status: 'success',
+      data: await models.Tasks.find(data).lean()
+    };
+  });
 
-  consumeRPCQueue(
-    'cards:tasks.findOne',
-    async ({ subdomain, data }) => {
-      const models = await generateModels(
-        subdomain
-      );
+  consumeRPCQueue('cards:tasks.findOne', async ({ subdomain, data }) => {
+    const models = await generateModels(subdomain);
 
-      return {
-        status: 'success',
-        data: await models.Tasks.findOne(
-          data
-        ).lean()
-      };
-    }
-  );
+    return {
+      status: 'success',
+      data: await models.Tasks.findOne(data).lean()
+    };
+  });
 
-  consumeRPCQueue(
-    'cards:pipelines.find',
-    async ({ subdomain, data }) => {
-      const models = await generateModels(
-        subdomain
-      );
+  consumeRPCQueue('cards:pipelines.find', async ({ subdomain, data }) => {
+    const models = await generateModels(subdomain);
 
-      return {
-        status: 'success',
-        data: await models.Pipelines.find(
-          data
-        ).lean()
-      };
-    }
-  );
+    return {
+      status: 'success',
+      data: await models.Pipelines.find(data).lean()
+    };
+  });
 
-  consumeRPCQueue(
-    'cards:boards.find',
-    async ({ subdomain, data }) => {
-      const models = await generateModels(
-        subdomain
-      );
+  consumeRPCQueue('cards:boards.find', async ({ subdomain, data }) => {
+    const models = await generateModels(subdomain);
 
-      return {
-        status: 'success',
-        data: await models.Boards.find(
-          data
-        ).lean()
-      };
-    }
-  );
+    return {
+      status: 'success',
+      data: await models.Boards.find(data).lean()
+    };
+  });
 
   consumeQueue(
     'cards:checklists.removeChecklists',
-    async ({
-      subdomain,
-      data: { type, itemIds }
-    }) => {
-      const models = await generateModels(
-        subdomain
-      );
+    async ({ subdomain, data: { type, itemIds } }) => {
+      const models = await generateModels(subdomain);
 
       return {
         status: 'success',
-        data: await models.Checklists.removeChecklists(
-          type,
-          itemIds
-        )
+        data: await models.Checklists.removeChecklists(type, itemIds)
       };
     }
   );
 
-  consumeRPCQueue(
-    'cards:conversationConvert',
-    async ({ subdomain, data }) => {
-      const models = await generateModels(
-        subdomain
-      );
+  consumeRPCQueue('cards:conversationConvert', async ({ subdomain, data }) => {
+    const models = await generateModels(subdomain);
 
+    return {
+      status: 'success',
+      data: await conversationConvertToCard(models, subdomain, data)
+    };
+  });
+
+  consumeRPCQueue('cards:deals.find', async ({ subdomain, data }) => {
+    const models = await generateModels(subdomain);
+
+    if (!data.query) {
       return {
         status: 'success',
-        data: await conversationConvertToCard(
-          models,
-          subdomain,
-          data
-        )
+        data: await models.Deals.find(data).lean()
       };
     }
-  );
 
-  consumeRPCQueue(
-    'cards:deals.find',
-    async ({ subdomain, data }) => {
-      const models = await generateModels(
-        subdomain
-      );
+    const { query, skip, limit, sort = {} } = data;
 
-      if (!data.query) {
-        return {
-          status: 'success',
-          data: await models.Deals.find(
-            data
-          ).lean()
-        };
-      }
+    return {
+      status: 'success',
+      data: await models.Deals.find(query)
+        .skip(skip || 0)
+        .limit(limit || 20)
+        .sort(sort)
+        .lean()
+    };
+  });
 
-      const {
-        query,
-        skip,
-        limit,
-        sort = {}
-      } = data;
+  consumeRPCQueue('cards:deals.count', async ({ subdomain, data }) => {
+    const models = await generateModels(subdomain);
 
-      return {
-        status: 'success',
-        data: await models.Deals.find(query)
-          .skip(skip || 0)
-          .limit(limit || 20)
-          .sort(sort)
-          .lean()
-      };
-    }
-  );
+    return {
+      status: 'success',
+      data: await models.Deals.find(data).count()
+    };
+  });
 
-  consumeRPCQueue(
-    'cards:deals.count',
-    async ({ subdomain, data }) => {
-      const models = await generateModels(
-        subdomain
-      );
+  consumeRPCQueue('cards:deals.findOne', async ({ subdomain, data }) => {
+    const models = await generateModels(subdomain);
 
-      return {
-        status: 'success',
-        data: await models.Deals.find(
-          data
-        ).count()
-      };
-    }
-  );
+    return {
+      status: 'success',
+      data: await models.Deals.findOne(data).lean()
+    };
+  });
 
-  consumeRPCQueue(
-    'cards:deals.findOne',
-    async ({ subdomain, data }) => {
-      const models = await generateModels(
-        subdomain
-      );
-
-      return {
-        status: 'success',
-        data: await models.Deals.findOne(
-          data
-        ).lean()
-      };
-    }
-  );
-
-  consumeRPCQueue(
-    'cards:deals.generateAmounts',
-    async productsData => {
-      return {
-        data: generateAmounts(productsData),
-        status: 'success'
-      };
-    }
-  );
+  consumeRPCQueue('cards:deals.generateAmounts', async productsData => {
+    return {
+      data: generateAmounts(productsData),
+      status: 'success'
+    };
+  });
 
   consumeRPCQueue(
     'cards:deals.generateProducts',
     async ({ subdomain, data }) => {
       return {
-        data: await generateProducts(
-          subdomain,
-          data
-        ),
+        data: await generateProducts(subdomain, data),
         status: 'success'
       };
     }
   );
 
-  consumeRPCQueue(
-    'cards:findItem',
-    async ({ subdomain, data }) => {
-      const models = await generateModels(
-        subdomain
-      );
+  consumeRPCQueue('cards:findItem', async ({ subdomain, data }) => {
+    const models = await generateModels(subdomain);
 
-      return {
-        data: await getCardItem(models, data),
-        status: 'success'
-      };
-    }
-  );
+    return {
+      data: await getCardItem(models, data),
+      status: 'success'
+    };
+  });
 
   consumeRPCQueue(
     'cards:findDealProductIds',
     async ({ subdomain, data: { _ids } }) => {
-      const models = await generateModels(
-        subdomain
-      );
+      const models = await generateModels(subdomain);
 
-      const dealProductIds = await await models.Deals.find(
-        {
-          'productsData.productId': { $in: _ids }
-        }
-      ).distinct('productsData.productId');
+      const dealProductIds = await await models.Deals.find({
+        'productsData.productId': { $in: _ids }
+      }).distinct('productsData.productId');
 
       return {
         data: dealProductIds,
@@ -402,19 +258,11 @@ export const initBroker = async cl => {
 
   consumeRPCQueue(
     'cards:tickets.updateMany',
-    async ({
-      subdomain,
-      data: { selector, modifier }
-    }) => {
-      const models = await generateModels(
-        subdomain
-      );
+    async ({ subdomain, data: { selector, modifier } }) => {
+      const models = await generateModels(subdomain);
 
       return {
-        data: await models.Tickets.updateMany(
-          selector,
-          modifier
-        ),
+        data: await models.Tickets.updateMany(selector, modifier),
         status: 'success'
       };
     }
@@ -422,19 +270,11 @@ export const initBroker = async cl => {
 
   consumeRPCQueue(
     'cards:tasks.updateMany',
-    async ({
-      subdomain,
-      data: { selector, modifier }
-    }) => {
-      const models = await generateModels(
-        subdomain
-      );
+    async ({ subdomain, data: { selector, modifier } }) => {
+      const models = await generateModels(subdomain);
 
       return {
-        data: await models.Tasks.updateMany(
-          selector,
-          modifier
-        ),
+        data: await models.Tasks.updateMany(selector, modifier),
         status: 'success'
       };
     }
@@ -442,19 +282,11 @@ export const initBroker = async cl => {
 
   consumeRPCQueue(
     'cards:deals.updateMany',
-    async ({
-      subdomain,
-      data: { selector, modifier }
-    }) => {
-      const models = await generateModels(
-        subdomain
-      );
+    async ({ subdomain, data: { selector, modifier } }) => {
+      const models = await generateModels(subdomain);
 
       return {
-        data: await models.Deals.updateMany(
-          selector,
-          modifier
-        ),
+        data: await models.Deals.updateMany(selector, modifier),
         status: 'success'
       };
     }
@@ -462,47 +294,29 @@ export const initBroker = async cl => {
 
   consumeRPCQueue(
     'cards:deals.updateOne',
-    async ({
-      subdomain,
-      data: { selector, modifier }
-    }) => {
-      const models = await generateModels(
-        subdomain
-      );
+    async ({ subdomain, data: { selector, modifier } }) => {
+      const models = await generateModels(subdomain);
 
       return {
-        data: await models.Deals.updateOne(
-          selector,
-          modifier
-        ),
+        data: await models.Deals.updateOne(selector, modifier),
         status: 'success'
       };
     }
   );
 
-  consumeRPCQueue(
-    'cards:notifiedUserIds',
-    async ({ subdomain, data }) => {
-      const models = await generateModels(
-        subdomain
-      );
+  consumeRPCQueue('cards:notifiedUserIds', async ({ subdomain, data }) => {
+    const models = await generateModels(subdomain);
 
-      return {
-        status: 'success',
-        data: await notifiedUserIds(models, data)
-      };
-    }
-  );
+    return {
+      status: 'success',
+      data: await notifiedUserIds(models, data)
+    };
+  });
 
   consumeRPCQueue(
     'cards:getLink',
-    async ({
-      subdomain,
-      data: { _id, type }
-    }) => {
-      const models = await generateModels(
-        subdomain
-      );
+    async ({ subdomain, data: { _id, type } }) => {
+      const models = await generateModels(subdomain);
 
       const item = await getCardItem(models, {
         contentTypeId: _id,
@@ -513,15 +327,9 @@ export const initBroker = async cl => {
         return '';
       }
 
-      const stage = await models.Stages.getStage(
-        item.stageId
-      );
-      const pipeline = await models.Pipelines.getPipeline(
-        stage.pipelineId
-      );
-      const board = await models.Boards.getBoard(
-        pipeline.boardId
-      );
+      const stage = await models.Stages.getStage(item.stageId);
+      const pipeline = await models.Pipelines.getPipeline(stage.pipelineId);
+      const board = await models.Boards.getBoard(pipeline.boardId);
 
       return {
         status: 'success',
@@ -532,13 +340,8 @@ export const initBroker = async cl => {
 
   consumeQueue(
     'cards:pipelinesChanged',
-    async ({
-      subdomain,
-      data: { pipelineId, action, data }
-    }) => {
-      const models = await generateModels(
-        subdomain
-      );
+    async ({ subdomain, data: { pipelineId, action, data } }) => {
+      const models = await generateModels(subdomain);
 
       graphqlPubsub.publish('pipelinesChanged', {
         pipelinesChanged: {
@@ -557,43 +360,23 @@ export const initBroker = async cl => {
 
   consumeQueue(
     'cards:publishHelperItems',
-    async ({
-      subdomain,
-      data: { addedTypeIds, removedTypeIds, doc }
-    }) => {
-      const targetTypes = [
-        'deal',
-        'task',
-        'ticket'
-      ];
-      const targetRelTypes = [
-        'company',
-        'customer'
-      ];
+    async ({ subdomain, data: { addedTypeIds, removedTypeIds, doc } }) => {
+      const targetTypes = ['deal', 'task', 'ticket'];
+      const targetRelTypes = ['company', 'customer'];
 
       if (
         targetTypes.includes(doc.mainType) &&
         targetRelTypes.includes(doc.relType)
       ) {
-        await publishHelper(
-          subdomain,
-          doc.mainType,
-          doc.mainTypeId
-        );
+        await publishHelper(subdomain, doc.mainType, doc.mainTypeId);
       }
 
       if (
         targetTypes.includes(doc.relType) &&
         targetRelTypes.includes(doc.mainType)
       ) {
-        for (const typeId of addedTypeIds.concat(
-          removedTypeIds
-        )) {
-          await publishHelper(
-            subdomain,
-            doc.relType,
-            typeId
-          );
+        for (const typeId of addedTypeIds.concat(removedTypeIds)) {
+          await publishHelper(subdomain, doc.relType, typeId);
         }
       }
 
@@ -626,9 +409,7 @@ export const sendInternalNotesMessage = async (
   });
 };
 
-export const sendCoreMessage = async (
-  args: ISendMessageArgs
-): Promise<any> => {
+export const sendCoreMessage = async (args: ISendMessageArgs): Promise<any> => {
   return sendMessage({
     client,
     serviceDiscovery,
@@ -692,9 +473,7 @@ export const sendNotificationsMessage = async (
   });
 };
 
-export const sendLogsMessage = async (
-  args: ISendMessageArgs
-): Promise<any> => {
+export const sendLogsMessage = async (args: ISendMessageArgs): Promise<any> => {
   return sendMessage({
     client,
     serviceDiscovery,
@@ -735,11 +514,7 @@ export const sendCommonMessage = async (
   });
 };
 
-export const fetchSegment = (
-  subdomain: string,
-  segmentId: string,
-  options?
-) =>
+export const fetchSegment = (subdomain: string, segmentId: string, options?) =>
   sendSegmentsMessage({
     subdomain,
     action: 'fetchSegment',
@@ -747,10 +522,7 @@ export const fetchSegment = (
     isRPC: true
   });
 
-export const sendToWebhook = ({
-  subdomain,
-  data
-}) => {
+export const sendToWebhook = ({ subdomain, data }) => {
   return sendWebhook(client, { subdomain, data });
 };
 
