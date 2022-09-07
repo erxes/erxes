2ed71f891c33acf6898ed1b6e30c4509e36ee731
Jonatan Rinckus <jonatan@loop3.com.br>
2022-09-07 13:45:23 -0300

    fix answer.content is undefined

--- packages/plugin-inbox-api/src/cronjobs/conversations.ts
+++ packages/plugin-inbox-api/src/cronjobs/conversations.ts
@@ -98,7 +98,7 @@ export const sendMessageEmail = async (subdomain: string) => {
 
       if (message.attachments.length !== 0) {
         for (const attachment of message.attachments) {
-          answer.content = answer.content.concat(
+          answer.content = (answer.content || '').concat(
             `<p><img src="${attachment.url}" alt="${attachment.name}"></p>`
           );
         }
