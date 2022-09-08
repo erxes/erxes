6066739e66b969c9534008d4e4a0bbd049b42b62
Ariukasq1 <Ariuka.sq@gmail.com>
2022-09-08 13:27:04 +0800

    url update

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -31,7 +31,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/'
+            'https://github.com/erxes/erxes/blob/erxes-docs-blog/docs/docs/'
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css')
