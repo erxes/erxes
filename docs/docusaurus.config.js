b3ed8a5761585c3543d8d7506df0d5e427893b8d
Ariukasq1 <Ariuka.sq@gmail.com>
2022-09-08 13:41:10 +0800

    update

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -30,7 +30,7 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: 'https://github.com/erxes/erxes/edit/erxes-docs-blog/docs'
+          editUrl: 'https://github.com/erxes/erxes/edit/erxes-docs-blog'
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css')
