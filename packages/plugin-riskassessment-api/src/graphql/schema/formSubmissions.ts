0461e97a488e5c96b7589a0935d1e530e144e622
Tuguldur Ch <tuguldur.ch@nmma.co>
2022-09-15 17:00:29 +0800

    removed risk assessment plugin

--- packages/plugin-riskassessment-api/src/graphql/schema/formSubmissions.ts
+++
@@ -1,9 +0,0 @@
-import { commonFormSubmissionsTypes } from './common';
-
-export const types = `
-`;
-export const queries = `
-`;
-export const mutations = `
-    riskFormSaveSubmissions(${commonFormSubmissionsTypes}):JSON
-`;
