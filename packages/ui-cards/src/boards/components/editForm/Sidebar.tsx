9dc195960c81c813eabcbee6d04aef2f1bd7de08
Tuguldur Ch <tuguldur.ch@nmma.co>
2022-09-15 15:22:54 +0800

    updates

--- packages/ui-cards/src/boards/components/editForm/Sidebar.tsx
+++ packages/ui-cards/src/boards/components/editForm/Sidebar.tsx
@@ -1,18 +1,16 @@
-import FormGroup from '@erxes/ui/src/components/form/Group'
-import ControlLabel from '@erxes/ui/src/components/form/Label'
-import SelectTeamMembers from '@erxes/ui/src/team/containers/SelectTeamMembers'
-import { isEnabled } from '@erxes/ui/src/utils/core'
-import React from 'react'
-import { RightContent } from '../../styles/item'
-import { IItem, IOptions } from '../../types'
-import SidebarConformity from './SidebarConformity'
+import FormGroup from '@erxes/ui/src/components/form/Group';
+import ControlLabel from '@erxes/ui/src/components/form/Label';
+import SelectTeamMembers from '@erxes/ui/src/team/containers/SelectTeamMembers';
+import { isEnabled } from '@erxes/ui/src/utils/core';
+import React from 'react';
+import { RightContent } from '../../styles/item';
+import { IItem, IOptions } from '../../types';
+import SidebarConformity from './SidebarConformity';
 
 type Props = {
   item: IItem;
   saveItem: (doc: { [key: string]: any }) => void;
-  sidebar?: (
-    saveItem?: (doc: { [key: string]: any }) => void
-  ) => React.ReactNode;
+  sidebar?: (saveItem?: (doc: { [key: string]: any }) => void) => React.ReactNode;
   options: IOptions;
   renderItems: () => React.ReactNode;
   updateTimeTrack: (
@@ -29,22 +27,23 @@ class Sidebar extends React.Component<Props> {
   render() {
     const { item, saveItem, sidebar } = this.props;
 
-    const userOnChange = usrs => saveItem({ assignedUserIds: usrs });
-    const assignedUserIds = (item.assignedUsers || []).map(user => user._id);
+    const userOnChange = (usrs) => saveItem({ assignedUserIds: usrs });
+    const assignedUserIds = (item.assignedUsers || []).map((user) => user._id);
 
     return (
       <RightContent>
         <FormGroup>
           <ControlLabel>Assigned to</ControlLabel>
           <SelectTeamMembers
-            label="Choose users"
-            name="assignedUserIds"
+            label='Choose users'
+            name='assignedUserIds'
             initialValue={assignedUserIds}
             onSelect={userOnChange}
           />
         </FormGroup>
 
         {isEnabled('products') && sidebar && sidebar(saveItem)}
+
         <SidebarConformity {...this.props} />
       </RightContent>
     );
