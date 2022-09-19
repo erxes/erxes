37a42e89e3f7099b021a490ef8c31433c28f4af2
Gerelsukh <48400228+lightii@users.noreply.github.com>
2022-09-19 10:18:56 +0800

    custom fields data for reports

---
+++ packages/dashboard/schema/CustomersCustomfieldsdata.js
@@ -0,0 +1,54 @@
+const { tableSchema } = require('../tablePrefix');
+
+cube(`CustomersCustomfieldsdata`, {
+  sql: `SELECT * FROM ${tableSchema()}.\`customers_customFieldsData\``,
+
+  preAggregations: {
+    // Pre-Aggregations definitions go here
+    // Learn more here: https://cube.dev/docs/caching/pre-aggregations/getting-started
+  },
+
+  joins: {
+    Fields: {
+      sql: `${CUBE}.\`customFieldsData.field\` = ${Fields}._id`,
+      relationship: `belongsTo`
+    },
+
+    Customers: {
+      sql: `${CUBE}._id = ${Customers}._id`,
+      relationship: `belongsTo`
+    }
+  },
+
+  measures: {},
+
+  dimensions: {
+    _id: {
+      sql: `CONCAT(${CUBE}._id)`,
+      type: `string`,
+      primaryKey: true
+    },
+
+    customfieldsdataField: {
+      sql: `${Fields}.\`text\``,
+      type: `string`,
+      title: `Fields`
+    },
+
+    customfieldsdataStringvalue: {
+      sql: `${CUBE}.\`customFieldsData.stringValue\``,
+      type: `string`,
+      title: `String Value`,
+      shown: false
+    },
+
+    customfieldsdataDatevalue: {
+      sql: `${CUBE}.\`customFieldsData.dateValue\``,
+      type: `time`,
+      title: `Date Value`,
+      shown: false
+    }
+  },
+
+  dataSource: `default`
+});
