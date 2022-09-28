1acbc093fe093b2b8afff31facc4baf8a5fe3b84
soyombo <40427263+soyombo-baterdene@users.noreply.github.com>
2022-09-28 15:36:20 +0800

    add socialPay handler

--- packages/plugin-payment-api/src/payments/socialPay/utils.ts
+++ packages/plugin-payment-api/src/payments/socialPay/utils.ts
@@ -4,10 +4,43 @@ import * as crypto from 'crypto';
 import { SOCIALPAY_ACTIONS, SOCIALPAY_ENDPOINT } from '../../../constants';
 import { IModels } from '../../connectionResolver';
 
-export const socialPayHandler = async (models: IModels, body) => {
-  console.log('body', body);
+export const socialPayHandler = async (models: IModels, data) => {
+  const { resp_code, resp_desc, amount, checksum, invoice, terminal } = data;
 
-  return models.PaymentConfigs.find({});
+  let status = 'success';
+  let description = resp_desc;
+
+  if (resp_code !== '00') {
+    status = 'failed';
+  }
+  try {
+    const { body } = await socialPayInvoiceCheck({
+      amount,
+      checksum,
+      invoice,
+      terminal
+    });
+
+    if (body.response.resp_code !== '00') {
+      status = 'failed';
+      description = body.response.resp_desc;
+
+      throw new Error(body.response.resp_desc);
+    }
+
+    const invoiceObj = await models.SocialPayInvoices.getSocialPayInvoice(
+      invoice
+    );
+
+    await models.SocialPayInvoices.updateOne(
+      { _id: invoiceObj._id },
+      { status: 'paid' }
+    );
+
+    return invoiceObj;
+  } catch (e) {
+    throw new Error(e.message);
+  }
 };
 
 export const socialPayInvoiceCheck = async data => {
