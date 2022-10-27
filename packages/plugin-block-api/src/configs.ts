33d71633202e3feb1ee8ab36c90e36b1b9b6081d
Enkhtuvshin Narmandakh <enkeeee@Enkhtuvshins-MacBook-Air.local>
2022-10-27 10:54:56 +0800

    update block

--- packages/plugin-block-api/src/configs.ts
+++ packages/plugin-block-api/src/configs.ts
@@ -5,6 +5,7 @@ import { initBroker } from './messageBroker';
 import { getSubdomain } from '@erxes/api-utils/src/core';
 import { generateModels, models } from './connectionResolver';
 import { routeErrorHandling } from '@erxes/api-utils/src/requests';
+import { debugInfo } from '@erxes/api-utils/src/debuggers';
 
 export let mainDb;
 export let debug;
@@ -39,6 +40,9 @@ export default {
     app.post(
       '/tdb-receive',
       routeErrorHandling(async (req, res) => {
+        debugInfo(`tdb-recieve: `);
+        console.log(JSON.stringify(req));
+
         const body = JSON.stringify(req.body);
 
         const subdomain = getSubdomain(req);
