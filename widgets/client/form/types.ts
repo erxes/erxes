e0cdcdbdfb7548e55bc1426d2752b2c8b2f756fe
soyombo <e11iot.soko@gmail.com>
2022-10-28 19:48:38 +0800

    Update types.ts

--- widgets/client/form/types.ts
+++ widgets/client/form/types.ts
@@ -117,7 +117,6 @@ export interface ISaveFormResponse {
   errors?: IFieldError[];
   invoiceType?: string;
   invoiceResponse?: string;
-  messageId?: string;
 }
 
 export interface ILocationOption {
