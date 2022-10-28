c3b5d68bcfe49e5bec0f7757d4a629444aa21bfb
munkhsaikhan.g@nmma.co <munkhsaikhan.g@nmma.co>
2022-10-28 18:49:58 +0800

    clean loop

--- packages/plugin-processes-api/src/graphql/resolvers/customResolver/jobRefer.ts
+++ packages/plugin-processes-api/src/graphql/resolvers/customResolver/jobRefer.ts
@@ -1,79 +1,75 @@
 import { IJobReferDocument } from '../../../models/definitions/jobs';
 import { IContext } from '../../../connectionResolver';
-import { JOB_CATEGORY_STATUSES } from '../../../models/definitions/constants';
 import { sendProductsMessage } from '../../../messageBroker';
+
+const getProductAndUoms = async (subdomain, productsData) => {
+  const productIds = productsData.map(n => n.productId);
+
+  const products = await sendProductsMessage({
+    subdomain,
+    action: 'find',
+    data: { query: { _id: { $in: productIds } } },
+    isRPC: true,
+    defaultValue: []
+  });
+
+  const productById = {};
+  for (const product of products) {
+    productById[product._id] = product;
+  }
+
+  const uomIds = productsData.map(n => n.uomId);
+  const uoms = await sendProductsMessage({
+    subdomain,
+    action: 'findUom',
+    data: { _id: { $in: uomIds } },
+    isRPC: true,
+    defaultValue: []
+  });
+
+  const uomById = {};
+  for (const uom of uoms) {
+    uomById[uom._id] = uom;
+  }
+  return { productById, uomById };
+};
 
 export default {
   __resolveReference({ _id }, { models }: IContext) {
     return models.JobRefers.findOne({ _id });
   },
 
-  async needProducts(
-    jobRefer: IJobReferDocument,
-    {},
-    { models, subdomain }: IContext
-  ) {
-    const jobRefers = await models.JobRefers.findOne({
-      _id: jobRefer._id,
-      status: { $ne: JOB_CATEGORY_STATUSES.ARCHIVED }
-    });
+  async needProducts(jobRefer: IJobReferDocument, {}, { subdomain }: IContext) {
+    const needProducts = jobRefer.needProducts || [];
 
-    const needProducts = jobRefers?.needProducts || [];
+    const { productById, uomById } = await getProductAndUoms(
+      subdomain,
+      needProducts
+    );
 
     for await (const need of needProducts) {
-      const uom =
-        (await sendProductsMessage({
-          subdomain,
-          action: 'findOneUom',
-          data: { _id: need.uomId || '' },
-          isRPC: true
-        })) || null;
-
-      const product =
-        (await sendProductsMessage({
-          subdomain,
-          action: 'findOne',
-          data: { _id: need.productId || '' },
-          isRPC: true
-        })) || null;
-
-      need.product = product;
-      need.uom = uom;
+      need.product = productById[need.productId] || {};
+      need.uom = uomById[need.uomId] || {};
     }
 
     return needProducts;
   },
+
   async resultProducts(
     jobRefer: IJobReferDocument,
     {},
-    { models, subdomain }: IContext
+    { subdomain }: IContext
   ) {
-    const jobRefers = await models.JobRefers.findOne({
-      _id: jobRefer._id,
-      status: { $ne: JOB_CATEGORY_STATUSES.ARCHIVED }
-    });
+    const resultProducts = jobRefer.resultProducts || [];
 
-    const resultProducts = jobRefers?.resultProducts || [];
+    const { productById, uomById } = await getProductAndUoms(
+      subdomain,
+      resultProducts
+    );
 
     for await (const result of resultProducts) {
-      const uom =
-        (await sendProductsMessage({
-          subdomain,
-          action: 'findOneUom',
-          data: { _id: result.uomId || '' },
-          isRPC: true
-        })) || null;
-
-      const product =
-        (await sendProductsMessage({
-          subdomain,
-          action: 'findOne',
-          data: { _id: result.productId || '' },
-          isRPC: true
-        })) || null;
-
-      result.product = product;
-      result.uom = uom;
+      result.product = productById[result.productId] || {};
+      result.uom = uomById[result.uomId] || {};
     }
 
     return resultProducts;
