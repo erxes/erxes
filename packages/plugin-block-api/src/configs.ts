a469e78b4e6f4fded79e1c4e4762d9843c4ca73e
lightii <gerelsukh0705@gmail.com>
2022-10-28 15:34:33 +0800

    bank status

--- packages/plugin-block-api/src/configs.ts
+++ packages/plugin-block-api/src/configs.ts
@@ -1,11 +1,12 @@
 import typeDefs from './graphql/typeDefs';
 import resolvers from './graphql/resolvers';
 
-import { initBroker } from './messageBroker';
+import { initBroker, sendContactsMessage } from './messageBroker';
 import { getSubdomain } from '@erxes/api-utils/src/core';
-import { generateModels, models } from './connectionResolver';
+import { generateModels } from './connectionResolver';
 import { routeErrorHandling } from '@erxes/api-utils/src/requests';
 import { debugInfo } from '@erxes/api-utils/src/debuggers';
+import { getBalance, sendSms, updateBalance } from './utils';
 
 export let mainDb;
 export let debug;
@@ -44,13 +45,51 @@ export default {
         console.log('/tdb/receive');
         console.log(JSON.stringify(req.body));
 
-        const body = JSON.stringify(req.body);
+        const body = req.body;
 
         const subdomain = getSubdomain(req);
 
         const models = await generateModels(subdomain);
 
-        await models.Transactions.create({ body });
+        const customer = await sendContactsMessage({
+          subdomain,
+          action: 'customers.findOne',
+          data: { primaryPhone: body.description },
+          isRPC: true,
+          defaultValue: {}
+        });
+
+        const erxesCustomerId = customer._id;
+
+        if (customer) {
+          const balance = await getBalance(subdomain, erxesCustomerId);
+
+          const newBalance =
+            balance + parseFloat(body.amount.replace(/,/g, ''));
+
+          try {
+            await updateBalance(subdomain, erxesCustomerId, newBalance);
+
+            await models.Transactions.create({
+              bankStatus: 'success',
+              body: JSON.stringify(req.body)
+            });
+
+            const msgBody = `Таны ${body.amount} төгрөгийн орлого амжилттай орлоо.`;
+
+            await sendSms(subdomain, customer.primaryPhone, msgBody);
+          } catch (e) {
+            await models.Transactions.create({
+              bankStatus: 'error',
+              body: JSON.stringify(req.body)
+            });
+          }
+        } else {
+          await models.Transactions.create({
+            bankStatus: 'error',
+            body: JSON.stringify(req.body)
+          });
+        }
 
         return res.json({ response: 'success' });
       })
