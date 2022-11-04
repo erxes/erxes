dddc7afbf1a766cc58d4a4ebbcdd166d8ea77e49
soyombo <e11iot.soko@gmail.com>
2022-11-04 09:22:04 +0800

    Update Notifications.tsx

--- client-portal/modules/main/components/notifications/Notifications.tsx
+++ client-portal/modules/main/components/notifications/Notifications.tsx
@@ -1,46 +1,24 @@
 import React from 'react';
-import Button from '../../../common/Button';
 
-import FormControl from '../../../common/form/Control';
 import { getConfigColor } from '../../../common/utils';
-import { NotificationList } from '../../../styles/main';
-import { TabContainers, TabTitle, Wrapper } from '../../../styles/tasks';
-import { INotification, IUser } from '../../../types';
-import Alert from '../../../utils/Alert';
-import DataWithLoader from '../DataWithLoader';
-import Row from './Row';
+import { TabContainers, TabTitle } from '../../../styles/tasks';
+import { IUser } from '../../../types';
 import Notifications from '../../containers/notifications/List';
 
 type Props = {
-  //   notifications: INotification[];
   count: number;
   currentUser: IUser;
-  //   loading: boolean;
-  //   refetch?: () => void;
-  //   markAsRead: (notificationIds?: string[]) => void;
   config: any;
 };
 
 const List = (props: Props) => {
-  //   const { loading, notifications, count } = props;
   const { config, count, currentUser } = props;
 
   const [currentTab, setCurrentTab] = React.useState('Recent');
-  // return (
-  //   <DataWithLoader
-  //     data={content}
-  //     loading={loading}
-  //     count={count}
-  //     emptyText="Looks like you are all caught up!"
-  //     emptyImage="/images/actions/17.svg"
-  //   />
-  // );
 
   const tabTitles = ['Recent', 'Unread'];
 
   const requireRead = currentTab === 'Unread';
-
-  // console.log('requireRead', requireRead);
 
   return (
     <>
