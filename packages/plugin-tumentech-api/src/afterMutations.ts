2a210729c65212a849882a3043504cc98082bce0
soyombo <e11iot.soko@gmail.com>
2022-11-09 15:41:34 +0800

    update tumentech aftermutations

--- packages/plugin-tumentech-api/src/afterMutations.ts
+++ packages/plugin-tumentech-api/src/afterMutations.ts
@@ -1,11 +1,7 @@
-import {
-  sendCardsMessage,
-  sendClientPortalMessage,
-  sendContactsMessage
-} from './messageBroker';
+import { afterDealCreate, afterDealUpdate } from './afterMutations/deals';
 
 export default {
-  'cards:deal': ['create']
+  'cards:deal': ['create', 'update'],
 };
 
 export const afterMutationHandlers = async (subdomain, params) => {
@@ -13,56 +9,13 @@ export const afterMutationHandlers = async (subdomain, params) => {
 
   if (type === 'cards:deal') {
     if (action === 'create') {
-      const deal = params.object;
+      await afterDealCreate(subdomain, params);
+    }
 
-      const stage = await sendCardsMessage({
-        subdomain,
-        action: 'stages.findOne',
-        data: {
-          _id: deal.stageId
-        },
-        isRPC: true,
-        defaultValue: {}
-      });
-
-      if (stage.code && stage.code === 'newOrder') {
-        const drivers = await sendContactsMessage({
-          subdomain,
-          action: 'customers.find',
-          data: {
-            tagIds: 'FwJtL7Tw7FWQT4nJW'
-          },
-          isRPC: true,
-          defaultValue: {}
-        });
-
-        const driverIds = drivers.map(driver => driver._id);
-
-        const cpUsers = await sendClientPortalMessage({
-          subdomain,
-          action: 'clientPortalUsers.find',
-          data: {
-            erxesCustomerId: { $in: driverIds },
-            clientPortalId: process.env.MOBILE_CP_ID || ''
-          },
-          isRPC: true,
-          defaultValue: []
-        });
-
-        sendClientPortalMessage({
-          subdomain,
-          action: 'sendNotification',
-          data: {
-            title: 'Шинэ зар орлоо',
-            content: 'Шинэ ажлын зар орсон байна!',
-            receivers: cpUsers.map(u => u._id),
-            notifType: 'system',
-            link: ``,
-            isMobile: true
-          }
-        });
-      }
+    if (action === 'update') {
+      await afterDealUpdate(subdomain, params);
     }
     return;
   }
+  
 };
