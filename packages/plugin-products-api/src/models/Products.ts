876f2381df04608e1275bb07ea78910ab85521fc
munkhsaikhan.g@nmma.co <munkhsaikhan.g@nmma.co>
2022-11-21 03:46:12 +0800

    update products

--- packages/plugin-products-api/src/models/Products.ts
+++ packages/plugin-products-api/src/models/Products.ts
@@ -45,6 +45,27 @@ export const loadProductClass = (models: IModels, subdomain: string) => {
       return product;
     }
 
+    static async checkUOM(doc) {
+      if (doc.uomId) {
+        return doc.uomId;
+      }
+
+      const configs = await models.ProductsConfigs.find({
+        code: { $in: ['isRequireUOM', 'defaultUOM'] }
+      }).lean();
+
+      const isRequireUOM = (configs.find(c => c.code === 'isRequireUOM') || {})
+        .value;
+      const defaultUOM = (configs.find(c => c.code === 'defaultUOM') || {})
+        .value;
+
+      if (isRequireUOM && defaultUOM) {
+        return defaultUOM;
+      }
+
+      return '';
+    }
+
     static async checkCodeDuplication(code: string) {
       const product = await models.Products.findOne({
         code,
@@ -110,6 +131,8 @@ export const loadProductClass = (models: IModels, subdomain: string) => {
         isRPC: true
       });
 
+      doc.uomId = await this.checkUOM(doc);
+
       return models.Products.create(doc);
     }
 
@@ -136,10 +159,10 @@ export const loadProductClass = (models: IModels, subdomain: string) => {
           isRPC: true
         });
       }
-
+      doc.uomId = await this.checkUOM(doc);
       await models.Products.updateOne({ _id }, { $set: doc });
 
-      return models.Products.findOne({ _id });
+      return await models.Products.findOne({ _id }).lean();
     }
 
     /**
@@ -257,6 +280,7 @@ export const loadProductClass = (models: IModels, subdomain: string) => {
         mergedIds: productIds,
         name,
         type,
+        uomId: await this.checkUOM(productFields),
         description,
         categoryId,
         vendorId
