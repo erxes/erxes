bd5d399d43ea5c1e88a6d5561d1dccf68cc8a27c
Buyantogtokh <buyantogtokh.s@nmma.co>
2022-11-29 04:43:08 +0800

    change fb account auth url

--- packages/plugin-facebook-ui/src/containers/Authorization.tsx
+++ packages/plugin-facebook-ui/src/containers/Authorization.tsx
@@ -19,7 +19,7 @@ type Props = {
 export const Authorization = (props: Props) => {
   const { queryParams } = props;
 
-  if (queryParams.fbAuthorized || queryParams.gmailAuthorized) {
+  if (queryParams.fbAuthorized) {
     window.opener.location.reload();
     window.close();
   }
