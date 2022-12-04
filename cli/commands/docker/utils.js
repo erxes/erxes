a57c2314f49f7ee795872b3cf478ef16ccb650d1
mambatukaa <sbatuka9@gmail.com>
2022-12-04 12:10:20 +0800

    update s3 object name

--- cli/commands/docker/utils.js
+++ cli/commands/docker/utils.js
@@ -144,7 +144,7 @@ const syncUI = async ({ name, image_tag, ui_location }) => {
       if (tag === 'dev') {
         s3_location = `https://erxes-dev-plugins.s3.us-west-2.amazonaws.com/uis/${plName}`;
       } else {
-        s3_location = `https://erxes-timeclock-plugin.s3.us-west-2.amazonaws.com/uis/${plName}/${tag}`;
+        s3_location = `https://erxes-timeclock-plugin.s3.us-west-2.amazonaws.com/uis/${plName}`;
       }
     }
 
@@ -564,7 +564,7 @@ const up = async ({ uis, fromInstaller }) => {
       pluginsMapLocation =
         'https://erxes-dev-plugins.s3.us-west-2.amazonaws.com/pluginsMap.js';
     } else {
-      pluginsMapLocation = `https://erxes-release-plugins.s3.us-west-2.amazonaws.com/${image_tag}/pluginsMap.js`;
+      pluginsMapLocation = `https://erxes-timeclock-plugin.s3.us-west-2.amazonaws.com/timeclock/pluginsMap.js`;
     }
   }
 
