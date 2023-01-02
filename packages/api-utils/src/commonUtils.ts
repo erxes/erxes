3d970bf6ba1dd1c48c6febbb4c98ed0fcdb4842c
mambatukaa <sbatuka9@gmail.com>
2023-01-02 14:38:48 +0800

    add return type

--- packages/api-utils/src/commonUtils.ts
+++ packages/api-utils/src/commonUtils.ts
@@ -110,7 +110,7 @@ export const removeExtraSpaces = text => {
 };
 
 // check if valid url
-export const isValidURL = (url: string) => {
+const isValidURL = (url: string): boolean => {
   try {
     return Boolean(new URL(url));
   } catch (e) {
