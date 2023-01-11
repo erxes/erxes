726d540ce9e7bd1b8b3e854c0c648cc1d38f5b49
enkhtuvshinD <87625210+enkhtuvshinD@users.noreply.github.com>
2023-01-11 17:37:38 +0800

    Update ClientPortal.ts
    back changes clientportal

--- packages/plugin-clientportal-api/src/models/ClientPortal.ts
+++ packages/plugin-clientportal-api/src/models/ClientPortal.ts
@@ -11,7 +11,6 @@ import {
   removeLastTrailingSlash,
   removeExtraSpaces
 } from '@erxes/api-utils/src/commonUtils';
-import { serviceDiscovery } from '../configs';
 
 export interface IClientPortalModel extends Model<IClientPortalDocument> {
   getConfig(_id: string): Promise<IClientPortalDocument>;
@@ -30,36 +29,8 @@ export const loadClientPortalClass = (models: IModels) => {
       return config;
     }
 
-    static removeFieldsWithoutPermission = async doc => {
-      const kbAvailable = await serviceDiscovery.isEnabled('knowledgebase');
-      const cardAvailable = await serviceDiscovery.isEnabled('cards');
-      const inboxAviable = await serviceDiscovery.isEnabled('inbox');
-
-      if (!cardAvailable) {
-        delete doc.ticketLabel;
-        delete doc.taskLabel;
-        delete doc.taskPublicBoardId;
-        delete doc.taskPublicPipelineId;
-        delete doc.taskStageId;
-        delete doc.taskPipelineId;
-        delete doc.taskBoardId;
-        delete doc.ticketStageId;
-        delete doc.ticketPipelineId;
-        delete doc.ticketBoardId;
-      }
-      if (!kbAvailable) {
-        delete doc.knowledgeBaseLabel;
-        delete doc.knowledgeBaseTopicId;
-      }
-      if (inboxAviable) {
-        delete doc.messengerBrandCode;
-      }
-      return doc;
-    };
-
     public static async createOrUpdateConfig({ _id, ...doc }: IClientPortal) {
       let config = await models.ClientPortals.findOne({ _id });
-      doc = await ClientPortal.removeFieldsWithoutPermission(doc);
 
       if (doc.url) {
         doc.url = removeExtraSpaces(removeLastTrailingSlash(doc.url));
