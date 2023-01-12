cc5f30115fa620e01733e017693a1fd1d102fe20
Dulguun Otgon <dulguun0225@gmail.com>
2023-01-12 16:31:28 +0800

    admin ui prog

--- packages/plugin-forum-ui/src/components/PostForm.tsx
+++ packages/plugin-forum-ui/src/components/PostForm.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useRef } from 'react';
 import PostContentEditor from './PostContentEditor';
 import CategorySelect from '../containers/CategorySelect';
+import PollOptions from './PollOptions';
 
 const PostForm: React.FC<{ post?: any; onSubmit?: (any) => any }> = ({
   post,
@@ -10,6 +11,18 @@ const PostForm: React.FC<{ post?: any; onSubmit?: (any) => any }> = ({
   const [thumbnail, setThumbnail] = useState(post?.thumbnail || '');
   const [categoryId, setCategoryId] = useState(post?.categoryId || '');
   const [description, setDescription] = useState(post?.description || '');
+
+  const initialPollOptionsById = {};
+  post?.pollOptions?.forEach(option => {
+    initialPollOptionsById[option._id] = option;
+  });
+
+  const [pollOptionsById, setPollOptionsById] = useState(
+    initialPollOptionsById
+  );
+
+  const pollOptions: any[] = Object.values(pollOptionsById);
+  pollOptions.sort((a: any, b: any) => a.order - b.order);
 
   const editorRef = useRef<any>(null);
   const preSubmit = e => {
@@ -61,6 +74,43 @@ const PostForm: React.FC<{ post?: any; onSubmit?: (any) => any }> = ({
         />
 
         <br />
+
+        <div style={{ margin: 20, padding: 20 }}>
+          Poll options:
+          <PollOptions
+            options={pollOptions}
+            onChange={(_id, title, order) => {
+              setPollOptionsById(prev => ({
+                ...prev,
+                [_id]: {
+                  _id,
+                  title,
+                  order
+                }
+              }));
+            }}
+            onAdd={() => {
+              const newPollOption = {
+                _id: Math.random()
+                  .toString(36)
+                  .slice(2),
+                title: '',
+                order: pollOptions[pollOptions.length - 1]?.order + 1 || 0,
+                isNew: true
+              };
+              setPollOptionsById(prev => ({
+                ...prev,
+                [newPollOption._id]: newPollOption
+              }));
+            }}
+            onRemove={_id => {
+              setPollOptionsById((prev: any) => {
+                const { [_id]: _, ...rest } = prev;
+                return rest;
+              });
+            }}
+          />
+        </div>
 
         <button>Submit</button>
       </form>
