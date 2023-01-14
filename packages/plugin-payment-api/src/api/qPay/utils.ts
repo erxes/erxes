8ec9cb0eb6873633e5b2667f9177247b98f95c3b
soyombo <e11iot.soko@gmail.com>
2023-01-14 22:41:37 +0800

    Update utils.ts

--- packages/plugin-payment-api/src/api/qPay/utils.ts
+++ packages/plugin-payment-api/src/api/qPay/utils.ts
@@ -89,8 +89,6 @@ export const createInvoice = async (
       callback_url: `${MAIN_API_DOMAIN}/pl:payment/callback/${PAYMENT_KINDS.QPAY}?identifier=${invoice.identifier}`
     };
 
-    console.log('data', data);
-
     const requestOptions = {
       url: `${QPAY_ENDPOINT}${QPAY_ACTIONS.INVOICE}`,
       method: 'POST',
@@ -130,8 +128,7 @@ export const getInvoice = async (
     };
 
     try {
-      // return sendRequest(requestOptions);
-      return { invoice_status: 'CLOSED' };
+      return sendRequest(requestOptions);
     } catch (e) {
       throw new Error(e.message);
     }
