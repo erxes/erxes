d93ef61eb0cd9da5ffda172e9a820c000eacc9e6
Ariukasq1 <Ariuka.sq@gmail.com>
2023-02-14 14:23:01 +0800

    topicId added

--- client-portal/pages/knowledge-base/article/index.tsx
+++ client-portal/pages/knowledge-base/article/index.tsx
@@ -1,18 +1,23 @@
-import { useRouter } from "next/router";
-import React from "react";
-import ArticleDetail from "../../../modules/knowledgeBase/containers/ArticleDetail";
-import ArticleListContainer from "../../../modules/knowledgeBase/containers/ArticleList";
-import Search from "../../../modules/main/components/Search";
-import Layout from "../../../modules/main/containers/Layout";
-import { Store } from "../../../modules/types";
+import { useRouter } from 'next/router';
+import React from 'react';
+import ArticleDetail from '../../../modules/knowledgeBase/containers/ArticleDetail';
+import ArticleListContainer from '../../../modules/knowledgeBase/containers/ArticleList';
+import Search from '../../../modules/main/components/Search';
+import Layout from '../../../modules/main/containers/Layout';
+import { Store } from '../../../modules/types';
 
 export default function Category() {
   const router = useRouter();
   const { searchValue } = router.query;
 
-  const renderContent = (props) => {
+  const renderContent = props => {
     if (searchValue) {
-      return <ArticleListContainer searchValue={searchValue} />;
+      return (
+        <ArticleListContainer
+          searchValue={searchValue}
+          topicId={props.config?.knowledgeBaseTopicId}
+        />
+      );
     }
 
     return <ArticleDetail {...props} queryParams={router.query} />;
