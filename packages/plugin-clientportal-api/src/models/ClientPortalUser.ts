ef849f8326772a7abd9cf34a9c13452fa7e2fbf5
soyombo <40427263+soyombo-baterdene@users.noreply.github.com>
2023-03-01 15:16:43 +0800

    client portal user manual verification (#4237)
    * add client portal user manual verification

--- packages/plugin-clientportal-api/src/models/ClientPortalUser.ts
+++ packages/plugin-clientportal-api/src/models/ClientPortalUser.ts
@@ -753,6 +753,16 @@ export const loadClientPortalUserClass = (models: IModels) => {
         throw new Error('User is not verified');
       }
 
+      const cp = await models.ClientPortals.getConfig(clientPortalId);
+
+      if (
+        cp.manualVerificationConfig &&
+        (!user.verificationRequest ||
+          user.verificationRequest.status !== 'approved')
+      ) {
+        throw new Error('User is not verified');
+      }
+
       const valid = await this.comparePassword(password, user.password);
 
       if (!valid) {
