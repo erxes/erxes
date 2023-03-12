ed0f64061b385ef8f192556ce2474323e72c80c1
munkhsaikhan.g@nmma.co <munkhsaikhan.g@nmma.co>
2023-03-13 02:34:02 +0800

    pricing performance up

--- packages/plugin-posclient-api/src/graphql/resolvers/queries/products.ts
+++ packages/plugin-posclient-api/src/graphql/resolvers/queries/products.ts
@@ -346,7 +346,7 @@ const productQueries = {
       data: {
         departmentId: config.departmentId,
         branchId: config.branchId,
-        product
+        products: [product]
       },
       isRPC: true,
       defaultValue: {}
