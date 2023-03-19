5874a74f0d81df6e9b19e7ccd803b20eeb80b2fc
BatAmar Battulga <mrbatamar@gmail.com>
2023-03-19 19:01:11 +0800

    rover command fix

--- packages/gateway/src/apollo-router/supergraph-compose.ts
+++ packages/gateway/src/apollo-router/supergraph-compose.ts
@@ -52,10 +52,12 @@ const createSupergraphConfig = (proxyTargets: ErxesProxyTarget[]) => {
 
 const supergraphComposeOnce = async () => {
   const superGraphqlNext = supergraphPath + '.next';
+
   execSync(
     `node dist/node_modules/.bin/rover supergraph compose --config ${supergraphConfigPath} --output ${superGraphqlNext} --elv2-license=accept`,
-    { stdio: 'pipe' }
+    { stdio: 'inherit' }
   );
+
   if (
     !fs.existsSync(supergraphPath) ||
     !isSameFile(supergraphPath, superGraphqlNext)
