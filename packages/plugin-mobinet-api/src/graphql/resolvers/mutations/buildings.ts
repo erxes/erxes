62e651718caa59f086846d4362c604b481b26207
soyombo <e11iot.soko@gmail.com>
2023-03-24 15:00:36 +0800

    debug

--- packages/plugin-mobinet-api/src/graphql/resolvers/mutations/buildings.ts
+++ packages/plugin-mobinet-api/src/graphql/resolvers/mutations/buildings.ts
@@ -111,8 +111,6 @@ const mutations = {
     { _id, buildingData, ticketData, quarterId },
     { models, subdomain, cpUser }: IContext
   ) => {
-    console.log('cpUser', cpUser);
-
     if (!cpUser) {
       throw new Error('login required');
     }
