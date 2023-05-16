4c17efb4963a9cbfeeaa35e25fe75be4719f24e6
uuganaa1007 <80632055+uuganaa1007@users.noreply.github.com>
2023-05-16 21:12:07 +0800

    Bug Fixed

---
+++ packages/ui-cards/src/purchases/containers/product/ProductForm.tsx
@@ -0,0 +1,83 @@
+import * as compose from 'lodash.flowright';
+import gql from 'graphql-tag';
+import ProductForm from '../../components/product/ProductForm';
+import React from 'react';
+import { AppConsumer } from 'coreui/appContext';
+import { graphql } from 'react-apollo';
+import { CostQueryResponse, IProduct } from '@erxes/ui-products/src/types';
+import { ProductCategoriesQueryResponse } from '@erxes/ui-products/src/types';
+import { queries } from '../../graphql';
+import { withProps } from '@erxes/ui/src/utils/core';
+import {
+  IPurchase,
+  IPaymentsData,
+  IProductData,
+  IExpensesData
+} from '../../types';
+
+type Props = {
+  onChangeProductsData: (productsData: IProductData[]) => void;
+  saveProductsData: () => void;
+  onChangePaymentsData: (paymentsData: IPaymentsData) => void;
+  onchangeExpensesData: (expensesData: IExpensesData[]) => void;
+  expensesData: IExpensesData[];
+  productsData: IProductData[];
+  products: IProduct[];
+  paymentsData?: IPaymentsData;
+  currentProduct?: string;
+  closeModal: () => void;
+  purchaseQuery: IPurchase;
+  productCategoriesQuery: ProductCategoriesQueryResponse;
+  costsQuery: any;
+};
+
+class ProductFormContainer extends React.Component<Props> {
+  render() {
+    return (
+      <AppConsumer>
+        {({ currentUser }) => {
+          if (!currentUser) {
+            return;
+          }
+
+          const configs = currentUser.configs || {};
+
+          const { productCategoriesQuery } = this.props;
+
+          const categories = productCategoriesQuery.productCategories || [];
+
+          const { costsQuery } = this.props;
+
+          const costsQueryData = costsQuery.costs || [];
+
+          const { purchaseQuery } = this.props;
+
+          const costPriceQuery = purchaseQuery.products || 0;
+          const extendedProps = {
+            ...this.props,
+            costsQueryData,
+            costPriceQuery,
+            categories: categories,
+            loading: productCategoriesQuery.loading,
+            uom: configs.dealUOM || [],
+            currencies: configs.dealCurrency || []
+          };
+          return <ProductForm {...extendedProps} />;
+        }}
+      </AppConsumer>
+    );
+  }
+}
+export default withProps<Props>(
+  compose(
+    graphql<{}, ProductCategoriesQueryResponse, {}>(
+      gql(queries.productCategories),
+      {
+        name: 'productCategoriesQuery'
+      }
+    ),
+    graphql<{}, CostQueryResponse, {}>(gql(queries.costs), {
+      name: 'costsQuery'
+    })
+  )(ProductFormContainer)
+);
---
+++ packages/ui-cards/src/purchases/graphql/queries.ts
@@ -0,0 +1,223 @@
+import { queries } from '@erxes/ui-products/src/graphql';
+import { commonFields, commonListFields } from '../../boards/graphql/mutations';
+import {
+  conformityQueryFieldDefs,
+  conformityQueryFields
+} from '../../conformity/graphql/queries';
+
+const commonParams = `
+  $companyIds: [String],
+  $customerIds: [String],
+  $assignedUserIds: [String],
+  $productIds: [String],
+  $labelIds: [String],
+  $search: String,
+  $priority: [String],
+  $date: ItemDate,
+  $pipelineId: String,
+  $parentId: String,
+  $closeDateType: String,
+  $sortField: String,
+  $sortDirection: Int,
+  $userIds: [String],
+  $segment: String,
+  $segmentData:String,
+  $assignedToMe: String,
+  $startDate: String,
+  $endDate: String,
+  $tagIds: [String],
+  $noSkipArchive: Boolean
+  $branchIds:[String]
+  $departmentIds:[String]
+  ${conformityQueryFields}
+`;
+
+const commonParamDefs = `
+  companyIds: $companyIds,
+  customerIds: $customerIds,
+  assignedUserIds: $assignedUserIds,
+  priority: $priority,
+  productIds: $productIds,
+  labelIds: $labelIds,
+  search: $search,
+  date: $date,
+  pipelineId: $pipelineId,
+  parentId: $parentId,
+  closeDateType: $closeDateType,
+  sortField: $sortField,
+  sortDirection: $sortDirection,
+  userIds: $userIds,
+  segment: $segment,
+  segmentData: $segmentData,
+  assignedToMe: $assignedToMe,
+  startDate: $startDate,
+  endDate: $endDate,
+  tagIds: $tagIds,
+  noSkipArchive: $noSkipArchive
+  branchIds: $branchIds,
+  departmentIds: $departmentIds,
+  ${conformityQueryFieldDefs}
+`;
+
+export const purchaseFields = `
+  products
+  productsData
+  paymentsData
+  expensesData
+  amount
+`;
+
+const purchasesTotalAmounts = `
+  query purchasesTotalAmounts(
+    ${commonParams}
+  ) {
+    purchasesTotalAmounts(
+      ${commonParamDefs}
+    ) {
+        _id
+        name
+        currencies {
+          name
+          amount
+        }
+      }
+    }
+`;
+
+const purchases = `
+  query purchases(
+    $initialStageId: String,
+    $stageId: String,
+    $skip: Int,
+    $limit: Int,
+    ${commonParams}
+  ) {
+    purchases(
+      initialStageId: $initialStageId,
+      stageId: $stageId,
+      skip: $skip,
+      limit: $limit,
+      ${commonParamDefs}
+    ) {
+      products
+      amount
+      ${commonListFields}
+    }
+  }
+`;
+
+const purchasesTotalCount = `
+  query purchasesTotalCount(
+    $initialStageId: String,
+    $stageId: String,
+    $skip: Int,
+    ${commonParams}
+  ) {
+    purchasesTotalCount(
+      initialStageId: $initialStageId,
+      stageId: $stageId,
+      skip: $skip,
+      ${commonParamDefs}
+    )
+  }
+`;
+
+const archivedPurchasesParams = `
+  $pipelineId: String!
+  $search: String
+  $userIds: [String]
+  $priorities: [String]
+  $assignedUserIds: [String]
+  $labelIds: [String]
+  $productIds: [String]
+  $companyIds: [String]
+  $customerIds: [String]
+  $startDate: String
+  $endDate: String
+`;
+
+const archivedPurchasesArgs = `
+  pipelineId: $pipelineId
+  search: $search
+  userIds: $userIds
+  priorities: $priorities
+  assignedUserIds: $assignedUserIds
+  labelIds: $labelIds
+  productIds: $productIds
+  companyIds: $companyIds
+  customerIds: $customerIds
+  startDate: $startDate
+  endDate: $endDate
+`;
+
+const archivedPurchases = `
+  query archivedPurchases(
+    $page: Int
+    $perPage: Int
+    ${archivedPurchasesParams}
+  ) {
+    archivedPurchases(
+      page: $page
+      perPage: $perPage
+      ${archivedPurchasesArgs}
+    ) {
+      ${purchaseFields}
+      ${commonFields}
+    }
+  }
+`;
+
+const archivedPurchasesCount = `
+  query archivedPurchasesCount(
+    ${archivedPurchasesParams}
+  ) {
+    archivedPurchasesCount(
+      ${archivedPurchasesArgs}
+    )
+  }
+`;
+
+const purchaseDetail = `
+  query purchaseDetail($_id: String!) {
+    purchaseDetail(_id: $_id) {
+      ${purchaseFields}
+      ${commonFields}
+    }
+  }
+`;
+
+const productDetail = `
+  query productDetail($_id: String!) {
+    productDetail(_id: $_id) {
+      _id
+      name
+    }
+  }
+`;
+
+const checkDiscount = `
+  query checkDiscount($_id: String!, $products: [ProductField]) {
+    checkDiscount(_id: $_id, products: $products)
+  }
+`;
+
+const productCategories = queries.productCategories;
+
+const costs = `
+  query costs {
+	  costs 
+  }
+`;
+
+export default {
+  purchases,
+  purchasesTotalCount,
+  purchaseDetail,
+  productDetail,
+  productCategories,
+  purchasesTotalAmounts,
+  archivedPurchases,
+  archivedPurchasesCount,
+  checkDiscount,
+  costs
+};
