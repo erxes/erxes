cb918d8a6f06b0300bf201c7689d9cfd3cd189b9
Tuguldur Ch <tuguldur.ch@nmma.co>
2023-05-19 11:35:30 +0800

    little

--- packages/plugin-cards-api/src/models/definitions/common.ts
+++ packages/plugin-cards-api/src/models/definitions/common.ts
@@ -37,7 +37,7 @@ const customFieldSchema = new Schema(
   {
     field: { type: String },
     value: { type: Schema.Types.Mixed },
-    extraValue: { type: String },
+    extraValue: { type: String, optional: true },
     stringValue: { type: String, optional: true },
     numberValue: { type: Number, optional: true },
     dateValue: { type: Date, optional: true },
