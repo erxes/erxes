ceef4a8c5ba6ca6610f9491e2d8a40b2d853a5b8
Nandinbold Norovsambuu <nandinboldn@gmail.com>
2023-05-23 17:41:15 +0800

    group by filter on Deals, Tasks, Ticket (#4428)

--- client-portal/modules/tasks/containers/Task.tsx
+++ client-portal/modules/tasks/containers/Task.tsx
@@ -1,11 +1,11 @@
-import { Config, IUser, Store } from "../../types";
-import { gql, useQuery } from "@apollo/client";
+import { Config, IUser, Store } from '../../types';
+import { gql, useQuery } from '@apollo/client';
 
-import { AppConsumer } from "../../appContext";
-import React from "react";
-import Spinner from "../../common/Spinner";
-import Task from "../components/Task";
-import { queries } from "../graphql";
+import { AppConsumer } from '../../appContext';
+import React from 'react';
+import Spinner from '../../common/Spinner';
+import Task from '../components/Task';
+import { queries } from '../graphql';
 
 type Props = {
   currentUser: IUser;
@@ -17,11 +17,38 @@ function TaskContainer({ currentUser, ...props }: Props) {
     gql(queries.clientPortalTasks),
     {
       skip: !currentUser,
-      fetchPolicy: "network-only",
+      fetchPolicy: 'network-only'
     }
   );
 
-  if (loading) {
+  const { loading: loadingStages, data: stages = {} as any } = useQuery(
+    gql(queries.stages),
+    {
+      skip: !props?.config?.taskPipelineId,
+      fetchPolicy: 'network-only',
+      variables: { pipelineId: props?.config?.taskPipelineId }
+    }
+  );
+
+  const { loading: loadingLable, data: pipeLinelabels = {} as any } = useQuery(
+    gql(queries.pipelineLabels),
+    {
+      skip: !props?.config?.taskPipelineId,
+      fetchPolicy: 'network-only',
+      variables: { pipelineId: props?.config?.taskPipelineId }
+    }
+  );
+
+  const {
+    loading: loadingAssignedUsers,
+    data: pipelineAssignedUsers = {} as any
+  } = useQuery(gql(queries.pipelineAssignedUsers), {
+    skip: !props?.config?.taskPipelineId,
+    fetchPolicy: 'network-only',
+    variables: { _id: props?.config?.taskPipelineId }
+  });
+
+  if (loading || loadingStages || loadingLable || loadingAssignedUsers) {
     return <Spinner objective={true} />;
   }
 
@@ -31,13 +58,16 @@ function TaskContainer({ currentUser, ...props }: Props) {
     ...props,
     tasks,
     loading,
-    currentUser,
+    stages,
+    pipeLinelabels,
+    pipelineAssignedUsers,
+    currentUser
   };
 
   return <Task {...updatedProps} />;
 }
 
-const WithConsumer = (props) => {
+const WithConsumer = props => {
   return (
     <AppConsumer>
       {({ currentUser }: Store) => {
