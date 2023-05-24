b1c184ec538eda63b47fb9b2688271b08d91b51e
munkhsaikhan.g@nmma.co <munkhsaikhan.g@nmma.co>
2023-05-24 09:56:20 +0800

    fix

--- packages/plugin-cards-api/src/essyncer.js
+++ packages/plugin-cards-api/src/essyncer.js
@@ -22,13 +22,8 @@ module.exports = [
     name: 'purchases',
     schema:
       "{ 'userId': { 'type': 'keyword' }, 'stageId': { 'type': 'keyword' }, 'modifiedBy': { 'type': 'keyword' }, 'status': { 'type': 'keyword' }, 'assignedUserIds': { 'type': 'keyword' }, 'watchedUserIds': { 'type': 'keyword' }, 'labelIds': { 'type': 'keyword' }, 'customFieldsData': <nested> }",
-    script: ''
-  },
-  {
-    name: 'tasks',
-    schema:
-      "{ 'userId': { 'type': 'keyword' }, 'stageId': { 'type': 'keyword' }, 'modifiedBy': { 'type': 'keyword' }, 'status': { 'type': 'keyword' }, 'assignedUserIds': { 'type': 'keyword' }, 'watchedUserIds': { 'type': 'keyword' }, 'labelIds': { 'type': 'keyword' }, 'customFieldsData': <nested> }",
-    script: ''
+    script:
+      "if(ns.indexOf('purchases') > -1) { if (doc.productsData) { var productsDataString = JSON.stringify(doc.productsData); var amount = 0; var productsData = JSON.parse(productsDataString); for (var i = 0; i < productsData.length; i++){ amount = amount + productsData[i].amount; } doc.amount = amount; } } "
   },
   {
     name: 'stages',
