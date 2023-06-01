df78bf60e43e1d3bfa31d2bcd825c3efcc818e3f
Dulguun Otgon <dulguun0225@gmail.com>
2023-06-01 12:34:08 +0800

    Revert "try without webpack - singleton"
    This reverts commit 2e4fc28685719f0b3bb3fc9b1fd67a36dab8f48a.

--- packages/core-ui/webpack.config.js
+++ packages/core-ui/webpack.config.js
@@ -30,7 +30,7 @@ const deps = require('./package.json').dependencies;
 const shared = {};
 
 for (const name of depNames) {
-  shared[name] = { eager: true, requiredVersion: deps[name] };
+  shared[name] = { eager: true, singleton: true, requiredVersion: deps[name] };
 }
 
 module.exports = (env, args) => {
