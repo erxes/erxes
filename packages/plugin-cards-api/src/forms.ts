cae695938af8bec860b4fbee03708108b0d7a9c8
uuganaa1007 <80632055+uuganaa1007@users.noreply.github.com>
2023-06-09 01:42:52 +0800

    Translation purchase (#4442)

--- packages/plugin-cards-api/src/forms.ts
+++ packages/plugin-cards-api/src/forms.ts
@@ -49,7 +49,7 @@ export default {
       relations: relations('cards:task')
     },
     {
-      description: 'purchase pipelines',
+      description: 'Purchases',
       type: 'purchase',
       relations: [
         ...relations('cards:purchase'),
