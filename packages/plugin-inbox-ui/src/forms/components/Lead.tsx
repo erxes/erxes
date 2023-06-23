045d3e7e4504a13e6fb561b197be6068af61c861
soyombo <40427263+soyombo-baterdene@users.noreply.github.com>
2023-06-23 17:07:01 +0800

    feat(form) product quantity (#4425)

--- packages/plugin-inbox-ui/src/forms/components/Lead.tsx
+++ packages/plugin-inbox-ui/src/forms/components/Lead.tsx
@@ -31,6 +31,7 @@ import { __ } from '@erxes/ui/src/utils/core';
 
 type Props = {
   integration?: ILeadIntegration;
+  integrationId?: string;
   loading?: boolean;
   isActionLoading: boolean;
   isReadyToSaveForm: boolean;
@@ -46,6 +47,8 @@ type Props = {
     visibility?: string;
     departmentIds?: string[];
   }) => void;
+  onChildProcessFinished?: (component: string) => void;
+  waitUntilFinish?: (obj: any) => void;
 };
 
 type State = {
@@ -98,7 +101,7 @@ type State = {
 class Lead extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props);
-    //ILeadIntegration
+    // ILeadIntegration
     const integration = props.integration || ({} as any);
 
     const { leadData = {} as ILeadData } = integration;
@@ -136,7 +139,7 @@ class Lead extends React.Component<Props, State> {
         title: form.title || 'Form Title',
         description: form.description || 'Form Description',
         buttonText: form.buttonText || 'Send',
-        fields: [],
+        fields: form.fields || [],
         type: form.type || '',
         numberOfPages: form.numberOfPages || 1
       },
@@ -158,6 +161,12 @@ class Lead extends React.Component<Props, State> {
       departmentIds: integration.departmentIds || [],
       visibility: integration.visibility || 'public'
     };
+  }
+
+  componentDidUpdate(_prevProps, prevState) {
+    if (prevState.formData !== this.state.formData) {
+      this.setState({ formData: this.state.formData });
+    }
   }
 
   handleSubmit = (e: React.FormEvent) => {
@@ -404,13 +413,17 @@ class Lead extends React.Component<Props, State> {
                   brand={brand}
                   theme={theme}
                   language={language}
-                  formData={formData}
+                  formData={this.state.formData}
                   isRequireOnce={isRequireOnce}
                   saveAsCustomer={saveAsCustomer}
                   channelIds={channelIds}
                   visibility={visibility}
                   departmentIds={departmentIds}
+                  integrationId={this.props.integrationId}
+                  isSubmitted={this.props.isReadyToSaveForm}
                   onChange={this.onChange}
+                  onChildProcessFinished={this.props.onChildProcessFinished}
+                  waitUntilFinish={this.props.waitUntilFinish}
                 />
               </Step>
 
--- packages/plugin-inbox-ui/src/forms/containers/CreateLead.tsx
+++ packages/plugin-inbox-ui/src/forms/containers/CreateLead.tsx
@@ -30,6 +30,8 @@ type Props = {
 type State = {
   isLoading: boolean;
   isReadyToSaveForm: boolean;
+  integrationId?: string;
+  mustWait?: any;
   doc?: {
     brandId: string;
     name: string;
@@ -44,8 +46,31 @@ class CreateLeadContainer extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props);
 
-    this.state = { isLoading: false, isReadyToSaveForm: false };
+    this.state = {
+      isLoading: false,
+      isReadyToSaveForm: false,
+      mustWait: { optionsStep: false }
+    };
   }
+
+  redirect = () => {
+    let canClose = true;
+
+    for (const key in this.state.mustWait) {
+      if (this.state.mustWait[key]) {
+        canClose = false;
+      } else {
+        canClose = true;
+      }
+    }
+
+    if (canClose) {
+      this.props.history.push({
+        pathname: '/forms',
+        search: `?popUpRefetchList=true&showInstallCode=${this.state.integrationId}`
+      });
+    }
+  };
 
   render() {
     const {
@@ -82,12 +107,10 @@ class CreateLeadContainer extends React.Component<Props, State> {
                 integrationsCreateLeadIntegration: { _id }
               }
             }) => {
+              this.setState({ integrationId: _id });
               Alert.success('You successfully added a form');
 
-              history.push({
-                pathname: '/forms',
-                search: `?popUpRefetchList=true&showInstallCode=${_id}`
-              });
+              this.redirect();
             }
           )
 
@@ -99,6 +122,11 @@ class CreateLeadContainer extends React.Component<Props, State> {
       }
     };
 
+    const waitUntilFinish = (obj: any) => {
+      const mustWait = { ...this.state.mustWait, ...obj };
+      this.setState({ mustWait });
+    };
+
     const save = doc => {
       this.setState({ isLoading: true, isReadyToSaveForm: true, doc });
     };
@@ -108,12 +136,23 @@ class CreateLeadContainer extends React.Component<Props, State> {
       fields: [],
       save,
       afterFormDbSave,
+      waitUntilFinish,
+      onChildProcessFinished: component => {
+        if (this.state.mustWait.hasOwnProperty(component)) {
+          const mustWait = { ...this.state.mustWait };
+          mustWait[component] = false;
+          this.setState({ mustWait });
+        }
+
+        this.redirect();
+      },
       isActionLoading: this.state.isLoading,
       isReadyToSaveForm: this.state.isReadyToSaveForm,
       emailTemplates: emailTemplatesQuery
         ? emailTemplatesQuery.emailTemplates || []
         : [],
-      configs: configsQuery.configs || []
+      configs: configsQuery.configs || [],
+      integrationId: this.state.integrationId
     };
 
     return <Lead {...updatedProps} />;
