08ad5cb889f8b6b3cea5ce17b98d90917626194c
baterdeneTS <125258377+hitchikermn@users.noreply.github.com>
2023-06-28 02:16:53 +0800

    Period detail (#222)

--- packages/plugin-loans-ui/src/periodLocks/components/PeriodLockDetails.tsx
+++ packages/plugin-loans-ui/src/periodLocks/components/PeriodLockDetails.tsx
@@ -7,14 +7,13 @@ import React from 'react';
 import { IGeneral, IPeriodLockDetail } from '../types';
 import DetailInfo from './PeriodLockDetailInfo';
 import {
-  CollateralColumn,
-  CollateralField,
   CollateralTableWrapper,
   ItemLabel,
   ItemValue,
-  RowCollateral,
   ScrollTableColls
 } from '../../contracts/styles';
+
+import CollapseContent from './CollapseContent';
 
 type Props = {
   periodLock: IPeriodLockDetail;
@@ -50,48 +49,113 @@ class ContractDetails extends React.Component<Props, State> {
       { title }
     ];
 
-    const renderRow = value => {
+    const renderRow = (value, style?: React.CSSProperties) => {
       return (
         <td style={{ padding: 10 }}>
-          <ItemValue>{value || '-'}</ItemValue>
+          <ItemValue style={style}>{value || '-'}</ItemValue>
         </td>
       );
     };
 
-    const renderCollateral = (general: IGeneral) => {
-      const { amount, generalNumber, payDate } = general;
+    const renderDtl = (
+      dtl?: {
+        amount: number;
+        account: string;
+        side: 'kt' | 'dt';
+      }[]
+    ) => {
       return (
-        <tr>
-          {renderRow(generalNumber)}
-          {renderRow(Number(amount || 0).toLocaleString())}
+        <table style={{ width: '100%' }}>
+          <thead>
+            <tr>
+              <td style={{ padding: 10 }}>
+                <ItemLabel>{__('Account')}</ItemLabel>
+              </td>
+              <td style={{ padding: 10 }}>
+                <ItemLabel>{__('Amount')}</ItemLabel>
+              </td>
+              <td style={{ padding: 10 }}>
+                <ItemLabel>{__('Side')}</ItemLabel>
+              </td>
+            </tr>
+          </thead>
+          <tbody>
+            {dtl?.map(row => (
+              <tr>
+                {renderRow(row.account)}
+                {renderRow(Number(row.amount || 0).toLocaleString(), {
+                  textAlign: 'right'
+                })}
 
-          {renderRow(new Date(payDate).toLocaleString())}
-        </tr>
+                {renderRow(row.side, {
+                  textAlign: 'right'
+                })}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      );
+    };
+
+    const renderCollateral = (general: IGeneral) => {
+      const { amount, generalNumber, payDate, description, dtl } = general;
+      return (
+        <CollapseContent
+          full
+          content={renderDtl(dtl)}
+          containerParent={({ children }) => (
+            <tr>
+              <td colSpan={4} style={{ padding: 0 }}>
+                {children}
+              </td>
+            </tr>
+          )}
+        >
+          <tr style={{ cursor: 'pointer' }}>
+            {renderRow(generalNumber)}
+            {renderRow(Number(amount || 0).toLocaleString(), {
+              textAlign: 'right'
+            })}
+            {renderRow(description, {
+              textAlign: 'right'
+            })}
+
+            {renderRow(new Date(payDate).toLocaleString(), {
+              textAlign: 'right'
+            })}
+          </tr>
+        </CollapseContent>
       );
     };
 
     const content = () => {
       return (
-        <ScrollTableColls>
+        <ScrollTableColls style={{ padding: 20 }}>
           <CollateralTableWrapper>
-            <thead>
-              <tr>
-                <td style={{ padding: 10 }}>
-                  <ItemLabel>{__('number')}</ItemLabel>
-                </td>
-                <td style={{ padding: 10 }}>
-                  <ItemLabel>{__('Amount')}</ItemLabel>
-                </td>
-                <td style={{ padding: 10 }}>
-                  <ItemLabel>{__('Date')}</ItemLabel>
-                </td>
-              </tr>
-            </thead>
-            <tbody>
-              {periodLock.generals?.map((general, index) =>
-                renderCollateral(general)
-              )}
-            </tbody>
+            <table style={{ width: '100%' }}>
+              <thead>
+                <tr>
+                  <td style={{ padding: 10 }}>
+                    <ItemLabel>{__('number')}</ItemLabel>
+                  </td>
+                  <td style={{ padding: 10 }}>
+                    <ItemLabel>{__('Amount')}</ItemLabel>
+                  </td>
+                  <td style={{ padding: 10 }}>
+                    <ItemLabel>{__('Description')}</ItemLabel>
+                  </td>
+
+                  <td style={{ padding: 10 }}>
+                    <ItemLabel>{__('Date')}</ItemLabel>
+                  </td>
+                </tr>
+              </thead>
+              <tbody>
+                {periodLock.generals?.map((general, index) =>
+                  renderCollateral(general)
+                )}
+              </tbody>
+            </table>
             {periodLock.generals?.length === 0 && (
               <EmptyState icon="list-ul" text="No items" />
             )}
