220b9ef5565d821929f3335582f8ae635344d0dc
enkhtuvshinD <87625210+enkhtuvshinD@users.noreply.github.com>
2023-07-24 16:28:35 +0800

    chat notif

--- packages/plugin-chats-ui/src/containers/Widget.tsx
+++ packages/plugin-chats-ui/src/containers/Widget.tsx
@@ -2,13 +2,14 @@ import { queries, subscriptions } from '../graphql';
 import { useQuery, useSubscription } from '@apollo/client';
 
 import { Alert } from '@erxes/ui/src/utils';
+import { sendDesktopNotification } from '@erxes/ui/src/utils/core';
 import Component from '../components/Widget';
 import { IUser } from '@erxes/ui/src/auth/types';
 import React from 'react';
 import Spinner from '@erxes/ui/src/components/Spinner';
 import { gql } from '@apollo/client';
 import withCurrentUser from '@erxes/ui/src/auth/containers/withCurrentUser';
-
+import strip from 'strip';
 type Props = {
   currentUser: IUser;
 };
@@ -20,18 +21,44 @@ const WdigetListContainer = (props: Props) => {
     gql(queries.getUnreadChatCount)
   );
 
+  const {
+    loading: notificationLoading,
+    error: notificationError,
+    data: notificationData
+  } = useQuery(gql(queries.notificationsGetConfigurations), {});
+
+  useSubscription(gql(subscriptions.chatInserted), {
+    variables: { userId: currentUser._id },
+    onSubscriptionData: data => {
+      const { notificationsGetConfigurations } = notificationData;
+
+      notificationsGetConfigurations?.map(notificationsGetConfiguration => {
+        if (
+          notificationsGetConfiguration.isAllowed &&
+          notificationsGetConfiguration.notifType === 'chatReceive'
+        ) {
+          sendDesktopNotification({
+            title: 'Chat recieved',
+            content: strip('1111')
+          });
+          return;
+        }
+      });
+    }
+  });
+
   useSubscription(gql(subscriptions.chatUnreadCountChanged), {
     variables: { userId: currentUser._id },
-    onSubscriptionData: () => {
+    onSubscriptionData: data => {
       refetch();
     }
   });
 
-  if (loading) {
+  if (loading || notificationLoading) {
     return <Spinner objective={true} />;
   }
 
-  if (error) {
+  if (error || notificationError) {
     Alert.error(error.message);
   }
 
