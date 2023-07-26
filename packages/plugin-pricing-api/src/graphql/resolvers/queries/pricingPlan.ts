49edf708a7306234c5dc9340c0e1be64c59390c7
munkhsaikhan <munkhsaikhan.g@nmma.co>
2023-07-27 04:05:14 +0800

    pricing filter only be

--- packages/plugin-pricing-api/src/graphql/resolvers/queries/pricingPlan.ts
+++ packages/plugin-pricing-api/src/graphql/resolvers/queries/pricingPlan.ts
@@ -3,20 +3,89 @@ import {
   moduleCheckPermission,
   moduleRequireLogin
 } from '@erxes/api-utils/src/permissions';
+import { paginate } from '@erxes/api-utils/src';
+import { getMainConditions } from '../../../utils';
+import { getAllowedProducts } from '../../../utils/product';
+import { IPricingPlanDocument } from '../../../models/definitions/pricingPlan';
+
+const generateFilter = async (subdomain, models, params) => {
+  const {
+    status,
+    branchId,
+    departmentId,
+    date,
+    productId,
+    prioritizeRule
+  } = params;
+  const filter: any = getMainConditions(branchId, departmentId, date);
+
+  if (status) filter.status = status;
+
+  if (prioritizeRule === 'only') {
+    filter.isPriority = true;
+  } else if (prioritizeRule === 'exclude') {
+    filter.isPriority = false;
+  }
+
+  if (productId) {
+    const planIds: string[] = [];
+    const plans: IPricingPlanDocument[] = await models.PricingPlans.find(
+      filter
+    ).sort({
+      isPriority: 1,
+      value: 1
+    });
+    let allowedProductIds: string[] = [];
+
+    for (const plan of plans) {
+      allowedProductIds = await getAllowedProducts(subdomain, plan, [
+        productId
+      ]);
+
+      if (!allowedProductIds.includes(productId)) {
+        continue;
+      }
+
+      planIds.push(plan._id);
+    }
+
+    filter._id = { $in: planIds };
+  }
+
+  return filter;
+};
 
 const pricingPlanQueries = {
   pricingPlans: async (
     _root: any,
-    { status }: { status: string },
-    { models }: IContext
+    params: any,
+    { subdomain, models }: IContext
   ) => {
-    let filter: any = {};
+    const filter = await generateFilter(subdomain, models, params);
 
-    if (status && status.length !== 0) filter.status = status;
+    const { sortField, sortDirection } = params;
 
-    return await models.PricingPlans.find(filter)
-      .sort({ updatedAt: -1 })
-      .lean();
+    const sort =
+      sortField && sortDirection
+        ? { [sortField]: sortDirection }
+        : { updatedAt: -1 };
+
+    return paginate(
+      models.PricingPlans.find(filter)
+        .sort(sort)
+        .lean(),
+      params
+    );
+  },
+
+  pricingPlansCount: async (
+    _root,
+    params: any,
+    { subdomain, models }: IContext
+  ) => {
+    const filter = await generateFilter(subdomain, models, params);
+
+    return await models.PricingPlans.find(filter).count();
   },
 
   pricingPlanDetail: async (
