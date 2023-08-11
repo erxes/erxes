aaf16d543b5cce8499e691698ce259464a1036a2
soyombo <e11iot.soko@gmail.com>
2023-08-11 13:06:31 +0800

    Update BasicInfoSection.tsx

--- packages/plugin-clientportal-ui/src/components/detail/BasicInfoSection.tsx
+++ packages/plugin-clientportal-ui/src/components/detail/BasicInfoSection.tsx
@@ -12,7 +12,7 @@ import { IClientPortalUser } from '../../types';
 import React from 'react';
 import SmsForm from '@erxes/ui-inbox/src/settings/integrations/containers/telnyx/SmsForm';
 import { loadDynamicComponent, __ } from '@erxes/ui/src/utils';
-import ExtendSubscription from '@erxes/ui-forum/src/containers/ExtendSubscriptionForm';
+// import ExtendSubscription from '@erxes/ui-forum/src/containers/ExtendSubscriptionForm';
 import EmailWidget from '@erxes/ui-inbox/src/inbox/components/EmailWidget';
 import { isEnabled } from '@erxes/ui/src/utils/core';
 
@@ -126,9 +126,11 @@ class BasicInfoSection extends React.Component<Props> {
       if (!isEnabled('forum')) {
         return null;
       }
-      return (
-        <ExtendSubscription {...props} clientPortalUser={clientPortalUser} />
-      );
+
+      // TODO: use loadDynamicComponent
+      // return (
+      //   <ExtendSubscription {...props} clientPortalUser={clientPortalUser} />
+      // );
     };
 
     return (
