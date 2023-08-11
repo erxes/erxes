986319ee99042faecea6820dc12c06d38fa0b5fe
Dulguun Otgon <dulguun0225@gmail.com>
2023-08-11 20:42:37 +0800

    fix Date custom scalar

--- packages/api-utils/src/customScalars.ts
+++ packages/api-utils/src/customScalars.ts
@@ -33,27 +33,26 @@ export default {
   Date: new GraphQLScalarType({
     name: 'Date',
     description: 'Date custom scalar type',
-    parseValue(value) {
-      return new Date(value); // value from the client
-    },
-    serialize: value => {
+    serialize(value) {
       if (value instanceof Date) {
-        return value;
+        return value.getTime(); // Convert outgoing Date to integer for JSON
       }
-
-      if (value.toISOString) {
-        return value.toISOString();
-      }
-
-      return new Date(value);
+      throw Error('GraphQL Date Scalar serializer expected a `Date` object');
     },
-
+    parseValue(value) {
+      if (typeof value === 'number') {
+        return new Date(value); // Convert incoming integer to Date
+      }
+      throw new Error('GraphQL Date Scalar parser expected a `number`');
+    },
     parseLiteral(ast) {
       if (ast.kind === Kind.INT) {
-        return parseInt(ast.value, 10); // ast value is always in string format
+        // Convert hard-coded AST string to integer and then to Date
+        return new Date(parseInt(ast.value, 10));
       }
+      // Invalid hard-coded value (not an integer)
       return null;
-    }
+    },
   }),
 
   JSON: new GraphQLScalarType({
