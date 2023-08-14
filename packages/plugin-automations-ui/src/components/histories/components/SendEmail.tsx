7560af63e505094eb6b5810dc08ba7020c6a2663
Tuguldur Ch <tuguldur.ch@nmma.co>
2023-08-14 11:59:15 +0800

    update(automations):detail form of history row

--- packages/plugin-automations-ui/src/components/histories/components/SendEmail.tsx
+++ packages/plugin-automations-ui/src/components/histories/components/SendEmail.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import EmailTemplate from '@erxes/ui-emailtemplates/src/containers/EmailTemplate';
 import { QueryResponse } from '@erxes/ui/src/types';
-import { ControlLabel, FormGroup, ModalTrigger, __ } from '@erxes/ui/src';
+import {
+  ControlLabel,
+  FormGroup,
+  Label,
+  ModalTrigger,
+  Tip,
+  __
+} from '@erxes/ui/src';
+import { Columns } from '@erxes/ui/src/styles/chooser';
+import { Column } from '@erxes/ui/src/styles/main';
 
 type Props = {
   result: any;
@@ -19,17 +28,64 @@ class SendEmail extends React.Component<FinalProps> {
     this.renderContent = this.renderContent.bind(this);
   }
 
-  renderContent() {
-    const { action } = this.props;
+  renderTemplate(action) {
     const { actionConfig } = action;
 
     return (
-      <>
-        <FormGroup>
-          <ControlLabel>{__('Email Template')}</ControlLabel>
-          <EmailTemplate templateId={actionConfig.templateId} onlyPreview />
-        </FormGroup>
-      </>
+      <FormGroup>
+        <ControlLabel>{__('Email Template')}</ControlLabel>
+        <EmailTemplate templateId={actionConfig.templateId} onlyPreview />
+      </FormGroup>
+    );
+  }
+
+  renderEmails({ fromEmail, title, responses }) {
+    const getLabelColor = response => {
+      if (response?.messageId) {
+        return 'success';
+      }
+      if (response?.error) {
+        return 'danger';
+      }
+      return 'default';
+    };
+
+    return (
+      <li>
+        <ul>
+          <strong>{`From: `}</strong>
+          {`${fromEmail || ''}`}
+        </ul>
+        <ul>
+          <strong>{`Subject: `}</strong>
+          {`${title || ''}`}
+        </ul>
+        <ul>
+          <strong>{`To:`}</strong>
+          {responses.map((response, i) => (
+            <>
+              <Tip
+                text={response?.error || response.messageId ? 'Sent' : '' || ''}
+              >
+                <Label key={i} lblStyle={getLabelColor(response)}>
+                  {response?.toEmail || ''}
+                </Label>
+              </Tip>
+            </>
+          ))}
+        </ul>
+      </li>
+    );
+  }
+
+  renderContent() {
+    const { action, result } = this.props;
+
+    return (
+      <Columns>
+        {this.renderTemplate(action)}
+        <Column>{this.renderEmails(result)}</Column>
+      </Columns>
     );
   }
 
@@ -39,7 +95,7 @@ class SendEmail extends React.Component<FinalProps> {
     return (
       <ModalTrigger
         title=""
-        size="xl"
+        size="lg"
         hideHeader
         trigger={trigger}
         content={this.renderContent}
