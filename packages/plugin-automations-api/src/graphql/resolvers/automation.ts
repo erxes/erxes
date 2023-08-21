76ef71a6d18a4bf797c92eb7f686df8b0935410c
Tuguldur Ch <tuguldur.ch@nmma.co>
2023-08-21 17:52:56 +0800

    update(automations):add abiltiy tag & archive

--- packages/plugin-automations-api/src/graphql/resolvers/automation.ts
+++ packages/plugin-automations-api/src/graphql/resolvers/automation.ts
@@ -1,18 +1,29 @@
-import { IContext } from "../../connectionResolver";
+import { IContext } from '../../connectionResolver';
 import { IAutomationDoc } from '../../models/definitions/automaions';
 
 export default {
   createdUser(automation: IAutomationDoc) {
-    return automation.createdBy && {
-      __typename: 'User',
-      _id: automation.createdBy
-    }
+    return (
+      automation.createdBy && {
+        __typename: 'User',
+        _id: automation.createdBy
+      }
+    );
   },
 
   updatedUser(automation: IAutomationDoc) {
-    return automation.updatedBy && {
-      __typename: 'User',
-      _id: automation.updatedBy
-    }
+    return (
+      automation.updatedBy && {
+        __typename: 'User',
+        _id: automation.updatedBy
+      }
+    );
+  },
+
+  async tags(automation: IAutomationDoc) {
+    return (automation.tagIds || []).map(_id => ({
+      __typename: 'Tag',
+      _id
+    }));
   }
 };
