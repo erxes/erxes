a7f16683ec7f764373f7458f33386bbef0143601
munkhsaikhan <munkhsaikhan.g@nmma.co>
2023-08-22 01:30:42 +0800

    Some remove permissions (#4427)

--- packages/plugin-documents-api/src/graphql/resolvers/documentQueries.ts
+++ packages/plugin-documents-api/src/graphql/resolvers/documentQueries.ts
@@ -111,8 +111,8 @@ const documentQueries = {
   }
 };
 
-checkPermission(documentQueries, 'documents', 'manageDocuments', []);
-checkPermission(documentQueries, 'documents', 'manageDocuments');
-checkPermission(documentQueries, 'documents', 'manageDocuments');
+checkPermission(documentQueries, 'documents', 'showDocuments', []);
+checkPermission(documentQueries, 'documents', 'showDocuments');
+checkPermission(documentQueries, 'documents', 'showDocuments');
 
 export default documentQueries;
