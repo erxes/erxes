1a9feb418b160e672bd451f97e1e31c37130f021
Daarii <daarii.bbb@gmail.com>
2023-08-31 18:53:04 +0800

    updated(meeting) - colorCodes in generateColorCode

--- packages/plugin-meetings-ui/src/utils.ts
+++ packages/plugin-meetings-ui/src/utils.ts
@@ -1,18 +1,34 @@
-import { colors } from '@erxes/ui/src/styles';
-
 const colorList = [
-  colors.colorPrimary,
-  colors.colorSecondary,
-  colors.colorCoreRed,
-  colors.colorCoreTeal,
-  colors.colorCoreYellow,
-  colors.colorCoreOrange,
-  colors.colorCoreGreen,
-  colors.colorCoreBlue,
-  colors.colorCoreSunYellow,
-  colors.bgLightPurple,
-  colors.socialFacebook,
-  colors.socialFacebookMessenger
+  '#FF5733',
+  '#1E90FF',
+  '#FFD700',
+  '#8A2BE2',
+  '#00FF00',
+  '#FF4500',
+  '#9932CC',
+  '#FF1493',
+  '#32CD32',
+  '#FFA500',
+  '#4682B4',
+  '#FF6347',
+  '#800080',
+  '#008000',
+  '#7B68EE',
+  '#FF69B4',
+  '#ADFF2F',
+  '#6A5ACD',
+  '#FFD700',
+  '#2E8B57',
+  '#BA55D3',
+  '#228B22',
+  '#8B008B',
+  '#00CED1',
+  '#9932CC',
+  '#00FFFF',
+  '#FF00FF',
+  '#8B4513',
+  '#20B2AA',
+  '#9370DB'
 ];
 
 export const generateColorCode = (userId: string) => {
@@ -22,7 +38,6 @@ export const generateColorCode = (userId: string) => {
 
   // Use modulo to ensure the index is within the bounds of the color list
   const index = Math.abs(hash) % colorList.length;
-
   // Return the color from the list
   return colorList[index];
 };
