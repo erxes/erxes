073cb8b17055bcfb0eb329af552ab232e8f617f1
munkhsaikhan <munkhsaikhan.g@nmma.co>
2023-09-05 17:21:00 +0800

    syncerkhet synchistory save

--- packages/plugin-syncerkhet-ui/src/graphql/queries.ts
+++ packages/plugin-syncerkhet-ui/src/graphql/queries.ts
@@ -8,6 +8,38 @@ const configs = `
   }
 `;
 
+const commonHistoryParams = `
+  $page: Int,
+  $perPage: Int,
+  $sortField: String,
+  $sortDirection: Int,
+  $userId: String,
+  $startDate: Date,
+  $endDate: Date,
+  $contentType: String,
+  $contentId: String,
+  $searchConsume: String,
+  $searchSend: String,
+  $searchResponse: String,
+  $searchError: String,
+`;
+
+const commonHistoryParamDefs = `
+  page: $page,
+  perPage: $perPage,
+  sortField: $sortField,
+  sortDirection: $sortDirection,
+  userId: $userId,
+  startDate: $startDate,
+  endDate: $endDate,
+  contentType: $contentType,
+  contentId: $contentId,
+  searchConsume: $searchConsume,
+  searchSend: $searchSend,
+  searchResponse: $searchResponse,
+  searchError: $searchError,
+`;
+
 const commonDealParams = `
   $skip: Int,
   $limit: Int,
@@ -183,8 +215,48 @@ const posOrderDetail = `
   }
 `;
 
+const syncHistories = `
+  query syncHistories(
+    ${commonHistoryParams}
+  ) {
+    syncHistories (
+      ${commonHistoryParamDefs}
+    ) {
+      _id
+      type
+      contentType
+      contentId
+      createdAt
+      createdBy
+      consumeData
+      consumeStr
+      sendData
+      sendStr
+      responseData
+      responseStr
+      error
+
+      content
+      createdUser
+    }
+  }
+`;
+
+const syncHistoriesCount = `
+  query syncHistoriesCount(
+    ${commonHistoryParams}
+  ) {
+    syncHistoriesCount (
+      ${commonHistoryParamDefs}
+    )
+  }
+`;
+
 export default {
   configs,
+  syncHistories,
+  syncHistoriesCount,
+
   checkSyncDeals,
   checkSyncDealsTotalCount,
   checkSyncOrdersTotalCount,
