daadb02e2874f33fc3fd839ead0972fa56621340
Anu-Ujin Bat-Ulzii <anuujin@Anu-Ujins-MacBook-Pro.local>
2023-09-14 18:35:11 +0800

    fix

--- packages/ui-inbox/src/settings/integrations/components/mail/styles.ts
+++ packages/ui-inbox/src/settings/integrations/components/mail/styles.ts
@@ -303,7 +303,7 @@ const WidgetWrapper = styledTS<{
     left: 50%;
     top: 6%;
     transform: translate(-50%, 0);
-    box-shadow: 0 0 0 50vmax rgba(0,0,0,.5);
+    box-shadow: 0 0 0 50vmax rgba(0,0,0,.3);
   `
       : `
     bottom: ${dimensions.unitSpacing}px;
