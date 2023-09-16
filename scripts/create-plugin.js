19ae6a12c9af49cc9f9485ee673adb3f0a0df39a
SEIKH NABAB UDDIN <93948993+nababuddin@users.noreply.github.com>
2023-09-16 17:59:31 +0530

    Update create-plugin.js

--- scripts/create-plugin.js
+++ scripts/create-plugin.js
@@ -1,46 +1,44 @@
-var { resolve } = require('path');
-var fs = require('fs-extra');
-var path = require('path');
+const { resolve } = require('path');
+const fs = require('fs-extra');
+const path = require('path');
 const execSync = require('child_process').exec;
 const { prompt, Select } = require('enquirer');
+
 const filePath = pathName => {
   if (pathName) {
     return resolve(__dirname, '..', pathName);
   }
-
   return resolve(__dirname, '..');
 };
+
 const promptLocation = new Select({
   name: 'location',
   message: 'Where do you want to place the plugin at?',
-  choices: ['settings', 'main navigation']
+  choices: ['settings', 'main navigation'],
 });
 
 const promptChoice = new Select({
   name: 'choice',
-  message:
-    'What type of plugin do you wanna create? (You can create general plugin or integration plugin)',
+  message: 'What type of plugin do you want to create? (You can create a general plugin or an integration plugin)',
   choices: [
     { name: 'general', message: 'General' },
     { name: 'integration', message: 'Integration' },
-  ]
+  ],
 });
 
 const promptIntegrationChoice = new Select({
   name: 'choice',
-  message:
-    'Choose the integration plugin`s ui template.',
+  message: 'Choose the integration plugin`s UI template.',
   choices: [
     { name: 'integration', message: 'With form' },
     { name: 'integrationDetail', message: 'With detail page' },
-  ]
+  ],
 });
 
 const promptBlank = new Select({
   name: 'location',
-  message:
-    'Do you wanna start building from an example template? (no will result in empty template)',
-  choices: ['yes', 'no']
+  message: 'Do you want to start building from an example template? (no will result in an empty template)',
+  choices: ['yes', 'no'],
 });
 
 const capitalizeFirstLetter = value =>
@@ -50,13 +48,11 @@ const pluralFormation = type => {
   if (type[type.length - 1] === 'y') {
     return type.slice(0, -1) + 'ies';
   }
-
   return type + 's';
 };
 
 const replacer = (fullPath, name) => {
   const JSONBuffer = fs.readFileSync(fullPath);
-
   const content = JSONBuffer.toString()
     .replace(/_name_/gi, name)
     .replace(/{name}s/g, pluralFormation(name))
@@ -64,34 +60,41 @@ const replacer = (fullPath, name) => {
     .replace(/{name}/g, name)
     .replace(/{Name}/g, capitalizeFirstLetter(name))
     .replace(/{NAME}/g, name.toUpperCase());
-
-  fs.writeFile(fullPath, content);
+  fs.writeFileSync(fullPath, content);
 };
 
-const loopDirFiles = async (dir, name) => {
-  fs.readdir(dir, (err, files) => {
-    if (err) {
-      console.error('Could not list the directory.', err);
-      process.exit(1);
-    }
-    files.forEach(file => {
-      var fullPath = path.join(dir, file);
-      fs.stat(fullPath, function(error, stat) {
-        if (error) {
-          console.error('Error stating file.', error);
-          return;
-        }
-        if (stat.isFile()) {
-          replacer(fullPath, name);
-        } else if (stat.isDirectory()) {
-          loopDirFiles(fullPath, name);
-        }
-      });
+const readdirPromise = dir => {
+  return new Promise((resolve, reject) => {
+    fs.readdir(dir, (err, files) => {
+      if (err) reject(err);
+      else resolve(files);
     });
   });
 };
 
-var createUi = async (name, location, type) => {
+const statPromise = fullPath => {
+  return new Promise((resolve, reject) => {
+    fs.stat(fullPath, (err, stat) => {
+      if (err) reject(err);
+      else resolve(stat);
+    });
+  });
+};
+
+const copyFiles = async (sourceDir, destinationDir) => {
+  const files = await readdirPromise(sourceDir);
+  for (const file of files) {
+    const fullPath = path.join(sourceDir, file);
+    const stat = await statPromise(fullPath);
+    if (stat.isFile()) {
+      fs.copySync(fullPath, path.join(destinationDir, file));
+    } else if (stat.isDirectory()) {
+      await copyFiles(fullPath, path.join(destinationDir, file));
+    }
+  }
+};
+
+const createUi = async (name, location, type) => {
   const dir = filePath('./packages/ui-plugin-template');
   const newDir = filePath(`./packages/plugin-${name}-ui`);
 
@@ -99,50 +102,36 @@ var createUi = async (name, location, type) => {
     default: filePath(`./packages/ui-plugin-template/source-default`),
     empty: filePath(`./packages/ui-plugin-template/source-empty`),
     integration: filePath(`./packages/ui-plugin-template/source-integration`),
-    integrationDetail: filePath(`./packages/ui-plugin-template/source-integration-detail`)
-  }
+    integrationDetail: filePath(`./packages/ui-plugin-template/source-integration-detail`),
+  };
 
-  fs.copySync(
-    dir,
-    newDir,
-    {
-      filter: path => {
-        return !/.*source.*/g.test(path);
-      }
-    },
-    fs.copySync(sourceDirs[type], `${newDir}/src`),
-    fs.copySync(sourceDirs[type], `${newDir}/.erxes/plugin-src`)
-  );
+  fs.copySync(dir, newDir, {
+    filter: path => !/.*source.*/g.test(path),
+  });
+  await copyFiles(sourceDirs[type], `${newDir}/src`);
+  await copyFiles(sourceDirs[type], `${newDir}/.erxes/plugin-src`);
   addIntoUIConfigs(name, location, () => loopDirFiles(newDir, name));
 };
 
-var createApi = async (name, type) => {
+const createApi = async (name, type) => {
   const dir = filePath('./packages/api-plugin-templ');
   const dotErxes = filePath('./packages/api-plugin-template.erxes');
-
   const newDir = filePath(`./packages/plugin-${name}-api`);
 
   const sourceDirs = {
     default: filePath(`./packages/api-plugin-templ/source-default`),
     empty: filePath(`./packages/api-plugin-templ/source-empty`),
-    integration: filePath(`./packages/api-plugin-templ/source-integration`)
-  }
+    integration: filePath(`./packages/api-plugin-templ/source-integration`),
+  };
 
-  fs.copySync(
-    dir,
-    newDir,
-    {
-      filter: path => {
-        return !/.*source.*/g.test(path);
-      }
-    },
-    fs.copySync(dotErxes, `${newDir}/.erxes`),
-    fs.copySync(sourceDirs[type], `${newDir}/src`)
-  );
+  fs.copySync(dir, newDir, {
+    filter: path => !/.*source.*/g.test(path),
+  });
+  fs.copySync(dotErxes, `${newDir}/.erxes`);
+  await copyFiles(sourceDirs[type], `${newDir}/src`);
 
   loopDirFiles(newDir, name);
 
-  // add plugin into configs.json
   addIntoApiConfigs(name);
 };
 
@@ -153,24 +142,23 @@ const addIntoUIConfigs = (name, location, callback) => {
           text: '{Name}s',
           url: '/{name}s',
           icon: 'icon-star',
-          location: 'mainNavigation'
+          location: 'mainNavigation',
         }
       : {
           text: '{Name}s',
           to: '/{name}s',
           image: '/images/icons/erxes-18.svg',
           location: 'settings',
-          scope: '{name}'
+          scope: '{name}',
         };
 
   const uiPath = filePath(`./packages/plugin-${name}-ui/src/configs.js`);
-  var ret = false;
   fs.readFile(uiPath, (err, data) => {
     if (err) {
       console.error(`Could not read the file at directory: ${uiPath}`, err);
       process.exit(1);
     }
-    var updated = data
+    const updated = data
       .toString()
       .replace(/menus:.*\[.*\]/g, `menus:[${JSON.stringify(menu)}]`);
     fs.writeFile(uiPath, updated, err2 => {
@@ -183,92 +171,77 @@ const addIntoUIConfigs = (name, location, callback) => {
 const addIntoApiConfigs = async name => {
   const configsPath = resolve(__dirname, '..', 'cli/configs.json');
 
-  var newPlugin = {
+  const newPlugin = {
     name: name,
-    ui: 'local'
+    ui: 'local',
   };
 
-  fs.readFile(configsPath, async (err, data) => {
-    if (err) {
-      if (err.code === 'ENOENT') {
-        console.log("configs.json doesn't exist, creating one...");
-
-        // File configs.json will be created
-        fs.copyFileSync(`${configsPath}.sample`, configsPath);
-
-        console.log('configs.json.sample was copied to configs.json');
-      }
-
-      data = await fs.readFile(configsPath);
-    }
-
-    var json = JSON.parse(data);
-
+  try {
+    const data = await fs.promises.readFile(configsPath);
+    const json = JSON.parse(data);
     json.plugins.push(newPlugin);
-
-    fs.writeFile(configsPath, JSON.stringify(json));
-  });
+    await fs.promises.writeFile(configsPath, JSON.stringify(json));
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      console.log("configs.json doesn't exist, creating one...");
+      await fs.promises.copyFile(`${configsPath}.sample`, configsPath);
+      console.log('configs.json.sample was copied to configs.json');
+    } else {
+      console.error('Error while adding plugin to configs.json:', error);
+    }
+  }
 };
 
-const installUiDeps = name => {
-  return `cd ` + filePath(`packages/plugin-${name}-ui && yarn install-deps`);
-};
-
-const installApiDeps = name => {
-  return `cd ` + filePath(`packages/plugin-${name}-api && yarn install-deps`);
-};
+const installUiDeps = name => `cd ${filePath(`packages/plugin-${name}-ui && yarn install-deps`)}`;
+const installApiDeps = name => `cd ${filePath(`packages/plugin-${name}-api && yarn install-deps`)}`;
 
 const installDeps = name => {
-  execSync(installUiDeps(name), (err, data) => {
-    if (err) console.error(err);
+  try {
+    execSync(installUiDeps(name));
     console.log('Installing UI dependencies...');
-    console.log(`successfully created plugin ${name}`);
-  });
+    console.log(`Successfully created plugin ${name}`);
+  } catch (err) {
+    console.error('Error installing UI dependencies:', err);
+  }
 
-  execSync(installApiDeps(name), (err, data) => {
-    if (err) console.error(err);
+  try {
+    execSync(installApiDeps(name));
     console.log('Installing API dependencies...');
-  });
+  } catch (err) {
+    console.error('Error installing API dependencies:', err);
+  }
 };
 
 const main = async () => {
-  const input = await prompt([
-    {
-      type: 'input',
-      name: 'name',
-      message: 'Please enter the plugin name:'
-    }
-  ]);
-  promptChoice.run().then(type => {
+  try {
+    const input = await prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Please enter the plugin name:',
+      },
+    ]);
+
+    const type = await promptChoice.run();
+
     if (type === 'integration') {
-      promptIntegrationChoice.run().then(templateType => {
-        const name = input.name;
-
-        createUi(name, '', templateType);
-        createApi(name, type);
-        installDeps(name);
-      });
+      const templateType = await promptIntegrationChoice.run();
+      const name = input.name;
+      createUi(name, '', templateType);
+      createApi(name, type);
+      installDeps(name);
     } else {
-      promptBlank
-        .run()
-        .then(defaultTemplate => {
-          promptLocation
-            .run()
-            .then(location => {
-              const name = input.name;
-
-              const type = defaultTemplate === 'no' ? 'empty' : 'default';
-
-              createUi(name, location, type);
-              createApi(name, type);
-
-              installDeps(name);
-            })
-            .catch(err => console.error(err));
-        })
-        .catch(err => console.error(err));
+      const defaultTemplate = await promptBlank.run();
+      const location = await promptLocation.run();
+      const name = input.name;
+      const templateType = defaultTemplate === 'no' ? 'empty' : 'default';
+      createUi(name, location, templateType);
+      createApi(name, type);
+      installDeps(name);
     }
-  })
+  } catch (err) {
+    console.error('Error:', err);
+  }
 };
 
 main();
