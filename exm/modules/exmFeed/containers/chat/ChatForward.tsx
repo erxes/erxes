66bf14aaf39e6c19c4d3b43670b439233853bea9
Dulguun Otgon <dulguun0225@gmail.com>
2023-09-22 14:08:32 +0800

    remove unused

--- exm/modules/exmFeed/containers/chat/ChatForward.tsx
+++ exm/modules/exmFeed/containers/chat/ChatForward.tsx
@@ -4,7 +4,6 @@ import { useMutation } from '@apollo/client';
 import Alert from '../../../utils/Alert';
 import ChatForward from '../../components/chat/ChatForward';
 import { IUser } from '../../../auth/types';
-import { useState } from 'react';
 
 type Props = {
   currentUser: IUser;
