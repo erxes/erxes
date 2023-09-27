b2ac96ffb9e18347d9d6cdf623495990e7d34379
soyombo <e11iot.soko@gmail.com>
2023-09-27 10:00:05 +0800

    fix(payment): fix qpay bank buttons rerender, not generating socialpay qr code after invoice cancelation

--- packages/plugin-payment-api/src/public/js/script.js
+++ packages/plugin-payment-api/src/public/js/script.js
@@ -12,6 +12,7 @@ async function onPaymentClick(payment, invoiceData, prefix) {
   const storepayInput = document.getElementById('storepayInput');
   const loader = document.querySelector('.loader');
   const deeplink = document.getElementById('deeplink');
+  const bankButtons = document.getElementById('bank-buttons');
 
   deeplink.href = '';
   deeplink.innerHTML = '';
@@ -183,8 +184,12 @@ async function onPaymentClick(payment, invoiceData, prefix) {
         });
     });
   }
+  
+  // hide bank buttons
+  bankButtons.style.display = 'none';
 
   if (apiResponse.deeplink && isMobile) {
+    deeplink.style.display = 'block';
     deeplink.href = apiResponse.deeplink;
     deeplink.target = '_self';
     deeplink.innerHTML = `Open in ${paymentObj.kind}`;
@@ -195,6 +200,13 @@ async function onPaymentClick(payment, invoiceData, prefix) {
   if (['qpay', 'qpayQuickqr'].includes(data.invoice.paymentKind) && isMobile){
     // hide qr image
     image.style.display = 'none';
+    bankButtons.style.display = 'block';
+
+    // clear previous bank buttons
+    while (bankButtons.firstChild) {
+      bankButtons.removeChild(bankButtons.firstChild);
+    }
+
     const urls = apiResponse.urls || [];
 
     urls.map((bankUrl) => {
