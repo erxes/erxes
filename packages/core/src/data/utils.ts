cc5faad7b89f1e998e261926954293ffb537f39c
munkhsaikhan <munkhsaikhan.g@nmma.co>
2023-09-29 12:18:32 +0800

    catch initfireBase

--- packages/core/src/data/utils.ts
+++ packages/core/src/data/utils.ts
@@ -270,9 +270,13 @@ export const initFirebase = async (models: IModels): Promise<void> => {
     const serviceAccount = JSON.parse(codeString);
 
     if (serviceAccount.private_key) {
-      await admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount)
-      });
+      try {
+        await admin.initializeApp({
+          credential: admin.credential.cert(serviceAccount)
+        });
+      } catch (e) {
+        console.log(`initFireBase error: ${e.message}`);
+      }
     }
   }
 };
