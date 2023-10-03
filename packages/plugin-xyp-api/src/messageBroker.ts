5312c7edc38bf7bec2b26d72e6ef9933d0ac4181
soyombo <e11iot.soko@gmail.com>
2023-10-03 18:37:46 +0800

    Update messageBroker.ts

--- packages/plugin-xyp-api/src/messageBroker.ts
+++ packages/plugin-xyp-api/src/messageBroker.ts
@@ -1,5 +1,7 @@
 import { ISendMessageArgs, sendMessage } from '@erxes/api-utils/src/core';
 import { serviceDiscovery } from './configs';
+import { IXypConfig } from './graphql/resolvers/queries';
+import { sendRequest } from '@erxes/api-utils/src';
 // import { Xyps } from "./models";
 
 let client;
@@ -21,6 +23,48 @@ export const initBroker = async cl => {
     return {
       status: 'success'
       // data: await Xyps.find({})
+    };
+  });
+
+  consumeRPCQueue('xyp:fetch', async ({ subdomain, data }) => {
+    const xypConfigs = await sendCommonMessage({
+      subdomain,
+      serviceName: 'core',
+      action: 'configs.findOne',
+      data: {
+        query: {
+          code: 'XYP_CONFIGS'
+        }
+      },
+      isRPC: true,
+      defaultValue: null
+    });
+
+    if (!xypConfigs) {
+      return {
+        status: 'failed',
+        message: 'XYP CONFIGS not found'
+      };
+    }
+
+    const { params, wsOperationName } = data;
+
+    const config: IXypConfig = xypConfigs && xypConfigs.value;
+
+    const response = await sendRequest({
+      url: config.url + '/api',
+      method: 'post',
+      headers: { token: config.token },
+      body: {
+        params,
+        wsOperationName
+      },
+      timeout: 5000
+    });
+
+    return {
+      status: 'success',
+      data: response
     };
   });
 };
