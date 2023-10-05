8cfb8a0fb5cb4409f2995ffc116132565fdcc406
munkhsaikhan <munkhsaikhan.g@nmma.co>
2023-10-05 17:10:10 +0800

    Kiosk pos update (#358)

--- pos/app/(main)/kiosk.tsx
+++ pos/app/(main)/kiosk.tsx
@@ -1,12 +1,22 @@
+"use client"
+
 import Link from "next/link"
+import { queries } from "@/modules/auth/graphql"
+import { useQuery } from "@apollo/client"
 
 import Image from "@/components/ui/image"
+import Loader from "@/components/ui/loader"
 
 const Welcome = () => {
+  const { data, loading } = useQuery(queries.uiOptions)
+  const { uiOptions } = data?.currentConfig || {}
+
+  if (loading) return <Loader className="h-screen" />
+
   return (
     <Link href={"/choose-type"} className="h-screen relative block">
       <Image
-        src={"/kiosk-welcome.png"}
+        src={uiOptions?.bgImage || "/background.png"}
         alt=""
         quality={100}
         sizes="100vw"
