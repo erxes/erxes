8f8ea456b1b603955f8bd14b8d7cfc9d3827acfd
Баялаг <40917206+Baylag321@users.noreply.github.com>
2023-10-12 16:23:20 +0800

    cleanin Department

--- packages/core-ui/src/modules/settings/team/components/Sidebar.tsx
+++ packages/core-ui/src/modules/settings/team/components/Sidebar.tsx
@@ -1,8 +1,6 @@
 import BranchList from '../containers/branch/List';
-import DepartmentList from '../containers/department/List';
 import React from 'react';
 import Sidebar from '@erxes/ui/src/layout/components/Sidebar';
-import UnitList from '../containers/unit/List';
 import { loadDynamicComponent } from '@erxes/ui/src/utils/core';
 
 export default function LeftSidebar({
@@ -13,8 +11,6 @@ export default function LeftSidebar({
   return (
     <Sidebar hasBorder>
       <BranchList />
-      <DepartmentList />
-      <UnitList />
       {loadDynamicComponent('teamMemberSidebarComp', {
         loadingMainQuery
       })}
