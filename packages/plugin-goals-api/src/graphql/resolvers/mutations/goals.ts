b64da77f2d463a07b35d90611449d4c9f52af7f7
uuganaa1007 <suuganbayr948@gmail.com>
2023-11-14 04:49:20 +0800

    comment remove

--- packages/plugin-goals-api/src/graphql/resolvers/mutations/goals.ts
+++ packages/plugin-goals-api/src/graphql/resolvers/mutations/goals.ts
@@ -21,34 +21,6 @@ const goalMutations = {
    */
 
   async goalsEdit(_root, { _id, ...doc }: IGoalsEdit, { models }: IContext) {
-    // const result = await models.Goals.findOne({ _id });
-    // if (result) {
-    //   let updated;
-    //   const str1 = result.specificPeriodGoals;
-    //   const str2 = doc.specificPeriodGoals;
-
-    //   // Check if str1 is defined before using the includes method
-    //   const equal = str1 && str1.includes(str2);
-
-    //   const period = await models.Goals.findOne({
-    //     $and: [{ _id }, { periodGoal: doc.periodGoal }]
-    //   });
-
-    //   if (period) {
-    //     updated = await models.Goals.updateGoal(_id, doc);
-    //   } else {
-    //     await models.Goals.updateOne(
-    //       { _id },
-    //       { $unset: { specificPeriodGoals: [] } }
-    //     );
-    //     updated = await models.Goals.updateGoal(_id, doc);
-    //   }
-    //   return updated;
-    // } else {
-    //   // Handle the case when the result is null
-    //   return null;
-    // }
-
     await models.Goals.updateOne(
       { _id },
       { $unset: { specificPeriodGoals: [] } }
@@ -65,7 +37,7 @@ const goalMutations = {
   async goalsRemove(
     _root,
     { goalTypeIds }: { goalTypeIds: string[] },
-    { models, user, subdomain }: IContext
+    { models }: IContext
   ) {
     await models.Goals.removeGoal(goalTypeIds);
 
