c64d6d61fdeb951f368d49adf373a557ac2f716c
Enkhtuvshin Narmandakh <enkhtuvshin@Enkhtuvshins-MacBook-Pro.local>
2023-11-13 16:18:43 +0800

    fix

--- packages/plugin-exmfeed-api/src/models/definitions/exm.ts
+++ packages/plugin-exmfeed-api/src/models/definitions/exm.ts
@@ -127,7 +127,7 @@ export const feedSchema = schemaHooksWrapper(
     department: field({ type: String, label: 'Department' }),
     branchIds: field({ type: [String], label: 'Branch Ids' }),
     unitId: field({ type: String, label: 'Unit', optional: true }),
-    category: field({ type: Date, label: 'Category', optional: true }),
+    category: field({ type: String, label: 'Category', optional: true }),
     ceremonyData: field({ type: ceremonyDataSchema }),
     eventData: field({ type: eventDataSchema }),
     startDate: field({ type: Date }),
