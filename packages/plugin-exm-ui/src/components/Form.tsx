4d95654acd76b7877f4b5c979d4ac1efe8ff3966
lightii <gerelsukh0705@gmail.com>
2023-11-14 21:09:46 +0800

    back button add

--- packages/plugin-exm-ui/src/components/Form.tsx
+++ packages/plugin-exm-ui/src/components/Form.tsx
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { BarItems, PageHeader } from '@erxes/ui/src';
 import { Tabs, TabTitle } from '@erxes/ui/src/components/tabs';
 import { __ } from '@erxes/ui/src/utils';
 import General from '../containers/General';
 import Appearance from './Appearance';
 import { IExm } from '../types';
+import Button from '@erxes/ui/src/components/Button';
 
 type Props = {
   exm?: IExm;
@@ -23,6 +26,15 @@ function Form(props: Props) {
 
   return (
     <>
+      <PageHeader>
+        <BarItems>
+          <Link to={`/erxes-plugin-exm/home`}>
+            <Button icon="leftarrow-3" btnStyle="link">
+              {__('Back')}
+            </Button>
+          </Link>
+        </BarItems>
+      </PageHeader>
       <Tabs full={true}>
         <TabTitle
           className={currentTab === 'Mobile Gallery' ? 'active' : ''}
