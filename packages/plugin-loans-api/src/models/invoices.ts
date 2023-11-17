0b1f640f7987e97f2a7f1680ffd07f832555a152
baterdeneTS <125258377+hitchikermn@users.noreply.github.com>
2023-11-17 09:52:38 +0800

    Loans liner bug (#4769)

--- packages/plugin-loans-api/src/models/invoices.ts
+++ packages/plugin-loans-api/src/models/invoices.ts
@@ -3,8 +3,12 @@ import { IInvoiceDocument } from './definitions/invoices';
 import { Model } from 'mongoose';
 import { IModels } from '../connectionResolver';
 import { FilterQuery } from 'mongodb';
+import { CONTRACT_STATUS, LEASE_TYPES } from './definitions/constants';
+import { getFullDate } from './utils/utils';
+import { getCalcedAmounts } from './utils/transactionUtils';
 export interface IInvoiceModel extends Model<IInvoiceDocument> {
   getInvoice(selector: FilterQuery<IInvoiceDocument>);
+  createCreditMassInvoice(subdomain, date);
   createInvoice(doc: IInvoice);
   updateInvoice(_id: string, doc: IInvoice);
   removeInvoices(_ids: string[]);
@@ -38,6 +42,59 @@ export const loadInvoiceClass = (models: IModels) => {
         (doc.undue || 0) +
         (doc.debt || 0);
       return models.Invoices.create(doc);
+    }
+
+    /**
+     * Create a invoice
+     */
+    public static async createCreditMassInvoice(subdomain, date = new Date()) {
+      const contracts = await models.Contracts.find({
+        leaseType: LEASE_TYPES.CREDIT,
+        status: CONTRACT_STATUS.NORMAL
+      });
+      const nowDate = getFullDate(date);
+
+      for await (let contract of contracts) {
+        const lastSchedule = await models.Schedules.findOne({
+          contractId: contract._id
+        })
+          .sort({ payDate: -1 })
+          .lean();
+        const payAmount = (lastSchedule?.balance || 0) * 0.1;
+
+        const invoiceDate = new Date(
+          nowDate.getFullYear(),
+          nowDate.getMonth(),
+          contract.scheduleDays[0] || 1
+        );
+
+        const isExistCurrentInvoice = await models.Invoices.findOne({
+          contractId: contract._id,
+          payDate: invoiceDate
+        });
+
+        if (
+          payAmount === 0 ||
+          isExistCurrentInvoice ||
+          invoiceDate <= contract.startDate
+        )
+          continue;
+        const calcInfo: any = getCalcedAmounts(models, subdomain, {
+          contractId: contract._id,
+          payDate: nowDate
+        });
+        calcInfo.payment = payAmount;
+        calcInfo.payDate = invoiceDate;
+
+        calcInfo.contractId = contract._id;
+        calcInfo.total =
+          (calcInfo.payment || 0) +
+          (calcInfo.undue || 0) +
+          (calcInfo.interestEve || 0) +
+          (calcInfo.interestNonce || 0);
+
+        await models.Invoices.create(calcInfo);
+      }
     }
 
     /**
