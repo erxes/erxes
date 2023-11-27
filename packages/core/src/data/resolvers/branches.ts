e2071379b2033f64c034a4c662d1fd552357dbc4
Enkhtuvshin Narmandakh <enkhtuvshin@Enkhtuvshins-MacBook-Pro.local>
2023-11-27 15:02:26 +0800

    rever logic of structure

--- packages/core/src/data/resolvers/branches.ts
+++ packages/core/src/data/resolvers/branches.ts
@@ -1,34 +1,14 @@
 import { IContext, IModels } from '../../connectionResolver';
 import { IBranchDocument } from '../../db/models/definitions/structures';
 
-const getAllChildrenIds = async (
-  models: IModels,
-  parentId: string,
-  allChildren: any[] = []
-) => {
-  const children = await models.Branches.find({
-    parentId
-  });
-
-  for (const child of children) {
-    console.log(child._id, 'children', parentId);
-    allChildren.push(child._id);
-    await getAllChildrenIds(models, child._id, allChildren);
-  }
-
-  return allChildren;
-};
-
 export default {
   __resolveReference({ _id }, { models }: IContext) {
     return models.Branches.findOne({ _id });
   },
 
   async users(branch: IBranchDocument, _args, { models }: IContext) {
-    const allChildrenIds = await getAllChildrenIds(models, branch._id);
-
     return models.Users.findUsers({
-      branchIds: { $in: [branch._id, ...allChildrenIds] },
+      branchIds: { $in: [branch._id] },
       isActive: true
     });
   },
@@ -46,10 +26,8 @@ export default {
   },
 
   async userIds(branch: IBranchDocument, _args, { models }: IContext) {
-    const allChildrenIds = await getAllChildrenIds(models, branch._id);
-
     const branchUsers = await models.Users.findUsers({
-      branchIds: { $in: [branch._id, ...allChildrenIds] },
+      branchIds: { $in: [branch._id] },
       isActive: true
     });
 
@@ -57,10 +35,8 @@ export default {
     return userIds;
   },
   async userCount(branch: IBranchDocument, _args, { models }: IContext) {
-    const allChildrenIds = await getAllChildrenIds(models, branch._id);
-
     return await models.Users.find({
-      branchIds: { $in: [branch._id, ...allChildrenIds] },
+      branchIds: { $in: [branch._id] },
       isActive: true
     }).count();
   }
