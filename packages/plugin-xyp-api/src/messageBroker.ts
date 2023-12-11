4efa7f75103f611a730c812e7409b1b0326b4cc9
orshih <orshih943@gmail.com>
2023-12-11 13:41:17 +0800

    xyp export

--- packages/plugin-xyp-api/src/messageBroker.ts
+++ packages/plugin-xyp-api/src/messageBroker.ts
@@ -120,12 +120,164 @@ export const initBroker = async cl => {
   });
 };
 
+export const sendSyncerkhetMessage = async (
+  args: ISendMessageArgs
+): Promise<any> => {
+  return sendMessage({
+    client,
+    serviceDiscovery,
+    serviceName: 'syncerkhet',
+    ...args
+  });
+};
+
+export const sendPosclientMessage = async (
+  args: ISendMessageArgs & {
+    pos: any;
+  }
+) => {
+  const { action, pos } = args;
+  let lastAction = action;
+  let serviceName = 'posclient';
+
+  const { ALL_AUTO_INIT } = process.env;
+
+  if (
+    ![true, 'true', 'True', '1'].includes(ALL_AUTO_INIT || '') &&
+    !pos.onServer
+  ) {
+    lastAction = `posclient:${action}_${pos.token}`;
+    serviceName = '';
+    args.data.thirdService = true;
+    args.isMQ = true;
+
+    if (args.isRPC) {
+      const response = await sendPosclientHealthCheck(args);
+      if (!response || response.healthy !== 'ok') {
+        throw new Error('syncing error not connected posclient');
+      }
+    }
+  }
+
+  args.data.token = pos.token;
+
+  return await sendMessage({
+    client,
+    serviceDiscovery,
+    serviceName,
+    ...args,
+    action: lastAction
+  });
+};
+
+export const sendPosclientHealthCheck = async ({
+  subdomain,
+  pos
+}: {
+  subdomain: string;
+  pos: any;
+}) => {
+  const { ALL_AUTO_INIT } = process.env;
+
+  if (
+    [true, 'true', 'True', '1'].includes(ALL_AUTO_INIT || '') ||
+    pos.onServer
+  ) {
+    return { healthy: 'ok' };
+  }
+
+  return await sendMessage({
+    subdomain,
+    client,
+    serviceDiscovery,
+    isRPC: true,
+    isMQ: true,
+    serviceName: '',
+    action: `posclient:health_check_${pos.token}`,
+    data: { token: pos.token, thirdService: true },
+    timeout: 1000,
+    defaultValue: { healthy: 'no' }
+  });
+};
+export const sendContactsMessage = async (
+  args: ISendMessageArgs
+): Promise<any> => {
+  return sendMessage({
+    client,
+    serviceDiscovery,
+    serviceName: 'contacts',
+    ...args
+  });
+};
+
+export const sendProductsMessage = async (
+  args: ISendMessageArgs
+): Promise<any> => {
+  return sendMessage({
+    client,
+    serviceDiscovery,
+    serviceName: 'products',
+    ...args
+  });
+};
+
+export const sendAutomationsMessage = async (
+  args: ISendMessageArgs
+): Promise<any> => {
+  return sendMessage({
+    client,
+    serviceDiscovery,
+    serviceName: 'automations',
+    ...args
+  });
+};
+
 export const sendCommonMessage = async (
   args: ISendMessageArgs & { serviceName: string }
-) => {
+): Promise<any> => {
   return sendMessage({
     serviceDiscovery,
     client,
+    ...args
+  });
+};
+export const sendSegmentsMessage = async (
+  args: ISendMessageArgs
+): Promise<any> => {
+  return sendMessage({
+    client,
+    serviceDiscovery,
+    serviceName: 'segments',
+    ...args
+  });
+};
+export const fetchSegment = (
+  subdomain: string,
+  segmentId: string,
+  options?,
+  segmentData?: any
+) =>
+  sendSegmentsMessage({
+    subdomain,
+    action: 'fetchSegment',
+    data: { segmentId, options, segmentData },
+    isRPC: true
+  });
+
+export const sendFormsMessage = (args: ISendMessageArgs): Promise<any> => {
+  return sendMessage({
+    client,
+    serviceDiscovery,
+    serviceName: 'forms',
+    ...args
+  });
+};
+
+export const sendCoreMessage = async (args: ISendMessageArgs): Promise<any> => {
+  return sendMessage({
+    client,
+    serviceDiscovery,
+    serviceName: 'core',
     ...args
   });
 };
