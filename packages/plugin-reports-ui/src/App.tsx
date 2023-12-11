4ed5ba21eca0b838dc848d3d21f701db9be3e2c1
Dulguun Otgon <dulguun0225@gmail.com>
2023-12-11 08:34:20 +0800

    update ui

--- packages/plugin-reports-ui/src/App.tsx
+++ packages/plugin-reports-ui/src/App.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import GeneralRoutes from './generalRoutes';
 import { PluginLayout } from '@erxes/ui/src/styles/main';
+import '@erxes/ui/src/styles/global-styles';
+import 'erxes-icon/css/erxes.min.css';
+import '@erxes/ui/src/styles/style.min.css';
 
 const App = () => {
   return (
