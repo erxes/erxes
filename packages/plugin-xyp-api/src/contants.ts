b00713aa3aada915acdb1fb587682fc617ad8488
orshih <orshih943@gmail.com>
2023-12-21 11:32:52 +0800

    xyp segment

---
+++ packages/plugin-xyp-api/src/contants.ts
@@ -0,0 +1,25 @@
+export const USER_FIELDS = {
+  _id: 1,
+  createdAt: 1,
+  username: 1,
+  password: 1,
+  isOwner: 1,
+  email: 1,
+  isActive: 1,
+  details: 1
+};
+
+export const EXTEND_FIELDS = [
+  // {
+  //   _id: Math.random(),
+  //   name: 'data.xyp_data',
+  //   label: 'data',
+  //   type: 'string'
+  // },
+  // {
+  //   _id: Math.random(),
+  //   name: 'data.xyp_data',
+  //   label: 'Хувь хүний мэдээлэл',
+  //   type: 'string'
+  // }
+];
---
+++ packages/plugin-xyp-api/src/forms.ts
@@ -0,0 +1,121 @@
+import { generateFieldsFromSchema, sendRequest } from '@erxes/api-utils/src';
+import { generateModels } from './connectionResolver';
+import { EXTEND_FIELDS } from './contants';
+import { sendCommonMessage } from './messageBroker';
+import { IXypConfig } from './graphql/resolvers/queries';
+
+export const getServiceToFields = async subdomain => {
+  const xypConfigs = await sendCommonMessage({
+    subdomain,
+    serviceName: 'core',
+    action: 'configs.findOne',
+    data: {
+      query: {
+        code: 'XYP_CONFIGS'
+      }
+    },
+    isRPC: true,
+    defaultValue: null
+  });
+
+  if (!xypConfigs) {
+    throw new Error('Config not found');
+  }
+
+  const config: IXypConfig = xypConfigs && xypConfigs.value;
+
+  const response = await sendRequest({
+    url: config.url + '/list',
+    method: 'post',
+    headers: { token: config.token },
+    timeout: 9000
+  });
+  const choosen = await sendCommonMessage({
+    subdomain,
+    serviceName: 'core',
+    action: 'configs.findOne',
+    data: {
+      query: {
+        code: 'XYP_CONFIGS'
+      }
+    },
+    isRPC: true,
+    defaultValue: null
+  });
+  if (!choosen) {
+    throw new Error('Config not found');
+  }
+
+  const list: any[] = [];
+  let fieldsForExcel: any[] = [];
+  choosen?.value?.servicelist.forEach(name => {
+    const element = response.find(x => x.wsOperationName == name);
+    fieldsForExcel = [
+      ...fieldsForExcel,
+      ...element.output.map(
+        x => `${element.wsOperationName}.${x.wsResponseName}`
+      )
+    ];
+
+    list.push({
+      _id: Math.random(),
+      name: `service.${element.wsOperationName}`,
+      label: `${element.wsOperationDetail}`,
+      type: 'string'
+    });
+  });
+
+  return { fieldsForExcel, list };
+};
+
+export default {
+  types: [{ description: 'Xyp', type: 'xyp_datas' }],
+  fields: async ({ subdomain, data }) => {
+    const { usageType } = data;
+    const models = await generateModels(subdomain);
+
+    const schema = models.XypData.schema as any;
+
+    let fields: Array<{
+      _id: number;
+      name: string;
+      group?: string;
+      label?: string;
+      type?: string;
+      validation?: string;
+      options?: string[];
+      selectOptions?: Array<{ label: string; value: string }>;
+    }> = [];
+
+    fields = EXTEND_FIELDS;
+
+    const { fieldsForExcel, list } =
+      (await getServiceToFields(subdomain)) || [];
+
+    if (schema) {
+      fields = [...list, ...(await generateFieldsFromSchema(schema, ''))];
+
+      for (const name of Object.keys(schema.paths)) {
+        const path = schema.paths[name];
+        // if (path.schema) {
+        //   fields = [
+        //     ...fields,
+        //     ...(await generateFieldsFromSchema(path.schema, `${name}.`))
+        //   ];
+        // }
+      }
+    }
+
+    return fields;
+  }
+  // systemFields: ({ data: { groupId, type } }) =>
+  //   POS_ORDER_INFO.ALL.map(e => ({
+  //     text: e.label,
+  //     type: e.field,
+  //     field: e.field,
+  //     canHide: false,
+  //     groupId,
+  //     contentType: `pos:pos`,
+  //     isDefinedByErxes: true
+  //   }))
+};
