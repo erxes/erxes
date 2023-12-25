7f152d2586bf2afd70cf7a2568bffe4353c1a2c8
Tuguldur Ch <tuguldur.ch@nmma.co>
2023-12-25 15:52:48 +0800

    pluginMaps.js

--- scripts/create-plugins-cis.js
+++ scripts/create-plugins-cis.js
@@ -26,8 +26,8 @@ var plugins = [
   { name: 'rcfa', ui: true, api: true },
   { name: 'syncsaas', ui: true, api: true },
   { name: 'golomtces', ui: false, api: true },
-  { name: 'aputpm', api: true },
-  { name: "insurance", ui: true, api: true },
+  { name: 'aputpm', ui: true, api: true },
+  { name: 'insurance', ui: true, api: true }
 ];
 
 const pluginsMap = {};
