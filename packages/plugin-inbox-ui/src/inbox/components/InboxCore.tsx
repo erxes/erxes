fd19b981b02282049ef6e622c2bd887a9ee65786
Anu-Ujin Bat-Ulzii <ujinka44@gmail.com>
2024-01-08 15:30:58 +0800

    undo inbox shortcuts temporarily

--- packages/plugin-inbox-ui/src/inbox/components/InboxCore.tsx
+++ packages/plugin-inbox-ui/src/inbox/components/InboxCore.tsx
@@ -1,13 +1,9 @@
 import { Contents, HeightedWrapper } from '@erxes/ui/src/layout/styles';
 
 import Header from '@erxes/ui/src/layout/components/Header';
-import Icon from '@erxes/ui/src/components/Icon';
-import InboxShortCuts from './InboxShortCuts';
 import React from 'react';
-import Tip from '@erxes/ui/src/components/Tip';
 import { __ } from 'coreui/utils';
 import asyncComponent from '@erxes/ui/src/components/AsyncComponent';
-import styled from 'styled-components';
 
 const Sidebar = asyncComponent(() =>
   import(
@@ -23,58 +19,15 @@ const ConversationDetail = asyncComponent(
   { height: 'auto', width: '100%', color: '#fff', margin: '10px 10px 10px 0' }
 );
 
-const AdditionalMenu = styled.div`
-  cursor: pointer;
-  display: inline-flex;
-  position: relative;
-  margin-bottom: 3px;
-
-  i {
-    margin-right: 10px;
-  }
-`;
-
 type Props = {
   queryParams: any;
   currentConversationId: string;
 };
-
-type State = {
-  shortcutModalShow: boolean;
-};
-
-class Inbox extends React.Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-
-    this.state = {
-      shortcutModalShow: false
-    };
-  }
-
+class Inbox extends React.Component<Props> {
   render() {
     const { currentConversationId, queryParams } = this.props;
-    const { shortcutModalShow } = this.state;
 
     const menuInbox = [{ title: 'Team Inbox', link: '/inbox/index' }];
-
-    const modalHandler = () => {
-      this.setState({ shortcutModalShow: !shortcutModalShow });
-    };
-
-    const shortcutHelp = (
-      <AdditionalMenu>
-        <Tip text="Help shortcuts (Ctrl + K)" placement="bottom">
-          <Icon
-            icon="keyboard-alt"
-            size={18}
-            onClick={() => modalHandler()}
-            color="#9f9f9f"
-            id="help-shortcuts"
-          />
-        </Tip>
-      </AdditionalMenu>
-    );
 
     return (
       <HeightedWrapper>
@@ -82,7 +35,6 @@ class Inbox extends React.Component<Props, State> {
           title={'Conversation'}
           queryParams={queryParams}
           submenu={menuInbox}
-          additionalMenuItem={shortcutHelp}
         />
         <Contents>
           <Sidebar
@@ -91,10 +43,6 @@ class Inbox extends React.Component<Props, State> {
           />
           <ConversationDetail currentId={currentConversationId} />
         </Contents>
-        <InboxShortCuts
-          shortcutModalShow={shortcutModalShow}
-          modalHandler={modalHandler}
-        />
       </HeightedWrapper>
     );
   }
