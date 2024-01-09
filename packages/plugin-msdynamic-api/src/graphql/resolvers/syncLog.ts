ed1dd0c7d864ae8b7e85612fb727804dbe95d787
lightii <gerelsukh0705@gmail.com>
2024-01-09 12:59:33 +0800

    content add on synclog resolver

--- packages/plugin-msdynamic-api/src/graphql/resolvers/syncLog.ts
+++ packages/plugin-msdynamic-api/src/graphql/resolvers/syncLog.ts
@@ -17,5 +17,32 @@ export default {
       data: { _id: syncLog.createdBy },
       isRPC: true
     });
+  },
+
+  async content(syncLog: ISyncLogDocument, _, {}: IContext) {
+    const { contentType, contentId } = syncLog;
+
+    if (contentType === 'pos:order') {
+      return syncLog.consumeData.number || contentId;
+    }
+
+    if (contentType === 'contacts:customer') {
+      const info = syncLog.consumeData.object || syncLog.consumeData.customer;
+
+      return (
+        info.code ||
+        info.primaryEmail ||
+        info.primaryPhone ||
+        `${info.firstName || ''}${info.lastName && ` ${info.lastName}`}` ||
+        contentId
+      );
+    }
+
+    if (contentType === 'contacts:company') {
+      const info = syncLog.consumeData.object || syncLog.consumeData.company;
+      return info.code || info.primaryName || contentId;
+    }
+
+    return contentId;
   }
 };
