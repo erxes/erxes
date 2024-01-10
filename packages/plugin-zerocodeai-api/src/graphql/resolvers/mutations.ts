3fd5ba0a2ef7b4c5bdd64c224bbb9724506c5f21
Dulguun Otgon <dulguun0225@gmail.com>
2024-01-10 16:25:57 +0800

    remove dead dependency: request

--- packages/plugin-zerocodeai-api/src/graphql/resolvers/mutations.ts
+++ packages/plugin-zerocodeai-api/src/graphql/resolvers/mutations.ts
@@ -1,6 +1,7 @@
-import * as request from 'request';
 import { moduleRequireLogin } from '@erxes/api-utils/src/permissions';
 import { IContext } from '../../connectionResolver';
+import fetch from 'node-fetch';
+import FormData from 'form-data';
 // import { generateAttachmentUrl } from '@erxes/api-utils/src/core';
 
 const mutations = {
@@ -16,29 +17,15 @@ const mutations = {
     const config = await models.Configs.findOne({});
 
     if (!config?.projectId) {
-      const response: {
-        status: string;
-        project_id: string;
-        token: string;
-      } = await new Promise((resolve, reject) => {
-        request(
-          {
-            method: 'POST',
-            url: 'https://zero-ai.com/bot/project/create',
-            formData: {
-              api_key: config?.apiKey || '',
-              name: config?.projectName || ''
-            }
-          },
-          (error, response) => {
-            if (error) {
-              return reject(error);
-            }
+      const body = new FormData();
+      body.append('api_key', config?.apiKey || '');
+      body.append('name', config?.projectName || '');
 
-            return resolve(JSON.parse(response.body));
-          }
-        );
+      const req = await fetch('https://zero-ai.com/bot/project/create', {
+        method: 'POST',
+        body
       });
+      const response = await req.json();
 
       await models.Configs.update(
         {},
@@ -56,29 +43,18 @@ const mutations = {
       throw new Error('Config not found');
     }
 
-    const response = await new Promise((resolve, reject) => {
-      request(
-        {
-          method: 'POST',
-          url: 'https://zero-ai.com/bot/dataset/update',
-          formData: {
-            api_key: config.apiKey,
-            id: config.projectId,
-            upload_file:
-              'https://demo-erxes.s3.amazonaws.com/0.013081930923371843datad-customer-import-template.csv' // generateAttachmentUrl(file)
-          }
-        },
-        (error, response) => {
-          if (error) {
-            return reject(error);
-          }
-
-          return resolve(JSON.parse(response.body));
-        }
-      );
+    const body = new FormData();
+    body.append('api_key', config.apiKey);
+    body.append('id', config.projectId);
+    body.append(
+      'upload_file',
+      'https://demo-erxes.s3.amazonaws.com/0.013081930923371843datad-customer-import-template.csv'
+    ); // generateAttachmentUrl(file)
+    const req = await fetch('https://zero-ai.com/bot/dataset/update', {
+      method: 'POST',
+      body
     });
-
-    return response;
+    return req.json();
   }
 };
 
