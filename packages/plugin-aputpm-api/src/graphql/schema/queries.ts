15db64181b7abc2c64aab6d3f0251e4062225976
Tuguldur Ch <tuguldur.ch@nmma.co>
2024-01-14 20:55:11 +0800

    update(aputpm):added query for get s&k list

--- packages/plugin-aputpm-api/src/graphql/schema/queries.ts
+++ packages/plugin-aputpm-api/src/graphql/schema/queries.ts
@@ -3,17 +3,50 @@ export const types = `
   extend type User @key(fields: "_id") {
       _id: String! @external
       submitStatus:String
-    }
+  }
+
+  extend type KnowledgeBaseArticle @key(fields: "_id") {
+      _id: String! @external
+  }
+
+  type KBCategory {
+    _id: String
+    title: String
+    description: String
+    articles: [KnowledgeBaseArticle]
+    icon: String
+    createdBy: String
+    createdDate: Date
+    modifiedBy: String
+    modifiedDate: Date
+    parentCategoryId: String
+
+    authors: [User]
+    numOfArticles: Float
+    countArticles:Int
+  } 
+
+  extend type Ticket @key(fields: "_id") {
+      _id: String! @external
+      kbCategory:KBCategory,
+      grant:JSON
+  }
 
   input DateFilter {
     from:String,
     to:String
   }
 
+  input fieldFilter {
+    field:String,
+    value:String,
+    values:[String],
+    regex:Boolean
+  }
+
 `;
 
 const commonParams = `
-  cardType:String,
   stageIds:[String],
   stageCodes:[String],
   branchIds:[String],
@@ -24,13 +57,20 @@ const commonParams = `
   createdAt:DateFilter,
   startedAt:DateFilter,
   closedAt:DateFilter,
+
+`;
+
+const commonUserParams = `
+  ${commonParams}
+  cardType:String,
   pipelineIds:[String],
   boardId:String,
   withoutUserFilter:Boolean
 `;
 
 export const queries = `
-  getCreatedUsersCards(${commonParams}):[User]
-  getAssignedUsersCards(${commonParams}):[User]
-  getCustomFieldUsersCards(fieldId:String,${commonParams}):[User]
+  getCreatedUsersCards(${commonUserParams}):[User]
+  getAssignedUsersCards(${commonUserParams}):[User]
+  getCustomFieldUsersCards(fieldId:String,${commonUserParams}):[User]
+  myStandartKnowledges(customFieldsFilter:[fieldFilter],grantFilters:[fieldFilter],kbFieldId:String!,${commonParams}):[Ticket]
 `;
