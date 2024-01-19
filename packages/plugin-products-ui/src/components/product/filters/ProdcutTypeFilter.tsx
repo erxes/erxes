34c29ed21bd1b00cfb8d88aa11ffcbcb01a6d244
Ariunzaya <ariunzaya.ei@gmail.com>
2024-01-19 11:25:52 +0800

    convert products

--- packages/plugin-products-ui/src/components/product/filters/ProdcutTypeFilter.tsx
+++ packages/plugin-products-ui/src/components/product/filters/ProdcutTypeFilter.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 
 import Box from '@erxes/ui/src/components/Box';
-import Icon from '@erxes/ui/src/components/Icon';
 import { IRouterProps } from '@erxes/ui/src/types';
 import { __, router } from '@erxes/ui/src/utils';
 import { FieldStyle, SidebarList } from '@erxes/ui/src/layout/styles';
@@ -12,47 +11,45 @@ interface IProps extends IRouterProps {
   searchable?: boolean;
 }
 
-class ProductTypeFilter extends React.Component<IProps> {
-  render() {
-    const { history } = this.props;
+const ProductTypeFilter: React.FC<IProps> = (props) => {
+  const { history } = props;
 
-    const paramKey = 'type';
+  const paramKey = 'type';
 
-    const onClick = (key, value) => {
-      router.setParams(history, { [key]: value });
-    };
+  const onClick = (key, value) => {
+    router.setParams(history, { [key]: value });
+  };
 
-    return (
-      <Box
-        title={__('Filter by type')}
-        name="showFilterByType"
-        isOpen={router.getParam(history, [paramKey])}
-      >
-        <SidebarList>
-          {productTypeChoises(__).map(
-            ({ value, label }: { value: string; label: string }) => {
-              return (
-                <li key={Math.random()}>
-                  <a
-                    href="#filter"
-                    tabIndex={0}
-                    className={
-                      router.getParam(history, [paramKey]) === value
-                        ? 'active'
-                        : ''
-                    }
-                    onClick={onClick.bind(this, paramKey, value)}
-                  >
-                    <FieldStyle>{label}</FieldStyle>
-                  </a>
-                </li>
-              );
-            }
-          )}
-        </SidebarList>
-      </Box>
-    );
-  }
-}
+  return (
+    <Box
+      title={__('Filter by type')}
+      name="showFilterByType"
+      isOpen={router.getParam(history, [paramKey])}
+    >
+      <SidebarList>
+        {productTypeChoises(__).map(
+          ({ value, label }: { value: string; label: string }) => {
+            return (
+              <li key={Math.random()}>
+                <a
+                  href="#filter"
+                  tabIndex={0}
+                  className={
+                    router.getParam(history, [paramKey]) === value
+                      ? 'active'
+                      : ''
+                  }
+                  onClick={onClick.bind(this, paramKey, value)}
+                >
+                  <FieldStyle>{label}</FieldStyle>
+                </a>
+              </li>
+            );
+          },
+        )}
+      </SidebarList>
+    </Box>
+  );
+};
 
 export default withRouter<IProps>(ProductTypeFilter);
