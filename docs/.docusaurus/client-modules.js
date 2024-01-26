1e8de9855691820de59635d7b4157a5418c6a9ba
lightii <gerelsukh0705@gmail.com>
2024-01-26 14:10:02 +0800

    pull from dev

---
+++ docs/.docusaurus/client-modules.js
@@ -0,0 +1,10 @@
+export default [
+  require("/home/gerelsukh/Documents/erxes/erxes/docs/node_modules/remark-admonitions/styles/infima.css"),
+  require("/home/gerelsukh/Documents/erxes/erxes/docs/node_modules/remark-admonitions/styles/infima.css"),
+  require("/home/gerelsukh/Documents/erxes/erxes/docs/node_modules/remark-admonitions/styles/infima.css"),
+  require("/home/gerelsukh/Documents/erxes/erxes/docs/node_modules/infima/dist/css/default/default.css"),
+  require("/home/gerelsukh/Documents/erxes/erxes/docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/home/gerelsukh/Documents/erxes/erxes/docs/src/css/custom.css"),
+  require("/home/gerelsukh/Documents/erxes/erxes/docs/node_modules/remark-admonitions/styles/infima.css"),
+  require("/home/gerelsukh/Documents/erxes/erxes/docs/node_modules/remark-admonitions/styles/infima.css"),
+];
