a8db09c5ab3421fdfd0f44dc92b01c739c3a66ba
Ujin Anu <ujin@Ujins-MacBook-Pro.local>
2024-01-31 17:19:25 +0800

    remove docs folder

--- docs/.docusaurus/client-modules.js
+++
@@ -1,8 +0,0 @@
-export default [
-  require("/Users/anuujin/Documents/works/openSource/erxes/docs/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/anuujin/Documents/works/openSource/erxes/docs/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/anuujin/Documents/works/openSource/erxes/docs/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/anuujin/Documents/works/openSource/erxes/docs/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/anuujin/Documents/works/openSource/erxes/docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/anuujin/Documents/works/openSource/erxes/docs/src/css/custom.css"),
-];
