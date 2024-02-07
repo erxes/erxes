ea7942ffc5d7e180059ea24cbd737e4fb0a51c0f
hitchikermn <hitchiker.mn@gmail.com>
2024-02-07 10:19:28 +0800

    update

--- packages/plugin-syncpolaris-api/src/utils/utils.ts
+++ packages/plugin-syncpolaris-api/src/utils/utils.ts
@@ -203,38 +203,6 @@ export const customFieldToObject = async (
   return object;
 };
 
-export const objectToCustomField = async (
-  subdomain,
-  object,
-  customFieldType: CustomFieldType,
-) => {
-  const fields = await sendCommonMessage({
-    subdomain,
-    serviceName: 'forms',
-    action: 'fields.find',
-    data: {
-      query: {
-        contentType: customFieldType,
-        code: { $exists: true, $ne: '' },
-      },
-      projection: {
-        groupId: 1,
-        code: 1,
-        _id: 1,
-      },
-    },
-    isRPC: true,
-    defaultValue: [],
-  });
-
-  const customFieldsData: any[] = object.customFieldsData || [];
-  for (const f of fields) {
-    const existingData = customFieldsData.find((c) => c.field === f._id);
-    object[f.code] = existingData?.value;
-  }
-  return object;
-};
-
 export const getSavingProduct = async (subdomain, _id) => {
   return await sendCommonMessage({
     subdomain,
