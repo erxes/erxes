add2fca1d515c149d89976872b7a2717bf41eb1d
hitchikermn <hitchiker.mn@gmail.com>
2024-02-07 10:17:40 +0800

    fix

--- packages/plugin-syncpolaris-ui/src/transactionSaving/containers/List.tsx
+++ packages/plugin-syncpolaris-ui/src/transactionSaving/containers/List.tsx
@@ -22,11 +22,7 @@ type FinalProps = {
 } & Props &
   IRouterProps;
 
-class transactionContainer extends React.Component<FinalProps> {
-  constructor(props) {
-    super(props);
-  }
-
+class TransactionContainer extends React.Component<FinalProps> {
   render() {
     const { queryParams, syncHistoriesQuery, syncHistoriesCountQuery } =
       this.props;
@@ -83,5 +79,5 @@ export default withProps<Props>(
         }),
       },
     ),
-  )(transactionContainer),
+  )(TransactionContainer),
 );
