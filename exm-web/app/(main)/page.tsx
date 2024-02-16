f55fd17fa332f8bb98435da08ce46a81ef171c5b
Ariunzaya <ariunzaya.ei@gmail.com>
2024-02-16 20:10:59 +0800

    fix

--- exm-web/app/(main)/page.tsx
+++ exm-web/app/(main)/page.tsx
@@ -2,19 +2,12 @@ import dynamic from "next/dynamic"
 
 const Feed = dynamic(() => import("@/modules/feed/component/Feed"))
 
-const RightSideBar = dynamic(
-  () => import("@/modules/feed/component/RightSideBar")
-)
-
 export default function IndexPage() {
   return (
     <>
       <div className="flex h-full w-[calc(100%-230px)] flex-col shrink-0">
         <Feed />
       </div>
-      {/* <div className="flex w-[22%] shrink-0 flex-col">
-        <RightSideBar />
-      </div> */}
     </>
   )
 }
