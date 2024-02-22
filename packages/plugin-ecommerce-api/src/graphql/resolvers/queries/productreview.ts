f998ad2c53f1b35c04928f8856f824d4d6196f36
gerelsukh <gerelsukh0705@gmail.com>
2024-02-22 16:01:18 +0800

    productreviews query or operator add

--- packages/plugin-ecommerce-api/src/graphql/resolvers/queries/productreview.ts
+++ packages/plugin-ecommerce-api/src/graphql/resolvers/queries/productreview.ts
@@ -4,7 +4,7 @@ const productreviewQueries = {
   productreview: async (
     _root,
     params,
-    { models: { ProductReview } }: IContext
+    { models: { ProductReview } }: IContext,
   ) => {
     const { productId } = params;
     const reviews = await ProductReview.find({ productId }).lean();
@@ -12,23 +12,26 @@ const productreviewQueries = {
       return {
         productId,
         average: 0,
-        length: 0
+        length: 0,
       };
 
     return {
       productId,
       average:
         reviews.reduce((sum, cur) => sum + cur.review, 0) / reviews.length,
-      length: reviews.length
+      length: reviews.length,
     };
   },
   productreviews: async (
     _root,
     params,
-    { models: { ProductReview } }: IContext
+    { models: { ProductReview } }: IContext,
   ) => {
     const { customerId, productIds } = params;
-    return ProductReview.find({ customerId, productId: { $in: productIds } });
-  }
+
+    return ProductReview.find({
+      $or: [{ customerId }, { productId: { $in: productIds } }],
+    });
+  },
 };
 export default productreviewQueries;
