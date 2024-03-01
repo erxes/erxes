d9d9cca852f400e271484e5d757670118b14810a
soyombo <40427263+soyombo-baterdene@users.noreply.github.com>
2024-03-01 15:58:59 +0800

    feat(verifier): Replace truemail with SendGrid Mail Verifier API
    * Due to the discontinuation of the truemail service, this commit removes truemail functionality and introduces the usage of the SendGrid Mail Verifier API as a replacement. This enhances the verifier feature and ensures continued email verification functionality.

--- widgets/client/messenger/components/AccquireInformation.tsx
+++ widgets/client/messenger/components/AccquireInformation.tsx
@@ -1,8 +1,11 @@
-import * as classNames from "classnames";
-import * as React from "react";
-import { iconRight } from "../../icons/Icons";
-import { __ } from "../../utils";
-import TopBar from "../containers/TopBar";
+import * as classNames from 'classnames';
+import * as React from 'react';
+import { iconRight } from '../../icons/Icons';
+import { __ } from '../../utils';
+import TopBar from '../containers/TopBar';
+import { connection } from '../connection';
+import { COUNTRIES } from '../../form/constants';
+import { ICountry } from '../../types';
 
 type Props = {
   save: (doc: State) => void;
@@ -17,17 +20,23 @@ type State = {
   value: string;
   isLoading: boolean;
   isValidated: boolean;
+  country?: ICountry;
 };
 
 class AccquireInformation extends React.PureComponent<Props, State> {
   constructor(props: Props) {
     super(props);
 
+    const currentCountry = COUNTRIES.find(
+      (country) => country.code === connection.browserInfo?.countryCode
+    );
+
     this.state = {
-      type: "email",
-      value: "",
+      type: 'email',
+      value: '',
       isValidated: true,
-      isLoading: props.loading
+      isLoading: props.loading,
+      country: currentCountry || COUNTRIES[0],
     };
 
     this.save = this.save.bind(this);
@@ -47,18 +56,28 @@ class AccquireInformation extends React.PureComponent<Props, State> {
   }
 
   onValueChange(e: React.FormEvent<HTMLInputElement>) {
-    this.setState({ value: e.currentTarget.value, isValidated: true });
+    const { type, country } = this.state;
+
+    if (type === 'email') {
+      return this.setState({ value: e.currentTarget.value, isValidated: true });
+    }
+
+    this.setState({
+      value: `${country?.dialCode} ${e.currentTarget.value}`,
+      isValidated: true,
+    });
   }
 
   isPhoneValid(phoneNumber: string) {
     const reg = /^\d{8,}$/;
-    return reg.test(phoneNumber.replace(/[\s()+\-\.]|ext/gi, ""));
+    return reg.test(phoneNumber.replace(/[\s()+\-\.]|ext/gi, ''));
   }
 
   isEmailValid(email: string) {
-    const reg = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+[^<>()\.,;:\s@\"]{2,})$/;
-    return reg.test(email);
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    return emailRegex.test(email);
   }
+
 
   save(e: React.FormEvent) {
     e.preventDefault();
@@ -66,7 +85,7 @@ class AccquireInformation extends React.PureComponent<Props, State> {
     const { value, type } = this.state;
 
     if (
-      (type === "email" && this.isEmailValid(value)) ||
+      (type === 'email' && this.isEmailValid(value)) ||
       this.isPhoneValid(value)
     ) {
       return this.props.save(this.state);
@@ -82,10 +101,10 @@ class AccquireInformation extends React.PureComponent<Props, State> {
 
     const title = (
       <div className="erxes-topbar-title">
-        <div>{__("Contact")}</div>
+        <div>{__('Contact')}</div>
         <span>
-          {__("Please leave your contact details to start a conversation") +
-            "."}
+          {__('Please leave your contact details to start a conversation') +
+            '.'}
         </span>
       </div>
     );
@@ -95,11 +114,11 @@ class AccquireInformation extends React.PureComponent<Props, State> {
 
   render() {
     const { color, textColor } = this.props;
-    const { type, isValidated, isLoading } = this.state;
-    const formClasses = classNames("form", { invalid: !isValidated });
+    const { type, isValidated, isLoading, country } = this.state;
+    const formClasses = classNames('form', { invalid: !isValidated });
 
     const placeholder =
-      type === "email" ? __("email@domain.com") : __("phone number");
+      type === 'email' ? __('email@domain.com') : __('phone number');
 
     return (
       <>
@@ -107,29 +126,78 @@ class AccquireInformation extends React.PureComponent<Props, State> {
         <div className="accquire-information slide-in">
           <p className="type">
             <span
-              className={type === "email" ? "current" : ""}
-              onClick={() => this.onTypeChange("email")}
+              className={type === 'email' ? 'current' : ''}
+              onClick={() => this.onTypeChange('email')}
               style={{ borderColor: color }}
             >
-              {__("Email")}
+              {__('Email')}
             </span>
 
             <span
-              className={type === "phone" ? "current" : ""}
-              onClick={() => this.onTypeChange("phone")}
+              className={type === 'phone' ? 'current' : ''}
+              onClick={() => this.onTypeChange('phone')}
               style={{ borderColor: color }}
             >
-              {__("SMS")}
+              {__('Phone')}
             </span>
           </p>
 
           <form className={formClasses} onSubmit={this.save}>
-            <input
-              onChange={this.onValueChange}
-              placeholder={placeholder ? placeholder.toString() : ""}
-              type={type === "email" ? "text" : "tel"}
-              autoFocus={true}
-            />
+            {type === 'phone' ? (
+              <div
+                style={{
+                  display: 'flex',
+                  alignItems: 'center',
+                  height: '100%',
+                }}
+              >
+                <input
+                  style={{ width: '100px' }}
+                  value={country && `${country.emoji} ${country.dialCode}`}
+                  readOnly={true}
+                />
+
+                <div style={{ height: '36px' }}>
+                  <select
+                    value={country?.code}
+                    onChange={(e) =>
+                      this.setState({
+                        country: COUNTRIES.find(
+                          (c) => c.code === e.target.value
+                        ),
+                      })
+                    }
+                    className="form-control"
+                    style={{
+                      width: '120px',
+                      opacity: 0,
+                      position: 'absolute',
+                      left: 0,
+                      cursor: 'pointer',
+                    }}
+                  >
+                    {COUNTRIES.map((country) => (
+                      <option key={country.code} value={country.code}>
+                        {country.name} {country.emoji}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+                <input
+                  onChange={this.onValueChange}
+                  placeholder={placeholder ? placeholder.toString() : ''}
+                  type="tel"
+                  autoFocus={true}
+                />
+              </div>
+            ) : (
+              <input
+                onChange={this.onValueChange}
+                placeholder={placeholder ? placeholder.toString() : ''}
+                type="text"
+                autoFocus={true}
+              />
+            )}
 
             <button
               onClick={this.save}
