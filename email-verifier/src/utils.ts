e94e7916dc2b62941418b1cdb9edae61f459272b
soyombo <e11iot.soko@gmail.com>
2024-03-01 15:37:44 +0800

    Update utils.ts

--- email-verifier/src/utils.ts
+++ email-verifier/src/utils.ts
@@ -92,6 +92,6 @@ export const getEnv = ({
 };
 
 export const isEmailValid = (email: string): boolean => {
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
   return emailRegex.test(email);
 };
