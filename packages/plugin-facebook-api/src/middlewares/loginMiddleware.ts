b3fe312ea498b2f1d2e53c3a2ee624afe83b89d3
Enkhtuvshin Narmandakh <enkhtuvshin@Enkhtuvshins-MacBook-Pro.local>
2024-03-01 11:01:35 +0800

    chore: facebook redirect state

--- packages/plugin-facebook-api/src/middlewares/loginMiddleware.ts
+++ packages/plugin-facebook-api/src/middlewares/loginMiddleware.ts
@@ -43,7 +43,7 @@ const loginMiddleware = async (req, res) => {
       client_id: conf.client_id,
       redirect_uri: conf.redirect_uri,
       scope: conf.scope,
-      state: DOMAIN,
+      state: `${DOMAIN}/gateway/pl:facebook`,
     });
 
     // checks whether a user denied the app facebook login/permissions
