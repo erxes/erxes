81a00fad5aabde6dec01187de491e442cd0f2a61
Dulguun Otgon <dulguun0225@gmail.com>
2024-03-17 00:02:52 +0800

    include subdomain in subscription context

--- packages/api-utils/src/headers/subdomain.ts
+++ packages/api-utils/src/headers/subdomain.ts
@@ -1,24 +1,6 @@
 import { IncomingMessage } from 'http';
-import getHostname from './get-hostname';
 
 export const erxesSubdomainHeaderName = 'erxes-subdomain';
-
-export const getSubdomain = (req: IncomingMessage): string => {
-  let hostname = req.headers['nginx-hostname'] || getHostname(req);
-  if (!hostname) {
-    throw new Error('Hostname not found');
-  }
-  if (Array.isArray(hostname)) {
-    hostname = hostname[0];
-  }
-  const subdomain = hostname.replace(/(^\w+:|^)\/\//, '').split('.')[0];
-  return subdomain;
-};
-
-export const setSubdomainHeader = (req: IncomingMessage): void => {
-  const subdomain = getSubdomain(req);
-  req.headers[erxesSubdomainHeaderName] = subdomain;
-};
 
 export const getSubomdainHeader = (
   req: IncomingMessage,
--- packages/plugin-notifications-api/src/graphql/subscriptionPlugin.js
+++ packages/plugin-notifications-api/src/graphql/subscriptionPlugin.js
@@ -16,7 +16,7 @@ module.exports = {
           payload,
           _params,
           { dataSources: { gatewayDataSource } },
-          info
+          info,
         ) {
           return gatewayDataSource.queryAndMergeMissingData({
             payload,
@@ -31,8 +31,16 @@ module.exports = {
           `,
           });
         },
-        subscribe: (_, { userId, subdomain }) =>
-          graphqlPubsub.asyncIterator(`notificationInserted:${subdomain}:${userId}`),
+        subscribe: (_, { userId, subdomain }, context) => {
+          if (subdomain !== context?.subdomain) {
+            console.error(
+              `subscribe error: Parameter subdomain ${subdomain} and context subdomain ${context.subdomain} doesn't match`,
+            );
+          }
+          return graphqlPubsub.asyncIterator(
+            `notificationInserted:${subdomain}:${userId}`,
+          );
+        },
       },
 
       notificationRead: {
