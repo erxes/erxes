8ea9c65c10b1afc20c447e958469a21908f34f5b
whoselen <131352339+whoselen@users.noreply.github.com>
2024-03-25 12:01:51 +0800

    fix(coreui):  Update tiptap editor schema & make editor section resizable (#5083)

--- packages/erxes-ui/src/components/richTextEditor/nodes/StyleNode.ts
+++
@@ -1,8 +0,0 @@
-import { Node } from '@tiptap/react';
-
-export const StyleNode = Node.create({
-  name: 'styletag',
-  content: 'text*',
-  parseDOM: [{ tag: 'style' }],
-  renderHTML: ({ HTMLAttributes }) => ['style', HTMLAttributes, 0]
-});
