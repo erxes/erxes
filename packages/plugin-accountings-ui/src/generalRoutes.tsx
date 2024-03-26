895a356a082c90f74751fbad877a4e1d680960a9
munkhsaikhan <munkhsaikhan.g@nmma.co>
2024-03-26 17:01:39 +0800

    ui renew

--- packages/plugin-accountings-ui/src/generalRoutes.tsx
+++ packages/plugin-accountings-ui/src/generalRoutes.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import ProductsRoutes from './routes';
+import AccountsRoutes from './routes';
 
 const Routes = () => (
   <Router>
-    <ProductsRoutes />
+    <AccountsRoutes />
   </Router>
 );
 
--- packages/plugin-accountings-ui/src/routes.tsx
+++ packages/plugin-accountings-ui/src/routes.tsx
@@ -1,65 +1,8 @@
-import asyncComponent from '@erxes/ui/src/components/AsyncComponent';
-import queryString from 'query-string';
 import React from 'react';
-import { Route } from 'react-router-dom';
-import Settings from './containers/config/Settings';
-import GeneralSettings from './components/config/GeneralSettings';
 
-const ProductList = asyncComponent(
-  () =>
-    import(
-      /* webpackChunkName: "Settings List - ProductService" */ './containers/account/AccountList'
-    ),
-);
-
-const ProductDetails = asyncComponent(
-  () =>
-    import(
-      /* webpackChunkName: "Settings List - ProductService" */ './containers/account/detail/AccountDetails'
-    ),
-);
-
-const details = ({ match }) => {
-  const id = match.params.id;
-
-  return <ProductDetails id={id} />;
-};
-
-const productService = ({ location, history }) => {
-  return (
-    <ProductList
-      queryParams={queryString.parse(location.search)}
-      history={history}
-    />
-  );
-};
-
-const generalSetting = () => {
-  return <Settings component={GeneralSettings} />;
-};
 
 const routes = () => (
   <React.Fragment>
-    <Route
-      path="/settings/accounts/details/:id"
-      exact={true}
-      key="/settings/accounts/details/:id"
-      component={details}
-    />
-
-    <Route
-      path="/settings/accounts/"
-      exact={true}
-      key="/settings/accounts/"
-      component={productService}
-    />
-
-    <Route
-      path="/settings/account-config/"
-      exact={true}
-      key="/settings/account-config/"
-      component={generalSetting}
-    />
   </React.Fragment>
 );
 
