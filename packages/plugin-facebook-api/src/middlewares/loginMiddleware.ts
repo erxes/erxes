04bcfda27b41cdeff7f05a8b86bf9d2c7caac160
uuganaa1007 <suuganbayr948@gmail.com>
2024-04-02 15:47:59 +0800

    login

--- packages/plugin-facebook-api/src/middlewares/loginMiddleware.ts
+++ packages/plugin-facebook-api/src/middlewares/loginMiddleware.ts
@@ -16,22 +16,23 @@ const loginMiddleware = async (req, res) => {
   const FACEBOOK_PERMISSIONS = await getConfig(
     models,
     'FACEBOOK_PERMISSIONS',
-    'pages_messaging,pages_manage_ads,pages_manage_engagement,pages_manage_metadata,pages_read_user_content',
+    'pages_messaging,pages_manage_ads,pages_manage_engagement,pages_manage_metadata,pages_read_user_content'
   );
 
   const DOMAIN = getEnv({ name: 'DOMAIN', subdomain });
 
+  const API_DOMAIN = DOMAIN.includes('ngrok') ? DOMAIN : `${DOMAIN}/gateway`;
   const FACEBOOK_LOGIN_REDIRECT_URL = await getConfig(
     models,
     'FACEBOOK_LOGIN_REDIRECT_URL',
-    `${DOMAIN}/gateway/pl:facebook/fblogin`,
+    `${API_DOMAIN}/pl:facebook/fblogin`
   );
 
   const conf = {
     client_id: FACEBOOK_APP_ID,
     client_secret: FACEBOOK_APP_SECRET,
     scope: FACEBOOK_PERMISSIONS,
-    redirect_uri: FACEBOOK_LOGIN_REDIRECT_URL,
+    redirect_uri: FACEBOOK_LOGIN_REDIRECT_URL
   };
 
   debugRequest(debugFacebook, req);
@@ -43,7 +44,7 @@ const loginMiddleware = async (req, res) => {
       client_id: conf.client_id,
       redirect_uri: conf.redirect_uri,
       scope: conf.scope,
-      state: `${DOMAIN}/gateway/pl:facebook`,
+      state: `${API_DOMAIN}/gateway/pl:facebook`
     });
 
     // checks whether a user denied the app facebook login/permissions
@@ -60,7 +61,7 @@ const loginMiddleware = async (req, res) => {
     client_id: conf.client_id,
     redirect_uri: conf.redirect_uri,
     client_secret: conf.client_secret,
-    code: req.query.code,
+    code: req.query.code
   };
 
   debugResponse(debugFacebook, req, JSON.stringify(config));
@@ -79,7 +80,7 @@ const loginMiddleware = async (req, res) => {
       last_name: string;
     } = await graphRequest.get(
       'me?fields=id,first_name,last_name',
-      access_token,
+      access_token
     );
 
     const name = `${userAccount.first_name} ${userAccount.last_name}`;
@@ -89,11 +90,11 @@ const loginMiddleware = async (req, res) => {
     if (account) {
       await models.Accounts.updateOne(
         { _id: account._id },
-        { $set: { token: access_token } },
+        { $set: { token: access_token } }
       );
 
       const integrations = await models.Integrations.find({
-        accountId: account._id,
+        accountId: account._id
       });
 
       for (const integration of integrations) {
@@ -104,11 +105,15 @@ const loginMiddleware = async (req, res) => {
         token: access_token,
         name,
         kind: 'facebook',
-        uid: userAccount.id,
+        uid: userAccount.id
       });
     }
 
-    const url = `${DOMAIN}/settings/fb-authorization?fbAuthorized=true`;
+    const reactAppUrl = !DOMAIN.includes('ngrok')
+      ? DOMAIN
+      : 'http://localhost:3000';
+
+    const url = `${reactAppUrl}/settings/fb-authorization?fbAuthorized=true`;
 
     debugResponse(debugFacebook, req, url);
 
--- packages/plugin-facebook-ui/src/containers/Authorization.tsx
+++ packages/plugin-facebook-ui/src/containers/Authorization.tsx
@@ -1,6 +1,7 @@
 import Spinner from '@erxes/ui/src/components/Spinner';
 import React from 'react';
 import styled from 'styled-components';
+import { __ } from '@erxes/ui/src/utils/core';
 
 const Wrapper = styled.div`
   position: fixed;
@@ -19,14 +20,20 @@ type Props = {
 export const Authorization = (props: Props) => {
   const { queryParams } = props;
 
-  if (queryParams.fbAuthorized && window.opener) {
-    window.opener.location.reload();
+  if (queryParams.fbAuthorized) {
+    if (window.opener) {
+      window.opener.location.reload();
+    }
     window.close();
   }
 
   return (
     <Wrapper>
-      <Spinner />
+      {queryParams.fbAuthorized ? (
+        <p>__('Facebook authorized, You can close this window') </p>
+      ) : (
+        <Spinner />
+      )}
     </Wrapper>
   );
 };
