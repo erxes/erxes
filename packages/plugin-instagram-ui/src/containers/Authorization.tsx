d899ec3f27ba2e5e521b867a0cf3d75d55f8cec5
uuganaa1007 <suuganbayr948@gmail.com>
2024-04-02 17:18:25 +0800

    reload

--- packages/plugin-instagram-ui/src/containers/Authorization.tsx
+++ packages/plugin-instagram-ui/src/containers/Authorization.tsx
@@ -19,13 +19,19 @@ type Props = {
 export const Authorization = (props: Props) => {
   const { queryParams } = props;
   if (queryParams.igAuthorized) {
-    window.opener.location.reload();
+    if (window.opener) {
+      window.opener.location.reload();
+    }
     window.close();
+    window.location.reload();
   }
-
   return (
     <Wrapper>
-      <Spinner />
+      {queryParams.igAuthorized ? (
+        <p>__('Instagram authorized, You can close this window') </p>
+      ) : (
+        <Spinner />
+      )}
     </Wrapper>
   );
 };
