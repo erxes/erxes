3bbcd74246142a611f8269cbea6a247989de4213
gerelsukh <gerelsukh0705@gmail.com>
2024-04-03 10:20:30 +0800

    feat: msdProductsRemainder query add on ms-dynamic plugin

--- packages/plugin-msdynamic-api/src/graphql/resolvers/queries/dynamic.ts
+++ packages/plugin-msdynamic-api/src/graphql/resolvers/queries/dynamic.ts
@@ -1,6 +1,8 @@
+import fetch from 'node-fetch';
 import { paginate } from '@erxes/api-utils/src';
 import { escapeRegExp, getPureDate } from '@erxes/api-utils/src/core';
 import { IContext } from '../../../messageBroker';
+import { getConfig } from '../../../utils';
 
 const generateFilter = (params) => {
   const {
@@ -62,6 +64,52 @@ const msdynamicQueries = {
   async syncMsdHistoriesCount(_root, params, { models }: IContext) {
     const selector = generateFilter(params);
     return models.SyncLogs.find(selector).count();
+  },
+
+  async msdProductsRemainder(
+    _root,
+    { brandId, productCodes }: { brandId: string; productCodes: string[] },
+    { subdomain }: IContext
+  ) {
+    const configs = await getConfig(subdomain, 'DYNAMIC', {});
+    const config = configs[brandId || 'noBrand'];
+
+    if (
+      !config.itemApi ||
+      !config.username ||
+      !config.password ||
+      !config.locationCode
+    ) {
+      throw new Error('MS Dynamic config not found.');
+    }
+
+    const { itemApi, username, password, locationCode } = config;
+
+    let filterSection = '';
+
+    for (let i = 0; i < productCodes.length; i++) {
+      const code = productCodes[i];
+      filterSection += `No eq '${code}' or `;
+    }
+
+    filterSection = filterSection.slice(0, -4) + '';
+
+    const url = `${itemApi}?$filter=(${filterSection}) and Location_Filter eq '${locationCode}'   &$select=No,Inventory`;
+
+    const response = await fetch(url, {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        Accept: 'application/json',
+        Authorization: `Basic ${Buffer.from(`${username}:${password}`).toString(
+          'base64'
+        )}`,
+      },
+      timeout: 60000,
+    }).then((res) => res.json());
+
+    if (response.value.length > 0) {
+      return response.value;
+    }
   },
 };
 
