6796daadc990b696be50fec0e302224f5570024f
hitchikermn <hitchiker.mn@gmail.com>
2024-04-17 11:37:37 +0800

    fix: key of map

--- packages/plugin-loans-ui/src/settings/components/LossSettings.tsx
+++ packages/plugin-loans-ui/src/settings/components/LossSettings.tsx
@@ -73,7 +73,7 @@ class GeneralSettings extends React.Component<Props, State> {
     return Object.keys(configs).map(key => {
       return (
         <PerSettings
-          key={`Loss${key}${Math.random()}`}
+          key={`Loss${key}`}
           configsMap={this.state.configsMap}
           config={configs[key]}
           currentConfigKey={key}
