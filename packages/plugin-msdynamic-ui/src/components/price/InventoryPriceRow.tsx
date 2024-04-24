b0ffdd0b4d453627d4fc1b8d395eb1c2bf73305c
Gerelsukh <48400228+Gerelsukh@users.noreply.github.com>
2024-04-24 11:07:42 +0800

    fix: Ms dynamic sync products price update (#5158)

--- packages/plugin-msdynamic-ui/src/components/price/InventoryPriceRow.tsx
+++ packages/plugin-msdynamic-ui/src/components/price/InventoryPriceRow.tsx
@@ -6,49 +6,18 @@ type Props = {
 };
 
 const Row = ({ price, action }: Props) => {
-  const {
-    Item_No,
-    Unit_Price,
-    Ending_Date,
-    code,
-    unitPrice,
-    syncStatus
-  } = price;
+  const { Item_No, Unit_Price, Ending_Date, code, unitPrice, syncStatus } =
+    price;
 
   return (
     <tr>
       <td>{action === 'DELETE' ? code : Item_No}</td>
       <td>{parseFloat(action === 'DELETE' ? unitPrice : Unit_Price)}</td>
       <td>{action === 'DELETE' ? '' : Ending_Date}</td>
-      {action === 'CREATE' ? (
-        <td>
-          {syncStatus === false ? (
-            <></>
-          ) : (
-            <span style={{ color: '#27ae60' }}> Synced </span>
-          )}
-        </td>
-      ) : (
-        <></>
-      )}
+
       {action === 'UPDATE' ? (
         <td>
-          {syncStatus === false ? (
-            <></>
-          ) : (
-            <span style={{ color: '#27ae60' }}> Synced </span>
-          )}
-        </td>
-      ) : (
-        <></>
-      )}
-      {action === 'DELETE' ? (
-        <td>
-          {syncStatus === false ? (
-            <></>
-          ) : (
-            <span style={{ color: '#27ae60' }}> Synced </span>
-          )}
+          <span style={{ color: '#27ae60' }}> Synced </span>
         </td>
       ) : (
         <></>
--- packages/plugin-msdynamic-ui/src/containers/InventoryPrice.tsx
+++ packages/plugin-msdynamic-ui/src/containers/InventoryPrice.tsx
@@ -2,10 +2,7 @@ import { gql } from '@apollo/client';
 import * as compose from 'lodash.flowright';
 import { graphql } from '@apollo/client/react/hoc';
 import { withProps } from '@erxes/ui/src/utils';
-import {
-  ToCheckPricesMutationResponse,
-  ToSyncPricesMutationResponse,
-} from '../types';
+import { ToSyncPricesMutationResponse } from '../types';
 import { router } from '@erxes/ui/src';
 import { Bulk } from '@erxes/ui/src/components';
 import Alert from '@erxes/ui/src/utils/Alert';
@@ -19,9 +16,7 @@ type Props = {
   queryParams: any;
 };
 
-type FinalProps = {} & Props &
-  ToCheckPricesMutationResponse &
-  ToSyncPricesMutationResponse;
+type FinalProps = {} & Props & ToSyncPricesMutationResponse;
 
 const InventoryPriceContainer = (props: FinalProps) => {
   const [items, setItems] = useState({});
@@ -37,17 +32,6 @@ const InventoryPriceContainer = (props: FinalProps) => {
     return <Spinner />;
   }
 
-  const setSyncStatusTrue = (data: any, prices: any, action: string) => {
-    data[action].items = data[action].items.map((i) => {
-      if (prices.find((c) => c.Item_No === i.Item_No)) {
-        const temp = i;
-        temp.syncStatus = true;
-        return temp;
-      }
-      return i;
-    });
-  };
-
   const setSyncStatus = (data: any, action: string) => {
     const createData = data[action].items.map((d) => ({
       ...d,
@@ -58,47 +42,23 @@ const InventoryPriceContainer = (props: FinalProps) => {
     return data;
   };
 
-  const toCheckPrices = () => {
+  const toSyncPrices = () => {
     setLoading(true);
     props
-      .toCheckMsdPrices({
+      .toSyncMsdPrices({
         variables: { brandId },
       })
       .then((response) => {
-        const data = response.data.toCheckMsdPrices;
+        const data = response.data.toSyncMsdPrices;
+
+        console.log(data, 'data');
 
         setSyncStatus(data, 'create');
         setSyncStatus(data, 'update');
         setSyncStatus(data, 'delete');
 
-        setItems(response.data.toCheckMsdPrices);
+        setItems(response.data.toSyncMsdPrices);
         setLoading(false);
-      })
-      .catch((e) => {
-        Alert.error(e.message);
-        setLoading(false);
-      });
-  };
-
-  const toSyncPrices = (action: string, prices: any[]) => {
-    setLoading(true);
-    props
-      .toSyncMsdPrices({
-        variables: {
-          brandId,
-          action,
-          prices,
-        },
-      })
-      .then(() => {
-        setLoading(false);
-        Alert.success('Success. Please check again.');
-      })
-      .finally(() => {
-        const data = items;
-
-        setSyncStatusTrue(data, prices, action.toLowerCase());
-        setItems(data);
       })
       .catch((e) => {
         Alert.error(e.message);
@@ -111,7 +71,6 @@ const InventoryPriceContainer = (props: FinalProps) => {
     loading,
     items,
     setBrand,
-    toCheckPrices,
     toSyncPrices,
   };
 
@@ -122,17 +81,11 @@ const InventoryPriceContainer = (props: FinalProps) => {
 
 export default withProps<Props>(
   compose(
-    graphql<Props, ToCheckPricesMutationResponse, {}>(
-      gql(mutations.toCheckPrices),
-      {
-        name: 'toCheckMsdPrices',
-      },
-    ),
     graphql<Props, ToSyncPricesMutationResponse, {}>(
       gql(mutations.toSyncPrices),
       {
         name: 'toSyncMsdPrices',
-      },
-    ),
-  )(InventoryPriceContainer),
+      }
+    )
+  )(InventoryPriceContainer)
 );
