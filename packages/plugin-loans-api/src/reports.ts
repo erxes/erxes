abed8168f91ae22e096e4d9cece536851af795d6
hitchikermn <hitchiker.mn@gmail.com>
2024-04-25 13:09:47 +0800

    create demo

--- packages/plugin-loans-api/src/reports.ts
+++ packages/plugin-loans-api/src/reports.ts
@@ -4,12 +4,6 @@ import * as dayjs from 'dayjs';
 import { IModels, generateModels } from './connectionResolver';
 import { loanExpiredReportData } from './reports/loanExpiredReportData';
 
-const MMSTOMINS = 60000;
-
-const MMSTOHRS = MMSTOMINS * 60;
-
-const INBOX_TAG_TYPE = 'inbox:conversation';
-
 const NOW = new Date();
 
 const reportTemplates = [
@@ -23,102 +17,6 @@ const reportTemplates = [
   }
 ];
 
-const checkFilterParam = (param: any) => {
-  return param && param.length;
-};
-
-const DATERANGE_TYPES = [
-  { label: 'All time', value: 'all' },
-  { label: 'Today', value: 'today' },
-  { label: 'Yesterday', value: 'yesterday' },
-  { label: 'This Week', value: 'thisWeek' },
-  { label: 'Last Week', value: 'lastWeek' },
-  { label: 'This Month', value: 'thisMonth' },
-  { label: 'Last Month', value: 'lastMonth' },
-  { label: 'This Year', value: 'thisYear' },
-  { label: 'Last Year', value: 'lastYear' },
-  { label: 'Custom Date', value: 'customDate' }
-];
-
-const INTEGRATION_TYPES = [
-  { label: 'XOS Messenger', value: 'messenger' },
-  { label: 'Email', value: 'email' },
-  { label: 'Call', value: 'calls' },
-  { label: 'Callpro', value: 'callpro' },
-  { label: 'SMS', value: 'sms' },
-  { label: 'Facebook Messenger', value: 'facebook-messenger' },
-  { label: 'Facebook Post', value: 'facebook-post' },
-  { label: 'All', value: 'all' }
-];
-
-const calculateAverage = (arr: number[]) => {
-  if (!arr || !arr.length) {
-    return 0; // Handle division by zero for an empty array
-  }
-
-  const sum = arr.reduce((acc, curr) => acc + curr, 0);
-  return (sum / arr.length).toFixed();
-};
-
-const returnDateRange = (dateRange: string, startDate: Date, endDate: Date) => {
-  const startOfToday = new Date(NOW.setHours(0, 0, 0, 0));
-  const endOfToday = new Date(NOW.setHours(23, 59, 59, 999));
-  const startOfYesterday = new Date(
-    dayjs(NOW).add(-1, 'day').toDate().setHours(0, 0, 0, 0)
-  );
-
-  let $gte;
-  let $lte;
-
-  switch (dateRange) {
-    case 'today':
-      $gte = startOfToday;
-      $lte = endOfToday;
-      break;
-    case 'yesterday':
-      $gte = startOfYesterday;
-      $lte = startOfToday;
-    case 'thisWeek':
-      $gte = dayjs(NOW).startOf('week').toDate();
-      $lte = dayjs(NOW).endOf('week').toDate();
-      break;
-
-    case 'lastWeek':
-      $gte = dayjs(NOW).add(-1, 'week').startOf('week').toDate();
-      $lte = dayjs(NOW).add(-1, 'week').endOf('week').toDate();
-      break;
-    case 'lastMonth':
-      $gte = dayjs(NOW).add(-1, 'month').startOf('month').toDate();
-      $lte = dayjs(NOW).add(-1, 'month').endOf('month').toDate();
-      break;
-    case 'thisMonth':
-      $gte = dayjs(NOW).startOf('month').toDate();
-      $lte = dayjs(NOW).endOf('month').toDate();
-      break;
-    case 'thisYear':
-      $gte = dayjs(NOW).startOf('year').toDate();
-      $lte = dayjs(NOW).endOf('year').toDate();
-      break;
-    case 'lastYear':
-      $gte = dayjs(NOW).add(-1, 'year').startOf('year').toDate();
-      $lte = dayjs(NOW).add(-1, 'year').endOf('year').toDate();
-      break;
-    case 'customDate':
-      $gte = new Date(startDate);
-      $lte = new Date(endDate);
-      break;
-    // all
-    default:
-      break;
-  }
-
-  if ($gte && $lte) {
-    return { $gte, $lte };
-  }
-
-  return {};
-};
-
 const chartTemplates = [
   {
     templateType: 'loanExpiredReportData',
@@ -128,21 +26,12 @@ const chartTemplates = [
       'table'
     ],
     getChartResult: async (
-      models: IModels,
-      filter: any,
-      dimension: any,
-      subdomain: string
     ) => {
         
-    console.log('filter',filter)
 
-      const rdata = await loanExpiredReportData(models,filter)
+      const data = [0,1,5,6,5] 
 
-      console.log('rdata',rdata)
-
-      const data = [0,1,5,6,5] //Object.values(usersMap).map((t: any) => t.avgRespondtime);
-
-      const labels = ['normal','expired','doubtful','negative','bad'] //Object.values(usersMap).map((t: any) => t.fullName);
+      const labels = ['normal','expired','doubtful','negative','bad'] 
 
       const title = 'Loan expiration Data';
 
