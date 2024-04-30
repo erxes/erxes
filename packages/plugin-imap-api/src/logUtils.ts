a2414cea6a0f2eec234ba6dde749c92e1ac3e6d8
bazraa <tsedendambatsendbazar@gmail.com>
2024-04-30 17:41:38 +0800

    test1

--- packages/plugin-imap-api/src/logUtils.ts
+++ packages/plugin-imap-api/src/logUtils.ts
@@ -1,5 +1,5 @@
-import { generateModels } from './connectionResolver';
-import { sendContactsMessage } from './messageBroker';
+import { generateModels } from "./connectionResolver";
+import { sendContactsMessage } from "./messageBroker";
 
 export default {
   collectItems: async ({ subdomain, data }) => {
@@ -7,45 +7,33 @@ export default {
 
     const customer = await sendContactsMessage({
       subdomain,
-      action: 'customers.findOne',
+      action: "customers.findOne",
       isRPC: true,
       data: {
-        _id: contentId
-      }
+        _id: contentId,
+      },
     });
 
     if (!customer) {
       return {
-        status: 'success',
-        data: []
+        status: "success",
+        data: [],
       };
     }
 
     const models = await generateModels(subdomain);
-    const messages = await models.Messages.find({
-      'to.address': customer.primaryEmail,
-    });
-    let getMessage: any = [];
-    if (!messages) {
-      const messages = await models.Messages.find({
-        'to.address': customer.primaryEmail,
-      });
-
-      getMessage = getMessage.concat(messages);
-    } else {
-      const messages = await models.Messages.find({
-        'from.address': customer.primaryEmail,
-      });
-
-      getMessage = getMessage.concat(messages);
-    }
+    const messages = (await models.Messages.find({
+      "to.address": customer.primaryEmail,
+    }))
+      ? await models.Messages.find({ "from.address": customer.primaryEmail })
+      : await models.Messages.find({ "to.address": customer.primaryEmail });
 
     const results: any = [];
 
-    for (const message of getMessage) {
+    for (const message of messages) {
       results.push({
         _id: message._id,
-        contentType: 'imap:customer',
+        contentType: "imap:customer",
         createdAt: message.createdAt,
         contentTypeDetail: message,
       });
--- packages/plugin-imap-ui/src/components/ActivityLog.tsx
+++ packages/plugin-imap-ui/src/components/ActivityLog.tsx
@@ -4,30 +4,18 @@ import {
   ActivityIcon,
   ActivityRow,
   IMapActivityContent,
-  SentWho
-} from '../styles';
+  SentWho,
+} from "../styles";
 
-import Button from '@erxes/ui/src/components/Button';
-import Icon from '@erxes/ui/src/components/Icon';
-import React from 'react';
-import Tip from '@erxes/ui/src/components/Tip';
-import { __ } from '@erxes/ui/src/utils/core';
-import dayjs from 'dayjs';
-import { getIconAndColor } from '@erxes/ui-log/src/activityLogs/utils';
-import { Link } from 'react-router-dom';
-
-
-import {
- 
-  CenterText,
-  Collapse,
-  CollapseTrigger,
-  ConversationContent,
-  Count,
-  FlexBody,
-  FlexCenterContent,
-  Header
-} from '@erxes/ui-log/src/activityLogs/styles';
+import Button from "@erxes/ui/src/components/Button";
+import Icon from "@erxes/ui/src/components/Icon";
+import React from "react";
+import Tip from "@erxes/ui/src/components/Tip";
+import { __ } from "@erxes/ui/src/utils/core";
+import dayjs from "dayjs";
+import { getIconAndColor } from "@erxes/ui-log/src/activityLogs/utils";
+import { Link } from "react-router-dom";
+import { CenterText } from "@erxes/ui-log/src/activityLogs/styles";
 type Props = {
   contentType: string;
   activity: any;
@@ -45,23 +33,20 @@ class ActivityItem extends React.Component<Props, State> {
     const { contentTypeDetail = {} } = props.activity;
 
     this.state = {
-      shrink: (contentTypeDetail.body || '').length > 380 ? true : false
+      shrink: (contentTypeDetail.body || "").length > 380 ? true : false,
     };
   }
 
   renderWhom(contentTypeDetail) {
     const { from, to } = contentTypeDetail;
-    const From = from ? from.map(f => f.name || f.address) : 'unknown';
-    const To = to ? to.map(f => f.name || f.address) : 'unknown';
+    const From = from ? from.map((f) => f.name || f.address) : "unknown";
+    const To = to ? to.map((f) => f.name || f.address) : "unknown";
 
-     
     return (
       <SentWho>
-        <strong>{From.map(f => f)}</strong>
-        {__('send email to ')}
-        <strong>{To.map(t => t)}</strong>
-
-        
+        <strong>{From.map((f) => f)}</strong>
+        {__("send email to ")}
+        <strong>{To.map((t) => t)}</strong>
       </SentWho>
     );
   }
@@ -71,48 +56,44 @@ class ActivityItem extends React.Component<Props, State> {
     const { shrink } = this.state;
 
     if (!shrink) {
-    {shrink ? __('Shrink email') : __('Expand email')}
+      {
+        shrink ? __("Shrink email") : __("Expand email");
+      }
     }
-    
+
     return (
-
-        <>
-      <Button
-        size="small"
-        btnStyle="warning"
-        onClick={() => this.setState({ shrink: !shrink })}
-      >
-        {shrink ? __('Expand email') : __('Shrink email')}
-
-      </Button>
-
-
-        
+      <>
+        <Button
+          size="small"
+          btnStyle="warning"
+          onClick={() => this.setState({ shrink: !shrink })}
+        >
+          {shrink ? __("Expand email") : __("Shrink email")}
+        </Button>
 
         <CenterText>
           <Link to={`/inbox/index?_id=${inboxConversationId}`}>
-            {__('See full mail')} <Icon icon="angle-double-right" />
+            {__("See full mail")} <Icon icon="angle-double-right" />
           </Link>
         </CenterText>
-        </>
+      </>
     );
   }
-  
 
   render() {
     const { activity } = this.props;
     const { contentTypeDetail, contentType } = activity;
     const { body, subject, createdAt } = contentTypeDetail;
 
-    if (contentType && !contentType.includes('imap')) {
+    if (contentType && !contentType.includes("imap")) {
       return null;
     }
 
-    const iconAndColor = getIconAndColor('email');
+    const iconAndColor = getIconAndColor("email");
 
     return (
       <ActivityRow>
-        <Tip text={'imap email'} placement="top">
+        <Tip text={"imap email"} placement="top">
           <ActivityIcon color={iconAndColor.color}>
             <Icon icon={iconAndColor.icon} />
           </ActivityIcon>
@@ -120,7 +101,7 @@ class ActivityItem extends React.Component<Props, State> {
 
         <AcitivityHeader>
           <strong>{subject}</strong>
-          <ActivityDate>{dayjs(createdAt).format('lll')}</ActivityDate>
+          <ActivityDate>{dayjs(createdAt).format("lll")}</ActivityDate>
         </AcitivityHeader>
         {this.renderWhom(contentTypeDetail)}
 
