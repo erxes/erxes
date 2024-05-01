201f22277c8dfc4d1e5c679d4923fd6a7afd5469
munkhsaikhan <munkhsaikhan.g@nmma.co>
2024-05-01 23:56:35 +0800

    check fix deals products

--- packages/ui-cards/src/deals/containers/product/ProductForm.tsx
+++ packages/ui-cards/src/deals/containers/product/ProductForm.tsx
@@ -44,7 +44,7 @@ class ProductFormContainer extends React.Component<Props> {
             ...this.props,
             categories: categories,
             loading: productCategoriesQuery.loading,
-            currencies: configs.dealCurrency || [],
+            currencies: (configs.dealCurrency || "").split(",") || [],
           };
 
           return <ProductForm {...extendedProps} />;
