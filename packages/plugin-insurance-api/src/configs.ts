fc7351b7114841ac621ce72e4d7c00959817c099
soyombo <e11iot.soko@gmail.com>
2024-05-06 19:58:35 +0800

    fix(insurance) open chrome no sandbox mode

--- packages/plugin-insurance-api/src/configs.ts
+++ packages/plugin-insurance-api/src/configs.ts
@@ -70,6 +70,7 @@ export default {
         headless: true,
         executablePath:
           '/usr/bin/google-chrome',
+          args: ['--no-sandbox', '--disable-setuid-sandbox']
       });
       const page = await browser.newPage();
 
