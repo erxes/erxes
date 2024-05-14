2a732479cd42712c4b48ffceb1fe6460aec1c9cb
orshih <orshih943@gmail.com>
2024-05-14 17:07:08 +0800

    config ticket stage ids

--- packages/plugin-assets-ui/src/configs.js
+++ packages/plugin-assets-ui/src/configs.js
@@ -1,41 +1,41 @@
 module.exports = {
   srcDir: __dirname,
-  name: 'assets',
-  port: 3012,
-  scope: 'assets',
+  name: "assets",
+  port: 3033,
+  scope: "assets",
   exposes: {
-    './routes': './src/routes.tsx',
-    './selectWithAsset': './src/common/SelectWithAssets.tsx',
+    "./routes": "./src/routes.tsx",
+    "./selectWithAsset": "./src/common/SelectWithAssets.tsx",
   },
   routes: {
-    url: 'http://localhost:3012/remoteEntry.js',
-    scope: 'assets',
-    module: './routes'
+    url: "http://localhost:3033/remoteEntry.js",
+    scope: "assets",
+    module: "./routes",
   },
   formsExtraFields: [
     {
-      scope: 'assets',
-      component: './selectWithAsset',
-      type: 'asset',
+      scope: "assets",
+      component: "./selectWithAsset",
+      type: "asset",
     },
   ],
   menus: [
     {
-      text: 'Assets',
-      to: '/settings/assets/',
-      image: '/images/icons/erxes-18.svg',
-      location: 'settings',
-      scope: 'assets',
-      action: 'assetsAll',
-      permissions: ['showAssets', 'manageAssets']
+      text: "Assets",
+      to: "/settings/assets/",
+      image: "/images/icons/erxes-18.svg",
+      location: "settings",
+      scope: "assets",
+      action: "assetsAll",
+      permissions: ["showAssets", "manageAssets"],
     },
     {
-      text: 'Asset & Movements',
-      url: '/asset-movements',
-      icon: 'icon-piggybank',
-      location: 'mainNavigation',
-      action: 'assetsAll',
-      permissions: ['showAssets', 'manageAssets']
-    }
-  ]
+      text: "Asset & Movements",
+      url: "/asset-movements",
+      icon: "icon-piggybank",
+      location: "mainNavigation",
+      action: "assetsAll",
+      permissions: ["showAssets", "manageAssets"],
+    },
+  ],
 };
