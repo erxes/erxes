62607713b19ed88d04d75400be3d7098c1380bea
soyombo <e11iot.soko@gmail.com>
2024-05-14 22:22:04 +0800

    feat(email-verifier): use clearout for email validation

--- email-verifier/src/models.ts
+++ email-verifier/src/models.ts
@@ -36,10 +36,11 @@ export const PHONE_VALIDATION_STATUSES = {
 
 export const EMAIL_VALIDATION_SOURCES = {
   ERXES: 'erxes',
+  CLEAROUT: 'clearout',
   SENDGRID: 'sendgrid',
   TRUEMAIL: 'truemail',
   REACHER: 'reacher',
-  ALL: ['erxes', 'sendgrid', 'reacher', 'truemail'],
+  ALL: ['erxes', 'sendgrid', 'reacher', 'truemail', 'clearout'],
 };
 
 interface IEmail {
