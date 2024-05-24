440b13cd20d0133bbef86d225d10e18193b1f8b9
enkhtuvshinD <da.enkhtuvshin@gmail.com>
2024-05-24 12:37:16 +0800

    add log

--- packages/plugin-calls-api/src/acceptCall.ts
+++ packages/plugin-calls-api/src/acceptCall.ts
@@ -71,7 +71,7 @@ const acceptCall = async (
   }
   //save on api
   try {
-    console.log(type, 'type', user?.details?.operatorPhone);
+    console.log(type, 'type', user?.details?.operatorPhone, user);
     const apiConversationResponse = await sendInboxMessage({
       subdomain,
       action: 'integrations.receive',
