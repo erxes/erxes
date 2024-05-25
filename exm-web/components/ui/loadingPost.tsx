be1876d8c571d8d0932764fe14c65137bee7e9ee
Mashbat <Mashbat0211@gmail.com>
2024-05-25 15:52:28 +0800

    exm-web/components/ui/loadingPost.tsx

--- exm-web/components/ui/loadingPost.tsx
+++ exm-web/components/ui/loadingPost.tsx
@@ -5,7 +5,7 @@ const LoadingPost = ({ text }: { text?: string }) => {
     <div className="backdrop-blur-sm z-10 flex flex-col items-center justify-center absolute w-full h-full">
       <Loader className="flex-none w-10 h-10 mb-5" size={50} />
       <h3 className="text-md font-semibold text-[#444]">
-        {(text && text) || "Posting"}
+        {text || "Posting"}
       </h3>
     </div>
   )
