e83c848c6b7eb054afdb7eac4a5154cdef0f0ade
Mashbat <Mashbat0211@gmail.com>
2024-05-25 14:19:30 +0800

    client-portal/modules/knowledgeBase/containers/ArticleDetail.tsx

--- client-portal/modules/knowledgeBase/containers/ArticleDetail.tsx
+++ client-portal/modules/knowledgeBase/containers/ArticleDetail.tsx
@@ -13,21 +13,31 @@ function ArticleDetailContainer({
   queryParams: { id, catId },
   ...props
 }: Props) {
-  const { loading, data = {} as any } =
-    id || ({} as any)
-      ? useQuery(gql(articleDetailQuery), {
-          variables: { _id: id || "" },
-          skip: !id,
-        })
-      : { loading: true };
+  let queryOptions = {};
+  if (id) {
+    queryOptions = {
+      variables: { _id: id },
+      skip: false,
+    };
+  } else {
+    queryOptions = { loading: true };
+  }
+  
+  const { loading, data = {} as any } = useQuery(gql(articleDetailQuery), queryOptions);
+  
 
-  const { data: catData = {} as any } =
-    catId ||
-    (({} as any) &&
-      useQuery(gql(categoryDetailQuery), {
-        variables: { _id: catId || "" },
-        skip: !catId,
-      }));
+  let queryOptions1 = {};
+  if (catId) {
+    queryOptions = {
+      variables: { _id: catId },
+      skip: false,
+    };
+  } else {
+    queryOptions = { skip: true };
+  }
+  
+  const { data: catData = {} as any } = useQuery(gql(categoryDetailQuery), queryOptions1);
+  
 
   const article = (data && data.knowledgeBaseArticleDetail) || {};
   const category = (catData && catData.knowledgeBaseCategoryDetail) || {};
