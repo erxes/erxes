a5cd73bcd7434ba520c021156465d176e010a417
uuganbayar.b <uuganaa1333@gmail.com>
2024-05-25 15:13:45 +0800

    chores:Fixed  Mark the props of the component as read-only. https://sonarcloud.io/project/issues?open=AYt0i9WKGNztN2zdPLN6&id=erxes_erxes

--- widgets/client/messenger/components/ConversationInit.tsx
+++ widgets/client/messenger/components/ConversationInit.tsx
@@ -1,27 +1,20 @@
-import * as React from "react";
-import { iconPlus } from "../../icons/Icons";
-import { __ } from "../../utils";
-import ConversationItem from "../containers/ConversationItem";
-import { IConversation } from "../types";
+import * as React from 'react';
+import { iconPlus } from '../../icons/Icons';
+import { __ } from '../../utils';
+import ConversationItem from '../containers/ConversationItem';
+import { IConversation } from '../types';
 
-type Props = {
-  conversations: IConversation[];
+type Props = Readonly<{
+  conversations: ReadonlyArray<IConversation>;
   goToConversation: (conversationId: string) => void;
   createConversation: (e: React.FormEvent<HTMLLIElement>) => void;
   loading: boolean;
   goToAllConversations: (e: React.FormEvent<HTMLLIElement>) => void;
   responseRate?: string;
-};
+}>;
 
 function ConversationInit(props: Props) {
-  const {
-    conversations,
-    goToConversation,
-    loading,
-    createConversation,
-    goToAllConversations,
-    responseRate
-  } = props;
+  const { conversations, goToConversation, loading, createConversation, goToAllConversations, responseRate } = props;
 
   if (loading) {
     return <div className="loader" />;
@@ -34,9 +27,10 @@ function ConversationInit(props: Props) {
           <span>{iconPlus}</span>
         </div>
         <div className="erxes-right-plus">
-          <div className="erxes-name">{__("Start new conversation")}</div>
+          <div className="erxes-name">{__('Start new conversation')}</div>
           <div className="erxes-last-message">
-            {__("Our usual response time")}<br /> 
+            {__('Our usual response time')}
+            <br />
             <strong>{responseRate}</strong>
           </div>
         </div>
@@ -48,23 +42,18 @@ function ConversationInit(props: Props) {
     return (
       <li onClick={goToAllConversations} className="erxes-list-item">
         <div className="erxes-right-side">
-          <div className="erxes-last-message">
-            {__("See all your conversations")}
-          </div>
+          <div className="erxes-last-message">{__('See all your conversations')}</div>
         </div>
       </li>
-    )
-  }
+    );
+  };
 
   return (
     <>
       <ul className="erxes-conversation-init">
-        {conversations.length > 0 &&
-          <ConversationItem
-            key={conversations[0]._id}
-            conversation={conversations[0]}
-            goToConversation={goToConversation}
-          />}
+        {conversations.length > 0 && (
+          <ConversationItem key={conversations[0]._id} conversation={conversations[0]} goToConversation={goToConversation} />
+        )}
         {conversations.length > 1 && seeAllConversationBtn()}
         {createButton()}
       </ul>
