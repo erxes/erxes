f66ae785e2ae04e85c55bfdec8465749ea729efb
munkhsaikhan.g@nmma.co <munkhsaikhan.g@nmma.co>
2024-05-25 16:23:29 +0800

    little

--- packages/plugin-loyalties-ui/src/loyalties/vouchers/components/CampaignList.tsx
+++ packages/plugin-loyalties-ui/src/loyalties/vouchers/components/CampaignList.tsx
@@ -1,10 +1,9 @@
 import { DataWithLoader, Icon, Tip } from "@erxes/ui/src/components";
 import { Sidebar, Wrapper } from "@erxes/ui/src/layout";
 import { __, router } from "@erxes/ui/src/utils";
-import { useLocation, useNavigate } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 
 import { IVoucherCampaign } from "../../../configs/voucherCampaign/types";
-import { Link } from "react-router-dom";
 import React from "react";
 import { SidebarListItem } from "../../common/styles";
 import queryString from "query-string";
