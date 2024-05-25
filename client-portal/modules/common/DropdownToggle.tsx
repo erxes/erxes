9d07017b3d21f3ca63f7fdd63e008282a840f7d1
Mashbat <Mashbat0211@gmail.com>
2024-05-25 13:14:24 +0800

    client-portal/modules/common/DropdownToggle.tsx

--- client-portal/modules/common/DropdownToggle.tsx
+++ client-portal/modules/common/DropdownToggle.tsx
@@ -6,19 +6,42 @@ type Props = {
 };
 
 class DropdownToggle extends React.Component<Props> {
-  handleClick = (e) => {
+  handleClick = (e: React.MouseEvent<HTMLDivElement>) => {
     e.preventDefault();
     e.stopPropagation();
 
     const { onClick } = this.props;
 
     if (onClick) {
-      onClick(e);
+      onClick(e as unknown as React.FormEvent); // Cast to React.FormEvent if needed
+    }
+  };
+
+  handleKeyDown = (e: React.KeyboardEvent<HTMLDivElement>) => {
+    if (e.key === 'Enter' || e.key === ' ') {
+      e.preventDefault();
+      e.stopPropagation();
+
+      const { onClick } = this.props;
+
+      if (onClick) {
+        onClick(e as unknown as React.FormEvent); // Cast to React.FormEvent if needed
+      }
     }
   };
 
   render() {
-    return <div onClick={this.handleClick}>{this.props.children}</div>;
+    return (
+      <div
+        onClick={this.handleClick}
+        onKeyDown={this.handleKeyDown}
+        role="button"
+        tabIndex={0}
+        style={{ cursor: 'pointer' }}
+      >
+        {this.props.children}
+      </div>
+    );
   }
 }
 
