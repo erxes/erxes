7452703205cb923ea90906f0ed0d5995a9885fd3
enkhtuvshinD <87625210+enkhtuvshinD@users.noreply.github.com>
2024-05-25 16:54:41 +0800

    chore: calls bugs (#5257)

--- packages/plugin-calls-api/src/models/definitions/utils.ts
+++ packages/plugin-calls-api/src/models/definitions/utils.ts
@@ -3,14 +3,13 @@ import { nanoid } from 'nanoid';
 /*
  * Mongoose field options wrapper
  */
-export const field = options => {
+export const field = (options) => {
   const { pkey, type, optional } = options;
 
   if (type === String && !pkey && !optional) {
     options.validate = /\S+/;
   }
 
-  // TODO: remove
   if (pkey) {
     options.type = String;
     options.default = () => nanoid();
@@ -19,7 +18,7 @@ export const field = options => {
   return options;
 };
 
-export const schemaWrapper = schema => {
+export const schemaWrapper = (schema) => {
   schema.add({ scopeBrandIds: [String] });
 
   return schema;
