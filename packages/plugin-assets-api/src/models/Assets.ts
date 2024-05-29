73b8d4d270d8495104be30548e9dfd81e42843af
jambaa <jambaa7575@gmail.com>
2024-05-29 18:42:33 +0800

    chore: used ?? instead of ||

--- packages/plugin-assets-api/src/models/Assets.ts
+++ packages/plugin-assets-api/src/models/Assets.ts
@@ -190,7 +190,7 @@ export const loadAssetClass = (models: IModels, subdomain: string) => {
 
       let customFieldsData: ICustomField[] = [];
       const name: string = assetFields.name || '';
-      const description: string = assetFields.description || '';
+      const description: string = assetFields.description ?? '';
       const categoryId = assetFields.categoryId || undefined;
       const parentId = assetFields.parentId || undefined;
       const vendorId: string = assetFields.vendorId || '';
