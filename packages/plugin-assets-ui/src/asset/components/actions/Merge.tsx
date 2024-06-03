0e80ab91798d9315c1d14bbaf08021e418b8c759
jambaa <jambaa7575@gmail.com>
2024-06-03 18:37:02 +0800

    chore: onclick issue

--- packages/plugin-assets-ui/src/asset/components/actions/Merge.tsx
+++ packages/plugin-assets-ui/src/asset/components/actions/Merge.tsx
@@ -97,16 +97,16 @@ const Merge = (props: Props) => {
 
   const renderAssetProperties = (key: string, value: string, icon: string) => {
     return (
-      <li key={key}>
-        <button onClick={() => handleChange(icon, key, value)}>
-          {renderTitle(key)}
-          {renderValue(key, value)}
-          <Icon icon={icon} />
-        </button>
+      <li key={key}
+        onClick={() => handleChange(icon, key, value)}
+        role="button" 
+      >
+        {renderTitle(key)}
+        {renderValue(key, value)}
+        <Icon icon={icon} />
       </li>
     );
-  };
-  
+  };  
 
   const renderTitle = (key: string) => {
     const title = ASSET_INFO[key];
