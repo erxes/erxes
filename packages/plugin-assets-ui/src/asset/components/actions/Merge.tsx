a34516cb86885b0ba8a6f38b22e42d151730839e
jambaa <jambaa7575@gmail.com>
2024-06-03 18:47:32 +0800

    chore: onClick issuye

--- packages/plugin-assets-ui/src/asset/components/actions/Merge.tsx
+++ packages/plugin-assets-ui/src/asset/components/actions/Merge.tsx
@@ -97,21 +97,21 @@ const Merge = (props: Props) => {
 
   const renderAssetProperties = (key: string, value: string, icon: string) => {
     return (
-      <li
-        key={key}
-        onClick={() => handleChange(icon, key, value)}
-        onKeyDown={(event) => {
-          if (event.key === 'Enter') {
-            handleChange(icon, key, value);
-          }
-        }}
-        tabIndex={0} 
-        role="button" 
-      >
-        {renderTitle(key)}
-        {renderValue(key, value)}
-        <Icon icon={icon} />
-      </li>
+      <li 
+      key={key}
+      onClick={() => handleChange(icon, key, value)}
+      onKeyDown={(event) => {
+        if (event.key === 'Enter') {
+          handleChange(icon, key, value);
+        }
+      }}
+      tabIndex={0} 
+    >
+      {renderTitle(key)}
+      {renderValue(key, value)}
+      <Icon icon={icon} />
+    </li>
+    
     );
   };
   
