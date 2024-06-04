814c5671e1c5df2613463255e5626db68dc171fa
jambaa <jambaa7575@gmail.com>
2024-06-04 20:23:46 +0800

    chores: removed <>

--- packages/plugin-clientportal-ui/src/components/FieldConfigForm.tsx
+++ packages/plugin-clientportal-ui/src/components/FieldConfigForm.tsx
@@ -1,12 +1,12 @@
-import CollapseContent from '@erxes/ui/src/components/CollapseContent';
-import FormGroup from '@erxes/ui/src/components/form/Group';
-import ControlLabel from '@erxes/ui/src/components/form/Label';
-import { IField } from '@erxes/ui/src/types';
-import { __ } from '@erxes/ui/src/utils/core';
-import React from 'react';
-import Select from 'react-select';
-import { FieldConfig } from '../containers/FieldConfigForm';
-import { ClientPortalConfig } from '../types';
+import CollapseContent from "@erxes/ui/src/components/CollapseContent";
+import FormGroup from "@erxes/ui/src/components/form/Group";
+import ControlLabel from "@erxes/ui/src/components/form/Label";
+import { IField } from "@erxes/ui/src/types";
+import { __ } from "@erxes/ui/src/utils/core";
+import React from "react";
+import Select from "react-select";
+import { FieldConfig } from "../containers/FieldConfigForm";
+import { ClientPortalConfig } from "../types";
 
 type Props = {
   field: IField;
@@ -30,8 +30,8 @@ function FieldConfigForm(props: Props) {
   const [allowedClientPortals, setAllowedClientPortals] = React.useState<
     ClientPortalConfig[]
   >(
-    props.clientPortals.filter(cp => {
-      return allowedClientPortalIds.includes(cp._id || '');
+    props.clientPortals.filter((cp) => {
+      return allowedClientPortalIds.includes(cp._id || "");
     })
   );
 
@@ -40,59 +40,61 @@ function FieldConfigForm(props: Props) {
       props.onChange({
         fieldId: field._id,
         allowedClientPortalIds,
-        requiredOn
+        requiredOn,
       });
     }
   }, [props.isSubmitted]);
 
   const onChange = (value: any) => {
-    setAllowedClientPortalIds(value.map(v => v.value));
-    const filtered = props.clientPortals.filter(cp => {
-      return value.map(v => v.value).includes(cp._id || '');
+    setAllowedClientPortalIds(value.map((v) => v.value));
+    const filtered = props.clientPortals.filter((cp) => {
+      return value.map((v) => v.value).includes(cp._id || "");
     });
 
     setAllowedClientPortals(filtered);
   };
 
-  const options = array => {
-    return array.map(clientPortal => {
+  const options = (array) => {
+    return array.map((clientPortal) => {
       return {
         value: clientPortal._id,
-        label: clientPortal.name
+        label: clientPortal.name,
       };
     });
   };
 
   return (
-    <>
-      <CollapseContent title={__('Business portal config')} compact={true}>
+    <CollapseContent title={__("Business portal config")} compact={true}>
+      <FormGroup>
+        <ControlLabel>{__("Allowed Business Portals")}</ControlLabel>
+        <p>{__("Please select client portals that can use this field.")}</p>
+        <Select
+          options={options(props.clientPortals)}
+          value={options(props.clientPortals).filter((o) =>
+            allowedClientPortalIds.includes(o.value)
+          )}
+          onChange={onChange}
+          isMulti={true}
+        />
+      </FormGroup>
+
+      {allowedClientPortals.length > 0 && (
         <FormGroup>
-          <ControlLabel>{__('Allowed Business Portals')}</ControlLabel>
-          <p>{__('Please select client portals that can use this field.')}</p>
+          <ControlLabel>{__("Required")}</ControlLabel>
+          <p>{__("Please select client portals that field is required")}</p>
           <Select
-            options={options(props.clientPortals)}
-            value={options(props.clientPortals).filter((o) => allowedClientPortalIds.includes(o.value))}
-            onChange={onChange}
+            options={options(allowedClientPortals)}
+            value={options(allowedClientPortals).filter((o) =>
+              requiredOn.includes(o.value)
+            )}
+            onChange={(value) => {
+              setrequiredOn(value.map((v) => v.value));
+            }}
             isMulti={true}
           />
         </FormGroup>
-
-        {allowedClientPortals.length > 0 && (
-          <FormGroup>
-            <ControlLabel>{__('Required')}</ControlLabel>
-            <p>{__('Please select client portals that field is required')}</p>
-            <Select
-              options={options(allowedClientPortals)}
-              value={options(allowedClientPortals).filter((o) => requiredOn.includes(o.value))}
-              onChange={value => {
-                setrequiredOn(value.map(v => v.value));
-              }}
-              isMulti={true}
-            />
-          </FormGroup>
-        )}
-      </CollapseContent>
-    </>
+      )}
+    </CollapseContent>
   );
 }
 
