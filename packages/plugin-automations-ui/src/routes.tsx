d2af9ddc97bafbf8887e00818f502a8b84956eb2
jambaa <jambaa7575@gmail.com>
2024-06-04 15:33:58 +0800

    chore: removed dupe

--- packages/plugin-automations-ui/src/routes.tsx
+++ packages/plugin-automations-ui/src/routes.tsx
@@ -1,5 +1,10 @@
-import { Route, Routes, useNavigate } from "react-router-dom";
-import { useLocation, useParams } from "react-router-dom";
+import {
+  Route,
+  Routes,
+  useNavigate,
+  useLocation,
+  useParams,
+} from "react-router-dom";
 
 import React from "react";
 import asyncComponent from "@erxes/ui/src/components/AsyncComponent";
