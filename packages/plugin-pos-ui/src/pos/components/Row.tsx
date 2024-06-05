75e6f9956c9189978bc262070d6630bd590013a7
jambaa <jambaa7575@gmail.com>
2024-06-05 19:07:11 +0800

    chore: fixed row props and imports

--- packages/plugin-pos-ui/src/pos/components/Row.tsx
+++ packages/plugin-pos-ui/src/pos/components/Row.tsx
@@ -1,45 +1,34 @@
-import CopyToClipboard from 'react-copy-to-clipboard';
-import dayjs from 'dayjs';
-import React from 'react';
-import {
-  __,
-  ActionButtons,
-  Alert,
-  Button,
-  Icon,
-  Tip,
-  WithPermission,
-} from '@erxes/ui/src';
-import { Capitalize } from '@erxes/ui-settings/src/permissions/styles';
-import { DateWrapper } from '@erxes/ui/src/styles/main';
-import { IPos } from '../../types';
-import { Link } from 'react-router-dom';
-import { RowTitle } from '../../styles';
+import CopyToClipboard from "react-copy-to-clipboard";
+import dayjs from "dayjs";
+import React from "react";
+import { __, ActionButtons, Alert, Button, Icon, Tip } from "@erxes/ui/src";
+import { Capitalize } from "@erxes/ui-settings/src/permissions/styles";
+import { DateWrapper } from "@erxes/ui/src/styles/main";
+import { IPos } from "../../types";
+import { Link } from "react-router-dom";
+import { RowTitle } from "../../styles";
 
 type Props = {
   pos: IPos;
-  isChecked: boolean;
-  toggleBulk: (pos: IPos, checked: boolean) => void;
   remove: (posId: string) => void;
-  showCode?: boolean;
 };
 
 const Row = (props: Props) => {
   const { pos, remove } = props;
 
-  const isOnline = pos.isOnline ? 'online' : 'offline pos';
-  const onServer = pos.onServer ? 'On server' : 'Without main server';
+  const isOnline = pos.isOnline ? "online" : "offline pos";
+  const onServer = pos.onServer ? "On server" : "Without main server";
 
   const createdUser = pos.user || {
-    _id: '',
-    details: { fullName: '' },
+    _id: "",
+    details: { fullName: "" },
   };
 
   const manageAction = (pos) => {
     return (
       <Link to={`/pos/edit/${pos._id}`}>
         <Button btnStyle="link">
-          <Tip text={__('Manage')} placement="top">
+          <Tip text={__("Manage")} placement="top">
             <Icon icon="edit-3" />
           </Tip>
         </Button>
@@ -51,7 +40,7 @@ const Row = (props: Props) => {
     const onClick = () => remove(pos._id);
 
     return (
-      <Tip text={__('Delete')} placement="top">
+      <Tip text={__("Delete")} placement="top">
         <Button
           id="integrationDelete"
           btnStyle="link"
@@ -64,13 +53,13 @@ const Row = (props: Props) => {
 
   const renderCopyAction = (object) => {
     const onCopy = () => {
-      Alert.success('Copied');
+      Alert.success("Copied");
     };
 
     return (
       <CopyToClipboard text={object.token} onCopy={onCopy}>
         <Button btnStyle="link">
-          <Tip text={__('Copy token')} placement="top">
+          <Tip text={__("Copy token")} placement="top">
             <Icon icon="copy" size={15} />
           </Tip>
         </Button>
@@ -93,10 +82,10 @@ const Row = (props: Props) => {
         <strong>{onServer}</strong>
       </td>
       <td>
-        <strong>{pos.branchTitle || ''}</strong>
+        <strong>{pos.branchTitle || ""}</strong>
       </td>
       <td>
-        <strong>{pos.departmentTitle || ''}</strong>
+        <strong>{pos.departmentTitle || ""}</strong>
       </td>
       <td>
         <div key={createdUser._id}>
@@ -106,8 +95,8 @@ const Row = (props: Props) => {
         </div>
       </td>
       <td>
-        <Icon icon="calender" />{' '}
-        <DateWrapper>{dayjs(pos.createdAt).format('ll')}</DateWrapper>
+        <Icon icon="calender" />{" "}
+        <DateWrapper>{dayjs(pos.createdAt).format("ll")}</DateWrapper>
       </td>
 
       <td>
