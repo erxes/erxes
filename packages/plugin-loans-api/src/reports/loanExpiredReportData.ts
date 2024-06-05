f68bf158903b9095c820e54ffe2246b94e86b68b
hitchikermn <hitchiker.mn@gmail.com>
2024-06-05 11:11:30 +0800

    update

--- packages/plugin-loans-api/src/reports/loanExpiredReportData.ts
+++ packages/plugin-loans-api/src/reports/loanExpiredReportData.ts
@@ -2,47 +2,37 @@ import BigNumber from 'bignumber.js';
 import { IModels } from '../connectionResolver';
 import * as moment from 'moment';
 import * as __ from 'lodash';
-
-export async function loanExpiredReportData(models: IModels, filter: any) {
-  const result = await models.Contracts.aggregate([
-    {
-      $group: {
-        _id: '$classification',
-        total_groups: { $sum: 1 }
-      }
-    }
-  ]);
-
-  return result;
-}
+import { generateData } from './utils';
 
 const DIMENSION_OPTIONS = [
   {
     label: 'Number',
     value: 'number',
-    aggregate: { path: 'number', value: 1 }
+    aggregate: { project: { path: 'number', value: 1 } }
   },
   {
     label: 'Classification',
     value: 'classification',
-    aggregate: { path: 'classification', value: 1 }
+    aggregate: { project: { path: 'classification', value: 1 } }
   },
   {
     label: 'Interest Rate',
     value: 'interestRate',
-    aggregate: { path: 'interestRate', value: 1 }
+    aggregate: { project: { path: 'interestRate', value: 1 } }
   },
   {
     label: 'Loss Percent',
     value: 'lossPercent',
-    aggregate: { path: 'lossPercent', value: 1 }
+    aggregate: { project: { path: 'lossPercent', value: 1 } }
   },
   {
     label: 'Contract Type',
     value: 'contractType',
     aggregate: {
-      path: 'contractType',
-      value: '$contractType.name',
+      project: {
+        path: 'contractType',
+        value: '$contractType.name'
+      },
       lookup: [
         {
           $lookup: {
@@ -62,19 +52,19 @@ const DIMENSION_OPTIONS = [
     label: 'StartDate',
     value: 'startDate',
     format: (v: Date | undefined) => v && moment(v).format('YYYY-MM-DD'),
-    aggregate: { path: 'startDate', value: 1 }
+    aggregate: { project: { path: 'startDate', value: 1 } }
   },
   {
     label: 'EndDate',
     value: 'endDate',
     format: (v: Date | undefined) => v && moment(v).format('YYYY-MM-DD'),
-    aggregate: { path: 'endDate', value: 1 }
+    aggregate: { project: { path: 'endDate', value: 1 } }
   },
   {
     label: 'MustPayDate',
     value: 'mustPayDate',
     format: (v: Date | undefined) => v && moment(v).format('YYYY-MM-DD'),
-    aggregate: { path: 'mustPayDate', value: 1 }
+    aggregate: { project: { path: 'mustPayDate', value: 1 } }
   }
 ];
 
@@ -82,98 +72,39 @@ const MEASURE_OPTIONS = [
   {
     label: 'Margin amount',
     value: 'marginAmount',
-    aggregate: { path: 'marginAmount', value: 1 },
+    aggregate: { project: { path: 'marginAmount', value: 1 } },
     format: (v: number = 0) => new BigNumber(v).toFormat()
   },
   {
     label: 'Balance Amount',
     value: 'loanBalanceAmount',
-    aggregate: { path: 'loanBalanceAmount', value: 1 },
+    aggregate: { project: { path: 'loanBalanceAmount', value: 1 } },
     format: (v: number = 0) => new BigNumber(v).toFormat()
   },
   {
     label: 'Loan Amount',
     value: 'leaseAmount',
-    aggregate: { path: 'leaseAmount', value: 1 },
+    aggregate: { project: { path: 'leaseAmount', value: 1 } },
     format: (v: number = 0) => new BigNumber(v).toFormat()
-  },
+  }
 ];
 
 const loanReportData = {
   templateType: 'loanExpiredReportData',
   serviceType: 'loans',
   name: 'Loan Expired Data',
-  chartTypes: ['table', 'bar'],
-  getChartResult: async (models: IModels, filter: any, chartType: string) => {
-    try {
-      let filterValue: any = {};
+  chartTypes: ['table'],
+  getChartResult: async (models: IModels, filter: any) => {
+    const data = await generateData(
+      models,
+      'Contracts',
+      DIMENSION_OPTIONS,
+      MEASURE_OPTIONS,
+      filter
+    );
 
-      if (filter.leaseExpertId) {
-        filterValue.leaseExpertId = filter.leaseExpertId;
-      }
-
-      let aggregate: any = [{ $project: { _id: 0 } }];
-      let project: any = { _id: 0 };
-
-      if (filter.dimension) {
-        for (let key of filter.dimension) {
-          const dimension = DIMENSION_OPTIONS.find((row) => row.value == key);
-          if (dimension) {
-            if (__.isArray(dimension.aggregate)) {
-              aggregate = [...aggregate, ...dimension.aggregate];
-            } else if (__.isObject(dimension.aggregate)) {
-              __.set(
-                project,
-                dimension.aggregate.path,
-                dimension.aggregate.value
-              );
-              if (dimension.aggregate.lookup) {
-                aggregate = [...aggregate, ...dimension.aggregate.lookup];
-              }
-            }
-          }
-        }
-      }
-
-      if (filter.measure) {
-        for (let key of filter.measure) {
-          const measure = MEASURE_OPTIONS.find((row) => row.value == key);
-          if (measure) {
-            if (__.isArray(measure.aggregate)) {
-              aggregate = [...aggregate, ...measure.aggregate];
-            } else if (__.isObject(measure.aggregate)) {
-              __.set(project, measure.aggregate.path, measure.aggregate.value);
-            }
-          }
-        }
-      }
-
-      const contractData = await models.Contracts.aggregate([
-        ...aggregate,
-        { $project: project }
-      ]);
-
-      contractData.forEach((row) => {
-        DIMENSION_OPTIONS.forEach((field) => {
-          if (field.format) {
-            row[field.value] = field.format(row[field.value]);
-          }
-        });
-        MEASURE_OPTIONS.forEach((field) => {
-          if (field.format) {
-            row[field.value] = field.format(row[field.value]);
-          }
-        });
-      });
-
-      const title = 'Loan expiration Data';
-
-      const datasets = { title, data: contractData };
-
-      return datasets;
-    } catch (error) {
-      console.log('error', error);
-    }
+    const title = 'Loan Expiration Data';
+    return { title, data };
   },
 
   filterTypes: [
---
+++ packages/plugin-loans-api/src/reports/utils.ts
@@ -0,0 +1,97 @@
+import * as __ from 'lodash';
+import { IModels } from '../connectionResolver';
+
+interface IOption {
+  label: string;
+  value: string;
+  aggregate?: {
+    project?: {
+      path: string;
+      value: any;
+    };
+    lookup?: any[];
+  };
+  format?: (v: any) => any;
+}
+
+export function generateAggregateOption(
+  aggregate: any[] = [],
+  project: any = {},
+  filter: string[],
+  options: IOption[]
+) {
+  for (let key of filter) {
+    const option = options.find((row) => row.value == key);
+    if (option) {
+      if (__.isArray(option.aggregate)) {
+        aggregate = [...aggregate, ...option.aggregate];
+      } else if (__.isObject(option.aggregate?.project)) {
+        __.set(
+          project,
+          option.aggregate.project.path,
+          option.aggregate.project.value
+        );
+        if (__.isArray(option.aggregate.lookup)) {
+          aggregate = [...aggregate, ...option.aggregate.lookup];
+        }
+      }
+    }
+  }
+  return { project, aggregate };
+}
+
+export async function generateData(
+  models: IModels,
+  modelName: keyof IModels,
+  DIMENSION_OPTIONS: IOption[],
+  MEASURE_OPTIONS: IOption[],
+  filter: { dimension?: string[]; measure?: string[] },
+) {
+  let aggregate: any = [];
+  let project: any = { _id: 0 };
+
+  let { dimension, measure, ...match } = filter;
+
+  if (dimension) {
+    const dimensionAggregate = generateAggregateOption(
+      aggregate,
+      project,
+      dimension,
+      DIMENSION_OPTIONS
+    );
+    aggregate = dimensionAggregate.aggregate;
+    project = dimensionAggregate.project;
+  }
+
+  if (measure) {
+    const measureAggregate = generateAggregateOption(
+      aggregate,
+      project,
+      measure,
+      MEASURE_OPTIONS
+    );
+    aggregate = measureAggregate.aggregate;
+    project = measureAggregate.project;
+  }
+
+  const resultData = await models[modelName].aggregate([
+    ...aggregate,
+    { $project: project },
+    { $match: match }
+  ]);
+
+  resultData.forEach((row) => {
+    DIMENSION_OPTIONS.forEach((field) => {
+      if (field.format && row[field.value]) {
+        row[field.value] = field.format(row[field.value]);
+      }
+    });
+    MEASURE_OPTIONS.forEach((field) => {
+      if (field.format && row[field.value]) {
+        row[field.value] = field.format(row[field.value]);
+      }
+    });
+  });
+
+  return resultData;
+}
