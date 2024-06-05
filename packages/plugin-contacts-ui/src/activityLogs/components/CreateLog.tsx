0d2733aae9d2d1e82f3a8855c11f9730e83144a3
jambaa <jambaa7575@gmail.com>
2024-06-05 12:13:11 +0800

    chore: used if over case

--- packages/plugin-contacts-ui/src/activityLogs/components/CreateLog.tsx
+++ packages/plugin-contacts-ui/src/activityLogs/components/CreateLog.tsx
@@ -1,19 +1,17 @@
-import CompanyCreateLog from './CompanyCreateLog';
-import CustomerCreateLog from './CustomerCreateLog';
-import { IActivityLogItemProps } from '@erxes/ui-log/src/activityLogs/types';
-import React from 'react';
+import CompanyCreateLog from "./CompanyCreateLog";
+import CustomerCreateLog from "./CustomerCreateLog";
+import { IActivityLogItemProps } from "@erxes/ui-log/src/activityLogs/types";
+import React from "react";
 
 class CreatedLog extends React.Component<IActivityLogItemProps> {
   render() {
     const { activity } = this.props;
     const { contentType } = activity;
 
-    switch (contentType) {
-      case 'contacts:customer':
-        return <CustomerCreateLog activity={activity} />;
-
-      default:
-        return <CompanyCreateLog activity={activity} />;
+    if (contentType === "contacts:customer") {
+      return <CustomerCreateLog activity={activity} />;
+    } else {
+      return <CompanyCreateLog activity={activity} />;
     }
   }
 }
