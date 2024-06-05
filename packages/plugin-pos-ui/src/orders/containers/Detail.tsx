03b9edd9eaaffd390faa718162c56d86e8799c4d
jambaa <jambaa7575@gmail.com>
2024-06-05 19:03:32 +0800

    chore: fixed imports

--- packages/plugin-pos-ui/src/orders/containers/Detail.tsx
+++ packages/plugin-pos-ui/src/orders/containers/Detail.tsx
@@ -1,15 +1,17 @@
-import * as compose from 'lodash.flowright';
-import Detail from '../components/Detail';
-import { gql, useQuery, useMutation } from '@apollo/client';
-import React from 'react';
-import { Alert } from '@erxes/ui/src/utils';
-import { graphql } from '@apollo/client/react/hoc';
-import { IOrderDet, PosOrderChangePaymentsMutationResponse } from '../types';
-import { mutations, queries } from '../graphql';
-import { OrderDetailQueryResponse } from '../types';
-import { IPos, PosDetailQueryResponse } from '../../types';
-import { queries as posQueries } from '../../pos/graphql';
-import { Spinner, withProps } from '@erxes/ui/src';
+import Detail from "../components/Detail";
+import { gql, useQuery, useMutation } from "@apollo/client";
+import React from "react";
+import { Alert } from "@erxes/ui/src/utils";
+
+import {
+  IOrderDet,
+  PosOrderChangePaymentsMutationResponse,
+  OrderDetailQueryResponse,
+} from "../types";
+import { mutations, queries } from "../graphql";
+import { IPos, PosDetailQueryResponse } from "../../types";
+import { queries as posQueries } from "../../pos/graphql";
+import { Spinner } from "@erxes/ui/src";
 
 type Props = {
   orderId: string;
@@ -25,8 +27,8 @@ const OrdersDetailContainer = (props: Props) => {
       variables: {
         _id: orderId,
       },
-      fetchPolicy: 'network-only',
-    },
+      fetchPolicy: "network-only",
+    }
   );
 
   const posDetailQuery = useQuery<PosDetailQueryResponse>(
@@ -36,16 +38,16 @@ const OrdersDetailContainer = (props: Props) => {
       variables: {
         _id: posToken,
       },
-      fetchPolicy: 'network-only',
-    },
+      fetchPolicy: "network-only",
+    }
   );
 
   const [posOrderChangePayments] =
     useMutation<PosOrderChangePaymentsMutationResponse>(
       gql(mutations.posOrderChangePayments),
       {
-        refetchQueries: ['posOrders', 'posOrdersSummary', 'posOrderDetail'],
-      },
+        refetchQueries: ["posOrders", "posOrdersSummary", "posOrderDetail"],
+      }
     );
 
   const onChangePayments = (orderId, cashAmount, mobileAmount, paidAmounts) => {
@@ -58,7 +60,7 @@ const OrdersDetailContainer = (props: Props) => {
       },
     })
       .then(() => {
-        Alert.success('You successfully synced erkhet.');
+        Alert.success("You successfully synced erkhet.");
       })
       .catch((e) => {
         Alert.error(e.message);
