2e6bdedd4cacc9cd254f5d586068b8c5e5243178
jambaa <jambaa7575@gmail.com>
2024-06-05 19:09:01 +0800

    chore: removed a prop

--- packages/plugin-pos-ui/src/pos/components/productGroup/CatProdItem.tsx
+++ packages/plugin-pos-ui/src/pos/components/productGroup/CatProdItem.tsx
@@ -1,16 +1,15 @@
-import React, { useState } from 'react';
-import SelectProductCategory from '@erxes/ui-products/src/containers/SelectProductCategory';
-import SelectProducts from '@erxes/ui-products/src/containers/SelectProducts';
-import { Button, ControlLabel, FormGroup } from '@erxes/ui/src';
-import { CatProd } from '../../../types';
-import { FlexRow } from '../../../styles';
-import FormControl from '@erxes/ui/src/components/form/Control';
-import { FormColumn } from '@erxes/ui/src/styles/main';
+import React, { useState } from "react";
+import SelectProductCategory from "@erxes/ui-products/src/containers/SelectProductCategory";
+import SelectProducts from "@erxes/ui-products/src/containers/SelectProducts";
+import { Button, ControlLabel, FormGroup } from "@erxes/ui/src";
+import { CatProd } from "../../../types";
+import { FlexRow } from "../../../styles";
+import FormControl from "@erxes/ui/src/components/form/Control";
+import { FormColumn } from "@erxes/ui/src/styles/main";
 
 type Props = {
   editMapping: (item: CatProd) => void;
   removeMapping: (_id: string) => void;
-  key: string;
   index: number;
   item: CatProd;
 };
@@ -19,10 +18,10 @@ const CatProdItem = (props: Props) => {
   const { item, index, editMapping, removeMapping } = props;
 
   const [state, setState] = useState({
-    categoryId: (item && item.categoryId) || '',
-    code: (item && item.code) || '',
-    name: (item && item.name) || '',
-    productId: (item && item.productId) || '',
+    categoryId: (item && item.categoryId) || "",
+    code: (item && item.code) || "",
+    name: (item && item.name) || "",
+    productId: (item && item.productId) || "",
   });
 
   const renderLabel = (label: string) => {
@@ -49,49 +48,49 @@ const CatProdItem = (props: Props) => {
     <FlexRow key={item._id}>
       <FormColumn>
         <FormGroup>
-          {renderLabel('Product Category')}
+          {renderLabel("Product Category")}
           <SelectProductCategory
             label="Choose product category"
             name="productCategoryId"
             initialValue={state.categoryId}
             customOption={{
-              value: '',
-              label: '...Clear product category filter',
+              value: "",
+              label: "...Clear product category filter",
             }}
-            onSelect={(categoryId) => onSelectChange('categoryId', categoryId)}
+            onSelect={(categoryId) => onSelectChange("categoryId", categoryId)}
             multi={false}
           />
         </FormGroup>
       </FormColumn>
       <FormColumn>
         <FormGroup>
-          {renderLabel('Product Code Contains')}
+          {renderLabel("Product Code Contains")}
           <FormControl
             name="code"
             type="text"
-            defaultValue={state.code || ''}
+            defaultValue={state.code || ""}
             onChange={onChangeInput}
           />
         </FormGroup>
       </FormColumn>
       <FormColumn>
         <FormGroup>
-          {renderLabel('Product Name Contains')}
+          {renderLabel("Product Name Contains")}
           <FormControl
             name="name"
             type="text"
-            defaultValue={state.name || ''}
+            defaultValue={state.name || ""}
             onChange={onChangeInput}
           />
         </FormGroup>
       </FormColumn>
       <FormColumn>
         <FormGroup>
-          {renderLabel('Packaging products')}
+          {renderLabel("Packaging products")}
           <SelectProducts
-            label={''}
+            label={""}
             name="productIds"
-            onSelect={(option) => onSelectChange('productId', option)}
+            onSelect={(option) => onSelectChange("productId", option)}
             initialValue={state.productId}
             multi={false}
           />
