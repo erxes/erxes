d532eb47afcd0a5c13b1b27ea992c44b685752ea
Saihna24 <saihnamunkhsaihan85@gmail.com>
2024-06-05 09:34:10 +0800

    Changed setPlatform to setSelectedPlatform

--- packages/plugin-automations-ui/src/settings/bots/components/index.tsx
+++ packages/plugin-automations-ui/src/settings/bots/components/index.tsx
@@ -45,7 +45,7 @@ const getBotsByPlatform = () => {
 };
 
 function Settings() {
-  const [selectedPlatform, setPlatform] = useState(null as any);
+  const [selectedPlatform, setSelectedPlatform] = useState(null as any);
 
   const header = (
     <HeaderDescription
@@ -65,7 +65,9 @@ function Settings() {
     }
 
     const handleSelectPlatform = (platform) => {
-      setPlatform(selectedPlatform?.name !== platform.name ? platform : null);
+      setSelectedPlatform(
+        selectedPlatform?.name !== platform.name ? platform : null
+      );
     };
 
     return platforms.map((platform) => (
