4b26e82fad0c7ddcdeafb8853790e1656d4a4a3a
hitchikermn <hitchiker.mn@gmail.com>
2024-06-06 18:22:28 +0800

    update

--- packages/plugin-savings-api/src/models/utils/closeUtils.ts
+++ packages/plugin-savings-api/src/models/utils/closeUtils.ts
@@ -1,5 +1,7 @@
+import BigNumber from 'bignumber.js';
 import { IModels } from '../../connectionResolver';
 import { IContractDocument } from '../definitions/contracts';
+import { calcInterest, getDiffDay } from './utils';
 
 export const getCloseInfo = async (
   models: IModels,
@@ -7,7 +9,29 @@ export const getCloseInfo = async (
   contract: IContractDocument,
   date: Date = new Date()
 ) => {
-  const result = {};
+  const day = getDiffDay(contract.startDate, date);
+  const preCloseInterest = calcInterest({
+    balance: contract.savingAmount,
+    interestRate: contract.closeInterestRate,
+    dayOfMonth: day,
+    fixed: 2
+  });
+  const result = {
+    balance: contract.savingAmount,
+    storedInterest: contract.storedInterest,
+    calcedInterest: 0,
+    preCloseInterest: preCloseInterest,
+    total: 0
+  };
 
+  if (date < contract.endDate) {
+    result.total = new BigNumber(contract.savingAmount)
+      .plus(preCloseInterest)
+      .toNumber();
+  } else {
+    result.total = new BigNumber(contract.savingAmount)
+      .plus(contract.storedInterest)
+      .toNumber();
+  }
   return result;
 };
