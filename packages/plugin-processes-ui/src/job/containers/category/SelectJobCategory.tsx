269b983f6972ff914c9b7482beac7fd81acaa433
Saihna24 <saihnamunkhsaihan85@gmail.com>
2024-06-06 15:15:15 +0800

    remove unused prop

--- packages/plugin-processes-ui/src/job/containers/category/SelectJobCategory.tsx
+++ packages/plugin-processes-ui/src/job/containers/category/SelectJobCategory.tsx
@@ -1,26 +1,26 @@
-import { IOption, IQueryParams } from '@erxes/ui/src/types';
+import { IOption, IQueryParams } from "@erxes/ui/src/types";
 
-import { IJobCategory } from '../../types';
-import React from 'react';
-import SelectWithSearch from '@erxes/ui/src/components/SelectWithSearch';
-import { queries } from '../../graphql';
+import { IJobCategory } from "../../types";
+import React from "react";
+import SelectWithSearch from "@erxes/ui/src/components/SelectWithSearch";
+import { queries } from "../../graphql";
 
 // get config options for react-select
 export function generateProductOptions(array: IJobCategory[] = []): IOption[] {
-  return array.map(item => {
+  return array.map((item) => {
     const category = item || ({} as IJobCategory);
 
     const foundedString = category.order.match(/[/]/gi);
 
-    let space = '';
+    let space = "";
 
     if (foundedString) {
-      space = '\u00A0 \u00A0'.repeat(foundedString.length);
+      space = "\u00A0 \u00A0".repeat(foundedString.length);
     }
 
     return {
       value: category._id,
-      label: `${space}${category.code} - ${category.name}`
+      label: `${space}${category.code} - ${category.name}`,
     };
   });
 }
@@ -32,7 +32,7 @@ export default ({
   multi = true,
   label,
   name,
-  customOption
+  customOption,
 }: {
   queryParams?: IQueryParams;
   label: string;
@@ -40,7 +40,6 @@ export default ({
   multi?: boolean;
   customOption?: IOption;
   initialValue?: string | string[];
-  setParam?: boolean;
   name: string;
 }) => {
   const defaultValue = queryParams ? queryParams[name] : initialValue;
