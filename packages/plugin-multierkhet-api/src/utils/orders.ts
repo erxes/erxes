55afec27729db5202ad42e6e3a21fe6e0aeed58d
Saihna24 <saihnamunkhsaihan85@gmail.com>
2024-06-06 10:15:42 +0800

    remove unnecessary conditional checks

--- packages/plugin-multierkhet-api/src/utils/orders.ts
+++ packages/plugin-multierkhet-api/src/utils/orders.ts
@@ -1,10 +1,10 @@
-import { generateModels } from '../connectionResolver';
+import { generateModels } from "../connectionResolver";
 import {
   sendContactsMessage,
   sendCoreMessage,
-  sendProductsMessage
-} from '../messageBroker';
-import { sendRPCMessage } from '../messageBrokerErkhet';
+  sendProductsMessage,
+} from "../messageBroker";
+import { sendRPCMessage } from "../messageBrokerErkhet";
 
 export const getPureDate = (date: Date) => {
   const ndate = new Date(date);
@@ -15,7 +15,7 @@ export const getPureDate = (date: Date) => {
 export const getPostData = async (subdomain, pos, order) => {
   const models = await generateModels(subdomain);
 
-  let erkhetConfig = await models.Configs.getConfig('ERKHET', {});
+  let erkhetConfig = await models.Configs.getConfig("ERKHET", {});
 
   if (
     !erkhetConfig ||
@@ -34,21 +34,21 @@ export const getPostData = async (subdomain, pos, order) => {
       (
         await sendCoreMessage({
           subdomain,
-          action: 'users.findOne',
+          action: "users.findOne",
           data: { _id: order.userId },
           isRPC: true,
-          defaultValue: {}
+          defaultValue: {},
         })
       ).email) ||
-    '';
+    "";
 
-  const productsIds = order.items.map(item => item.productId);
+  const productsIds = order.items.map((item) => item.productId);
   const products = await sendProductsMessage({
     subdomain,
-    action: 'find',
+    action: "find",
     data: { query: { _id: { $in: productsIds } } },
     isRPC: true,
-    defaultValue: []
+    defaultValue: [],
   });
 
   const productCodeById = {};
@@ -71,7 +71,7 @@ export const getPostData = async (subdomain, pos, order) => {
       amount,
       discount: item.discountAmount,
       inventoryCode: productCodeById[item.productId],
-      workerEmail
+      workerEmail,
     });
   }
 
@@ -100,31 +100,31 @@ export const getPostData = async (subdomain, pos, order) => {
     payments[pos.erkhetConfig.defaultPay] = sumSaleAmount;
   }
 
-  let customerCode = '';
+  let customerCode = "";
   if (order.customerId) {
-    const customerType = order.customerType || 'customer';
-    if (customerType === 'company') {
+    const customerType = order.customerType || "customer";
+    if (customerType === "company") {
       customerCode = customerCode = (
         (await sendContactsMessage({
           subdomain,
-          action: 'companies.findOne',
+          action: "companies.findOne",
           data: {
-            _id: order.customerId
+            _id: order.customerId,
           },
           isRPC: true,
-          defaultValue: {}
+          defaultValue: {},
         })) || {}
       ).code;
     } else {
       customerCode = (
         (await sendContactsMessage({
           subdomain,
-          action: 'customers.findOne',
+          action: "customers.findOne",
           data: {
-            _id: order.customerId
+            _id: order.customerId,
           },
           isRPC: true,
-          defaultValue: {}
+          defaultValue: {},
         })) || {}
       ).code;
     }
@@ -132,27 +132,21 @@ export const getPostData = async (subdomain, pos, order) => {
 
   const orderInfos = [
     {
-      date: getPureDate(order.paidDate)
-        .toISOString()
-        .slice(0, 10),
+      date: getPureDate(order.paidDate).toISOString().slice(0, 10),
       orderId: order._id,
       hasVat: order.taxInfo
         ? order.taxInfo.hasVat
-        : pos.ebarimtConfig && pos.ebarimtConfig.hasVat
-        ? true
-        : false,
+        : pos.ebarimtConfig && pos.ebarimtConfig.hasVat,
       hasCitytax: order.taxInfo
         ? order.taxInfo.hasCitytax
-        : pos.ebarimtConfig && pos.ebarimtConfig.hasCitytax
-        ? true
-        : false,
+        : pos.ebarimtConfig && pos.ebarimtConfig.hasCitytax,
       billType: order.billType,
       customerCode,
       description: `${pos.name}`,
-      number: `${pos.erkhetConfig.beginNumber || ''}${order.number}`,
+      number: `${pos.erkhetConfig.beginNumber || ""}${order.number}`,
       details,
-      ...payments
-    }
+      ...payments,
+    },
   ];
 
   let userEmail = pos.erkhetConfig.userEmail;
@@ -162,14 +156,14 @@ export const getPostData = async (subdomain, pos, order) => {
     token: erkhetConfig.apiToken,
     apiKey: erkhetConfig.apiKey,
     apiSecret: erkhetConfig.apiSecret,
-    orderInfos: JSON.stringify(orderInfos)
+    orderInfos: JSON.stringify(orderInfos),
   };
 };
 
 export const orderDeleteToErkhet = async (subdomain, pos, order) => {
   const models = await generateModels(subdomain);
 
-  let erkhetConfig = await models.Configs.getConfig('ERKHET', {});
+  let erkhetConfig = await models.Configs.getConfig("ERKHET", {});
 
   if (
     !erkhetConfig ||
@@ -181,19 +175,19 @@ export const orderDeleteToErkhet = async (subdomain, pos, order) => {
   }
 
   const syncLog = await models.SyncLogs.syncLogsAdd({
-    contentType: 'pos:order',
+    contentType: "pos:order",
     createdAt: new Date(),
     contentId: order._id,
     consumeData: order,
-    consumeStr: JSON.stringify(order)
+    consumeStr: JSON.stringify(order),
   });
   try {
     const orderInfos = [
       {
         date: order.paidDate,
         orderId: order._id,
-        returnKind: 'hard'
-      }
+        returnKind: "hard",
+      },
     ];
 
     let userEmail = pos.erkhetConfig.userEmail;
@@ -203,19 +197,19 @@ export const orderDeleteToErkhet = async (subdomain, pos, order) => {
       token: erkhetConfig.apiToken,
       apiKey: erkhetConfig.apiKey,
       apiSecret: erkhetConfig.apiSecret,
-      orderInfos: JSON.stringify(orderInfos)
+      orderInfos: JSON.stringify(orderInfos),
     };
 
     return await sendRPCMessage(
       models,
       syncLog,
-      'rpc_queue:erxes-automation-erkhet',
+      "rpc_queue:erxes-automation-erkhet",
       {
-        action: 'get-response-return-order',
+        action: "get-response-return-order",
         isJson: true,
         isEbarimt: false,
         payload: JSON.stringify(postData),
-        thirdService: true
+        thirdService: true,
       }
     );
   } catch (e) {
