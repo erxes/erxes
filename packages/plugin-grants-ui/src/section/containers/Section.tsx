7ab4205f6b9a30ebfc435116c4f95f89d136fc7b
ganbaaaa <ganbayo53@gmail.com>
2024-06-07 17:08:58 +0800

    chore: '../../common/type' imported multiple times

--- packages/plugin-grants-ui/src/section/containers/Section.tsx
+++ packages/plugin-grants-ui/src/section/containers/Section.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { gql } from '@apollo/client';
 import { queries } from '../graphql';
-import { RequestQueryResponse } from '../../common/type';
+import { RequestQueryResponse, IGrantRequest } from '../../common/type';
 import { Spinner, withCurrentUser } from '@erxes/ui/src';
 
 import SectionComponent from '../components/Section';
 import { IUser } from '@erxes/ui/src/auth/types';
 import { useQuery } from '@apollo/client';
-import { IGrantRequest } from '../../common/type';
 
 type Props = {
   history: any;
