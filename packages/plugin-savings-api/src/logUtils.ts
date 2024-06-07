51dc84f16720d109799ffc6a84c7d31b6108b22e
Saihna24 <saihnamunkhsaihan85@gmail.com>
2024-06-07 10:15:40 +0800

    remove unused import

--- packages/plugin-savings-api/src/logUtils.ts
+++ packages/plugin-savings-api/src/logUtils.ts
@@ -1,12 +1,11 @@
-import { getSchemaLabels } from '@erxes/api-utils/src/logUtils';
+import { getSchemaLabels } from "@erxes/api-utils/src/logUtils";
 
-import { contractSchema } from './models/definitions/contracts';
-import { transactionSchema } from './models/definitions/transactions';
-import { periodLockSchema } from './models/definitions/periodLocks';
-import { contractTypeSchema } from './models/definitions/contractTypes';
+import { contractSchema } from "./models/definitions/contracts";
+import { transactionSchema } from "./models/definitions/transactions";
+import { periodLockSchema } from "./models/definitions/periodLocks";
+import { contractTypeSchema } from "./models/definitions/contractTypes";
 
-import { putCreateLog, putDeleteLog, putUpdateLog } from '@erxes/api-utils/src';
-import * as _ from 'underscore';
+import { putCreateLog, putDeleteLog, putUpdateLog } from "@erxes/api-utils/src";
 
 const gatherContractFieldNames = async (_models, _doc, prevList = null) => {
   let options = [];
@@ -30,17 +29,17 @@ export const gatherDescriptions = async (params: IParams) => {
   const { models } = extraParams;
 
   let extraDesc = [];
-  let description = '';
+  let description = "";
 
   switch (type) {
-    case 'contract': {
+    case "contract": {
       description = `${object.number} has been ${action}d`;
 
       extraDesc = await gatherContractFieldNames(models, object);
       break;
     }
 
-    case 'collateral': {
+    case "collateral": {
       description = `${object.code} has been ${action}d`;
 
       extraDesc = await gatherContractFieldNames(models, object);
@@ -62,7 +61,7 @@ export async function createLog(subdomain, user, logData) {
       ...descriptions,
       type: `savings:${logData.type}`,
     },
-    user,
+    user
   );
 }
 
@@ -76,7 +75,7 @@ export async function updateLog(subdomain, user, logData) {
       ...descriptions,
       type: `savings:${logData.type}`,
     },
-    user,
+    user
   );
 }
 
@@ -85,18 +84,18 @@ export async function deleteLog(subdomain, user, logData) {
   await putDeleteLog(
     subdomain,
     { ...logData, ...descriptions, type: `savings:${logData.type}` },
-    user,
+    user
   );
 }
 
 export default {
   getSchemaLabels: ({ data: { type } }) => ({
-    status: 'success',
+    status: "success",
     data: getSchemaLabels(type, [
-      { name: 'contract', schemas: [contractSchema] },
-      { name: 'transaction', schemas: [transactionSchema] },
-      { name: 'periodLock', schemas: [periodLockSchema] },
-      { name: 'contractType', schemas: [contractTypeSchema] },
+      { name: "contract", schemas: [contractSchema] },
+      { name: "transaction", schemas: [transactionSchema] },
+      { name: "periodLock", schemas: [periodLockSchema] },
+      { name: "contractType", schemas: [contractTypeSchema] },
     ]),
   }),
 };
