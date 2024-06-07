75c267491546f48e9a86b3a498627f1a2855cbe6
Saihna24 <saihnamunkhsaihan85@gmail.com>
2024-06-07 10:44:48 +0800

    remove unused props

--- packages/plugin-savings-ui/src/contracts/components/schedules/SchedulesList.tsx
+++ packages/plugin-savings-ui/src/contracts/components/schedules/SchedulesList.tsx
@@ -1,17 +1,14 @@
-import { Spinner, Table } from '@erxes/ui/src';
+import { Spinner, Table } from "@erxes/ui/src";
 
-import { ContractsTableWrapper } from '../../styles';
-import { ITransaction } from '../../../transactions/types';
-import React from 'react';
-import ScheduleRow from './ScheduleRow';
-import { __ } from 'coreui/utils';
+import { ContractsTableWrapper } from "../../styles";
+import { ITransaction } from "../../../transactions/types";
+import React from "react";
+import ScheduleRow from "./ScheduleRow";
+import { __ } from "coreui/utils";
 
 interface IProps {
-  contractId: string;
   transactions: ITransaction[];
   loading: boolean;
-  currentYear: number;
-  onClickYear: (year: number) => void;
 }
 
 const SchedulesList = (props: IProps) => {
@@ -26,12 +23,12 @@ const SchedulesList = (props: IProps) => {
       <Table>
         <thead>
           <tr>
-            <th>{__('Date')}</th>
-            <th>{__('Type')}</th>
-            <th>{__('Saving Balance')}</th>
-            <th>{__('Amount')}</th>
-            <th>{__('Stored Interest')}</th>
-            <th>{__('Total')}</th>
+            <th>{__("Date")}</th>
+            <th>{__("Type")}</th>
+            <th>{__("Saving Balance")}</th>
+            <th>{__("Amount")}</th>
+            <th>{__("Stored Interest")}</th>
+            <th>{__("Total")}</th>
           </tr>
         </thead>
         <tbody id="schedules">
