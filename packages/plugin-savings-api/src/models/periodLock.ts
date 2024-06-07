8e544471e108abc2ab60362d9a37a2cbf40af109
Saihna24 <saihnamunkhsaihan85@gmail.com>
2024-06-07 10:22:40 +0800

    remove unused import

--- packages/plugin-savings-api/src/models/periodLock.ts
+++ packages/plugin-savings-api/src/models/periodLock.ts
@@ -1,18 +1,15 @@
 import {
   IPeriodLock,
   IPeriodLockDocument,
-  periodLockSchema
-} from './definitions/periodLocks';
-import { Model } from 'mongoose';
-import { IModels } from '../connectionResolver';
-import { FilterQuery } from 'mongoose';
-import { IStoredInterestDocument } from './definitions/storedInterest';
-import { getDaysInMonth, getDiffDay, getFullDate } from './utils/utils';
-import {
-  CONTRACT_STATUS,
-  STORE_INTEREST_INTERVAL
-} from './definitions/constants';
-import storeInterest from './utils/storeInterestUtils';
+  periodLockSchema,
+} from "./definitions/periodLocks";
+import { Model } from "mongoose";
+import { IModels } from "../connectionResolver";
+import { FilterQuery } from "mongoose";
+import { IStoredInterestDocument } from "./definitions/storedInterest";
+import { getDiffDay, getFullDate } from "./utils/utils";
+import { CONTRACT_STATUS } from "./definitions/constants";
+import storeInterest from "./utils/storeInterestUtils";
 
 export const loadPeriodLockClass = (models: IModels) => {
   class PeriodLock {
@@ -25,7 +22,7 @@ export const loadPeriodLockClass = (models: IModels) => {
       const periodLock = await models.PeriodLocks.findOne(selector);
 
       if (!periodLock) {
-        throw new Error('PeriodLock not found');
+        throw new Error("PeriodLock not found");
       }
 
       return periodLock;
@@ -36,7 +33,7 @@ export const loadPeriodLockClass = (models: IModels) => {
      */
     public static async createPeriodLock(doc: IPeriodLock, subdomain: string) {
       const nextLock = await models.PeriodLocks.findOne({
-        date: { $gte: doc.date }
+        date: { $gte: doc.date },
       })
         .sort({ date: -1 })
         .lean();
@@ -51,7 +48,7 @@ export const loadPeriodLockClass = (models: IModels) => {
 
       const contracts = await models.Contracts.find({
         status: CONTRACT_STATUS.NORMAL,
-        interestRate: { $gt: 0 }
+        interestRate: { $gt: 0 },
       }).lean();
 
       for await (let contract of contracts) {
@@ -75,15 +72,15 @@ export const loadPeriodLockClass = (models: IModels) => {
       await models.PeriodLocks.updateOne({ _id }, { $set: doc });
 
       const prevLock = await models.PeriodLocks.findOne({
-        date: { $lt: doc.date }
+        date: { $lt: doc.date },
       }).sort({ date: -1 });
 
       const transactions = await models.Transactions.find({
         payDate: {
           $lte: doc.date,
-          ...(prevLock?.date ? { $gt: prevLock?.date } : {})
+          ...(prevLock?.date ? { $gt: prevLock?.date } : {}),
         },
-        contractId: { $nin: doc.excludeContracts || [], $exists: true }
+        contractId: { $nin: doc.excludeContracts || [], $exists: true },
       });
 
       return models.PeriodLocks.findOne({ _id });
@@ -93,23 +90,24 @@ export const loadPeriodLockClass = (models: IModels) => {
      * Remove PeriodLock
      */
     public static async removePeriodLocks(_ids: string[]) {
-      const storedInterestList: IStoredInterestDocument[] = await models.StoredInterest.find(
-        { periodLockId: { $in: _ids } }
-      ).lean();
+      const storedInterestList: IStoredInterestDocument[] =
+        await models.StoredInterest.find({
+          periodLockId: { $in: _ids },
+        }).lean();
       for (const storedInterst of storedInterestList) {
         await models.Contracts.updateOne(
           { _id: storedInterst.contractId },
           {
             $set: {
               storedInterst: { $inc: storedInterst.amount * -1 },
-              lastStoredDate: storedInterst.prevStoredDate
-            }
+              lastStoredDate: storedInterst.prevStoredDate,
+            },
           }
         );
       }
 
       await models.StoredInterest.deleteMany({
-        _id: storedInterestList.map(a => a._id)
+        _id: storedInterestList.map((a) => a._id),
       });
       return models.PeriodLocks.deleteMany({ _id: { $in: _ids } });
     }
