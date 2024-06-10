46f5b0cd8acea9ce2587e6625e6e1913f66c9cbb
ganbaaaa <ganbayo53@gmail.com>
2024-06-10 15:18:22 +0800

    chore: Remove unused import

--- packages/plugin-insight-ui/src/components/Sidebar.tsx
+++ packages/plugin-insight-ui/src/components/Sidebar.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import Wrapper from '@erxes/ui/src/layout/components/Wrapper';
-import { isEnabled } from '@erxes/ui/src/utils/core';
 
 import Goals from '../containers/section/Goal';
 import Dashboard from '../containers/section/Dashboard';
