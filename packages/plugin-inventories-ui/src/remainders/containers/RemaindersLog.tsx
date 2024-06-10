fa599017e7f6349929c2efb171e9197e603122d1
jambaa <jambaa7575@gmail.com>
2024-06-10 21:11:42 +0800

    chore: merged a dupe

--- packages/plugin-inventories-ui/src/remainders/containers/RemaindersLog.tsx
+++ packages/plugin-inventories-ui/src/remainders/containers/RemaindersLog.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { useQuery } from '@apollo/client';
-import { gql } from '@apollo/client';
+import { useQuery , gql } from '@apollo/client';
 
 //erxes
 import Spinner from '@erxes/ui/src/components/Spinner';
