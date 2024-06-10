e5e6e8fdba7daa40d17d7f969b1eac74fe6b894b
jambaa <jambaa7575@gmail.com>
2024-06-10 14:29:19 +0800

    chore: value+setters and import

--- packages/plugin-timeclock-ui/src/components/List.tsx
+++ packages/plugin-timeclock-ui/src/components/List.tsx
@@ -1,19 +1,18 @@
-import { menuTimeClock } from '../constants';
-import { __ } from '@erxes/ui/src/utils';
-import React, { useState, useEffect } from 'react';
-import Wrapper from '@erxes/ui/src/layout/components/Wrapper';
-import DataWithLoader from '@erxes/ui/src/components/DataWithLoader';
-import SideBarList from '../containers/sidebar/SideBarList';
-import ConfigList from '../containers/config/ConfigList';
-import TimeclockList from '../containers/timeclock/TimeclockList2';
-import AbsenceList from '../containers/absence/AbsenceList';
-import ReportList from '../containers/report/ReportList';
-import ScheduleList from '../containers/schedule/ScheduleList';
-import LogsList from '../containers/logs/LogsList';
-import { IBranch, IDepartment } from '@erxes/ui/src/team/types';
-import { IScheduleConfig } from '../types';
-import { isEnabled } from '@erxes/ui/src/utils/core';
-import { IUser } from '@erxes/ui/src/auth/types';
+import { menuTimeClock } from "../constants";
+import { __ } from "@erxes/ui/src/utils";
+import React, { useState, useEffect } from "react";
+import Wrapper from "@erxes/ui/src/layout/components/Wrapper";
+import DataWithLoader from "@erxes/ui/src/components/DataWithLoader";
+import SideBarList from "../containers/sidebar/SideBarList";
+import ConfigList from "../containers/config/ConfigList";
+import TimeclockList from "../containers/timeclock/TimeclockList2";
+import AbsenceList from "../containers/absence/AbsenceList";
+import ReportList from "../containers/report/ReportList";
+import ScheduleList from "../containers/schedule/ScheduleList";
+import LogsList from "../containers/logs/LogsList";
+import { IBranch, IDepartment } from "@erxes/ui/src/team/types";
+import { isEnabled } from "@erxes/ui/src/utils/core";
+import { IUser } from "@erxes/ui/src/auth/types";
 
 type Props = {
   currentUser: IUser;
@@ -33,91 +32,90 @@ type Props = {
 };
 
 function List(props: Props) {
-  const { queryParams, isCurrentUserAdmin, route, searchFilter } =
-    props;
+  const { queryParams, isCurrentUserAdmin, route, searchFilter } = props;
 
   const [showSideBar, setShowSideBar] = useState(true);
   const [rightActionBar, setRightActionBar] = useState(<div />);
-  const [Component, setModalComponent] = useState(<div />);
-  const [PaginationFooter, setPagination] = useState(<div />);
+  const [component, setComponent] = useState(<div />);
+  const [paginationFooter, setPaginationFooter] = useState(<div />);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     switch (route) {
-      case 'config':
+      case "config":
         if (isCurrentUserAdmin) {
-          setModalComponent(
+          setComponent(
             <ConfigList
               {...props}
-              getPagination={setPagination}
+              getPagination={setPaginationFooter}
               showSideBar={setShowSideBar}
               getActionBar={setRightActionBar}
               queryParams={queryParams}
-            />,
+            />
           );
         }
         setLoading(false);
         break;
-      case 'report':
-        setModalComponent(
+      case "report":
+        setComponent(
           <ReportList
             {...props}
             reportType={queryParams.reportType}
             showSideBar={setShowSideBar}
             getActionBar={setRightActionBar}
             queryParams={queryParams}
-            getPagination={setPagination}
-          />,
+            getPagination={setPaginationFooter}
+          />
         );
         setLoading(false);
         break;
-      case 'schedule':
-        setModalComponent(
+      case "schedule":
+        setComponent(
           <ScheduleList
             {...props}
             showSideBar={setShowSideBar}
-            getPagination={setPagination}
+            getPagination={setPaginationFooter}
             getActionBar={setRightActionBar}
             queryParams={queryParams}
-          />,
+          />
         );
         setLoading(false);
         break;
-      case 'requests':
-        setModalComponent(
+      case "requests":
+        setComponent(
           <AbsenceList
             {...props}
             showSideBar={setShowSideBar}
-            getPagination={setPagination}
+            getPagination={setPaginationFooter}
             getActionBar={setRightActionBar}
             queryParams={queryParams}
-          />,
+          />
         );
         setLoading(false);
         break;
-      case 'logs':
-        if (!isEnabled('bichil')) {
-          setModalComponent(
+      case "logs":
+        if (!isEnabled("bichil")) {
+          setComponent(
             <LogsList
               {...props}
               showSideBar={setShowSideBar}
-              getPagination={setPagination}
+              getPagination={setPaginationFooter}
               getActionBar={setRightActionBar}
               queryParams={queryParams}
-            />,
+            />
           );
         }
         setLoading(false);
         break;
       default:
-        setModalComponent(
+        setComponent(
           <TimeclockList
             {...props}
             showSideBar={setShowSideBar}
             getActionBar={setRightActionBar}
-            getPagination={setPagination}
+            getPagination={setPaginationFooter}
             queryParams={queryParams}
-          />,
+          />
         );
         setLoading(false);
     }
@@ -127,24 +125,22 @@ function List(props: Props) {
     <Wrapper
       header={
         <Wrapper.Header
-          title={__('Timeclocks')}
+          title={__("Timeclocks")}
           submenu={menuTimeClock(searchFilter, isCurrentUserAdmin)}
         />
       }
       actionBar={rightActionBar}
-      footer={PaginationFooter}
+      footer={paginationFooter}
       content={
         <DataWithLoader
-          data={Component}
+          data={component}
           loading={loading}
-          emptyText={__('Theres no timeclock')}
+          emptyText={__("Theres no timeclock")}
           emptyImage="/images/actions/8.svg"
         />
       }
       leftSidebar={
-        showSideBar && (
-          <SideBarList {...props} queryParams={queryParams} />
-        )
+        showSideBar && <SideBarList {...props} queryParams={queryParams} />
       }
       transparent={true}
       hasBorder={true}
