0257ccb94614397d7e1b113ca3ea5c5218136824
Saihna24 <saihnamunkhsaihan85@gmail.com>
2024-06-10 12:45:04 +0800

    remove unused import

--- packages/plugin-syncpolaris-api/src/utils/deposit/outcomeDeposit.ts
+++ packages/plugin-syncpolaris-api/src/utils/deposit/outcomeDeposit.ts
@@ -1,26 +1,34 @@
 import {
   customFieldToObject,
   fetchPolaris,
-  getCustomer,
-  getContract,
   sendMessageBrokerData,
-} from '../utils';
+} from "../utils";
 
 export const outcomeDeposit = async (subdomain, params) => {
   const transaction = params.object;
 
-  const savingContract = await sendMessageBrokerData(subdomain,'savings','contracts.findOne',{_id:transaction.contractId})
+  const savingContract = await sendMessageBrokerData(
+    subdomain,
+    "savings",
+    "contracts.findOne",
+    { _id: transaction.contractId }
+  );
 
-  const customer = await sendMessageBrokerData(subdomain,'contacts','customers.findOne',{_id:savingContract.customerId})
+  const customer = await sendMessageBrokerData(
+    subdomain,
+    "contacts",
+    "customers.findOne",
+    { _id: savingContract.customerId }
+  );
 
   const customerData = await customFieldToObject(
     subdomain,
-    'contacts:customer',
-    customer,
+    "contacts:customer",
+    customer
   );
 
   const sendData = {
-    operCode: '13610010',
+    operCode: "13610010",
     txnAcntCode: savingContract.number,
     txnAmount: transaction.total,
     rate: 1,
@@ -31,25 +39,25 @@ export const outcomeDeposit = async (subdomain, params) => {
     tcustName: customerData.firstName,
     tcustAddr: customerData.firstName,
     tcustRegister: customerData.registerCode,
-    tcustRegisterMask: '3',
+    tcustRegisterMask: "3",
     tcustContact: customerData.phones[0],
     tranAmt: transaction.total,
     tranCurCode: transaction.currency,
-    sourceType: 'OI',
+    sourceType: "OI",
     isPreview: 0,
     isTmw: 1,
     aspParam: [
       [
         {
           acntCode: savingContract.number,
-          acntType: 'EXPENSE',
+          acntType: "EXPENSE",
         },
       ],
     ],
   };
 
   return await fetchPolaris({
-    op: '13610010',
+    op: "13610010",
     data: [sendData],
     subdomain,
   });
--- packages/plugin-syncpolaris-api/src/utils/loan/createLoan.ts
+++ packages/plugin-syncpolaris-api/src/utils/loan/createLoan.ts
@@ -1,44 +1,52 @@
 import {
   getBranch,
-  getCustomer,
   getUser,
   fetchPolaris,
   customFieldToObject,
   getFullDate,
   updateContract,
-  getProduct,
   sendMessageBrokerData,
-} from '../utils';
-import { activeLoan } from './activeLoan';
-import { createSavingLoan } from './createSavingLoan';
+} from "../utils";
+import { activeLoan } from "./activeLoan";
+import { createSavingLoan } from "./createSavingLoan";
 
 export const createLoan = async (subdomain, params) => {
   const loan = params.updatedDocument || params.object;
 
-  if (loan.leaseType === 'saving')
+  if (loan.leaseType === "saving")
     return await createSavingLoan(subdomain, params);
 
-  const loanData = await customFieldToObject(subdomain, 'loans:contract', loan);
+  const loanData = await customFieldToObject(subdomain, "loans:contract", loan);
 
-  const customer = await sendMessageBrokerData(subdomain,'contacts','customers.findOne', {_id:loan.customerId});
+  const customer = await sendMessageBrokerData(
+    subdomain,
+    "contacts",
+    "customers.findOne",
+    { _id: loan.customerId }
+  );
 
-  const loanProduct = await sendMessageBrokerData(subdomain,'loans','contractType.findOne', {_id:loan.contractTypeId});
+  const loanProduct = await sendMessageBrokerData(
+    subdomain,
+    "loans",
+    "contractType.findOne",
+    { _id: loan.contractTypeId }
+  );
 
   const leasingExpert = await getUser(subdomain, loan.leasingExpertId);
 
   const branch = await getBranch(subdomain, loan.branchId);
-  
+
   let sendData: any = {
     custCode: customer.code,
     name: `${customer.code} ${customer.firstName} ${customer.code} ${customer.lastName}`,
     name2: `${customer.code} ${customer.firstName} ${customer.code} ${customer.lastName}`,
     prodCode: loanProduct.code,
-    prodType: 'LOAN',
+    prodType: "LOAN",
     purpose: loanData.purpose,
     subPurpose: loanData.subPurpose,
     isNotAutoClass: 0,
     comRevolving: 0,
-    dailyBasisCode: 'ACTUAL/365',
+    dailyBasisCode: "ACTUAL/365",
     curCode: loanData.currency,
     approvAmount: loanData.leaseAmount,
     impairmentPer: 0,
@@ -48,14 +56,14 @@ export const createLoan = async (subdomain, params) => {
     startDate: getFullDate(loanData.startDate),
     endDate: getFullDate(loanData.endDate),
     termLen: loanData.tenor,
-    IsGetBrchFromOutside: '0',
-    segCode: '1',
-    status: 'N',
-    slevel: '1',
-    classNoTrm: '1',
-    classNoQlt: '1',
-    classNo: '1',
-    termBasis: 'M',
+    IsGetBrchFromOutside: "0",
+    segCode: "1",
+    status: "N",
+    slevel: "1",
+    classNoTrm: "1",
+    classNoQlt: "1",
+    classNo: "1",
+    termBasis: "M",
     isBrowseAcntOtherCom: 0,
     repayPriority: 0,
     useSpclAcnt: 0,
@@ -66,19 +74,19 @@ export const createLoan = async (subdomain, params) => {
   };
 
   const result = await fetchPolaris({
-    op: '13610253',
+    op: "13610253",
     data: [sendData],
     subdomain,
   }).then((a) => JSON.parse(a));
 
-  if (typeof result === 'string') {
+  if (typeof result === "string") {
     await updateContract(
       subdomain,
       { _id: loan._id },
       { $set: { number: result } },
-      'loans',
+      "loans"
     );
-    await activeLoan(subdomain, [result, 'данс нээв', null]);
+    await activeLoan(subdomain, [result, "данс нээв", null]);
   }
 
   return result;
