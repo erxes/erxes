d450f0b02d9b363cf15bf26ff90b2a3124ada2ab
ganbaaaa <ganbayo53@gmail.com>
2024-06-11 16:31:04 +0800

    chore: Remove unused import

--- packages/plugin-khanbank-ui/src/modules/configs/types.ts
+++ packages/plugin-khanbank-ui/src/modules/configs/types.ts
@@ -1,5 +1,3 @@
-import { IUser } from '@erxes/ui/src/auth/types';
-import { IDepartment } from '@erxes/ui/src/team/types';
 import { QueryResponse } from '@erxes/ui/src/types';
 
 export interface IKhanbankConfigsItem {
