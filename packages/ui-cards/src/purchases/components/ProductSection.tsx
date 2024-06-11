4cb35c2dc22ce48b635757c06226a59858a69078
jambaa <jambaa7575@gmail.com>
2024-06-11 19:20:52 +0800

    chore: changed boolean

--- packages/ui-cards/src/purchases/components/ProductSection.tsx
+++ packages/ui-cards/src/purchases/components/ProductSection.tsx
@@ -105,7 +105,7 @@ function ProductSection({
           {productName}
           {quantity && (
             <Quantity>
-              ({quantity} {uom ? uom : 'PC'})
+              ({quantity} {uom || 'PC'})
             </Quantity>
           )}
         </div>
