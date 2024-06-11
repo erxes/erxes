0fbc8d8116a9b03bacaf92318d6993b456c5ef35
ganbaaaa <ganbayo53@gmail.com>
2024-06-11 12:08:03 +0800

    chore:Remove unused imort

--- packages/plugin-insight-ui/src/containers/utils/SelectMembersBox.tsx
+++ packages/plugin-insight-ui/src/containers/utils/SelectMembersBox.tsx
@@ -6,7 +6,6 @@ import Alert from "@erxes/ui/src/utils/Alert/index";
 import React from "react";
 import SelectMembersBox from "../../components/utils/SelectMembersBox";
 import { UsersQueryResponse } from "@erxes/ui/src/auth/types";
-import { __ } from "@erxes/ui/src/utils/index";
 import { queries as teamQueries } from "@erxes/ui/src/team/graphql";
 
 type Props = {
