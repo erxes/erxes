e475d1e9203ab170dc04cba915d26f5daa6d2ec9
jambaa <jambaa7575@gmail.com>
2024-06-11 19:08:39 +0800

    chore: replaced boolean

--- packages/ui-cards/src/boards/containers/editForm/CustomFieldsSection.tsx
+++ packages/ui-cards/src/boards/containers/editForm/CustomFieldsSection.tsx
@@ -83,7 +83,7 @@ export default (props: Props) => {
             },
           },
         }),
-        skip: !isEnabled('forms') ? true : false,
+        skip: !isEnabled('forms'),
       }),
       graphql<Props, SaveMutation, IItemParams>(
         gql(options.mutations.editMutation),
