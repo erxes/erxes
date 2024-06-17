a3ae094bd8dd123543b22ab6bf268dcff59f04af
Enkhtuvshin Narmandakh <37796969+Enkhtuvshin0513@users.noreply.github.com>
2024-06-17 11:54:20 +0800

    perf(monitoring): add posthog on ui & add sentry on api

--- packages/core-ui/src/routes.tsx
+++ packages/core-ui/src/routes.tsx
@@ -20,6 +20,7 @@ import { getVersion } from "@erxes/ui/src/utils/core";
 import queryString from "query-string";
 import withCurrentUser from "modules/auth/containers/withCurrentUser";
 import { getEnv } from "@erxes/ui/src/utils";
+import posthog from "posthog-js";
 
 const MainLayout = asyncComponent(
   () =>
@@ -71,7 +72,11 @@ const renderRoutes = currentUser => {
   }
 
   const { VERSION } = getVersion();
-  const { REACT_APP_SENTRY_URL } = getEnv();
+  const {
+    REACT_APP_SENTRY_URL,
+    REACT_APP_PUBLIC_POSTHOG_KEY,
+    REACT_APP_PUBLIC_POSTHOG_HOST
+  } = getEnv();
 
   if (REACT_APP_SENTRY_URL) {
     Sentry.init({
@@ -84,6 +89,12 @@ const renderRoutes = currentUser => {
       tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
       replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
       replaysOnErrorSampleRate: 1.0 // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+    });
+  }
+
+  if (REACT_APP_PUBLIC_POSTHOG_KEY && REACT_APP_PUBLIC_POSTHOG_HOST) {
+    posthog.init(REACT_APP_PUBLIC_POSTHOG_KEY, {
+      api_host: REACT_APP_PUBLIC_POSTHOG_HOST
     });
   }
 
