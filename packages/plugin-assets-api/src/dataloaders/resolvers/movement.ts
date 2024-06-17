768695c0d160c2cbba584b44765c0f09fbfd76f1
Temuq <Temuujintemka680@gmail.com>
2024-06-17 15:08:21 +0800

    Chore : Unexpected empty object pattern.

--- packages/plugin-assets-api/src/dataloaders/resolvers/movement.ts
+++ packages/plugin-assets-api/src/dataloaders/resolvers/movement.ts
@@ -6,13 +6,13 @@ export default {
     return models.Assets.findOne({ _id });
   },
 
-  async user(movement: IMovementDocument, {}, { dataLoaders }: IContext) {
+  async user(movement: IMovementDocument, { dataLoaders }: IContext) {
     return (
       (movement.userId && dataLoaders.teamMember.load(movement.userId)) || null
     );
   },
 
-  async items(movement: IMovementDocument, {}, { models }: IContext) {
+  async items(movement: IMovementDocument, { models }: IContext) {
     return await models.MovementItems.find({ movementId: movement._id });
   }
 };
