b148512d1848237c4fa87ad25f90b5fa39bff2d5
AimuldirA <aimoshaibek03@gmail.com>
2024-06-17 16:04:08 +0800

    Remove this useless assignment to variable "show".

--- packages/plugin-notifications-ui/src/components/Notifier.tsx
+++ packages/plugin-notifications-ui/src/components/Notifier.tsx
@@ -9,7 +9,6 @@ const NotifierContainer = styled.div`
 `;
 
 const Notifier = () => {
-  const [activeRoute, setActiveRoute] = useState<string>('feature');
 
   const renderNotifications = () => {
     return (
