f92778426ef987bf53ebbf03d43401933aa3a613
AimuldirA <aimoshaibek03@gmail.com>
2024-06-19 10:45:50 +0800

    chore: comment

--- packages/plugin-notifications-api/src/cronjobs/notifications.ts
+++ packages/plugin-notifications-api/src/cronjobs/notifications.ts
@@ -1,3 +1,4 @@
+import * as schedule from 'node-schedule';
 import { generateModels } from '../connectionResolver';
 import { sendCoreMessage } from '../messageBroker';
 
@@ -28,4 +29,19 @@ export const removeOldNotifications = async (subdomain: string) => {
   });
 };
 
+/**
+ * *    *    *    *    *    *
+ * ┬    ┬    ┬    ┬    ┬    ┬
+ * │    │    │    │    │    |
+ * │    │    │    │    │    └ day of week (0 - 7) (0 or 7 is Sun)
+ * │    │    │    │    └───── month (1 - 12)
+ * │    │    │    └────────── day of month (1 - 31)
+ * │    │    └─────────────── hour (0 - 23)
+ * │    └──────────────────── minute (0 - 59)
+ * └───────────────────────── second (0 - 59, OPTIONAL)
+ */
+// schedule.scheduleJob('0 45 23 * * *', async () => {
+//   debugCrons('Remove old notifications ....');
 
+//   await removeOldNotifications();
+// });
