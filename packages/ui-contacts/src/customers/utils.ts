00855658c80a1452559c1b5954c8e32c229bafb9
“batmnkh2344” <batmnkh2344@gmail.com>
2024-06-23 19:03:37 +0800

    chore: update

--- packages/ui-contacts/src/customers/utils.ts
+++ packages/ui-contacts/src/customers/utils.ts
@@ -14,7 +14,7 @@ export const genderChoices = __ => {
 };
 
 export const isValidPhone = (phone: string) => {
-  const phoneRegex = /^(\+*)(\d*)([(]\d{1,3}[)]*)*(\s?\d+|\+\d{2,3}\s\d+|\d+)[\s|-]?\d+([\s|-]?\d+){1,2}(\s)*$/gm;
+  const phoneRegex = /^(\+{0,})(\d{0,})([(]{1}\d{1,3}[)]{0,}){0,}(\s?\d+|\+\d{2,3}\s{1}\d+|\d+){1}[\s|-]?\d+([\s|-]?\d+){1,2}(\s){0,}$/gm;
 
   return phoneRegex.test(phone);
 };
