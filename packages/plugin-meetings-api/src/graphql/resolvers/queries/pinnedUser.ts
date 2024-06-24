7ff0eae25b4d85aef4954974063b42a088cd4607
alliumiu <boloruyanga07@gmail.com>
2024-06-24 10:42:26 +0800

    chore: replaced empty object pattern

--- packages/plugin-meetings-api/src/graphql/resolvers/queries/pinnedUser.ts
+++ packages/plugin-meetings-api/src/graphql/resolvers/queries/pinnedUser.ts
@@ -2,7 +2,7 @@ import { paginate } from '@erxes/api-utils/src';
 import { IContext } from '../../../messageBroker';
 
 const pinnedUserQueries = {
-  async meetingPinnedUsers(_root, {}, { models, user }: IContext) {
+  async meetingPinnedUsers(_root, _, { models, user }: IContext) {
     return await paginate(models.PinnedUsers.findOne({ userId: user._id }), {});
   }
 };
