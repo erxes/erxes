57fbe19685d14e0c786c0a3271db02661fed8d73
Tuguldur Ch <tuguldur.ch@nmma.co>
2024-06-25 23:56:54 +0800

    fix(template): change ui scope

--- packages/plugin-template-ui/src/configs.js
+++ packages/plugin-template-ui/src/configs.js
@@ -1,15 +1,23 @@
 module.exports = {
   srcDir: __dirname,
-  name: "templates",
+  name: 'template',
   port: 3128,
-  scope: "templates",
+  scope: 'template',
   exposes: {
-    "./routes": "./src/routes.tsx",
+    './routes': './src/routes.tsx'
   },
   routes: {
-    url: "http://localhost:3128/remoteEntry.js",
-    scope: "templates",
-    module: "./routes",
+    url: 'http://localhost:3128/remoteEntry.js',
+    scope: 'template',
+    module: './routes'
   },
-  menus: [{ "text": "Templates", "to": "/settings/templates", "image": "/images/icons/erxes-18.svg", "location": "settings", "scope": "templates" }],
+  menus: [
+    {
+      text: 'Templates',
+      to: '/settings/templates',
+      image: '/images/icons/erxes-18.svg',
+      location: 'settings',
+      scope: 'template'
+    }
+  ]
 };
