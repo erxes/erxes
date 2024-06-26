0514d478466fae88cbdd41680492f9326939dbb7
ganbaaaa <ganbayo53@gmail.com>
2024-06-26 14:00:17 +0800

    chore: Remove unused import

--- packages/plugin-insight-ui/src/components/Form.tsx
+++ packages/plugin-insight-ui/src/components/Form.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useRef} from "react";
 
 import { CSSTransition } from "react-transition-group";
 import { RightDrawerContainer } from "../styles";
