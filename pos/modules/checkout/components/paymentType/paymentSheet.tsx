96ad3f72671adf2cec63aebacd1cf37308b04857
KhBaterdene <95618276+KhBaterdene@users.noreply.github.com>
2024-08-06 14:05:14 +0800

    fix: pos mobile print (#5565)

--- pos/modules/checkout/components/paymentType/paymentSheet.tsx
+++ pos/modules/checkout/components/paymentType/paymentSheet.tsx
@@ -1,10 +1,16 @@
 import dynamic from "next/dynamic"
-import { currentPaymentTypeAtom, modeAtom } from "@/store"
+import {
+  checkoutDialogOpenAtom,
+  currentPaymentTypeAtom,
+  modeAtom,
+} from "@/store"
 import { paymentSheetAtom } from "@/store/ui.store"
-import { useAtom, useAtomValue } from "jotai"
+import { useAtom, useAtomValue, useSetAtom } from "jotai"
+import { XIcon } from "lucide-react"
 
 import { ALL_BANK_CARD_TYPES, BANK_CARD_TYPES } from "@/lib/constants"
 import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
 import { LoaderIcon, LoaderText, LoaderWrapper } from "@/components/ui/loader"
 import { Sheet, SheetContent } from "@/components/ui/sheet"
 
@@ -12,6 +18,7 @@ import HandleNotPaidAmount from "./HandleNotPaidAmount"
 
 const PaymentSheet = () => {
   const [openSheet, setOpenSheet] = useAtom(paymentSheetAtom)
+  const setOpenDialog = useSetAtom(checkoutDialogOpenAtom)
   const type = useAtomValue(currentPaymentTypeAtom)
   const mode = useAtomValue(modeAtom)
   const isKiosk = mode === "kiosk"
@@ -27,11 +34,23 @@ const PaymentSheet = () => {
         <SheetContent
           className={cn(
             "flex flex-col",
-            type === "mobile" && "sm:max-w-3xl",
+            type === "mobile" && "sm:max-w-3xl p-0",
             isKiosk && "h-2/3 rounded-t-3xl sm:max-w-none"
           )}
           side={isKiosk ? "bottom" : undefined}
         >
+          {mode === "mobile" && (
+            <Button
+              variant="secondary"
+              className="absolute right-4 top-4"
+              onClick={() => {
+                setOpenSheet(false)
+                setOpenDialog(true)
+              }}
+            >
+              <XIcon strokeWidth={1.5} className="h-5 w-5 mr-1 -ml-1" /> Хаах
+            </Button>
+          )}
           {type === "mobile" && <MobileSheet />}
           {type === BANK_CARD_TYPES.KHANBANK && <KhanSheet />}
           {type === BANK_CARD_TYPES.TDB && <TDBSheet />}
