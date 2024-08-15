8c47230b68bb790f30f2a31f7e7dda5ecfc4956c
baterdenets <miiga0509@gmail.com>
2024-08-15 22:25:58 +0800

    fix: register number duplicated get CIF from polaris

--- packages/plugin-syncpolaris-api/src/utils/customer/createCustomer.ts
+++ packages/plugin-syncpolaris-api/src/utils/customer/createCustomer.ts
@@ -1,63 +1,78 @@
-import { customFieldToObject, updateCustomer, fetchPolaris } from '../utils';
-import { IPolarisCustomer } from './types';
-import { validateObject } from './validator';
+import { customFieldToObject, updateCustomer, fetchPolaris } from "../utils";
+import { getCustomerDetailByRegister } from "./getCustomerDetailByRegister";
+import { IPolarisCustomer } from "./types";
+import { validateObject } from "./validator";
 
 export const createCustomer = async (subdomain: string, params) => {
   const customer = params.updatedDocument || params.object;
 
   const data = await customFieldToObject(
     subdomain,
-    'contacts:customer',
-    customer,
+    "contacts:customer",
+    customer
   );
 
   let sendData: IPolarisCustomer = {
     lastName: data.lastName,
     firstName: data.firstName,
     familyName: data.familyName,
-    email: data.emails.join(','),
-    mobile: data.phones.join(','),
+    email: data.emails.join(","),
+    mobile: data.phones.join(","),
     birthDate: data.birthDate,
-
-    custSegCode: '81',
+    custSegCode: "81",
     isVatPayer: data.isVatPayer,
     sexCode: data.sexCode,
-    taxExemption: data.taxExemption ?? '0',
-    status: '1',
+    taxExemption: data.taxExemption ?? "0",
+    status: "1",
     noCompany: data.noCompany ?? 0,
     isCompanyCustomer: 1,
     industryId: data.industryId,
     birthPlaceId: data.birthPlaceId,
     shortName: data.middleName,
-    registerMaskCode: '3',
+    registerMaskCode: "3",
     registerCode: data.registerCode,
-    countryCode: '496',
-    industryName: data.industryName ?? '',
-    catId: data.catId ?? '',
-    ethnicGroupId: data.ethnicGroupId ?? '',
-    langCode: data.langCode ?? '1',
-    maritalStatus: data.maritalStatus ?? '1',
-    birthPlaceName: data.birthPlaceName ?? '',
-    birthPlaceDetail: data.birthPlaceDetail ?? '',
-    phone: data.phones.join(','),
-    fax: data.fax ?? '',
-    isBl: data.isBl ?? '0',
-    isPolitical: data.isPolitical ?? '0',
+    countryCode: "496",
+    industryName: data.industryName ?? "",
+    catId: data.catId ?? "",
+    ethnicGroupId: data.ethnicGroupId ?? "",
+    langCode: data.langCode ?? "1",
+    maritalStatus: data.maritalStatus ?? "1",
+    birthPlaceName: data.birthPlaceName ?? "",
+    birthPlaceDetail: data.birthPlaceDetail ?? "",
+    phone: data.phones.join(","),
+    fax: data.fax ?? "",
+    isBl: data.isBl ?? "0",
+    isPolitical: data.isPolitical ?? "0",
   };
 
   await validateObject(sendData);
 
   const customerCode = await fetchPolaris({
     subdomain,
-    op: '13610313',
+    op: "13610313",
     data: [sendData],
-  }).then((res) => JSON.parse(res));
+  }).catch(async (e) => {
+    //check register number duplicated
+    if (e.message.includes("41020330")) {
+      let customerData = await getCustomerDetailByRegister(subdomain, {
+        register: sendData.registerCode,
+      });
+
+      if (customerData) {
+        await updateCustomer(
+          subdomain,
+          { _id: customer._id },
+          { code: customerData.custCode }
+        );
+      }
+    }
+  });
 
   if (customerCode) {
     await updateCustomer(
       subdomain,
       { _id: customer._id },
-      { code: customerCode },
+      { code: customerCode }
     );
   }
 
---
+++ packages/plugin-syncpolaris-api/src/utils/customer/getCustomerDetailByRegister.ts
@@ -0,0 +1,22 @@
+import { fetchPolaris } from "../utils";
+
+interface IParams {
+  register: string;
+}
+
+export const getCustomerDetailByRegister = async (
+  subdomain: string,
+  params: IParams
+) => {
+  if (!params.register) throw new Error("Register required!");
+
+  let sendData = [params.register];
+
+  const customerDetail = await fetchPolaris({
+    subdomain,
+    op: "13610335",
+    data: sendData,
+  });
+
+  return JSON.parse(customerDetail);
+};
