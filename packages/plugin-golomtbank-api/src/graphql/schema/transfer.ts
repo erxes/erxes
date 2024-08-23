3e42727be44cf7fc7134eed440b58acde8bf4443
Enkhtuvshin Narmandakh <37796969+Enkhtuvshin0513@users.noreply.github.com>
2024-08-23 09:24:04 +0800

    fix khanbank plugin graphql type

--- packages/plugin-golomtbank-api/src/graphql/schema/transfer.ts
+++ packages/plugin-golomtbank-api/src/graphql/schema/transfer.ts
@@ -5,22 +5,8 @@ export const types = `
     transferid: String
     systemDate: String
   }
-
-  enum TransferType {
-    domestic
-    interbank
-  }
-  
-  input TransferInput {
-      genericType: String
-      registerNumber: String
-      type: String
-      refCode: String
-      initiator: JSON
-      receives: [JSON]
-      remarks: String
-  }
 `;
+
 export const mutations = `
   golomtBankTransfer(configId: String!, fromAccount: String, toAccount: String, toAccountName: String, toBank: String, toCurrency: String, fromCurrency: String, toDescription: String, fromDescription: String, toAmount: String, fromAmount: String, refCode: String): JSON
 `;
