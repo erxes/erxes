22726a2ffc8ecedec9f5e3c8fb20b1d80a699657
Batu <dev@erxes.io>
2024-09-16 12:42:10 +0800

    add footer header text

--- packages/plugin-pos-ui/src/pos/components/step/EbarimtConfig.tsx
+++ packages/plugin-pos-ui/src/pos/components/step/EbarimtConfig.tsx
@@ -1,11 +1,17 @@
-import React, { useState } from "react";
-import { __, ControlLabel, FormControl, FormGroup } from "@erxes/ui/src";
-import { Block, BlockRow, FlexColumn, FlexItem } from "../../../styles";
-import { LeftItem } from "@erxes/ui/src/components/step/styles";
-import { IPos } from "../../../types";
+import React, { useState } from 'react';
+import {
+  __,
+  ControlLabel,
+  FormControl,
+  FormGroup,
+  Textarea,
+} from '@erxes/ui/src';
+import { Block, BlockRow, FlexColumn, FlexItem } from '../../../styles';
+import { LeftItem } from '@erxes/ui/src/components/step/styles';
+import { IPos } from '../../../types';
 
 type Props = {
-  onChange: (name: "ebarimtConfig", value: any) => void;
+  onChange: (name: 'ebarimtConfig', value: any) => void;
   pos?: IPos;
 };
 
@@ -16,27 +22,28 @@ const EbarimtConfig = (props: Props) => {
     props.pos && props.pos.ebarimtConfig
       ? props.pos.ebarimtConfig
       : {
-        companyName: "",
-        ebarimtUrl: "",
-        checkCompanyUrl: "",
-        hasVat: false,
-        hasCitytax: false,
-        defaultPay: "debtAmount",
-        districtCode: "",
-        companyRD: "",
-        defaultGSCode: "",
-        vatPercent: 0,
-        cityTaxPercent: 0,
-        footerText: "",
-        hasCopy: false,
-      }
+          companyName: '',
+          ebarimtUrl: '',
+          checkCompanyUrl: '',
+          hasVat: false,
+          hasCitytax: false,
+          defaultPay: 'debtAmount',
+          districtCode: '',
+          companyRD: '',
+          defaultGSCode: '',
+          vatPercent: 0,
+          cityTaxPercent: 0,
+          headerText: '',
+          footerText: '',
+          hasCopy: false,
+        }
   );
 
   const onChangeConfig = (code: string, value) => {
     const newConfig = { ...config, [code]: value };
 
     setConfig(newConfig);
-    onChange("ebarimtConfig", newConfig);
+    onChange('ebarimtConfig', newConfig);
   };
 
   const onChangeCheckbox = (code: string, e) => {
@@ -51,15 +58,17 @@ const EbarimtConfig = (props: Props) => {
     key: string,
     title?: string,
     description?: string,
-    type?: string
+    type?: string,
+    textarea?: boolean
   ) => {
+    const Control = textarea ? Textarea : FormControl;
     return (
       <FormGroup>
         <ControlLabel>{title || key}</ControlLabel>
         {description && <p>{__(description)}</p>}
-        <FormControl
+        <Control
           value={config[key]}
-          type={type || "text"}
+          type={type || 'text'}
           onChange={onChangeInput.bind(this, key)}
           required={true}
         />
@@ -113,30 +122,31 @@ const EbarimtConfig = (props: Props) => {
           </Block>
 
           <Block>
-            <h4>{__("VAT")}</h4>
+            <h4>{__('VAT')}</h4>
             <BlockRow>
-              {renderCheckbox("hasVat", "Has VAT", "")}
-              {renderInput("vatPercent", "VAT Percent", "", "number")}
+              {renderCheckbox('hasVat', 'Has VAT', '')}
+              {renderInput('vatPercent', 'VAT Percent', '', 'number')}
             </BlockRow>
           </Block>
           <Block>
-            <h4>{__("UB city tax")}</h4>
+            <h4>{__('UB city tax')}</h4>
             <BlockRow>
-              {renderCheckbox("hasCitytax", "Has UB city tax", "")}
+              {renderCheckbox('hasCitytax', 'Has UB city tax', '')}
               {renderInput(
-                "cityTaxPercent",
-                "UB city tax Percent",
-                "",
-                "number"
+                'cityTaxPercent',
+                'UB city tax Percent',
+                '',
+                'number'
               )}
             </BlockRow>
           </Block>
           <Block />
           <Block>
-            <h4>{__("Footer")}</h4>
+            <h4>{__('UI Config')}</h4>
             <BlockRow>
-              {renderInput("footerText", "Footer text", "")}
-              {renderCheckbox("hasCopy", "Has copy", "")}
+              {renderInput('headerText', 'Header text', '', 'text', true)}
+              {renderInput('footerText', 'Footer text', '', 'text', true)}
+              {renderCheckbox('hasCopy', 'Has copy', '')}
             </BlockRow>
           </Block>
           <Block />
