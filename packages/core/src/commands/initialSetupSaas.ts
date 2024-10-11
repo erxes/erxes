1cf5248cf7e9088912b59004f5b8ae26536892da
Enkhtuvshin Narmandakh <37796969+Enkhtuvshin0513@users.noreply.github.com>
2024-10-11 13:26:06 +0800

    update saas initial setup

---
+++ packages/core/src/commands/initialSetupSaas.ts
@@ -0,0 +1,25 @@
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+import { getOrganizations } from "@erxes/api-utils/src/saas/saas";
+
+const { MONGO_URL = "" } = process.env;
+
+if (!MONGO_URL) {
+  throw new Error(`Environment variable MONGO_URL not set.`);
+}
+
+const command = async () => {
+  const organizations = await getOrganizations();
+
+  for (const org of organizations) {
+    fetch(`https://${org.subdomain}.api.erxes.io/api/initial-setup`);
+  }
+
+  console.log(`Process finished at: ${new Date()}`);
+
+  process.exit();
+};
+
+command();
