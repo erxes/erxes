ead72d3353ea30dbf970dc15cb73c7f05943eb55
Tuguldur Ch <tuguldur.ch@nmma.co>
2024-10-17 15:44:01 +0800

    debug:facebook bot

--- packages/plugin-facebook-api/src/automations/messages.ts
+++ packages/plugin-facebook-api/src/automations/messages.ts
@@ -15,6 +15,7 @@ import {
 } from "./utils";
 import { getEnv } from "../commonUtils";
 import graphqlPubsub from "@erxes/api-utils/src/graphqlPubsub";
+import { debugInfo } from "@erxes/api-utils/src/debuggers";
 
 const generateMessages = async (
   subdomain: string,
@@ -159,6 +160,10 @@ const generateMessages = async (
 };
 
 export const checkMessageTrigger = async (subdomain, { target, config }) => {
+  debugInfo(
+    `Received message trigger data on ${subdomain} ${JSON.stringify({ target, config })}`
+  );
+
   const { conditions = [], botId } = config;
 
   if (target.botId !== botId) {
