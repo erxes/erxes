600a9f9ab1dcad8004a4f3a8d86a2058f1c809a0
soyombo <e11iot.soko@gmail.com>
2024-10-29 01:45:29 +0800

    Delete yaml.js

--- packages/plugin-knowledgebase-api/src/yaml.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    beforeRow: 'yarn build plugin knowledgebase',
-    additionalBuildSteps: [
-      'cd erxes/packages/plugin-knowledgebase-api',
-      'RUN apt-get update && apt-get install -y poppler-utils',
-    ],
-  };
-  
