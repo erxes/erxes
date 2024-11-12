692c7628902ffb498c6fcbf5999c1eb4dcb228e7
Ganbat <ganbayo53@gmail.com>
2024-11-12 11:48:52 +0800

    chore: exporterByUrl

--- packages/core/src/type.ts
+++ packages/core/src/type.ts
@@ -1,1 +1,15 @@
 export type Result = { [key: string]: string | number | undefined };
+export type BoardItemsFilter = {
+    _id?: { $in: string[] };
+};
+type CreatedInfo = {
+    _id: string;
+    type: string;
+    validation: string;
+    text: string;
+    value: string | Date;
+};
+export type MessageData = {
+    datas: any;
+    createdInfo: CreatedInfo;
+}
