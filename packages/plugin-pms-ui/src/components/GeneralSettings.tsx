1d8b78ecefd5c6a42dfda914aa6dfeb6ee4d21a1
orshih <orshih943@gmail.com>
2024-12-02 12:31:21 +0800

    fix:(pms) fix

--- packages/plugin-pms-ui/src/components/GeneralSettings.tsx
+++ packages/plugin-pms-ui/src/components/GeneralSettings.tsx
@@ -1,14 +1,14 @@
-import { MainStyleTitle as Title } from '@erxes/ui/src/styles/eindex';
-import { __ } from '@erxes/ui/src/utils';
-import { Button } from '@erxes/ui/src/components';
-import { Wrapper } from '@erxes/ui/src/layout';
-import React, { useState } from 'react';
+import { MainStyleTitle as Title } from "@erxes/ui/src/styles/eindex";
+import { __ } from "@erxes/ui/src/utils";
+import { Button } from "@erxes/ui/src/components";
+import { Wrapper } from "@erxes/ui/src/layout";
+import React, { useState } from "react";
 
-import { ContentBox } from '../styles';
-import { IConfigsMap } from '../types';
-import Header from './Header';
-import PerSettings from './GeneralPerSettings';
-import Sidebar from './Sidebar';
+import { ContentBox } from "../styles";
+import { IConfigsMap } from "../types";
+import Header from "./Header";
+import PerSettings from "./GeneralPerSettings";
+import Sidebar from "./SideBar";
 
 type Props = {
   save: (configsMap: IConfigsMap) => void;
@@ -22,7 +22,7 @@ type State = {
 const GeneralSettings = (props: Props) => {
   const [configsMap, setConfigsMap] = useState<IConfigsMap>(props.configsMap);
 
-  const add = e => {
+  const add = (e) => {
     e.preventDefault();
 
     if (!configsMap.erkhetConfig) {
@@ -31,43 +31,43 @@ const GeneralSettings = (props: Props) => {
 
     // must save prev item saved then new item
     const newBrandId = {
-      title: 'New Erkhet Config',
-      brandId: '',
-      apiKey: '',
-      apiSecret: '',
-      apiToken: '',
-      costAccount: '',
-      saleAccount: '',
-      productCategoryCode: '',
-      consumeDescription: '',
-      customerDefaultName: '',
-      customerCategoryCode: '',
-      companyCategoryCode: '',
-      debtAccounts: '',
-      userEmail: '',
-      defaultCustomer: '',
+      title: "New Pms Config",
+      brandId: "",
+      apiKey: "",
+      apiSecret: "",
+      apiToken: "",
+      costAccount: "",
+      saleAccount: "",
+      productCategoryCode: "",
+      consumeDescription: "",
+      customerDefaultName: "",
+      customerCategoryCode: "",
+      companyCategoryCode: "",
+      debtAccounts: "",
+      userEmail: "",
+      defaultCustomer: ""
     };
 
-    setConfigsMap(prevConfigsMap => ({
+    setConfigsMap((prevConfigsMap) => ({
       ...prevConfigsMap,
       erkhetConfig: {
         ...prevConfigsMap.erkhetConfig,
-        newBrandId,
-      },
+        newBrandId
+      }
     }));
   };
 
   const deleteHandler = (currentConfigKey: string) => {
     delete configsMap.erkhetConfig[currentConfigKey];
-    delete configsMap.erkhetConfig['newBrandId'];
+    delete configsMap.erkhetConfig["newBrandId"];
 
     setConfigsMap(configsMap);
 
     props.save(configsMap);
   };
 
-  const renderConfigs = configs => {
-    return Object.keys(configs).map(key => {
+  const renderConfigs = (configs) => {
+    return Object.keys(configs).map((key) => {
       return (
         <PerSettings
           key={key}
@@ -85,19 +85,19 @@ const GeneralSettings = (props: Props) => {
     const configs = configsMap.erkhetConfig || {};
 
     return (
-      <ContentBox id={'GeneralSettingsMenu'}>
+      <ContentBox id={"GeneralSettingsMenu"}>
         {renderConfigs(configs)}
       </ContentBox>
     );
   };
 
   const breadcrumb = [
-    { title: __('Settings'), link: '/settings' },
-    { title: __('Erkhet config') },
+    { title: __("Settings"), link: "/settings" },
+    { title: __("Pms config") }
   ];
 
   const actionButtons = (
-    <Button btnStyle='primary' onClick={add} icon='plus' uppercase={false}>
+    <Button btnStyle="primary" onClick={add} icon="plus" uppercase={false}>
       New config
     </Button>
   );
@@ -105,12 +105,12 @@ const GeneralSettings = (props: Props) => {
   return (
     <Wrapper
       header={
-        <Wrapper.Header title={__('Pms config')} breadcrumb={breadcrumb} />
+        <Wrapper.Header title={__("Pms config")} breadcrumb={breadcrumb} />
       }
       mainHead={<Header />}
       actionBar={
         <Wrapper.ActionBar
-          left={<Title>{__('Pms configs')}</Title>}
+          left={<Title>{__("Pms configs")}</Title>}
           right={actionButtons}
         />
       }
