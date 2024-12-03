6f771a132177958a6846d9c8b025319b339bf225
munkhsaikhan <munkhsaikhan.g@nmma.co>
2024-12-03 17:17:46 +0800

    pull polaris config saved

--- packages/plugin-syncpolaris-ui/src/settings/components/PullCustomerSettings.tsx
+++ packages/plugin-syncpolaris-ui/src/settings/components/PullCustomerSettings.tsx
@@ -5,20 +5,20 @@ import { Row, Title } from '@erxes/ui-settings/src/styles';
 import client from '@erxes/ui/src/apolloClient';
 import {
   Button,
-  CollapseContent,
   ControlLabel,
   FormControl,
   FormGroup,
-  Icon,
 } from '@erxes/ui/src/components';
+import ActionButtons from "@erxes/ui/src/components/ActionButtons";
 import { Wrapper } from '@erxes/ui/src/layout';
 import { FormColumn } from '@erxes/ui/src/styles/main';
 import { __ } from '@erxes/ui/src/utils';
 import React, { useEffect, useState } from 'react';
+import { SYNC_TYPES } from '../../constants';
 import { ContentBox } from '../../styles';
 import { IConfigsMap } from '../types';
+import PullCustomerExtra from './PullCustomerExtra';
 import Sidebar from './SideBar';
-import { SYNC_TYPES } from '../../constants';
 
 type Props = {
   save: (configsMap: IConfigsMap) => void;
@@ -46,126 +46,74 @@ const PullCustomerSettings = (props: Props) => {
 
   const save = (e) => {
     e.preventDefault();
-    props.save({ PULL_POLARIS: currentMap });
+    const tempMap: any[] = []
+    for (const item of currentMap) {
+      if (item.kind === 'prop') {
+        if (!item.extra) {
+          tempMap.push({ ...item, extra: {} })
+          continue
+        }
+
+        const extra = {}
+        for (const extraKey of Object.keys(item.extra)) {
+          const value = item.extra[extraKey];
+          if (value?.respKey) {
+            extra[value.respKey] = value
+          }
+        }
+        tempMap.push({ ...item, extra })
+      } else {
+        tempMap.push(item)
+      }
+    }
+    props.save({ PULL_POLARIS: tempMap });
   };
 
   const onAdd = () => {
-    setCurrentMap([...currentMap, { _id: Math.random().toString(), contentType: 'customer', kind: 'load' }])
+    setCurrentMap([...currentMap, { _id: Math.random().toString(), contentType: 'customer', kind: 'load', extra: {} }])
   }
 
   const onDelete = (id) => {
     setCurrentMap([...currentMap.filter(c => c._id !== id)])
   }
 
-  const renderInput = (key: string) => {
-    const onChangeInput = (value) => {
-      setCurrentMap({ ...currentMap, [key]: { ...currentMap[key] || {}, value } })
-    };
-
-    return (
-      <FormControl
-        value={currentMap[key]?.value}
-        onChange={(e) => onChangeInput((e.target as any).value)}
-      />
-    );
-  };
-
-  const renderFields = (key: string) => {
-    const setFieldGroup = (value) => {
-      setCurrentMap({ ...currentMap, [key]: { ...currentMap[key] || {}, groupId: value } })
-    }
-
-    const setFormField = (value) => {
-      const currentFields = ((fieldGroups || []).find(
-        (fg) => fg._id === currentMap[key]?.groupId
-      ) || {}).fields;
-      const field = currentFields?.find(cf => cf._id === value);
-      let propType: string | undefined = undefined
-
-      if (field?.isDefinedByErxes) {
-        propType = field.type;
-      }
-
-      setCurrentMap({ ...currentMap, [key]: { ...currentMap[key] || {}, fieldId: value, propType } })
-    }
-
-    return (
-      <>
-        <FormColumn maxwidth='30%'>
-          <FormControl
-            name="fieldGroup"
-            componentclass="select"
-            options={[
-              { value: "", label: "Empty" },
-              ...(fieldGroups || []).map((fg) => ({
-                value: fg._id,
-                label: `${fg.code} - ${fg.name}`,
-              })),
-            ]}
-            value={currentMap[key]?.groupId}
-            onChange={(e) => setFieldGroup((e.target as any).value)}
-          />
-        </FormColumn>
-        <FormColumn maxwidth='30%'>
-          <FormControl
-            name="formField"
-            componentclass="select"
-            options={[
-              { value: "", label: "Empty" },
-              ...(
-                (
-                  (
-                    (fieldGroups || []).find(
-                      (fg) => fg._id === currentMap[key]?.groupId
-                    ) || {}
-                  ).fields || []
-                ) || []
-              ).map((f) => ({
-                value: f._id,
-                label: `${f.code} - ${f.text}`,
-              })),
-            ]}
-            value={currentMap[key]?.fieldId}
-            onChange={(e) => setFormField((e.target as any).value)}
-          />
-        </FormColumn>
-      </>
-    );
-  };
-
-  const renderPerExtra = (curr) => {
-    if (curr.kind === 'prop') {
-      return <Row>
-        <FormColumn maxwidth='5%'></FormColumn>
-        <FormColumn maxwidth='25%'>
-          {renderInput('key')}
-        </FormColumn>
-        {renderFields('key')}
-        <FormColumn maxwidth='10%'>
-          {renderInput('key')}
-        </FormColumn>
-      </Row>
-    }
-
-    return <></>;
-  }
-
-  const renderItem = (curr) => {
+  const renderItem = (item) => {
     const setValue = (key, value) => {
       setCurrentMap([...currentMap.map(
-        c => c._id === curr._id && { ...curr, [key]: value } || { ...c }
+        c => c._id === item._id && { ...item, [key]: value } || { ...c }
       )]);
     }
 
+    const onAddExtra = (id) => {
+      setCurrentMap([...currentMap.map(
+        c => c._id === item._id && {
+          ...item,
+          extra: {
+            ...(item.extra || {}),
+            [Math.random().toString()]: {
+              respKey: '',
+              groupId: '',
+              fieldId: '',
+              propType: '',
+            }
+          }
+        } || { ...c }
+      )]);
+    }
+
+    const editExtra = (values) => {
+      setValue('extra', values)
+    }
+
     return (
-      <FormGroup key={curr._id}>
+      <FormGroup key={item._id}>
         <Row>
           <FormColumn maxwidth='20%'>
             <FormControl
               name="code"
               componentclass="select"
               options={SYNC_TYPES.map(st => ({ value: st.code, label: st.code }))}
-              value={curr.code}
+              value={item.code}
               onChange={(e) => setValue('code', (e.target as any).value)}
             />
           </FormColumn>
@@ -174,11 +122,11 @@ const PullCustomerSettings = (props: Props) => {
               name="title"
               componentclass="select"
               options={SYNC_TYPES.map(st => ({ value: st.code, label: st.title }))}
-              value={curr.code}
+              value={item.code}
               disabled={true}
             />
           </FormColumn>
-          <FormColumn maxwidth='30%'>
+          <FormColumn maxwidth='35%'>
             <FormControl
               name="kind"
               componentclass="select"
@@ -188,15 +136,18 @@ const PullCustomerSettings = (props: Props) => {
                 { value: 'prop', label: 'property save' },
                 { value: 'history', label: 'history save' },
               ]}
-              value={curr.kind}
+              value={item.kind}
               onChange={(e) => setValue('kind', (e.target as any).value)}
             />
           </FormColumn>
-          <FormColumn maxwidth='10%'>
-            <Button btnStyle="link" icon="times-circle" onClick={onDelete.bind(this, curr._id)} />
+          <FormColumn maxwidth='5%'>
+            <ActionButtons>
+              <Button btnStyle="link" icon="times-circle" onClick={onDelete.bind(this, item._id)} />
+              {item.kind === 'prop' && <Button btnStyle="link" icon="add" onClick={onAddExtra.bind(this, item._id)} />}
+            </ActionButtons>
           </FormColumn>
         </Row>
-        {renderPerExtra(curr)}
+        <PullCustomerExtra fieldGroups={fieldGroups} item={{ ...item, extra: item.extra || {} }} editExtra={editExtra} />
       </FormGroup >
     );
   }
@@ -204,16 +155,25 @@ const PullCustomerSettings = (props: Props) => {
   const renderContent = () => {
     return (
       <ContentBox id={'PolarisCustomerSettingsMenu'}>
-        <CollapseContent
-          title="Customer data config"
-          beforeTitle={<Icon icon="settings" />}
-          transparent={true}
-          open={true}
-        >
-          {
-            currentMap.map(curr => renderItem(curr))
-          }
-        </CollapseContent>
+        <FormGroup>
+          <Row>
+            <FormColumn maxwidth='20%'>
+              <ControlLabel>Pull code</ControlLabel>
+            </FormColumn>
+            <FormColumn maxwidth='40%'>
+              <ControlLabel>Pull title</ControlLabel>
+            </FormColumn>
+            <FormColumn maxwidth='35%'>
+              <ControlLabel>Pull type</ControlLabel>
+            </FormColumn>
+            <FormColumn maxwidth='5%'>
+              <ControlLabel>Actions</ControlLabel>
+            </FormColumn>
+          </Row>
+        </FormGroup >
+        {
+          currentMap.map(item => renderItem(item))
+        }
       </ContentBox>
     );
   };
