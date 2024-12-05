6bbe8412db107776ad256a2379a648ec3d5f60c8
munkhsaikhan <munkhsaikhan.g@nmma.co>
2024-12-05 16:04:05 +0800

    fix: sales settings stage row

--- packages/plugin-sales-ui/src/settings/boards/components/StageItem.tsx
+++ packages/plugin-sales-ui/src/settings/boards/components/StageItem.tsx
@@ -8,7 +8,7 @@ import { PROBABILITY } from "../constants";
 import React from "react";
 import Select from "react-select";
 import SelectTeamMembers from "@erxes/ui/src/team/containers/SelectTeamMembers";
-import { StageItemContainer } from "@erxes/ui-sales/src/settings/boards/styles";
+import { StageItemContainer, StageItemRow } from "@erxes/ui-sales/src/settings/boards/styles";
 
 type Props = {
   stage: IStage;
@@ -25,14 +25,14 @@ class StageItem extends React.Component<Props> {
     const { _id, visibility, memberIds, departmentIds } = stage;
 
     if (!visibility || visibility === "public") {
-      return;
+      return <><div></div><div></div></>;
     }
 
     const generateValue = () => {
       const selected = this.props.departments.filter(
-        department => departmentIds.includes(department._id) || []
+        (department) => (departmentIds || []).includes(department._id)
       );
-      return selected.map(s => ({ value: s._id, label: s.title }));
+      return selected.map((s) => ({ value: s._id, label: s.title }));
     };
 
     return (
@@ -41,7 +41,7 @@ class StageItem extends React.Component<Props> {
           label={__("Members")}
           name="memberIds"
           initialValue={memberIds}
-          onSelect={ids => onChange(_id, "memberIds", ids)}
+          onSelect={(ids) => onChange(_id, "memberIds", ids)}
         />
         <Select
           value={generateValue()}
@@ -50,14 +50,14 @@ class StageItem extends React.Component<Props> {
             null,
             (node, level) => ({
               value: node._id,
-              label: `${"---".repeat(level)} ${node.title}`
+              label: `${"---".repeat(level)} ${node.title}`,
             })
           )}
-          onChange={options =>
+          onChange={(options) =>
             onChange(
               _id,
               "departmentIds",
-              (options || []).map(o => o.value)
+              (options || []).map((o) => o.value)
             )
           }
           placeholder={__("Department ...")}
@@ -80,74 +80,74 @@ class StageItem extends React.Component<Props> {
     };
 
     return (
-      <StageItemContainer key={stage._id}>
-        <FormControl
-          defaultValue={stage.name}
-          type="text"
-          placeholder={__("Stage name")}
-          onKeyPress={onKeyPress}
-          autoFocus={true}
-          name="name"
-          onChange={onChangeFormControl.bind(this, stage._id)}
-        />
+      <StageItemContainer>
+        <StageItemRow key={stage._id}>
+          <FormControl
+            defaultValue={stage.name}
+            type="text"
+            placeholder={__("Stage name")}
+            onKeyPress={onKeyPress}
+            autoFocus={true}
+            name="name"
+            onChange={onChangeFormControl.bind(this, stage._id)}
+          />
 
-        <FormControl
-          defaultValue={stage.probability}
-          componentclass="select"
-          name="probability"
-          onChange={onChangeFormControl.bind(this, stage._id)}
-        >
-          {probabilties.map((p, index) => (
-            <option key={index} value={p}>
-              {p}
+          <FormControl
+            defaultValue={stage.probability}
+            componentclass="select"
+            name="probability"
+            onChange={onChangeFormControl.bind(this, stage._id)}
+          >
+            {probabilties.map((p, index) => (
+              <option key={index} value={p}>
+                {p}
+              </option>
+            ))}
+          </FormControl>
+
+          <FormControl
+            defaultValue={stage.status}
+            componentclass="select"
+            name="status"
+            className={""}
+            onChange={onChangeFormControl.bind(this, stage._id)}
+          >
+            <option key="active" value="active">
+              {__("Active")}
             </option>
-          ))}
-        </FormControl>
+            <option key="archived" value="archived">
+              {__("Archived")}
+            </option>
+          </FormControl>
 
-        <FormControl
-          defaultValue={stage.status}
-          componentclass="select"
-          name="status"
-          className={""}
-          onChange={onChangeFormControl.bind(this, stage._id)}
-        >
-          <option key="active" value="active">
-            {__("Active")}
-          </option>
-          <option key="archived" value="archived">
-            {__("Archived")}
-          </option>
-        </FormControl>
+          <FormControl
+            defaultValue={stage.visibility}
+            componentclass="select"
+            name="visibility"
+            onChange={onChangeFormControl.bind(this, stage._id)}
+          >
+            <option key={0} value="public">
+              {__("Public")}
+            </option>
+            <option key={1} value="private">
+              {__("Private")}
+            </option>
+          </FormControl>
 
-        <FormControl
-          defaultValue={stage.visibility}
-          componentclass="select"
-          name="visibility"
-          onChange={onChangeFormControl.bind(this, stage._id)}
-        >
-          <option key={0} value="public">
-            {__("Public")}
-          </option>
-          <option key={1} value="private">
-            {__("Private")}
-          </option>
-        </FormControl>
+          <FormControl
+            defaultValue={stage.code}
+            name="code"
+            placeholder={__("Code")}
+            onChange={onChangeFormControl.bind(this, stage._id)}
+          />
 
-        <FormControl
-          defaultValue={stage.code}
-          name="code"
-          placeholder={__("Code")}
-          onChange={onChangeFormControl.bind(this, stage._id)}
-        />
+          <FormControl
+            defaultValue={stage.age}
+            name="age"
+            placeholder={__("Age")}
+            onChange={onChangeFormControl.bind(this, stage._id)}
+          />
 
-        <FormControl
-          defaultValue={stage.age}
-          name="age"
-          placeholder={__("Age")}
-          onChange={onChangeFormControl.bind(this, stage._id)}
-        />
-
-        {(["deal"].includes(type) && (
           <FormControl
             componentclass="checkbox"
             checked={
@@ -160,30 +160,32 @@ class StageItem extends React.Component<Props> {
             autoFocus={true}
             onChange={onChangeCheckbox.bind(this, stage._id)}
           />
-        )) || <></>}
 
-        {this.renderSelectMembers()}
+          <Button
+            btnStyle="link"
+            size="small"
+            onClick={remove.bind(this, stage._id)}
+            icon="times"
+          />
+        </StageItemRow>
+        <StageItemRow>
+          {this.renderSelectMembers()}
 
-        <SelectTeamMembers
-          label={__("Can move members")}
-          name="canMoveMemberIds"
-          initialValue={stage.canMoveMemberIds}
-          onSelect={ids => onChange(stage._id, "canMoveMemberIds", ids)}
-        />
+          <SelectTeamMembers
+            label="Can move members"
+            name="canMoveMemberIds"
+            initialValue={stage.canMoveMemberIds}
+            onSelect={(ids) => onChange(stage._id, "canMoveMemberIds", ids)}
+          />
 
-        <SelectTeamMembers
-          label={__("Can edit members")}
-          name="canEditMemberIds"
-          initialValue={stage.canEditMemberIds}
-          onSelect={ids => onChange(stage._id, "canEditMemberIds", ids)}
-        />
+          <SelectTeamMembers
+            label="Can edit members"
+            name="canEditMemberIds"
+            initialValue={stage.canEditMemberIds}
+            onSelect={(ids) => onChange(stage._id, "canEditMemberIds", ids)}
+          />
 
-        <Button
-          btnStyle="link"
-          size="small"
-          onClick={remove.bind(this, stage._id)}
-          icon="times"
-        />
+        </StageItemRow>
       </StageItemContainer>
     );
   }
