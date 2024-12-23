d90c03a6c4cd04fcf0e81cc1ee6077319a110303
gerelsukh <gerelsukh0705@gmail.com>
2024-12-23 16:35:20 +0800

    feat: unused resolver remove

--- packages/plugin-activedirectory-api/src/graphql/typeDefs.ts
+++ packages/plugin-activedirectory-api/src/graphql/typeDefs.ts
@@ -1,13 +1,9 @@
 import gql from 'graphql-tag';
 
-import { types as adTypes } from './schema/active';
-
 const typeDefs = async () => {
   return gql`
     scalar JSON
     scalar Date
-
-    ${adTypes()}
   `;
 };
 
