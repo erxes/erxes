7ebf1432b9a96a3d52e0e0608b36158056eb43a5
Tuguldur Ch <tuguldur.ch@nmma.co>
2025-01-29 15:50:19 +0800

    update

--- packages/api-utils/src/instrumentation.ts
+++ packages/api-utils/src/instrumentation.ts
@@ -1,15 +1,23 @@
-import * as opentelemetry from '@opentelemetry/sdk-node';
-import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
+import { NodeSDK } from '@opentelemetry/sdk-node';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-grpc';
+import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
 import { diag, DiagConsoleLogger, DiagLogLevel } from '@opentelemetry/api';
 
+diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.ERROR);
 
-diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
+const traceExporter = new OTLPTraceExporter({
+  url: 'http://localhost:4318/v1/traces'
+});
 
-const sdk = new opentelemetry.NodeSDK({
-  traceExporter: new OTLPTraceExporter({}),
-
+const sdk = new NodeSDK({
+  traceExporter,
   instrumentations: [getNodeAutoInstrumentations()]
 });
 
-sdk.start();
+sdk
+  .start()
+
+process.on('SIGTERM', async () => {
+  await sdk.shutdown();
+  console.log('Tracing terminated');
+});
