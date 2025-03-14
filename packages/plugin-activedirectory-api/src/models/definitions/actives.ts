b123d8e2111add7f0aa6744ccc47d16431af5f35
gerelsukh <gerelsukh0705@gmail.com>
2025-03-14 15:36:34 +0800

    refactor: change control panel name distinguished

--- packages/plugin-activedirectory-api/src/models/definitions/actives.ts
+++ packages/plugin-activedirectory-api/src/models/definitions/actives.ts
@@ -31,7 +31,7 @@ export const configSchema = schemaWrapper(
       label: 'Modified at',
     }),
     apiUrl: field({ type: String, optional: true, label: 'apiUrl' }),
-    useDN: field({ type: Boolean, label: 'use Disquinshed Name' }),
+    useDN: field({ type: Boolean, label: 'use Distinguished Name' }),
     baseDN: field({ type: String, optional: true, label: 'baseDN' }),
     adminDN: field({ type: String, optional: true, label: 'adminDN' }),
     adminPassword: field({
