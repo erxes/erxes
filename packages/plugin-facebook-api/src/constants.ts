912e488163c791b0eba6acd13601814057dfc7a3
Wlkr7575 <tuguldurch737@gmail.com>
2025-04-04 21:35:52 +0800

    fix: can't subscribe fields in facebook

--- packages/plugin-facebook-api/src/constants.ts
+++ packages/plugin-facebook-api/src/constants.ts
@@ -20,7 +20,7 @@ export const INTEGRATION_KINDS = {
 };
 
 
-export const BOT_SUBSCRIcBE_FIELDS=  [
+export const BOT_SUBSCRIBE_FIELDS=  [
   "messages",
   "messaging_postbacks",
   "messaging_referrals"
