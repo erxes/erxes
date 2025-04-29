144855ed2b4a4d752cecf4134d3bbf340f0d5679
Wlkr7575 <tuguldurch737@gmail.com>
2025-04-30 00:21:42 +0800

    debug

--- packages/core/src/commands/syncSavingsPolaris.js
+++ packages/core/src/commands/syncSavingsPolaris.js
@@ -160,8 +160,7 @@ const command = async () => {
               createdAt: new Date(detailDeposit.createdDate),
             };
 
-            // @ts-ignore
-            await SavingContracts.insertOne({ ...document });
+            // await SavingContracts.insertOne({ ...document });
           }
         }
       }
