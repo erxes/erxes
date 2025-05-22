968ee87abb25c00d0b2d1e4b5a11b2cbe5b05b34
Buyantogtokh Sambuu <sjbuyanaa@gmail.com>
2025-05-22 14:40:17 +0800

    add duplicated ids check in ProductRule doc

--- packages/core/src/db/models/ProductRules.ts
+++ packages/core/src/db/models/ProductRules.ts
@@ -1,4 +1,5 @@
 import { Model } from 'mongoose';
+import * as _ from 'lodash';
 
 import { IProductRule, IProductRuleDocument, productRuleSchema } from "./definitions/productRules";
 import { IModels } from '../../connectionResolver';
@@ -9,6 +10,31 @@ export interface IProductRuleModel extends Model<IProductRuleDocument> {
   updateRule(_id: string, doc: IProductRule): Promise<IProductRuleDocument>;
   removeRule(_id: string): void;
 };
+
+
+// clean duplicated values in both fields
+const cleanDuplicates = (inc: string[] = [], exc: string[] = []) => {
+  return {
+    includes: _.difference(inc, exc),
+    excludes: _.difference(exc, inc)
+  };
+}
+
+const prepareDoc = (doc: IProductRule): IProductRule => {
+  const categories = cleanDuplicates(doc.categoryIds, doc.excludeCategoryIds);
+  const products = cleanDuplicates(doc.productIds, doc.excludeProductIds);
+  const tags = cleanDuplicates(doc.tagIds, doc.excludeTagIds);
+
+  return {
+    ...doc,
+    categoryIds: categories.includes,
+    excludeCategoryIds: categories.excludes,
+    productIds: products.includes,
+    excludeProductIds: products.excludes,
+    tagIds: tags.includes,
+    excludeTagIds: tags.excludes  
+  };
+}
 
 export const loadProductRuleClass = (models: IModels, _subdomain: string) => {
   class ProductRule {
@@ -23,13 +49,15 @@ export const loadProductRuleClass = (models: IModels, _subdomain: string) => {
     }
 
     public static async createRule(doc: IProductRule) {
-      return models.ProductRules.create(doc);
+      const preparedDoc = prepareDoc(doc);
+      return models.ProductRules.create(preparedDoc);
     }
 
     public static async updateRule(_id: string, doc: IProductRule) {
       const rule = await models.ProductRules.getRule(_id);
+      const preparedDoc = prepareDoc(doc);
 
-      await models.ProductRules.updateOne({ _id: rule._id }, { $set: doc });
+      await models.ProductRules.updateOne({ _id: rule._id }, { $set: preparedDoc });
 
       return models.ProductRules.findOne({ _id });
     }
