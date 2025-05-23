617938bde7a8585005f1ceb4ab9bb04a6719a6f7
Buyantogtokh Sambuu <sjbuyanaa@gmail.com>
2025-05-23 18:17:15 +0800

    write system logs when create/update/delete agents

--- packages/plugin-loyalties-api/src/models/definitions/agents.ts
+++ packages/plugin-loyalties-api/src/models/definitions/agents.ts
@@ -32,27 +32,28 @@ export interface IAgentDocument extends Document, IAgent {
   _id: string;
 }
 
+// With label fields defined, the label will be shown in system logs
 export const agentSchema = new Schema({
   _id: field({ pkey: true }),
-  number: field({ type: String, unique: true, required: true }),
-  customerIds: field({ type: [String] }),
-  companyIds: field({ type: [String] }),
-  status: field({ type: String, enum: AGENT_STATUSES.ALL, default: AGENT_STATUSES.DRAFT }),
-  startDate: field({ type: Date }),
-  endDate: field({ type: Date }),
-  startMonth: field({ type: Date }),
-  endMonth: field({ type: Date }),
-  startDay: field({ type: Date }),
-  endDay: field({ type: Date }),
+  number: field({ type: String, unique: true, required: true, label: 'Number' }),
+  customerIds: field({ type: [String], label: 'Customer ids' }),
+  companyIds: field({ type: [String], label: 'Company ids' }),
+  status: field({ type: String, enum: AGENT_STATUSES.ALL, default: AGENT_STATUSES.DRAFT, label: 'Status' }),
+  startDate: field({ type: Date, label: 'Start date' }),
+  endDate: field({ type: Date, label: 'Ending date' }),
+  startMonth: field({ type: Date, label: 'Starting month' }),
+  endMonth: field({ type: Date, label: 'Ending month' }),
+  startDay: field({ type: Date, label: 'Starting day' }),
+  endDay: field({ type: Date, label: 'Ending day' }),
   hasReturn: field({ type: Boolean, label: 'Whether agent returns money or not' }),
   productRuleIds: field({ type: [String], label: 'Product specific rules' }),
   // if hasReturn === true
-  returnAmount: field({ type: Number, min: 0 }),
+  returnAmount: field({ type: Number, min: 0, label: 'Return amount' }),
   // returnAmount will override returnPercent
-  returnPercent: field({ type: Number, min: 0, max: 100 }),
+  returnPercent: field({ type: Number, min: 0, max: 100, label: 'Return percent' }),
   // if hasReturn === false
-  prepaidPercent: field({ type: Number, min: 0, max: 100 }),
-  discountPercent: field({ type: Number, min: 0, max: 100 }),
+  prepaidPercent: field({ type: Number, min: 0, max: 100, label: 'Prepaid percent' }),
+  discountPercent: field({ type: Number, min: 0, max: 100, label: 'Discount percent' }),
 });
 
 // TODO: add indexes on other fields later depending on usage
