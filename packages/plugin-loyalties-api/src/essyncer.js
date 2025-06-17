3a301a488d99d49b4b6c2b147adf312a7d4106e3
Wlkr7575 <tuguldurch737@gmail.com>
2025-06-18 00:17:00 +0800

    update(loyalties):add ability for customer segmentation by voucher campaign

---
+++ packages/plugin-loyalties-api/src/essyncer.js
@@ -0,0 +1,8 @@
+module.exports = [
+  {
+    name: "vouchers",
+    schema:
+      "{'userId': { 'type': 'keyword' },'ownerType': { 'type': 'keyword' },'ownerId': { 'type': 'keyword' },'campaignId': { 'type': 'keyword' }}",
+    script: ""
+  }
+];
