261525c26abe2be2aa2d5af77370edce3f66856c
Gerelsukh <gerelsukh0705@gmail.com>
2025-06-30 14:20:39 +0800

    feat: close loan integration add

--- packages/plugin-syncpolaris-api/src/utils/loan/closeLoan.ts
+++ packages/plugin-syncpolaris-api/src/utils/loan/closeLoan.ts
@@ -1,4 +1,9 @@
-import { fetchPolaris, getDepositAccount, getContract } from '../utils';
+import {
+  fetchPolaris,
+  getDepositAccount,
+  getContract,
+  getCustomer
+} from "../utils";
 
 export const createLoanClose = async (
   subdomain,
@@ -8,7 +13,7 @@ export const createLoanClose = async (
   const loanContract = await getContract(
     subdomain,
     { _id: transaction.contractId },
-    'loans'
+    "loans"
   );
 
   const depositAccount = await getDepositAccount(
@@ -16,45 +21,63 @@ export const createLoanClose = async (
     loanContract.customerId
   );
 
+  const customer = await getCustomer(subdomain, loanContract.customerId);
+
+  const getAccounts = await fetchPolaris({
+    op: "13610312",
+    data: [customer?.code, 0, 20],
+    subdomain,
+    polarisConfig
+  });
+
+  const customerAccount = getAccounts.filter(
+    (account) => account.acntType === "CA"
+  );
+
+  const polarisNumber =
+    customerAccount && customerAccount.length > 0
+      ? customerAccount[0].acntCode
+      : "";
+
   const loanClose = {
     txnAcntCode: loanContract.number,
     txnAmount: transaction.total,
     curCode: transaction.currency,
     rate: 1,
-    rateTypeId: '4',
-    contAcntCode: depositAccount.number,
+    rateTypeId: "4",
+    contAcntCode: depositAccount?.number ?? polarisNumber ?? "",
     contAmount: transaction.total,
     contRate: 1,
     contCurCode: transaction.currency,
-    txnDesc: transaction.description,
-    sourceType: 'OI',
+    txnDesc: transaction?.description ?? "loan closing",
+    sourceType: "OI",
     isPreview: 0,
     isPreviewFee: null,
     isTmw: 1,
     aspParam: [
       [
         {
-          acntCode: depositAccount.transAccount,
-          acntType: 'INCOME'
+          acntCode: depositAccount?.number ?? polarisNumber,
+          acntType: "INCOME"
         },
         {
           acntCode: loanContract.number,
-          acntType: 'EXPENSE'
+          acntType: "EXPENSE"
         }
       ],
       13600107
     ],
     addParams: [
       {
-        contAcntType: 'CASA',
-        chkAcntInt: 'N'
+        contAcntType: "CASA",
+        chkAcntInt: "N"
       }
     ]
   };
 
   const loanGiveReponse = await fetchPolaris({
     subdomain,
-    op: '13610267',
+    op: "13610267",
     data: [loanClose],
     polarisConfig
   });
