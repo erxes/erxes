4322ff42c91401f238671e435a4563b085d34904
sekulya <secheikhen1999@gmail.com>
2025-08-04 16:59:38 +0800

    4319 tickets export TagsIds without models/tags

--- packages/plugin-tickets-api/src/tags.ts
+++ packages/plugin-tickets-api/src/tags.ts
@@ -1,11 +1,51 @@
-import { generateModels } from "./connectionResolver";
-import { sendCommonMessage } from "./messageBroker";
+import { generateModels } from './connectionResolver';
+import { sendCommonMessage } from './messageBroker';
+import _ from 'lodash';
+
+const setRelatedIds = async (models, tag) => {
+  if (tag.parentId) {
+    const parentTag = await models.Tags.findOne({ _id: tag.parentId });
+
+    if (parentTag) {
+      let relatedIds: string[] = tag.relatedIds || [];
+      relatedIds.push(tag._id);
+      relatedIds = _.union(relatedIds, parentTag.relatedIds || []);
+
+      await models.Tags.updateOne({ _id: parentTag._id }, { $set: { relatedIds } });
+
+      const updated = await models.Tags.findOne({ _id: tag.parentId });
+      if (updated) {
+        await setRelatedIds(models, updated);
+      }
+    }
+  }
+};
+
+const removeRelatedIds = async (models, tag) => {
+  const tags = await models.Tags.find({ relatedIds: { $in: [tag._id] } });
+  if (tags.length === 0) return;
+
+  const relatedIds: string[] = tag.relatedIds || [];
+  relatedIds.push(tag._id);
+
+  const doc = tags.map(t => {
+    const ids = (t.relatedIds || []).filter(id => !relatedIds.includes(id));
+    return {
+      updateOne: {
+        filter: { _id: t._id },
+        update: { $set: { relatedIds: ids } }
+      }
+    };
+  });
+
+  await models.Tags.bulkWrite(doc);
+};
 
 export default {
   types: [
     {
-      description: "Tickets",
-      type: "ticket"
+      description: 'Tickets',
+      type: 'ticket'
     }
   ],
 
@@ -13,28 +53,36 @@ export default {
     const { type, targetIds, tagIds, _ids, action } = data;
 
     const models = await generateModels(subdomain);
-
-    let response = {};
     const model: any = models.Tickets;
 
-    if (action === "count") {
+    let response = {};
+
+    if (action === 'count') {
       response = await model.countDocuments({ tagIds: { $in: _ids } });
     }
 
-    if (action === "tagObject") {
+    if (action === 'tagObject') {
       await model.updateMany(
         { _id: { $in: targetIds } },
         { $set: { tagIds } },
         { multi: true }
       );
 
+      // Set related tagIds for each tag
+      const tags = await models.Tags.find({ _id: { $in: tagIds } });
+
+      for (const tag of tags) {
+        await setRelatedIds(models, tag);
+      }
+
       response = await model.find({ _id: { $in: targetIds } }).lean();
+
       sendCommonMessage({
-        serviceName: "automations",
+        serviceName: 'automations',
         subdomain,
-        action: "trigger",
+        action: 'trigger',
         data: {
-          type: "tickets:ticket",
+          type: 'tickets:ticket',
           targets: [response]
         },
         isRPC: true,
@@ -45,31 +93,40 @@ export default {
     return response;
   },
 
-  fixRelatedItems: async ({
-    subdomain,
-    data: { sourceId, destId, action }
-  }) => {
+  fixRelatedItems: async ({ subdomain, data: { sourceId, destId, action } }) => {
     const models = await generateModels(subdomain);
     const model: any = models.Tickets;
 
-    if (action === "remove") {
+    if (action === 'remove') {
       await model.updateMany(
         { tagIds: { $in: [sourceId] } },
         { $pull: { tagIds: { $in: [sourceId] } } }
       );
+
+      const tag = await models.Tags.findOne({ _id: sourceId });
+      if (tag) {
+        await removeRelatedIds(models, tag);
+      }
     }
 
-    if (action === "merge") {
+    if (action === 'merge') {
       const itemIds = await model
         .find({ tagIds: { $in: [sourceId] } }, { _id: 1 })
-        .distinct("_id");
+        .distinct('_id');
 
-      // add to the new destination
       await model.updateMany(
         { _id: { $in: itemIds } },
-        { $set: { "tagIds.$[elem]": destId } },
+        { $set: { 'tagIds.$[elem]': destId } },
         { arrayFilters: [{ elem: { $eq: sourceId } }] }
       );
+
+      const sourceTag = await models.Tags.findOne({ _id: sourceId });
+      const destTag = await models.Tags.findOne({ _id: destId });
+
+      if (sourceTag && destTag) {
+        await removeRelatedIds(models, sourceTag);
+        await setRelatedIds(models, destTag);
+      }
     }
   }
 };
