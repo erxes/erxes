ed4f81324ca35cb0d9932d8bc99bb8330a422adc
Wlkr7575 <tuguldurch737@gmail.com>
2025-08-27 13:31:31 +0800

    refactor(clientPortalNotifications): standardize string quotes and clean up schema definition

--- packages/plugin-clientportal-api/src/models/definitions/clientPortalNotifications.ts
+++ packages/plugin-clientportal-api/src/models/definitions/clientPortalNotifications.ts
@@ -1,12 +1,12 @@
-import { Document, Schema } from "mongoose";
-import { field } from "./utils";
+import { Document, Schema } from 'mongoose';
+import { field } from './utils';
 
 export interface ICPNotification {
   title?: string;
   content?: string;
   link?: string;
   receiver?: string;
-  notifType?: "engage" | "system";
+  notifType?: 'engage' | 'system';
   clientPortalId: string;
   eventData?: any | null;
   groupId?: string;
@@ -32,33 +32,33 @@ export const cpNotificationSchema = new Schema({
   createdAt: field({
     type: Date,
     default: Date.now,
-    expires: 60 * 60 * 24 * 30, // 30 days
+    expires: 60 * 60 * 24 * 30 // 30 days
   }),
   isRead: field({
     type: Boolean,
-    default: false,
+    default: false
   }),
   notifType: field({
-    type: String,
+    type: String
   }),
   clientPortalId: field({
     type: String,
-    index: true,
+    index: true
   }),
   eventData: field({
     type: Schema.Types.Mixed,
-    optional: true,
+    optional: true
   }),
   groupId: field({
     type: String,
-    optional: true,
+    optional: true
   }),
   type: field({
-    type: String,
-  }),
+    type: String
+  })
 });
 
-cpNotificationSchema.index(
-  { createdAt: 1, receiver: 1, clientPortalId: 1 },
-  { expireAfterSeconds: 60 * 60 * 24 * 30 }
-);
+// cpNotificationSchema.index(
+//   { createdAt: 1, receiver: 1, clientPortalId: 1 },
+//   { expireAfterSeconds: 60 * 60 * 24 * 30 }
+// );
