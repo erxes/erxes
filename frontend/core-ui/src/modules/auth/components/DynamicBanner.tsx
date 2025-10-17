997f222a3339a943e2218b6baa4ff11e57b1190a
KhBaterdene <95618276+KhBaterdene@users.noreply.github.com>
2025-10-17 15:26:12 +0800

    feat: improve dark mode

--- frontend/core-ui/src/modules/auth/components/DynamicBanner.tsx
+++ frontend/core-ui/src/modules/auth/components/DynamicBanner.tsx
@@ -2,14 +2,14 @@ import { Polygons } from '../components/Polygons';
 import { Logo } from '@/auth/components/Logo';
 export const DynamicBanner = () => {
   return (
-    <div className="hidden lg:block lg:w-1/2 h-screen relative bg-foreground overflow-hidden z-10">
+    <div className="hidden lg:block lg:w-1/2 h-dvh relative overflow-hidden z-10 bg-foreground dark:bg-background">
       <Polygons />
       <div className="w-full h-full flex flex-col items-center justify-center text-primary-foreground">
-        <div className="absolute top-7 ">
+        <div className="absolute top-7">
           <Logo />
         </div>
         <div className="max-w-[500px] flex-col flex justify-center gap-2">
-          <h1 className="text-2xl font-semibold leading-7 text-background">
+          <h1 className="text-2xl font-semibold leading-7 text-background dark:text-foreground">
             Grow your business better and faster
           </h1>
           <p className="text-lg font-medium leading-6 text-muted-foreground">
