9a6e82113eaaefc74bf20c7ba4ca1b3cd5a3940d
batmnkh2344 <batmnkh2344@gmail.com>
2025-10-21 20:12:41 +0800

    chore: roles in gateway

--- backend/core-api/src/modules/permissions/db/definitions/roles.ts
+++ backend/core-api/src/modules/permissions/db/definitions/roles.ts
@@ -1,24 +1,3 @@
-import { Schema } from 'mongoose';
-import { PERMISSION_ROLES } from '~/modules/permissions/db/constants';
+import { roleSchema } from 'erxes-api-shared/core-modules';
 
-export const roleSchema = new Schema(
-  {
-    userId: { 
-      type: String,
-      label: 'User',
-      index: true,
-      unique: true,
-      required: true,
-    },
-    role: {
-      type: String,
-      enum: PERMISSION_ROLES.ALL,
-      label: 'Role',
-      index: true,
-      required: true,
-    },
-  },
-  {
-    timestamps: true,
-  },
-);
+export { roleSchema };
