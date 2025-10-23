1662d2b3cf21e292d8629a312e6e1fe1196655c5
Wlkr7575 <tuguldurch737@gmail.com>
2025-10-23 12:02:10 +0800

    refactor: simplify customAttributions mapping in SetProperty component for better readability

--- packages/plugin-automations-ui/src/components/forms/actions/subForms/SetProperty.tsx
+++ packages/plugin-automations-ui/src/components/forms/actions/subForms/SetProperty.tsx
@@ -272,13 +272,13 @@ class SetProperty extends React.Component<Props, State> {
             attrWithSegmentConfig={true}
             additionalContent={additionalContent()}
             componentClass={rule.isExpression ? "textarea" : "input"}
-            customAttributions={!triggerType.includes(config?.module)?fields.map((f) => {
-              const moduleName:string =config.module.replace(":", ".").split(".")[1]
+            customAttributions={!triggerType.includes(config?.module)?fields.map(({label ='', name ='', type ='', validation =''}: FieldsCombinedByType) => {
+              const moduleName:string =config.module.replace(":", ".").split(".")[1] || ''
               return {
                 _id: String(Math.random()),
-                label: `${capitalizeFirstLetter(moduleName)} - ${f.label}`,
-                name: `${moduleName}.${f.name}`,
-                type: ['select','input'].includes(f.type) ? capitalizeFirstLetter(f.validation) : f.type,
+                label: `${capitalizeFirstLetter(moduleName)} - ${label}`,
+                name: `${moduleName}.${name}`,
+                type: ['select','input'].includes(type) ? capitalizeFirstLetter(validation || '') : type,
                 group:config.module
               }
             }):[]}
