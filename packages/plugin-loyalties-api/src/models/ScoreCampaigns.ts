2d75db9a9235bcf08053af2d91051a9e84b4af2b
Wlkr7575 <tuguldurch737@gmail.com>
2025-10-24 16:18:56 +0800

    debug: add additional console logs for score calculations and updated custom fields in ScoreCampaigns

--- packages/plugin-loyalties-api/src/models/ScoreCampaigns.ts
+++ packages/plugin-loyalties-api/src/models/ScoreCampaigns.ts
@@ -355,12 +355,15 @@ export const loadScoreCampaignClass = (models: IModels, subdomain: string) => {
 
       let oldScore = score;
 
+      console.log({ customFieldsData,score ,changeScore });
       if (campaign.fieldId) {
         const fieldScore =
           customFieldsData.find(({ field }) => field === campaign.fieldId)
             ?.value || 0;
         oldScore = fieldScore;
       }
+
+      console.log({ oldScore });
 
       const newScore =
         actionMethod === "subtract"
@@ -399,12 +402,14 @@ export const loadScoreCampaignClass = (models: IModels, subdomain: string) => {
         );
       }
 
+      console.log({ updatedCustomFieldsData, });
+
       await this.updateOwnerScore({
         ownerId,
         ownerType,
         updatedCustomFieldsData,
       });
-
+console.log("score log");
       return await models.ScoreLogs.create({
         ownerId,
         ownerType,
