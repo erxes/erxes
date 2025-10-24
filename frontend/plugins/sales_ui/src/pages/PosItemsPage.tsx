847647f550d305b2b661eaf1ae210792152df7d2
huslen <huslens1027@gmail.com>
2025-10-24 13:30:12 +0800

    a

--- frontend/plugins/sales_ui/src/pages/PosItemsPage.tsx
+++ frontend/plugins/sales_ui/src/pages/PosItemsPage.tsx
@@ -1,7 +1,6 @@
 import { PageHeader } from 'ui-modules';
 import { Breadcrumb, Separator } from 'erxes-ui';
 import { useParams } from 'react-router-dom';
-import { PosFilter } from '~/modules/pos/pos/PosFilter';
 import { PosBreadCrumb } from '~/modules/pos/pos/breadcumb/PosBreadCumb';
 import { PosItemsRecordTable } from '~/modules/pos/pos-items/components/PosItemsRecordTable';
 
@@ -24,9 +23,7 @@ export const PosItemsPage = () => {
           </Breadcrumb>
         </PageHeader.Start>
       </PageHeader>
-      {/* <PageHeader>
-        <PosFilter />
-      </PageHeader> */}
+
       <PosItemsRecordTable posId={posId} />
     </>
   );
