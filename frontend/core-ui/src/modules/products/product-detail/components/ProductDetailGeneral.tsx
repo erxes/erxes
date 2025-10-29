66c2a231cdffd0a510c25e876bcbb5a742d0c5e2
Khe <baterdne.k@gmail.com>
2025-10-29 19:04:24 +0800

    feat: enhance product detail component and update category selection
    - Updated ProductDetailGeneral to include category selection using SelectCategory.
    - Refactored SelectCategory component for improved state management and UI consistency.
    - Adjusted styling in Command and Combobox components for better responsiveness and user experience.
    - Cleaned up unused properties in GraphQL queries related to product categories.

--- frontend/core-ui/src/modules/products/product-detail/components/ProductDetailGeneral.tsx
+++ frontend/core-ui/src/modules/products/product-detail/components/ProductDetailGeneral.tsx
@@ -1,11 +1,12 @@
 import { CurrencyField, Input, Label } from 'erxes-ui';
-import { IProductDetail, SelectProductType } from 'ui-modules';
+import { IProductDetail, SelectCategory, SelectProductType } from 'ui-modules';
 
 export const ProductDetailGeneral = ({
   name,
   code,
   shortName,
   description,
+  categoryId,
   currency,
   unitPrice,
   type,
@@ -29,8 +30,8 @@ export const ProductDetailGeneral = ({
         <SelectProductType value={type} onValueChange={() => null} />
       </div>
       <div className="space-y-2">
-        <Label>Type</Label>
-        <SelectProductType value={type} onValueChange={() => null} />
+        <Label>Category</Label>
+        <SelectCategory selected={categoryId} onSelect={() => null} />
       </div>
       <div className="space-y-2 col-start-1">
         <Label>Currency</Label>
@@ -39,6 +40,9 @@ export const ProductDetailGeneral = ({
       <div className="space-y-2">
         <Label>Unit price</Label>
         <CurrencyField.ValueInput value={unitPrice} onChange={() => null} />
+      </div>
+      <div className="col-span-3">
+        <Label>Description</Label>
       </div>
     </div>
   );
