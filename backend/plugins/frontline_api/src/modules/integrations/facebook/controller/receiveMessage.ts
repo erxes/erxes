638446874e35fa6cac65bdee979bfe3b7c245414
uuganaa1007 <80632055+uuganaa1007@users.noreply.github.com>
2025-10-30 15:03:03 +0800

    debug: facebook

--- backend/plugins/frontline_api/src/modules/integrations/facebook/controller/receiveMessage.ts
+++ backend/plugins/frontline_api/src/modules/integrations/facebook/controller/receiveMessage.ts
@@ -19,9 +19,6 @@ export const receiveMessage = async (
   activity: Activity,
 ) => {
   try {
-    console.log(activity, 'activity');
-    console.log(integration, 'integration');
-
     debugFacebook(
       `Received message: ${activity.text} from ${activity.from.id}`,
     );
