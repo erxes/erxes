b85cca7971b719c0d4f3cc5c388f168f291be270
enkhtuvshinD <da.enkhtuvshin@gmail.com>
2025-11-20 20:28:06 +0800

    chore: remove log

--- frontend/plugins/frontline_ui/src/modules/utils.ts
+++ frontend/plugins/frontline_ui/src/modules/utils.ts
@@ -21,18 +21,15 @@ let memoizedApiUrl: string | null = null;
 
 const getWidgetUrl = (): string => {
   // if (memoizedApiUrl) return memoizedApiUrl;
-  console.log(
-    process.env.REACT_APP_WIDGETS_URL,
-    'process.env.REACT_APP_WIDGETS_URL',
-  );
+
   const envApiUrl =
     window.env?.REACT_APP_WIDGETS_URL ??
     (process.env.REACT_APP_WIDGETS_URL || getDefaultUrl());
-  console.log(envApiUrl, 'envApiUrl');
+
   memoizedApiUrl = envApiUrl?.includes('<subdomain>')
     ? envApiUrl.replace('<subdomain>', getSubdomain())
     : envApiUrl;
-  console.log(memoizedApiUrl, 'memoizedApiUrl');
+
   return memoizedApiUrl;
 };
 
