c35bf6b077b52dcfde48c62bc842ab447a8fba64
Khe <baterdne.k@gmail.com>
2025-11-30 20:11:11 +0800

    refactor: replace ComponentsPath enum with ComponentsPaths map and add ReportPage route

--- frontend/core-ui/src/modules/app/components/ComponentsRoutes.tsx
+++ frontend/core-ui/src/modules/app/components/ComponentsRoutes.tsx
@@ -1,11 +1,17 @@
 import { lazy, Suspense } from 'react';
 import { Navigate, Route, Routes } from 'react-router';
 
-import { ComponentsPath } from '@/types/paths/ComponentsPath';
+import { ComponentsPaths } from '@/types/paths/ComponentsPaths';
 
 const SelectComponentsIndexPage = lazy(() =>
   import('~/pages/components/SelectComponentIndexPage').then((module) => ({
     default: module.SelectComponentIndexPage,
+  })),
+);
+
+const ReportPage = lazy(() =>
+  import('~/pages/components/Reportpage').then((module) => ({
+    default: module.ReportPage,
   })),
 );
 
@@ -17,15 +23,16 @@ export const ComponentsRoutes = () => {
           path="/"
           element={
             <Navigate
-              to={`${ComponentsPath.Index}${ComponentsPath.Select}`}
+              to={`${ComponentsPaths.Index}${ComponentsPaths.Select}`}
               replace
             />
           }
         />
         <Route
-          path={ComponentsPath.Select}
+          path={ComponentsPaths.Select}
           element={<SelectComponentsIndexPage />}
         />
+        <Route path={ComponentsPaths.Report} element={<ReportPage />} />
       </Routes>
     </Suspense>
   );
