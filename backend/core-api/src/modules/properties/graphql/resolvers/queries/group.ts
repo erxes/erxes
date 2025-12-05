bd724a0aead912548bbdd2363658f644b164056a
batmnkh2344 <batmnkh2344@gmail.com>
2025-12-05 20:13:32 +0800

    chore: get params properly

--- backend/core-api/src/modules/properties/graphql/resolvers/queries/group.ts
+++ backend/core-api/src/modules/properties/graphql/resolvers/queries/group.ts
@@ -24,7 +24,7 @@ const generateFilter = async (params: IFieldGroupParams) => {
 export const groupQueries = {
   fieldGroups: async (
     _: undefined,
-    params: IFieldGroupParams,
+    { params }: { params: IFieldGroupParams },
     { models }: IContext,
   ) => {
     const filter = await generateFilter(params);
