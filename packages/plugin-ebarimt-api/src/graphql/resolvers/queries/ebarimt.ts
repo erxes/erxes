e7fbda4eaeeea3aab1ac946e130acc9c1c6d8ec1
uuganaa1007 <suuganbayr948@gmail.com>
2026-01-07 15:15:09 +0800

    test ci ebarimt

--- packages/plugin-ebarimt-api/src/graphql/resolvers/queries/ebarimt.ts
+++ packages/plugin-ebarimt-api/src/graphql/resolvers/queries/ebarimt.ts
@@ -13,6 +13,7 @@ import { getFullDate, getTomorrow } from "./utils";
 
 const generateFilter = async (subdomain, params, commonQuerySelector) => {
 
+  //ebarimt filter
   const filter: any = commonQuerySelector;
 
   if (params.search) {
