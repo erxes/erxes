987f01b57b94cdb13ce6af60072773d0382c085b
batzorig11 <lbatzorig11@gmail.com>
2026-01-13 20:20:22 +0800

    fix: update usePipelineDetail hook to include pipeline ID type in query options

--- frontend/libs/ui-modules/src/modules/products/hooks/usePipeline.ts
+++ frontend/libs/ui-modules/src/modules/products/hooks/usePipeline.ts
@@ -4,15 +4,19 @@ import { GET_PIPELINE_DETAIL } from '../graphql/queries/pipeLineQueries';
 import { IPipeline } from '../types/Pipeline';
 
 export const usePipelineDetail = (
-  options?: QueryHookOptions<{ salesPipelineDetail: IPipeline }>,
+  options?: QueryHookOptions<
+    { salesPipelineDetail: IPipeline },
+    { _id: string }
+  >,
 ) => {
   const [pipelineIdFromQuery] = useQueryState('pipelineId');
 
-  const pipelineId = options?.variables?._id || pipelineIdFromQuery;
+  const pipelineId = (options?.variables?._id || pipelineIdFromQuery) as string;
 
-  const { data, loading, error } = useQuery<{
-    salesPipelineDetail: IPipeline;
-  }>(GET_PIPELINE_DETAIL, {
+  const { data, loading, error } = useQuery<
+    { salesPipelineDetail: IPipeline },
+    { _id: string }
+  >(GET_PIPELINE_DETAIL, {
     ...options,
     variables: {
       ...options?.variables,
