4ce6d4152a20612c62d0cf4c5dff8e63abe2858f
Kato-111 <gundsambuuganzil111@gmail.com>
2026-01-15 11:57:24 +0800

    removed unnecessary autoFocus attribute

--- frontend/core-ui/src/modules/settings/tags/components/fields/TagsListNameField.tsx
+++ frontend/core-ui/src/modules/settings/tags/components/fields/TagsListNameField.tsx
@@ -166,7 +166,6 @@ export const TagsListNameField = ({
               )();
             }
           }}
-          autoFocus
           {...restRegister}
           ref={(e) => {
             registerRef(e);
