b7d71c77aae9620554495188211f121a41b27d8c
Anu-Ujin <ujinka44@gmail.com>
2026-01-16 20:00:13 +0800

    fix: sales router

--- frontend/plugins/sales_ui/src/config.tsx
+++ frontend/plugins/sales_ui/src/config.tsx
@@ -1,6 +1,7 @@
 import { IconBriefcase, IconSandbox } from '@tabler/icons-react';
+import { Suspense, lazy } from 'react';
+
 import { IUIConfig } from 'erxes-ui';
-import { lazy, Suspense } from 'react';
 
 const MainNavigation = lazy(() =>
   import('./modules/MainNavigation').then((module) => ({
@@ -22,7 +23,7 @@ const PosOrderNavigation = lazy(() =>
 
 export const CONFIG: IUIConfig = {
   name: 'sales',
-  icon: IconBriefcase,
+  path: 'sales',
   navigationGroup: {
     name: 'sales',
     icon: IconBriefcase,
@@ -31,7 +32,7 @@ export const CONFIG: IUIConfig = {
         <MainNavigation />
       </Suspense>
     ),
-    subGroups: () => (
+    subGroup: () => (
       <Suspense fallback={<div />}>
         <SalesSubNavigation />
         <PosOrderNavigation />
@@ -43,21 +44,15 @@ export const CONFIG: IUIConfig = {
       name: 'sales',
       icon: IconBriefcase,
       path: 'sales',
-      hasSettings: false,
-      hasRelationWidget: true,
-      hasFloatingWidget: false,
     },
     {
       name: 'deals',
       path: 'sales/deals',
-      settingsOnly: true,
     },
     {
       name: 'pos',
       icon: IconBriefcase,
       path: 'sales/pos',
-      hasSettings: true,
-      hasAutomation: false,
     },
   ],
   widgets: {
