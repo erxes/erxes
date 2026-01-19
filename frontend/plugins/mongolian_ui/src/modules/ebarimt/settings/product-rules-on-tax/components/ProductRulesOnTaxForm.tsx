b3b3a3dbd1d553c8ba0f531a01a6d7869cf2e4dc
huslen <huslens1027@gmail.com>
2026-01-19 14:01:06 +0800

    update

--- frontend/plugins/mongolian_ui/src/modules/ebarimt/settings/product-rules-on-tax/components/ProductRulesOnTaxForm.tsx
+++ frontend/plugins/mongolian_ui/src/modules/ebarimt/settings/product-rules-on-tax/components/ProductRulesOnTaxForm.tsx
@@ -1,9 +1,8 @@
 import { UseFormReturn } from 'react-hook-form';
 import { Button, Dialog, Form, Input, Select, Spinner } from 'erxes-ui';
-import { IconX } from '@tabler/icons-react';
 import {
-  TaxCode,
   TaxType,
+  TAX_CODE_OPTIONS,
 } from '@/ebarimt/settings/product-rules-on-tax/constants/productRulesOnTaxDefaultValues';
 import { SelectProductCategories } from '@/ebarimt/settings/product-rules-on-tax/components/selects/SelectProductCategories';
 import { SelectProducts } from '@/ebarimt/settings/product-rules-on-tax/components/selects/SelectProducts';
@@ -11,7 +10,9 @@ import { SelectExcludeProducts } from '@/ebarimt/settings/product-rules-on-tax/c
 import { SelectTags } from '@/ebarimt/settings/product-rules-on-tax/components/selects/SelectTags';
 import { SelectExcludeTags } from '@/ebarimt/settings/product-rules-on-tax/components/selects/SelectExcludeTags';
 import { TProductRulesOnTaxForm } from '@/ebarimt/settings/product-rules-on-tax/constants/productRulesOnTaxSchema.ts';
-import { useCallback } from 'react';
+import { useCallback, useEffect } from 'react';
+import { useWatch } from 'react-hook-form';
+import { SelectCategory } from 'ui-modules';
 
 export const ProductRulesOnTaxForm = ({
   form,
@@ -22,6 +23,36 @@ export const ProductRulesOnTaxForm = ({
   onSubmit: (data: TProductRulesOnTaxForm) => void;
   loading: boolean;
 }) => {
+  const taxType = useWatch({ control: form.control, name: 'taxType' });
+  const kind = useWatch({ control: form.control, name: 'kind' });
+
+  useEffect(() => {
+    if (taxType) {
+      switch (taxType) {
+        case TaxType.CityTax:
+          form.setValue('kind', 'ctax');
+          form.setValue('taxCode', '');
+          form.setValue('percent', 0);
+          break;
+        case TaxType.Inner:
+          form.setValue('kind', 'vat');
+          form.setValue('taxCode', '');
+          form.setValue('percent', 0);
+          break;
+        case TaxType['0 percent']:
+          form.setValue('kind', 'vat');
+          form.setValue('percent', 0);
+          break;
+        case TaxType.Free:
+          form.setValue('kind', 'vat');
+          form.setValue('percent', 0);
+          break;
+        default:
+          break;
+      }
+    }
+  }, [taxType, form]);
+
   const handleNumberChange = useCallback(
     (value: string, onChange: (value: number) => void) => {
       let cleanedValue = '';
@@ -72,17 +103,16 @@ export const ProductRulesOnTaxForm = ({
         />
         <Form.Field
           control={form.control}
-          name="productCategories"
+          name="productCategoryIds"
           render={({ field }) => (
             <Form.Item>
-              <Form.Label>Product Categories</Form.Label>
-              <SelectProductCategories
-                value={field.value}
-                onValueChange={(value) => {
-                  field.onChange(value);
-                }}
-                disabled={loading}
-              />
+              <Form.Label>PRODUCT CATEGORIES</Form.Label>
+              <Form.Control>
+                <SelectCategory
+                  selected={field.value}
+                  onSelect={field.onChange}
+                />
+              </Form.Control>
             </Form.Item>
           )}
         />
@@ -119,7 +149,7 @@ export const ProductRulesOnTaxForm = ({
         />
         <Form.Field
           control={form.control}
-          name="excludeCategories"
+          name="excludeCategoryIds"
           render={({ field }) => (
             <Form.Item>
               <Form.Label>Exclude Categories</Form.Label>
@@ -139,7 +169,11 @@ export const ProductRulesOnTaxForm = ({
           render={({ field }) => (
             <Form.Item>
               <Form.Label>Tax Code</Form.Label>
-              <Select value={field.value} onValueChange={field.onChange}>
+              <Select
+                value={field.value}
+                onValueChange={field.onChange}
+                disabled={false}
+              >
                 <Form.Control>
                   <Select.Trigger className="text-muted-foreground">
                     <Select.Value placeholder="Select a tax code">
@@ -154,11 +188,21 @@ export const ProductRulesOnTaxForm = ({
                   </Select.Trigger>
                 </Form.Control>
                 <Select.Content>
-                  {Object.values(TaxCode).map((kind) => (
-                    <Select.Item key={kind} value={kind} className="capitalize">
-                      {kind}
-                    </Select.Item>
-                  ))}
+                  {taxType && Object.keys(TAX_CODE_OPTIONS).includes(taxType)
+                    ? TAX_CODE_OPTIONS[
+                        taxType as keyof typeof TAX_CODE_OPTIONS
+                      ].map((code) => (
+                        <Select.Item
+                          key={code}
+                          value={code}
+                          className="capitalize"
+                        >
+                          <div className="w-full min-w-0">
+                            <span className="truncate">{code}</span>
+                          </div>
+                        </Select.Item>
+                      ))
+                    : null}
                 </Select.Content>
               </Select>
             </Form.Item>
@@ -166,7 +210,7 @@ export const ProductRulesOnTaxForm = ({
         />
         <Form.Field
           control={form.control}
-          name="products"
+          name="productIds"
           render={({ field }) => (
             <Form.Item>
               <Form.Label>Products</Form.Label>
@@ -190,6 +234,7 @@ export const ProductRulesOnTaxForm = ({
                 <Input
                   value={field.value}
                   onChange={(e) => field.onChange(e.target.value)}
+                  disabled={true}
                 />
               </Form.Control>
             </Form.Item>
@@ -197,7 +242,7 @@ export const ProductRulesOnTaxForm = ({
         />
         <Form.Field
           control={form.control}
-          name="excludeProducts"
+          name="excludeProductIds"
           render={({ field }) => (
             <Form.Item>
               <Form.Label>Exclude Products</Form.Label>
@@ -221,12 +266,12 @@ export const ProductRulesOnTaxForm = ({
                 <Input
                   type="text"
                   inputMode="decimal"
-                  value={field.value || ''}
+                  value={field.value || 0}
                   onChange={(e) => {
                     handleNumberChange(e.target.value, field.onChange);
                   }}
                   placeholder="Enter percent"
-                  disabled={loading}
+                  disabled={loading || kind === 'ctax'}
                 />
               </Form.Control>
             </Form.Item>
@@ -234,7 +279,7 @@ export const ProductRulesOnTaxForm = ({
         />
         <Form.Field
           control={form.control}
-          name="tags"
+          name="tagIds"
           render={({ field }) => (
             <Form.Item>
               <Form.Label>Tags</Form.Label>
@@ -250,7 +295,7 @@ export const ProductRulesOnTaxForm = ({
         />
         <Form.Field
           control={form.control}
-          name="excludeTags"
+          name="excludeTagIds"
           render={({ field }) => (
             <Form.Item>
               <Form.Label>Exclude Tags</Form.Label>
@@ -301,7 +346,7 @@ export const EBarimtDialog = ({
             size="icon"
             className="absolute right-4 top-3"
           >
-            <IconX />
+            ×
           </Button>
         </Dialog.Close>
       </Dialog.Header>
