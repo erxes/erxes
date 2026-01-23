919389cb92171192b738ad4a3a8561afbe002eb5
munkhsaikhan <munkhsaikhan.g@nmma.co>
2026-01-23 12:39:53 +0800

    little

--- frontend/plugins/mongolian_ui/src/modules/ebarimt/put-response/put-responses-by-date/constants/billTypeData.ts
+++ frontend/plugins/mongolian_ui/src/modules/ebarimt/put-response/put-responses-by-date/constants/billTypeData.ts
@@ -1,5 +1,5 @@
 export const BILL_TYPE_DATA = [
   { value: 'all', label: 'All' },
-  { value: 'B2C_RECEIPT', label: 'B2C Reciept' },
-  { value: 'B2B_RECEIPT', label: 'B2B Reciept' },
+  { value: 'B2C_RECEIPT', label: 'B2C RECEIPT' },
+  { value: 'B2B_RECEIPT', label: 'B2B RECEIPT' },
 ];
