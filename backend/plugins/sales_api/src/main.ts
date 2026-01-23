38a23558bd39e01301b9e0c35982652833823b7d
orshih <orshih943@gmail.com>
2026-01-23 09:41:34 +0800

    fix: revert

--- backend/plugins/sales_api/src/main.ts
+++ backend/plugins/sales_api/src/main.ts
@@ -11,7 +11,7 @@ import { createLoaders } from './modules/sales/graphql/resolvers/loaders';
 
 startPlugin({
   name: 'sales',
-  port: 3309,
+  port: 3305,
   graphql: async () => ({
     typeDefs: await typeDefs(),
     resolvers,
