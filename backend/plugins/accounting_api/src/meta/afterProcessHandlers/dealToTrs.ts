00093d79195401f54f86bbead26c9f0db16e4f44
munkhsaikhan <munkhsaikhan.g@nmma.co>
2026-02-02 01:55:24 +0800

    continu

--- backend/plugins/accounting_api/src/meta/afterProcessHandlers/dealToTrs.ts
+++ backend/plugins/accounting_api/src/meta/afterProcessHandlers/dealToTrs.ts
@@ -1,5 +1,5 @@
 import { IUserDocument } from "erxes-api-shared/core-types"
-import { sendTRPCMessage } from "erxes-api-shared/utils"
+import { fixNum, sendTRPCMessage } from "erxes-api-shared/utils"
 import { nanoid } from "nanoid"
 import { IModels } from "~/connectionResolvers"
 import { JOURNALS, TR_SIDES } from "~/modules/accounting/@types/constants"
@@ -11,6 +11,14 @@ export const dealToTrs = async ({
   subdomain: string, models: IModels, user: IUserDocument, deal: any, config: {
     date: 'alwaysNow' | 'syncedDateOrNow',
     saleAccountId: string,
+    saleOutAccountId: string,
+    saleCostAccountId: string,
+    branchId: string,
+    departmentId: string,
+    hasVat: boolean,
+    hasCtax: boolean,
+    vatRowId: string,
+    ctaxRowId: string,
 
   }
 }) => {
@@ -23,8 +31,30 @@ export const dealToTrs = async ({
     return;
   }
 
-  const trCommonDoc: ITransaction = {
-    date: new Date, journal: JOURNALS.INV_SALE, contentType, contentId, details: []
+  const mainId = nanoid();
+  const ptrId = nanoid();
+  const saleTrDoc: ITransaction = {
+    _id: mainId,
+    ptrId: ptrId,
+    parentId: mainId,
+    date: new Date,
+    journal: JOURNALS.INV_SALE,
+    followInfos: {
+      saleOutAccountId: config.saleOutAccountId,
+      saleCostAccountId: config.saleCostAccountId,
+    },
+    branchId: deal.branchId || config.branchId,
+    departmentId: deal.departmentId || config.departmentId,
+    assignedUserIds: deal.assignedUserIds,
+
+    hasVat: config.hasVat,
+    hasCtax: config.hasCtax,
+    vatRowId: config.vatRowId,
+    ctaxRowId: config.ctaxRowId,
+
+    contentType,
+    contentId,
+    details: []
   };
 
   const companyIds = await sendTRPCMessage({
@@ -41,8 +71,8 @@ export const dealToTrs = async ({
   });
 
   if (companyIds?.length) {
-    trCommonDoc.customerType = 'company';
-    trCommonDoc.customerId = companyIds[0];
+    saleTrDoc.customerType = 'company';
+    saleTrDoc.customerId = companyIds[0];
   } else {
     const customerIds = await sendTRPCMessage({
       subdomain,
@@ -57,38 +87,36 @@ export const dealToTrs = async ({
       defaultValue: []
     });
 
-    trCommonDoc.customerType = 'customer';
-    trCommonDoc.customerId = customerIds[0];
+    saleTrDoc.customerType = 'customer';
+    saleTrDoc.customerId = customerIds[0];
   }
 
   const saleDetails: ITrDetail[] = []
+  const vatRow = await models.VatRows.getVatRow(config.vatRowId);
+  const ctaxRow = await models.CtaxRows.getCtaxRow(config.ctaxRowId);
+
+  const taxPercent = fixNum((vatRow.percent ?? 0) + (ctaxRow.percent ?? 0));
+
+  let diffAmount = 0;
   for (const productData of activeProductsData) {
-    saleDetails.push({
+    diffAmount = diffAmount + deal.amount;
+    const taxAmount = fixNum((deal.amount * taxPercent) / (100 + taxPercent), 8);
+    const amount = fixNum(deal.amount - taxAmount, 8);
+    saleTrDoc.details.push({
       _id: nanoid(),
       accountId: config.saleAccountId,
-
-      // followInfos?: any;
-      // originId?: string;
-      // originType?: string;
-      // originSubId?: string;
-
       side: TR_SIDES.CREDIT,
-      amount: productData.amount,
+      amount,
       currency: productData.currency,
-      // currencyAmount?: number;
-      // customRate?: number;
-
-      // assignedUserId?: string;
-
-      // excludeVat?: boolean;
-      // excludeCtax?: boolean;
 
       productId: productData.productId,
       count: productData.quantity,
       unitPrice: productData.unitPrice
-
     })
   }
+
+  deal.paymentsData
+
 
 
 
