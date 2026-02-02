27d0dff185da11551d811a52db246a97d9a0f177
munkhsaikhan <munkhsaikhan.g@nmma.co>
2026-02-02 15:50:25 +0800

    navigation

--- frontend/plugins/loyalty_ui/src/modules/navigation/MainNavigation.tsx
+++ frontend/plugins/loyalty_ui/src/modules/navigation/MainNavigation.tsx
@@ -1,16 +1,9 @@
-import { IconClipboard, IconUserFilled } from '@tabler/icons-react';
+import { IconUserFilled } from '@tabler/icons-react';
 import { NavigationMenuLinkItem } from 'erxes-ui';
 
 export const MainNavigation = () => {
   return (
     <>
-      <NavigationMenuLinkItem
-        name="Pricing"
-        icon={IconClipboard}
-        pathPrefix="loyalty"
-        path="pricing"
-      />
-
       <NavigationMenuLinkItem
         name="Loyalty"
         icon={IconUserFilled}
