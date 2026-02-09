60056e5f28d80e238659fb754fc61fcc5af42cd8
Dulguunzaya <dukmongonshagai@gmail.com>
2026-02-09 22:01:05 +0800

    fix: update checklist and widget card on sales

--- frontend/plugins/sales_ui/src/widgets/relation/modules/DealWidgetCard.tsx
+++ frontend/plugins/sales_ui/src/widgets/relation/modules/DealWidgetCard.tsx
@@ -1,84 +1,66 @@
-import { Card, Separator, useQueryState } from 'erxes-ui';
-import { Suspense, lazy } from 'react';
+import { Card, Separator } from 'erxes-ui';
 
 import { DateSelectDeal } from '@/deals/components/deal-selects/DateSelectDeal';
 import { IDeal } from '@/deals/types/deals';
 import { ItemFooter } from '@/deals/cards/components/item/Footer';
-import { SelectDealPriority } from '@/deals/components/deal-selects/SelectDealPriority';
-import { SelectLabels } from '@/deals/components/common/filters/SelectLabel';
-import { dealDetailSheetState } from '@/deals/states/dealDetailSheetState';
-import { useAtom } from 'jotai';
-
-const DealDetailSheet = lazy(() =>
-  import('@/deals/cards/components/detail/SalesItemDetail').then((module) => ({
-    default: module.SalesItemDetail,
-  })),
-);
+import Labels from '@/deals/cards/components/detail/overview/label/Labels';
 
 export const DealWidgetCard = ({ deal }: { deal: IDeal }) => {
   const {
     startDate,
     closeDate,
     name,
-    priority,
     _id,
     createdAt,
     assignedUsers,
+    labels,
+    boardId,
+    pipeline,
   } = deal || {};
-  const [activeDeal, setActiveDeal] = useAtom(dealDetailSheetState);
-  const [, setSalesItemId] = useQueryState<string>('salesItemId');
 
   const onCardClick = () => {
-    setSalesItemId(_id);
-    setActiveDeal(_id);
+    const dealUrl = `/sales/deals?boardId=${boardId}&pipelineId=${pipeline._id}&salesItemId=${_id}`;
+    window.open(dealUrl, '_blank');
   };
 
   return (
-    <>
-      <Card className="bg-background" onClick={() => onCardClick()}>
-        <div className="px-2 h-9 flex flex-row items-center justify-between space-y-0">
-          <DateSelectDeal
-            value={startDate}
-            id={_id}
-            type="startDate"
-            variant="card"
-            placeholder="Start Date"
-          />
-          <DateSelectDeal
-            value={closeDate}
-            id={_id}
-            type="closeDate"
-            variant="card"
-            placeholder="Close Date"
-          />
-        </div>
-        <Separator />
-        <div className="p-3">
-          <div className="flex flex-col gap-1">
-            <h5 className="font-semibold">{name}</h5>
-          </div>
-          <div className="flex flex-wrap gap-1 pt-2 pb-1">
-            <SelectDealPriority
-              dealId={_id}
-              value={priority || ''}
-              variant="card"
-            />
-            <SelectLabels.FilterBar
-              filterKey=""
-              mode="multiple"
-              label="By Label"
-              variant="card"
-            />
-          </div>
-        </div>
-        <Separator />
-        <ItemFooter
-          createdAt={createdAt}
-          assignedUsers={assignedUsers || []}
+    <Card
+      className="bg-background cursor-pointer"
+      onClick={() => onCardClick()}
+    >
+      <div className="px-2 h-9 flex flex-row items-center justify-between space-y-0">
+        <DateSelectDeal
+          value={startDate}
           id={_id}
+          type="startDate"
+          variant="card"
+          placeholder="Start Date"
         />
-      </Card>
-      <Suspense>{activeDeal && <DealDetailSheet />}</Suspense>
-    </>
+        <DateSelectDeal
+          value={closeDate}
+          id={_id}
+          type="closeDate"
+          variant="card"
+          placeholder="Close Date"
+        />
+      </div>
+      <Separator />
+      <div className="p-3">
+        {labels && labels.length !== 0 && (
+          <div className="flex flex-wrap items-center gap-2 mb-2">
+            <Labels labels={labels} type="toggle" />
+          </div>
+        )}
+        <div className="flex flex-col gap-1">
+          <h5 className="font-semibold">{name}</h5>
+        </div>
+      </div>
+      <Separator />
+      <ItemFooter
+        createdAt={createdAt}
+        assignedUsers={assignedUsers || []}
+        id={_id}
+      />
+    </Card>
   );
 };
