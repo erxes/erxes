4fd4b898d9dba72b89e7814de36de372cc44d630
Khe <baterdne.k@gmail.com>
2026-02-10 11:15:44 +0800

    refactor: enhance lead submission structure in ErxesForm component
    - Updated the submission logic in ErxesForm to transform values into a structured format, including field type and text.
    - Improved data handling for lead submissions by mapping over form values to ensure consistency and clarity.
    - This change enhances the overall data management during form submissions, aligning with previous improvements in message handling.

--- apps/frontline-widgets/src/app/form/components/ErxesForm.tsx
+++ apps/frontline-widgets/src/app/form/components/ErxesForm.tsx
@@ -52,7 +52,15 @@ export const ErxesForm = ({
       saveLead({
         variables: {
           formId: formData._id,
-          submissions: values,
+          submissions: Object.entries(values).map(([key, value]) => {
+            const field = fields.find((field) => field._id === key);
+            return {
+              _id: key,
+              type: field?.type || 'input',
+              text: field?.text || key,
+              value,
+            };
+          }),
           browserInfo,
         },
       });
@@ -248,3 +256,39 @@ export const ErxesFormItem = ({
 }: React.ComponentProps<typeof Form.Item> & { span: number }) => (
   <Form.Item {...props} className={cn(props.className, span && `col-span-2`)} />
 );
+
+// {
+//   "formId": "3wuh4tvP06VTrweVXtBE8",
+//   "browserInfo": {
+//     "remoteAddress": "202.21.102.0/23",
+//     "region": "Ulaanbaatar",
+//     "countryCode": "MN",
+//     "city": "Ulaanbaatar",
+//     "country": "Mongolia",
+//     "url": "//test",
+//     "hostname": "https://w.office.erxes.io",
+//     "language": "en-US",
+//     "userAgent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36"
+//   },
+//   "submissions": [
+//     {
+//       "_id": "z-IF2Maz0IYFQwi-cu3Am",
+//       "type": "input",
+//       "text": "asdasdasd",
+//       "value": "ff",
+//       "validation": null,
+//       "associatedFieldId": "",
+//       "column": null
+//     },
+//     {
+//       "_id": "TV_o24TLna-fPhchiv-vN",
+//       "type": "input",
+//       "text": "vfevdfvdfv",
+//       "value": "ff",
+//       "validation": null,
+//       "associatedFieldId": "",
+//       "column": null
+//     }
+//   ],
+//   "cachedCustomerId": "RcZ03QmavkVqcTlolTbHG"
+// }
