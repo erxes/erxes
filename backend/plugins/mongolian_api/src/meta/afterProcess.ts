91aa87ecee4e56135f8df58fe5fb20fa461bd6e7
Sekulya <secheikheno@gmail.com>
2026-02-24 19:34:31 +0800

    fix: afterProcess

--- backend/plugins/mongolian_api/src/meta/afterProcess.ts
+++ backend/plugins/mongolian_api/src/meta/afterProcess.ts
@@ -1,33 +1,67 @@
-import { AfterProcessConfigs, IAfterProcessRule } from 'erxes-api-shared/utils';
+import {
+  AfterProcessConfigs,
+  IAfterProcessRule,
+} from 'erxes-api-shared/utils';
 import { generateModels } from '~/connectionResolvers';
 import { afterMutationHandlers as dealAfterEbarimt } from '~/modules/ebarimt/afterMutations';
+import { afterMutationHandlers as productPlacesAfterMutation } from '~/modules/productPlaces/afterMutations';
 
 const ebarimtMutationNames = ['dealsChange', 'dealsEdit', 'dealsAdd'];
+
+// You can reuse same mutation names because productPlaces
+const productPlacesMutationNames = ['dealsChange', 'dealsEdit'];
+
 const allRules: IAfterProcessRule[] = [
   {
     type: 'afterMutation',
-    mutationNames: ebarimtMutationNames
-  }
+    mutationNames: [...ebarimtMutationNames, ...productPlacesMutationNames],
+  },
 ];
-
 
 export const afterProcess: AfterProcessConfigs = {
   rules: allRules,
+
   afterMutation: async (ctx, input) => {
     const { mutationName, args, result, userId } = input?.data ?? {};
 
+    if (!mutationName) return;
+
+    const { itemId, destinationStageId, sourceStageId } = args || {};
+
     if (ebarimtMutationNames.includes(mutationName)) {
-      const { itemId, destinationStageId, sourceStageId } = args;
-      const { stageId } = result;
-      if (destinationStageId && destinationStageId !== sourceStageId && destinationStageId === stageId && itemId) {
+      const { stageId } = result || {};
+
+      if (
+        destinationStageId &&
+        destinationStageId !== sourceStageId &&
+        destinationStageId === stageId &&
+        itemId
+      ) {
         const models = await generateModels(ctx.subdomain);
-        await dealAfterEbarimt(models, ctx.subdomain, ctx.processId || '', {
-          sourceStageId,
-          destinationStageId,
-          deal: result,
-          userId
-        });
+
+        await dealAfterEbarimt(
+          models,
+          ctx.subdomain,
+          ctx.processId || '',
+          {
+            sourceStageId,
+            destinationStageId,
+            deal: result,
+            userId,
+          }
+        );
       }
     }
-  }
+
+    // PRODUCT PLACES
+    if (productPlacesMutationNames.includes(mutationName)) {
+      await productPlacesAfterMutation(ctx.subdomain, {
+        type: 'sales:deal',
+        action: 'update',
+        updatedDocument: result,
+        object: result,
+        user: userId,
+      });
+    }
+  },
 };
