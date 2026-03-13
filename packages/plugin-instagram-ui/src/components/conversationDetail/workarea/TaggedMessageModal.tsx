9e6aa8a895dd118e4df1dcc222f908bc3d05a7c9
uuganaa1007 <80632055+uuganaa1007@users.noreply.github.com>
2026-03-13 12:29:54 +0800

    fix: facebook-messenger add HUMAN_AGENT tag support for replies outside 24-hour window"

--- packages/plugin-instagram-ui/src/components/conversationDetail/workarea/TaggedMessageModal.tsx
+++ packages/plugin-instagram-ui/src/components/conversationDetail/workarea/TaggedMessageModal.tsx
@@ -35,6 +35,7 @@ class Modal extends React.Component<Props, {}> {
       { label: "Confirmed Event Update", value: "CONFIRMED_EVENT_UPDATE" },
       { label: "Post-Purchase Update", value: "POST_PURCHASE_UPDATE" },
       { label: "Account Update", value: "ACCOUNT_UPDATE" },
+      { label: "Human Agent", value: "HUMAN_AGENT" }
     ];
 
     const { extraInfo = { tag: "" } } = this.props;
