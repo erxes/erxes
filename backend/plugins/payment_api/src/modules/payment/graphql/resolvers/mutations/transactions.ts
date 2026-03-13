32de6714a5295de1717c034d7dcc911c92875d43
batuktech <batuukka@gmail.com>
2026-03-13 15:37:51 +0800

    fix:cptransactionadd

--- backend/plugins/payment_api/src/modules/payment/graphql/resolvers/mutations/transactions.ts
+++ backend/plugins/payment_api/src/modules/payment/graphql/resolvers/mutations/transactions.ts
@@ -43,27 +43,6 @@ const mutations: Record<string, Resolver> = {
       details: { ...input.details, ...invoice.data },
     });
   },
-
-  async cpPaymentTransactionsAdd(
-    _root,
-    args: any,
-    { models, subdomain }: IContext,
-  ) {
-    const { input } = args;
-    const invoice = await models.Invoices.getInvoice(
-      { _id: input.invoiceId },
-      true,
-    );
-
-    const description = invoice.description || invoice.invoiceNumber;
-
-    return models.Transactions.createTransaction({
-      ...input,
-      subdomain,
-      description,
-      details: { ...input.details, ...invoice.data },
-    });
-  },
 };
 
 mutations.paymentTransactionsAdd.wrapperConfig = {
