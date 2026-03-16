07c80e93a5fae6b51ed030993e1043bc006cdf6b
batuktech <batuukka@gmail.com>
2026-03-16 14:43:27 +0800

    fix: generate slug on page update2

--- backend/plugins/content_api/src/modules/cms/db/models/Page.ts
+++ backend/plugins/content_api/src/modules/cms/db/models/Page.ts
@@ -52,14 +52,22 @@ export const loadPageClass = (models: IModels) => {
       }
     
       if (doc.name && doc.name !== existing.name) {
-        const baseSlug = doc.slug || slugify(doc.name, { lower: true });
-    
-        doc.slug = await generateUniqueSlug(
-          models.Pages,
-          doc.clientPortalId,
-          'slug',
-          baseSlug
-        );
+        if (!doc.slug) {
+          const baseSlug = slugify(doc.name, { lower: true });
+          doc.slug = await generateUniqueSlug(
+            models.Pages,
+            doc.clientPortalId,
+            'slug',
+            baseSlug,
+          );
+        } else {
+          doc.slug = await generateUniqueSlug(
+            models.Pages,
+            doc.clientPortalId,
+            'slug',
+            doc.slug,
+          );
+        }
       } else {
         doc.slug = existing.slug;
       }
