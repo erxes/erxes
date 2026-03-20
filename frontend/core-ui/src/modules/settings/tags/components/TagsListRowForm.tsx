ef6ebc67dd0a9af69645d7e2b5b9dce6158105c7
kiannidev <kiannidev@gmail.com>
2026-03-20 20:36:30 +0200

    fix javascript issues

--- frontend/core-ui/src/modules/settings/tags/components/TagsListRowForm.tsx
+++ frontend/core-ui/src/modules/settings/tags/components/TagsListRowForm.tsx
@@ -47,11 +47,9 @@ export const TagsListRowForm = () => {
     <div
       className={cn(
         'h-10 w-full shadow-xs flex items-center pr-12 pl-14 group relative hover:bg-foreground/10 bg-background ',
-        addingTag &&
-          addingTag.parentId &&
+        addingTag?.parentId &&
           'pl-20 last:[--svg-height:calc(2.5rem/2-10px)] [--svg-height:calc(2.5rem)] [&>div>svg]:block',
-        addingTag &&
-          !addingTag.parentId &&
+        addingTag && !addingTag?.parentId &&
           'first:rounded-t-lg last:rounded-b-lg',
       )}
     >
@@ -67,8 +65,8 @@ export const TagsListRowForm = () => {
           aria-hidden="true"
           xmlns="http://www.w3.org/2000/svg"
         >
-          <path d="M0 0h1v1c0 2.5 2.212 3.546 2.212 3.546L9.737 8.06c.568.306.094 1.186-.474.88l-6.48-3.488S0 4 0 1V0Z"></path>
-        </svg>
+          <path d="M0 0h1v1c0 2.5 2.212 3.546 2.212 3.546L9.737 8.06c.568.306.094 1.186-.474.88l-6.48-3.488S0 4 0 1V0Z"/>
+        </svg>>
       </div>
       <TagsListCell className="w-full md:max-w-[30%] gap-2">
         <TagsListColorField
@@ -82,7 +80,7 @@ export const TagsListRowForm = () => {
         />
         <TagsListNameField
           name=""
-          defaultOpen={true}
+          defaultOpen
           handleSave={(value) => handleSave(value)}
           isForm
           onEscape={() => {
