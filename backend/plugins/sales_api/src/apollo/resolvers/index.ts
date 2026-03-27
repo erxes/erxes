0e32541760d7c05f216ec7ae282b1b85949129b3
Sekulya <secheikheno@gmail.com>
2026-03-27 12:22:57 +0800

    fix: resolve duplicate Query issue and safely merge customResolvers

--- backend/plugins/sales_api/src/apollo/resolvers/index.ts
+++ backend/plugins/sales_api/src/apollo/resolvers/index.ts
@@ -3,15 +3,26 @@ import { mutations } from './mutations';
 import { queries } from './queries';
 import { customResolvers } from './resolvers';
 
+const cr = customResolvers as any;
+
 const resolvers: any = {
   Mutation: {
     ...mutations,
+    ...(cr.Mutation || {}), // safe
   },
+
   Query: {
     ...queries,
+    ...(cr.Query || {}),
   },
+
   ...apolloCustomScalars,
-  ...customResolvers,
+
+  ...Object.fromEntries(
+    Object.entries(cr).filter(
+      ([key]) => key !== 'Query' && key !== 'Mutation'
+    )
+  ),
 };
 
 export default resolvers;
