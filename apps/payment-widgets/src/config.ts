08a3f01a528701a3e80a988c9a671367d39e5b71
Sekulya <secheikheno@gmail.com>
2026-03-30 10:29:13 +0800

    fix: only specific configs

--- apps/payment-widgets/src/config.ts
+++
@@ -1,9 +0,0 @@
-declare global {
-  interface Window {
-    WIDGET_CONFIG?: {
-      API_URL: string;
-    };
-  }
-}
-
-export const API_URL = window.WIDGET_CONFIG?.API_URL || 'http://localhost:4000';
--- apps/payment-widgets/src/payment/App.tsx
+++
@@ -1,7 +0,0 @@
-import Widgets from './Widgets';
-
-const App = () => {
-  return <Widgets />;
-};
-
-export default App;
