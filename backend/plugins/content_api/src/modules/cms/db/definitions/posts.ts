9d02e52a7d44273ab4892b5d00a3672776093b2c
batuktech <batuukka@gmail.com>
2026-04-06 13:42:54 +0800

    feat: add count field on post that increase itself automatically

--- backend/plugins/content_api/src/modules/cms/db/definitions/posts.ts
+++ backend/plugins/content_api/src/modules/cms/db/definitions/posts.ts
@@ -15,6 +15,7 @@ export const postSchema = new Schema<IPostDocument>(
     _id: mongooseStringRandomId,
     clientPortalId: { type: String, required: true },
     webId: { type: String, optional: true },
+    count: { type: Number, default: 0 },
     title: { type: String, required: true },
     type: { type: String, required: true, default: 'post' },
     slug: { type: String, required: true, unique: true },
