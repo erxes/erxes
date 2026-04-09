83058704b95705fda1a570be2148d4026afd4e34
Justturuu <Turbold2019@gmail.com>
2026-04-09 12:07:58 +0800

    fix: sonar resolve scripts

--- scripts/create-plugin.js
+++ scripts/create-plugin.js
@@ -592,16 +592,16 @@ export default config;
   );
 
   // Create jest.config.ts
-  const jestConfig = String.raw`
+  const jestConfig = `/* eslint-disable */
 export default {
   displayName: '${kebabCaseName}-ui',
   preset: '../../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
-    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+    '^.+\\\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/plugins/${kebabCaseName}-ui',
+  coverageDirectory: '../../coverage/plugins/${kebabCaseName}_ui',
 };
 `;
 
