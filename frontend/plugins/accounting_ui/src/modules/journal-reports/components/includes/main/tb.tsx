7b09dbdfd46cfac557f1e17ef24740e75818f3af
Munkhsaikhan <munkhsaikhan.g@nmma.co>
2026-04-14 17:08:16 +0800

    return code to main branch

--- frontend/plugins/accounting_ui/src/modules/journal-reports/components/includes/main/tb.tsx
+++ frontend/plugins/accounting_ui/src/modules/journal-reports/components/includes/main/tb.tsx
@@ -1,19 +1,14 @@
-import { displayNum, ReportTable } from 'erxes-ui';
-import { IGroupRule } from '~/modules/journal-reports/types/reportsMap';
-import { TR_SIDES } from '~/modules/transactions/types/constants';
-import { CalcReportResult } from '..';
+import { displayNum, ReportTable } from "erxes-ui";
+import { IGroupRule } from "~/modules/journal-reports/types/reportsMap";
+import { TR_SIDES } from "~/modules/transactions/types/constants";
+import { CalcReportResult } from "..";
 
-export const HandleMainTB = (
-  dic: any,
-  _groupRule: IGroupRule,
-  _attr: string,
-): CalcReportResult => {
+export const HandleMainTB = (dic: any, _groupRule: IGroupRule, _attr: string): CalcReportResult => {
   const { items } = dic;
   let [fr_diff, tr_dt, tr_ct, lr_diff] = [0, 0, 0, 0];
 
   for (const rec of items) {
-    const [dtAmount, ctAmount] =
-      rec.side === TR_SIDES.DEBIT ? [rec.sumAmount, 0] : [0, rec.sumAmount];
+    const [dtAmount, ctAmount] = rec.side === TR_SIDES.DEBIT ? [rec.sumAmount, 0] : [0, rec.sumAmount];
     const diffAmount = dtAmount - ctAmount;
 
     if (rec.isBetween) {
@@ -26,22 +21,16 @@ export const HandleMainTB = (
     }
   }
 
-  return (
-    <>
-      <ReportTable.Cell>
-        {(fr_diff > 0 && displayNum(fr_diff)) || ''}
-      </ReportTable.Cell>
-      <ReportTable.Cell>
-        {(fr_diff < 0 && displayNum(-1 * fr_diff)) || ''}
-      </ReportTable.Cell>
-      <ReportTable.Cell>{displayNum(tr_dt)}</ReportTable.Cell>
-      <ReportTable.Cell>{displayNum(tr_ct)}</ReportTable.Cell>
-      <ReportTable.Cell>
-        {(lr_diff > 0 && displayNum(lr_diff)) || ''}
-      </ReportTable.Cell>
-      <ReportTable.Cell>
-        {(lr_diff < 0 && displayNum(-1 * lr_diff)) || ''}
-      </ReportTable.Cell>
-    </>
-  );
-};
+  return {
+    lastNode: (
+      <>
+        <ReportTable.Cell>{displayNum(Math.max(fr_diff, 0))}</ReportTable.Cell>
+        <ReportTable.Cell>{displayNum(-1 * Math.min(fr_diff, 0))}</ReportTable.Cell>
+        <ReportTable.Cell>{displayNum(tr_dt)}</ReportTable.Cell>
+        <ReportTable.Cell>{displayNum(tr_ct)}</ReportTable.Cell>
+        <ReportTable.Cell>{displayNum(Math.max(lr_diff, 0))}</ReportTable.Cell>
+        <ReportTable.Cell>{displayNum(-1 * Math.min(lr_diff, 0))}</ReportTable.Cell>
+      </>
+    )
+  }
+}
