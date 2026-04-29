39258bc511ce498363ca1dfbb89f9c3a0201b71f
honilee <dev@erxes.io>
2026-04-29 18:09:47 +0800

    added sales mobile payment callback

--- backend/plugins/sales_api/src/main.ts
+++ backend/plugins/sales_api/src/main.ts
@@ -67,6 +67,65 @@ startPlugin({
       ],
     },
     notifications,
+    payments: {
+      transactionCallback: async (subdomain, data) => {
+        // TODO: implement transaction callback if necessary
+      },
+      callback: async ({ subdomain }, data) => {
+        const {
+          status,
+          contentType,
+          contentTypeId,
+          amount = 0,
+          _id,
+          currency,
+        } = data;
+
+        if (contentType !== 'sales:deal') {
+          return;
+        }
+        if (status !== 'paid') {
+          return;
+        }
+
+        const models = await generateModels(subdomain);
+        const deal = await models.Deals.getDeal(contentTypeId);
+        const oldPaymentsData = deal.paymentsData || {};
+        const bankData = oldPaymentsData.bank || { info: {} };
+
+        bankData.info = bankData.info || {};
+
+        const oldInfo = (bankData.info.invoices || []).find(
+          (invoice) => invoice._id === _id,
+        );
+        const newAmount =
+          (bankData.amount || 0) - (oldInfo?.amount || 0) + amount;
+
+        bankData.amount = newAmount <= 0 ? 0 : newAmount;
+        bankData.currency = currency || 'MNT';
+        bankData.info.invoices = [
+          ...(bankData.info.invoices || []).filter(
+            (invoice) => invoice._id !== _id,
+          ),
+          {
+            _id,
+            amount,
+          },
+        ];
+
+        await models.Deals.updateOne(
+          { _id: contentTypeId },
+          {
+            $set: {
+              paymentsData: {
+                ...oldPaymentsData,
+                bank: bankData,
+              },
+            },
+          },
+        );
+      },
+    },
     afterProcess,
     importExport: {
       export: {
