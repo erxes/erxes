2584c4ad0813c0bdcf2b8b6d56ca4b8414cc04ee
Munkhsaikhan <munkhsaikhan.g@nmma.co>
2026-04-29 18:52:21 +0800

    accountin invSale form add bulk products

--- frontend/plugins/accounting_ui/src/modules/transactions/transaction-form/components/forms/InvSaleForm/AddInventoryRow.tsx
+++ frontend/plugins/accounting_ui/src/modules/transactions/transaction-form/components/forms/InvSaleForm/AddInventoryRow.tsx
@@ -3,6 +3,7 @@ import { Button } from 'erxes-ui';
 import { useWatch } from 'react-hook-form';
 import { ITransactionGroupForm, TInvDetail } from '../../../types/JournalForms';
 import { getTempId } from '../../utils';
+import { SelectProductsBulk } from 'ui-modules';
 
 export const AddDetailRowButton = ({
   append,
@@ -41,20 +42,26 @@ export const AddDetailRowButton = ({
         <IconPlus />
         Add Empty Row
       </Button>
-      <Button
-        variant="secondary"
-        className="bg-border"
-        onClick={() =>
-          append([
-            detailDefaultValues,
-            detailDefaultValues,
-            detailDefaultValues,
-          ])
+      <SelectProductsBulk
+        productIds={
+          preDetails
+            .map((det) => det.productId ?? '')
+            .filter((prodId) => !!prodId) || []
         }
+        onSelect={(productIds) => {
+          append(
+            productIds.map((productId) => ({
+              ...detailDefaultValues,
+              productId,
+            })),
+          );
+        }}
       >
-        <IconPlus />
-        Add Many Products
-      </Button>
+        <Button variant="secondary" className="bg-border">
+          <IconPlus />
+          Add Many Products
+        </Button>
+      </SelectProductsBulk>
     </>
   );
 };
