e810cef80a5b2b9a4e2f49dcc06d7036aa7293b9
Uuganbayar [SFE] <uuganbayarsfe@Uuganbayars-Mac-mini.local>
2026-05-05 13:54:27 +0800

    feat: cms menu mapping

---
+++ frontend/plugins/content_ui/src/modules/cms/menus/graphql/menuDrawerQueries.ts
@@ -0,0 +1,79 @@
+import { gql } from '@apollo/client';
+
+export const MENU_PAGES_QUERY = gql`
+  query MenuPages($clientPortalId: String!, $limit: Int) {
+    cmsPageList(clientPortalId: $clientPortalId, limit: $limit) {
+      pages {
+        _id
+        name
+        slug
+      }
+    }
+  }
+`;
+
+export const MENU_POSTS_QUERY = gql`
+  query MenuPosts($clientPortalId: String!, $limit: Int, $type: String) {
+    cmsPostList(clientPortalId: $clientPortalId, limit: $limit, type: $type) {
+      posts {
+        _id
+        title
+        slug
+      }
+    }
+  }
+`;
+
+export const MENU_CUSTOM_TYPES_QUERY = gql`
+  query MenuCustomTypes($clientPortalId: String) {
+    cmsCustomPostTypes(clientPortalId: $clientPortalId) {
+      _id
+      code
+      label
+    }
+  }
+`;
+
+export const MENU_CATEGORIES_QUERY = gql`
+  query MenuCategories($clientPortalId: String!, $limit: Int) {
+    cmsCategories(clientPortalId: $clientPortalId, limit: $limit) {
+      list {
+        _id
+        name
+        slug
+      }
+    }
+  }
+`;
+
+export const MENU_TAGS_QUERY = gql`
+  query MenuTags($clientPortalId: String, $limit: Int) {
+    cmsTags(clientPortalId: $clientPortalId, limit: $limit) {
+      tags {
+        _id
+        name
+        slug
+      }
+    }
+  }
+`;
+
+export const MENU_DETECT_QUERY = gql`
+  query MenuDetect($clientPortalId: String!, $limit: Int) {
+    cmsPageList(clientPortalId: $clientPortalId, limit: $limit) {
+      pages {
+        _id
+        slug
+      }
+    }
+    cmsPostList(clientPortalId: $clientPortalId, limit: $limit) {
+      posts {
+        _id
+        slug
+        customPostType {
+          _id
+        }
+      }
+    }
+  }
+`;
