3ecdaa106a5940e397f1a348e190b8fcc87c50e1
Sekulya <secheikheno@gmail.com>
2026-06-08 10:28:06 +0800

    feat: add golomt resolvers to apollo

--- backend/plugins/payment_api/src/apollo/resolvers/index.ts
+++ backend/plugins/payment_api/src/apollo/resolvers/index.ts
@@ -2,12 +2,16 @@ import { apolloCustomScalars } from 'erxes-api-shared/utils';
 import { customResolvers } from './resolvers';
 import { mutations } from './mutations';
 import { queries } from './queries';
+import golomtResolvers from '@/corporateGateway/golomtbank/graphql/resolvers';
 const resolvers: any = {
+  ...golomtResolvers,
   Mutation: {
     ...mutations,
+    ...(golomtResolvers.Mutation || {}),
   },
   Query: {
     ...queries,
+    ...(golomtResolvers.Query || {}),
   },
   ...apolloCustomScalars,
   ...customResolvers,
