5aafb53caeecb2cd43768781c42321028392268d
Zolb646 <zolb646@gmail.com>
2026-06-10 11:09:02 +0800

    sonar

--- frontend/plugins/mongolian_ui/src/modules/erkhet-sync/sync-erkhet-history/components/SyncErkhetHistoryColumns.tsx
+++ frontend/plugins/mongolian_ui/src/modules/erkhet-sync/sync-erkhet-history/components/SyncErkhetHistoryColumns.tsx
@@ -38,11 +38,12 @@ export const syncErkhetHistoryColumns: ColumnDef<ISyncHistory>[] = [
     header: () => (
       <RecordTable.InlineHead label="Created At" icon={IconCalendarPlus} />
     ),
-    cell: ({ cell }) => {
+    cell: ({ getValue }) => {
+      const createdAt = getValue() as string;
       return (
-        <RelativeDateDisplay value={cell.getValue() as string} asChild>
+        <RelativeDateDisplay value={createdAt} asChild>
           <RecordTableInlineCell className="text-xs font-medium text-muted-foreground">
-            <RelativeDateDisplay.Value value={cell.getValue() as string} />
+            <RelativeDateDisplay.Value value={createdAt} />
           </RecordTableInlineCell>
         </RelativeDateDisplay>
       );
