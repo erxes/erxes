fbfb48a8c1aec2097055f9219c9823211175c122
khuslen <huslens1027@gmail.com>
2026-06-16 18:13:40 +0800

    update

---
+++ frontend/core-ui/src/modules/products/settings/components/productsConfig/productRule/ProductRuleFilter.tsx
@@ -0,0 +1,76 @@
+import { Combobox, Command, Filter, useQueryState } from 'erxes-ui';
+import { SelectCategory, SelectProduct } from 'ui-modules';
+import { ProductRuleTotalCount } from './ProductRuleTotalCount';
+
+function CategoriesFilterBar() {
+  const [categoryIds] = useQueryState<string[]>('categoryIds');
+
+  if (!categoryIds?.length) {
+    return null;
+  }
+
+  return (
+    <SelectCategory.FilterBar
+      mode="multiple"
+      filterKey="categoryIds"
+      label="Category"
+    />
+  );
+}
+
+function ProductsFilterBar() {
+  const [productIds] = useQueryState<string[]>('productIds');
+
+  if (!productIds?.length) {
+    return null;
+  }
+
+  return (
+    <SelectProduct.FilterBar
+      mode="multiple"
+      filterKey="productIds"
+      label="Product"
+    />
+  );
+}
+
+export const ProductRuleFilter = () => {
+  return (
+    <Filter id="product-rule-filter">
+      <Filter.Bar>
+        <Filter.Popover>
+          <Filter.Trigger />
+          <Combobox.Content>
+            <Filter.View>
+              <Command>
+                <Filter.CommandInput placeholder="Filter" variant="secondary" />
+                <Command.List className="p-1">
+                  <Filter.SearchValueTrigger />
+                  <SelectCategory.FilterItem
+                    value="categoryIds"
+                    label="Category"
+                  />
+                  <SelectProduct.FilterItem
+                    value="productIds"
+                    label="Product"
+                  />
+                </Command.List>
+              </Command>
+            </Filter.View>
+            <SelectCategory.FilterView filterKey="categoryIds" mode="multiple" />
+            <SelectProduct.FilterView filterKey="productIds" mode="multiple" />
+          </Combobox.Content>
+        </Filter.Popover>
+        <Filter.Dialog>
+          <Filter.View filterKey="searchValue" inDialog>
+            <Filter.DialogStringView filterKey="searchValue" label="Name" />
+          </Filter.View>
+        </Filter.Dialog>
+        <Filter.SearchValueBarItem />
+        <CategoriesFilterBar />
+        <ProductsFilterBar />
+        <ProductRuleTotalCount />
+      </Filter.Bar>
+    </Filter>
+  );
+};
