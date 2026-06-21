01e37b5dc7862a6fc1307d45657f7523465c0f7f
Wlkr7575 <tuguldurch737@gmail.com>
2026-06-21 21:23:37 +0800

    fix: update export statements for TPlaceholderInputSuggestion and related types

--- frontend/libs/ui-modules/src/modules/automations/index.ts
+++ frontend/libs/ui-modules/src/modules/automations/index.ts
@@ -13,8 +13,8 @@ export * from './components/AutomationNodeMetaInfoRow';
 export * from './hooks/useFormValidationErrorHandler';
 export * from './utils/automationVariableDragUtils';
 export * from './utils/automationVariableBlockEditorUtils';
-export {
-  TPlaceholderInputSuggestion,
+export { TPlaceholderInputSuggestion } from './types/placeholderInputTypes';
+export type {
   TPlaceholderInputSuggestionsOption,
   TPlaceholderInputSuggestionType,
 } from './types/placeholderInputTypes';
