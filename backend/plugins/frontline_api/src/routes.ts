b17ab01c7c93981acb9bed90b1a1266c29f8cfee
dev@erxes.io <dev@erxes.io>
2026-06-23 16:41:54 +0800

    add integration route

--- backend/plugins/frontline_api/src/routes.ts
+++ backend/plugins/frontline_api/src/routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import { router as facebookRouter } from './modules/integrations/facebook/routes';
 import { router as instagramRouter } from './modules/integrations/instagram/routes';
-
+import { router as whatsappRouter } from './modules/integrations/whatsapp/routes';
 export const router: Router = Router();
 
 router.use('/facebook', facebookRouter);
 router.use('/instagram', instagramRouter);
+router.use('/whatsapp', whatsappRouter);
 
