376231b974368b094a63b258050d5ec162608d2b
Sekulya <secheikheno@gmail.com>
2026-07-21 19:45:45 +0800

    feat(clientportal): add cpDepartments query

--- backend/core-api/src/modules/organization/structure/graphql/resolvers/queries/deparments.ts
+++ backend/core-api/src/modules/organization/structure/graphql/resolvers/queries/deparments.ts
@@ -5,7 +5,7 @@ import {
 import { cursorPaginate } from 'erxes-api-shared/utils';
 import { IContext } from '~/connectionResolvers';
 import { generateFilters } from './utils';
-
+import { STRUCTURE_STATUSES } from 'erxes-api-shared/core-modules';
 export const deparmentQueries = {
   async departments(
     _parent: undefined,
@@ -54,5 +54,48 @@ export const deparmentQueries = {
 
   async departmentDetail(_parent: undefined, { _id }, { models }: IContext) {
     return models.Departments.getDepartment({ _id });
+  },
+
+  async cpDepartments(
+    _root,
+    params,
+    { models }: IContext,
+  ) {
+    const filter: any = {
+      status: { $ne: STRUCTURE_STATUSES.DELETED },
+    };
+
+    if (params?.ids?.length) {
+      filter._id = {
+        [params.excludeIds ? '$nin' : '$in']: params.ids,
+      };
+    }
+
+    if (params?.status) {
+      filter.status = params.status;
+    }
+
+    if (params?.onlyFirstLevel) {
+      filter.parentId = { $in: [null, ''] };
+    }
+
+    if (params?.parentId) {
+      filter.parentId = params.parentId;
+    }
+
+    if (params?.searchValue) {
+      const regex = {
+        $regex: `.*${params.searchValue.trim()}.*`,
+        $options: 'i',
+      };
+
+      filter.$or = [
+        { title: regex },
+        { description: regex },
+        { code: regex },
+      ];
+    }
+
+    return models.Departments.find(filter).sort({ order: 1 });
   },
 };
