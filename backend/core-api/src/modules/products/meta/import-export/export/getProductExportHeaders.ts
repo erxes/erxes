fdedacc9544b44e36ce91723af376f35e270ce5d
uuganaa1007 <suuganbayr948@gmail.com>
2026-07-27 00:19:57 +0800

    Revert "feat(core): product customProperty "
    This reverts commit e9250c9304b93db59278d771ea3255cf273a41d9.

--- backend/core-api/src/modules/products/meta/import-export/export/getProductExportHeaders.ts
+++ backend/core-api/src/modules/products/meta/import-export/export/getProductExportHeaders.ts
@@ -2,38 +2,27 @@ import {
   ImportHeaderDefinition,
   IImportExportContext,
 } from 'erxes-api-shared/core-modules';
-import { IModels } from '~/connectionResolvers';
-import { getCustomPropertyHeaders } from '~/meta/import-export/utils';
 
 export async function getProductExportHeaders(
   _data: any,
-  ctx: IImportExportContext<IModels>,
+  _ctx: IImportExportContext,
 ): Promise<ImportHeaderDefinition[]> {
-  const models = ctx?.models as IModels | undefined;
-
-  const customHeaders = models
-    ? await getCustomPropertyHeaders(models, 'core:product')
-    : [];
-
   return [
     { label: 'Name', key: 'name', isDefault: true },
     { label: 'Code', key: 'code', isDefault: true },
     { label: 'Unit Price', key: 'unitPrice', isDefault: true },
     { label: 'UOM', key: 'uom', isDefault: true },
-    { label: 'Category', key: 'categoryName' },
+    { label: 'Category', key: 'categoryId' },
     { label: 'Vendor', key: 'vendorId' },
-    { label: 'Brands', key: 'scopeBrandIds' },
-    { label: 'Tags', key: 'tags' },
+    { label: 'Brands', key: 'scopeBrandIds' }, 
+    { label: 'Tags', key: 'tagIds' }, 
     { label: 'Short Name', key: 'shortName' },
     { label: 'Type', key: 'type' },
     { label: 'Status', key: 'status' },
     { label: 'Description', key: 'description' },
     { label: 'Barcodes', key: 'barcodes' },
     { label: 'Barcode Description', key: 'barcodeDescription' },
-    { label: 'Image', key: 'imageUrl' },
-    { label: 'Additional Images', key: 'imageUrls' },
     { label: 'Created At', key: 'createdAt' },
     { label: 'Updated At', key: 'updatedAt' },
-    ...customHeaders,
   ];
 }
