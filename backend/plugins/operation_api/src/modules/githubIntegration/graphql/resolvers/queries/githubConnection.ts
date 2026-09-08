fb5a841c35cb92e5c34cd778cac6a97e30a91776
Munkhtenger <munkhtenger.aldar@gmail.com>
2026-09-08 23:28:19 +0200

    feat: Enhance GitHub integration with team-specific configurations and permissions

--- backend/plugins/operation_api/src/modules/githubIntegration/graphql/resolvers/queries/githubConnection.ts
+++ backend/plugins/operation_api/src/modules/githubIntegration/graphql/resolvers/queries/githubConnection.ts
@@ -5,20 +5,41 @@ export const githubConnectionQueries = {
   async getGithubConnection(
     _parent: undefined,
     _args: undefined,
-    { models, subdomain }: IContext,
+    { models, subdomain, checkPermission }: IContext,
   ) {
-    const connection = await models.GithubConnection.findOne({
+    await checkPermission('teamRead');
+
+    return models.GithubConnection.findOne({
       isActive: true,
       subdomain,
-    }).lean();
-    return connection;
+    })
+      .sort({ orgName: 1 })
+      .lean();
+  },
+
+  async getGithubConnections(
+    _parent: undefined,
+    _args: undefined,
+    { models, subdomain, checkPermission }: IContext,
+  ) {
+    await checkPermission('teamRead');
+
+    const connections = await models.GithubConnection.find({
+      isActive: true,
+      subdomain,
+    })
+      .sort({ orgName: 1 })
+      .lean();
+    return connections;
   },
 
   async getGithubRepositories(
     _parent: undefined,
     { installationId }: { installationId: number },
-    { models, subdomain }: IContext,
+    { models, subdomain, checkPermission }: IContext,
   ) {
+    await checkPermission('teamRead');
+
     const connection = await models.GithubConnection.findOne({
       installationId,
       subdomain,
