5e286f68d7529a6e54f1a4fc20502ef7b50f4af0
zolb646 <zolb646@gmail.com>
2026-09-17 15:24:30 +0800

    refactor(frontline): mount the mail composer as a floating widget on a generic event

--- frontend/plugins/frontline_ui/src/widgets/FloatingWidget.tsx
+++ frontend/plugins/frontline_ui/src/widgets/FloatingWidget.tsx
@@ -1,10 +1,12 @@
 import { CallWidget } from '@/integrations/call/components/CallWidget';
 import { SipContainer } from '@/integrations/call/components/SipContainer';
+import { DirectMailComposer } from '@/integrations/mail/components/DirectMailComposer';
 
 const FloatingWidget = () => {
   return (
     <SipContainer>
       <CallWidget />
+      <DirectMailComposer />
     </SipContainer>
   );
 };
