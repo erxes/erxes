3a0b3be32cd83a19db4191ae760de54b4d63ee67
munkhsaikhan <munkhsaikhan.g@nmma.co>
2026-09-18 02:32:20 +0800

    fix (accountings): reports improve (#9362)

--- frontend/plugins/accounting_ui/src/modules/journal-reports/components/includes/handlers/debt.ts
+++ frontend/plugins/accounting_ui/src/modules/journal-reports/components/includes/handlers/debt.ts
@@ -1,6 +1,11 @@
-import { HandleMainTB } from '../main/tb';
-import { CalcReportHandler } from '../types';
+import { HandleMainAC } from '../main/ac';
+import { HandleMainACMore } from '../main/acMore';
+import { CalcReportHandler, RenderMoreHandler } from '../types';
 
 export const debtCalcReportHandlers: Record<string, CalcReportHandler> = {
-  debt: HandleMainTB,
+  debt: HandleMainAC,
+};
+
+export const debtRenderMoreHandlers: Record<string, RenderMoreHandler> = {
+  debt: HandleMainACMore,
 };
